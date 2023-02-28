8e23e59cc018e2faf74b424233a14dc6aba8579d
shackstack <kms1061@naver.com>
2023-02-28 16:58:05 +0900

    feat: RestaurantList클래스 음식점 추가 기능 구현

---
+++ src/domain/RestaurantList.ts
@@ -0,0 +1,25 @@
+import Restaurant from "./Restaurant";
+
+type Category = "한식" | "중식" | "일식" | "아시안" | "양식" | "기타";
+
+type Distance = 5 | 10 | 15 | 20 | 30;
+
+interface RestaurantInfo {
+  name: string;
+  category: Category;
+  distance: Distance;
+  description?: string;
+  url?: string;
+}
+
+export default class RestaurantList {
+  #restaurantList: Restaurant[];
+
+  constructor() {
+    this.#restaurantList = [];
+  }
+
+  addRestaurant(info: RestaurantInfo): void {
+    this.#restaurantList.push(new Restaurant(info));
+  }
+}
