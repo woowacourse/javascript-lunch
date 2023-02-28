0f3f3da17f330ccdc34bdd959b39374213ccb9fb
NaveOWO <pianowell96@gmail.com>
2023-02-28 16:49:43 +0900

    test : 음식점 이름순 & 거리순 정렬 테스트

--- __tests__/Restaurants.test.ts
+++ __tests__/Restaurants.test.ts
@@ -1,52 +1,130 @@
-import { Restaurant, Category, Distance } from "../src/type/restaurant";
+import { Restaurant, Category, Distance, Cate } from "../src/type/restaurant";
 import Restaurants from "../src/domain/Restaurants";
 
 describe("레스토랑 테스트", () => {
+  const restaurants = new Restaurants();
+  const category: Category = "양식";
+  const restaurantList: Restaurant[] = [
+    {
+      name: "맥도날드",
+      category: category,
+      distance: 5,
+      description: "",
+      link: "",
+    },
+    {
+      name: "롯데리아",
+      category: category,
+      distance: 20,
+      description: "",
+      link: "",
+    },
+    {
+      name: "버거킹",
+      category: category,
+      distance: 30,
+      description: "",
+      link: "",
+    },
+    {
+      name: "맘스터치",
+      category: category,
+      distance: 10,
+      description: "",
+      link: "",
+    },
+    {
+      name: "KFC",
+      category: category,
+      distance: 15,
+      description: "",
+      link: "",
+    },
+  ];
+  restaurantList.forEach((restaurant: Restaurant) =>
+    restaurants.add(restaurant)
+  );
   test("음식점 목록 입력 테스트", () => {
-    const restaurants = new Restaurants();
-    const category: Category = "양식";
-    const distance: Distance = 10;
-    const restaurantList = [
+    expect(restaurants.getList()).toEqual(restaurantList);
+  });
+
+  test("음식점 목록 이름순 정렬 테스트", () => {
+    expect(restaurants.sortByName()).toEqual([
       {
-        name: "맥도날드",
+        name: "KFC",
         category: category,
-        distance: distance,
+        distance: 15,
         description: "",
         link: "",
       },
       {
         name: "롯데리아",
         category: category,
-        distance: distance,
-        description: "",
-        link: "",
-      },
-      {
-        name: "버거킹",
-        category: category,
-        distance: distance,
+        distance: 20,
         description: "",
         link: "",
       },
       {
         name: "맘스터치",
         category: category,
-        distance: distance,
+        distance: 10,
+        description: "",
+        link: "",
+      },
+      {
+        name: "맥도날드",
+        category: category,
+        distance: 5,
+        description: "",
+        link: "",
+      },
+      {
+        name: "버거킹",
+        category: category,
+        distance: 30,
+        description: "",
+        link: "",
+      },
+    ]);
+  });
+
+  test("음식점 목록 거리순 정렬 테스트", () => {
+    expect(restaurants.sortByDistance()).toEqual([
+      {
+        name: "맥도날드",
+        category: category,
+        distance: 5,
+        description: "",
+        link: "",
+      },
+      {
+        name: "맘스터치",
+        category: category,
+        distance: 10,
         description: "",
         link: "",
       },
       {
         name: "KFC",
         category: category,
-        distance: distance,
+        distance: 15,
         description: "",
         link: "",
       },
-    ];
-    restaurantList.forEach((restaurant: Restaurant) =>
-      restaurants.add(restaurant)
-    );
-
-    expect(restaurants.getList()).toEqual(restaurantList);
+      {
+        name: "롯데리아",
+        category: category,
+        distance: 20,
+        description: "",
+        link: "",
+      },
+      {
+        name: "버거킹",
+        category: category,
+        distance: 30,
+        description: "",
+        link: "",
+      },
+    ]);
   });
 });
