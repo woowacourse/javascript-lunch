0654b4eb0f5da728f4fb88b81d7a9a80d6a5a413
shackstack <kms1061@naver.com>
2023-02-28 17:09:48 +0900

    test: 음식점 목록을 이름순으로 정렬하는 기능에 대한 테스트

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -1,4 +1,5 @@
 import Restaurant from "../src/domain/Restaurant";
+import RestaurantList from "../src/domain/RestaurantList";
 
 describe("Restaurant 클래스 테스트", () => {
   test("Restaurant 객체를 생성하면 해당 정보를 저장한다.", () => {
@@ -17,5 +18,38 @@ describe("Restaurant 클래스 테스트", () => {
 
     //then
     expect(restaurantInfo).toEqual(data);
+  });
+});
+
+describe("RestaurantList 클래스 테스트", () => {
+  test("음식점들을 이름순으로 정렬해서 가져온다.", () => {
+    //given
+    const restaurantList = new RestaurantList();
+
+    //when
+    restaurantList.addRestaurant({
+      name: "나식당",
+      category: "한식",
+      distance: 5,
+    });
+    restaurantList.addRestaurant({
+      name: "가식당",
+      category: "중식",
+      distance: 10,
+    });
+    restaurantList.addRestaurant({
+      name: "다식당",
+      category: "일식",
+      distance: 15,
+    });
+
+    const sortedList = restaurantList.getSortedListByName();
+
+    //then
+    expect(sortedList.map((restaurant) => restaurant.getInfo().name)).toEqual([
+      "가식당",
+      "나식당",
+      "다식당",
+    ]);
   });
 });
