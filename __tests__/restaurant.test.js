5340c9626f517a8557d450a15764dbfa6921c40d
Suyoung <jsy8492@gmail.com>
2023-02-28 14:45:06 +0900

    feat: 음식점 목록을 카테고리별로 필터링하는 기능 및 테스트 작성

--- __tests__/restaurant.test.js
+++ __tests__/restaurant.test.js
@@ -13,3 +13,34 @@ test('새로운 음식점을 추가한다.', () => {
 
   expect(restaurants.list).toEqual([newRestaurant]);
 });
+
+test('음식점들을 카테고리별로 필터링한다.', () => {
+  const category = '일식';
+  const restaurantList = [
+    {
+      category: '일식',
+      name: '돈카라',
+      distance: 5,
+      description: '상세 설명',
+      link: 'link',
+    },
+    {
+      category: '한식',
+      name: '김돈이',
+      distance: 5,
+      description: '상세 설명',
+      link: 'link',
+    },
+    {
+      category: '일식',
+      name: '지구당',
+      distance: 5,
+      description: '상세 설명',
+      link: 'link',
+    },
+  ];
+  restaurantList.forEach((restaurant) => restaurants.add(restaurant));
+  const filteredRestaurants = restaurants.filterByCategory(category);
+
+  expect(filteredRestaurants.every((restaurant) => restaurant.category === category)).toBe(true);
+});
