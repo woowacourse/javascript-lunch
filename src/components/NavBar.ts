1fb402cec72ca403f8c3d7d6e88846517f1fa8af
kangyeongmin <kym6050@hanyang.ac.kr>
2023-02-28 17:39:53 +0900

    feat(Component): AddRestaurant 구현

--- src/components/NavBar.ts
+++ src/components/NavBar.ts
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return `
     <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-    <button type="button" class="gnb__button" aria-label="음식점 추가">
+    <button id="addButton" type="button" class="gnb__button" aria-label="음식점 추가">
       <img src="../templates/add-button.png" alt="음식점 추가">
     </button>
   `;
