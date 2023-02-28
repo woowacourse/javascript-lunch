3a212cc2e351f02b9a010ebfc271377d2ccfa975
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-28 18:05:29 +0900

    test: 이름 유효성 검사 기능 테스트 케이스 추가
    빈 입력 검사 기능에 대한 테스트 케이스 추가

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -5,3 +5,21 @@ test('정해진 카테고리 외의 카테고리 값인 경우, 에러가 발생
 
   expect(() => Validator.validateCategory(selectedCategory)).toThrow();
 });
+
+describe('이름 입력 유효성 검사', () => {
+  test('입력값이 비어있으면, true를 반환한다.', () => {
+    const rastaurantName = '';
+
+    const expected = Validator.isEmpty(rastaurantName);
+
+    expect(expected).toBe(true);
+  });
+
+  test('입력값이 존재하면, false를 반환한다.', () => {
+    const rastaurantName = '돈카라';
+
+    const expected = Validator.isEmpty(rastaurantName);
+
+    expect(expected).toBe(false);
+  });
+});
