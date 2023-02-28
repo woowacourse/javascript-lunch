fde745f680c79e3f2e7fe0bdeaba23dc1473f2da
chex <chex417@naver.com>
2023-02-28 18:01:07 +0900

    chore: 카테고리, 거리 타입 추가

--- src/domains/types.ts
+++ src/domains/types.ts
@@ -1,7 +1,10 @@
-export type RestaurantType = {
-  category: string;
+export type Category = "한식" | "중식" | "일식" | "아시안" | "양식" | "기타";
+export type Distance = 5 | 10 | 15 | 20 | 30;
+
+export type Restaurant = {
+  category: Category;
   name: string;
-  distance: number;
-  description: string;
-  link: string;
+  distance: Distance;
+  description?: string;
+  link?: string;
 };
