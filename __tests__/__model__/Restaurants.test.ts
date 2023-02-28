55f43f41105f1a7d02c30a692c6d4a64a32d13ff
n0eyes <doll4586633@naver.com>
2023-02-28 17:21:46 +0900

    feat: 음식점 목록 필터링 기능 추가

--- __tests__/__model__/Restaurants.test.ts
+++ __tests__/__model__/Restaurants.test.ts
@@ -10,6 +10,9 @@ const getDummyInfo = (category: Category) => {
   };
 };
 
+const categoryList: Category[] = ['한식', '중식', '일식', '양식', '기타'];
+const restaurantList = categoryList.map((category) => new Restaurant(getDummyInfo(category)));
+
 describe('주어진 정보로 생성된 음식점 모델 테스트', () => {
   let restaurant: Restaurant;
   let restaurants: Restaurants;
@@ -17,6 +20,7 @@ describe('주어진 정보로 생성된 음식점 모델 테스트', () => {
 
   beforeEach(() => {
     correctInfo = getDummyInfo('한식');
+    restaurants = new Restaurants(restaurantList);
     restaurant = new Restaurant(correctInfo);
   });
   test('음식점은 카테고리, 이름, 거리정보를 필수로 가져야 한다', () => {
@@ -40,5 +44,13 @@ describe('주어진 정보로 생성된 음식점 모델 테스트', () => {
     restaurants.add(restaurant);
 
     expect(restaurants.getList().length).toBe(6);
+  });
+
+  test('음식점 목록을 카테고리에 따라 필터링이 가능하다', () => {
+    const filteredRestaurants = restaurants.filterByCategory('한식');
+
+    filteredRestaurants.forEach((restaurant) => {
+      expect(restaurant.getSomeInfo('category')).toBe('한식');
+    });
   });
 });
