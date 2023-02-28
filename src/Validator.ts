374f49ceee334e8282ff1cb36887bac2ce1e183a
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-02-28 17:30:32 +0900

    feat: 카테고리 유효성 검사 기능 구현

---
+++ src/Validator.ts
@@ -0,0 +1,11 @@
+const categories = ['한식', '중식', '일식', '아시안', '양식', '기타'];
+
+const Validator = {
+  validateCategory(categoryOption: string) {
+    if (!categories.includes(categoryOption)) {
+      throw new Error('[ERROR] 알 수 없는 카테고리입니다');
+    }
+  },
+};
+
+export default Validator;
