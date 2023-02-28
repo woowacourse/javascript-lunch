9bacc74bdc588169cea697b4d279e074df68e0d5
ukkodeveloper <ukkodeveloper@gmail.com>
2023-02-28 17:49:24 +0900

    refactor: validateNamInput 메서드 파라미터명 변경

--- src/model/validator/restaurantInputValidator.ts
+++ src/model/validator/restaurantInputValidator.ts
@@ -2,8 +2,8 @@ import { errorMessage } from '@res/constants/messages';
 import { validatorUtils } from './validatorUtils';
 
 export const restaurantInputValidator = {
-  validateNameInput(nameInput: string): void {
-    if (validatorUtils.isNameLengthValidRange(nameInput)) {
+  validateNameInput(input: string): void {
+    if (validatorUtils.isNameLengthValidRange(input)) {
       throw new Error(errorMessage.nameInputLength);
     }
   },
