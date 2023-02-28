13630dd06169c99d405cc3099afa594f3c89d86d
Suyoung <jsy8492@gmail.com>
2023-02-28 15:22:56 +0900

    feat: ListItem 컴포넌트 및 테스트 작성

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,3 +1,21 @@
 /**
  * @jest-environment jsdom
  */
+
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/dom';
+import { ListItem } from '../src/view/ListItem';
+
+test('화면에 ListItem을 렌더링한다.', () => {
+  const listItem = ListItem({
+    category: '일식',
+    name: '지구당',
+    distance: 5,
+    description: '상세 설명',
+    link: 'link',
+  });
+
+  document.body.insertAdjacentHTML('beforeend', listItem);
+
+  expect(screen.getByText('지구당')).toBeInTheDocument();
+});
