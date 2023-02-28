0c44a1861f62171773b89ec1480ea1b109e75d6b
kyw0716 <kyw0716@naver.com>
2023-02-28 15:22:05 +0900

    feat: Restaurants 클래스 새로운 음식점 추가 메서드 구현

---
+++ src/domain/Restaurants.ts
@@ -0,0 +1,28 @@
+type Category = "한식" | "중식" | "일식" | "아시안" | "양식" | "기타";
+type Distance = 5 | 10 | 15 | 20 | 30;
+
+export interface Restaurant {
+  category: Category;
+  name: string;
+  distance: Distance;
+  description?: string;
+  link?: string;
+}
+
+class Restaurants {
+  #list;
+
+  constructor(list: Restaurant[]) {
+    this.#list = list;
+  }
+
+  getList() {
+    return this.#list;
+  }
+
+  add(restaurant: Restaurant) {
+    this.#list = [...this.#list, restaurant];
+  }
+}
+
+export default Restaurants;
