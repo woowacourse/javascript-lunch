c30d8f035c869f93b5348c9306d64c5970cd60ed
jeongwusi <jeongwusi@gmail.com>
2023-03-01 16:58:33 +0900

    ✨✨ feat: 이미지 경로 수정 후 이미지 상수 파일 생성

---
+++ src/img/images.js
@@ -0,0 +1,19 @@
+import plusImage from './add-button.png';
+import categoryAsian from './category-asian.png';
+import categoryChinese from './category-chinese.png';
+import categoryJapanese from './category-japanese.png';
+import categoryKorean from './category-korean.png';
+import categoryWestern from './category-western.png';
+import categoryEtc from './category-etc.png';
+
+const images = {
+  추가버튼: plusImage,
+  아시안: categoryAsian,
+  중식: categoryChinese,
+  일식: categoryJapanese,
+  한식: categoryKorean,
+  양식: categoryWestern,
+  기타: categoryEtc,
+};
+
+export default images;
