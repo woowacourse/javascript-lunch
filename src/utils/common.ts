6372623b8eb3fcd1c71c083f1eb81961e3257299
unknown <tkdrb15@gmail.com>
2023-03-01 15:20:50 +0900

    feat: 문자열이 빈 문자열인지 유효성을 검사하는 기능을 추가하다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,3 +3,9 @@ export const compareString = (a: string, b: string): number => {
   if (a < b) return -1;
   return 0;
 };
+
+export const validateBlankString = (str: string): boolean => {
+  if (str.trim().length === 0) return false;
+
+  return true;
+};
