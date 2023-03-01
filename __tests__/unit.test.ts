ce36f4c551dd8feb49a5bd7cecd2bd39ea40416c
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-01 14:16:27 +0900

    test(Validator): isValidCategory 메서드 분리로 인한 테스트케이스 추가

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -1,5 +1,23 @@
 import Validator from '../src/Validator';
 
+describe('Validator.isValidCategory', () => {
+  test('정해진 카테고리 외의 값을 선택한 경우, false를 반환한다.', () => {
+    const selectedCategory = '인도식';
+
+    const expected = Validator.isValidCategory(selectedCategory);
+
+    expect(expected).toBe(false);
+  });
+
+  test('정해진 카테고리 내의 값을 선택한 경우, true를 반환한다.', () => {
+    const selectedCategory = '한식';
+
+    const expected = Validator.isValidCategory(selectedCategory);
+
+    expect(expected).toBe(true);
+  });
+});
+
 test('정해진 카테고리 외의 카테고리 값인 경우, 에러가 발생한다.', () => {
   const selectedCategory = '인도식';
 
