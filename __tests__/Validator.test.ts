c8010d7ffedeb465f4eca8fe82b0d293efef03f5
turtle601 <turtle601@naver.com>
2023-03-01 11:34:27 +0900

    style: .prettierrc 수정으로 인한 코드 스타일 변경

--- __tests__/Validator.test.ts
+++ __tests__/Validator.test.ts
@@ -1,37 +1,37 @@
-import Validator from "../src/domain/Validator";
+import Validator from '../src/domain/Validator';
 
-describe("카테고리 유효성 검사", () => {
-  test.each([["몰?루"], [""], ["한식1"], [" 한식"]])(
-    "(실패 사례) 카테고리 입력값 %s 유효성 검사",
+describe('카테고리 유효성 검사', () => {
+  test.each([['몰?루'], [''], ['한식1'], [' 한식']])(
+    '(실패 사례) 카테고리 입력값 %s 유효성 검사',
     (input) => {
-      expect(() => Validator.checkCategory(input)).toThrow("에러 1");
+      expect(() => Validator.checkCategory(input)).toThrow('에러 1');
     }
   );
 
-  test.each([["한식"], ["중식"], ["일식"], ["아시안"], ["양식"], ["기타"]])(
-    "(성공 사례) 카테고리 입력값 %s 유효성 검사",
+  test.each([['한식'], ['중식'], ['일식'], ['아시안'], ['양식'], ['기타']])(
+    '(성공 사례) 카테고리 입력값 %s 유효성 검사',
     (input) => {
       expect(() => Validator.checkCategory(input)).not.toThrow();
     }
   );
 });
 
-describe("이름 유효성 검사", () => {
-  test.each([[""], ["    "]])(
-    "(실패 사례) 이름 입력값 %s 유효성 검사",
+describe('이름 유효성 검사', () => {
+  test.each([[''], ['    ']])(
+    '(실패 사례) 이름 입력값 %s 유효성 검사',
     (input) => {
-      expect(() => Validator.checkName(input)).toThrow("에러 2");
+      expect(() => Validator.checkName(input)).toThrow('에러 2');
     }
   );
 
   test.each([
-    ["푸만능"],
-    ["ChatGPT"],
-    ["요술 토끼"],
-    ["123"],
-    ["몰★루"],
-    ["/?! 0%vZ _w"],
-  ])("(성공 사례) 이름 입력값 %s 유효성 검사", (input) => {
+    ['푸만능'],
+    ['ChatGPT'],
+    ['요술 토끼'],
+    ['123'],
+    ['몰★루'],
+    ['/?! 0%vZ _w'],
+  ])('(성공 사례) 이름 입력값 %s 유효성 검사', (input) => {
     expect(() => Validator.checkName(input)).not.toThrow();
   });
 });
--- src/domain/Validator.ts
+++ src/domain/Validator.ts
@@ -1,16 +1,16 @@
 const RESTAURANT_CATEGORIES = [
-  "한식",
-  "중식",
-  "일식",
-  "아시안",
-  "양식",
-  "기타",
+  '한식',
+  '중식',
+  '일식',
+  '아시안',
+  '양식',
+  '기타',
 ];
 
 const Validator = {
   checkCategory: (category: string) => {
     if (!Validator.isRestaurantCategory(category)) {
-      throw new Error("에러 1");
+      throw new Error('에러 1');
     }
   },
 
@@ -20,7 +20,7 @@ const Validator = {
 
   checkName: (name: string) => {
     if (Validator.isBlank(name.trim())) {
-      throw new Error("에러 2");
+      throw new Error('에러 2');
     }
   },
 
