6f79a8bf4fd1f58038c99154af6840a77f54c06f
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-01 14:04:53 +0900

    test(Validator): isEmpty 메서드 시그니처 및 사용처 변경으로 테스트 케이스 변경

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -6,19 +6,17 @@ test('정해진 카테고리 외의 카테고리 값인 경우, 에러가 발생
   expect(() => Validator.validateCategory(selectedCategory)).toThrow();
 });
 
-describe('이름 입력 유효성 검사', () => {
-  test('입력값이 비어있으면, true를 반환한다.', () => {
-    const rastaurantName = '';
+describe('Validator.isEmptyFormValue', () => {
+  test('form value가 빈 문자열이면, true를 반환한다.', () => {
+    const formValue = '';
 
-    const expected = Validator.isEmpty(rastaurantName);
+    const expected = Validator.isEmptyFormValue(formValue);
 
     expect(expected).toBe(true);
   });
 
-  test('입력값이 존재하면, false를 반환한다.', () => {
-    const rastaurantName = '돈카라';
-
-    const expected = Validator.isEmpty(rastaurantName);
+  test.each(['일식', '돈카라', '5'])('form value가 존재하면, false를 반환한다.', (formValue) => {
+    const expected = Validator.isEmptyFormValue(formValue);
 
     expect(expected).toBe(false);
   });
