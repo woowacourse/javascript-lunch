4addb424e875669f1ad67b558fd313d12492f29e
chex <chex417@naver.com>
2023-03-01 16:45:35 +0900

    feat: restaurantItem 컴포넌트 추가

---
+++ src/constants/constants.ts
@@ -0,0 +1,8 @@
+export const RESTAURANT_IMAGE = {
+  한식: "./assets/category-korean.png",
+  중식: "./assets/category-chinese.png",
+  일식: "./assets/category-japanese.png",
+  아시안: "./assets/category-asian.png",
+  양식: "./assets/category-western.png",
+  기타: "./assets/category-etc.png",
+};
