d9269dd8d22a6c9e03acb2afb0c40abd86220a5f
turtle601 <turtle601@naver.com>
2023-03-01 11:24:04 +0900

    test(Validator): 이름 입력값 유효성 검사 테스트 작성

--- __tests__/Validator.test.ts
+++ __tests__/Validator.test.ts
@@ -16,3 +16,22 @@ describe("카테고리 유효성 검사", () => {
   );
 });
 
+describe("이름 유효성 검사", () => {
+  test.each([[""], ["    "]])(
+    "(실패 사례) 이름 입력값 %s 유효성 검사",
+    (input) => {
+      expect(() => Validator.checkName(input)).toThrow("에러 2");
+    }
+  );
+
+  test.each([
+    ["푸만능"],
+    ["ChatGPT"],
+    ["요술 토끼"],
+    ["123"],
+    ["몰★루"],
+    ["/?! 0%vZ _w"],
+  ])("(성공 사례) 이름 입력값 %s 유효성 검사", (input) => {
+    expect(() => Validator.checkName(input)).not.toThrow();
+  });
+});
