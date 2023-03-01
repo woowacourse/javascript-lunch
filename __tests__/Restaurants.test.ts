ada1764936af57415520b8ef8a61c1cdabfc6d97
turtle601 <turtle601@naver.com>
2023-03-01 15:15:07 +0900

    test(Restaurants): 음식점을 이름으로 정렬하는 기능 테스트 작성

--- __tests__/Restaurants.test.ts
+++ __tests__/Restaurants.test.ts
@@ -7,11 +7,37 @@ const menu1 = {
   description: '너무 맛있어',
   link: 'https://machogalbi.com',
 };
+
+const menu2 = {
+  category: '한식',
+  name: '마초갈비',
+  distance: '20',
+  description: '너무 맛있어',
+  link: 'https://machogalbi.com',
+};
+
+const menu3 = {
+  category: '중식',
+  name: '간다간다뿅간다 짜장',
+  distance: '30',
+  description: '너무 맛있어',
+  link: 'https://machogalbi.com',
+};
+
 describe('Restaurants 클래스 테스트', () => {
   test('addRestaurant 메서드 테스트', () => {
     const restaurants = new Restaurants();
     restaurants.addRestaurant(menu1);
 
     expect(restaurants.getRestaurants()).toEqual([menu1]);
+  });
+
+  test('sortByName 메서드 테스트', () => {
+    const restaurants = new Restaurants();
+    restaurants.addRestaurant(menu1);
+    restaurants.addRestaurant(menu2);
+    restaurants.addRestaurant(menu3);
+
+    expect(restaurants.sortByName()).toEqual([menu3, menu2, menu1]);
   });
 });
