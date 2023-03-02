9669272b42c6bf7bfe14d914d2d590ddc8db799a
NaveOWO <pianowell96@gmail.com>
2023-03-02 15:17:12 +0900

    test : 모달창 열고 닫는 함수 테스트

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,3 +1,26 @@
 /**
  * @jest-environment jsdom
  */
+
+import { screen, fireEvent } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+import Modal from "../src/util/Modal";
+
+document.body.innerHTML = `<div class="modal" role="modal">
+<div class="modal-backdrop" ></div>
+<div class="modal-container"></div>
+</div>`;
+
+const modal = new Modal();
+
+test("모달창 열기 테스트", () => {
+    const myModal = screen.getByRole("modal");
+    modal.open();
+    expect(myModal.classList.contains('modal--open')).toBe(true);
+});
+
+test("모달창 닫기 테스트", () => {
+    const myModal = screen.getByRole("modal");
+    modal.close();
+    expect(myModal.classList.contains('modal--open')).toBe(false);
+});
