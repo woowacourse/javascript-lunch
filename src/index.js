45a85086ec50e4bb7c232a088bd42aeaf1e64e9c
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 17:51:22 +0900

    feat(index.js): RestaurantList 구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import AddRestaurant from "./components/AddRestaurant";
 import BottomSheet from "./components/BottomSheet";
 import NavBar from "./components/NavBar";
+import RestaurantList from "./components/RestaurantList";
 import "./css/style.css";
 import Controller from "./domain/Controller";
 
@@ -10,3 +11,4 @@ globalThis.controller = controller;
 customElements.define("nav-bar", NavBar);
 customElements.define("bottom-sheet", BottomSheet);
 customElements.define("add-restaurant", AddRestaurant);
+customElements.define("restaurant-list", RestaurantList);
