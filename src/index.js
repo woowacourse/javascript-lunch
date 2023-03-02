155f2a34e7782aebae653b8e573ee18abb630b21
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 21:48:48 +0900

    feat(index.js): SelectingBox들 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import AddRestaurant from "./components/AddRestaurant";
 import BottomSheet from "./components/BottomSheet";
+import CategorySelectBox from "./components/CategorySelectBox";
 import NavBar from "./components/NavBar";
 import RestaurantList from "./components/RestaurantList";
+import SortingSelectBox from "./components/SortingSelectBox";
 import "./css/style.css";
 import Controller from "./domain/Controller";
 
@@ -10,5 +12,7 @@ globalThis.controller = controller;
 
 customElements.define("nav-bar", NavBar);
 customElements.define("bottom-sheet", BottomSheet);
+customElements.define("category-select-box", CategorySelectBox);
+customElements.define("sorting-select-box", SortingSelectBox);
 customElements.define("add-restaurant", AddRestaurant);
 customElements.define("restaurant-list", RestaurantList);
