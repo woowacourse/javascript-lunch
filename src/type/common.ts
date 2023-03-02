a949839782705e939770befee45c2fd9776b0d92
turtle601 <turtle601@naver.com>
2023-03-02 18:51:20 +0900

    feat(type): RestaurantCategoryType, RestaurnatSortingType 생성

--- src/type/common.ts
+++ src/type/common.ts
@@ -1,9 +1,20 @@
 type Restaurant = {
-  category: '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+  category: RestaurantCategoryType;
   name: string;
   distance: string;
   description: string;
   link: string;
 };
 
-export { Restaurant };
+type RestaurantCategoryType =
+  | '전체'
+  | '한식'
+  | '중식'
+  | '일식'
+  | '아시안'
+  | '양식'
+  | '기타';
+
+type RestaurantSortingType = 'name' | 'distance';
+
+export { Restaurant, RestaurantCategoryType, RestaurantSortingType };
