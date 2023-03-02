fc9fc208f1f311054b0988f72a03f0dc92954366
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 16:51:46 +0900

    feat(index.js): controller 호출 로직 구현

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import AddRestaurant from "./components/AddRestaurant";
 import BottomSheet from "./components/BottomSheet";
 import NavBar from "./components/NavBar";
 import "./css/style.css";
+import Controller from "./domain/Controller";
+
+const controller = new Controller();
+globalThis.controller = controller;
 
 customElements.define("nav-bar", NavBar);
 customElements.define("bottom-sheet", BottomSheet);
+customElements.define("add-restaurant", AddRestaurant);
