251ec75125859af1692070f8b3108443f875a973
NaveOWO <pianowell96@gmail.com>
2023-03-02 17:51:52 +0900

    test : 이름 및 링크 유효성 검사 테스트

---
+++ __tests__/Validator.test.ts
@@ -0,0 +1,27 @@
+import Validator from "../src/Validator";
+
+test("이름이 공백일 때 true를 반환하는 함수 테스트", () => {
+  expect(Validator.isEmptyName("")).toBeTruthy();
+});
+
+test("이름이 공백이 아닐 때 false를 반환하는 함수 테스트", () => {
+  expect(Validator.isEmptyName("역전우동")).toBeFalsy();
+});
+
+test("링크가 형식에 맞을 때 true를 반환하는 함수 테스트", () => {
+  expect(Validator.isCorrectLink("https://www.woowa.io")).toBeTruthy();
+});
+
+test("링크가 형식에 맞을 때 true를 반환하는 함수 테스트", () => {
+  expect(
+    Validator.isCorrectLink("https://www.woowa.io/qwer/asdf")
+  ).toBeTruthy();
+});
+
+test("링크에 http(s)가 없을 때 false를 반환하는 함수 테스트", () => {
+  expect(Validator.isCorrectLink("www.woowa.io")).toBeFalsy();
+});
+
+test("링크에 최상위 도메인명이 없을 때 false를 반환하는 함수 테스트", () => {
+  expect(Validator.isCorrectLink("https://woowa")).toBeFalsy();
+});
