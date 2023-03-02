fa5831d006e21a7a0ac57e98c0931e862413776c
Dain Lee <feb.dain@gmail.com>
2023-03-02 20:07:32 +0900

    feat: 모달창 닫으면 인풋값 초기화하는 기능 추가

--- src/ui/modal.ts
+++ src/ui/modal.ts
@@ -2,6 +2,9 @@ import { $ } from "../util/selector";
 
 export const handleModalCancelButtonClick = (selector: string) => {
   $(selector)?.classList.remove("modal--open");
+
+  const newRestaurantInputs = $("#new-restaurant-form") as HTMLFormElement;
+  newRestaurantInputs.reset();
 };
 
 export const handleModalOpenButtonClick = (selector: string) => {
