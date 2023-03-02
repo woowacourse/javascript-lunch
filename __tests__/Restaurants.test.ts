437fee39c55da2fb5df6cfa60c3fa3ed1753916a
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-02 17:27:58 +0900

    test(Restaurants): 음식점 카테고리별 필터링 메서드에 대한 테스트 케이스 추가

--- __tests__/Restaurants.test.ts
+++ __tests__/Restaurants.test.ts
@@ -15,3 +15,47 @@ test('Restaurants.addRestaurant', () => {
 
   expect(addedRestaurant).toEqual(restaurant);
 });
+
+describe('Restaurants.getFilteredRestaurantsByCategory', () => {
+  test('선택된 카테고리와 일치하는 음식점의 목록을 반환한다.', () => {
+    const dummy: Restaurant[] = [
+      {
+        category: '한식',
+        name: '맛이쪙 돈까스',
+        distance: '10',
+        description: '평양 출신의 할머니가 수십 년간 운영해온 비지 전문점',
+        link: 'www.naver.com',
+      },
+      {
+        category: '한식',
+        name: '김치찌개 장인',
+        distance: '5',
+      },
+      {
+        category: '일식',
+        name: '돈카라',
+        distance: '5',
+      },
+    ];
+    const restaurants = new Restaurants();
+    dummy.forEach((v) => restaurants.addRestaurant(v));
+
+    const filterdRestaurants = restaurants.getFilteredRestaurantsByCategory('한식');
+    const expected: Restaurant[] = [
+      {
+        category: '한식',
+        name: '맛이쪙 돈까스',
+        distance: '10',
+        description: '평양 출신의 할머니가 수십 년간 운영해온 비지 전문점',
+        link: 'www.naver.com',
+      },
+      {
+        category: '한식',
+        name: '김치찌개 장인',
+        distance: '5',
+      },
+    ];
+
+    expect(expected).toEqual(filterdRestaurants);
+  });
+});
