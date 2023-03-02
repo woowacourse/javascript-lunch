b240a38dfa2b979c645b4c65a757b3aec9f93555
n0eyes <doll4586633@naver.com>
2023-03-02 19:49:18 +0900

    feat: Event 관리 사이클 함수 추가

--- src/utils/core/index.ts
+++ src/utils/core/index.ts
@@ -1,8 +1,16 @@
 import { debounce } from '../common/debounce';
 
+interface Event {
+  $parent: string;
+  $target: string;
+  event: keyof HTMLElementEventMap;
+  callback: (this: Element, e: HTMLElementEventMap[Event['event']]) => void;
+}
 interface Options<T = unknown> {
+  currentEventKey: number;
   currentStateKey: number;
   states: T[];
+  events: Event[];
   root: null | HTMLElement;
   rootComponent: null | (() => string);
 }
@@ -12,8 +20,10 @@ type UnPack<T> = T extends (infer U)[] ? U : T;
 
 function Core() {
   const options: Options<UnPack<Parameters<typeof useState>>> = {
+    currentEventKey: 0,
     currentStateKey: 0,
     states: [],
+    events: [],
     root: null,
     rootComponent: null,
   };
@@ -39,7 +49,31 @@ function Core() {
     if (!root || !rootComponent) return;
     root.innerHTML = rootComponent();
     options.currentStateKey = 0;
+
+    _addEvent();
+    options.currentEventKey = 0;
   });
+
+  const $ = (t: string) => document.querySelector(t);
+  const $$ = (selector: string, target: ParentNode | null = document) =>
+    target ? target.querySelectorAll(selector) : target;
+
+  function useEvents($parent: string) {
+    function addEvent(
+      event: Event['event'],
+      $target: Event['$target'],
+      callback: Event['callback']
+    ) {
+      const { currentEventKey: key, events } = options;
+
+      if (events.length === key) {
+        events.push({ event, $target, $parent, callback });
+        options.currentEventKey += 1;
+      }
+    }
+
+    return [addEvent];
+  }
 
   function render(rootComponent: Options['rootComponent'], root: Options['root']) {
     options.root = root;
@@ -47,7 +81,25 @@ function Core() {
     _render();
   }
 
-  return { useState, render };
+  function _addEvent() {
+    options.events.forEach(({ $parent, $target, event, callback }) => {
+      const children = $$($target, $($parent));
+      const isTarget = (target: EventTarget | null) => {
+        if (target instanceof Element && children) {
+          return [...children].includes(target) || target.closest($target);
+        }
+
+        return false;
+      };
+
+      $($parent)?.addEventListener(event, (e) => {
+        const parent = $($parent);
+        if (isTarget(e.target) && parent) callback.call(parent, e);
+      });
+    });
+  }
+
+  return { useState, useEvents, render };
 }
 
-export const { useState, render } = Core();
+export const { useState, useEvents, render } = Core();
