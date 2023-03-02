188ab64760b15b5df6afdb3400c1d1d00bf20a76
turtle601 <turtle601@naver.com>
2023-03-02 18:56:51 +0900

    feat(Selector): 카테고리, 이름, 거리순으로 정렬하는 Selector 컴포넌트 구현

---
+++ src/view/components/Selector.ts
@@ -0,0 +1,66 @@
+import {
+  RestaurantCategoryType,
+  RestaurantSortingType,
+} from '../../type/common';
+
+import { $ } from '../../utils/querySelector';
+
+type SelectorProps = {
+  $target: HTMLElement;
+  info: {
+    name: 'category' | 'sorting';
+    id: 'category-filter' | 'sorting-filter';
+    options: { value: string; name: string }[];
+    selected: RestaurantCategoryType | RestaurantSortingType;
+  };
+  onChangeEvent: (value: string) => void;
+};
+
+class Selector {
+  #target;
+  #info;
+  #onChangeEvent;
+
+  constructor({ $target, info, onChangeEvent }: SelectorProps) {
+    this.#target = $target;
+    this.#info = info;
+    this.#onChangeEvent = onChangeEvent;
+
+    this.render();
+    this.#setEvent();
+  }
+
+  #template() {
+    return `
+      <select name="${this.#info.name}" id="${
+      this.#info.id
+    }" class="restaurant-filter">
+        ${this.#info.options
+          .map(
+            (option) =>
+              `<option value="${option.value}" ${
+                this.#info.selected === option.value ? 'selected' : ''
+              }>${option.name}</option>`
+          )
+          .join('')}
+      </select>
+    `;
+  }
+
+  render() {
+    this.#target.innerHTML += this.#template();
+  }
+
+  #setEvent() {
+    this.#target.addEventListener('change', (e: Event) => {
+      if (
+        e.target instanceof HTMLSelectElement &&
+        e.target.closest(`#${this.#info.id}`)
+      ) {
+        this.#onChangeEvent(e.target.value);
+      }
+    });
+  }
+}
+
+export default Selector;
