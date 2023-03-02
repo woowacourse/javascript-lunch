e220a7ac05cc312e1f4594163d1e0584061154ac
gyeongza <gyeongza@naver.com>
2023-03-02 18:11:27 +0900

    feat(CategoryBox): 카테고리 컴포넌트 구현
    Co-authored-by: Gilpop8663 <wolfye@naver.com>

---
+++ src/components/CategoryBox.js
@@ -0,0 +1,65 @@
+import GLOBAL_CSS from '../constants';
+
+class CategoryBox extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  createOption(title) {
+    return `<option value="${title}">${title}</option>`;
+  }
+
+  connectedCallback() {
+    const globalStyle = document.createElement('style');
+    const componentStyle = document.createElement('style');
+    globalStyle.textContent = GLOBAL_CSS;
+    componentStyle.textContent = `
+    select {
+    height: 44px;
+    min-width: 125px;
+  
+    border: 1px solid #d0d5dd;
+    border-radius: 8px;
+    background: transparent;
+  
+    font-size: 16px;
+  }
+  
+  .restaurant-filter {
+    padding: 8px;
+  }
+`;
+
+    const template = document.createElement('template');
+
+    const name = this.getAttribute('name');
+    const id = this.getAttribute('id');
+    const optionsAttribute = this.getAttribute('options').split(',');
+    const options = optionsAttribute.map((option) => this.createOption(option));
+
+    template.innerHTML = `
+    <select name=${name} id=${id} class="restaurant-filter">
+        ${options.join('\n')}
+      </select>
+    `;
+
+    const cloneNode = template.content.cloneNode(true);
+
+    this.shadowRoot.appendChild(globalStyle);
+    this.shadowRoot.appendChild(componentStyle);
+    this.shadowRoot.appendChild(cloneNode);
+  }
+
+  static get observedAttributes() {
+    return ['name', 'id', 'options'];
+  }
+
+  attributeChangedCallback(name) {
+    if (name === 'name' && name === 'id' && name === 'options') {
+      this.connectedCallback();
+    }
+  }
+}
+
+export default CategoryBox;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import CategoryBox from './components/CategoryBox';
 import Header from './components/Header';
 import './styles/index.css';
 
 customElements.define('lunch-header', Header);
+customElements.define('category-box', CategoryBox);
