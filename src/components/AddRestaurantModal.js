bce8ef32d42f352d7bd36b801d32fa739bee825d
gyeongza <gyeongza@naver.com>
2023-03-02 20:34:00 +0900

    feat(AddTextInput): 텍스트 인풋 컴포넌트 구현
    Co-authored-by: Gilpop8663 <wolfye@naver.com>

--- src/components/AddRestaurantModal.js
+++ src/components/AddRestaurantModal.js
@@ -32,7 +32,7 @@ class AddRestaurantModal extends HTMLElement {
         position: fixed;
         width: 100%;
         bottom: 0;
-        max-width: 390px;
+        max-width: 360px;
         padding: 32px 16px;
         border-radius: 8px 8px 0px 0px;
         background: var(--grey-100);
@@ -56,11 +56,14 @@ class AddRestaurantModal extends HTMLElement {
       id="category"
       options="한식,중식,일식,양식,아시안,기타"
     ></add-category-list>
-      <add-category-list
-      name="거리(도보 이동 시간)"
-      id="distance"
-      options="5,10,15,20,30"
+    <add-text-input name="이름" id="name" ></add-text-input>
+    <add-category-list
+    name="거리(도보 이동 시간)"
+    id="distance"
+    options="5,10,15,20,30"
     ></add-category-list>
+    <add-text-input name="설명" id="description" caption="메뉴 등 추가 정보를 입력해 주세요." ></add-text-input>
+    <add-text-input name="참고 링크" id="link" caption="매장 정보를 확인할 수 있는 링크를 입력해 주세요." ></add-text-input>
         <div class="button-container">
         </div>
       </form>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import Header from './components/Header';
 import RestaurantBox from './components/RestaurantBox';
 import AddCategoryList from './components/AddCategoryList';
 import './styles/index.css';
+import AddTextInput from './components/AddTextInput';
 
 customElements.define('lunch-header', Header);
 customElements.define('category-list', CategoryList);
 customElements.define('restaurant-box', RestaurantBox);
 customElements.define('add-restaurant-modal', AddRestaurantModal);
 customElements.define('add-category-list', AddCategoryList);
+customElements.define('add-text-input', AddTextInput);
