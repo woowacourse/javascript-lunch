e0dec9a799f5c34760580b46ef2065aa1452a03b
bassyu <yrkdgus@gmail.com>
2023-03-02 13:06:37 +0900

    feat(type): 컴포넌트, 데이터 모델 타입 선언

---
+++ src/type.ts
@@ -0,0 +1,18 @@
+export interface Component<T> {
+  $component: HTMLElement;
+  state: T;
+  setState: (state: T) => void;
+  render: () => void;
+}
+
+export type Category = '전체' | '한식' | '중식' | '일식' | '양식' | '아시안' | '기타';
+export type SortBy = 'name' | 'distance';
+export type Distance = 5 | 10 | 15 | 20 | 30;
+
+export type Restaurant = {
+  name: string;
+  category: Category;
+  distance: Distance;
+  description?: string;
+  link?: string;
+};
