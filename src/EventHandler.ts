8ae72f76c7c75e894d08a012847f3925291ee093
kyw0716 <kyw0716@naver.com>
2023-03-02 17:17:03 +0900

    feat: 전체 기능 완성

--- src/EventHandler.ts
+++ src/EventHandler.ts
@@ -3,11 +3,17 @@ export const setModalToggleHandler = (
   closeModal: () => void
 ) => {
   const $modalOpenButton = document.querySelector(".gnb__button");
+  const $modalBackdrop = document.querySelector(".modal-backdrop");
   const $modalCloseButton = document.querySelector(".button--secondary");
 
   $modalOpenButton?.addEventListener("click", openModal);
 
+  $modalBackdrop?.addEventListener("click", closeModal);
   $modalCloseButton?.addEventListener("click", closeModal);
+
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") closeModal();
+  });
 };
 
 export const setSortingSelectChangeHandler = (
