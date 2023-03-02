d91b42cd7f1ca5e7683bc6c832fe4ad651635cdd
NaveOWO <pianowell96@gmail.com>
2023-03-02 15:17:32 +0900

    feat : 모달객체 class로 변경

--- src/util/Modal.ts
+++ src/util/Modal.ts
@@ -1,11 +1,19 @@
 import { $ } from "./querySelector";
 
-const Modal = {
+class Modal {
+  #isOpen = false;
+
   open() {
+    if (this.#isOpen) return;
     $(".modal")?.classList.add("modal--open");
-  },
+    this.#isOpen = true;
+  }
 
   close() {
+    if (!this.#isOpen) return;
     $(".modal")?.classList.remove("modal--open");
-  },
-};
+    this.#isOpen = false;
+  }
+}
+
+export default Modal;
