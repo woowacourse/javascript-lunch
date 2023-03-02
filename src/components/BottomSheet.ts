656cd0b98799be643cffd086451ddc22211ef897
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 14:00:43 +0900

    refactor(components): Bottomsheet CustomElement로 수정

--- src/components/BottomSheet.ts
+++ src/components/BottomSheet.ts
@@ -1,10 +1,39 @@
-const BottomSheet = (children: string) => {
-  return `
-    <div class="modal-backdrop"></div>
-    <div class="modal-container">
-      ${children}
-    </div>
-  `;
-};
+class BottomSheet extends HTMLElement {
+  state: { isOpen: boolean };
+
+  constructor() {
+    super();
+    this.state = { isOpen: false };
+    this.render();
+    this.onClickBackdrop();
+  }
+
+  render() {
+    this.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <div id="modalBackdrop" class="modal-backdrop"></div>
+      <div class="modal-container"></div>
+    `
+    );
+  }
+
+  onClickBackdrop() {
+    const modalBackdrop = document.getElementById("modalBackdrop");
+    modalBackdrop?.addEventListener("click", () => {
+      this.close();
+    });
+  }
+
+  open() {
+    const bottomSheet = document.getElementById("bottomSheet");
+    bottomSheet?.classList.add("modal--open");
+  }
+
+  close() {
+    const bottomSheet = document.getElementById("bottomSheet");
+    bottomSheet?.classList.remove("modal--open");
+  }
+}
 
 export default BottomSheet;
