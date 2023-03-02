7bcf414e5fd734b9cd26842480b130c0425edbac
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 14:02:35 +0900

    refactor(components): NavBar CustomElement로 수정

--- src/components/NavBar.ts
+++ src/components/NavBar.ts
@@ -1,10 +1,31 @@
-const NavBar = () => {
-  return `
-    <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-    <button id="addButton" type="button" class="gnb__button" aria-label="음식점 추가">
-      <img src="../templates/add-button.png" alt="음식점 추가">
-    </button>
-  `;
-};
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  render() {
+    this.insertAdjacentHTML(
+      "afterbegin",
+      `
+    <header class="gnb">
+      <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+      <button id="addButton" type="button" class="gnb__button" aria-label="음식점 추가">
+        <img src="../templates/add-button.png" alt="음식점 추가">
+      </button>
+    <header>
+  `
+    );
+    this.handleClick();
+  }
+
+  handleClick() {
+    const addButton = document.getElementById("addButton");
+    addButton?.addEventListener("click", () => {
+      const bottomSheet: any = document.getElementById("bottomSheet");
+      bottomSheet?.open();
+    });
+  }
+}
 
 export default NavBar;
