419db9d79d44c01af52dfd8eea659b72317013b1
Suyoung <jsy8492@gmail.com>
2023-03-02 13:12:35 +0900

    feat: 음식점 정보 목록을 나타내는 RestaurantList custom element 작성

---
+++ src/view/RestaurantList.js
@@ -0,0 +1,28 @@
+import { restaurants } from '../domain/restaurants';
+
+customElements.define(
+  'restaurant-list',
+  class RestaurantList extends HTMLElement {
+    constructor() {
+      super();
+
+      const { list } = restaurants;
+
+      this.innerHTML = /* html */ `
+      <ul class="restaurant-list">
+      ${list
+        .map(
+          (restaurant) => /* html */ `
+          <restaurant-list-item
+            category="${restaurant.category}"
+            restaurantName="${restaurant.name}"
+            distance="${restaurant.distance}"
+            description="${restaurant.description}"
+          ></restaurant-list-item>
+        `
+        )
+        .join('')}
+      </ul>`;
+    }
+  }
+);
