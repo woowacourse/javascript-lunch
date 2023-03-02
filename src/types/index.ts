2968da44d986b5c2ebd2d9d40f37cd42e64a4601
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-02 16:11:32 +0900

    feat: types 디렉터리 생성 및 Restaurant interface 정의

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+export type Distance = '5' | '10' | '15' | '20' | '30';
+
+export interface Restaurant {
+  category: Category;
+  name: string;
+  distance: Distance;
+  describe?: string;
+  referenceLink?: string;
+}
