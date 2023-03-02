9d9c4bdf54a1ba9b36f9a1c559a6e0a2cf95ae70
bassyu <yrkdgus@gmail.com>
2023-03-02 17:47:53 +0900

    feat(RestaurantList): 랜더링 및 정렬 기능 구현

--- src/components/RestaurantList.ts
+++ src/components/RestaurantList.ts
@@ -1,27 +1,62 @@
-import { Component, Restaurant } from '../type';
+import { Category, Component, Restaurant, SortBy } from '../type';
+import RestaurantListItem from './RestaurantListItem';
 
 type RestaurantListState = {
-  restaurant: Restaurant[];
+  restaurants: Restaurant[];
+  category: Category;
+  sortBy: SortBy;
+};
+
+type RestaurantListProps = {
+  $parent: HTMLElement;
+  restaurants: Restaurant[];
+  category: Category;
+  sortBy: SortBy;
 };
 
 class RestaurantList implements Component<RestaurantListState> {
   $component: HTMLElement;
   state: RestaurantListState;
 
-  constructor($parent: HTMLElement, state: RestaurantListState) {
+  constructor({ $parent, restaurants, category, sortBy }: RestaurantListProps) {
     this.$component = document.createElement('div');
-    this.state = state;
+    this.state = {
+      restaurants,
+      category,
+      sortBy,
+    };
+    $parent.append(this.$component);
   }
 
-  setState(newState: RestaurantListState) {
+  setState = (newState: RestaurantListState) => {
     this.state = newState;
-
     this.render();
-  }
+  };
 
-  render() {
-    // TODO: 매핑으로 띄워주기
-  }
+  render = () => {
+    const fragment = document.createDocumentFragment();
+
+    this.getShowRestaurants().forEach((restaurant) => {
+      new RestaurantListItem({ $parent: fragment, restaurant }).render();
+    });
+
+    this.$component.append(fragment);
+  };
+
+  getShowRestaurants = () => {
+    const { category, sortBy } = this.state;
+    const filtered = this.state.restaurants.filter(
+      (restaurant) => category === '전체' || restaurant.category === category
+    );
+    const getPivot = (restaurant: Restaurant) =>
+      sortBy === 'name' ? restaurant.name : Number(restaurant.distance);
+
+    return filtered.sort((a, b) => {
+      if (getPivot(a) > getPivot(b)) return 1;
+      if (getPivot(a) < getPivot(b)) return -1;
+      return 0;
+    });
+  };
 }
 
 export default RestaurantList;
