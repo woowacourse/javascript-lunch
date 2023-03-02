11f78ef99f6b69875448f4823fc9edf06addbce8
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-02 19:50:47 +0900

    test(Restaurants): 음식점 이름순 정렬 메서드에 대한 테스트 케이스 추가

--- __tests__/Restaurants.test.ts
+++ __tests__/Restaurants.test.ts
@@ -59,3 +59,52 @@ describe('Restaurants.getFilteredRestaurantsByCategory', () => {
     expect(expected).toEqual(filterdRestaurants);
   });
 });
+
+describe('Restaurants.getSortedRestaurantsByName', () => {
+  test('', () => {
+    const dummy: Restaurant[] = [
+      {
+        category: '한식',
+        name: '맛이쪙 돈까스',
+        distance: '10',
+        description: '평양 출신의 할머니가 수십 년간 운영해온 비지 전문점',
+        link: 'www.naver.com',
+      },
+      {
+        category: '한식',
+        name: '김치찌개 장인',
+        distance: '5',
+      },
+      {
+        category: '일식',
+        name: '돈카라',
+        distance: '5',
+      },
+    ];
+    const restaurants = new Restaurants();
+    dummy.forEach((v) => restaurants.addRestaurant(v));
+
+    const sortedRestaurants = restaurants.getSortedRestaurantsByName();
+    const expected: Restaurant[] = [
+      {
+        category: '한식',
+        name: '김치찌개 장인',
+        distance: '5',
+      },
+      {
+        category: '일식',
+        name: '돈카라',
+        distance: '5',
+      },
+      {
+        category: '한식',
+        name: '맛이쪙 돈까스',
+        distance: '10',
+        description: '평양 출신의 할머니가 수십 년간 운영해온 비지 전문점',
+        link: 'www.naver.com',
+      },
+    ];
+
+    expect(expected).toEqual(sortedRestaurants);
+  });
+});
