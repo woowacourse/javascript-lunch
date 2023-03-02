ca5e34145d2aad23da49ec1302ebeb66daa5619e
semnil5202 <semnil5202@gmail.com>
2023-03-02 20:10:29 +0900

    refactor: 음식점 관리 클래스의 기능 및 반환 타입 수정
    Co-authored-by: 임우찬 <dlaxodud1217@gmail.com>

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -3,39 +3,30 @@ import { INIT_DATA } from '../constants/initData';
 import { Restaurant, SortBy, Category } from '../types/Restaurant.js';
 
 export default class RestaurantManager {
-  initRestaurantList() {
-    const restaurantData = localStorage.getItem('restaurantList');
+  private restaurantList: Restaurant[];
 
+  constructor() {
+    const restaurantData = localStorage.getItem('restaurantList');
     if (restaurantData === null || restaurantData.length === 0) {
       localStorage.setItem('restaurantList', JSON.stringify(INIT_DATA));
+      this.restaurantList = JSON.parse(JSON.stringify(INIT_DATA));
+    } else {
+      this.restaurantList = JSON.parse(restaurantData);
     }
   }
 
   getRestaurantList(): Restaurant[] {
-    const restaurantData = localStorage.getItem('restaurantList');
-    if (restaurantData !== null) {
-      return JSON.parse(restaurantData);
-    }
-    return [];
+    return [...this.restaurantList];
   }
 
   addRestaurant(restaurant: Restaurant) {
-    const restaurantData = localStorage.getItem('restaurantList');
-
-    if (restaurantData !== null) {
-      const restaurantList: Restaurant[] = JSON.parse(restaurantData);
-      restaurantList.push(restaurant);
-
-      localStorage.setItem('restaurantList', JSON.stringify(restaurantList));
-    }
+    this.restaurantList.push(restaurant);
+    localStorage.setItem('restaurantList', JSON.stringify(this.restaurantList));
   }
 
-  sortRestaurantList(standard: SortBy) {
-    const restaurantData = localStorage.getItem('restaurantList');
-
-    if (restaurantData !== null && standard === '이름순') {
-      const restaurantList: Restaurant[] = JSON.parse(restaurantData);
-      restaurantList.sort((data1: Restaurant, data2: Restaurant): number => {
+  sortRestaurantList(standard: SortBy): Restaurant[] {
+    if (standard === 'name') {
+      this.restaurantList.sort((data1: Restaurant, data2: Restaurant): number => {
         return data1.storeName.localeCompare(data2.storeName, undefined, {
           numeric: true,
           sensitivity: 'base',
@@ -43,22 +34,15 @@ export default class RestaurantManager {
       });
     }
 
-    if (restaurantData !== null && standard === '거리순') {
-      const restaurantList: Restaurant[] = JSON.parse(restaurantData);
-      restaurantList.sort(
+    if (standard === 'distance') {
+      this.restaurantList.sort(
         (data1: Restaurant, data2: Restaurant): number => data1.distance - data2.distance
       );
     }
+    return this.restaurantList;
   }
 
   filterRestaurantList(category: Category): Restaurant[] {
-    const restaurantData = localStorage.getItem('restaurantList');
-
-    if (restaurantData !== null) {
-      const restaurantList: Restaurant[] = JSON.parse(restaurantData);
-      return restaurantList.filter((data) => data.category === category);
-    }
-
-    return [];
+    return this.restaurantList.filter((data) => data.category === category);
   }
 }
