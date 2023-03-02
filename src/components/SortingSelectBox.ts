e496eeba86a9da85d00cefaa68e14d9703e95c71
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 21:47:08 +0900

    feat(components): SortingSelectBox 구현

---
+++ src/components/SortingSelectBox.ts
@@ -0,0 +1,31 @@
+class SortingSelectBox extends HTMLElement {
+  controller;
+
+  constructor() {
+    super();
+    this.controller = globalThis.controller;
+    this.render();
+    this.onSelectOption();
+  }
+
+  render() {
+    this.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <select name="sorting" id="sorting-filter" class="restaurant-filter">
+        <option id="sortingOptionName" value="name">이름순</option>
+        <option value="distance">거리순</option>
+      </select>
+      `
+    );
+  }
+
+  onSelectOption() {
+    const sortingFilter: any = document.getElementById("sorting-filter");
+    sortingFilter?.addEventListener("change", () => {
+      this.controller.sortRestaurants(sortingFilter.value);
+    });
+  }
+}
+
+export default SortingSelectBox;
