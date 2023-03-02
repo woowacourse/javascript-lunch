d066834ed88c4616215f05ffb74aa63ae5c62e8d
guridaek <xylitol79@gmail.com>
2023-03-02 20:06:32 +0900

    feat: RestaurantFilter 컴포넌트 구현

---
+++ src/components/RestaurantFilter.ts
@@ -0,0 +1,6 @@
+import CategoryComboBox from './CategoryComboBox';
+import SortComboBox from './SortComboBox';
+
+const RestaurantFilter = `<section class="restaurant-filter-container">${CategoryComboBox}${SortComboBox}</section>`;
+
+export default RestaurantFilter;
