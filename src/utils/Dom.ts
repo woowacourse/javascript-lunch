e5b3eabfb1eb8a998095752b80427955f2d6549a
Dahye <06robin11@gmail.com>
2023-03-02 13:23:57 +0900

    feat(dom util): 속성값을 받아서 요소 생성후 반환

--- src/utils/Dom.ts
+++ src/utils/Dom.ts
@@ -3,7 +3,36 @@ export const $ = (selector: string, target = document) =>
 export const $$ = (selector: string, target = document) =>
   target.querySelectorAll(selector);
 
-export const createElement = (tag: string) => document.createElement(tag);
+interface Atrribute {
+  id?: string;
+  class?: string[];
+  name?: string;
+  required?: boolean;
+  src?: string;
+  alt?: string;
+  ["aria-label"]?: string;
+  type?: string;
+  for?: string;
+  cols?: string;
+  rows?: string;
+  value?: string;
+}
+
+export const createElement = (tag: string, atrribute: Atrribute = {}) => {
+  const element = document.createElement(tag);
+
+  Object.entries(atrribute).forEach(([attributeName, value]) => {
+    if (attributeName === "class") {
+      value.forEach((className: string) => {
+        element.classList.add(className);
+      });
+      return;
+    }
+    element.setAttribute(attributeName, value);
+  });
+
+  return element;
+};
 
 export const render = (element: Element) => {
   $(".body")?.append(element);
