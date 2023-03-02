d1803013f86399a06b941f5241c9c48fc414bf0d
bassyu <yrkdgus@gmail.com>
2023-03-02 16:34:04 +0900

    feat(RestaurantListItem): RestaurantListItem 랜더링 구현

--- src/components/RestaurantListItem.ts
+++ src/components/RestaurantListItem.ts
@@ -1,34 +1,55 @@
 import { Component, Restaurant } from '../type';
 
-type RestaurantListItemState = Restaurant;
+type RestaurantListItemState = {
+  restaurant: Restaurant;
+};
+
+type RestaurantListItemProps = {
+  $parent: DocumentFragment;
+  restaurant: Restaurant;
+};
 
 class RestaurantListItem implements Component<RestaurantListItemState> {
   $component: HTMLElement;
   state: RestaurantListItemState;
 
-  constructor($parent: HTMLElement, state: RestaurantListItemState) {
-    this.$component = document.createElement('div');
-    this.state = state;
+  constructor({ $parent, restaurant }: RestaurantListItemProps) {
+    this.$component = document.createElement('li');
+    this.$component.classList.add('restaurant');
+    this.state = {
+      restaurant,
+    };
+    $parent.append(this.$component);
   }
 
-  setState(newState: RestaurantListItemState) {
+  setState = (newState: RestaurantListItemState) => {
     this.state = newState;
-
     this.render();
-  }
+  };
 
-  render() {
-    // TODO: 상태로 받은 li 띄우기
+  render = () => {
+    const imageMap: {
+      [key: string]: string;
+    } = {
+      한식: 'category-korean',
+      중식: 'category-chinese',
+      일식: 'category-japanese',
+      양식: 'category-western',
+      아시안: 'category-asian',
+      기타: 'category-etc',
+    };
+    const { category, name, distance, description } = this.state.restaurant;
     this.$component.innerHTML = `
-      <li class="restaurant">
-        <div class="restaurant__category">${/* category img */ 1}</div>
-        <div class="restaurant__info">
-          <h3 class="restaurant__name text-subtitle">이태리키친</h3>
-          <span class="restaurant__distance text-body">캠퍼스부터 20분 내</span>
-          <p class="restaurant__description text-body">늘 변화를 추구하는 이태리키친입니다.</p>
-        </div>
-      </li>`;
-  }
+      <div class="restaurant__category">
+        <img src="../../image/${imageMap[category]}.png" alt="중식" class="category-icon" />
+      </div>
+      <div class="restaurant__info">
+        <h3 class="restaurant__name text-subtitle">${name}</h3>
+        <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+        <p class="restaurant__description text-body">${description}</p>
+      </div>
+    `;
+  };
 }
 
 export default RestaurantListItem;
