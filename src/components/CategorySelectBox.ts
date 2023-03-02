39439bc7dae3916170887e4dc4c666b9a5a2dfbf
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 21:47:23 +0900

    feat(components): CategorySelectBox 구현

---
+++ src/components/CategorySelectBox.ts
@@ -0,0 +1,36 @@
+class CategorySelectBox extends HTMLElement {
+  controller;
+
+  constructor() {
+    super();
+    this.controller = globalThis.controller;
+    this.render();
+    this.onSelectOption();
+  }
+
+  render() {
+    this.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <select name="category" id="category-filter" class="restaurant-filter">
+        <option value="전체">전체</option>
+        <option value="한식">한식</option>
+        <option value="중식">중식</option>
+        <option value="일식">일식</option>
+        <option value="양식">양식</option>
+        <option value="아시안">아시안</option>
+        <option value="기타">기타</option>
+      </select>
+      `
+    );
+  }
+
+  onSelectOption() {
+    const categoryFilter: any = document.getElementById("category-filter");
+    categoryFilter?.addEventListener("change", () => {
+      this.controller.filterRestaurants(categoryFilter.value);
+    });
+  }
+}
+
+export default CategorySelectBox;
