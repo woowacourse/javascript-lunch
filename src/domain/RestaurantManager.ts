afda601efc548a29c7139c4fadadfdb676606433
semnil5202 <semnil5202@gmail.com>
2023-03-02 16:43:49 +0900

    feat: 음식점 목록에 새로운 음식점을 추가할 수 있는 기능
    Co-authored-by: 임우찬 <dlaxodud1217@gmail.com>

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -8,4 +8,15 @@ export default class RestaurantManager {
       localStorage.setItem('restaurantList', JSON.stringify(INIT_DATA));
     }
   }
+
+  addRestaurant(restaurant: Restaurant) {
+    const restaurantData = localStorage.getItem('restaurantList');
+
+    if (restaurantData !== null) {
+      const restaurantList: Restaurant[] = JSON.parse(restaurantData);
+      restaurantList.push(restaurant);
+
+      localStorage.setItem('restaurantList', JSON.stringify(restaurantList));
+    }
+  }
 }
