e8ac357fcd6f9ef6023407d704a8297617e3268d
Suyoung <jsy8492@gmail.com>
2023-03-02 21:28:21 +0900

    refactor: 불필요한 컴포넌트 파일 제거

--- src/view/List.js
+++
@@ -1,8 +0,0 @@
-import { ListItem } from './ListItem';
-
-export const List = (restaurants) => {
-  return /* html */ `
-  <ul class="restaurant-list">
-    ${restaurants.map((restaurant) => ListItem(restaurant)).join('')}
-  </ul>`;
-};
