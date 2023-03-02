2254866f7243f6d8433ab074e41f645c688b30a5
nlom0218 <nlom0218@naver.com>
2023-03-02 06:24:24 +0000

    test: Restaurant의 참고링크가 올바르지 않을 때의 테스트 작성
    Co-authored-by: solo5star <solo_5@naver.com>

--- __tests__/domain/Validation.test.ts
+++ __tests__/domain/Validation.test.ts
@@ -36,4 +36,41 @@ describe('Validation', () => {
       );
     });
   });
+
+  describe('음식점 참고 링크', () => {
+    context('링크가 올바를 때', () => {
+      it.each(['www.naver.com', 'www.gmail.com'])(
+        '%p: Restaurant 객체가 생성되어야 한다.',
+        (referenceUrl) => {
+          // given
+          const restaurant = new Restaurant({
+            category: new Category('기타', 'etc.png'),
+            name: '한식당',
+            distanceByMinutes: 5,
+            referenceUrl,
+          });
+
+          // then
+          expect(restaurant).toBeInstanceOf(Restaurant);
+        },
+      );
+    });
+
+    context('링크가 올바르지 않을 때', () => {
+      it.each(['www.navercom', 'htt://www.naver.com', 'file:///C:/User'])(
+        '%p:  에러가 발생해야 한다.',
+        (referenceUrl) => {
+          const createRestaurant = () =>
+            new Restaurant({
+              category: new Category('기타', 'etc.png'),
+              name: '한식당',
+              distanceByMinutes: 5,
+              referenceUrl,
+            });
+
+          expect(createRestaurant).toThrowError();
+        },
+      );
+    });
+  });
 });
