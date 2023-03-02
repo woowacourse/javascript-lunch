e10a3696371d6d296106ae5e57a4b80fee99855a
kyw0716 <kyw0716@naver.com>
2023-03-02 16:16:32 +0900

    refactor: 모달 내 customElement 추가

--- src/components/CustomModal.ts
+++ src/components/CustomModal.ts
@@ -2,8 +2,6 @@ class CustomModal extends HTMLElement {
   constructor() {
     super();
 
-    const className = this.getAttribute("className");
-
     this.innerHTML = `
       <div class="modal">
         <div class="modal-backdrop"></div>
@@ -12,15 +10,7 @@ class CustomModal extends HTMLElement {
           <form>
             <div class="form-item form-item--required">
               <label for="category text-caption">카테고리</label>
-              <select name="category" id="category" required>
-                <option value="">선택해 주세요</option>
-                <option value="한식">한식</option>
-                <option value="중식">중식</option>
-                <option value="일식">일식</option>
-                <option value="양식">양식</option>
-                <option value="아시안">아시안</option>
-                <option value="기타">기타</option>
-              </select>
+              <custom-select name="category" id="category" required></custom-select>
             </div>
             <div class="form-item form-item--required">
               <label for="name text-caption">이름</label>
@@ -28,14 +18,7 @@ class CustomModal extends HTMLElement {
             </div>
             <div class="form-item form-item--required">
               <label for="distance text-caption">거리(도보 이동 시간) </label>
-              <select name="distance" id="distance" required>
-                <option value="">선택해 주세요</option>
-                <option value="5">5분 내</option>
-                <option value="10">10분 내</option>
-                <option value="15">15분 내</option>
-                <option value="20">20분 내</option>
-                <option value="30">30분 내</option>
-              </select>
+              <custom-select name="distance" id="distance" required></custom-select>
             </div>
             <div class="form-item">
               <label for="description text-caption">설명</label>
