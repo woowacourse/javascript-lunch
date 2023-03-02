446fd3da376c55cb350647050fd4f44e41fec26d
Suyoung <jsy8492@gmail.com>
2023-03-02 19:42:08 +0900

    refactor: 이미지 파일들을 images 디렉터리 하위로 이동

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import '../templates/style.css';
 
+import '../images/add-button.png';
+import '../images/category-asian.png';
+import '../images/category-chinese.png';
+import '../images/category-etc.png';
+import '../images/category-japanese.png';
+import '../images/category-korean.png';
+import '../images/category-western.png';
+import '../images/favorite-icon-filled.png';
+import '../images/favorite-icon-lined.png';
+
 import LunchMenuController from './controller/LunchMenuController';
 
 import RestaurantList from './view/components/RestaurantList';
