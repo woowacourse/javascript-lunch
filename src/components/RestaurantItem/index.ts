d3bf0f0602351dc1f1689caa7e80d4f877a8438d
yogjin <youngjin988@gmail.com>
2023-03-02 20:18:15 +0900

    refactor: type을 파일로 분리

--- src/components/RestaurantItem/index.ts
+++ src/components/RestaurantItem/index.ts
@@ -1,16 +1,5 @@
 import $template from './index.html';
 
-type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
-type Distance = 5 | 10 | 15 | 20 | 30;
-
-export interface Restaurant {
-  category: Category;
-  name: string;
-  distance: Distance;
-  description?: string;
-  link?: string;
-}
-
 class RestaurantItem extends HTMLElement {
   constructor() {
     super();
