e6c236211479091cce296e5a3ed5061d75fe9748
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 14:05:43 +0900

    refactor(index.js): CustomElement로 수정

--- src/index.js
+++ src/index.js
@@ -3,24 +3,5 @@ import BottomSheet from "./components/BottomSheet";
 import NavBar from "./components/NavBar";
 import "./css/style.css";
 
-const handleBottomSheet = () => {
-  //추가 동작 구현
-  const closeBottomSheet = document.getElementById("closeBottomSheet");
-  closeBottomSheet.addEventListener("click", () => {
-    const bottomSheet = document.getElementById("bottomSheet");
-    bottomSheet.classList.remove("modal--open");
-  });
-};
-
-// navBar
-const navBar = document.getElementById("navBar");
-navBar.innerHTML = NavBar();
-
-// 음식점 추가 바텀시트 열기
-const bottomSheet = document.getElementById("bottomSheet");
-const addButton = document.getElementById("addButton");
-addButton.addEventListener("click", () => {
-  bottomSheet.classList.add("modal--open");
-  bottomSheet.innerHTML = BottomSheet(AddRestaurant());
-  handleBottomSheet();
-});
+customElements.define("nav-bar", NavBar);
+customElements.define("bottom-sheet", BottomSheet);
