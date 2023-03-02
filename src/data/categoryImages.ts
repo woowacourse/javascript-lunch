658ab5452088a9d7da01bde67e81056f083392d7
guridaek <xylitol79@gmail.com>
2023-03-02 15:31:51 +0900

    feat: category Images 불러오는 기능 구현

---
+++ src/data/categoryImages.ts
@@ -0,0 +1,20 @@
+import asainImage from '../../templates/category-asian.png';
+import chineseImage from '../../templates/category-chinese.png';
+import japaneseImage from '../../templates/category-japanese.png';
+import koreanImage from '../../templates/category-korean.png';
+import westernImage from '../../templates/category-western.png';
+import etcImage from '../../templates/category-etc.png';
+import { Category } from './type';
+
+const categoryImages = {
+  한식: koreanImage,
+  중식: chineseImage,
+  일식: japaneseImage,
+  아시안: asainImage,
+  양식: westernImage,
+  기타: etcImage,
+};
+
+const getCategoryImage = (category: Category) => categoryImages[category];
+
+export { getCategoryImage };
