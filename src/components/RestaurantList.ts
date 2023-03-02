80d20b3626e0f9f0351a58de1db8fd88dca1c7cb
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 17:47:39 +0900

    feat(components): RestaurantList component 구현

---
+++ src/components/RestaurantList.ts
@@ -0,0 +1,28 @@
+import RestaurantItem from "./RestaurantItem";
+
+class RestaurantList extends HTMLElement {
+  controller;
+
+  constructor() {
+    super();
+    this.controller = globalThis.controller;
+    this.render();
+  }
+
+  render() {
+    this.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <section class="restaurant-list-container">
+        <ul class="restaurant-list">
+        ${this.controller
+          .getRestaurants()
+          .map((restaurant) => new RestaurantItem().render(restaurant))}
+        </ul>
+      </section>
+    `
+    );
+  }
+}
+
+export default RestaurantList;
