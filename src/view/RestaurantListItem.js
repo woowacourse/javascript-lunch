9c9ebd35497eb889f7016c8d687944807f3e035b
Suyoung <jsy8492@gmail.com>
2023-03-02 13:12:09 +0900

    feat: 음식점 하나의 정보를 나타내는 RestaurantListItem custom element 작성

---
+++ src/view/RestaurantListItem.js
@@ -0,0 +1,25 @@
+customElements.define(
+  'restaurant-list-item',
+  class RestaurantListItem extends HTMLElement {
+    constructor() {
+      super();
+
+      const category = this.getAttribute('category');
+      const restaurantName = this.getAttribute('restaurantName');
+      const distance = this.getAttribute('distance');
+      const description = this.getAttribute('description');
+
+      this.innerHTML = /* html */ `
+      <li class="restaurant">
+        <div class="restaurant__category">
+          <img src="./category-korean.png" alt="${category}" class="category-icon" />
+        </div>
+        <div class="restaurant__info">
+          <h3 class="restaurant__name text-subtitle">${restaurantName}</h3>
+          <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+          <p class="restaurant__description text-body">${description}</p>
+        </div>
+      </li>`;
+    }
+  }
+);
