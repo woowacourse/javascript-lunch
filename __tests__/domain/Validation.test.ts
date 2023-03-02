788f758f1f3b650a3da43b7ae9ec9c57d64d8bd4
nlom0218 <nlom0218@naver.com>
2023-03-02 06:06:42 +0000

    test: Validation 같은 의미 단위로 그룹핑
    Co-authored-by: solo5star <solo_5@naver.com>

--- __tests__/domain/Validation.test.ts
+++ __tests__/domain/Validation.test.ts
@@ -2,36 +2,38 @@ import Category from '../../src/domain/Category';
 import Restaurant from '../../src/domain/Restaurant';
 
 describe('Validation', () => {
-  context('음식점 이름의 길이가 올바를 때', () => {
-    it.each(['한식', '짱', '좀길이가엄청긴음식점좀길이가엄청긴음식점'])(
-      '%p: Restaurant 객체가 생성되어야 한다.',
-      (restaurantName) => {
-        // given
-        const restaurant = new Restaurant({
-          category: new Category('기타', 'etc.png'),
-          name: restaurantName,
-          distanceByMinutes: 5,
-        });
-
-        // then
-        expect(restaurant).toBeInstanceOf(Restaurant);
-      },
-    );
-  });
-
-  context('음식점 이름의 길이가 올바르지 않을 때', () => {
-    it.each(['', '좀길이가엄청긴음식점좀길이가엄청긴음식점+'])(
-      '%p: 에러가 발생해야 한다.',
-      (restaurantName) => {
-        const createRestaurant = () =>
-          new Restaurant({
+  describe('음식점 이름', () => {
+    context('길이가 올바를 때', () => {
+      it.each(['한식', '짱', '좀길이가엄청긴음식점좀길이가엄청긴음식점'])(
+        '%p: Restaurant 객체가 생성되어야 한다.',
+        (restaurantName) => {
+          // given
+          const restaurant = new Restaurant({
             category: new Category('기타', 'etc.png'),
             name: restaurantName,
             distanceByMinutes: 5,
           });
 
-        expect(createRestaurant).toThrowError();
-      },
-    );
+          // then
+          expect(restaurant).toBeInstanceOf(Restaurant);
+        },
+      );
+    });
+
+    context('길이가 올바르지 않을 때', () => {
+      it.each(['', '좀길이가엄청긴음식점좀길이가엄청긴음식점+'])(
+        '%p: 에러가 발생해야 한다.',
+        (restaurantName) => {
+          const createRestaurant = () =>
+            new Restaurant({
+              category: new Category('기타', 'etc.png'),
+              name: restaurantName,
+              distanceByMinutes: 5,
+            });
+
+          expect(createRestaurant).toThrowError();
+        },
+      );
+    });
   });
 });
