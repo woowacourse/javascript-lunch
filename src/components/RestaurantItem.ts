2e3721ac37aa1516e6e59d7fa7996146d6fcab33
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-02 17:49:11 +0900

    feat(components): RestaurantItem component 구현

---
+++ src/components/RestaurantItem.ts
@@ -0,0 +1,20 @@
+import RestaurantType from "../type/Restaurant";
+
+class RestaurantItem {
+  render(restaurant: RestaurantType) {
+    return `
+    <li class="restaurant">
+        <div class="restaurant__category">
+          <img src="./category-asian.png" alt="아시안" class="category-icon">
+        </div>
+        <div class="restaurant__info">
+          <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
+          <span class="restaurant__distance text-body">${restaurant.distance}</span>
+          <p class="restaurant__description text-body">${restaurant.description}</p>
+        </div>
+      </li>
+    `;
+  }
+}
+
+export default RestaurantItem;
