4f4359e735d42503e09af8d847d9b88badfa4f35
gyeongza <gyeongza@naver.com>
2023-03-02 18:08:38 +0900

    feat(Header): 헤더 컴포넌트 Shadow Dom 으로 수정
    Co-authored-by: Gilpop8663 <wolfye@naver.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,66 @@
-import { $ } from '../utils/index.js';
+import GLOBAL_CSS from '../constants';
 
 class Header extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: 'open' });
   }
 
   connectedCallback() {
-    const header = document.createElement('header');
-    header.classList.add('gnb');
-    header.innerHTML = `
-    <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-    <button type="button" class="gnb__button" aria-label="음식점 추가">
-      <img src="./assets/add-button.png" alt="음식점 추가" />
-    </button>
+    const globalStyle = document.createElement('style');
+    const componentStyle = document.createElement('style');
+    globalStyle.textContent = GLOBAL_CSS;
+    componentStyle.textContent = `
+    .gnb {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 64px;
+    
+      padding: 0 16px;
+    
+      background-color: var(--primary-color);
+    }
+    
+    .gnb__title {
+      color: #fcfcfd;
+    }
+    
+    .gnb__button {
+      height: 40px;
+    
+      border: none;
+      border-radius: 8px;
+      background: transparent;
+    
+      font-size: 24px;
+      cursor: pointer;
+    }
+    
+    .gnb__button img {
+      display: block;
+      width: 40px;
+      height: 40px;
+      object-fit: contain;
+    }
+`;
+
+    const template = document.createElement('template');
+
+    template.innerHTML = `
+    <header class="gnb">
+      <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+      <button type="button" class="gnb__button" aria-label="음식점 추가">
+        <img src="./assets/add-button.png" alt="음식점 추가" />
+      </button>
+    </header>
     `;
 
-    $('#app').appendChild(header);
+    const cloneNode = template.content.cloneNode(true);
+
+    this.shadowRoot.appendChild(globalStyle);
+    this.shadowRoot.appendChild(componentStyle);
+    this.shadowRoot.appendChild(cloneNode);
   }
 }
 
