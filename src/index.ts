10622d5962aea30be2bd67a01a5f2d7c7ad8f22e
n0eyes <doll4586633@naver.com>
2023-03-02 15:50:19 +0900

    feat: Header 구현

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './styles/reset.css';
+import './styles/style.css';
 import { render } from './utils/core';
 import { App } from './view/components/App';
 
--- src/view/components/App.ts
+++ src/view/components/App.ts
@@ -1,6 +1,8 @@
+import { Header } from './Header';
+
 function App() {
   return `
-    <div>hello</div>
+    ${Header()}
   `;
 }
 
