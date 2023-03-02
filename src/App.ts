85f34d7ddebe195535c3b2ebdd3648a727d2643d
minjaeKim <smallkdb@gmail.com>
2023-03-02 19:53:09 +0900

    feat: 버튼 클릭 시 모달창 닫는 이벤트 추가

--- src/App.ts
+++ src/App.ts
@@ -34,7 +34,7 @@ class App {
           ${this.#restaurantList.template()}
         </section>
 
-        <div class="modal modal--open">
+        <div class="modal">
           <div class="modal-backdrop"></div>
           <div class="modal-container">
             ${ModalHeader.template()}
@@ -57,6 +57,7 @@ class App {
     FilterButton.setEvent();
     ModalButton.setEvent();
     SortButton.setEvent();
+    AddButton.setEvent();
     this.#restaurantList.setEvent();
   }
 }
