100e9ec1e769e2c8b194367f7db1946141a6e1f3
yogjin <youngjin988@gmail.com>
2023-03-02 20:23:44 +0900

    feat: `modal`에서 `restaurant-item` 추가하는 기능 구현

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,4 +1,6 @@
 import $template from './index.html';
+import { Category, Distance } from '../../types';
+import store from '../../store';
 
 class Modal extends HTMLElement {
   constructor() {
@@ -11,6 +13,31 @@ class Modal extends HTMLElement {
 
   connectedCallback() {
     this.render();
+    const $modal = document.querySelector('.modal');
+
+    const $cancelButton = document.querySelector('#cancel-button');
+    $cancelButton?.addEventListener('click', () => {
+      $modal?.classList.remove('modal--open');
+    });
+
+    const $form = document.getElementById('add-restaurant');
+    $form?.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const $category = document.getElementById('category') as HTMLSelectElement;
+      const $name = document.getElementById('name') as HTMLInputElement;
+      const $distance = document.getElementById('distance') as HTMLSelectElement;
+      const $description = document.getElementById('description') as HTMLTextAreaElement;
+      const $link = document.getElementById('link') as HTMLInputElement;
+
+      store.addRestaurants({
+        category: $category.value as Category,
+        name: $name.value,
+        distance: Number($distance.value) as Distance,
+        description: `${$description.value}`,
+        link: $link.value,
+      });
+      $modal?.classList.remove('modal--open');
+    });
   }
 }
 
