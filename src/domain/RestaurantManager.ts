380282573e3d5dc15256ef1b640e238455290d11
semnil5202 <semnil5202@gmail.com>
2023-03-02 17:12:15 +0900

    feat: 음식점 리스트를 받아오는 기능 추가
    Co-authored-by: 임우찬 <dlaxodud1217@gmail.com>

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -4,9 +4,19 @@ import { Restaurant, SortBy, Category } from '../types/Restaurant.js';
 
 export default class RestaurantManager {
   initRestaurantList() {
-    if (localStorage.getItem('restaurantList')?.length === 0) {
+    const restaurantData = localStorage.getItem('restaurantList');
+
+    if (restaurantData === null || restaurantData.length === 0) {
       localStorage.setItem('restaurantList', JSON.stringify(INIT_DATA));
     }
+  }
+
+  getRestaurantList(): Restaurant[] {
+    const restaurantData = localStorage.getItem('restaurantList');
+    if (restaurantData !== null) {
+      return JSON.parse(restaurantData);
+    }
+    return [];
   }
 
   addRestaurant(restaurant: Restaurant) {
