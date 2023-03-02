4b742574053eb2e1b3edbebe3df80b39946fd949
unknown <tkdrb15@gmail.com>
2023-03-02 13:45:21 +0900

    feat:컴포넌트 추상클래스를 만들다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

---
+++ src/components/Component.js
@@ -0,0 +1,30 @@
+class Component {
+  $target;
+  state;
+
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.render();
+  }
+
+  setup() {}
+
+  template() {
+    return '';
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+
+  setEvent() {}
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.render();
+  }
+}
+
+export default Component;
