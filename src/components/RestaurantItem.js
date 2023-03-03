63637e9d275febb4b1d959aa874f25fec790d709
unknown <tkdrb15@gmail.com>
2023-03-03 13:38:17 +0900

    feat: RestaurantItem 컴포넌트를 만들다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

---
+++ src/components/RestaurantItem.js
@@ -0,0 +1,34 @@
+import { CATEGORY_TO_FILENAME } from '../constants/constants';
+
+class RestaurantItem {
+  constructor(restaurants) {
+    this.restaurants = restaurants;
+  }
+
+  makeRestaurantInfoItem(restaurant) {
+    const imageFile = CATEGORY_TO_FILENAME[restaurant.category];
+
+    return `
+      <li class="restaurant">
+        <div class="restaurant__category">
+            <img src="./${imageFile}.png" alt="${restaurant.category}" class="category-icon">
+        </div>
+        <div class="restaurant__info">
+            <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
+            <span class="restaurant__distance text-body">캠퍼스부터 ${restaurant.distance}분 내</span>
+            <p class="restaurant__description text-body">${restaurant.description}</p>
+        </div>
+      </li>
+      `;
+  }
+
+  template() {
+    return this.restaurants.reduce((result, restaurant) => result + this.makeRestaurantInfoItem(restaurant), '');
+  }
+
+  render($restaurantList) {
+    $restaurantList.insertAdjacentHTML('beforeend', this.template());
+  }
+}
+
+export default RestaurantItem;
