1abb15ffeaa932b1a2eeca7e9021fe2bde2af36c
hozzijeong <ghwls0905@naver.com>
2023-03-03 16:50:55 +0900

    test: dom 테스트 modal 창 띄우기 작성
        Co-authored-by: cruelladevil <dev.timetravel@gmail.com>

--- __tests__/dom.test.js
+++ __tests__/dom.test.js
@@ -6,22 +6,29 @@ import { screen, fireEvent } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import App from '../src/App';
 
-function Appdiv() {
-  const div = document.createElement('div');
-  const app = new App(div);
+function render() {
+  document.body.innerHTML = `
+    <div id="app"></div>
+    <div class="modal" title="modal">
+      <div class="modal-backdrop"></div>
+      <div class="modal-container"></div>
+    </div>
+  `;
 
-  return app;
+  const $app = document.getElementById('app');
+
+  new App($app);
 }
 
 describe('dom 테스트를 시작합니다.', () => {
-  test.skip('버튼 클릭시에 modal창이 띄워진다.', () => {
-    const container = Appdiv();
+  beforeEach(render);
 
-    const button = screen.getByLabelText(container, '음식점 추가');
+  test('버튼 클릭시에 modal창이 띄워진다.', () => {
+    const button = screen.getByLabelText('음식점 추가');
 
     fireEvent.click(button);
 
-    const addButton = screen.findByText('추가하기');
+    const addButton = screen.getByText('추가하기');
 
     expect(addButton).toBeInTheDocument();
   });
