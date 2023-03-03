001e1f106b125142160ef3944454c3aec7873c26
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-03 13:22:41 +0900

    fix: 음식점 거리 문구 "분 이내" 추가하여 수정

--- src/components/ListContainer.ts
+++ src/components/ListContainer.ts
@@ -27,7 +27,7 @@ class ListContainer extends Component {
       </div>
       <div class="restaurant__info">
         <h3 class="restaurant__name text-subtitle">${name}</h3>
-        <span class="restaurant__distance text-body">캠퍼스부터 ${distance}</span>
+        <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 이내</span>
         <p class="restaurant__description text-body">${description}</p>
       </div>
     </li>`;
