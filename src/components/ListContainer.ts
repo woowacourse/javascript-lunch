af262c6fe0ce28e592ab6dcb255f149636788c28
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-03 16:42:31 +0900

    refactor: 거리 등록 안할 시 rendering 안하게 구현

--- src/components/ListContainer.ts
+++ src/components/ListContainer.ts
@@ -24,11 +24,17 @@ class ListContainer extends Component {
         const { category, name, distance, description } = restaurant;
         return `<li class="restaurant">
       <div class="restaurant__category">
-        <img src=${ImageByCategory[category]} alt=${category} class="category-icon"/>
+        <img src=${
+          ImageByCategory[category]
+        } alt=${category} class="category-icon"/>
       </div>
       <div class="restaurant__info">
         <h3 class="restaurant__name text-subtitle">${name}</h3>
-        <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 이내</span>
+        ${
+          +distance
+            ? `<span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 이내</span>`
+            : ''
+        }
         <p class="restaurant__description text-body">${description}</p>
       </div>
     </li>`;
