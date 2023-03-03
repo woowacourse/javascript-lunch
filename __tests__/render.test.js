ae68c0bb83aaf8cc7f425c62a9992faf9a1fb171
Dain Lee <feb.dain@gmail.com>
2023-03-03 17:10:56 +0900

    test: 작동하지 않는 테스트 코드 삭제

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,17 +1,3 @@
 /**
  * @jest-environment jsdom
  */
-
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { $ } from "../src/util/selector";
-import { handleModalCancelButtonClick } from "../src/ui/modal";
-
-describe("모달창 닫기 테스트", () => {
-  test("취소하기 버튼을 클릭하면 모달창이 종료된다.", () => {
-    const modal = $(".modal");
-    handleModalCancelButtonClick();
-
-    expect(getByText(modal, "새로운 음식점")).not.toBeNull();
-  });
-});
