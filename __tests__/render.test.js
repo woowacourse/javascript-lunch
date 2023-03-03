69e0713250f3de8b495cd036e529291e26972540
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-03-03 14:40:46 +0900

    test: 모달 렌더링 테스트

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,3 +1,55 @@
+import { screen, fireEvent } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+import HeaderComponent from "../src/components/HeaderComponent";
+import ModalComponent from "../src/components/ModalComponent";
+import RestaurantsComponent from "../src/components/RestaurantsComponent";
+import dispatcher from "../src/domain/Dispatcher";
+import { RESTAURANT_ACTION } from "../src/abstracts/constants";
+import RestaurantComponent from "../src/components/RestaurantComponent";
+
 /**
  * @jest-environment jsdom
  */
+
+describe("모달 렌더링 테스트", () => {
+  beforeEach(() => {
+    document.body.innerHTML = "";
+  });
+  test("음식점 추가 버튼 클릭하면 모달 창이 열린다.", () => {
+    // given
+    const header = new HeaderComponent();
+    const modal = new ModalComponent();
+
+    document.body.innerHTML = `
+        <header-element></header-element>
+        <modal-element></modal-element>
+    `;
+
+    // when
+    fireEvent.click(screen.getByLabelText("음식점 추가"));
+
+    // then
+    expect(
+      document.querySelector(".modal").classList.contains("modal--open")
+    ).toBeTruthy();
+  });
+
+  test("취소하기 버튼 클릭하면 모달 창이 닫힌다.", () => {
+    //given
+    const header = new HeaderComponent();
+    const modal = new ModalComponent();
+    document.body.innerHTML = `
+        <header-element></header-element>
+        <modal-element></modal-element>
+    `;
+    header.show();
+
+    //when
+    fireEvent.click(screen.getByText("취소하기"));
+
+    //then
+    expect(
+      document.querySelector(".modal").classList.contains("modal--open")
+    ).toBeFalsy();
+  });
+});
