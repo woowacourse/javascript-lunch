d49a4aa21c8f576ba911186ad9de5cea57d3313e
woo-jk <201702042@o.cnu.ac.kr>
2023-03-03 17:32:28 +0900

    refactor: RestaurantFilter 클래스 함수들 줄바꿈 수정

--- src/domain/RestaurantFilter.ts
+++ src/domain/RestaurantFilter.ts
@@ -1,22 +1,13 @@
 import "../types/restaurant";
 
 const RestaurantFilter = {
-  categorizeRestaurants(
-    category: Category,
-    restaurants: RestaurantInfo[]
-  ): RestaurantInfo[] {
+  categorizeRestaurants(category: Category, restaurants: RestaurantInfo[]): RestaurantInfo[] {
     if (category === "전체") return restaurants;
-    return [...restaurants].filter(
-      (restaurant) => restaurant.category === category
-    );
+    return [...restaurants].filter((restaurant) => restaurant.category === category);
   },
 
-  sortRestaurants(
-    sortingWay: SortingWay,
-    restaurants: RestaurantInfo[]
-  ): RestaurantInfo[] {
-    if (sortingWay === "distance")
-      return [...restaurants].sort((a, b) => a.distance - b.distance);
+  sortRestaurants(sortingWay: SortingWay, restaurants: RestaurantInfo[]): RestaurantInfo[] {
+    if (sortingWay === "distance") return [...restaurants].sort((a, b) => a.distance - b.distance);
 
     return [...restaurants].sort((a, b) => (a.name > b.name ? 1 : -1));
   },
