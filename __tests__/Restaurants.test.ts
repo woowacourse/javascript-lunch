0bfdde99371c56bb9dc625849039169dd1ec9ad7
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-03 16:51:37 +0900

    test(Restaurants): 테스트 케이스 오류 수정 및 추가

--- __tests__/Restaurants.test.ts
+++ __tests__/Restaurants.test.ts
@@ -1,24 +1,28 @@
 import Restaurants from '../src/domain/Restaurants';
 import { Restaurant } from '../src/types';
 
-test('Restaurants.addRestaurant', () => {
-  const restaurants = new Restaurants();
+describe('Restaurants.addRestaurant', () => {
+  test('음식점을 추가하면, 음식점 리스트에 맨 뒤에 추가된다.', () => {
+    const restaurants = new Restaurants();
 
-  const restaurant: Restaurant = {
-    category: '한식',
-    name: '돈카라',
-    distance: '10',
-  };
+    const restaurant: Restaurant = {
+      category: '한식',
+      name: '돈카라',
+      distance: '10',
+    };
 
-  restaurants.addRestaurant(restaurant);
-  const addedRestaurant = restaurants.getRestaurants()[0];
+    restaurants.addRestaurant(restaurant);
 
-  expect(addedRestaurant).toEqual(restaurant);
+    const restaurantList = restaurants.getRestaurants();
+    const addedRestaurant = restaurantList[restaurantList.length - 1];
+
+    expect(addedRestaurant).toEqual(restaurant);
+  });
 });
 
 describe('Restaurants.getFilteredRestaurantsByCategory', () => {
   test('선택된 카테고리와 일치하는 음식점의 목록을 반환한다.', () => {
-    const dummy: Restaurant[] = [
+    const dummyRestaurants: Restaurant[] = [
       {
         category: '한식',
         name: '맛이쪙 돈까스',
@@ -37,8 +41,7 @@ describe('Restaurants.getFilteredRestaurantsByCategory', () => {
         distance: '5',
       },
     ];
-    const restaurants = new Restaurants();
-    dummy.forEach((v) => restaurants.addRestaurant(v));
+    const restaurants = new Restaurants(dummyRestaurants);
 
     const filterdRestaurants = restaurants.getFilteredRestaurantsByCategory('한식');
     const expected: Restaurant[] = [
@@ -58,11 +61,9 @@ describe('Restaurants.getFilteredRestaurantsByCategory', () => {
 
     expect(expected).toEqual(filterdRestaurants);
   });
-});
 
-describe('Restaurants.getSortedRestaurantsByName', () => {
-  test('', () => {
-    const dummy: Restaurant[] = [
+  test(`선택된 카테고리가 '전체'라면 모든 음식점의 목록을 반환한다.`, () => {
+    const dummyRestaurants: Restaurant[] = [
       {
         category: '한식',
         name: '맛이쪙 돈까스',
@@ -81,10 +82,38 @@ describe('Restaurants.getSortedRestaurantsByName', () => {
         distance: '5',
       },
     ];
-    const restaurants = new Restaurants();
-    dummy.forEach((v) => restaurants.addRestaurant(v));
+    const restaurants = new Restaurants(dummyRestaurants);
 
-    const sortedRestaurants = restaurants.getSortedRestaurantsByName();
+    const filterdRestaurants = restaurants.getFilteredRestaurantsByCategory('전체');
+
+    expect(filterdRestaurants).toEqual(dummyRestaurants);
+  });
+});
+
+describe('Restaurants.getSortedRestaurantsByName', () => {
+  test('음식점 리스트를 받아, 이름순으로 정렬된 리스트를 반환한다.', () => {
+    const restaurants = new Restaurants();
+    const dummyRestaurants: Restaurant[] = [
+      {
+        category: '한식',
+        name: '맛이쪙 돈까스',
+        distance: '10',
+        description: '평양 출신의 할머니가 수십 년간 운영해온 비지 전문점',
+        link: 'www.naver.com',
+      },
+      {
+        category: '한식',
+        name: '김치찌개 장인',
+        distance: '5',
+      },
+      {
+        category: '일식',
+        name: '돈카라',
+        distance: '5',
+      },
+    ];
+
+    const sortedRestaurants = restaurants.getSortedRestaurantsByName(dummyRestaurants);
     const expected: Restaurant[] = [
       {
         category: '한식',
@@ -110,8 +139,9 @@ describe('Restaurants.getSortedRestaurantsByName', () => {
 });
 
 describe('Restaurants.getSortedRestaurantsByDistance', () => {
-  test('', () => {
-    const dummy: Restaurant[] = [
+  test('음식점 리스트를 받아, 거리순으로 정렬된 리스트를 반환한다.', () => {
+    const restaurants = new Restaurants();
+    const dummyRestaurants: Restaurant[] = [
       {
         category: '한식',
         name: '맛이쪙 돈까스',
@@ -130,10 +160,8 @@ describe('Restaurants.getSortedRestaurantsByDistance', () => {
         distance: '5',
       },
     ];
-    const restaurants = new Restaurants();
-    dummy.forEach((v) => restaurants.addRestaurant(v));
 
-    const sortedRestaurants = restaurants.getSortedRestaurantsByDistance();
+    const sortedRestaurants = restaurants.getSortedRestaurantsByDistance(dummyRestaurants);
     const expected: Restaurant[] = [
       {
         category: '한식',
