ad51bd1e1f34a48bb4599fcfd2a14a2e0ec79dd0
unknown <tkdrb15@gmail.com>
2023-03-03 13:37:21 +0900

    feat: Modal과 관련된 이벤트 처리 기능을 구현하다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,7 @@
 class Modal {
-  constructor($target) {
+  constructor($target, restaurantsList) {
     this.$target = $target;
+    this.restaurantsList = restaurantsList;
   }
 
   template() {
@@ -11,7 +12,6 @@ class Modal {
         <h2 class="modal-title text-title">새로운 음식점</h2>
         <form>
 
-          <!-- 카테고리 -->
           <div class="form-item form-item--required">
             <label for="category text-caption">카테고리</label>
             <select name="category" id="category" required>
@@ -25,13 +25,11 @@ class Modal {
             </select>
           </div>
 
-          <!-- 음식점 이름 -->
           <div class="form-item form-item--required">
             <label for="name text-caption">이름</label>
             <input type="text" name="name" id="name" required>
           </div>
 
-          <!-- 거리 -->
           <div class="form-item form-item--required">
             <label for="distance text-caption">거리(도보 이동 시간) </label>
             <select name="distance" id="distance" required>
@@ -44,21 +42,18 @@ class Modal {
             </select>
           </div>
 
-          <!-- 설명 -->
           <div class="form-item">
             <label for="description text-caption">설명</label>
             <textarea name="description" id="description" cols="30" rows="5"></textarea>
             <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
           </div>
 
-          <!-- 링크 -->
           <div class="form-item">
             <label for="link text-caption">참고 링크</label>
             <input type="text" name="link" id="link">
             <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
           </div>
 
-          <!-- 취소/추가 버튼 -->
           <div class="button-container">
             <button type="button" class="button button--secondary text-caption">취소하기</button>
             <button class="button button--primary text-caption">추가하기</button>
@@ -70,12 +65,55 @@ class Modal {
   }
 
   render() {
-    this.$target.replaceChildren();
-    this.$target.insertAdjacentHTML('beforeend', this.template());
-    this.setEvent();
+    if (!document.querySelector(`.modal`)) {
+      this.$target.insertAdjacentHTML('beforeend', this.template());
+    }
+
+    this.setSubmitEvent();
+    this.setModalCloseEvent();
   }
 
-  setEvent() {}
+  setSubmitEvent() {
+    const _this = this;
+
+    const $modalForm = document.querySelector('.modal form');
+    $modalForm.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      const category = this.category.value;
+      const name = this.name.value;
+      const distance = this.distance.value;
+      const description = this.description.value;
+      const link = this.link.value;
+
+      const restaurant = _this.makeRestaurant({ category, name, distance, description, link });
+      _this.restaurantsList.restaurants.add(restaurant);
+      localStorage.setItem('restaurants', JSON.stringify(_this.restaurantsList.restaurants.restaurants));
+      _this.restaurantsList.setState(restaurant);
+      document.querySelector('.modal').remove();
+    });
+  }
+
+  setModalCloseEvent() {
+    const $cancelButton = document.querySelector('.modal .button--secondary');
+    $cancelButton.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector('.modal').remove();
+    });
+  }
+
+  makeRestaurant(restaurant) {
+    const { category, name, distance, description, link } = restaurant;
+
+    return {
+      category: category,
+      name: name,
+      distance: distance,
+      description: description,
+      link: link,
+    };
+  }
 }
 
 export default Modal;
