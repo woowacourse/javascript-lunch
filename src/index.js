10279135f3c072b6ff826154eae2a6c50ad57bfa
unknown <tkdrb15@gmail.com>
2023-03-03 13:42:05 +0900

    feat: App 실행점을 정의하다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import './css/index.css';
+import '../templates/add-button.png';
+import '../templates/category-western.png';
+import '../templates/category-asian.png';
+import '../templates/category-chinese.png';
+import '../templates/category-etc.png';
+import '../templates/category-japanese.png';
+import '../templates/category-korean.png';
+import '../templates/favorite-icon-filled.png';
+import '../templates/favorite-icon-lined.png';
+import Header from './components/Header';
+import Modal from './components/Modal';
+import RestaurantFilter from './components/RestaurantFilter';
+import RestaurantsList from './components/RestaurantsList';
 import Restaurants from './domain/Restaurants';
 
 if (localStorage.getItem('restaurants') === null) {
@@ -5,3 +19,14 @@ if (localStorage.getItem('restaurants') === null) {
 }
 
 const restaurants = new Restaurants(JSON.parse(localStorage.getItem('restaurants')));
+
+const $header = document.querySelector('.gnb');
+const $main = document.querySelector('main');
+
+const header = new Header($header);
+const restaurantFilter = new RestaurantFilter($main);
+const restaurantsList = new RestaurantsList($main, restaurants);
+const modal = new Modal($main, restaurantsList);
+
+header.render(modal);
+restaurantFilter.render(restaurantsList);
