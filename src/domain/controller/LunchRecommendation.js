d5c585b2d289043c0482ac292b7f4b96a4131674
gyeongza <gyeongza@naver.com>
2023-03-03 16:59:53 +0900

    feat(Controller): localStorage/음식점 정보를 유지하는 기능 구현
    Co-authored-by: Gilpop8663 <wolfye@naver.com>

--- src/domain/controller/LunchRecommendation.js
+++ src/domain/controller/LunchRecommendation.js
@@ -7,10 +7,48 @@ class LunchRecommendation {
   #restaurants = new RestaurantList();
 
   constructor() {
+    const userList = JSON.parse(localStorage.getItem('userList'));
+    console.log(userList);
+
+    if (userList) {
+      userList.forEach((restaurant) => {
+        const objectToClass = new Restaurant(restaurant);
+        this.#restaurants.add(objectToClass);
+      });
+    }
+
     webView.renderRestaurantList(this.#restaurants.getList('전체', 'name'));
   }
 
   play() {
+    this.modalEvent();
+    this.addRestaurantEvent();
+  }
+
+  modalEvent() {
+    $$$('lunch-header', '#openModal').addEventListener(
+      'click',
+      webView.toggleModal
+    );
+
+    $$$('add-restaurant-modal', '#cancleModal').addEventListener(
+      'click',
+      webView.toggleModal
+    );
+
+    $$$('add-restaurant-modal', '#modalBackdrop').addEventListener(
+      'click',
+      webView.toggleModal
+    );
+
+    document.addEventListener('keydown', (event) => {
+      if (event.code === 'Escape') {
+        webView.toggleModal();
+      }
+    });
+  }
+
+  addRestaurantEvent() {
     $$$('add-restaurant-modal', '#addRestraunt').addEventListener(
       'click',
       (event) => {
@@ -32,32 +70,22 @@ class LunchRecommendation {
           link,
         });
         this.#restaurants.add(restaurant);
+        const restaurants = this.#restaurants.getList('전체', 'name');
+
+        const classToObject = restaurants.reduce(
+          (accumulator, currentRestaurant) => {
+            return [...accumulator, currentRestaurant.getInfo()];
+          },
+          []
+        );
+
+        const restaurantsString = JSON.stringify(classToObject);
+        window.localStorage.setItem('userList', restaurantsString);
         webView.toggleModal();
         webView.resetForm();
-        webView.renderRestaurantList(this.#restaurants.getList('전체', 'name'));
+        webView.renderRestaurantList(restaurants);
       }
     );
-
-    $$$('lunch-header', '#openModal').addEventListener(
-      'click',
-      webView.toggleModal
-    );
-
-    $$$('add-restaurant-modal', '#cancleModal').addEventListener(
-      'click',
-      webView.toggleModal
-    );
-
-    $$$('add-restaurant-modal', '#modalBackdrop').addEventListener(
-      'click',
-      webView.toggleModal
-    );
-
-    document.addEventListener('keydown', (event) => {
-      if (event.code === 'Escape') {
-        webView.toggleModal();
-      }
-    });
   }
 }
 
