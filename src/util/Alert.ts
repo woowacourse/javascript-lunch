accf65b836867a131955102aa2370675ec61a560
NaveOWO <pianowell96@gmail.com>
2023-03-03 14:13:06 +0900

    feat: 커스텀 Alert 생성 기능 구현

---
+++ src/util/Alert.ts
@@ -0,0 +1,25 @@
+import { $ } from "./querySelector";
+
+class Alert {
+  #isOpen;
+  #targetElement;
+  constructor(id: string) {
+    this.#isOpen = false;
+    this.#targetElement = $(`${id}`);
+  }
+
+  show(message: string) {
+    if (this.#isOpen) return;
+    this.#targetElement?.classList.remove("hidden");
+    if (this.#targetElement) this.#targetElement.textContent = message;
+    this.#isOpen = true;
+  }
+
+  hide() {
+    if (!this.#isOpen) return;
+    this.#targetElement?.classList.add("hidden");
+    this.#isOpen = false;
+  }
+}
+
+export default Alert;
