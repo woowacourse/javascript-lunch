4f9905cf43f7e101a34857f1613d1fb08c3c4b4a
Dahye <06robin11@gmail.com>
2023-03-03 14:05:26 +0900

    chore(dom): 사용 안하는 함수 삭제

--- src/utils/Dom.ts
+++ src/utils/Dom.ts
@@ -2,38 +2,3 @@ export const $ = (selector: string, target = document) =>
   target.querySelector(selector);
 export const $$ = (selector: string, target = document) =>
   target.querySelectorAll(selector);
-
-interface Atrribute {
-  id?: string;
-  class?: string[];
-  name?: string;
-  required?: boolean;
-  src?: string;
-  alt?: string;
-  ["aria-label"]?: string;
-  type?: string;
-  for?: string;
-  cols?: string;
-  rows?: string;
-  value?: string;
-}
-
-export const createElement = (tag: string, atrribute: Atrribute = {}) => {
-  const element = document.createElement(tag);
-
-  Object.entries(atrribute).forEach(([attributeName, value]) => {
-    if (attributeName === "class") {
-      value.forEach((className: string) => {
-        element.classList.add(className);
-      });
-      return;
-    }
-    element.setAttribute(attributeName, value);
-  });
-
-  return element;
-};
-
-export const render = (element: Element) => {
-  $(".body")?.append(element);
-};
