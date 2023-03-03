4dc4f676bf9272599a212d5f4e999ab05b01e940
hozzijeong <ghwls0905@naver.com>
2023-03-03 14:20:42 +0900

    feat: header 기능 구현 및 음식점 추가 버튼 이벤트 부착
     Co-authored-by: cruelladevil <dev.timetravel@gmail.com>

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import addButton from '../../templates/add-button.png';
+
+export default function Header($root, eventListener) {
+  const $header = document.createElement('header');
+  $header.className = 'gnb';
+
+  this.init = () => {
+    this.render();
+    $header.addEventListener('click', eventListener);
+    $root.appendChild($header);
+  };
+
+  this.render = () => {
+    $header.innerHTML = `
+    <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+     <button type="button" class="gnb__button" aria-label="음식점 추가">
+       <img src="${addButton}" alt="음식점 추가">
+     </button>
+   `;
+  };
+
+  this.init();
+}
