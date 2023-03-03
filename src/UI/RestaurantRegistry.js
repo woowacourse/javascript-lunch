3475009a476fc061e4ec7964e8a36d630dee94df
regularPark <srk99279@naver.com>
2023-03-03 16:12:06 +0900

    💄 style: 이미지 파일 적용

--- src/UI/RestaurantRegistry.js
+++ src/UI/RestaurantRegistry.js
@@ -2,15 +2,31 @@ import { $ } from "../utils/Dom";
 
 export default class RestaurantRegistry {
   appendRestaurant(restaurantInfo) {
+    const category = {
+      한식: "./category-korean.png",
+      일식: "./category-japanese.png",
+      양식: "./category-western.png",
+      중식: "./category-chinese.png",
+      아시안: "./category-asian.png",
+      기타: "./category-etc.png",
+    };
     const template = `
     <li class="restaurant">
           <div class="restaurant__category">
-            <img src="" alt="" class="category-icon">
+            <img src="${category[restaurantInfo.category]}" alt="${
+      restaurantInfo.category
+    }" class="category-icon">
           </div>
           <div class="restaurant__info">
-            <h3 class="restaurant__name text-subtitle">${restaurantInfo.name}</h3>
-            <span class="restaurant__distance text-body">${restaurantInfo.distance}</span>
-            <p class="restaurant__description text-body">${restaurantInfo.description}</p>
+            <h3 class="restaurant__name text-subtitle">${
+              restaurantInfo.name
+            }</h3>
+            <span class="restaurant__distance text-body">${
+              restaurantInfo.distance
+            }</span>
+            <p class="restaurant__description text-body">${
+              restaurantInfo.description
+            }</p>
           </div>
     </li>
     `;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import { App } from "./App";
 import Css from "./css/style.css";
 import "./assets/add-button.png";
+import "./assets/category-asian.png";
+import "./assets/category-korean.png";
+import "./assets/category-chinese.png";
+import "./assets/category-etc.png";
+import "./assets/category-japanese.png";
+import "./assets/category-western.png";
+import "./assets/favorite-icon-filled.png";
+import "./assets/favorite-icon-lined.png";
 
 new App();
