cf420cc38860765a0dabdcc16415fad03ef10083
unknown <tkdrb15@gmail.com>
2023-03-03 13:39:52 +0900

    feat: category값을 이미지 파일명으로 치환하는 상수를 만들다
    Co-authored-by: yeopto <gunyeop530@gmail.com>

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,3 +7,12 @@ export const VALUE = {
     nameOrder: 'name',
   },
 };
+
+export const CATEGORY_TO_FILENAME = {
+  한식: 'category-korean',
+  중식: 'category-chinese',
+  일식: 'category-japanese',
+  양식: 'category-western',
+  아시안: 'category-asian',
+  기타: 'category-etc',
+};
