36777339ef05294d717a17b301862ed33f2a6d53
hae-on <solbi2004@naver.com>
2023-03-04 12:35:02 +0900

    refactor: Object freeze를 Readonly로 변경

--- src/constant/Constants.ts
+++ src/constant/Constants.ts
@@ -1,21 +1,21 @@
-export const CategoryImg = Object.freeze({
+export const CategoryImg: Readonly<{ [key: string]: string }> = {
   아시안: "category-asian.png",
   한식: "category-korean.png",
   양식: "category-western.png",
   중식: "category-chinese.png",
   일식: "category-japanese.png",
   기타: "category-etc.png",
-});
+};
 
-export const OptionValue = Object.freeze({
+export const OptionValue: Readonly<{ [key: string]: string }> = {
   TOTAL: "전체",
   NAME_ORDER: "이름순",
   DISTANCE_ORDER: "거리순",
-});
+};
 
-export const Constants = Object.freeze({
+export const Constants: Readonly<{ [key: string]: string }> = {
   RESTAURANT_LIST: "restuarantList",
   KOREAN: "ko-KR",
   CATEGORY_FILTER: "category-filter",
   SORTING_FILTER: "sorting-filter",
-});
+};
