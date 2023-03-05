535194ea9ce6676a0a39dfb571816d6b03125705
Dahye <06robin11@gmail.com>
2023-03-05 15:11:52 +0900

    refactor(식당ticket):List와 상응하게 Item으로 변경

--- src/App.ts
+++ src/App.ts
@@ -2,7 +2,7 @@ import Header from "./component/disposable/Header";
 import Modal from "./component/disposable/Modal";
 import RestaurantList from "./component/disposable/RestaurantList";
 import SelectContainer from "./component/disposable/SelectContainer";
-import RestaurantTicket from "./component/reusable/RestaurantTicket";
+import RestaurantItem from "./component/reusable/RestaurantItem";
 import { Constants, OptionValue } from "./constant/Constants";
 import restaurantListHandler from "./domain/restaurantListHandler";
 import { Restaurant } from "./type/type";
@@ -56,7 +56,7 @@ class App {
     RestaurantList.replaceTemplate(
       this.restaurantList
         .map((restaurant: Restaurant) =>
-          new RestaurantTicket(restaurant).template()
+          new RestaurantItem(restaurant).template()
         )
         .join("")
     );
