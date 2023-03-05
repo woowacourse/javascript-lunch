bc8c2814bb5aaafcaca84ebb9660a33ff8064c7a
yeopto <gunyeop530@gmail.com>
2023-03-06 02:09:47 +0900

    refactor: 상수를 객체동결시키고 js파일로 변경하다

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,6 @@
-export const VALUE = {
+import { deepFreeze } from '../utils/common';
+
+export const VALUE = deepFreeze({
   catgory: {
     all: '전체',
   },
@@ -6,13 +8,13 @@ export const VALUE = {
   sortType: {
     nameOrder: 'name',
   },
-};
+});
 
-export const CATEGORY_TO_FILENAME = {
+export const CATEGORY_TO_FILENAME = Object.freeze({
   한식: 'category-korean',
   중식: 'category-chinese',
   일식: 'category-japanese',
   양식: 'category-western',
   아시안: 'category-asian',
   기타: 'category-etc',
-};
+});
