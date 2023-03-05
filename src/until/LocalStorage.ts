4bfa89ddcbdbda27f3e9983acd7335bb5e12173e
chsua <csj1919@naver.com>
2023-03-06 07:53:55 +0900

    refactor: local storage 관련 함수 분리

---
+++ src/until/LocalStorage.ts
@@ -0,0 +1,19 @@
+import { RestaurantType } from "../Template";
+
+export const LocalStorage = {
+  dataName: "restaurantList",
+
+  setDate(data: RestaurantType[]) {
+    localStorage.setItem(this.dataName, JSON.stringify(data));
+  },
+
+  getData() {
+    const localList = localStorage.getItem(this.dataName);
+    return localList && JSON.parse(localList);
+  },
+
+  addData(data: RestaurantType[], newData: RestaurantType) {
+    const inputData = [newData, ...data];
+    this.setDate(inputData);
+  },
+};
