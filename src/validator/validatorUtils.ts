4ef1fc6a16f798182ed46c35f00d454b7a2884c5
sh981013s <sh981013s@gmail.com>
2023-03-06 01:11:50 +0900

    refactor: console 문 제거

--- src/validator/validatorUtils.ts
+++ src/validator/validatorUtils.ts
@@ -6,23 +6,18 @@ interface IValidatorUtils {
 
 export const validatorUtils: IValidatorUtils = {
   name: (input: string): boolean => {
-    console.log(regex.nameInputLength.test(input), input, 'name');
     return regex.nameInputLength.test(input);
   },
   category: (input: string): boolean => {
-    console.log(input !== '', input, 'category');
     return input !== '';
   },
   distance: (input: string): boolean => {
-    console.log(input !== '', input, 'distance');
     return input !== '';
   },
   description: (input: string): boolean => {
-    console.log(regex.descriptionInputLength.test(input), input, 'description');
     return regex.descriptionInputLength.test(input);
   },
   link: (input: string): boolean => {
-    console.log(input === '' || regex.urlInput.test(input), input, 'link');
     return regex.urlInput.test(input);
   },
 };
