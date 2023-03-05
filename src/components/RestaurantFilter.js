96065d3eba5d338d5faba0704ae8f0bf063aa88a
yeopto <gunyeop530@gmail.com>
2023-03-05 21:55:53 +0900

    refactor: 템플릿 공백을 삭제하다

--- src/components/RestaurantFilter.js
+++ src/components/RestaurantFilter.js
@@ -16,7 +16,6 @@ class RestaurantFilter {
           <option value="아시안">아시안</option>
           <option value="기타">기타</option>
         </select>
-
         <select name="sorting" id="sorting-filter" class="restaurant-filter">
           <option value="name">이름순</option>
           <option value="distance">거리순</option>
