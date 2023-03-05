b1971bf6c594a79f439248c6961ed67dfdfda0fc
yeopto <gunyeop530@gmail.com>
2023-03-05 21:43:11 +0900

    refactor: Modal 컴포넌트를 수정하다
    모달창을 토글 방식으로 변경, render와 setEvent를 분리, setEvent 인자 callback을 받도록 변경

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 class Modal {
-  constructor($target, restaurantsList) {
+  constructor($target) {
     this.$target = $target;
-    this.restaurantsList = restaurantsList;
+    this.render();
   }
 
   template() {
@@ -11,7 +11,6 @@ class Modal {
       <div class="modal-container">
         <h2 class="modal-title text-title">새로운 음식점</h2>
         <form>
-
           <div class="form-item form-item--required">
             <label for="category text-caption">카테고리</label>
             <select name="category" id="category" required>
@@ -24,12 +23,10 @@ class Modal {
               <option value="기타">기타</option>
             </select>
           </div>
-
           <div class="form-item form-item--required">
             <label for="name text-caption">이름</label>
             <input type="text" name="name" id="name" required>
           </div>
-
           <div class="form-item form-item--required">
             <label for="distance text-caption">거리(도보 이동 시간) </label>
             <select name="distance" id="distance" required>
@@ -41,19 +38,16 @@ class Modal {
               <option value="30">30분 내</option>
             </select>
           </div>
-
           <div class="form-item">
             <label for="description text-caption">설명</label>
             <textarea name="description" id="description" cols="30" rows="5"></textarea>
             <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
           </div>
-
           <div class="form-item">
             <label for="link text-caption">참고 링크</label>
             <input type="text" name="link" id="link">
             <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
           </div>
-
           <div class="button-container">
             <button type="button" class="button button--secondary text-caption">취소하기</button>
             <button class="button button--primary text-caption">추가하기</button>
@@ -64,61 +58,46 @@ class Modal {
     `;
   }
 
-  render() {
-    if (!document.querySelector(`.modal`)) {
-      this.$target.insertAdjacentHTML('beforeend', this.template());
-    }
+  toggleModalOpen() {
+    const $modal = document.querySelector('.modal');
 
-    this.setSubmitEvent();
-    this.setModalCloseEvent();
+    $modal.classList.toggle('modal--open');
   }
 
-  setSubmitEvent() {
+  render() {
+    this.$target.insertAdjacentHTML('beforeend', this.template());
+
+    this.toggleModalOpen();
+  }
+
+  setSubmitEvent(setStateCallback, addCallback) {
     const $modalForm = document.querySelector('.modal form');
 
-    $modalForm.addEventListener('submit', e => {
-      e.preventDefault();
-
-      const category = document.querySelector('#category').value;
-      const name = document.querySelector('#name').value;
-      const distance = document.querySelector('#distance').value;
-      const description = document.querySelector('#description').value;
-      const link = document.querySelector('#link').value;
-
-      const restaurant = this.makeRestaurant({ category, name, distance, description, link });
-
-      this.changeRestaurantsState(restaurant);
-
-      document.querySelector('.modal').remove();
+    $modalForm.addEventListener('submit', () => {
+      this.changeRestaurantsState(this.makeNewRestaurant(), setStateCallback, addCallback);
     });
   }
 
-  changeRestaurantsState(restaurant) {
-    this.restaurantsList.restaurants.add(restaurant);
+  makeNewRestaurant() {
+    const category = document.querySelector('#category').value;
+    const name = document.querySelector('#name').value;
+    const distance = document.querySelector('#distance').value;
+    const description = document.querySelector('#description').value;
+    const link = document.querySelector('#link').value;
 
-    this.restaurantsList.setState(restaurant);
+    return { category, name, distance, description, link };
+  }
+
+  changeRestaurantsState(restaurant, setStateCallback, addCallback) {
+    setStateCallback(addCallback(restaurant));
+
+    this.toggleModalOpen();
   }
 
   setModalCloseEvent() {
     const $cancelButton = document.querySelector('.modal .button--secondary');
 
-    $cancelButton.addEventListener('click', e => {
-      e.preventDefault();
-
-      document.querySelector('.modal').remove();
-    });
-  }
-
-  makeRestaurant(restaurant) {
-    const { category, name, distance, description, link } = restaurant;
-
-    return {
-      category: category,
-      name: name,
-      distance: distance,
-      description: description,
-      link: link,
-    };
+    $cancelButton.addEventListener('click', this.toggleModalOpen);
   }
 }
 
