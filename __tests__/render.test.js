32d5b4a70e5650ace7bb0a7fb08edbb2695f6168
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-03-05 20:06:31 +0900

    refactor(render.test): 중복되는 코드 리팩터링

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -2,25 +2,23 @@ import { screen, fireEvent } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 import HeaderComponent from "../src/components/HeaderComponent";
 import ModalComponent from "../src/components/modal/ModalComponent";
+import RestaurantAddFormComponent from "../src/components/modal/RestaurantAddFormComponent";
 
 /**
  * @jest-environment jsdom
  */
 
 describe("모달 렌더링 테스트", () => {
-  beforeEach(() => {
-    document.body.innerHTML = "";
-  });
+  //given
+  const header = new HeaderComponent();
+  const modal = new ModalComponent();
+  const form = new RestaurantAddFormComponent();
+  document.body.innerHTML = `
+      <header-element></header-element>
+      <modal-element></modal-element>
+  `;
+
   test("음식점 추가 버튼 클릭하면 모달 창이 열린다.", () => {
-    // given
-    const header = new HeaderComponent();
-    const modal = new ModalComponent();
-
-    document.body.innerHTML = `
-        <header-element></header-element>
-        <modal-element></modal-element>
-    `;
-
     // when
     fireEvent.click(screen.getByLabelText("음식점 추가"));
 
@@ -31,15 +29,6 @@ describe("모달 렌더링 테스트", () => {
   });
 
   test("취소하기 버튼 클릭하면 모달 창이 닫힌다.", () => {
-    //given
-    const header = new HeaderComponent();
-    const modal = new ModalComponent();
-    document.body.innerHTML = `
-        <header-element></header-element>
-        <modal-element></modal-element>
-    `;
-    header.show();
-
     //when
     fireEvent.click(screen.getByText("취소하기"));
 
