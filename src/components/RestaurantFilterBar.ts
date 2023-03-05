1fa820df3c78520350fe11e979c7b544b2f6743d
guridaek <xylitol79@gmail.com>
2023-03-05 21:20:43 +0900

    refactor: Restaurant 관련 정보 상수화

--- src/components/RestaurantFilterBar.ts
+++ src/components/RestaurantFilterBar.ts
@@ -1,20 +1,17 @@
+import { FILTER_CATEGORY, SORT_CONDITION } from '../data/Constants';
 import { FilterCategory, SortCondition } from '../domain/RestaurantList';
 import { $ } from '../util/dom';
-
-const categoryList: FilterCategory[] = ['전체', '한식', '중식', '일식', '양식', '아시안', '기타'];
-
-const sortConditionList: SortCondition[] = ['이름', '거리'];
 
 class RestaurantFilterBar {
   template = () => `
     <section class="restaurant-filter-container">
-      <select name="category" id="category-filter" class="restaurant-filter">${categoryList
-        .map((category) => `<option value="${category}">${category}</option>`)
-        .join('')}
+      <select name="category" id="category-filter" class="restaurant-filter">${FILTER_CATEGORY.map(
+        (category) => `<option value="${category}">${category}</option>`,
+      ).join('')}
       </select>
-      <select name="sorting" id="sorting-filter" class="restaurant-filter">${sortConditionList
-        .map((condition) => `<option value="${condition}">${condition}순</option>`)
-        .join('')}
+      <select name="sorting" id="sorting-filter" class="restaurant-filter">${SORT_CONDITION.map(
+        (condition) => `<option value="${condition}">${condition}순</option>`,
+      ).join('')}
       </select>
     </section>`;
 
