2840afc821ba55700be769e22c8d4f96252cf7e9
sh981013s <sh981013s@gmail.com>
2023-03-06 01:12:13 +0900

    refactor: navbar 컴포넌트 props 인터페이스 구현

--- src/components/TopNavBar.ts
+++ src/components/TopNavBar.ts
@@ -1,6 +1,10 @@
 import Component from '@res/core/Component';
 
-class TopNavBar extends Component {
+interface ITopNavBarProps {
+  toggleModal: () => void;
+}
+
+class TopNavBar extends Component<ITopNavBarProps> {
   template() {
     return `<h1 class="gnb__title text-title">점심 뭐 먹지</h1>
     <button type="button" class="gnb__button nav-add-button" aria-label="음식점 추가">
