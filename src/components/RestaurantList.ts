18567a9abaece2faf20b0094780783d5694e414a
chsua <csj1919@naver.com>
2023-03-06 08:16:41 +0900

    refactor: restaurantList에서 초기  local data 관리 및 정리
    - 로컬 데이터가 있으면 로컬데이터로 현재 레스토랑 리스트 재생성
    - 로컬 데이터가 없으면 현재 레스토랑 리스트 로컬에 저장
    - 분리된 함수 삭제

--- src/components/RestaurantList.ts
+++ src/components/RestaurantList.ts
@@ -1,81 +1,21 @@
-import { Restaurant, RestaurantType } from "./Restaurant";
-interface listInterface {
-  originList: Restaurant[];
-  filterState: string;
-  sortState: string;
-  defaultList: (restaurantList: RestaurantType[]) => void;
-  addRestaurant: (restaurant: RestaurantType) => void;
-  // template(restaurantList: Restaurant[]): string;
-  // listUp(category: string, priority: string): Restaurant[];
-  // filter(category: string): Restaurant[];
-  // sort(restaurantList: Restaurant[], priority: string): Restaurant[];
+import { RestaurantType } from "../Template";
+import { LocalData } from "../until/LocalData";
+
+interface RestaurantListType {
+  list: RestaurantType[];
+  settingList: () => void;
 }
 
-const RestaurantList: listInterface = {
-  originList: [],
-  filterState: "전체",
-  sortState: "name",
+export const RestaurantList: RestaurantListType = {
+  list: [],
 
-  defaultList(restaurantList: RestaurantType[]) {
-    const list = restaurantList.map((item) => {
-      const restaurant = new Restaurant(item);
-      return restaurant;
-    });
-
-    const localList = localStorage.getItem("restaurantList");
-    if (localList !== null) {
-      this.originList = JSON.parse(localList).map((item: RestaurantType) => {
-        const restaurant = new Restaurant(item);
-        return restaurant;
-      });
-    } else {
-      localStorage.setItem("restaurantList", JSON.stringify(restaurantList));
-      this.originList = list;
+  settingList() {
+    const isLocalData = LocalData.getData();
+    if (isLocalData) {
+      this.list = isLocalData;
+      return;
     }
+    LocalData.setDate(this.list);
+    return;
   },
-
-  // addRestaurant(restaurant: RestaurantType) {
-  //   const newRestaurantList = [
-  //     new Restaurant(restaurant),
-  //     ...this.originList,
-  //   ].map((item: Restaurant) => item.getRestaurant());
-  //   localStorage.setItem("restaurantList", JSON.stringify(newRestaurantList));
-  //   this.originList = [new Restaurant(restaurant), ...this.originList];
-  // },
-
-  // template(restaurantList): string {
-  //   return `<ul class='restaurant-list'>
-  //   ${restaurantList.map((restaurant) => restaurant.template()).join("")}
-  //   </ul>`;
-  // },
-
-  // listUp(category, priority) {
-  //   return this.sort(this.filter(category), priority);
-  // },
-
-  // filter(category) {
-  //   this.filterState = category;
-  //   if (category === "전체") return this.originList;
-
-  //   return this.originList.filter(
-  //     (item) => item.getRestaurant().category === category && item
-  //   );
-  // },
-  // sort(restaurantList, priority) {
-  //   this.sortState = priority;
-  //   if (priority === "distance") {
-  //     const result = restaurantList.sort((current, next) => {
-  //       return current.getRestaurant().takeTime - next.getRestaurant().takeTime;
-  //     });
-
-  //     return result;
-  //   }
-
-  //   const result = restaurantList.sort((current, next) => {
-  //     return current.getRestaurant().name > next.getRestaurant().name ? 1 : -1;
-  //   });
-
-  //   return result;
-  // },
 };
-export default RestaurantList;
