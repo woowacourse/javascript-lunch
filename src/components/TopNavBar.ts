e97c12f165c442054003edf7a3d8f02ce12b0a31
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-06 21:36:26 +0900

    refactor: 스크린리더를 반영한 음식점 추가 버튼 alt 값 변경

--- src/components/TopNavBar.ts
+++ src/components/TopNavBar.ts
@@ -24,7 +24,7 @@ class TopNavBar extends Component {
   template() {
     return `<h1 class="gnb__title text-title">점심 뭐 먹지</h1>
     <button type="button" class="gnb__button add-button" aria-label="음식점 추가">
-      <img src='./add-button.png' alt="음식점 추가" />
+      <img src='./add-button.png' alt="음식점 추가를 위한 입력 버튼" />
     </button>
     `;
   }
