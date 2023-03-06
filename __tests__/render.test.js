290b677cc186679bbb0999f060a5573704a9d492
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-06 14:38:19 +0900

    test: 식당 추가 여부 확인 테스트 추가

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -17,14 +17,12 @@ describe("NavBar component 테스트", () => {
 });
 
 describe("RestaurantItem, RestaurantList component 테스트", () => {
-  test("추가된 RestaurantItem이 RestaurantList에 있는지 확인한다.", () => {
-
-    customElements.define("restaurant-list", RestaurantList);
+  customElements.define("restaurant-list", RestaurantList);
+  test("추가된 RestaurantItem이 RestaurantList에 그려졌는지 확인한다.", () => {
     document.body.innerHTML = `
       <restaurant-list id="restaurantList"></restaurant-list>
     `;
     const restaurantList = document.getElementById("restaurantList");
-
     restaurantList.addRestaurant({
       category: "중식",
       name: "봉피양",
@@ -34,4 +32,20 @@ describe("RestaurantItem, RestaurantList component 테스트", () => {
     const result = `봉피양`;
     expect(restaurantList).toContainHTML(result);
   });
+  test("추가된 Restaurant이 RestaurantList의 배열에 존재하는지 확인한다.", () => {
+    document.body.innerHTML = `
+      <restaurant-list id="restaurantList"></restaurant-list>
+    `;
+    const restaurantList = document.getElementById("restaurantList");
+    restaurantList.addRestaurant({
+      category: "중식",
+      name: "봉피양",
+      distance: 10,
+      description: "왕",
+    });
+    const result = `봉피양`;
+    expect(restaurantList.state.restaurants.map((r) => r.name)).toContain(result);
+  });
 });
+
+
