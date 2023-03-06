b6726d472906dac3708c6c53a39db50ed9a2e0c2
hafnium1923 <hafnium1923@gmail.com>
2023-03-06 17:39:39 +0900

    refactor: 종속적이지 않은 dispatcher

--- src/domain/Dispatcher.ts
+++ src/domain/Dispatcher.ts
@@ -1,12 +1,16 @@
 import { Action, Restaurant, Category, SortMethod } from "../abstracts/types";
 import StoreInstance from "./Store";
 
+const reducer = {
+  ...StoreInstance.reducer,
+};
+
 const dispatcher = (
   type: string,
   data?: Restaurant | Category | SortMethod
 ) => {
   const action: Action = { type, data };
-  StoreInstance.reducer[type](action);
+  reducer[type](action);
 };
 
 export default dispatcher;
