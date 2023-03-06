7b046da5c0b53590e9e2bb9990f3fff33f312afa
gyeongza <gyeongza@naver.com>
2023-03-07 00:43:08 +0900

    refactor: 음식점 타입을 지정

--- src/domain/model/Restaurant.ts
+++ src/domain/model/Restaurant.ts
@@ -1,10 +1,10 @@
 type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
 
+type Distance = 5 | 10 | 15 | 20 | 30;
+
 export type CategoryAll = '전체' | Category;
 
 export type SortTypeAll = 'name' | 'distance';
-
-type Distance = 5 | 10 | 15 | 20 | 30;
 
 export interface RestaurantInfo {
   category: Category;
@@ -13,29 +13,3 @@ export interface RestaurantInfo {
   description?: string;
   link?: string;
 }
-
-class Restaurant {
-  #information: RestaurantInfo;
-
-  constructor({
-    category,
-    name,
-    distance,
-    description = '',
-    link = '',
-  }: RestaurantInfo) {
-    this.#information = {
-      category,
-      name,
-      distance,
-      description,
-      link,
-    };
-  }
-
-  getInfo() {
-    return this.#information;
-  }
-}
-
-export default Restaurant;
