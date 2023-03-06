f931b272af888b7e6d23fffdcb4c19543840dcce
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-06 21:20:44 +0900

    refactor(restaurantaddmodal): 불필요한 속성 제거, 하위 컴포넌트 제거 -> 간단화

--- src/view/RestaurantAddModal.ts
+++ src/view/RestaurantAddModal.ts
@@ -1,164 +1,100 @@
 import { $ } from '../util/querySelector';
-import FormInput from './components/FormInput';
-import FormSelect from './components/FormSelect';
-import FormTextArea from './components/FormTextArea';
-import FormButtonPack from './components/FormButtonPack';
-import getFormData from '../util/getFormData';
 import { Restaurant } from '../type';
+import getFormData from '../util/getFormData';
 
 type RestaurantAddModalType = {
   parentElement: HTMLElement;
-  info: {
-    modalId: string;
-    modalFormId: string;
-    modalTitle: string;
-  };
   parentEvent: {
+    onModalCancelButtonClicked: () => void;
     onModalAddButtonClicked: (restaurantData: Restaurant) => void;
-    onToggleVisibilityEvent: () => void;
   };
 };
 
 class RestaurantAddModal {
   #parentElement;
-  #info;
   #parentEvent;
 
-  constructor({ parentElement, info, parentEvent }: RestaurantAddModalType) {
+  constructor({ parentElement, parentEvent }: RestaurantAddModalType) {
     this.#parentElement = parentElement;
-    this.#info = info;
     this.#parentEvent = parentEvent;
+
     this.#render();
-    this.#renderChild();
+    this.#setListeners();
   }
 
-  toggleVisibility() {
-    $(`#${this.#info.modalId}`).classList.toggle('modal--open');
-  }
-
-  #onModalCancelButtonClicked() {
-    this.toggleVisibility();
-  }
-
-  #onModalAddButtonClicked() {
-    console.log('Add Clicked - Modal');
-    this.toggleVisibility();
-    const restaurantData = getFormData(
-      $(`#${this.#info.modalFormId}`) as HTMLFormElement
+  #setListeners() {
+    ($(`#modal-cancel-button`) as HTMLButtonElement).addEventListener(
+      'click',
+      () => {
+        this.#parentEvent.onModalCancelButtonClicked();
+      }
     );
 
-    console.log({ data: restaurantData });
-    this.#parentEvent.onModalAddButtonClicked(restaurantData as Restaurant);
+    ($(`#modal-add-button`) as HTMLButtonElement).addEventListener(
+      'click',
+      () => {
+        const restaurantData = getFormData(
+          $(`#modal-add-form`) as HTMLFormElement
+        );
+
+        this.#parentEvent.onModalAddButtonClicked(restaurantData as Restaurant);
+      }
+    );
   }
 
   #render() {
-    const element = `
-      <div class="modal" id="${this.#info.modalId}">
-        <div class="modal-backdrop"></div>
-        <div class="modal-container">
-          <h2 class="modal-title text-title">${this.#info.modalTitle}</h2>
-          <form id="${this.#info.modalFormId}"></form>
-        </div>
-      </div>`;
+    const template = `
+      <div class="modal modal--open">
+      <div class="modal-backdrop"></div>
+      <div class="modal-container">
+        <h2 class="modal-title text-title">새로운 음식점</h2>
+        <form id="modal-add-form">
+          <div class="form-item form-item--required">
+            <label for="category text-caption">카테고리</label>
+            <select name="category" id="category" required>
+              <option value="">선택해 주세요</option>
+              <option value="한식">한식</option>
+              <option value="중식">중식</option>
+              <option value="일식">일식</option>
+              <option value="양식">양식</option>
+              <option value="아시안">아시안</option>
+              <option value="기타">기타</option>
+            </select>
+          </div>
+          <div class="form-item form-item--required">
+            <label for="name text-caption">이름</label>
+            <input type="text" name="name" id="name" required>
+          </div>
+          <div class="form-item form-item--required">
+            <label for="distance text-caption">거리(도보 이동 시간) </label>
+            <select name="distance" id="distance" required>
+              <option value="">선택해 주세요</option>
+              <option value="5">5분 내</option>
+              <option value="10">10분 내</option>
+              <option value="15">15분 내</option>
+              <option value="20">20분 내</option>
+              <option value="30">30분 내</option>
+            </select>
+          </div>
+          <div class="form-item">
+            <label for="description text-caption">설명</label>
+            <textarea name="description" id="description" cols="30" rows="5"></textarea>
+            <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
+          </div>
+          <div class="form-item">
+            <label for="link text-caption">참고 링크</label>
+            <input type="text" name="link" id="link">
+            <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
+          </div>
+          <div class="button-container">
+            <button type="button" class="button button--secondary text-caption" id="modal-cancel-button">취소하기</button>
+            <button class="button button--primary text-caption" id="modal-add-button">추가하기</button>
+          </div>
+        </form>
+      </div>
+    </div>`;
 
-    if (this.#parentElement) {
-      this.#parentElement.insertAdjacentHTML(
-        'beforeEnd' as InsertPosition,
-        element
-      );
-    }
-  }
-
-  #renderChild() {
-    new FormSelect({
-      parentElement: $(`#${this.#info.modalFormId}`) as HTMLElement,
-      info: {
-        id: 'category-filter',
-        name: 'category',
-        title: '카테고리',
-        choices: [
-          { value: '전체', displayName: '전체' },
-          { value: '한식', displayName: '한식' },
-          { value: '중식', displayName: '중식' },
-          { value: '일식', displayName: '일식' },
-          { value: '양식', displayName: '양식' },
-          { value: '아시안', displayName: '아시안' },
-          { value: '기타', displayName: '기타' },
-        ],
-      },
-    });
-
-    new FormInput({
-      parentElement: $(`#${this.#info.modalFormId}`) as HTMLElement,
-      info: {
-        id: 'name',
-        name: 'name',
-        title: '이름',
-        isRequired: true,
-        hasDescription: false,
-      },
-    });
-
-    new FormSelect({
-      parentElement: $(`#${this.#info.modalFormId}`) as HTMLElement,
-      info: {
-        id: 'distance',
-        name: 'distance',
-        title: '거리(도보 이동 시간)',
-        choices: [
-          { value: '5', displayName: '5분 내' },
-          { value: '10', displayName: '10분 내' },
-          { value: '15', displayName: '15분 내' },
-          { value: '20', displayName: '20분 내' },
-          { value: '25', displayName: '25분 내' },
-          { value: '30', displayName: '30분 내' },
-        ],
-      },
-    });
-
-    new FormTextArea({
-      parentElement: $(`#${this.#info.modalFormId}`) as HTMLElement,
-      info: {
-        id: 'description',
-        name: 'description',
-        title: '설명',
-        isRequired: false,
-        hasDescription: true,
-        description: '메뉴 등 추가 정보를 입력해 주세요.',
-        cols: 30,
-        rows: 5,
-      },
-    });
-
-    new FormInput({
-      parentElement: $(`#${this.#info.modalFormId}`) as HTMLElement,
-      info: {
-        id: 'link',
-        name: 'link',
-        title: '참고 링크',
-        isRequired: false,
-        hasDescription: true,
-        description: '매장 정보를 확인할 수 있는 링크를 입력해 주세요.',
-      },
-    });
-
-    new FormButtonPack({
-      parentElement: $(`#${this.#info.modalFormId}`) as HTMLElement,
-      event: {
-        onCancel: () => {
-          this.#onModalCancelButtonClicked();
-        },
-        onAdd: () => {
-          this.#onModalAddButtonClicked();
-        },
-      },
-      info: {
-        cancelButtonId: 'modal-cancel-button',
-        addButtonId: 'modal-add-button',
-        cancelButtonDisplayName: '취소하기',
-        addButtonDisplayName: '추가하기',
-      },
-    });
+    ($(`#${this.#parentElement}`) as HTMLElement).innerHTML = template;
   }
 }
 
