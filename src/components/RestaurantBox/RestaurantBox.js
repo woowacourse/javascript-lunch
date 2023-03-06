6ad5c8e0ef3927a2c10a17aa22e91d13996bb2ee
gyeongza <gyeongza@naver.com>
2023-03-06 20:43:57 +0900

    refactor: RestaurantBox 컴포넌트 분리

---
+++ src/components/RestaurantBox/RestaurantBox.js
@@ -0,0 +1,43 @@
+import './index.css';
+
+class RestaurantBox extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const name = this.getAttribute('name');
+    const category = this.getAttribute('category');
+    const distance = this.getAttribute('distance');
+    const description = this.getAttribute('description') || '';
+
+    this.innerHTML = `
+    <li class="restaurant">
+        <div class="restaurant__category">
+          <img src="../../assets/category-${category}.png';" alt=${category} class="category-icon">
+        </div>
+        <div class="restaurant__info">
+          <h3 class="restaurant__name text-subtitle">${name}</h3>
+          <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+          <p class="restaurant__description text-body">${description}</p>
+        </div>
+    </li>
+    `;
+  }
+
+  static get observedAttributes() {
+    return ['category', 'name', 'distance', 'description'];
+  }
+
+  attributeChangedCallback(name) {
+    if (name === 'category' && name === 'name' && name === 'distance') {
+      this.connectedCallback();
+    }
+  }
+}
+
+export default RestaurantBox;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import FilterList from './components/FilterList';
 import Header from './components/Header';
 import Modal from './components/Modal';
+import RestaurantBox from './components/RestaurantBox/RestaurantBox';
 import './styles/index.css';
 
 customElements.define('lunch-header', Header);
 customElements.define('restaurant-modal', Modal);
 customElements.define('filter-list', FilterList);
+customElements.define('restaurant-box', RestaurantBox);
