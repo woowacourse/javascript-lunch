cec277e2073ab4f0debbcca5af688982b609a468
bassyu <yrkdgus@gmail.com>
2023-03-06 20:17:31 +0900

    refactor(RestaurantFilterContainer): Component 상속 및 이름 변경

---
+++ src/components/RestaurantFilterContainer.ts
@@ -0,0 +1,48 @@
+import { Category, SortBy } from '../type';
+import Component from './Component';
+import { CATEGORIES } from '../utils/constants';
+
+interface RestaurantFilterContainerProps {
+  currentCategory: Category;
+  currentSortBy: SortBy;
+  onChangeCategory: (e: Event) => void;
+  onChangeSortBy: (e: Event) => void;
+}
+
+interface RestaurantFilterContainerState {}
+
+class RestaurantFilterContainer extends Component<
+  RestaurantFilterContainerProps,
+  RestaurantFilterContainerState
+> {
+  constructor($parent: HTMLElement, props: RestaurantFilterContainerProps) {
+    super({ $parent, props, tagName: 'section', initialState: {} });
+    this.$wrapper.className = 'restaurant-filter-container';
+  }
+
+  drawInnerHTML() {
+    const { currentCategory, currentSortBy } = this.props;
+    return `
+      <select name="category" id="category-filter" class="restaurant-filter">
+        ${CATEGORIES.map(
+          (category) => `
+            <option value="${category}" ${
+            currentCategory === category ? 'selected' : ''
+          }>${category}</option>
+          `
+        ).join('')}
+      </select>
+      <select name="sorting" id="sorting-filter" class="restaurant-filter">
+        <option value="name" ${currentSortBy === 'name' ? 'selected' : ''}>이름순</option>
+        <option value="distance" ${currentSortBy === 'distance' ? 'selected' : ''}>거리순</option>
+      </select>
+    `;
+  }
+
+  addEvent() {
+    this.$('#category-filter')?.addEventListener('change', this.props.onChangeCategory);
+    this.$('#sorting-filter')?.addEventListener('change', this.props.onChangeSortBy);
+  }
+}
+
+export default RestaurantFilterContainer;
