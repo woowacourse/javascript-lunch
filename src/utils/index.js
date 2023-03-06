b735858745603c51763e3a4b38c12fb75f50e424
gilpop8663 <wolfye@naver.com>
2023-03-06 11:28:59 +0900

    fix(utils): shadowRoot 를 선택하는 유틸 함수 수정
    shadowRoot안의 Node를 직접 찾는 것이 아닌 함수형으로 코드 작성하기 위하여 수정하였습니다

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,35 +2,9 @@ const $ = (selector) => document.querySelector(selector);
 
 const $$ = (selector) => document.querySelectorAll(selector);
 
-const $$$ = (rootNode, selector) => {
-  const arr = [];
+const $$$ = (parent, selector) => $(parent).shadowRoot.querySelector(selector);
 
-  const traverser = (node) => {
-    if (node.nodeType !== Node.ELEMENT_NODE) return;
+const $$$$ = (parent, selector) =>
+  $(parent).shadowRoot.querySelectorAll(selector);
 
-    if (node.matches(selector)) {
-      arr.push(node);
-    }
-
-    const children = node.children;
-    if (children.length) {
-      for (const child of children) {
-        traverser(child);
-      }
-    }
-
-    const shadowRoot = node.shadowRoot;
-    if (shadowRoot) {
-      const shadowChildren = shadowRoot.children;
-      for (const shadowChild of shadowChildren) {
-        traverser(shadowChild);
-      }
-    }
-  };
-
-  traverser($(rootNode));
-
-  return arr[0];
-};
-
-export { $, $$, $$$ };
+export { $, $$, $$$, $$$$ };
