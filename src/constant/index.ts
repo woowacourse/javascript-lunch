679b9de7a21e38375a7ba00867063fed4fb22eb4
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-07 02:12:06 +0900

    feat: 상수 정리, 이미지 경로 추가

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,15 +2,14 @@ const REGEX = Object.freeze({
   link: /^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+)\.([a-z]+).*$/,
 });
 
-const RESTAURANT_CATEGORIES = [
-  '전체',
-  '한식',
-  '중식',
-  '일식',
-  '아시안',
-  '양식',
-  '기타',
-];
+const CATEGORY_IMAGE_PATH = Object.freeze({
+  한식: './category-korean.png',
+  중식: './category-chinese.png',
+  양식: './category-western.png',
+  일식: './category-japanese.png',
+  아시안: './category-asian.png',
+  기타: './category-etc.png',
+});
 
 const ERROR_MESSAGE = {
   category: '카테고리를 선택해 주세요!',
@@ -19,14 +18,24 @@ const ERROR_MESSAGE = {
   link: '올바른 주소를 입력해 주세요!',
 };
 
-const RESTAURANT_DISTANCES = ['5', '10', '15', '20', '25', '30'];
+const RESTAURANT_DISTANCES_IN_MINUTE = ['5', '10', '15', '20', '25', '30'];
+
+const RESTAURANT_CATEGORIES = [
+  '한식',
+  '중식',
+  '일식',
+  '아시안',
+  '양식',
+  '기타',
+];
 
 const LOCAL_STORAGE_KEY = 'RESTAURANT_APP';
 
 export {
   REGEX,
   ERROR_MESSAGE,
+  CATEGORY_IMAGE_PATH,
+  RESTAURANT_DISTANCES_IN_MINUTE,
   RESTAURANT_CATEGORIES,
-  RESTAURANT_DISTANCES,
   LOCAL_STORAGE_KEY,
 };
