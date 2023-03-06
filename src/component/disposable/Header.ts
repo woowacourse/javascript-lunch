1b11818374bc09ddee9d7d4abbdeef26f081f4f5
Dahye <06robin11@gmail.com>
2023-03-06 14:39:41 +0900

    refactor(modal,header):불필요한 속성 제거

--- src/component/disposable/Header.ts
+++ src/component/disposable/Header.ts
@@ -5,7 +5,7 @@ class Header {
     return `
     <header class="gnb">
       <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-      <button type="button" class="gnb__button" aria-label="음식점 추가">
+      <button type="button" class="gnb__button">
       <img src="./add-button.png" alt="음식점 추가">
       </button>
     </header>`;
--- src/component/disposable/Modal.ts
+++ src/component/disposable/Modal.ts
@@ -25,12 +25,12 @@ class Modal {
       <h2 class="modal-title text-title">새로운 음식점</h2>
       <form class="modal-form">
         <div class="form-item form-item--required">
-          <label for="category text-caption">카테고리</label>
+          <label for="category">카테고리</label>
           ${this.categorySelect.template()}
         </div>
 
         <div class="form-item form-item--required">
-          <label for="name text-caption">이름</label>
+          <label for="name">이름</label>
           <input type="text" name="name" id="name" required>
         </div>
 
@@ -40,13 +40,13 @@ class Modal {
         </div>
 
         <div class="form-item">
-          <label for="description text-caption">설명</label>
+          <label for="description">설명</label>
           <textarea name="description" id="description" cols="30" rows="5"></textarea>
           <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
         </div>
 
         <div class="form-item">
-          <label for="link text-caption">참고 링크</label>
+          <label for="link">참고 링크</label>
           <input type="url" name="link" id="link">
           <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
         </div>
