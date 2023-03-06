3125963321af8bff6c051b1bc707610e6fea17db
gyeongza <gyeongza@naver.com>
2023-03-06 17:31:44 +0900

    refactor: SelectList 컴포넌트 분리

---
+++ src/components/SelectList/index.js
@@ -0,0 +1,38 @@
+import './index.css';
+
+class SelectList extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    const name = this.getAttribute('name');
+    const id = this.getAttribute('id');
+    const optionsAttribute = this.getAttribute('options').split(',');
+    const options = optionsAttribute.map((option) => this.createOption(option));
+
+    this.innerHTML = `<select name="${name}" id="${id}" class="restaurant-filter">
+    ${options.join('\n')}
+  </select>`;
+  }
+
+  createOption(title) {
+    return `<option value="${title}">${title}</option>`;
+  }
+
+  static get observedAttributes() {
+    return ['name', 'id', 'options'];
+  }
+
+  attributeChangedCallback(name) {
+    if (name === 'name' && name === 'id' && name === 'options') {
+      this.connectedCallback();
+    }
+  }
+}
+
+export default SelectList;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-import AddRestaurantModal from './components/AddRestaurantModal';
-import CategoryList from './components/CategoryList';
 import Header from './components/Header';
-import RestaurantBox from './components/RestaurantBox';
-import AddCategoryList from './components/AddCategoryList';
+import Modal from './components/Modal';
+import SelectList from './components/SelectList';
 import './styles/index.css';
-import AddTextInput from './components/AddTextInput';
-import Button from './components/Button';
-import LunchRecommendation from './domain/controller/LunchRecommendation';
 
 customElements.define('lunch-header', Header);
-customElements.define('category-list', CategoryList);
-customElements.define('restaurant-box', RestaurantBox);
-customElements.define('add-restaurant-modal', AddRestaurantModal);
-customElements.define('add-category-list', AddCategoryList);
-customElements.define('add-text-input', AddTextInput);
-customElements.define('lunch-button', Button);
-
-new LunchRecommendation().play();
+customElements.define('restaurant-modal', Modal);
+customElements.define('select-list', SelectList);
