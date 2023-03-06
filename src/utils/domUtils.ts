1af4cb3659cbf1bf01ec8d18112f1fa8ae523235
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-06 11:50:11 +0900

    feat: domUtils 인자 타입 변경

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -1,18 +1,28 @@
-export const $ = (selector: string, scope: Document | HTMLElement = document): Element =>{
-  if (!selector) throw new Error("no selector");
+export const $ = (
+  selector: string,
+  scope: Document | Element | HTMLElement = document
+): HTMLElement => {
+  if (!selector) throw new Error('no selector');
 
   return scope.querySelector(selector)!;
-}
+};
 
-export const all$ = (selector: string,  scope: Document | HTMLElement = document): Element[] => {
-  if (!selector) throw new Error("no selector");
+export const all$ = <T>(
+  selector: string,
+  scope: Document | Element | HTMLElement = document
+): HTMLElement[] => {
+  if (!selector) throw new Error('no selector');
 
   return Array.from(scope.querySelectorAll(selector));
-}
+};
 
-export const on = (target: EventTarget, eventName: string, handler: (event:Event) => void): void => {
+export const on = (
+  target: EventTarget,
+  eventName: string,
+  handler: (event: Event) => void
+): void => {
   target.addEventListener(eventName, handler);
-}
+};
 
 export const newState = (state: any, handler: any) => {
   return new Proxy(state, {
@@ -22,10 +32,3 @@ export const newState = (state: any, handler: any) => {
     },
   });
 };
-
-
-
-
-
-
-
