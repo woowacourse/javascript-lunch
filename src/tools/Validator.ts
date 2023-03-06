cacf940cbdc13527b8e5c26e02bd5771af547718
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-06 11:13:14 +0900

    fix: 링크 주소의 유효성을 검사하지 않던 문제 수정

--- src/tools/Validator.ts
+++ src/tools/Validator.ts
@@ -7,10 +7,19 @@ const Validator = {
       throw new Error("공백만으로 이루어진 입력이 있습니다.");
     }
   },
+  checkUrl(input: string) {
+    const regExp = /^http[s]?:\/\//i;
+    if (input.length > 0 && !regExp.test(input)) {
+      throw new Error(
+        "링크 주소의 형식이 http:// 또는 https:// 으로 시작해야 합니다."
+      );
+    }
+  },
   checkRestaurant(restaurantForm: IRestaurant) {
     console.log(restaurantForm);
     this.checkBlankWord(restaurantForm.name);
     this.checkBlankWord(restaurantForm.description as string);
+    this.checkUrl(restaurantForm.link as string);
   },
 };
 export default Validator;
