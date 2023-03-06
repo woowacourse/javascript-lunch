c371414cf9e5e25a80f24c1d39175ba4754491ea
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-07 02:24:25 +0900

    refactor: 기본 데이터의 link를 undefined로 변경

--- src/tools/defaultRestaurants.ts
+++ src/tools/defaultRestaurants.ts
@@ -4,21 +4,21 @@ const defaultRestaurants = [
     name: "찐친",
     distance: 5,
     description: "찐친들이랑 가는 중국집",
-    link: "",
+    link: undefined,
   },
   {
     category: "etc",
     name: "커피베네",
     distance: 5,
     description: "성담빌딩 건물에 있는 커피베네",
-    link: "",
+    link: undefined,
   },
   {
     category: "etc",
     name: "스타벅스 선릉역점",
     distance: 5,
     description: "면담하기 좋은 곳 스타벅스 시끌벅적함",
-    link: "",
+    link: undefined,
   },
   {
     category: "korean",
@@ -26,7 +26,7 @@ const defaultRestaurants = [
     distance: 15,
     description:
       "멀긴 한데 그래도 9,000원이라는 가격에 저렴하게 적게 먹을 수 있는 곳. 지갑 다이어트하기 좋은 곳",
-    link: "",
+    link: undefined,
   },
   {
     category: "korean",
@@ -34,28 +34,28 @@ const defaultRestaurants = [
     distance: 15,
     description:
       "영동칼국수집 옆에 있는 연어덮밥, 스테이크덮밥 등을 파는 덮밥 집",
-    link: "",
+    link: undefined,
   },
   {
     category: "japanese",
     name: "돈카레",
     distance: 10,
     description: "도밥이 좋아하는 돈카레",
-    link: "",
+    link: undefined,
   },
   {
     category: "chinese",
     name: "우육면가",
     distance: 10,
     description: "크론이 한 번도 안 먹어 본 우육면가",
-    link: "",
+    link: undefined,
   },
   {
     category: "korean",
     name: "시골밥상머리",
     distance: 10,
     description: "시골에서 주는 밥처럼 나오는 상머리",
-    link: "",
+    link: undefined,
   },
 ];
 
