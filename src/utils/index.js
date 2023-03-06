8a7e277d49f33be37e2f50b75ffa9017892a3f81
gyeongza <gyeongza@naver.com>
2023-03-06 16:17:44 +0900

    refactor: shadow DOM 제거를 위한 DOM selector 삭제

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,35 +2,4 @@ const $ = (selector) => document.querySelector(selector);
 
 const $$ = (selector) => document.querySelectorAll(selector);
 
-const $$$ = (rootNode, selector) => {
-  const arr = [];
-
-  const traverser = (node) => {
-    if (node.nodeType !== Node.ELEMENT_NODE) return;
-
-    if (node.matches(selector)) {
-      arr.push(node);
-    }
-
-    const children = node.children;
-    if (children.length) {
-      for (const child of children) {
-        traverser(child);
-      }
-    }
-
-    const shadowRoot = node.shadowRoot;
-    if (shadowRoot) {
-      const shadowChildren = shadowRoot.children;
-      for (const shadowChild of shadowChildren) {
-        traverser(shadowChild);
-      }
-    }
-  };
-
-  traverser($(rootNode));
-
-  return arr[0];
-};
-
-export { $, $$, $$$ };
+export { $, $$ };
