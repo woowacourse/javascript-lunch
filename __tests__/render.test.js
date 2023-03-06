15762b6b7bf875543ea6374a2af8bdb66b1b2c56
n0eyes <doll4586633@naver.com>
2023-03-06 15:58:30 +0900

    test: 음식점 추가 모달 오픈 테스트

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,3 +1,33 @@
 /**
  * @jest-environment jsdom
  */
+import { fireEvent, waitFor } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import { $ } from '../src/utils/common/domHelper.ts';
+import { App } from '../src/view/components/App.ts';
+import { render } from '../src/utils/core/index.ts';
+
+const waitElement = async (getElementCallback, options) => {
+  return waitFor(() => {
+    const element = getElementCallback();
+    if (!element) throw new Error();
+
+    expect(element).toBeInTheDocument();
+  }, options);
+};
+
+describe('앱 렌더링 테스트', () => {
+  beforeEach(() => {
+    document.body.innerHTML = '<div id="app" />';
+    render(App, $('#app'));
+  });
+
+  test('헤더의 음식점 추가 버튼을 누르면 음식점 생성 모달이 나타난다', async () => {
+    await waitElement(() => $('.gnb__button'));
+
+    const modalTriggerBtn = $('.gnb__button');
+    fireEvent.click(modalTriggerBtn);
+
+    await waitElement(() => $('.modal'));
+  });
+});
