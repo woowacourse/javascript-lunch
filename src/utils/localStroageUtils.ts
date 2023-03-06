2510b478f18851a97b5ab4c145601f7266344dbd
sh981013s <sh981013s@gmail.com>
2023-03-06 22:21:34 +0900

    refactor: 단수 복수형 의미있게 리네이밍

--- src/utils/localStroageUtils.ts
+++ src/utils/localStroageUtils.ts
@@ -2,7 +2,7 @@ const setLocalStorageItem = <T>(key: string, value: T): void => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-const getLocalStorageItem = <T>(key: string): T | null => {
+const getLocalStorageItems = <T>(key: string): T | null => {
   const value = localStorage.getItem(key);
   return value ? JSON.parse(value) : null;
 };
@@ -11,4 +11,4 @@ const removeLocalStorageItem = (key: string): void => {
   localStorage.removeItem(key);
 };
 
-export { setLocalStorageItem, getLocalStorageItem, removeLocalStorageItem };
+export { setLocalStorageItem, getLocalStorageItems, removeLocalStorageItem };
