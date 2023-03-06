c427310e5773147bb24434332818f9d74fc5646c
chsua <csj1919@naver.com>
2023-03-06 19:23:07 +0900

    refactor: 새로 정렬, 필터된 리스트 출력되는 함수 리팩토링

--- src/components/FilterSortButton.ts
+++ src/components/FilterSortButton.ts
@@ -1,11 +1,12 @@
 import { $, FilterSort, Render } from "../until/ControlDom";
-import { RestaurantList } from "./RestaurantList";
+import { MakeNewList } from "../until/MakeNewList";
+import { RestaurantList } from "../RestaurantList";
 
 const addEventFilterSortButton = () => {
   const restaurantFilterContainer = $(".restaurant-filter-container");
   restaurantFilterContainer?.addEventListener("change", () => {
     FilterSort.setState();
-    Render.restaurantList(RestaurantList.list);
+    Render.restaurantList(MakeNewList.getNewList(RestaurantList.list));
   });
 };
 
