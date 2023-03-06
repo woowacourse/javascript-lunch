11d4767b3c0cdbdd2789d90ec00cf2feba9cd9f0
hozzijeong <ghwls0905@naver.com>
2023-03-06 21:19:44 +0900

    refactor: modal.js -> ts로 변경

--- src/modal.ts
+++ src/modal.ts
@@ -1,11 +1,17 @@
 export const showModal = () => {
   const $modal = document.querySelector('.modal');
+
+  if (!$modal) return;
+
   document.body.style.overflow = 'hidden';
   $modal.classList.add('modal--open');
 };
 
 export const closeModal = () => {
   const $modal = document.querySelector('.modal');
+
+  if (!$modal) return;
+
   document.body.style.overflow = 'visible';
   $modal.classList.remove('modal--open');
 };
