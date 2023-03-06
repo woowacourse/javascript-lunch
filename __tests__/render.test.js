83be1ebdaa21d87f0bda6159292c9d91a5aa8707
n0eyes <doll4586633@naver.com>
2023-03-06 17:24:52 +0900

    test: 음식점 추가 모달 닫기 테스트

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,13 +1,13 @@
 /**
  * @jest-environment jsdom
  */
-import { fireEvent, waitFor } from '@testing-library/dom';
+import { fireEvent, waitFor, waitForElementToBeRemoved } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import { $ } from '../src/utils/common/domHelper.ts';
 import { App } from '../src/view/components/App.ts';
 import { render } from '../src/utils/core/index.ts';
 
-const waitElement = async (getElementCallback, options) => {
+const waitElement = (getElementCallback, options) => {
   return waitFor(() => {
     const element = getElementCallback();
     if (!element) throw new Error();
@@ -17,10 +17,8 @@ const waitElement = async (getElementCallback, options) => {
 };
 
 describe('앱 렌더링 테스트', () => {
-  beforeEach(() => {
-    document.body.innerHTML = '<div id="app" />';
-    render(App, $('#app'));
-  });
+  document.body.innerHTML = '<div id="app" />';
+  render(App, $('#app'));
 
   test('헤더의 음식점 추가 버튼을 누르면 음식점 생성 모달이 나타난다', async () => {
     await waitElement(() => $('.gnb__button'));
@@ -29,5 +27,16 @@ describe('앱 렌더링 테스트', () => {
     fireEvent.click(modalTriggerBtn);
 
     await waitElement(() => $('.modal'));
+  });
+
+  test('음식점 추가 모달의 취소하기 버튼을 누르면 모달이 사라진다', async () => {
+    await waitElement(() => $('.modal'));
+
+    const cancelBtn = $('.button--secondary');
+    fireEvent.click(cancelBtn);
+
+    await waitForElementToBeRemoved($('.modal'));
+
+    expect($('.modal')).not.toBeInTheDocument();
   });
 });
