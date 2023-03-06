8ea7d2400ff7bc8a2bcf9e71ad6a6919e4a902e4
Q Kim <qoli0703@gmail.com>
2023-03-06 20:32:08 +0900

    feat: 모달에 관한 기능들을 담당하는 객체 구현

---
+++ src/util/Modal.js
@@ -0,0 +1,50 @@
+const Modal = {
+  create() {
+    const modalRoot = document.createElement("div");
+    modalRoot.setAttribute("class", "modal");
+
+    Modal.addBackdrop(modalRoot);
+    Modal.addContainer(modalRoot);
+
+    return modalRoot;
+  },
+
+  open(element) {
+    if (!element.classList.contains("modal--open")) {
+      element.classList.add("modal--open");
+    }
+  },
+
+  close(element) {
+    if (element.classList.contains("modal--open")) {
+      element.classList.remove("modal--open");
+    }
+  },
+
+  setInnerHTML(element, string) {
+    element.querySelector(".modal-container").innerHTML = string;
+  },
+
+  addBackdrop(modalRoot) {
+    const backdrop = document.createElement("div");
+    backdrop.setAttribute("class", "modal-backdrop");
+
+    backdrop.addEventListener("click", () => Modal.close(modalRoot));
+    document.addEventListener("keyup", (event) => {
+      if (event.key === "Escape" && modalRoot.classList.contains("modal--open")) {
+        Modal.close(modalRoot);
+      }
+    });
+
+    modalRoot.appendChild(backdrop);
+  },
+
+  addContainer(modalRoot) {
+    const container = document.createElement("div");
+    container.setAttribute("class", "modal-container");
+
+    modalRoot.appendChild(container);
+  },
+};
+
+export default Modal;
