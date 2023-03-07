40d1ddd98b27ff1b35790e0212b3dc981f51aa79
semnil5202 <semnil5202@gmail.com>
2023-03-07 13:32:47 +0900

    refactor: 초기 정적 렌더링 방식 변경 및 index.js에 컴포넌트 생명주기 상속

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import '../style.css';
+import '../templates/add-button.png';
+import { geid, qs } from './utils/domHelpers';
+
 import RestaurantManager from './domain/RestaurantManager';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import AddModal from './components/AddModal.js';
-import { qs } from './utils/domHelpers';
-import '../templates/add-button.png';
+import Component from './components/Component.js';
 
-const restaurantManager = new RestaurantManager();
-const header = new Header();
-const main = new Main(restaurantManager);
-const addModal = new AddModal(restaurantManager, main);
+class App extends Component {
+  constructor() {
+    super(geid('app'));
 
-qs('.gnb').innerHTML = header.render();
-qs('.restaurant-list').innerHTML = main.render();
-qs('.modal').innerHTML = addModal.render();
+    new Header(qs('.gnb'));
+    new Main(qs('.restaurant-list'));
+    new AddModal(qs('.modal'));
+  }
+
+  template() {
+    return `
+    <header class="gnb"></header>
+
+    <section class="restaurant-filter-container">
+      <select name="category" id="category-filter" class="restaurant-filter">
+        <option value="전체">전체</option>
+        <option value="한식">한식</option>
+        <option value="중식">중식</option>
+        <option value="일식">일식</option>
+        <option value="양식">양식</option>
+        <option value="아시안">아시안</option>
+        <option value="기타">기타</option>
+      </select>
+
+      <select name="sorting" id="sorting-filter" class="restaurant-filter">
+        <option value="name">이름순</option>
+        <option value="distance">거리순</option>
+      </select>
+    </section>
+
+    <section class="restaurant-list-container">
+      <ul class="restaurant-list"></ul>
+    </section>
+
+    <div class="modal"></div>
+    `;
+  }
+}
+
+new App();
