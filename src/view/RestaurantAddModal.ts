0290262dd6fcc63fc2fd299b57167c99a8e9c928
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-07 22:46:03 +0900

    refactor(restaurantaddmodal): 모달 컴포넌트 생성 후, 모달과 조합

--- src/view/RestaurantAddModal.ts
+++ src/view/RestaurantAddModal.ts
@@ -1,5 +1,6 @@
 import { $, $$ } from '../util/querySelector';
 import { Restaurant } from '../type';
+import Modal from './components/Modal';
 import getFormData from '../util/getFormData';
 
 type RestaurantAddModalType = {
@@ -13,17 +14,26 @@ type RestaurantAddModalType = {
 class RestaurantAddModal {
   #parentElement;
   #parentEvent;
+  #modal;
 
   constructor({ parentElement, parentEvent }: RestaurantAddModalType) {
     this.#parentElement = parentElement;
     this.#parentEvent = parentEvent;
 
-    this.#render();
+    this.#modal = new Modal({
+      parentElement: this.#parentElement,
+      info: {
+        id: 'restaurant-add-modal',
+        innerId: 'restaurant-add-modal-contents',
+      },
+    });
+
+    this.#fillContentsInModal();
     this.#setListeners();
   }
 
   toggleModal() {
-    $(`#restaurant-add-modal`).classList.toggle('modal--open');
+    this.#modal.toggleModal();
   }
 
   clearAllInputs() {
@@ -38,6 +48,57 @@ class RestaurantAddModal {
         input.value = '';
       }
     });
+  }
+
+  #fillContentsInModal() {
+    const template = `
+      <h2 class="modal-title text-title">새로운 음식점</h2>
+      <form id="modal-add-form">
+        <div class="form-item form-item--required">
+          <label for="category text-caption">카테고리</label>
+          <select name="category" id="category" required>
+            <option value="">선택해 주세요</option>
+            <option value="한식">한식</option>
+            <option value="중식">중식</option>
+            <option value="일식">일식</option>
+            <option value="양식">양식</option>
+            <option value="아시안">아시안</option>
+            <option value="기타">기타</option>
+          </select>
+        </div>
+        <div class="form-item form-item--required">
+          <label for="name">이름</label>
+          <input type="text" name="name" id="name" required>
+        </div>
+        <div class="form-item form-item--required">
+          <label for="distance">거리(도보 이동 시간) </label>
+          <select name="distanceInMinutes" id="distance" required>
+            <option value="">선택해 주세요</option>
+            <option value="5">5분 내</option>
+            <option value="10">10분 내</option>
+            <option value="15">15분 내</option>
+            <option value="20">20분 내</option>
+            <option value="30">30분 내</option>
+          </select>
+        </div>
+        <div class="form-item">
+          <label for="description">설명</label>
+          <textarea name="description" id="description" cols="30" rows="5"></textarea>
+          <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
+        </div>
+        <div class="form-item">
+          <label for="link">참고 링크</label>
+          <input type="text" name="link" id="link">
+          <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
+        </div>
+        <div class="button-container">
+          <button type="button" class="button button--secondary text-caption" id="modal-cancel-button">취소하기</button>
+          <button class="button button--primary text-caption" id="modal-add-button">추가하기</button>
+        </div>
+      </form>
+    `;
+
+    $('#restaurant-add-modal-contents').innerHTML = template;
   }
 
   #setListeners() {
@@ -57,61 +118,6 @@ class RestaurantAddModal {
         this.#parentEvent.onModalAddButtonClicked(restaurantData as Restaurant);
       }
     });
-  }
-
-  #render() {
-    const template = `
-      <div class="modal" id="restaurant-add-modal">
-      <div class="modal-backdrop"></div>
-      <div class="modal-container">
-        <h2 class="modal-title text-title">새로운 음식점</h2>
-        <form id="modal-add-form">
-          <div class="form-item form-item--required">
-            <label for="category text-caption">카테고리</label>
-            <select name="category" id="category" required>
-              <option value="">선택해 주세요</option>
-              <option value="한식">한식</option>
-              <option value="중식">중식</option>
-              <option value="일식">일식</option>
-              <option value="양식">양식</option>
-              <option value="아시안">아시안</option>
-              <option value="기타">기타</option>
-            </select>
-          </div>
-          <div class="form-item form-item--required">
-            <label for="name">이름</label>
-            <input type="text" name="name" id="name" required>
-          </div>
-          <div class="form-item form-item--required">
-            <label for="distance">거리(도보 이동 시간) </label>
-            <select name="distanceInMinutes" id="distance" required>
-              <option value="">선택해 주세요</option>
-              <option value="5">5분 내</option>
-              <option value="10">10분 내</option>
-              <option value="15">15분 내</option>
-              <option value="20">20분 내</option>
-              <option value="30">30분 내</option>
-            </select>
-          </div>
-          <div class="form-item">
-            <label for="description">설명</label>
-            <textarea name="description" id="description" cols="30" rows="5"></textarea>
-            <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
-          </div>
-          <div class="form-item">
-            <label for="link">참고 링크</label>
-            <input type="text" name="link" id="link">
-            <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
-          </div>
-          <div class="button-container">
-            <button type="button" class="button button--secondary text-caption" id="modal-cancel-button">취소하기</button>
-            <button class="button button--primary text-caption" id="modal-add-button">추가하기</button>
-          </div>
-        </form>
-      </div>
-    </div>`;
-
-    this.#parentElement.innerHTML = template;
   }
 }
 
