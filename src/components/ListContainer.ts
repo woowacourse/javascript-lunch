d293a43f7d4ed790fec2d7d2e1658a92727a5a01
sh981013s <sh981013s@gmail.com>
2023-03-07 21:43:06 +0900

    feat: 탭바 구현

--- src/components/ListContainer.ts
+++ src/components/ListContainer.ts
@@ -2,31 +2,75 @@ import Component from '../core/Component';
 import IRestaurantInput from '../interfaces/IRestaurantInput';
 import { IComponentPropState } from '../interfaces/IComponent';
 import imagePaths from '../constants/imagePaths';
+import preferenceTabs from '../constants/preferenceTabs';
 
 class ListContainer extends Component<IComponentPropState> {
+  setup() {
+    this.$state = {
+      activeTab: 'all',
+    };
+  }
+
   template() {
     const { restaurantList } = this.$props;
+    const { activeTab } = this.$state;
 
-    return `<ul class="restaurant-list">
-    ${restaurantList
+    const filteredRestaurants =
+      activeTab === 'all'
+        ? restaurantList
+        : restaurantList.filter(
+            (restaurant: IRestaurantInput) => restaurant.isFavorite
+          );
+
+    const restaurantListHtml = filteredRestaurants
       .map((restaurant: IRestaurantInput) => {
         const { category, name, distance, description } = restaurant;
         return `<li class="restaurant">
-      <div class="restaurant__category">
-        <img src=${imagePaths.mainListIconImage[category]} alt=${category} class="category-icon"/>
-      </div>
-      <div class="restaurant__info">
-        <h3 class="restaurant__name text-subtitle">${name}</h3>
+          <div class="restaurant__category">
+            <img src=${imagePaths.mainListIconImage[category]} alt=${category} class="category-icon"/>
+          </div>
+          <div class="restaurant__info">
+            <h3 class="restaurant__name text-subtitle">${name}</h3>
             <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 이내</span>
-        <p class="restaurant__description text-body">${description}</p>
-      </div>
-    </li>`;
+            <p class="restaurant__description text-body">${description}</p>
+          </div>
+        </li>`;
       })
-      .join('')}
-    </ul>`;
+      .join('');
+
+    return `
+      <div class="tabview">
+        <nav class="tabview__nav">
+          ${preferenceTabs
+            .map(
+              (tab) => `
+                <button
+                  class="tabview__nav__button ${
+                    activeTab === tab.id ? 'active' : ''
+                  }"
+                  data-tab="${tab.id}"
+                >
+                  ${tab.label}
+                </button>
+              `
+            )
+            .join('')}
+        </nav>
+        <div class="tabview__content">
+          ${restaurantListHtml}
+        </div>
+      </div>
+    `;
   }
 
-  setEvent(): void {}
+  setEvent(): void {
+    this.addEvent('click', '.tabview__nav__button', (event: Event) => {
+      const tabId = (event.target as HTMLElement).dataset.tab;
+      if (tabId) {
+        this.setState({ activeTab: tabId });
+      }
+    });
+  }
 }
 
 export default ListContainer;
---
+++ src/constants/preferenceTabs.ts
@@ -0,0 +1,6 @@
+const preferenceTabs = [
+  { id: 'all', label: '모든 음식점' },
+  { id: 'favorites', label: '자주 가는 음식점' },
+];
+
+export default preferenceTabs;
