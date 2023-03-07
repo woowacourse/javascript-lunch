2b46242034183dfd7ccfee60f3402968dd1b8140
Dain Lee <feb.dain@gmail.com>
2023-03-07 21:57:37 +0900

    refactor: 음식점 목록 컴포넌트 단위로 분리

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,3 +1,20 @@
 /**
  * @jest-environment jsdom
  */
+
+import { screen, fireEvent } from "@testing-library/dom";
+import "@testing-library/jest-dom";
+
+import { renderRestaurantList } from "../src/component/restaurantList";
+import { initialRestaurantData } from "../src/constant/initialRestaurants";
+
+beforeEach(() => {
+  document.body.innerHTML = `<section class="restaurant-list-container"></section>`;
+});
+
+test("초기 음식점 목록을 렌더링한다.", () => {
+  const restaurants = renderRestaurantList(initialRestaurantData);
+  document.body.insertAdjacentHTML("beforeend", restaurants);
+
+  expect(screen.queryByText("친친")).toBeInTheDocument();
+});
