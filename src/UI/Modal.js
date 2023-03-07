de400ded79d1df3be721e1bc2ea744731cda44f4
regularPark <srk99279@naver.com>
2023-03-07 09:44:08 +0900

    🐛 fix: 모달에서 렌더를 두번하는 오류 해결

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -149,9 +149,6 @@ export default class Modal {
     const categoryFilter = $("#category-filter");
     categoryFilter.value = CATEGORY_NAME.total;
     categoryFilter.dispatchEvent(new Event("change"));
-    this.restaurantList.listRestaurant.forEach((restaurant) => {
-      this.restaurantItem.render(restaurant);
-    });
   }
 
   closeModal = () => {
