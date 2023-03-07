cd25076d965cddf3e017686be29ac95f7664f0ab
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-07 16:13:02 +0900

    refactor(formCondition): 상수 타입 변경
    Object.freeze -> readonly 사용

--- src/constants/formCondition.ts
+++ src/constants/formCondition.ts
@@ -1,2 +1,2 @@
-export const CATEGORIES = Object.freeze(['한식', '중식', '일식', '아시안', '양식', '기타']);
-export const DISTANCES = Object.freeze(['5', '10', '15', '20', '30']);
+export const CATEGORIES: readonly string[] = ['한식', '중식', '일식', '아시안', '양식', '기타'];
+export const DISTANCES: readonly string[] = ['5', '10', '15', '20', '30'];
