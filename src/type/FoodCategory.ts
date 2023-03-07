857d250b3244ff3ee2f57010119120c5f26319f6
Q Kim <qoli0703@gmail.com>
2023-03-07 14:21:31 +0900

    refactor: 상수는 SNAKE_CASE로 변경

--- src/type/FoodCategory.ts
+++ src/type/FoodCategory.ts
@@ -1,11 +1,11 @@
-const foodCategoryList = ["한식", "중식", "일식", "아시안", "양식", "기타"] as const;
+const FOOD_CATEGORY_LIST = ["한식", "중식", "일식", "아시안", "양식", "기타"] as const;
 
-type FoodCategory = typeof foodCategoryList[number];
+type FoodCategory = typeof FOOD_CATEGORY_LIST[number];
 
-const getFoodCategoryMemberList = () => [...foodCategoryList];
+const getFoodCategoryMemberList = () => [...FOOD_CATEGORY_LIST];
 
 const isValidFoodCategory = (category: string): category is FoodCategory => (
-  foodCategoryList.includes(<FoodCategory>category)
+  FOOD_CATEGORY_LIST.includes(<FoodCategory>category)
 );
 
 export { FoodCategory, getFoodCategoryMemberList, isValidFoodCategory };
