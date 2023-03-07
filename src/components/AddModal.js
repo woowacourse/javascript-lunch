9afbf148af25812554c00f5745c64361c6d7e23e
semnil5202 <semnil5202@gmail.com>
2023-03-07 22:01:00 +0900

    fix: 예외 처리 로직 오류 수정

--- src/components/AddModal.js
+++ src/components/AddModal.js
@@ -88,7 +88,7 @@ export default class AddModal extends Component {
       return el.value;
     });
 
-    if (inputData[2] === '' || !inputData[2].match(/^[a-zA-Z0-9]*$/).length) {
+    if (inputData[1] === '' || !inputData[1].match(/^[a-zA-Z0-9]*$/)) {
       alert('음식점 이름은 적어도 하나의 문자를 입력해야 합니다.');
       return;
     }
