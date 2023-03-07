0398c7d758ecebbf3459ab85a66034c1eff8fd4a
Leejin-Yang <ejin1996@gmail.com>
2023-03-07 22:14:54 +0900

    test: select 전체 옵션에 대한 테스트 작성

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -3,24 +3,40 @@
  */
 import { screen } from "@testing-library/dom";
 import "@testing-library/jest-dom";
-import createCustomSelect from "../src/components/CustomSelect";
-import createModal from "../src/components/CustomModal";
 
-createCustomSelect();
-createModal();
+import RestaurantOptionSelect from "../src/components/RestaurantOptionSelect";
+
+customElements.define("restaurant-option-select", RestaurantOptionSelect, {
+  extends: "select",
+});
+
+beforeEach(() => {
+  document.body.innerHTML = "";
+});
 
 test.each([
-  ["distance", "15분 내"],
-  ["category", "한식"],
-  ["category-filter", "한식"],
-  ["sorting-filter", "이름순"],
+  [
+    "category-filter",
+    ["전체", "한식", "중식", "일식", "양식", "아시안", "기타"],
+  ],
+  ["sorting-filter", ["이름순", "거리순"]],
+  [
+    "category",
+    ["선택해 주세요", "한식", "중식", "일식", "양식", "아시안", "기타"],
+  ],
+  [
+    "distance",
+    ["선택해 주세요", "5분 내", "10분 내", "15분 내", "20분 내", "30분 내"],
+  ],
 ])(
-  "CustomSelect 생성시 id에 따라 적절한 옵션값이 추가된다.",
-  (id, optionText) => {
-    document.body.innerHTML = `
-    <select is="custom-select" id=${id}></select>
-  `;
+  "restaurant-option-select 생성시 id에 따라 적절한 옵션값이 추가된다.",
+  (id, optionTexts) => {
+    const restaurantOptionSelect = `<select is="restaurant-option-select" id=${id}></select>`;
 
-    expect(screen.getByText(optionText));
+    document.body.insertAdjacentHTML("beforeend", restaurantOptionSelect);
+
+    optionTexts.forEach((text) => {
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
   }
 );
