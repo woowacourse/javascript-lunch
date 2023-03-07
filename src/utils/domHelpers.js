51ce59be8107f487f36c105bac427ccaea4b78c6
semnil5202 <semnil5202@gmail.com>
2023-03-07 09:55:24 +0900

    refactor: domHelper의 가독성 개선을 위한 함수명 변경

--- src/utils/domHelpers.js
+++ src/utils/domHelpers.js
@@ -1,1 +1,3 @@
-export const $ = (target) => document.querySelector(target);
+export const qs = (target) => document.querySelector(target);
+export const qsa = (target) => document.querySelectorAll(target);
+export const geid = (target) => document.getElementById(target);
