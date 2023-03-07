2f944a10cad31e603ce405a4ef0106031bad8d8a
unknown <tkdrb15@gmail.com>
2023-03-07 15:51:27 +0900

    feat: restaurant 인터페이스에 ID, favorties 프로퍼티를 추가하다

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -1,7 +1,9 @@
 export interface Restaurant {
+  ID: number;
   category: '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
   name: string;
   distance: 5 | 10 | 15 | 20 | 30;
+  favorites: boolean;
   description?: string;
   link?: string;
 }
