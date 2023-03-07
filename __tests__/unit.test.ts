7d8a72e9a21d8b6c3e7fa071ec1adeea462fd8ee
Leejin-Yang <ejin1996@gmail.com>
2023-03-07 21:54:02 +0900

    test: 유닛 테스트 수정

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -28,14 +28,7 @@ test("새로운 음식점을 음식점 리스트에 추가한다.", () => {
     distance: 5,
   });
 
-  expect(
-    restaurants.getListByOption({ filter: "전체", sort: "name" })
-  ).toMatchObject([
-    {
-      category: "한식",
-      name: "경주 은희네 해장국",
-      distance: 10,
-    },
+  expect(restaurants.getList()).toMatchObject([
     {
       category: "일식",
       name: "스시야좋아",
@@ -46,78 +39,6 @@ test("새로운 음식점을 음식점 리스트에 추가한다.", () => {
       name: "쌀국수맛있다",
       distance: 20,
     },
-    {
-      category: "한식",
-      name: "제주 은희네 해장국",
-      distance: 5,
-    },
-  ]);
-});
-
-test("음식점 리스트를 이름순으로 정렬한다.", () => {
-  const sortedList = restaurants.getListByOption({
-    filter: "전체",
-    sort: "name",
-  });
-
-  expect(sortedList).toMatchObject([
-    {
-      category: "한식",
-      name: "경주 은희네 해장국",
-      distance: 10,
-    },
-    {
-      category: "일식",
-      name: "스시야좋아",
-      distance: 15,
-    },
-    {
-      category: "아시안",
-      name: "쌀국수맛있다",
-      distance: 20,
-    },
-    {
-      category: "한식",
-      name: "제주 은희네 해장국",
-      distance: 5,
-    },
-  ]);
-});
-
-test("음식점 리스트를 거리순으로 정렬한다.", () => {
-  const sortedList = restaurants.getListByOption({
-    filter: "전체",
-    sort: "distance",
-  });
-
-  expect(sortedList).toMatchObject([
-    {
-      category: "한식",
-      name: "제주 은희네 해장국",
-      distance: 5,
-    },
-    {
-      category: "한식",
-      name: "경주 은희네 해장국",
-      distance: 10,
-    },
-    {
-      category: "일식",
-      name: "스시야좋아",
-      distance: 15,
-    },
-    {
-      category: "아시안",
-      name: "쌀국수맛있다",
-      distance: 20,
-    },
-  ]);
-});
-
-test("음식점 리스트를 카테고리별로 필터링한다.", () => {
-  const filteredList = restaurants.filterByCategory("한식");
-
-  expect(filteredList).toMatchObject([
     {
       category: "한식",
       name: "경주 은희네 해장국",
