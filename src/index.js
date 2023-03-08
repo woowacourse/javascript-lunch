08aa1cbf8128006c7bc69dbfac6d7ae1a042f941
woo-jk <201702042@o.cnu.ac.kr>
2023-03-08 17:57:13 +0900

    refactor: (컴포넌트 리팩토링) 리팩토링 한 컴포넌트들 렌더링 구현 완료

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import "../templates/style.css";
 import "./images/add-button.png";
 import "./images/category-asian.png";
 import "./images/category-chinese.png";
@@ -7,6 +6,8 @@ import "./images/category-japanese.png";
 import "./images/category-korean.png";
 import "./images/category-western.png";
 
-import App from "./App.js";
+// import App from "./App.js";
+import "../templates/style.css";
+import "./AppTmp";
 
-new App(document.querySelector("#app"));
+// new App(document.querySelector("#app"));
