108e637fe019cbda434254d4bbfe9074f17a2564
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-08 15:00:25 +0900

    test: 레스토랑 List Item 렌더링 테스트 구현

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,3 +1,21 @@
 /**
  * @jest-environment jsdom
  */
+
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/dom';
+import { ListItem } from '../src/view/ListItem';
+
+test('화면에 ListItem을 렌더링한다.', () => {
+  const listItem = ListItem({
+    category: '한식',
+    name: '김돈이',
+    distance: 5,
+    description: '설명',
+    link: 'link',
+  });
+
+  document.body.insertAdjacentHTML('beforeend', listItem);
+
+  expect(screen.getByText('김돈이')).toBeInTheDocument();
+});
