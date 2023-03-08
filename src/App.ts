c08f8fdd1c3cb9a182eb6538d38c8e6f7028c742
ashleysyheo <ashleysyheo@gmail.com>
2023-03-08 13:54:02 +0900

    refactor: App에서 페이지 초기 렌더링

--- src/App.ts
+++ src/App.ts
@@ -1,59 +1,43 @@
-import { CategoryOptions, SortingCriterion, Restaurant } from './types/types';
+import { RestaurantFilter } from './types/types';
 import { INITIAL_RESTAURANT_DATA } from './constants/data';
-import { saveToLocalStorage, getLocalStorage } from './utils/localStorage';
+import { $ } from './utils/domSelectors';
+import { getLocalStorage } from './utils/localStorage';
+import Header from './components/Header';
+import RestaurantFilters from './components/RestaurantFilters';
 import RestaurantService from './domains/RestaurantService';
-import MainView from './views/MainView';
-import ModalView from './views/ModalView';
+import RestaurantListContainer from './components/RestaurantListContainer';
+import Modal from './components/Modal';
 
-export class App {
+class App {
   private restaurantService: RestaurantService;
-  private mainView = new MainView();
-  private modalView = new ModalView();
-  private currentCategory: CategoryOptions = '전체';
-  private currentSortingCriterion: SortingCriterion = 'name';
+  private currentDisplayStatus: RestaurantFilter = { category: '전체', sorting: 'name' };
+  private body = $('body') as HTMLBodyElement;
 
   constructor() {
     const restaurantList = getLocalStorage() ?? INITIAL_RESTAURANT_DATA;
     this.restaurantService = new RestaurantService(restaurantList);
-    this.bindEventHandlers();
-    this.mainView.renderRestaurantList(
-      this.restaurantService.filterAndSort(this.currentCategory, this.currentSortingCriterion)
-    );
+    this.render();
+    // this.addEvents();
   }
 
-  bindEventHandlers() {
-    this.modalView.addSubmitEventHandler(this.onSubmitRestaurantAddForm);
-    this.mainView.addCategoryChangeEventHandler(this.onChangeCategoryFilter);
-    this.mainView.addSortingChangeEventHandler(this.onChangeSortingFilter);
+  create() {
+    return `
+      ${Header.create()}
+      <main>
+        ${RestaurantFilters.create()}
+        ${RestaurantListContainer.create()}
+        ${Modal.create()}
+      </main>
+    `;
   }
 
-  onSubmitRestaurantAddForm = (restaurantItem: Restaurant) => {
-    this.restaurantService.add(restaurantItem);
-    const restaurantList = this.restaurantService.filterAndSort(
-      this.currentCategory,
-      this.currentSortingCriterion
-    );
-    saveToLocalStorage(restaurantList);
-    this.mainView.renderRestaurantList(restaurantList);
-  };
+  render() {
+    this.body.insertAdjacentHTML('beforeend', this.create());
+  }
 
-  onChangeCategoryFilter = (category: CategoryOptions) => {
-    this.currentCategory = category;
-    const filteredRestaurantList: Restaurant[] = this.restaurantService.filterAndSort(
-      this.currentCategory,
-      this.currentSortingCriterion
-    );
-    this.mainView.renderRestaurantList(filteredRestaurantList);
-  };
-
-  onChangeSortingFilter = (criterion: SortingCriterion) => {
-    this.currentSortingCriterion = criterion;
-    const sortedRestaurantList: Restaurant[] = this.restaurantService.filterAndSort(
-      this.currentCategory,
-      this.currentSortingCriterion
-    );
-    this.mainView.renderRestaurantList(sortedRestaurantList);
-  };
+  // addEvents() {
+  //   RestaurantFilters.addEvent();
+  // }
 }
 
-new App();
+export default new App();
--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -42,4 +42,4 @@ class Modal {
   }
 }
 
-export default Modal;
+export default new Modal();
