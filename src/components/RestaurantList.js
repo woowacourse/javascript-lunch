c14570f94255472cd49471b6ab4eb22208e137a2
cruelladevil <dev.timetravel@gmail.com>
2023-03-08 09:36:14 +0900

    refactor: RestaurantList 컴포넌트 클래스로 변경

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -5,68 +5,52 @@ import western from '../../templates/category-western.png';
 import asian from '../../templates/category-asian.png';
 import etc from '../../templates/category-etc.png';
 
-export default function RestaurantList($root, restaurants) {
-  const $restaurantListSection = document.createElement('section');
-  $restaurantListSection.className = 'restaurant-list-cotainer';
+const CATEGORY_IMAGE_PATH = {
+  한식: korean,
+  중식: chinese,
+  일식: japanese,
+  양식: western,
+  아시안: asian,
+  기타: etc,
+};
 
-  this.state = {
-    restaurantList: [],
-  };
+export default class RestaurantList {
+  constructor(rootElement, restaurants) {
+    this.$root = rootElement;
+    this.restaurants = restaurants;
+  }
 
-  this.init = () => {
-    this.state.restaurantList = restaurants;
-    this.render();
+  render() {
+    this.$root.insertAdjacentHTML('beforeend', this.template());
+  }
 
-    $root.appendChild($restaurantListSection);
-  };
-
-  this.render = () => {
-    $restaurantListSection.innerHTML = `
-    <ul class="restaurant-list">
-      ${this.state.restaurantList.reduce((html, restaurant) => {
-        return html + RestaurantItemTemplate(restaurant.getRestaurantInfo());
-      }, '')}
-    </ul>
+  template() {
+    return `
+      <ul class="restaurant-list">
+        ${this.restaurants.reduce((html, restaurant) => {
+          return html + RestaurantItemTemplate(restaurant);
+        }, '')}
+      </ul>
     `;
-  };
+  }
 
-  this.setState = (state) => {
-    this.state = { ...this.state, ...state };
-    this.render();
-  };
-
-  this.init();
+  remove() {
+    const list = this.$root.querySelector('.restaurant-list');
+    list.remove();
+  }
 }
 
-function RestaurantItemTemplate({ category, distance, name, description }) {
+const RestaurantItemTemplate = ({ category, distance, name, description }) => {
   return `
     <li class="restaurant">
       <div class="restaurant__category">
-        <img src="${categoryImageSource(
-          category
-        )}" alt="${category}" class="category-icon">
+        <img src="${CATEGORY_IMAGE_PATH[category]}" alt="${category}" class="category-icon">
       </div>
       <div class="restaurant__info">
         <h3 class="restaurant__name text-subtitle">${name}</h3>
         <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
         <p class="restaurant__description text-body">${description ?? ''}</p>
       </div>
-    </li>`;
-}
-
-function categoryImageSource(category) {
-  switch (category) {
-    case '한식':
-      return korean;
-    case '중식':
-      return chinese;
-    case '일식':
-      return japanese;
-    case '양식':
-      return western;
-    case '아시안':
-      return asian;
-    case '기타':
-      return etc;
-  }
-}
+    </li>
+  `;
+};
