76702349b2d258114fe8acc6a777f8223a60f2ca
ashleysyheo <ashleysyheo@gmail.com>
2023-03-08 13:41:04 +0900

    refactor: RestaurantItem 컴포넌트 클래스로 수정

--- src/components/RestaurantItem.ts
+++ src/components/RestaurantItem.ts
@@ -1,25 +1,36 @@
 import { Restaurant } from '../types/types';
 import { RESTAURANT_IMAGE } from '../constants/images';
 
-const createRestaurantItem = function (restaurant: Restaurant) {
-  return `<li class="restaurant">
-  <div class="restaurant__category">
-    <img
-      src="${RESTAURANT_IMAGE[restaurant.category]}"
-      alt="${restaurant.category}"
-      class="category-icon"
-    />
-  </div>
-  <div class="restaurant__info">
-    <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
-    <span class="restaurant__distance text-body"
-      >캠퍼스부터 ${restaurant.distance}분 내</span
-    >
-    <p class="restaurant__description text-body">
-      ${restaurant.description ?? ''}
-    </p>
-  </div>
-  </li>`;
-};
+class RestaurantItem {
+  private restaurant: Restaurant;
 
-export default createRestaurantItem;
+  constructor(restaurant: Restaurant) {
+    this.restaurant = restaurant;
+  }
+
+  create() {
+    return `
+      <li class="restaurant">
+        <div class="restaurant__category">
+          <img
+            src="${RESTAURANT_IMAGE[this.restaurant.category]}"
+            alt="${this.restaurant.category}"
+            class="category-icon"
+          />
+        </div>
+        <div class="restaurant__info-container">
+          <div class="restaurant__info">
+            <h3 class="restaurant__name text-subtitle">${this.restaurant.name}</h3>
+            <span class="restaurant__distance text-body"
+              >캠퍼스부터 ${this.restaurant.distance}분 내</span
+            >
+            <p class="restaurant__description text-body">
+              ${this.restaurant.description ?? ''}
+            </p>
+          </div>
+        </div>
+      </li>`;
+  }
+}
+
+export default RestaurantItem;
