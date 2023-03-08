dadf06f31875de319f3bc5807fa4f13bc87a1c3e
cruelladevil <dev.timetravel@gmail.com>
2023-03-08 09:10:47 +0900

    refactor: Header 컴포넌트 클래스로 변환

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,28 @@
-import RestaurantForm from './RestaurantForm';
 import { showModal } from '../modal';
 import addButton from '../../templates/add-button.png';
 
-export default function Header($root) {
-  const $header = document.createElement('header');
-  $header.className = 'gnb';
+export default class Header {
+  constructor(rootElement) {
+    this.$root = rootElement;
+  }
 
-  this.init = () => {
-    this.render();
-    $header.addEventListener('click', showRestaurantAddUI);
-    $root.appendChild($header);
-  };
+  render() {
+    this.$root.insertAdjacentHTML('afterbegin', this.template());
+  }
 
-  this.render = () => {
-    $header.innerHTML = `
-      <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-      <button type="button" class="gnb__button" aria-label="음식점 추가">
-        <img src="${addButton}" alt="음식점 추가">
-      </button>
+  template() {
+    return `
+      <header class="gnb">
+        <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+        <button id="add-restaurant-button" type="button" class="gnb__button" aria-label="음식점 추가">
+          <img src="./add-button.png" alt="음식점 추가">
+        </button>
+      </header>
     `;
-  };
+  }
 
-  this.init();
+  bindEvents() {
+    const modalOpenButton = this.$root.querySelector('#add-restaurant-button');
+    modalOpenButton.addEventListener('click', showModal);
+  }
 }
-
-const showRestaurantAddUI = () => {
-  showModal();
-  const $modalContainer = document.querySelector('.modal-container');
-  new RestaurantForm($modalContainer);
-};
