cfcdf7092337375da7e14b93170bd768db8c4dab
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-08 14:31:53 +0900

    feat: Restaurant.ts -> 새로운 음식점을 추가하는 기능 구현

--- src/domain/Restaurant.ts
+++ src/domain/Restaurant.ts
@@ -1,4 +1,5 @@
-import { Category, MinutesToCampus } from '../data/type';
+type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+type MinutesToCampus = 5 | 10 | 15 | 20 | 30;
 
 interface Restaurant {
   category: Category;
@@ -8,4 +9,16 @@ interface Restaurant {
   link?: string;
 }
 
-export default Restaurant;
+interface Restaurants {
+  list: Restaurant[];
+  add(restaurant: Restaurant): void;
+  filterByCategory(category: Category): Restaurant[];
+}
+
+export const restaurants: Restaurants = {
+  list: [],
+
+  add(restaurant) {
+    this.list = [...this.list, restaurant];
+  },
+};
