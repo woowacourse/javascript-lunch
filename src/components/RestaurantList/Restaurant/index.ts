f6de4e18e9b5f2323928e64365d39e45f7ecd828
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-08 17:51:20 +0900

    refactor(Restaurant): 음식점 컴포넌트 코드 스타일 개선
    구조분해할당 적용 및 이미지 경로 수정

--- src/components/RestaurantList/Restaurant/index.ts
+++ src/components/RestaurantList/Restaurant/index.ts
@@ -4,26 +4,28 @@ type ImgFileName = {
   [key in RestaurantCategory]: string;
 };
 const imgFileName: ImgFileName = {
-  한식: 'category-korean',
-  중식: 'category-chinese',
-  일식: 'category-japanese',
-  아시안: 'category-asian',
-  양식: 'category-western',
-  기타: 'category-etc',
+  한식: 'category-korean.png',
+  중식: 'category-chinese.png',
+  일식: 'category-japanese.png',
+  아시안: 'category-asian.png',
+  양식: 'category-western.png',
+  기타: 'category-etc.png',
 };
 
 const Restaurant = {
   getTemplate(restaurant: IRestaurant) {
+    const { category, name, distance, description } = restaurant;
+
     return `
     <li class="restaurant">
       <div class="restaurant__category">
-        <img src="./${imgFileName[restaurant.category]}.png" alt="${restaurant.category}"
+        <img src="./${imgFileName[category]}" alt="${category}"
         class="category-icon" />
       </div>
       <div class="restaurant__info">
-        <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
-        <span class="restaurant__distance text-body">캠퍼스부터 ${restaurant.distance}분 내</span>
-        <p class="restaurant__description text-body">${restaurant.description}</p>
+        <h3 class="restaurant__name text-subtitle">${name}</h3>
+        <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+        <p class="restaurant__description text-body">${description}</p>
       </div>
     </li>`;
   },
