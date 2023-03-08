009afbc60f006335a15adc4c8359d48c6d80b821
woo-jk <201702042@o.cnu.ac.kr>
2023-03-08 17:56:03 +0900

    refactor: (컴포넌트 리팩토링) Modal 컴포넌트

---
+++ src/componentsTmp/modal/ModalContainer.js
@@ -0,0 +1,40 @@
+import AddRestaurantFormModal from "./AddRestaurantFormModal";
+
+class ModalContainer {
+  $target;
+
+  constructor($target) {
+    this.$target = $target;
+    this.render();
+    this.setEvent();
+  }
+
+  template() {
+    return `
+    <div class="modal-backdrop"></div>
+    <div class="modal-container"></div>
+        `;
+  }
+
+  render() {
+    this.$target.insertAdjacentHTML("beforeend", this.template());
+  }
+
+  toggle() {
+    this.$target.classList.toggle("modal--open");
+  }
+
+  setAddRestaurantForm(restaurantList) {
+    const $modalContainer = this.$target.querySelector(".modal-container");
+    const formModal = new AddRestaurantFormModal($modalContainer);
+
+    formModal.setSubmitEvent(restaurantList);
+    formModal.setCancelEvent(this.toggle.bind(this));
+  }
+
+  setEvent() {
+    this.$target.querySelector(".modal-backdrop").addEventListener("click", this.toggle.bind(this));
+  }
+}
+
+export default ModalContainer;
