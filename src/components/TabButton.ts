0d0e7ae3f9affc17869b8cdbef7072fdf1c4075d
ashleysyheo <ashleysyheo@gmail.com>
2023-03-08 16:32:02 +0900

    feat: 탭 버튼 컴포넌트 생성

---
+++ src/components/TabButton.ts
@@ -0,0 +1,38 @@
+import { Attribute, ButtonContent } from '../types/types';
+import { $ } from '../utils/domSelectors';
+
+class TabButton {
+  private attributes: Attribute;
+  private content: ButtonContent;
+
+  constructor(attributes: Attribute, content: ButtonContent) {
+    this.attributes = attributes;
+    this.content = content;
+  }
+
+  create() {
+    return `
+      <button 
+        id=${this.attributes.id}
+        class="tab-button text-body ${this.attributes.className}">
+        ${this.content}
+      </button>`;
+  }
+
+  addEvent() {
+    const tabButton = $(`#${this.attributes.id}`) as HTMLButtonElement;
+
+    tabButton.addEventListener('click', (event: Event) => {
+      const target = event.target as HTMLButtonElement;
+      const tabButtons = document.querySelectorAll('.tab-button');
+
+      tabButtons.forEach((tabButton) => {
+        tabButton.classList.remove('tab-button--active');
+      });
+
+      target.classList.add('tab-button--active');
+    });
+  }
+}
+
+export default TabButton;
