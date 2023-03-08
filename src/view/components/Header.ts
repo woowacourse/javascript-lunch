afe5bf6120747e7b9166e96d8ad92b8d33fa13a2
turtle601 <turtle601@naver.com>
2023-03-08 09:55:03 +0900

    refactor(Header): 외부로 분리된 store 로직을 Header 컴포넌트에 적용

--- src/view/components/Header.ts
+++ src/view/components/Header.ts
@@ -1,38 +1,32 @@
-type HeaderProps = {
-  $target: HTMLElement;
-  addRestaurantButtonEvent: () => void;
-};
+import { $ } from '../../utils/querySelector';
 
 class Header {
-  #target;
-  #addRestaurantButtonEvent;
+  #root;
 
-  constructor({ $target, addRestaurantButtonEvent }: HeaderProps) {
-    this.#target = $target;
-    this.#addRestaurantButtonEvent = addRestaurantButtonEvent;
-
-    this.#render();
-    this.#setEvent();
+  constructor($root: HTMLElement) {
+    this.#root = $root;
   }
 
   #template() {
     return `
-      <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-      <button type="button" class="gnb__button" aria-label="음식점 추가">
-        <img src="./add-button.png" alt="음식점 추가">
-      </button>
+      <header class="gnb">
+        <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+        <button type="button" class="gnb__button" aria-label="음식점 추가">
+          <img src="./add-button.png" alt="음식점 추가">
+        </button>
+      </header>
     `;
   }
 
-  #render() {
-    this.#target.innerHTML = this.#template();
+  render() {
+    this.#root.insertAdjacentHTML('beforeend', this.#template());
+
+    this.setEvent();
   }
 
-  #setEvent() {
-    this.#target.addEventListener('click', (e: MouseEvent) => {
-      if (e.target instanceof HTMLImageElement) {
-        this.#addRestaurantButtonEvent();
-      }
+  setEvent() {
+    $('.gnb__button')?.addEventListener('click', () => {
+      $('.modal')?.classList.add('modal--open');
     });
   }
 }
