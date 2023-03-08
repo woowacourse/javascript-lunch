2744f4daa93ba104d45d91d8263390240660cffe
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-08 15:26:24 +0900

    test: List 렌더링 테스트 구현

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -5,11 +5,12 @@
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/dom';
 import { ListItem } from '../src/view/ListItem';
+import { List } from '../src/view/List';
 
 test('화면에 ListItem을 렌더링한다.', () => {
   const listItem = ListItem({
-    category: '한식',
-    name: '김돈이',
+    category: '일식',
+    name: '돈카라',
     distance: 5,
     description: '설명',
     link: 'link',
@@ -17,5 +18,38 @@ test('화면에 ListItem을 렌더링한다.', () => {
 
   document.body.insertAdjacentHTML('beforeend', listItem);
 
-  expect(screen.getByText('김돈이')).toBeInTheDocument();
+  expect(screen.getByText('돈카라')).toBeInTheDocument();
+});
+
+test('화면에 List를 렌더링한다.', () => {
+  const restaurantList = [
+    {
+      category: '일식',
+      name: '돈카라',
+      distance: 5,
+      description: '상세 설명',
+      link: 'link',
+    },
+    {
+      category: '한식',
+      name: '김돈이',
+      distance: 5,
+      description: '상세 설명',
+      link: 'link',
+    },
+    {
+      category: '일식',
+      name: '작은동경',
+      distance: 10,
+      description: '상세 설명',
+      link: 'link',
+    },
+  ];
+  const list = List(restaurantList);
+
+  document.body.insertAdjacentHTML('beforeend', list);
+
+  const isRendered = restaurantList.every((restaurant) => screen.getByText(restaurant.name));
+
+  expect(isRendered).toBe(true);
 });
