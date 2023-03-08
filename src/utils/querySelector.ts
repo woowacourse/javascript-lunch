f8583a685d719f0b8383cee0c574bfb238a837a8
turtle601 <turtle601@naver.com>
2023-03-08 09:50:28 +0900

    refactor(querySelector): dom 객체가 null일 경우 로직 추가

--- src/utils/querySelector.ts
+++ src/utils/querySelector.ts
@@ -1,4 +1,12 @@
-const $ = (selector: string) => document.querySelector(selector);
+const $ = (selector: string) => {
+  const dom = document.querySelector(selector);
+
+  if (!dom) {
+    throw new Error('[ERROR] 해당 돔을 찾을 수 없습니다!!');
+  }
+
+  return dom;
+};
 const $$ = (selector: string) => document.querySelectorAll(selector);
 
 export { $, $$ };
