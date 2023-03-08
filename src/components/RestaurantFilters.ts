bfdddb40931c0e99b7eb2e95e376dd0344a094d0
ashleysyheo <ashleysyheo@gmail.com>
2023-03-08 11:16:16 +0900

    feat: 음식점 목록 필터링/정렬 할 수 있는 Restaurant Filters 컴포넌트 생성

---
+++ src/components/RestaurantFilters.ts
@@ -0,0 +1,29 @@
+import { SELECT_OPTIONS } from '../constants/constants';
+import { FILTER_ATTRIBUTE } from '../constants/domAttributes';
+import Select from './Select';
+
+class RestaurantFilters {
+  private categoryFilter: Select;
+  private sortingFilter: Select;
+
+  constructor() {
+    this.categoryFilter = new Select(FILTER_ATTRIBUTE.CATEGORY, SELECT_OPTIONS.CATEGORY_FILTER);
+    this.sortingFilter = new Select(FILTER_ATTRIBUTE.SORTING, SELECT_OPTIONS.SORTING_FILTER);
+  }
+
+  addEvent(changeFilter: CallableFunction) {
+    this.categoryFilter.addSelectChangeEvent(changeFilter);
+    this.sortingFilter.addSelectChangeEvent(changeFilter);
+  }
+
+  create() {
+    return `
+      <section class="restaurant-filter-container">
+        ${this.categoryFilter.create()}
+        ${this.sortingFilter.create()}
+      </section>  
+    `;
+  }
+}
+
+export default new RestaurantFilters();
