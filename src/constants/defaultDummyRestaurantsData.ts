93fd1bb5cc92ebd99fc1ec58ccb74b226df84bea
sh981013s <sh981013s@gmail.com>
2023-03-08 21:56:09 +0900

    feat: 레스토랑 dummy data 한개 추가

--- src/constants/defaultDummyRestaurantsData.ts
+++ src/constants/defaultDummyRestaurantsData.ts
@@ -3,20 +3,28 @@ import IRestaurantInput from '../interfaces/IRestaurantInput';
 
 const defaultDummyRestaurantsData: IRestaurantInput[] = [
   {
-    name: '스시마이우 선릉역점',
-    category: Category.Japanese,
-    distance: '10',
-    description: '스시 너무 맛있어요',
-    link: 'https://www.siksinhot.com/P/408170',
-    isFavorite: true,
-  },
-  {
     name: '김돈이 본점',
     category: Category.Korean,
     distance: '5',
     description: '점심 김치찌개 너무 맛있어용',
     link: 'https://binaural.tistory.com/272',
     isFavorite: false,
+  },
+  {
+    name: '나기',
+    category: Category.Japanese,
+    distance: '5',
+    description: '나기!!',
+    link: 'https://binaural.tistory.com/272',
+    isFavorite: true,
+  },
+  {
+    name: '스시마이우 선릉역점',
+    category: Category.Japanese,
+    distance: '10',
+    description: '스시 너무 맛있어요',
+    link: 'https://www.siksinhot.com/P/408170',
+    isFavorite: true,
   },
 ];
 
