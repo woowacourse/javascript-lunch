7b3857954949aa3eb63bf5a3e24e6caf899b610b
solo5star <solo_5@naver.com>
2023-03-09 01:23:31 +0900

    refactor: 불필요한 스타일 제거

--- src/style/style.ts
+++ src/style/style.ts
@@ -1,15 +1,5 @@
 const style = new CSSStyleSheet();
 style.replace(`
-:root {
-  --primary-color: #ec4a0a;
-  --lighten-color: #f6a88a;
-  --grey-100: #ffffff;
-  --grey-200: #d0d5dd;
-  --grey-300: #667085;
-  --grey-400: #344054;
-  --grey-500: #000000;
-}
-
 * {
   padding: 0;
   margin: 0;
@@ -19,12 +9,6 @@ style.replace(`
 ul,
 li {
   list-style: none;
-}
-
-html,
-body {
-  font-family: sans-serif;
-  font-size: 16px;
 }
 
 .text-title {
