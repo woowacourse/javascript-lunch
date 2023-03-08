48e85e96667bc82821ee52e469625c8d1981c882
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-08 19:47:24 +0900

    design: 자주 가는 음식점 아이콘 마크업 및 경로 지정

--- src/components/RestaurantList/Restaurant/index.ts
+++ src/components/RestaurantList/Restaurant/index.ts
@@ -14,7 +14,9 @@ const imgFileName: ImgFileName = {
 
 const Restaurant = {
   getTemplate(restaurant: IRestaurant) {
-    const { category, name, distance, description } = restaurant;
+    const { category, name, distance, description, isFavorite } = restaurant;
+    const favoriteIconSrc = isFavorite ? 'favorite-icon-filled.png' : 'favorite-icon-lined.png';
+    const favoriteIconAlt = isFavorite ? 'favorite-icon-filled' : 'favorite-icon-lined';
 
     return `
     <li class="restaurant">
@@ -26,6 +28,9 @@ const Restaurant = {
         <h3 class="restaurant__name text-subtitle">${name}</h3>
         <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
         <p class="restaurant__description text-body">${description}</p>
+      </div>
+      <div class="restaurant__favorite">
+        <img src="./${favoriteIconSrc}" alt="${favoriteIconAlt}" class="favorite-icon"/>
       </div>
     </li>`;
   },
