5a1762c0a197c740989e21f932ed4af92e75fe35
chsua <csj1919@naver.com>
2023-03-09 17:12:29 +0900

    feat: 가게 선택 시 이벤트 발생되도록 구현
    - 별 이미지를 선택하는 경우 선호가게 선택/해제되어 리스트 렌더링 수행

--- src/components/MainPage/RestaurantList.ts
+++ src/components/MainPage/RestaurantList.ts
@@ -1,4 +1,6 @@
+import { RestaurantService } from "../../domain/RestaurantService";
 import { RestaurantType } from "../../Template";
+import { $, Render } from "../../until/ControlDom";
 import { Restaurant } from "./Restarant";
 
 export const RestaurantList = {
@@ -8,5 +10,34 @@ export const RestaurantList = {
           .map((restaurant) => Restaurant.template(restaurant))
           .join("")}
         </ul>`;
+  },
+
+  setEvent() {
+    const restaurantListContainer = $(".restaurant-list-container");
+    restaurantListContainer?.addEventListener("click", (event) => {
+      const target = event.target as HTMLElement;
+      const targetId = target?.closest("li")?.id;
+
+      if (!targetId) return;
+      //click star
+
+      if (target.className === "likeImg") {
+        RestaurantService.turnLikeUnlike(+targetId!);
+
+        const restaurantListContainer = $(
+          ".restaurant-list-container"
+        ) as HTMLElement;
+
+        const showSection =
+          restaurantListContainer.classList.length === 1
+            ? RestaurantService.allList
+            : RestaurantService.likeList;
+
+        Render.restaurantList(showSection);
+
+        console.log(RestaurantService.likeList);
+      }
+      //click restaurant
+    });
   },
 };
