3b66de96f7f6fe8e96f5aed0c6496030a8b0988c
TaeeunKim <xodms0309@naver.com>
2023-03-09 15:30:07 +0900

    feat: 즐겨찾기 아이콘 파일 import

--- src/image.ts
+++ src/image.ts
@@ -5,6 +5,9 @@ import western from '../assets/category-western.png';
 import asian from '../assets/category-asian.png';
 import etc from '../assets/category-etc.png';
 
+import filledIcon from '../assets/favorite-icon-filled.png';
+import linedIcon from '../assets/favorite-icon-lined.png';
+
 export const imgSrc = {
   한식: korean,
   중식: chinese,
@@ -12,4 +15,9 @@ export const imgSrc = {
   양식: western,
   아시안: asian,
   기타: etc,
+};
+
+export const iconSrc = {
+  filled: filledIcon,
+  lined: linedIcon,
 };
