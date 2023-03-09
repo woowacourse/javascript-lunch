fdad71a573846a59f671de099cac0ca04108e4bc
shackstack <kms1061@naver.com>
2023-03-09 16:26:58 +0900

    test: 음식점 즐겨찾기 기능 구현 테스트 및 리팩터링

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,26 +1,40 @@
-describe("template spec", () => {
-  it("음식점 추가기능 구현 테스트", () => {
+describe("음식점 관리 기능 구현 테스트", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:8080/");
     cy.get(".gnb__button").click();
     cy.get("#category").select("중식");
     cy.get("#name").type("홍콩반점");
     cy.get("#distance").select("10");
     cy.get("#add-button").click();
-    cy.get("#홍콩반점").should("be.visible");
   });
 
-  it("음식점 삭제기능 구현 테스트", () => {
-    cy.visit("http://localhost:8080/");
+  it("음식점 추가 기능 구현 테스트", () => {
+    cy.get("#홍콩반점").should("be.visible");
+    cy.get(".restaurant-list")
+      .find(".star-container")
+      .find(".star-icon")
+      .should("have.attr", "src")
+      .and("include", "lined");
+  });
 
-    cy.visit("http://localhost:8080/");
-    cy.get(".gnb__button").click();
-    cy.get("#category").select("중식");
-    cy.get("#name").type("홍콩반점");
-    cy.get("#distance").select("10");
-    cy.get("#add-button").click();
-
+  it("음식점 삭제 기능 구현 테스트", () => {
     cy.get("#홍콩반점").click();
     cy.get("#remove-button").click();
     cy.get("#홍콩반점").should("not.exist");
+  });
+
+  it("음식점 즐겨찾기 기능 구현 테스트", () => {
+    cy.get("#홍콩반점").click();
+    cy.get(".modal").find(".star-container").find(".star-icon").should("have.attr", "src").and("include", "lined");
+    cy.get(".modal").find(".star-container").find(".star-icon").click();
+    cy.get(".modal").find(".star-container").find(".star-icon").should("have.attr", "src").and("include", "filled");
+    cy.get(".restaurant-list")
+      .find(".star-container")
+      .find(".star-icon")
+      .should("have.attr", "src")
+      .and("include", "filled");
+    cy.get("#quit-button").click();
+    cy.get("#favorite-restaurants").click();
+    cy.get("#홍콩반점").should("be.visible");
   });
 });
