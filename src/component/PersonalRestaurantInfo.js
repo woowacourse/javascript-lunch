e4d8f3cc48b0117dd3b31f87d693f1f33124da3e
Q Kim <qoli0703@gmail.com>
2023-03-09 11:15:43 +0900

    refactor: 코드상 중복되는 부분 제거

--- src/component/PersonalRestaurantInfo.js
+++ src/component/PersonalRestaurantInfo.js
@@ -4,6 +4,7 @@ import RestaurantInfo from "./RestaurantInfo";
 const createFavoriteButton = (isFilled) => {
   const button = document.createElement("button");
   button.setAttribute("type", "button");
+  button.setAttribute("class", "favorite-button");
 
   const image = document.createElement("img");
   image.src = isFilled ? IMAGE.FILLED_STAR_BTN : IMAGE.EMPTY_STAR_BTN;
@@ -14,39 +15,31 @@ const createFavoriteButton = (isFilled) => {
 
 const favoriteChangeEvent = new CustomEvent("favoriteChange", { bubbles: true });
 
+const createElement = (personalRestaurant, createFn) => {
+  const element = createFn(personalRestaurant.restaurant);
+  const button = createFavoriteButton(personalRestaurant.favorite);
+  element.prepend(button);
+
+  button.addEventListener("click", () => {
+    personalRestaurant.favorite = !personalRestaurant.favorite;
+
+    button.firstElementChild.src = (
+      personalRestaurant.favorite ? IMAGE.FILLED_STAR_BTN : IMAGE.EMPTY_STAR_BTN
+    );
+
+    button.dispatchEvent(favoriteChangeEvent);
+  });
+
+  return element;
+};
+
 const PersonalRestaurantInfo = {
   createSummaryElement(personalRestaurant) {
-    const summary = RestaurantInfo.createSummaryElement(personalRestaurant.restaurant);
-    const button = createFavoriteButton(personalRestaurant.favorite);
-    summary.prepend(button);
-
-    button.addEventListener("click", () => {
-      personalRestaurant.favorite = !personalRestaurant.favorite;
-
-      button.firstElementChild.src = (
-        personalRestaurant.favorite ? IMAGE.FILLED_STAR_BTN : IMAGE.EMPTY_STAR_BTN
-      );
-
-      button.dispatchEvent(favoriteChangeEvent);
-    });
-    return summary;
+    return createElement(personalRestaurant, RestaurantInfo.createSummaryElement);
   },
 
   createDetailedElement(personalRestaurant) {
-    const detailed = RestaurantInfo.createDetailedElement(personalRestaurant.restaurant);
-    const button = createFavoriteButton(personalRestaurant.favorite);
-    detailed.prepend(button);
-
-    button.addEventListener("click", () => {
-      personalRestaurant.favorite = !personalRestaurant.favorite;
-
-      button.firstElementChild.src = (
-        personalRestaurant.favorite ? IMAGE.FILLED_STAR_BTN : IMAGE.EMPTY_STAR_BTN
-      );
-
-      button.dispatchEvent(favoriteChangeEvent);
-    });
-    return detailed;
+    return createElement(personalRestaurant, RestaurantInfo.createDetailedElement);
   },
 };
 
