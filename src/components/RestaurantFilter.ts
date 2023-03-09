581906b62b3ab68434c18cfbaa9a19aa5a4587e2
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-09 17:32:39 +0900

    feat: 음식점 filter 컴포넌트 생성

--- src/components/RestaurantFilter.ts
+++ src/components/RestaurantFilter.ts
@@ -1,34 +1,23 @@
-import { Category, SortCondition } from '../data/type';
-import $ from '../util/dom';
+const RestaurantFilter = {
+  template() {
+    return `
+    <select name="category" id="category-filter" class="restaurant-filter">
+    <option value="전체">전체</option>
+    <option value="한식">한식</option>
+    <option value="중식">중식</option>
+    <option value="일식">일식</option>
+    <option value="양식">양식</option>
+    <option value="아시안">아시안</option>
+    <option value="기타">기타</option>
+    </select>`;
+  },
+  setEvent() {
+    const restaurantFilter = document.querySelector('.restaurant-filter');
 
-class RestaurantFilter {
-  #template;
-
-  constructor() {
-    this.#template = `<section class="restaurant-filter-container"></section>`;
-
-    document.body.insertAdjacentHTML('beforeend', this.#template);
-  }
-
-  static setEventHandler(handler: (condition: SortCondition, category?: Category) => void) {
-    $('.restaurant-filter-container').childNodes.forEach((node) => {
-      node.addEventListener('change', (event) => {
-        event.preventDefault();
-
-        const selectedCondition = document.querySelector(
-          '#sorting-filter  > option:checked',
-        ) as HTMLInputElement;
-
-        const selectedCategory = document.querySelector(
-          '#category-filter > option:checked',
-        ) as HTMLInputElement;
-
-        selectedCategory.value === '전체'
-          ? handler(selectedCondition.value as SortCondition)
-          : handler(selectedCondition.value as SortCondition, selectedCategory.value as Category);
-      });
+    restaurantFilter?.addEventListener('click', () => {
+      // 이벤트 추가 필요
     });
-  }
-}
+  },
+};
 
 export default RestaurantFilter;
