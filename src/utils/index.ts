6959701476204e729ba66c15361fff9c600c60c3
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-09 13:35:42 +0900

    refactor: findImage 메소드 유틸화

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,12 @@
 import BottomSheet from "../components/BottomSheet";
+import {
+  categoryAsian,
+  categoryChinese,
+  categoryJapanese,
+  categoryKorean,
+  categoryWestern,
+  categoryEtc,
+} from "../assets/";
 
 export const closeBottomSheet = () => {
   const bottomSheet = document.getElementById("bottomSheet");
@@ -6,4 +14,16 @@ export const closeBottomSheet = () => {
     return;
   }
   bottomSheet?.close();
+};
+
+export const findImage = (category: string) => {
+  const imageSrc: { [key: string]: string } = {
+    한식: categoryKorean,
+    중식: categoryChinese,
+    일식: categoryJapanese,
+    양식: categoryWestern,
+    아시안: categoryAsian,
+    기타: categoryEtc,
+  };
+  return imageSrc[category];
 };
