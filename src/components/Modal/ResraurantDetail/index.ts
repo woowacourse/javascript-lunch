e073129fa1d067dca1faa2db8394a2cf1488a671
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-03-09 21:52:39 +0900

    design: 음식점 상세정보 모달 마크업 및 스타일 추가

--- src/components/Modal/ResraurantDetail/index.ts
+++ src/components/Modal/ResraurantDetail/index.ts
@@ -1,11 +1,44 @@
+import { RestaurantCategory, IRestaurant } from '../../../types';
+
+type ImgFileName = {
+  [key in RestaurantCategory]: string;
+};
+const imgFileName: ImgFileName = {
+  한식: 'category-korean.png',
+  중식: 'category-chinese.png',
+  일식: 'category-japanese.png',
+  아시안: 'category-asian.png',
+  양식: 'category-western.png',
+  기타: 'category-etc.png',
+};
+
 const RestaurantDetail = {
-  getTemplate() {
+  getTemplate(restaurant: IRestaurant) {
+    const { id, category, distance, isFavorite, name, description, link } = restaurant;
+    const favoriteIconSrc = isFavorite ? 'favorite-icon-filled.png' : 'favorite-icon-lined.png';
+    const favoriteIconAlt = isFavorite ? 'favorite-icon-filled' : 'favorite-icon-lined';
+
     return `
-      <h2 class="modal-title text-title"></h2>
-        <div>
-      ㅎㅇㅇㅎㅇㅎㅇㅎㅇ
-        </div>
-      </form>
+    <div class='modal-detail-view'>
+      <div class="restaurant__category">
+        <img src="./${imgFileName[category]}" alt="${category}" class="category-icon" />
+      </div>
+      <h2 class="modal-title text-title">${name}</h2>    
+      <div class="restaurant__info">    
+        <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+        <p class="restaurant__description text-body">${description}</p>
+      </div>
+      <a href="${link}" target="_blank" class="restaurant__link">${link}</a>         
+      <div class="button-container">
+        <button type="button" id="restaurant-remove-button" class="button button--secondary text-caption">
+          삭제하기
+        </button>
+        <button id="modal-close-button" class="button button--primary text-caption">닫기</button>
+      </div>
+      <div class="restaurant__favorite">
+        <img src="./${favoriteIconSrc}" alt="${favoriteIconAlt}" id="favorite-icon" class="favorite-icon"/>
+      </div>
+    </div>    
     `;
   },
 };
