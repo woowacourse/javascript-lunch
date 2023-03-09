2b2833d2179f5d4db9da39b38b52717b73a50f6e
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-09 11:28:19 +0900

    refactor: 전체 파일 코드 삭제 - Reset

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,16 +1,1 @@
-import HeaderImage from '../../templates/add-button.png';
-
-class Header {
-  #template;
-
-  constructor() {
-    this.#template = `<header class="gnb"><h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-    <button type="button" class="gnb__button" aria-label="음식점 추가">
-      <img src= ${HeaderImage} alt="음식점 추가">
-    </button></header>`;
-
-    document.body.insertAdjacentHTML('afterbegin', this.#template);
-  }
-}
-
-export default Header;
+//reset
--- src/view/List.js
+++ src/view/List.js
@@ -1,8 +1,1 @@
-import { ListItem } from './ListItem';
-
-export const List = (restaurants) => {
-  return `
-  <ul class="restaurant-list">
-    ${restaurants.map((restaurant) => ListItem(restaurant)).join('')}
-  </ul>`;
-};
+//reset
