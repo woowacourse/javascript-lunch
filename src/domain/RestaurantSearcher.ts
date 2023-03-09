648b0741590796ffe94839c95d61fe55e515ebd2
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-09 17:48:58 +0900

    feat(restaurantsearcher): 즐겨찾기에 해당하는 레스토랑 반환을 할 경우 무조건 이름순으로 반환되도록 구현

--- src/domain/RestaurantSearcher.ts
+++ src/domain/RestaurantSearcher.ts
@@ -2,7 +2,7 @@ import { Restaurant, FilterProperties } from '../type';
 
 class RestaurantSearcher {
   /**
-   * 요청한 필터링 옵션이 즐겨찾기한 레스토랑일 경우 즐겨찾기된 레스토랑을 반환하고,
+   * 요청한 필터링 옵션이 즐겨찾기한 레스토랑일 경우 즐겨찾기된 레스토랑을 '이름 순으로' 반환하고,
    * 그렇지 않을 경우에는 카테고리/정렬 옵션으로 필터링한 레스토랑을 반환합니다.
    *
    * @param restaurants - 검색을 실행할 레스토랑 목록
@@ -28,9 +28,11 @@ class RestaurantSearcher {
   }
 
   #getFavoriteRestaurants(restaurants: Restaurant[]) {
-    return [...restaurants].filter(
+    const favoriteRestaurants = [...restaurants].filter(
       (restaurant) => restaurant.isFavorite === true
     );
+
+    return this.#getSortedRestaurants(favoriteRestaurants, 'name');
   }
 
   #getFilteredRestaurantsByCategory(
