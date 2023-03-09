462599c6bbede8bb1307aab17b9f974520803de7
Jungwoo <evencoding@gmail.com>
2023-03-10 00:23:25 +0900

    refactor: header 태그를 정적으로 두고 내부의 html을 정적으로 삽입하도록 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import { $ } from '../utils/dom';
 
-const header = `
-  <header class="gnb">
-    <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-    <button type="button" class="modal-open-button gnb__button" aria-label="음식점 추가">
-      <img src="./add-button.png" alt="음식점 추가" />
-    </button>
-  </header>`;
+const html = `
+  <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+  <button type="button" class="modal-open-button gnb__button" aria-label="음식점 추가">
+    <img src="./add-button.png" alt="음식점 추가" />
+  </button>`;
 
 export default class Header {
   constructor(onClickAddRestaurantButton) {
-    $('body').insertAdjacentHTML('beforebegin', header);
+    $('header').innerHTML = html;
 
     $('.modal-open-button').addEventListener('click', onClickAddRestaurantButton.bind(this));
   }
