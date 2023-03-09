6b9818a9d8c09586d25a6c6464b59d3150b111aa
Jungwoo <evencoding@gmail.com>
2023-03-10 00:17:28 +0900

    refactor: 음식점 item 하나하나에서 click 이벤트를 조작하지 않고 items에서 이벤트 버블링을 하도록 변경

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,8 +1,5 @@
 import { $ } from '../utils/dom';
 
-import store from '../utils/store';
-import RestaurantDetailModal from './RestaurantDetailModal';
-
 const imgFileName = {
   한식: 'category-korean',
   중식: 'category-chinese',
@@ -30,34 +27,8 @@ const html = ({ id, category, name, distance, description, liked }) => `
 
 export default class RestaurantItem {
   restaurant;
-  updateRestaurant;
-  deleteRestaurant;
 
-  constructor(restaurant, updateRestaurant, deleteRestaurant) {
-    this.restaurant = restaurant;
-    this.updateRestaurant = updateRestaurant;
-    this.deleteRestaurant = deleteRestaurant;
-
+  constructor(restaurant) {
     $('.restaurant-list').insertAdjacentHTML('beforeend', html(restaurant));
-
-    $(`#${restaurant.id}`).addEventListener('click', this.onClickRestaurant.bind(this));
-  }
-
-  onClickStarIcon() {
-    this.restaurant.liked = !this.restaurant.liked;
-
-    const likeStar = $(`.${this.restaurant.id} .like-star`);
-    this.restaurant.liked ? likeStar.classList.remove('hidden') : likeStar.classList.add('hidden');
-
-    const updatedRestaurants = this.updateRestaurant(this.restaurant.id, this.restaurant.liked);
-    store.setLocalStorage(updatedRestaurants);
-  }
-
-  onClickRestaurant(e) {
-    if ([...e.target.classList].includes('favorite-icon')) {
-      return this.onClickStarIcon();
-    }
-
-    new RestaurantDetailModal(this.restaurant, this.deleteRestaurant);
   }
 }
