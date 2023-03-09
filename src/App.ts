30431815edc4afc204ec06b250e778bc90f1d080
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-09 23:04:57 +0900

    feat: App class 생성

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,25 @@
-//reset
+import RestaurantFilter from './components/RestaurantFilter';
+import Header from './components/Header';
+import ModalButton from './components/ModalButton';
+
+class App {
+  #app;
+
+  constructor() {
+    this.#app = document.querySelector('#app') as HTMLElement;
+  }
+
+  paint() {
+    this.#app.innerHTML = `
+      ${Header.template()}
+      ${RestaurantFilter.template()}
+    `;
+  }
+
+  setEvent() {
+    RestaurantFilter.setEvent();
+    ModalButton.setEvent();
+  }
+}
+
+export default App;
