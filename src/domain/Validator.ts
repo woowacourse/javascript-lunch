1c67a8b91d3d73518b68f8c029552d69c39ed13e
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-09 16:47:01 +0900

    feat(validator): 레스토랑 데이터 검증 기능 구현

--- src/domain/Validator.ts
+++ src/domain/Validator.ts
@@ -4,47 +4,39 @@ import {
   RESTAURANT_DISTANCES_IN_MINUTE,
   ERROR_MESSAGE,
 } from '../constant/';
+import { UserRestaurantInput } from '../type';
 
-const Validator = {
-  checkCategory: (category: string) => {
-    if (!Validator.isRestaurantCategory(category)) {
-      throw new Error(ERROR_MESSAGE.category);
+class Validator {
+  errorIfInvalidRestaurant(restaurant: UserRestaurantInput) {
+    this.#errorIfInvalidCategory(restaurant.category);
+    this.#errorIfEmptyName(restaurant.name);
+    this.#errorIfInvalidDistanceInMinutes(restaurant.distanceInMinutes);
+    this.#errorIfInvalidLink(restaurant.link);
+  }
+
+  #errorIfInvalidCategory(category: string) {
+    if (!RESTAURANT_CATEGORIES.includes(category)) {
+      throw new Error(ERROR_MESSAGE.categoryIsEmpty);
     }
-  },
+  }
 
-  isRestaurantCategory: (category: string) => {
-    return RESTAURANT_CATEGORIES.includes(category);
-  },
-
-  checkName: (name: string) => {
-    if (Validator.isBlank(name.trim())) {
-      throw new Error(ERROR_MESSAGE.name);
+  #errorIfEmptyName(name: string) {
+    if (name.trim().length === 0) {
+      throw new Error(ERROR_MESSAGE.nameIsEmpty);
     }
-  },
+  }
 
-  isBlank: (name: string) => {
-    return name.length === 0;
-  },
-
-  checkDistance: (distance: string) => {
-    if (!Validator.isRestaurantDistance(distance)) {
-      throw new Error(ERROR_MESSAGE.distance);
+  #errorIfInvalidDistanceInMinutes(distanceInMinutes: string) {
+    if (distanceInMinutes.trim().length === 0) {
+      throw new Error(ERROR_MESSAGE.distanceInMinutesIsEmpty);
     }
-  },
+  }
 
-  isRestaurantDistance: (distance: string) => {
-    return RESTAURANT_DISTANCES_IN_MINUTE.includes(distance);
-  },
-
-  checkLink: (link: string) => {
-    if (!Validator.isLink(link)) {
-      throw new Error(ERROR_MESSAGE.link);
+  #errorIfInvalidLink(link: string) {
+    if (!REGEX.link.test(link)) {
+      throw new Error(ERROR_MESSAGE.linkIsInvalid);
     }
-  },
-
-  isLink: (link: string) => {
-    return REGEX.link.test(link);
-  },
-};
+  }
+}
 
 export default Validator;
