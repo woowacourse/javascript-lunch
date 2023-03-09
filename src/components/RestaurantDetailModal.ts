2ca045de694e5d5e658fc837e744e5bd017dfb34
chex <chex417@naver.com>
2023-03-09 22:44:52 +0900

    fix: 음식점 상세정보 모달에 href 속성 값 추가

--- src/components/RestaurantDetailModal.ts
+++ src/components/RestaurantDetailModal.ts
@@ -18,7 +18,14 @@ export const RestaurantDetailModal = (restaurant: Restaurant, categoryImageUrl: 
             <h3 class="restaurant__name text-subtitle">${name}</h3>
             <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
             <p class="restaurant__description text-body">${description ?? ''}</p>
-            <a class="restaurant__link text-body" href="#">${link ?? ''}</a>
+            <a class="restaurant__link text-body" href="${link ?? ''}">${link ?? ''}</a>
         </div>
+
+        <div class="button-container">
+        <button type="button" id="modal-remove-button" class="button button--secondary text-caption">
+          삭제하기
+        </button>
+        <button class="button button--primary text-caption">닫기</button>
+      </div>
     </div>`;
 };
