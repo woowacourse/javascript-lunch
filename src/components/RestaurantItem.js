89c0763a7a8d38290a6fa4db0b6e3885766c531b
unknown <tkdrb15@gmail.com>
2023-03-09 17:20:27 +0900

    style: RetaurantItem컴포넌트에 hover 애니메이션 효과 추가

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -19,14 +19,16 @@ class RestaurantItem {
     return `
       <li id="restaurant${this.restaurant.ID}" class="restaurant">
 
-        <div class="restaurant__category">
-            <img src="./${imageFile}.png" alt="${this.restaurant.category}" class="category-icon">
-        </div>
+        <div class="restaurant__category__info">
+          <div class="restaurant__category">
+              <img src="./${imageFile}.png" alt="${this.restaurant.category}" class="category-icon">
+          </div>
 
-        <div class="restaurant__info">
-            <h3 class="restaurant__name text-subtitle">${this.restaurant.name}</h3>
-            <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurant.distance}분 내</span>
-            <p class="restaurant__description text-body">${this.restaurant.description}</p>
+          <div class="restaurant__info">
+              <h3 class="restaurant__name text-subtitle">${this.restaurant.name}</h3>
+              <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurant.distance}분 내</span>
+              <p class="restaurant__description text-body">${this.restaurant.description}</p>
+          </div>
         </div>
 
         <img src="./${iconName}.png" class="favorite-icon">
