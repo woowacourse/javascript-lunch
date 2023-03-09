c48fdf41f393974464d20658e6a2184ace2f0911
minjaeKim <smallkdb@gmail.com>
2023-03-09 19:17:09 +0900

    fix: favorite ui가 변하지 않는 형상 수정

--- src/components/RestaurantList/FavoriteIcon.ts
+++ src/components/RestaurantList/FavoriteIcon.ts
@@ -4,16 +4,16 @@ import RestaurantListItem from '../../domain/RestaurantListItem';
 const FavoriteIcon = {
   template(favorite: boolean, id: string) {
     return `
-    <div class="favorite" data-id="${id}" data-active="false">
+    <div class="favorite" data-id="${id}" data-active="${favorite}">
       <img src="${favorite ? Favorite.filled : Favorite.lined}" alt="자주 가는 음식점 버튼" id="favorite-lined"/>
     </div>`;
   },
   setEvent(RestaurantListItem: RestaurantListItem) {
     const favorite = document.querySelectorAll('.favorite') as NodeListOf<HTMLDivElement>;
-
     favorite.forEach((item: HTMLDivElement) => {
       item?.addEventListener('click', (e) => {
         e.stopPropagation();
+
         const id = item.dataset.id;
         const isActive = item.dataset.active;
 
