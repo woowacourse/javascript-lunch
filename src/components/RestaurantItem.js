141991e6fa6fbedbb9e4fe9ccc948fb8625e548c
unknown <tkdrb15@gmail.com>
2023-03-09 14:51:39 +0900

    feat: RestaurantItem 컴포넌트에서 음식점 즐겨찾기 여부를 아이콘으로 표시하다

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -8,17 +8,23 @@ class RestaurantItem {
 
   template() {
     const imageFile = CATEGORY_TO_FILENAME[this.restaurant.category];
+    const iconName = FOVORITES_TO_FILENAME[this.restaurant.favorites];
 
     return `
       <li id="restaurant${this.restaurant.ID}" class="restaurant">
+
         <div class="restaurant__category">
             <img src="./${imageFile}.png" alt="${this.restaurant.category}" class="category-icon">
         </div>
+
         <div class="restaurant__info">
             <h3 class="restaurant__name text-subtitle">${this.restaurant.name}</h3>
             <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurant.distance}분 내</span>
             <p class="restaurant__description text-body">${this.restaurant.description}</p>
         </div>
+
+        <img src="./${iconName}.png" class="favorite-icon">
+
       </li>
       `;
   }
