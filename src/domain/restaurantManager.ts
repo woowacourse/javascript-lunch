11f986cfa2d48302085b3b91b67e91d7e05c2026
Suyoung <jsy8492@gmail.com>
2023-03-09 22:12:04 +0900

    refactor: 음식점 샘플 데이터에 link 프로퍼티 추가

--- src/domain/restaurantManager.ts
+++ src/domain/restaurantManager.ts
@@ -5,7 +5,7 @@ import { getLocalStorage } from '../utils/localStorage';
 type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
 type Distance = 5 | 10 | 15 | 20 | 30;
 
-interface Restaurant {
+export interface Restaurant {
   id: string;
   category: Category;
   name: string;
--- src/domain/sampleRestaurants.ts
+++ src/domain/sampleRestaurants.ts
@@ -1,4 +1,6 @@
-export const sampleRestaurants = [
+import { Restaurant } from './restaurantManager';
+
+export const sampleRestaurants: Restaurant[] = [
   {
     id: '61aa0b38-8afa-47ec-8b24-326811667941',
     category: '한식',
@@ -10,6 +12,7 @@ export const sampleRestaurants = [
     선보인다. 콩비지와 피양 만두가 이곳의 대표 메뉴지만, 할머니가 옛날 방식을 고수하며
     만들어내는 비지전골 또한 이 집의 역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이
     먹고 싶은 만큼 덜어 먹을 수 있게 준비돼 있다.`,
+    link: 'https://naver.me/GWr9PPyQ',
   },
   {
     id: '72450846-e579-4cd4-8169-4726e243dc52',
@@ -18,6 +21,7 @@ export const sampleRestaurants = [
     distance: 5,
     description: `Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과 정성으로 정통 중식의 세계를
     펼쳐갑니다.`,
+    link: 'https://naver.me/535UmYXk',
   },
   {
     id: 'c655a6bc-ee61-4e98-b8a1-860be3a88126',
@@ -26,6 +30,7 @@ export const sampleRestaurants = [
     distance: 10,
     description: `잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은 정성을 이길 수 없다는
     신념으로 모든 음식에 최선을 다하는 잇쇼우는 고객 한분 한분께 최선을 다하겠습니다`,
+    link: 'https://naver.me/Fvn7VlT8',
   },
   {
     id: '5651a460-54a1-49c8-8c02-de98c5082c04',
@@ -33,6 +38,7 @@ export const sampleRestaurants = [
     name: '이태리키친',
     distance: 20,
     description: `늘 변화를 추구하는 이태리키친입니다.`,
+    link: 'https://naver.me/Goz3Wdxi',
   },
   {
     id: 'f63e7177-3224-49b6-ad25-cadd6bbf489c',
@@ -40,6 +46,7 @@ export const sampleRestaurants = [
     name: '호아빈 삼성점',
     distance: 15,
     description: `푸짐한 양에 국물이 일품인 쌀국수`,
+    link: 'https://naver.me/xivwmLnp',
   },
   {
     id: '40ce4535-8f85-4701-a6a4-e259b3036573',
@@ -47,5 +54,6 @@ export const sampleRestaurants = [
     name: '도스타코스 선릉점',
     distance: 5,
     description: `멕시칸 캐주얼 그릴`,
+    link: 'https://naver.me/5xlLtY0m',
   },
 ];
