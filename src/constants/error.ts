165bf1835dc6562a81047d3db7045854a4d63d84
n0eyes <doll4586633@naver.com>
2023-03-09 14:58:51 +0900

    refactor: 에러 메세지 오타 수정

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -9,6 +9,7 @@ const INVALID_ERROR_CODE = '[ERROR] 잘못된 에러코드 입니다.';
 const NOT_STRING = `[ERROR] 문자열만 입력해 주세요.`;
 const EXCEED_MAXIMUM_NAME_LENGTH = `[ERROR] 10글자 이하로 입력해 주세요.`;
 const EMPTY_VALUE = `[ERROR] 필수 항목입니다.`;
+const INVALID_CATEGORY = `[ERROR] 유효하지 않은 항목 입니다.`;
 
 export const ERROR_CODE = Object.freeze({
   UNEXPECTED_ERROR: 'UNEXPECTED_ERROR',
@@ -16,6 +17,7 @@ export const ERROR_CODE = Object.freeze({
   EXCEED_MAXIMUM_NAME_LENGTH: 'EXCEED_MAXIMUM_NAME_LENGTH',
   NOT_STRING: 'NOT_STRING',
   EMPTY_VALUE: 'EMPTY_VALUE',
+  INVALID_CATEGORY: 'INVALID_CATEGORY',
 });
 
 export const ERROR_MESSAGE: CodeToMessage<typeof ERROR_CODE> = Object.freeze({
@@ -24,6 +26,7 @@ export const ERROR_MESSAGE: CodeToMessage<typeof ERROR_CODE> = Object.freeze({
   EXCEED_MAXIMUM_NAME_LENGTH,
   NOT_STRING,
   EMPTY_VALUE,
+  INVALID_CATEGORY,
 });
 
 export type ErrorCode = typeof ERROR_CODE;
