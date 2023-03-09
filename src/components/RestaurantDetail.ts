ad67cd5612693baff68896d3c46de4a316c71a9c
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-09 13:44:25 +0900

    feat: RestaurantDetail component 구현

---
+++ src/components/RestaurantDetail.ts
@@ -0,0 +1,39 @@
+import Controller from "../domain/Controller";
+import RestaurantType from "../type/Restaurant";
+import { findImage } from "../utils";
+class RestaurantDetail extends HTMLElement {
+  private controller;
+  constructor() {
+    super();
+    this.controller = Controller.getInstance();
+    this.render(this.controller.getSelectedRestaurant());
+  }
+  render(restaurant: RestaurantType) {
+    this.innerHTML = `
+      <div class="detail-container">
+        <div class="restaurant__category">
+          <img src="${findImage(restaurant.category)}" alt="${
+      restaurant.category
+    }" class="category-icon">
+        </div>
+        <div id="restaurantInfo" class="restaurant__detail__info">
+          <h3 class="restaurant__detail__name text-subtitle">${
+            restaurant.name
+          }</h3>
+          <span class="restaurant__detail__distance text-body">캠퍼스부터 ${
+            restaurant.distance
+          }분 내</span>
+          <p class="restaurant__detail__description text-body">${
+            restaurant.description
+          }</p>
+          <a href="${restaurant.link}" class="text-body">${restaurant.link}</a>
+        </div>
+        <div class="button-container">
+          <button id="cancelButton" type="button" class="button button--secondary text-caption">삭제하기</button>
+          <button type="submit" class="button button--primary text-caption">닫기</button>
+        </div>
+      </div>
+    `;
+  }
+}
+export default RestaurantDetail;
