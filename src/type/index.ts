5c99c4179e2b03deaeb216151f7555fb57814bb9
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-09 13:03:33 +0900

    feat: 레스토랑 검색 필터 타입 추가

--- src/type/index.ts
+++ src/type/index.ts
@@ -11,7 +11,19 @@ type Restaurant = UserRestaurantInput & {
   itemId: number;
 };
 
+type FilterProperties = {
+  filterBy: string;
+  sortBy: string;
+  favoriteBy: string;
+};
+
 type CategoryType = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
 type SelectCategoryType = CategoryType | '전체';
 
-export { UserRestaurantInput, Restaurant, CategoryType, SelectCategoryType };
+export {
+  UserRestaurantInput,
+  Restaurant,
+  FilterProperties,
+  CategoryType,
+  SelectCategoryType,
+};
