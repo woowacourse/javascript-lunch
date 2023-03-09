e9ce3df64462342aeae465a4322e557a095cfb7f
shackstack <kms1061@naver.com>
2023-03-09 13:41:23 +0900

    style: 하이퍼링크 스타일 수정

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -13,19 +13,18 @@ export default class RestaurantDetail {
   render() {
     const { category, name, distance, description, link } = this.props;
     this.$target.innerHTML = `
-    <div>
+  <div>
     <div class="flex justify-between">
       <div class="restaurant__category">
         <img src="./category-${translateCategory[category]}.png" alt="${category}" class="category-icon" />
       </div>
-      <div class="star-container">
-      </div>
+      <div class="star-container"></div>
     </div>
-  </div>
+   </div>
   <h2 class="mt-16">${name}</h1>
   <div>
     <p class="mt-16 text-body restaurant__distance">캠퍼스로부터 ${distance}분 내</p>
-    <div class="mt-16">${description}</div>
+    <div class="mt-16 mb-16">${description}</div>
     <a href="${link}">${link}</a>
     <div class="mt-16 button-container">
     <button id="remove-button" class="button button--secondary text-caption">
