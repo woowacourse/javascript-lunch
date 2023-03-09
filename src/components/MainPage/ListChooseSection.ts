bee27ab967227704a766f3ed7fb8e4c96a592a92
chsua <csj1919@naver.com>
2023-03-09 19:01:36 +0900

    refactor: 모든, 선호 가게 영역 전환 이벤트 리팩토링

--- src/components/MainPage/ListChooseSection.ts
+++ src/components/MainPage/ListChooseSection.ts
@@ -15,53 +15,35 @@ export const ListChooseSection = {
     const listChooseSection = $(".list-choose-section");
     listChooseSection?.addEventListener("click", (event) => {
       const target = event.target as HTMLElement;
-      if (target.className === "list-choose-section") return;
 
       //무의미한 클릭 이벤트 제거
+      if (target.className === "list-choose-section") return;
       if (target.classList.length === 2) return;
 
-      //모든 음식점 클릭
-      if (target.classList[0] === "allList") {
-        //list like로 토글
-        const restaurantListContainer = $(
-          ".restaurant-list-container"
-        ) as HTMLElement;
-        BottomSheetForm.showClose(restaurantListContainer, "show-Like-List");
-        console.log(restaurantListContainer);
-
-        //상대 섹션 select 풀기 => css 적용
-        target.classList.add("selectList");
-        const oppositeSection = $(".likeList") as HTMLElement;
-        oppositeSection.className = "likeList";
-
-        // 필터 섹션 보이기
-        const filterSection = $(".restaurant-filter-container") as HTMLElement;
-        filterSection.style.display = "flex";
-
-        //모든 레스토랑 보이기
-        RestaurantList.renderRestaurantList(RestaurantData.allList);
-        return;
-      }
-      //선호음식점 클릭
-      if (target.classList[0] === "likeList") {
-        //list like로 토글
-        const restaurantListContainer = $(
-          ".restaurant-list-container"
-        ) as HTMLElement;
-        BottomSheetForm.showClose(restaurantListContainer, "show-Like-List");
-        console.log(restaurantListContainer);
-
-        //상대 섹션 select 풀기
-        target.classList.add("selectList");
-        const oppositeSection = $(".allList") as HTMLElement;
-        oppositeSection.className = "allList";
-
-        // 필터 섹션 감추기
-        const filterSection = $(".restaurant-filter-container") as HTMLElement;
-        filterSection.style.display = "none";
-        // 선호 레스토랑 보이기
-        RestaurantList.renderRestaurantList(RestaurantData.likeList);
-      }
+      this.transSection(target, target.classList[0]);
     });
+  },
+
+  transSection(target: HTMLElement, ListClass: string) {
+    const oppositeList = ListClass === "likeList" ? "allList" : "likeList";
+
+    BottomSheetForm.showClose(
+      $(".restaurant-list-container") as HTMLElement,
+      "show-Like-List"
+    );
+    BottomSheetForm.showClose(
+      $(".restaurant-filter-container") as HTMLElement,
+      "show-filter"
+    );
+
+    target.classList.add("selectList");
+    const oppositeSection = $(`.${oppositeList}`) as HTMLElement;
+    oppositeSection.className = oppositeList;
+
+    RestaurantList.renderRestaurantList(
+      oppositeList === "likeList"
+        ? RestaurantData.allList
+        : RestaurantData.likeList
+    );
   },
 };
