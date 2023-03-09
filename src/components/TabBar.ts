f7e6098a1b9141f83db1f99af0a216719e620691
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-09 15:23:52 +0900

    refactor: TabBar component radio 형식으로 변경

--- src/components/TabBar.ts
+++ src/components/TabBar.ts
@@ -1,43 +1,29 @@
 import Controller from "../domain/Controller";
 class TabBar extends HTMLElement {
   private controller;
-  private selectedIndex;
+
   constructor() {
     super();
     this.controller = Controller.getInstance();
-    this.selectedIndex = 0;
     this.render();
     this.onClickTab();
   }
   render() {
     this.innerHTML = `
-      <div class="tab-container">
-        <div id="tab-${0}" class="tab tab-selected">모든 음식점</div>
-        <div id="tab-${1}" class="tab">자주 가는 음식점</div>
-      </div>
+      <form id="tabContainer" class="tab-container">
+        <input id="all" name="tab" value="all" type="radio" class="radio" checked>
+        <label id="tab" class="tab tab-selected" for="all">모든 음식점</label>
+        <input id="favorite" name="tab" value="favorite" type="radio" class="radio">
+        <label id="tab" class="tab" for="favorite">자주 가는 음식점</label>
+      </form>
     `;
   }
   onClickTab() {
-    const tabs = this.querySelectorAll(".tab");
-    tabs.forEach((tab, index) => {
-      tab.addEventListener("click", () => {
-        this.selectedIndex = index;
-        if (this.selectedIndex == 0) {
-          document
-            .getElementById(`tab-${this.selectedIndex}`)
-            ?.classList.add("tab-selected");
-          document.getElementById(`tab-${1}`)?.classList.remove("tab-selected");
-          this.controller.loadLocalStorage();
-          this.controller.renderRestaurantList();
-        }
-        if (this.selectedIndex == 1) {
-          document
-            .getElementById(`tab-${this.selectedIndex}`)
-            ?.classList.add("tab-selected");
-          document.getElementById(`tab-${0}`)?.classList.remove("tab-selected");
-          this.controller.renderFavoriteRestaurantList();
-        }
-      });
+    const tabContainer = document.getElementById("tabContainer");
+    tabContainer?.addEventListener("change", (event: any) => {
+      if (event.target.value == "favorite") {
+        this.controller.renderFavoriteRestaurantList();
+      }
     });
   }
 }
