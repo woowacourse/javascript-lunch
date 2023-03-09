4258349c1978e551b39b82241b4aef22afbd2868
AhnGeunTaek <dksrmsxor@gmail.com>
2023-03-09 11:42:09 +0900

    feat: 레스토랑 Modal 컴포넌트 생성

--- src/components/RestaurantAddModal.ts
+++ src/components/RestaurantAddModal.ts
@@ -1,66 +1,19 @@
-class RestaurantAddModal {
-  #template;
+import RestaurantaddButtonImg from '../../templates/add-button.png';
 
-  constructor() {
-    this.#template = `<div class="modal">
-    <div class="modal-backdrop"></div>
-    <div class="modal-container">
-      <h2 class="modal-title text-title">새로운 음식점</h2>
-      <form>
-        <div class="form-item form-item--required">
-        
-        <!-- 카테고리 -->
-          <label for="category text-caption">카테고리</label>
-          <select name="category" id="category" required>
-            <option value="">선택해 주세요</option>
-            ${['한식', '중식', '일식', '양식', '아시안', '기타']
-              .map((category) => `<option value="${category}">${category}</option>`)
-              .join('')}
-          </select>
-        </div>
-        
-        <!-- 음식점 이름 -->
-        <div class="form-item form-item--required">
-          <label for="name text-caption">이름</label>
-          <input type="text" name="name" id="name" required>
-        </div>
-        
-        <!-- 거리 -->
-        <div class="form-item form-item--required">
-          <label for="distance text-caption">거리(도보 이동 시간) </label>
-          <select name="distance" id="distance" required>
-            <option value="">선택해 주세요</option>
-           ${[5, 10, 15, 20, 30]
-             .map((distance) => `<option value="${distance}">${distance}분 내</option>`)
-             .join('')}
-          </select>
-        </div>
+const ModalButton = {
+  print() {
+    const tamplate = `
+<button type="button" class="gnb__button" aria-label="음식점 추가">
+  <img src=${RestaurantaddButtonImg} alt="음식점 추가">
+</button>`;
 
-        <!-- 설명 -->
-        <div class="form-item">
-          <label for="description text-caption">설명</label>
-          <textarea name="description" id="description" cols="30" rows="5"></textarea>
-          <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
-        </div>
+    return tamplate;
+  },
+  setEvent() {
+    document.querySelector('.gnb__button').addEventListener('click', () => {
+      // TODO: 이벤트 추가
+    });
+  },
+};
 
-        <!-- 링크 -->
-        <div class="form-item">
-          <label for="link text-caption">참고 링크</label>
-          <input type="text" name="link" id="link">
-          <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
-        </div>
-
-        <!-- 취소/추가 버튼 -->
-        <div class="button-container">
-          <button type="button" class="button button--secondary text-caption">취소하기</button>
-          <button class="button button--primary text-caption">추가하기</button>
-        </div>
-      </form>
-    </div>
-    </div>`;
-
-    document.body.insertAdjacentHTML('beforeend', this.#template);
-  }
-}
-
-export default RestaurantAddModal;
+export default ModalButton;
