bbc4a2a0c60ef38bf87256e161bdc240340fba7a
Dahye <06robin11@gmail.com>
2023-03-09 19:33:14 +0900

    test: 정렬, 카테고리 필터링 테스트

--- cypress/e2e/lunch.cy.js
+++ cypress/e2e/lunch.cy.js
@@ -16,4 +16,93 @@ describe("My First Test", () => {
 
     cy.contains("본죽");
   });
+
+  it("카테고리 선택시 해당 카테고리 식당만 보여준다.", () => {
+    cy.viewport(390, 880);
+
+    cy.visit("http://localhost:8080/");
+
+    window.localStorage.setItem(
+      "restuarantList",
+      JSON.stringify([
+        {
+          category: "한식",
+          name: "피양콩할마니",
+          takingTime: 10,
+          description: `할머니가 옛날 방식을 고수하며 만들어내는 비지전골 또한 이 집의 역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은 만큼 덜어 먹을 수 있게 준비돼 있다.`,
+          link: "https://naver.me/G6DyD9tg",
+          id: "123",
+          bookmarked: false,
+        },
+        {
+          category: "일식",
+          name: "일일일",
+          takingTime: 15,
+          description: `이 집의 역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은 만큼 덜어 먹을 수 있게 준비돼 있다.`,
+          link: "https://naver.me/G6DyD9tg",
+          id: "4646654",
+          bookmarked: false,
+        },
+        {
+          category: "일식",
+          name: "잇쇼우",
+          takingTime: 5,
+          description: `잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은 정성을 이길...`,
+          link: "",
+          id: "565656",
+          bookmarked: false,
+        },
+      ])
+    );
+
+    cy.get("#category-filter").select("일식");
+
+    cy.contains("일일일");
+    cy.contains("잇쇼우");
+
+    cy.get("#category-filter").select("한식");
+    cy.contains("피양콩");
+  });
+
+  it("거리순으로 정렬 옵션 클릭시 거리순으로 정렬한다.", () => {
+    cy.viewport(390, 880);
+
+    cy.visit("http://localhost:8080/");
+
+    window.localStorage.setItem(
+      "restuarantList",
+      JSON.stringify([
+        {
+          category: "한식",
+          name: "피양콩할마니",
+          takingTime: 10,
+          description: `할머니가 옛날 방식을 고수하며 만들어내는 비지전골 또한 이 집의 역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은 만큼 덜어 먹을 수 있게 준비돼 있다.`,
+          link: "https://naver.me/G6DyD9tg",
+          id: "123",
+          bookmarked: false,
+        },
+        {
+          category: "일식",
+          name: "일일일",
+          takingTime: 15,
+          description: `이 집의 역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은 만큼 덜어 먹을 수 있게 준비돼 있다.`,
+          link: "https://naver.me/G6DyD9tg",
+          id: "4646654",
+          bookmarked: false,
+        },
+        {
+          category: "일식",
+          name: "잇쇼우",
+          takingTime: 5,
+          description: `잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은 정성을 이길...`,
+          link: "",
+          id: "565656",
+          bookmarked: false,
+        },
+      ])
+    );
+
+    cy.get("#sorting-filter").select("거리순");
+    cy.get(".restaurant-list").first().contains("잇쇼우");
+  });
 });
