c72bde711cc32315e76b1accdb1c8fd05a3d5582
2yunseong <dbsdltjd3701@naver.com>
2023-03-09 16:23:50 +0900

    refactor: 메서드 분리

--- src/pages/RestaurantListPage/RestaurantList.ts
+++ src/pages/RestaurantListPage/RestaurantList.ts
@@ -37,6 +37,7 @@ export default class RestaurantList implements Component<RestaurantListState> {
   }: RestaurantListProps) {
     this.$target = document.createElement('ul');
     this.$target.classList.add('restaurant-list');
+
     this.state = {
       restaurants,
       category,
@@ -49,7 +50,7 @@ export default class RestaurantList implements Component<RestaurantListState> {
     $parent.append(this.$target);
   }
 
-  render() {
+  getTemplate() {
     const fragment = document.createDocumentFragment();
 
     this.categorizeRestaurantByOption().forEach((restaurant) => {
@@ -61,26 +62,39 @@ export default class RestaurantList implements Component<RestaurantListState> {
       }).render();
     });
 
-    this.$target.append(fragment);
+    return fragment;
   }
 
-  // TODO: 메서드 분할하기 (너무 크다)
-  categorizeRestaurantByOption() {
-    const { category, sortBy } = this.state;
-    const filteredByTabBarSelect = this.state.restaurants.filter((restaurant) =>
+  render() {
+    this.$target.append(this.getTemplate());
+  }
+
+  filterByTabBarSelect(restaurants: Restaurant[]) {
+    return restaurants.filter((restaurant) =>
       this.state.tabBarSelect === 'favorite' ? restaurant.isFavorite : restaurant
     );
-    const filteredByCategory = filteredByTabBarSelect.filter(
+  }
+
+  filterByCategory(restaurants: Restaurant[], category: Category) {
+    return restaurants.filter(
       (restaurant) => category === DEFAULT_CATEGORY || restaurant.category === category
     );
+  }
 
+  filterBySorting(restaurants: Restaurant[], sortBy: SortBy) {
     const getPivot = (restaurant: Restaurant) =>
       sortBy === 'name' ? restaurant.name : Number(restaurant.distance);
-
-    return filteredByCategory.sort((a, b) => {
+    return restaurants.sort((a, b) => {
       if (getPivot(a) > getPivot(b)) return 1;
       if (getPivot(a) < getPivot(b)) return -1;
       return 0;
     });
+  }
+
+  categorizeRestaurantByOption() {
+    return this.filterBySorting(
+      this.filterByCategory(this.filterByTabBarSelect(this.state.restaurants), this.state.category),
+      this.state.sortBy
+    );
   }
 }
