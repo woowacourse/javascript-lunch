e98102dfbf7a588674f652264ff008be77104f3c
2yunseong <dbsdltjd3701@naver.com>
2023-03-09 13:45:07 +0900

    test: 음식 종류 별 정렬 기능 테스트

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,5 @@
+import { initialList } from '../data';
+
 const TEST_URL = 'http://localhost:8080/';
 
 describe('음식점 추가 창', () => {
@@ -14,4 +16,50 @@ describe('음식점 추가 창', () => {
     cy.get('#modal-cancel').click();
     cy.get('.modal').should('not.exist');
   });
+
+  it('카테고리, 이름, 거리, 설명, 링크 `입력창`을 작성하고 `추가하기 버튼`을 `클릭`하면 음식점이 목록에 추가된다. (음식점 목록 추가)', () => {
+    cy.visit(TEST_URL);
+    cy.get('.gnb__button').click();
+    cy.contains('새로운 음식점');
+    cy.get('#category').select('한식');
+    cy.get('#name').type('얌샘김밥');
+    cy.get('#distance').select('5');
+    cy.get('#description').type('아직 한번도 안가봄');
+    cy.get('#link').type('http://www.naver.com');
+    cy.get('.button-container').contains('추가하기').click();
+    cy.get('.app').contains('5분 내');
+  });
+});
+
+describe('음식점 목록, 드롭다운 메뉴/탭바', () => {
+  it('드롭다운 메뉴를 통해 음식점 목록을 음식 종류 별로 정렬할 수 있다.', () => {
+    cy.visit(TEST_URL, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem('restaurants', JSON.stringify(initialList));
+      },
+    });
+    cy.get('#category-filter').select('한식');
+    cy.get('.app').contains('가_한식');
+    cy.get('.app').contains('나_한식');
+    cy.get('.app').contains('다_중식').should('not.exist');
+    cy.get('.app').contains('라_일식').should('not.exist');
+    cy.get('.app').contains('하_오분거리 양식').should('not.exist');
+    cy.get('.app').contains('하_오분거리 아시안').should('not.exist');
+  });
+
+  it('드롭다운 메뉴를 통해 음식점 목록을 이름/거리 순으로 정렬할 수 있다.', () => {});
+
+  it('탭바를 통해 음식점 목록을 모든 음식점/자주 가는 음식점 으로 분류하여 볼 수 있다.', () => {});
+
+  it('음식점 목록에서 `별표 버튼`을 `클릭`해 자주 가는 음식점으로 `등록/해제` 할 수 있다.', () => {});
+});
+
+describe('음식점 상세 정보 창', () => {
+  it('음식점 목록을 클릭해 음식점 상세 정보창을 열 수 있다.', () => {});
+
+  it('음식점 상세 정보창에서 `별표 버튼`을 `클릭`해 자주 가는 음식점으로 `등록/해제` 할 수 있다.', () => {});
+
+  it('음식점 상세 정보창에서 `닫기 버튼`을 `클릭`해 음식점 상세 정보 창을 닫을 수 있다.', () => {});
+
+  it('음식점 상세 정보창에서 `삭제 버튼`을 `클릭`해 음식점을 목록에서 삭제할 수 있다.', () => {});
 });
