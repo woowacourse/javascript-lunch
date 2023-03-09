2a7a81e16d6c9aa7606d2c72f48ba53a7136d22e
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-09 20:38:45 +0900

    refactor: 메서드 체이닝을 위한 eventBus 객체 반환값 수정

--- src/core/eventBus.ts
+++ src/core/eventBus.ts
@@ -4,24 +4,28 @@ interface EventList {
 
 interface EventBus {
   eventList: EventList;
-  dispatch<T>(eventName: string, originData?: T, handler?: (val: T) => any): void;
-  subscribe(eventName: string, handler: (val: any) => void): void;
+  dispatch<T>(eventName: string, originData?: T, handler?: (val: T) => any): EventBus;
+  subscribe(eventName: string, handler: (val: any) => void): EventBus;
 }
 
 export const eventBus: EventBus = {
   eventList: {},
 
-  dispatch<T>(eventName: string, originData: T, handler: (val: T) => any = (val) => val): void {
+  dispatch<T>(eventName: string, originData: T, handler: (val: T) => any = (val) => val): EventBus {
     this.eventList[eventName] = new CustomEvent(eventName, {
       detail: handler(originData),
     });
 
     document.dispatchEvent(this.eventList[eventName]);
+
+    return eventBus;
   },
 
-  subscribe(eventName: string, handler: (event: any) => void): void {
+  subscribe(eventName: string, handler: (event: any) => void): EventBus {
     document.addEventListener(eventName, ({ detail }: any): void => {
       handler(detail);
     });
+
+    return eventBus;
   },
 };
