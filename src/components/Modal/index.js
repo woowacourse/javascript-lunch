398311a3b589c7a172bb774ec663d1921caffae0
gyeongza <gyeongza@naver.com>
2023-03-09 23:06:38 +0900

    [step2]fix: 모달 배경 클릭시 닫히지 않는 오류 수정

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,10 @@
+import { $ } from '../../utils';
 import './index.css';
 
 class Modal extends HTMLElement {
   connectedCallback() {
     this.render();
+    this.modalHandler();
   }
 
   render() {
@@ -11,6 +13,15 @@ class Modal extends HTMLElement {
       <div id="modalBackdrop" class="modal-backdrop"></div>
     </div>
     `;
+  }
+
+  modalHandler() {
+    $('#modalBackdrop').addEventListener('click', this.closeModal);
+  }
+
+  closeModal() {
+    $('#modalContainer').classList.remove('modal--open');
+    $('add-restaurant').remove();
   }
 }
 
