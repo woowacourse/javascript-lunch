28b96e9954bde8ac80a5e4f4de69fac2ad35dbc8
shackstack <kms1061@naver.com>
2023-03-09 15:38:24 +0900

    test: 음식점 삭제기능 구현 테스트

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,11 +6,19 @@ describe("template spec", () => {
     cy.get("#name").type("홍콩반점");
     cy.get("#distance").select("10");
     cy.get("#add-button").click();
+  });
 
+  it("음식점 삭제기능 구햔 테스트", () => {
+    cy.visit("http://localhost:8080/");
+
+    cy.visit("http://localhost:8080/");
     cy.get(".gnb__button").click();
     cy.get("#category").select("중식");
-    cy.get("#name").type("보배반점");
+    cy.get("#name").type("홍콩반점");
     cy.get("#distance").select("10");
     cy.get("#add-button").click();
+
+    cy.get("#홍콩반점").click();
+    cy.get("#remove-button").click();
   });
 });
