b02e7431bf5f10d36e007ecbe2e9d8868705c58b
n0eyes <doll4586633@naver.com>
2023-03-09 19:34:49 +0900

    refactor: Restaurant data-id(key) 추가

--- src/view/components/Restaurant.ts
+++ src/view/components/Restaurant.ts
@@ -8,16 +8,16 @@ interface RestaurantProps {
 
 function Restaurant(props: RestaurantProps) {
   const {
-    info: { category, name, distance, description },
+    info: { id, category, name, distance, description },
   } = props;
 
   return `
-        <li class="restaurant">
+        <li class="restaurant" data-id=${id}>
             <div class="restaurant__category">
                 <img src=${getImageSrc(category)} alt=${category} class="category-icon">
             </div>
             <div class="restaurant__info">
-                <img src='./favorite-icon-filled.png' alt="favorite-icon" class="favorite-icon">
+                <img src='./favorite-icon-lined.png' alt="favorite-icon" class="favorite-icon">
                 <h3 class="restaurant__name text-subtitle">${name}</h3>
                 <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
                 <p class="restaurant__description text-body">${description}</p>
