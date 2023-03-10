33ee15dfd5d3cfca7c9b4c02ec0bc1fe86bba095
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-10 13:27:08 +0900

    feat: 선택된 음식점이 없을시의 숫자 상수 선언

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,15 @@
 const ALL_CATEGORY = "전체";
 const SORT_FAILED_NUMBER = 0;
 const NO_ELEMENT = "none";
+const NOT_SELECTED_INDEX = -1;
 
 const URL_REGEX =
   /(http[s]?|ftp):\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}/g;
 
-export { ALL_CATEGORY, SORT_FAILED_NUMBER, NO_ELEMENT, URL_REGEX };
+export {
+  ALL_CATEGORY,
+  SORT_FAILED_NUMBER,
+  NO_ELEMENT,
+  URL_REGEX,
+  NOT_SELECTED_INDEX,
+};
