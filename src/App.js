7c79dd717fba26d2460a47968bd2a8537fe30f40
bassyu <yrkdgus@gmail.com>
2023-03-10 14:44:02 +0900

    feat(App): 이벤트 정의 함수 구현
    - root에 모든 이벤트 연결
    - 특정 이벤트가 발생했을 때 이벤트 타겟이 원하는 selector가 아니면 return

--- src/App.js
+++ src/App.js
@@ -32,6 +32,81 @@ class App {
       </main>
     `;
   }
+
+  addEvents() {
+    [
+      {
+        selectors: 'header button',
+        event: 'click',
+        actions: [this.#toggleModal],
+      },
+      {
+        selectors: '.modal #cancel',
+        event: 'click',
+        actions: [this.#toggleModal],
+      },
+      {
+        selectors: 'form',
+        event: 'submit',
+        actions: [this.#submitForm, this.#toggleModal, this.#initRestaurants, this.#renderList],
+      },
+      {
+        selectors: '#category-filter',
+        event: 'change',
+        actions: [this.#renderList],
+      },
+      {
+        selectors: '#sorting-filter',
+        event: 'change',
+        actions: [this.#renderList],
+      },
+    ].forEach(({ selectors, event, actions }) => {
+      this.addEvent({ selectors, event, actions });
+    });
+  }
+
+  addEvent({ selectors, event, actions }) {
+    this.#root.addEventListener(event, (e) => {
+      if (!$(selectors).contains(e.target)) return;
+      actions.forEach((action) => {
+        action(e);
+      });
+    });
+  }
+
+  #initRestaurants = () => {
+    this.#model = new Restaurants(this.#storage.getList());
+  };
+
+  #toggleModal = () => {
+    $('.modal').classList.toggle('modal--open');
+  };
+
+  #submitForm = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const restaurant = {
+      category: form.get('category'),
+      name: form.get('name'),
+      takeMinute: form.get('takeMinute'),
+      description: form.get('description'),
+      link: form.get('link'),
+    };
+
+    this.#storage.setList([...this.#storage.getList(), restaurant]);
+  };
+
+  #renderList = () => {
+    const $categoryFilter = $('#category-filter');
+    const $sortingFilter = $('#sorting-filter');
+
+    const category = $categoryFilter.options[$categoryFilter.selectedIndex].value;
+    const sorting = $sortingFilter.options[$sortingFilter.selectedIndex].value;
+
+    $('.restaurant-list').outerHTML = restaurantList({
+      restaurants: this.#model.getFiltered(category, sorting),
+    });
+  };
 }
 
 export default App;
