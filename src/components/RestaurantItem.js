fcd9fdebb710dec03f7abe53bde927c5b149be05
unknown <tkdrb15@gmail.com>
2023-03-10 12:50:38 +0900

    refactor: 상수 변수명을 수정하다

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,29 +1,29 @@
 import { CATEGORY_TO_FILENAME, FOVORITES_TO_FILENAME } from '../constants/constants';
 
 class RestaurantItem {
-  constructor(restaurant, restaurants) {
-    this.restaurant = restaurant;
+  constructor(restaurantInfo, restaurants) {
+    this.restaurantInfo = restaurantInfo;
     this.restaurants = restaurants;
     this.render();
     this.setClickFavoritesIconEvent();
   }
 
   template() {
-    const imageFile = CATEGORY_TO_FILENAME[this.restaurant.category];
-    const iconName = FOVORITES_TO_FILENAME[this.restaurant.favorites];
+    const imageFile = CATEGORY_TO_FILENAME[this.restaurantInfo.category];
+    const iconName = FOVORITES_TO_FILENAME[this.restaurantInfo.favorites];
 
     return `
-      <li id="restaurant${this.restaurant.ID}" class="restaurant">
+      <li id="restaurant${this.restaurantInfo.ID}" class="restaurant">
 
         <div class="restaurant__category__info">
           <div class="restaurant__category">
-              <img src="./${imageFile}.png" alt="${this.restaurant.category}" class="category-icon">
+              <img src="./${imageFile}.png" alt="${this.restaurantInfo.category}" class="category-icon">
           </div>
 
           <div class="restaurant__info">
-              <h3 class="restaurant__name text-subtitle">${this.restaurant.name}</h3>
-              <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurant.distance}분 내</span>
-              <p class="restaurant__description text-body">${this.restaurant.description}</p>
+              <h3 class="restaurant__name text-subtitle">${this.restaurantInfo.name}</h3>
+              <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurantInfo.distance}분 내</span>
+              <p class="restaurant__description text-body">${this.restaurantInfo.description}</p>
           </div>
         </div>
 
@@ -38,14 +38,14 @@ class RestaurantItem {
   }
 
   updateIcon() {
-    const iconName = FOVORITES_TO_FILENAME[this.restaurant.favorites];
+    const iconName = FOVORITES_TO_FILENAME[this.restaurantInfo.favorites];
 
-    const $favoriteIcon = document.querySelector(`#restaurant${this.restaurant.ID} .favorite-icon`);
+    const $favoriteIcon = document.querySelector(`#restaurant${this.restaurantInfo.ID} .favorite-icon`);
     $favoriteIcon.setAttribute('src', `./${iconName}.png`);
   }
 
   setClickItemEvent(onClickRestaurantItem) {
-    const $info = document.querySelector(`#restaurant${this.restaurant.ID} .restaurant__category__info`);
+    const $info = document.querySelector(`#restaurant${this.restaurantInfo.ID} .restaurant__category__info`);
     $info.addEventListener('click', e => {
       e.preventDefault();
       onClickRestaurantItem(this.restaurant);
@@ -53,12 +53,12 @@ class RestaurantItem {
   }
 
   setClickFavoritesIconEvent() {
-    const $favoriteIcon = document.querySelector(`#restaurant${this.restaurant.ID} .favorite-icon`);
+    const $favoriteIcon = document.querySelector(`#restaurant${this.restaurantInfo.ID} .favorite-icon`);
 
     $favoriteIcon.addEventListener('click', e => {
       e.preventDefault();
 
-      this.restaurants.swapFavoritesByID(this.restaurant.ID);
+      this.restaurants.swapFavoritesByID(this.restaurantInfo.ID);
       this.updateIcon();
       localStorage.setItem('restaurants', JSON.stringify(this.restaurants.restaurants));
     });
