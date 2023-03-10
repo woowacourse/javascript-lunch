2c49de54876fd2c0e4dc3d2ac38c0111cca96b5c
wzrabbit <curious.wzrabbit@gmail.com>
2023-03-10 19:36:48 +0900

    refactor: 테스트 케이스 파일 이동

--- testcase/unit-testcase.ts
+++ testcase/unit-testcase.ts
@@ -1,14 +1,12 @@
-import { Restaurant } from '../type';
+import { UserRestaurantInput } from '../src/type';
 
-const dummyRestaurants: Restaurant[] = [
+const correctUserInputs: UserRestaurantInput[] = [
   {
     category: '한식',
     name: '한옥식당',
     distanceInMinutes: '20',
     description: '전통적인 한식과 한옥의 아름다움을 동시에 느낄 수 있는 곳',
     link: 'https://hanok.co.kr',
-    isFavorite: false,
-    itemId: 0,
   },
   {
     category: '일식',
@@ -17,8 +15,6 @@ const dummyRestaurants: Restaurant[] = [
     description:
       '신선한 재료와 정교한 손질법으로 만든 최상의 회와 일본식 요리를 즐길 수 있는 일식 전문점',
     link: 'https://sambesushi.com/main.html',
-    isFavorite: true,
-    itemId: 1,
   },
   {
     category: '중식',
@@ -27,17 +23,13 @@ const dummyRestaurants: Restaurant[] = [
     description:
       '짜장면, 짬뽕, 탕수육 등 대표적인 중화요리와 함께 새로운 중국 요리도 만나볼 수 있는 중식 전문점',
     link: 'https://baguni.io',
-    isFavorite: true,
-    itemId: 2,
   },
   {
     category: '양식',
     name: '더 그릴',
     distanceInMinutes: '30',
-    description: '프라임 스테이크와 와인을 즐길 수 있는 퓨전 양식 레스토랑',
-    link: 'https://grillsite.net',
-    isFavorite: true,
-    itemId: 3,
+    description: '',
+    link: '',
   },
   {
     category: '중식',
@@ -46,9 +38,7 @@ const dummyRestaurants: Restaurant[] = [
     description:
       '육즙이 풍부한 탕수육과 함께 고추가루를 올린 루왕탕수육을 맛볼 수 있는 중식당',
     link: 'https://www.luwangtangsuik.com/',
-    isFavorite: false,
-    itemId: 4,
   },
 ];
 
-export default dummyRestaurants;
+export { correctUserInputs };
