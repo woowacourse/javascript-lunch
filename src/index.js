be7722ef6d9f8c0bd5105ed7db1495f29f84d228
yeopto <gunyeop530@gmail.com>
2023-03-10 20:11:47 +0900

    feat: 이벤트 핸들링 로직을 추가하다

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './css/index.css';
 import '../image/image';
 import Header from './components/Header';
-import Modal from './components/Modal';
+import AddModal from './components/modal/AddModal';
 import RestaurantFilter from './components/RestaurantFilter';
 import RestaurantsList from './components/RestaurantsList';
 import Restaurants from './domain/Restaurants';
+import Tabbar from './components/Tabbar';
 import { $ } from './utils/common';
+import InfoModal from './components/modal/InfoModal';
+import RestaurantItem from './components/RestaurantItem';
 
 if (localStorage.getItem('restaurants') === null) {
   localStorage.setItem('restaurants', JSON.stringify([]));
@@ -21,11 +24,28 @@ const $header = $('.gnb');
 const $main = $('main');
 
 const header = new Header($header);
-const modal = new Modal($main);
+const addModal = new AddModal($main);
+const infoModal = new InfoModal();
+const tabbar = new Tabbar($main);
 const restaurantFilter = new RestaurantFilter($main);
 const restaurantsList = new RestaurantsList($main, restaurants);
+const restaurantItem = new RestaurantItem();
 
-header.setEvent(modal.toggleModalOpen.bind(modal));
-modal.setSubmitEvent(restaurantsList.setState.bind(restaurantsList), restaurants.add.bind(restaurants));
-modal.setModalCloseEvent();
+header.setEvent(
+  addModal.render.bind(addModal),
+  restaurantsList.setState.bind(restaurantsList),
+  restaurants.add.bind(restaurants)
+);
+
+tabbar.setEvent(
+  restaurantsList.render.bind(restaurantsList),
+  restaurantsList.renderFavoriteRestaurant.bind(restaurantsList),
+  restaurantFilter.openFilter.bind(restaurantFilter),
+  restaurantFilter.closeFilter.bind(restaurantFilter)
+);
+
 restaurantFilter.setEvent(restaurantsList.render.bind(restaurantsList));
+
+restaurantsList.setEvent(restaurants.setFavoriteState.bind(restaurants));
+
+restaurantItem.setEvent(restaurants.restaurantsList, infoModal.render.bind(infoModal));
