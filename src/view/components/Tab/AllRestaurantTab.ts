eea3f118387be4ed2a6f2280df9c94cba44dd619
turtle601 <turtle601@naver.com>
2023-03-10 09:24:00 +0900

    feat(AllRestaurantTab): 모든 음식점 Tab 기능을 수행하는 컴포넌트 구현

---
+++ src/view/components/Tab/AllRestaurantTab.ts
@@ -0,0 +1,22 @@
+import actions from '../../../hooks/actions';
+import { $ } from '../../../utils/querySelector';
+import RestaurantList from '../RestaurantList';
+import Tab from './abstract/Tab';
+
+class AllRestaurantTab extends Tab {
+  constructor($target: Element) {
+    super($target);
+  }
+
+  addEvent(eventTarget: Element) {
+    if (eventTarget.closest('.tab--all-restaurant')) {
+      actions.filterRestaurantsCategory('전체');
+
+      new RestaurantList($('.restaurant-list-wrapper')).render();
+    }
+
+    return this;
+  }
+}
+
+export default AllRestaurantTab;
