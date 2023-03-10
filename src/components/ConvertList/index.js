26b2feaa1b69693c65bd26c54a1142df410b2e78
gyeongza <gyeongza@naver.com>
2023-03-10 21:44:54 +0900

    [step2]feat: 모든 음식점과 자주 가는 음식점 전환 컴포넌트 이벤트 추가

--- src/components/ConvertList/index.js
+++ src/components/ConvertList/index.js
@@ -1,13 +1,42 @@
+import { restaurant } from '../../domain/restaurant';
+import { $ } from '../../utils';
+import RestaurantBox from '../RestaurantBox';
 import './index.css';
 
 class ConvertList extends HTMLElement {
   connectedCallback() {
+    this.render();
+  }
+
+  render() {
     this.innerHTML = `
     <div class="convert-list-container">
-        <div class="convert-list clicked">모든 음식점</div>
-        <div class="convert-list">자주 가는 음식점</div>
+        <div id="allRestaurant" class="convert-list clicked">모든 음식점</div>
+        <div id="favoriteRestaurant" class="convert-list">자주 가는 음식점</div>
     </div>
     `;
+    this.modalHandler();
+  }
+
+  modalHandler() {
+    $('#allRestaurant').addEventListener('click', this.showAllRestaurants);
+    $('#favoriteRestaurant').addEventListener(
+      'click',
+      this.showFavoriteRestaurants
+    );
+  }
+
+  showAllRestaurants() {
+    $('#allRestaurant').classList.add('clicked');
+    $('#favoriteRestaurant').classList.remove('clicked');
+    const allRestaurants = new RestaurantBox();
+    allRestaurants.renderRestaurantList(restaurant.restaurants);
+  }
+  showFavoriteRestaurants() {
+    $('#allRestaurant').classList.remove('clicked');
+    $('#favoriteRestaurant').classList.add('clicked');
+    const allRestaurants = new RestaurantBox();
+    allRestaurants.renderRestaurantList([]);
   }
 }
 
