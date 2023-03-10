1f31b541bdc19915f52f38cedae15fe8d200a57e
NaveOWO <pianowell96@gmail.com>
2023-03-10 14:55:08 +0900

    feat : 카테고리 선택하는 영역 컴포넌트화

---
+++ src/components/Category.ts
@@ -0,0 +1,59 @@
+import { CategoryType } from "../type/category";
+import { $ } from "../util/querySelector";
+
+class Category {
+  #selected: CategoryType = "all";
+
+  get selected() {
+    return this.#selected;
+  }
+
+  create() {
+    return `
+        <section class="nav-container">
+      <label class="category-label all">
+        <input class="category-nav " value="모든 음식점" type="radio" name="category" style="display: none"></input>
+        모든 음식점
+      </label>
+      <label class="category-label favorate">
+        <input class="category-nav " value="자주 가는 음식점" type="radio" name="category" style="display: none"></input>
+        자주 가는 음식점
+      </label>
+  </section>
+        `;
+  }
+
+  render() {
+    const mainSection = $("main");
+    const categoryContainer = document.createElement("div");
+
+    categoryContainer.innerHTML = this.create();
+    mainSection?.prepend(categoryContainer);
+    this.checkSelectedOption();
+  }
+
+  checkSelectedOption() {
+    const optionContainer = $(".nav-container");
+    optionContainer?.addEventListener("click", (e) => {
+      this.changeSelectedStyle(e);
+    });
+  }
+
+  changeSelectedStyle(e: Event) {
+    const allOption = $(".all");
+    const favorateOption = $(".favorate");
+    if (e.target === allOption) {
+      this.#selected = "all";
+      allOption?.classList.add("selected");
+      favorateOption?.classList.remove("selected");
+      return;
+    }
+    if (e.target === favorateOption) {
+      this.#selected = "favorite";
+      favorateOption?.classList.add("selected");
+      allOption?.classList.remove("selected");
+    }
+  }
+}
+
+export default Category;
