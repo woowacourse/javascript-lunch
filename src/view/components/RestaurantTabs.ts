68963b370a906389bc2768af220fae226d2ef6e1
turtle601 <turtle601@naver.com>
2023-03-10 09:25:22 +0900

    feat(RestaurantTabs): 여러 탭들을 모아둔 RestaurantTabs 컴포넌트 구현

---
+++ src/view/components/RestaurantTabs.ts
@@ -0,0 +1,35 @@
+import { $ } from '../../utils/querySelector';
+import AllRestaurantTab from './Tab/AllRestaurantTab';
+import FavoriteTab from './Tab/FavoritTab';
+
+class RestaurantTabs {
+  #root;
+
+  constructor($root: HTMLElement) {
+    this.#root = $root;
+  }
+
+  #template() {
+    return `
+      <ul class="tabs"></ul>
+    `;
+  }
+
+  #mounted() {
+    new AllRestaurantTab($('.tabs'))
+      .render('tab--all-restaurant', '모든 음식점')
+      .setEvent();
+
+    new FavoriteTab($('.tabs'))
+      .render('tab--favorite-restaurant', '자주 가는 음식점')
+      .setEvent();
+  }
+
+  render() {
+    this.#root.insertAdjacentHTML('beforeend', this.#template());
+
+    this.#mounted();
+  }
+}
+
+export default RestaurantTabs;
