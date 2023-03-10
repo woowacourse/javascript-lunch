86d818753fbf44fdd918eb1a2d1a16ba9de876ca
TaeeunKim <xodms0309@naver.com>
2023-03-10 23:22:57 +0900

    refactor: 통일성을 위해 화살표함수->this binding으로 리팩토링

--- src/index.ts
+++ src/index.ts
@@ -43,14 +43,14 @@ class App {
   }
 
   initHandler() {
-    this.header.addModalHandler(this.openModalButtonHandler);
-    this.navBar.addRouteHandler(this.routeHandler);
-    this.selectBox.addSelectBoxHandler(this.selectBoxHandler);
-    this.modal.addRestaurantHandler(this.restaurantHandler);
-    this.restaurantItems.addBottomSheetHandler(this.openDetailBottomSheetHandler);
-    this.restaurantItems.addFavoriteButtonHandler(this.favoriteButtonHandler);
-    this.detailBottomSheet.addDeleteHandler(this.deleteRestaurantHandler);
-    this.detailBottomSheet.addFavoriteButtonHandler(this.favoriteButtonHandler);
+    this.header.addModalHandler(this.openModalButtonHandler.bind(this));
+    this.navBar.addRouteHandler(this.routeHandler.bind(this));
+    this.selectBox.addSelectBoxHandler(this.selectBoxHandler.bind(this));
+    this.modal.addRestaurantHandler(this.restaurantHandler.bind(this));
+    this.restaurantItems.addBottomSheetHandler(this.openDetailBottomSheetHandler.bind(this));
+    this.restaurantItems.addFavoriteButtonHandler(this.favoriteButtonHandler.bind(this));
+    this.detailBottomSheet.addDeleteHandler(this.deleteRestaurantHandler.bind(this));
+    this.detailBottomSheet.addFavoriteButtonHandler(this.favoriteButtonHandler.bind(this));
     this.store.initRestaurants();
   }
 
@@ -65,35 +65,35 @@ class App {
     this.selectBox.hide();
   };
 
-  openModalButtonHandler = () => {
+  openModalButtonHandler() {
     this.modal.openModal();
-  };
+  }
 
-  selectBoxHandler = (categoryFilter: CategoryFilter, sortFilter: SortFilter) => {
+  selectBoxHandler(categoryFilter: CategoryFilter, sortFilter: SortFilter) {
     this.store.filterRestaurants(categoryFilter);
     this.store.sortRestaurants(sortFilter);
     this.restaurantItems.render(this.store.renderedRestaurants);
-  };
+  }
 
-  restaurantHandler = (restaurant: Restaurant) => {
+  restaurantHandler(restaurant: Restaurant) {
     this.store.addRestaurants(restaurant);
     this.restaurantItems.render(this.store.renderedRestaurants);
-  };
+  }
 
-  openDetailBottomSheetHandler = (restaurant: Restaurant) => {
+  openDetailBottomSheetHandler(restaurant: Restaurant) {
     this.detailBottomSheet.render(restaurant);
     this.detailBottomSheet.toggle();
-  };
+  }
 
-  deleteRestaurantHandler = (id: string) => {
+  deleteRestaurantHandler(id: string) {
     this.store.deleteRestaurant(id);
     this.restaurantItems.render(this.store.renderedRestaurants);
-  };
+  }
 
-  favoriteButtonHandler = (id: string) => {
+  favoriteButtonHandler(id: string) {
     this.store.toggleFavoriteRestaurant(id);
     this.restaurantItems.render(this.store.renderedRestaurants);
-  };
+  }
 }
 
 export default App;
