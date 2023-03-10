9b6d059d28009267148325f35a86d0b0cf14f4f5
solo5star <solo_5@naver.com>
2023-03-10 15:45:38 +0900

    feat: RestaurantList 클릭 시 RestaurantDetailModal이 뜨도록 구현

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import NewRestaurantModal from './components/restaurant/modal/NewRestaurantModal';
+import RestaurantDetailModal from './components/restaurant/modal/RestaurantDetailModal';
+import RestaurantList, { RestaurantClickEvent } from './components/restaurant/RestaurantList';
+import restaurants from './states/restaurants';
 
 class App {
   $modalOpenButton = document.querySelector<HTMLButtonElement>('#modal-open-button')!;
+
+  $restaurantList = document.querySelector<RestaurantList>('r-restaurant-list')!;
+
+  $restaurantDetailModal = document.querySelector<RestaurantDetailModal>(
+    'r-restaurant-detail-modal',
+  )!;
 
   init() {
     this.initEventHandlers();
@@ -11,6 +20,15 @@ class App {
   initEventHandlers() {
     this.$modalOpenButton.addEventListener('click', () => {
       document.querySelector<NewRestaurantModal>('r-new-restaurant-modal')?.open();
+    });
+    this.$restaurantList?.addEventListener('click', (e: Event) => {
+      const event = e as RestaurantClickEvent;
+
+      const restaurantId = event.detail;
+      const restaurant = restaurants.getRestaurant(restaurantId);
+      if (!restaurant) return;
+
+      this.$restaurantDetailModal.open(restaurant);
     });
   }
 }
