fff3ddb85b58e8e00de55149d7d1a32d2e6051e0
Q Kim <qoli0703@gmail.com>
2023-03-10 18:13:34 +0900

    test(E2E): 자주 가는 음식점 테스트

---
+++ cypress/e2e/favorite.spec.cy.ts
@@ -0,0 +1,35 @@
+const validateFavoriteList = (favoriteNames) => {
+  cy.get('nav label')
+  .contains('자주 가는 음식점')
+  .click();
+
+  cy.get('.restaurant-list-container')
+    .children()
+    .should('have.length', favoriteNames.length)
+    .each((element, index) => {
+      (cy.wrap(element))
+        .should('contain', favoriteNames[index]);
+    });
+};
+
+describe('자주 가는 음식점 테스트', () => {
+  it('자주 가는 음식점 클릭 시 표시', () => {
+    cy.visit('http://localhost:8080');
+
+    validateFavoriteList(['늘봄막국수', '바나프레소']);
+  });
+
+  it('별표 클릭 시 자주 가는 음식점에서 제거', () => {
+    cy.visit('http://localhost:8080');
+
+    cy.get('.restaurant')
+      .contains('바나프레소')
+      .parent()
+      .parent()
+      .children()
+      .last()
+      .click();
+
+    validateFavoriteList(['늘봄막국수']);
+  });
+});
