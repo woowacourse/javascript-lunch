20c2edd9eac4a228b22bf80dea79c2113f8a9d83
solo5star <solo_5@naver.com>
2023-03-10 15:43:59 +0900

    feat: RestaurantListItem 클릭 이벤트 delegate

--- src/components/restaurant/RestaurantList/index.ts
+++ src/components/restaurant/RestaurantList/index.ts
@@ -1,7 +1,10 @@
 import Restaurant from '../../../domain/Restaurant';
 import Component from '../../Component';
 import { define } from '../../decorators';
+import RestaurantListItem from '../RestaurantListItem';
 import style from './index.css';
+
+export type RestaurantClickEvent = CustomEvent<Restaurant['id']>;
 
 @define('r-restaurant-list')
 class RestaurantList extends Component {
@@ -20,17 +23,40 @@ class RestaurantList extends Component {
     return this.#restaurants;
   }
 
+  onClickItem(event: MouseEvent) {
+    if (!(event.target instanceof RestaurantListItem)) return;
+
+    const $restaurantListItem = event.target;
+    const restaurantId = $restaurantListItem.getAttribute('restaurant-id');
+    if (!restaurantId) return;
+
+    event.stopPropagation();
+    this.dispatchEvent(
+      new CustomEvent('click', {
+        bubbles: true,
+        detail: restaurantId,
+      }),
+    );
+  }
+
   override renderTemplate() {
     return `
       <ul>
         ${this.#restaurants
           .map((restaurant) => {
-            return `<r-restaurant-list-item
-              name="${restaurant.getName()}"
-              distance="${restaurant.getDistance()}"
-              description="${restaurant.getDescription() ?? ''}"
-              category="${restaurant.getCategory() ?? ''}"
-            ></r-restaurant-list-item>`;
+            return `
+              <r-restaurant-list-item
+                restaurant-id="${restaurant.getId()}"
+                category="${restaurant.getCategory() ?? ''}"
+                name="${restaurant.getName()}"
+                distance="${restaurant.getDistance()}"
+                description="${restaurant.getDescription() ?? ''}"
+                reference-url="${restaurant.getReferenceUrl() ?? ''}"
+                ${restaurant.isFavorite() ? 'favorite' : ''}
+
+                onclick="this.host.onClickItem(event)"
+              ></r-restaurant-list-item>
+            `;
           })
           .join('')}
       </ul>
