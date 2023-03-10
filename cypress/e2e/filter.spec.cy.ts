1a5518a59e28fb687f8217c5bfdf46634ba9da39
Q Kim <qoli0703@gmail.com>
2023-03-10 18:13:50 +0900

    test(E2E): 카테고리, 정렬 테스트

---
+++ cypress/e2e/filter.spec.cy.ts
@@ -0,0 +1,32 @@
+const validateOrder = (answer) => {
+  cy.get('.restaurant-list-container')
+    .children()
+    .should('have.length', answer.length)
+    .each((element, index) => {
+      (cy.wrap(element))
+        .should('contain', answer[index]);
+    });
+};
+
+describe('필터 테스트', () => {
+  it('한식 카테고리, 이름순 정렬 테스트', () => {
+    cy.visit('http://localhost:8080');
+
+    cy.get('#category-filter')
+      .select('한식');
+
+    validateOrder(["김밥지옥", "늘봄막국수", "천국김밥"]);
+  });
+
+  it('한식 카테고리, 거리순 정렬 테스트', () => {
+    cy.visit('http://localhost:8080');
+
+    cy.get('#category-filter')
+      .select('한식');
+    
+    cy.get('#sorting-filter')
+      .select('거리순');
+
+    validateOrder(["김밥지옥", "천국김밥", "늘봄막국수"]);
+  });
+});
