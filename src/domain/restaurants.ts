09337d04e771f4c24ab4c9ad5259295cd5471422
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-10 13:53:03 +0900

    refactor: 초기 상태 값을 변수로 분리함

--- src/domain/restaurants.ts
+++ src/domain/restaurants.ts
@@ -1,37 +1,31 @@
 import { renderRestaurantList } from "../components/RestaurantList/handleRestaurantList";
 import IRestaurant from "../type/IRestaurant";
 
+const initState = {
+  restaurants: [] as IRestaurant[],
+  filter: "all",
+  sort: "name",
+  menuTab: "tab-all",
+};
+
 const restaurants = {
-  state: {
-    restaurants: [] as IRestaurant[],
-    filter: "all",
-    sort: "name",
-    menuTab: "tab-all",
-  },
+  state: initState,
   create() {
-    this.state = new Proxy(
-      {
-        restaurants: [],
-        filter: "all",
-        sort: "name",
-        menuTab: "tab-all",
+    this.state = new Proxy(initState, {
+      set: (obj, prop, value) => {
+        // type-guard (최적화 필요)
+        if (
+          prop === "restaurants" ||
+          prop === "filter" ||
+          prop === "sort" ||
+          prop === "menuTab"
+        ) {
+          obj[prop] = value;
+        }
+        renderRestaurantList();
+        return true;
       },
-      {
-        set: (obj, prop, value) => {
-          // type-guard (최적화 필요)
-          if (
-            prop === "restaurants" ||
-            prop === "filter" ||
-            prop === "sort" ||
-            prop === "menuTab"
-          ) {
-            obj[prop] = value;
-          }
-          renderRestaurantList();
-          return true;
-        },
-      }
-    );
+    });
   },
 };
 
