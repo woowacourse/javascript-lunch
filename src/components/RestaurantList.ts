ade339f41b43fdc1d120fd51b242a6c3e6f9c9e4
NaveOWO <pianowell96@gmail.com>
2023-03-11 23:02:05 +0900

    feat : 즐겨찾기를 누른 컴포넌트 변경

--- src/components/RestaurantList.ts
+++ src/components/RestaurantList.ts
@@ -1,38 +1,53 @@
 import "../../css/style.css";
-import { Category } from "../type/restaurant";
+import Elements from "../Element";
 import { $ } from "../util/querySelector";
+import Modal from "./Modal";
 
 class RestaurantList {
-  templateList: Element | null;
-  name: string;
-  distance: string;
-  description: string;
   img: string;
+  currentIndex: number;
+  element: any;
+  starImage: string;
+  starEmpty: string;
+  isFavorate: boolean;
 
   constructor(
-    name: string,
-    distance: string,
-    description: Category,
-    img: string
+    element: any,
+    img: string,
+    index: number,
+    starImage: string,
+    starEmpty: string
   ) {
-    this.templateList = $("#list-template");
-    this.name = name;
-    this.distance = distance;
-    this.description = description;
+    this.element = element;
     this.img = img;
+    this.currentIndex = index;
+    this.starImage = starImage;
+    this.starEmpty = starEmpty;
+    this.isFavorate = false;
   }
+
+  getRestaurantInfo() {}
 
   create() {
     return `
-        <li class="restaurant">
-        <div class="restaurant__category">
-          <img src=${this.img} alt="" class="category-icon" />
-        </div>
-        <div class="restaurant__info">
-          <h3 class="restaurant__name text-subtitle">${this.name}</h3>
-          <span class="restaurant__distance text-body">${this.distance}</span>
-          <p class="restaurant__description text-body">${this.description}</p>
-        </div>
+      <li class="restaurant" id=${this.currentIndex}>
+        <div class="list-info-wrapper">
+          <div class="restaurant__category">
+            <img src=${this.img} alt="" class="category-icon" />
+            </div>
+            </div>
+            <div class="restaurant__info">
+            <h3 class="restaurant__name text-subtitle">${this.element.name}</h3>
+            <span class="restaurant__distance text-body">${
+              this.element.distance
+            }</span>
+            <p class="restaurant__description text-body">${
+              this.element.description
+            }</p>
+            </div>
+            <img class="star" id=star-${this.currentIndex} src=${
+      this.isFavorate ? this.starImage : this.starEmpty
+    } alt="star"/>
       </li>
         `;
   }
@@ -43,6 +58,30 @@ class RestaurantList {
 
     listWrapper.innerHTML = this.create();
     listSection.appendChild(listWrapper);
+  }
+
+  getListInfo(callback: any) {
+    document
+      .getElementById(String(this.currentIndex))
+      ?.addEventListener("click", (e) => {
+        console.log(e.target);
+        const starIcon = document.getElementById(`star-${this.currentIndex}`);
+        if (e.target === starIcon) {
+          callback(e, this.isFavorate);
+          return;
+        }
+        this.showRestaurantDetail();
+      });
+  }
+
+  getModalContent() {
+    console.log(this.element);
+    return Elements.listDetailContents(this.element, this.img);
+  }
+
+  showRestaurantDetail() {
+    const detailModal = new Modal(this.getModalContent());
+    detailModal.render();
   }
 }
 
