b32648c1f6fc047b8a6058486d1fe4a7f83766f7
solo5star <solo_5@naver.com>
2023-03-12 03:29:29 +0900

    feat: 목록이 비어있을 때 비어있는 텍스트 표시하도록 수정

--- src/components/restaurant/RestaurantList/index.ts
+++ src/components/restaurant/RestaurantList/index.ts
@@ -2,12 +2,17 @@ import Restaurant from '../../../domain/Restaurant';
 import Component from '../../Component';
 import { define } from '../../decorators';
 import RestaurantListItem from '../RestaurantListItem';
+import style from './index.css';
 
 export type RestaurantClickEvent = CustomEvent<Restaurant['id']>;
 
 @define('r-restaurant-list')
 class RestaurantList extends Component {
   #restaurants: Restaurant[] = [];
+
+  override getCSSStyleSheets() {
+    return [...super.getCSSStyleSheets(), style];
+  }
 
   setRestaurants(restaurants: Restaurant[]) {
     this.#restaurants = restaurants;
@@ -35,26 +40,32 @@ class RestaurantList extends Component {
   }
 
   override renderTemplate() {
-    return `
-      <ul>
-        ${this.#restaurants
-          .map((restaurant) => {
-            return `
-              <r-restaurant-list-item
-                restaurant-id="${restaurant.getId()}"
-                category="${restaurant.getCategory() ?? ''}"
-                name="${restaurant.getName()}"
-                distance="${restaurant.getDistance()}"
-                description="${restaurant.getDescription() ?? ''}"
-                reference-url="${restaurant.getReferenceUrl() ?? ''}"
-                ${restaurant.isFavorite() ? 'favorite' : ''}
+    const restaurants = this.#restaurants
+      .map((restaurant) => {
+        return `
+          <r-restaurant-list-item
+            restaurant-id="${restaurant.getId()}"
+            category="${restaurant.getCategory() ?? ''}"
+            name="${restaurant.getName()}"
+            distance="${restaurant.getDistance()}"
+            description="${restaurant.getDescription() ?? ''}"
+            reference-url="${restaurant.getReferenceUrl() ?? ''}"
+            ${restaurant.isFavorite() ? 'favorite' : ''}
 
-                onclick="this.host.onClickItem(event)"
-              ></r-restaurant-list-item>
-            `;
-          })
-          .join('')}
-      </ul>
+            onclick="this.host.onClickItem(event)"
+          ></r-restaurant-list-item>
+        `;
+      })
+      .join('');
+
+    return `
+      <ul>${restaurants}</ul>
+
+      <slot name="placeholder">
+        <div class="placeholder">
+          목록이 비어있습니다
+        </div>
+      </slot>
     `;
   }
 }
