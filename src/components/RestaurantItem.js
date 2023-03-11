625a61a36d6bdb710c482ca5a28caa8845a7e678
dladncks1217 <dlaxodud1217@gmail.com>
2023-03-11 15:47:21 +0900

    feat(RestaurantItem): 아이템 리스트 모달띄우기/즐겨찾기 추가 및 제거 이벤트핸들러 추가

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,14 +1,25 @@
+import { FAVORITE, RESTAURANT_IMAGE } from '../constants/images';
+import { $ } from '../utils/domHelpers';
+import InformationModal from './InformationModal';
+
 export default class RestaurantItem {
   #restaurantImage;
+  #restaurantManager;
+  #informationModal;
 
-  constructor(restaurantImage) {
+  constructor(restaurantImage, restaurantManager) {
+    this.#restaurantManager = restaurantManager;
     this.#restaurantImage = restaurantImage;
+    this.#informationModal = new InformationModal(RESTAURANT_IMAGE, restaurantManager, this.render);
+    this.componentClickedEvent = this.componentClickedEvent.bind(this);
+
+    this.componentClickedEvent();
   }
 
-  render({ category, storeName, distance, detail }) {
+  render({ category, storeName, distance, detail, favorite }, key) {
     return `
-        <li class="restaurant">
-        <div class="restaurant__category">
+     <li class="restaurant" id="${key}">
+        <div class="restaurant__category" >
           <img src="${this.#restaurantImage[category]}" alt="${category}" class="category-icon">
         </div>
         <div class="restaurant__info">
@@ -16,7 +27,35 @@ export default class RestaurantItem {
           <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
           <p class="restaurant__description text-body">${detail}</p>
         </div>
+        ${
+          favorite
+            ? `<img src="${FAVORITE.FILLED}" alt="채워짐" class="category-filled"/>`
+            : `<img src="${FAVORITE.LINED}" alt="비워짐" class="category-lined"/>`
+        }
       </li>
         `;
+  }
+
+  componentClickedEvent() {
+    $('.restaurant-list').addEventListener('click', (e) => {
+      if (e.target.className === 'category-filled' || e.target.className === 'category-lined') {
+        const nowID = e.target.closest('li').id;
+        const storeName = $(`#${nowID} .restaurant__name`).textContent;
+        const renderData = this.#restaurantManager.reverseFavorite(storeName);
+
+        document.querySelector(`#${e.target.parentElement.id}`).outerHTML = this.render(
+          renderData,
+          nowID
+        );
+      } else if (e.target.closest('li') || e.target.tagName === 'LI') {
+        const restaurantName = e.target
+          .closest('LI')
+          .querySelector('.restaurant__name').textContent;
+        const renderData = this.#restaurantManager.findRestaurantData(restaurantName);
+        const modal = $('.information-modal--close');
+        modal.innerHTML = this.#informationModal.render(renderData);
+        modal.className = 'information-modal--open';
+      }
+    });
   }
 }
