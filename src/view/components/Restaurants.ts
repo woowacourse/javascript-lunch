e1719e83a8665f2113f6ad69c5f2cb625197c956
n0eyes <doll4586633@naver.com>
2023-03-11 16:45:39 +0900

    refactor: 음식점 목록 클릭, 좋아요 버튼 클릭 이벤트 구분하여 처리

--- src/view/components/Restaurants.ts
+++ src/view/components/Restaurants.ts
@@ -1,26 +1,44 @@
-import { Restaurant, RestaurantInfo } from '../../domain/model/LunchRecommendation';
-import { getClosest } from '../../utils/common/domHelper';
+import {
+  lunchRecommendation,
+  Restaurant,
+  RestaurantInfo,
+} from '../../domain/model/LunchRecommendation';
+import { $, getClosest } from '../../utils/common/domHelper';
 import { useEvents } from '../../utils/core';
 import { Restaurant as RestaurantItem } from './Restaurant';
 
 interface RestaurantProps {
   restaurants: Restaurant[];
   handleFavoriteBtn(id: RestaurantInfo['id']): void;
+  onClick(restaurant: Restaurant): void;
 }
 
-function Restaurants({ restaurants, handleFavoriteBtn }: RestaurantProps) {
+function Restaurants({ restaurants, handleFavoriteBtn, onClick }: RestaurantProps) {
   const [addEvent] = useEvents('.restaurant-list-container');
 
-  addEvent('click', '.favorite-icon', (e) => {
+  addEvent('click', '.restaurant', (e) => {
     const restaurantId = getClosest(e.target, '.restaurant')?.dataset.id;
 
-    if (restaurantId) handleFavoriteBtn(Number(restaurantId));
-  });
+    if (!restaurantId) return;
 
+    e.target === $('.favorite-icon', $('.restaurant'))
+      ? handleFavoriteBtn(Number(restaurantId))
+      : onClick(lunchRecommendation.getRestaurant(Number(restaurantId))!);
+  });
   return `
     <section class="restaurant-list-container">
       <ul class="restaurant-list">
-        ${restaurants.map(({ info }) => RestaurantItem({ info })).join('')}
+        ${restaurants
+          .map(({ info }) =>
+            RestaurantItem({
+              info,
+              attribute: {
+                class: 'restaurant',
+                'data-id': String(info.id),
+              },
+            })
+          )
+          .join('')}
       </ul>
     </section>
   `;
