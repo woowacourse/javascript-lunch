91a75d8c5e4b3baba373f5203b142bad96518ef7
ashleysyheo <ashleysyheo@gmail.com>
2023-03-11 23:16:07 +0900

    refactor: DOM selecting 시 요소가 존재하지 않으면 에러 발생

--- src/utils/domSelectors.ts
+++ src/utils/domSelectors.ts
@@ -1,5 +1,11 @@
 function $<E extends Element>(selector: string): E {
-  return <E>document.querySelector(selector);
+  const element = document.querySelector(selector);
+
+  if (!element) {
+    throw new Error(`DOM에 ${selector} 요소가 존재하지 않습니다.`);
+  }
+
+  return <E>element;
 }
 
 function $$<E extends Element>(selector: string): NodeListOf<E> {
