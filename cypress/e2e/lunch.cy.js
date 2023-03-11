1b1743e4d3ee43351a135fb8a6e7b56223c556f0
hae-on <solbi2004@naver.com>
2023-03-12 00:42:07 +0900

    test: 식당 카테고리 정렬 테스트

--- cypress/e2e/lunch.cy.js
+++ cypress/e2e/lunch.cy.js
@@ -43,6 +43,26 @@ describe("lunch e2e test", () => {
     cy.get(".bookmark-page").click();
     cy.contains("피양콩할마니");
   });
+
+  it("식당을 카테고리별로 정렬한다", () => {
+    restaurantList(
+      "한식",
+      "피양콩할마니",
+      "10",
+      "맛집입니다.",
+      "https://naver.me/G6DyD9tg"
+    );
+    restaurantList(
+      "일식",
+      "일일일",
+      "15",
+      "이 집의 역사를 느낄 수 있는 특별한 메뉴다.",
+      "https://naver.me/G6DyD9tg"
+    );
+
+    cy.get("#category-filter").select("한식");
+    cy.contains("피양콩할마니");
+  });
 });
 
 function restaurantList(category, name, distance, description, link) {
