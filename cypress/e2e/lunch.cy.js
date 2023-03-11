b21b2b707db32ea5425831c12d0ce8b5086fec1b
hae-on <solbi2004@naver.com>
2023-03-12 00:36:18 +0900

    test: 레스토랑 삭제 테스트

--- cypress/e2e/lunch.cy.js
+++ cypress/e2e/lunch.cy.js
@@ -15,6 +15,20 @@ describe("lunch e2e test", () => {
 
     cy.contains("피양콩할마니");
   });
+
+  it("레스토랑을 삭제하면 목록에서 사라진다.", () => {
+    restaurantList(
+      "한식",
+      "피양콩할마니",
+      "10",
+      "맛집입니다.",
+      "https://naver.me/G6DyD9tg"
+    );
+
+    cy.contains("피양콩할마니").click();
+    cy.get(".item-sheet--delete").click();
+    cy.contains("피양콩할마니").should("not.exist");
+  });
 });
 
 function restaurantList(category, name, distance, description, link) {
