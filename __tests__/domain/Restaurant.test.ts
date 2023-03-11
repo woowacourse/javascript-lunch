96c4994dea8ec6cd3923cb35199ac6d42263d378
solo5star <solo_5@naver.com>
2023-03-12 04:36:22 +0900

    test: Restaurant 테스트 최신화

--- __tests__/domain/Restaurant.test.ts
+++ __tests__/domain/Restaurant.test.ts
@@ -4,6 +4,7 @@ describe('Restaurant', () => {
   context('Restaurant는 카테고리, 이름, 거리, 설명, 참고 링크를 입력받았을 떄', () => {
     it('정상적으로 생성이 되어야 한다.', () => {
       // given
+      const id = '1';
       const category = '한식';
       const name = '한식당';
       const distance = 5;
@@ -12,6 +13,7 @@ describe('Restaurant', () => {
 
       // when
       const restaurant = new Restaurant({
+        id,
         category,
         name,
         distance,
@@ -21,26 +23,6 @@ describe('Restaurant', () => {
 
       // then
       expect(restaurant).toBeInstanceOf(Restaurant);
-    });
-  });
-
-  context('검삭하고자 하는 카테고리가 맞다면', () => {
-    it('true를 반환한다.', () => {
-      // given
-      const serachCategory = '한식';
-      const category = '한식';
-      const name = '한식당';
-      const distance = 5;
-      const restaurant = new Restaurant({
-        category,
-        name,
-        distance,
-      });
-
-      // when
-      const isMatched = restaurant.isMatchCategory(serachCategory);
-
-      expect(isMatched).toBeTruthy();
     });
   });
 });
