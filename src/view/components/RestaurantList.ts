3cc57ddea00dec8e5239d0962b7afb539add6ce9
turtle601 <turtle601@naver.com>
2023-03-11 20:20:56 +0900

    fix(RestaurantList): state.isFavorite 에 따라 Selector 존재 여부 나타내기

--- src/view/components/RestaurantList.ts
+++ src/view/components/RestaurantList.ts
@@ -23,34 +23,36 @@ class RestaurantList {
   }
 
   #mounted() {
-    new Selector({
-      $target: $('.restaurant-filter-container'),
-      info: {
-        name: 'category',
-        id: 'category-filter',
-        options: [
-          { value: '전체', name: '전체' },
-          { value: '한식', name: '한식' },
-          { value: '중식', name: '중식' },
-          { value: '일식', name: '일식' },
-          { value: '양식', name: '양식' },
-          { value: '아시안', name: '아시안' },
-          { value: '기타', name: '기타' },
-        ],
-      },
-    }).render(state.categorySelector);
+    if (!actions.getValue('isFavorite')) {
+      new Selector({
+        $target: $('.restaurant-filter-container'),
+        info: {
+          name: 'category',
+          id: 'category-filter',
+          options: [
+            { value: '전체', name: '전체' },
+            { value: '한식', name: '한식' },
+            { value: '중식', name: '중식' },
+            { value: '일식', name: '일식' },
+            { value: '양식', name: '양식' },
+            { value: '아시안', name: '아시안' },
+            { value: '기타', name: '기타' },
+          ],
+        },
+      }).render(state.categorySelector);
 
-    new Selector({
-      $target: $('.restaurant-filter-container'),
-      info: {
-        name: 'sorting',
-        id: 'sorting-filter',
-        options: [
-          { value: 'name', name: '이름순' },
-          { value: 'distance', name: '거리순' },
-        ],
-      },
-    }).render(state.sortSelector);
+      new Selector({
+        $target: $('.restaurant-filter-container'),
+        info: {
+          name: 'sorting',
+          id: 'sorting-filter',
+          options: [
+            { value: 'name', name: '이름순' },
+            { value: 'distance', name: '거리순' },
+          ],
+        },
+      }).render(state.sortSelector);
+    }
 
     actions.getValue('restaurants')?.forEach((restaurant: Restaurant) => {
       new RestaurantItem($('.restaurant-list')).render(restaurant).setEvent();
