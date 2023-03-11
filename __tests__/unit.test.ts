b3f5c69f489e70f9c8b9628cb298969395f41c6b
chsua <csj1919@naver.com>
2023-03-11 21:17:23 +0900

    test: 단위 테스트 오류 해결

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -1,6 +1,5 @@
 import { mockList } from "../src/data/mockRestaurant";
 import { FilterSort } from "../src/domain/FilterSort";
-import { ListChooseSection } from "../src/components/MainPage/ListChooseSection";
 import { RestaurantData } from "../src/domain/RestaurantData";
 
 describe("필터/정렬이 적용된 레스토랑 리스트 만들기", () => {
@@ -36,24 +35,10 @@ describe("필터/정렬이 적용된 레스토랑 리스트 만들기", () => {
   });
 });
 
-// 오류나는 테스트 코드
-// describe("클릭한 영역element가 이미 선택되어있는 영역인지 확인", () => {
-//   test("class에 list-choose-section이 포함되어 있으면 이미 선택된 영역이므로 false 반환", () => {
-//     const div = document.createElement("div");
-//     div.className = "likeList list-choose-section";
-//     expect(ListChooseSection.targetValidation(div)).toBeFalsy();
-//   });
-//   test("class에 list-choose-section이 미포함되어 있으면 선택된 영역이 아니므로 true 반환", () => {
-//     const div = document.createElement("div");
-//     div.className = "likeList";
-//     expect(ListChooseSection.targetValidation(div)).toBeTruthy();
-//   });
-// });
-
-// describe("선호 레스토랑을 추가/삭제 및 전체목록에서 특정 레스토랑 고르기/삭제하기", () => {
-//   RestaurantData.allList = mockList;
-//   test("선호 레스토랑을 추가 요청 시 레스토랑 데이터 선호 목록에 해당 레스토랑 추가", () => {
-//     RestaurantData.addLikeRestaurant(0);
-//     expect(RestaurantData.likeList).toEqual([mockList[0]]);
-//   });
-// });
+describe("선호 레스토랑을 추가/삭제 및 전체목록에서 특정 레스토랑 고르기/삭제하기", () => {
+  RestaurantData.allList = mockList.slice();
+  test("선호 레스토랑을 추가 요청 시 레스토랑 데이터 선호 목록에 해당 레스토랑 추가", () => {
+    RestaurantData.addLikeRestaurant(0);
+    expect(RestaurantData.likeList[0]).toEqual(RestaurantData.allList[0]);
+  });
+});
