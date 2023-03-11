71387fdc73dcf70ddc7abd09e3a210bc9d43648d
solo5star <solo_5@naver.com>
2023-03-12 01:46:01 +0900

    feat: Modal backdrop 클릭 시 닫히도록 구현

--- src/components/common/Modal/index.ts
+++ src/components/common/Modal/index.ts
@@ -27,21 +27,28 @@ class Modal extends Component {
 
   private onClick(event: MouseEvent) {
     event.stopPropagation();
+    this.close();
   }
 
-  private onClose(event: CloseEvent) {
+  private onClickModal(event: MouseEvent) {
+    event.stopPropagation();
+  }
+
+  private onClose() {
     this.dispatchEvent(new CloseEvent('close'));
   }
 
   override renderTemplate() {
     return `
       <dialog onclose="this.host.onClose(event)">
-        <slot name="header">
-          <h2 class="text-title">${this.getAttribute('title') ?? ''}</h2>
-        </slot>
-        <slot name="content"></slot>
-        <hr>
-        <slot name="actions"></slot>
+        <section onclick="this.host.onClickModal(event)">
+          <slot name="header">
+            <h2 class="text-title">${this.getAttribute('title') ?? ''}</h2>
+          </slot>
+          <slot name="content"></slot>
+          <hr>
+          <slot name="actions"></slot>
+        </section>
       </dialog>
     `;
   }
