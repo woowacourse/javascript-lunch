144acafce36c38f2b2e6840625e812ab90e60c57
n0eyes <doll4586633@naver.com>
2023-03-11 16:43:29 +0900

    refactor: Button 컴포넌트의 이벤트 위임 로직 수정

--- src/view/components/Button.ts
+++ src/view/components/Button.ts
@@ -1,4 +1,4 @@
-import { Attribute, parseAttribute } from '../../utils/common/domHelper';
+import { Attribute, parseAttribute, parseClassToSelector } from '../../utils/common/domHelper';
 import { useEvents } from '../../utils/core';
 
 interface ButtonProps {
@@ -9,9 +9,10 @@ interface ButtonProps {
 
 function Button(props: ButtonProps) {
   const { onClick, attribute, children } = props;
-  const [addEvent] = useEvents(attribute.class);
 
-  onClick && addEvent('click', '.button--secondary', onClick);
+  const [addEvent] = useEvents(parseClassToSelector(attribute.class));
+
+  onClick && addEvent('click', parseClassToSelector(attribute.class), onClick);
 
   return `<button ${attribute ? parseAttribute(attribute) : ''}>${children}</button>`;
 }
