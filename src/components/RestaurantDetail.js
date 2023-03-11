66de37cf894bfb6e8a529ce31d59c5369c861018
shackstack <kms1061@naver.com>
2023-03-11 21:08:40 +0900

    refactor: 템플릿 메서드로 분리

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -11,33 +11,37 @@ export default class RestaurantDetail {
     this.setEvent();
   }
 
-  render() {
+  template() {
     const { category, name, distance, description, link } = this.props;
-    this.$target.innerHTML = `
-  <div>
-    <div class="flex justify-between">
-      <div class="restaurant__category">
-        <img src="./category-${translateCategory[category]}.png" alt="${category}" class="category-icon" />
-      </div>
-      <div class="star-container"></div>
-    </div>
-   </div>
-  <h2 class="mt-16">${name}</h1>
-  <div>
-    <p class="mt-16 text-body restaurant__distance">캠퍼스로부터 ${distance}분 내</p>
-    <div class="mt-16 mb-16">${description}</div>
-    <a href="${link}">${link}</a>
-    <div class="mt-16 button-container">
-    <button id="remove-button" class="button button--secondary text-caption">
-    삭제하기
-</button>
-    <button id="quit-button" type="button" class="button button--primary text-caption">
-    닫기
-    </button>
-    </div>
-  </div>
-  `;
 
+    return `
+<div>
+  <div class="flex justify-between">
+    <div class="restaurant__category">
+      <img src="./category-${translateCategory[category]}.png" alt="${category}" class="category-icon" />
+    </div>
+    <div class="star-container"></div>
+  </div>
+ </div>
+<h2 class="mt-16">${name}</h1>
+<div>
+  <p class="mt-16 text-body restaurant__distance">캠퍼스로부터 ${distance}분 내</p>
+  <div class="mt-16 mb-16">${description}</div>
+  <a href="${link}">${link}</a>
+  <div class="mt-16 button-container">
+  <button id="remove-button" class="button button--secondary text-caption">
+  삭제하기
+</button>
+  <button id="quit-button" type="button" class="button button--primary text-caption">
+  닫기
+  </button>
+  </div>
+</div>
+`;
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
     this.mounted();
   }
 
