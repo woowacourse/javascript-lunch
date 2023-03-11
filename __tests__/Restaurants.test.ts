d12922e3420281278389efe1166dfe0267287814
Dohyeon Lee <dlehgus1110@naver.com>
2023-03-11 23:02:33 +0900

    test(Restaurants): 테스트 케이스 추가
    음식점 삭제, 특정 음식점 반환 메서드에 대한 테스트 케이스 추가

--- __tests__/Restaurants.test.ts
+++ __tests__/Restaurants.test.ts
@@ -23,7 +23,7 @@ describe('Restaurants.addRestaurant', () => {
 });
 
 describe('Restaurants.toggleFavoriteRestaurant', () => {
-  test('메서드가 실행되면, id값에 해당하는 음식점 객체의 isFavorite(Boolean) 값이 반대가 된다.', () => {
+  test('id값이 일치하는 음식점 객체의 isFavorite(Boolean) 값이 반대가 된다.', () => {
     const restaurants = new Restaurants([]);
 
     const restaurant: Restaurant = {
@@ -43,5 +43,46 @@ describe('Restaurants.toggleFavoriteRestaurant', () => {
     const afterFavorite = restaurants.getRestaurants()[0].isFavorite;
 
     expect(expected).toEqual(afterFavorite);
+  });
+});
+
+describe('Restaurants.deleteRestaurant', () => {
+  test('id값이 일치하는 음식점 객체가 배열에서 삭제된다.', () => {
+    const restaurants = new Restaurants([]);
+
+    const restaurant: Restaurant = {
+      id: 1,
+      category: '한식',
+      name: '돈카라',
+      distance: '10',
+      isFavorite: false,
+    };
+
+    restaurants.addRestaurant(restaurant);
+
+    restaurants.deleteRestaurant(1);
+    const restaurantList = restaurants.getRestaurants();
+
+    expect(restaurantList).toEqual([]);
+  });
+});
+
+describe('Restaurants.getRestaurantById', () => {
+  test('id값이 일치하는 음식점 객체를 반환한다.', () => {
+    const restaurants = new Restaurants([]);
+
+    const restaurant: Restaurant = {
+      id: 1,
+      category: '한식',
+      name: '돈카라',
+      distance: '10',
+      isFavorite: false,
+    };
+
+    restaurants.addRestaurant(restaurant);
+
+    const expectedRestaurant = restaurants.getRestaurantById(1);
+
+    expect(expectedRestaurant).toEqual(restaurant);
   });
 });
