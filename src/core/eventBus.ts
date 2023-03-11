ec34c60e33cc912f192e704baf3160c1e3256ab5
ukkodeveloper <ukkodeveloper@gmail.com>
2023-03-11 13:45:49 +0900

    feat: eventBus delete 기능 추가

--- src/core/eventBus.ts
+++ src/core/eventBus.ts
@@ -6,17 +6,18 @@ interface EventBus {
   eventList: EventList;
   dispatch<T>(eventName: string, originData?: T, handler?: (val: T) => any): EventBus;
   subscribe(eventName: string, handler: (val: any) => void): EventBus;
+  deleteEvent(eventName: string): EventBus;
 }
 
 export const eventBus: EventBus = {
   eventList: {},
 
   dispatch<T>(eventName: string, originData: T, handler: (val: T) => any = (val) => val): EventBus {
-    this.eventList[eventName] = new CustomEvent(eventName, {
+    eventBus.eventList[eventName] = new CustomEvent(eventName, {
       detail: handler(originData),
     });
 
-    document.dispatchEvent(this.eventList[eventName]);
+    document.dispatchEvent(eventBus.eventList[eventName]);
 
     return eventBus;
   },
@@ -25,6 +26,15 @@ export const eventBus: EventBus = {
     document.addEventListener(eventName, ({ detail }: any): void => {
       handler(detail);
     });
+
+    return eventBus;
+  },
+
+  deleteEvent(eventName: string): EventBus {
+    const { eventList } = eventBus;
+    if (eventName in eventList) {
+      delete eventBus.eventList[eventName];
+    }
 
     return eventBus;
   },
