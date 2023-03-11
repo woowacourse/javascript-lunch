ee4cf8b046a79beb3902627652ce6b1ee5095536
turtle601 <turtle601@naver.com>
2023-03-11 14:51:56 +0900

    feat(AddRestaurantModalButton): 음식점을 추가하는 버튼 컴포넌트 구현

---
+++ src/view/components/Button/AddRestaurantModalButton.ts
@@ -0,0 +1,36 @@
+import actions from '../../../hooks/actions';
+import { Restaurant } from '../../../type/common';
+import getFormData from '../../../utils/getFormData';
+import { $ } from '../../../utils/querySelector';
+import RestaurantList from '../RestaurantList';
+import Button from './abstract/Button';
+
+class AddRestaurantModalButton extends Button {
+  constructor($target: Element) {
+    super($target);
+  }
+
+  addRestaurantEvent() {
+    const dom = $('form');
+
+    if (!(dom instanceof HTMLFormElement)) return;
+
+    const restaurant = getFormData(dom);
+
+    if (!restaurant) return;
+
+    actions.addRestaurant(restaurant as Restaurant);
+    new RestaurantList($('.restaurant-list-wrapper')).render();
+  }
+
+  addEvent(eventTarget: Element) {
+    if (eventTarget.classList.contains('add')) {
+      this.addRestaurantEvent();
+    }
+
+    this.closeModal();
+    return this;
+  }
+}
+
+export default AddRestaurantModalButton;
