380eb14d4af7d27ee328ba7ddf7649c71e06b278
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-11 10:23:12 +0900

    feat: 탭 색상 전환하는 기능 구현

--- src/components/MenuTab/handleMenuTab.ts
+++ src/components/MenuTab/handleMenuTab.ts
@@ -1,11 +1,25 @@
 import { restaurants } from "../../domain/restaurants";
 
 export const onChangeMenuTabs = () => {
+  changeTabColor();
   const form = document.getElementById("menuTabForm");
   form?.addEventListener("change", (event) => {
     if (event.target instanceof HTMLInputElement) {
+      changeTabColor();
       const newMenu = event.target.value;
       restaurants.state.menuTab = newMenu;
     }
   });
 };
+
+const changeTabColor = () => {
+  const elements = document.querySelectorAll('[name="tab-menu"]') as NodeListOf<HTMLInputElement>;
+  elements.forEach((e: HTMLInputElement) => {
+    if (e.checked) {
+      e.parentElement?.classList.add("tab-menu-open");
+    }
+    else {
+      e.parentElement?.classList.remove("tab-menu-open");
+    }
+  })
+}
