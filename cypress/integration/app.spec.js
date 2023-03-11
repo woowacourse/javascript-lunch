ad06c44efeb5f4a048f0b006445d203d0c53f209
sh981013s <sh981013s@gmail.com>
2023-03-11 15:26:27 +0900

    test: 음식점 등록 테스트코드 작성

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,12 +3,12 @@ describe('App component', () => {
     cy.visit('/');
   });
 
-  it('헤더와 메인 섹션을 렌더링한다.', () => {
+  it('헤더와 메인 섹션을 렌더링 해야한다..', () => {
     cy.get('header.gnb').should('exist');
     cy.get('main').should('exist');
   });
 
-  it('초기 음식점 리스트 렌더링 테스트.', () => {
+  it('초기 음식점 리스트(더미 데이터) 3개를 렌더링 해야한다.', () => {
     cy.get('.restaurant-list-container .tabview__content').should('exist');
 
     cy.get('.tabview__content').should(($items) => {
@@ -16,6 +16,25 @@ describe('App component', () => {
       expect($items.find('#restaurant-2')).to.exist;
       expect($items.find('#restaurant-3')).to.exist;
     });
+  });
+
+  it('음식점 등록 후 리스트에 해당 음식점이 존재해야한다.', () => {
+    cy.get('.nav-add-button').click();
+    cy.get('.modal-container').should('be.visible');
+    cy.get('.restaurant-form').within(() => {
+      cy.get('#name').type('르산테');
+      cy.get('#category').select('양식');
+      cy.get('#distance').select('10');
+      cy.get('#description').type('최고 양식집');
+      cy.get('#link').type('https://www.example.com');
+      cy.get('.submit-restaurant').click();
+    });
+    cy.get('.restaurant-list-container .restaurant')
+      .last()
+      .within(() => {
+        cy.get('.restaurant__name').should('contain', '르산테');
+        cy.get('.restaurant__category img').should('have.attr', 'alt', '양식');
+      });
   });
 
   // it('filters restaurants by category and order', () => {
@@ -49,23 +68,4 @@ describe('App component', () => {
   //   );
   // });
   //
-  // it('adds a new restaurant', () => {
-  //   cy.get('.add-restaurant-button').click();
-  //   cy.get('.bottom-sheet-container').should('be.visible');
-  //   cy.get('.restaurant-form').within(() => {
-  //     cy.get('#name').type('New Restaurant');
-  //     cy.get('#category').select('양식');
-  //     cy.get('#distance').type('10');
-  //     cy.get('#description').type('A new restaurant added by Cypress');
-  //     cy.get('#link').type('https://www.example.com');
-  //     cy.get('.add-restaurant-submit').click();
-  //   });
-  //   cy.get('.bottom-sheet-container').should('not.be.visible');
-  //   cy.get('.restaurant-list-container .restaurant-item')
-  //     .last()
-  //     .within(() => {
-  //       cy.get('.restaurant__name').should('contain', 'New Restaurant');
-  //       cy.get('.restaurant__category img').should('have.attr', 'alt', '양식');
-  //     });
-  // });
 });
