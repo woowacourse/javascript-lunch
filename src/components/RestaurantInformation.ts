2cf79e04bac7b90167df0b61077c6e5a72fab793
ashleysyheo <ashleysyheo@gmail.com>
2023-03-11 18:22:17 +0900

    refactor: 음식점 상세 정보 컴포넌트 수정

--- src/components/RestaurantInformation.ts
+++ src/components/RestaurantInformation.ts
@@ -3,78 +3,97 @@ import { RESTAURANT_IMAGE, getFavoriteIcon } from '../constants/images';
 import { $ } from '../utils/domSelectors';
 import { changeRestaurantFavoriteIcon } from './utils';
 
-function addCloseButtonClickEvent(closeModal: CallableFunction) {
-  const closeButton = $<HTMLButtonElement>('#restaurant-information-close-button');
+class RestaurantInformation {
+  addCloseButtonClickEvent(handleCloseModal: CallableFunction) {
+    const closeButton = $<HTMLButtonElement>('#restaurant-information-close-button');
 
-  closeButton.addEventListener('click', () => {
-    closeModal();
-  });
-}
+    closeButton.addEventListener('click', () => {
+      handleCloseModal();
+    });
+  }
 
-function addDeleteButtonClickEvent(
-  closeModal: CallableFunction,
-  deleteRestaurant: CallableFunction
-) {
-  const deleteButton = $<HTMLButtonElement>('#restaurant-information-delete-button');
+  addDeleteButtonClickEvent(handleCloseModal: CallableFunction, handleDeleteRestaurant: CallableFunction) {
+    const deleteButton = $<HTMLButtonElement>('#restaurant-information-delete-button');
 
-  deleteButton.addEventListener('click', (event: Event) => {
-    const target = event.target as HTMLButtonElement;
-    deleteRestaurant(Number(target.dataset.id));
-    closeModal();
-  });
-}
+    deleteButton.addEventListener('click', () => {
+      const itemElement = $<HTMLDivElement>('#restaurant-information');
+      console.log(itemElement.dataset.id);
+      handleDeleteRestaurant(Number(itemElement.dataset.id));
+      handleCloseModal();
+    });
+  }
 
-function addFavoriteIconClickEvent(onFavoriteIconClick: CallableFunction) {
-  const favoriteIcon = $<HTMLImageElement>('#restaurant-information-star');
+  addFavoriteIconClickEvent(onFavoriteIconClick: CallableFunction) {
+    const favoriteIcon = $<HTMLImageElement>('#restaurant-information-star');
 
-  favoriteIcon.addEventListener('click', (event: Event) => {
-    const target = event.target as HTMLImageElement;
-    const restaurantListItemFavoriteIcon = $<HTMLImageElement>(
-      `.restaurant[data-id="${target.dataset.id}"] img.restaurant-star-icon`
-    );
-    changeRestaurantFavoriteIcon(target);
-    changeRestaurantFavoriteIcon(restaurantListItemFavoriteIcon);
-    onFavoriteIconClick(Number(target.dataset.id));
-  });
-}
+    favoriteIcon.addEventListener('click', (event: Event) => {
+      const target = event.target as HTMLImageElement;
+      const restaurantListItemFavoriteIcon = $<HTMLImageElement>(
+        `.restaurant[data-id="${target.dataset.id}"] img.restaurant-star-icon`
+      );
+      changeRestaurantFavoriteIcon(target);
+      changeRestaurantFavoriteIcon(restaurantListItemFavoriteIcon);
+      onFavoriteIconClick(Number(target.dataset.id));
+    });
+  }
 
-function addRestaurantInformationEvents(
-  closeModal: CallableFunction,
-  deleteRestaurant: CallableFunction,
-  onFavoriteIconClick: CallableFunction
-) {
-  addCloseButtonClickEvent(closeModal);
-  addDeleteButtonClickEvent(closeModal, deleteRestaurant);
-  addFavoriteIconClickEvent(onFavoriteIconClick);
-}
+  addButtonEvents(handleCloseModal: CallableFunction, handleDeleteRestaurant: CallableFunction) {
+    this.addCloseButtonClickEvent(handleCloseModal);
+    this.addDeleteButtonClickEvent(handleCloseModal, handleDeleteRestaurant);
+  }
 
-function createRestaurantInformationContent(restaurant: Restaurant) {
-  return `
-      <div class="restaurant-detail__info-container">
+  addContentEvent(handleFavoriteIconClick: CallableFunction) {
+    this.addFavoriteIconClickEvent(handleFavoriteIconClick);
+  }
+
+  create() {
+    return `
+      <div class="restaurant-detail__info-container"></div>
+      <div class="button-container">
+        <button
+          type="button"
+          id="restaurant-information-delete-button"
+          class="button button--secondary text-caption"
+        >
+          삭제하기
+        </button>
+        <button
+          id="restaurant-information-close-button"
+          class="button button--primary text-caption modal-close-button"
+        >
+          닫기
+        </button>
+      </div>
+      `;
+  }
+
+  createRestaurantInformationContent(restaurant: Restaurant) {
+    return `
+      <div class="information">
         <div class="left-section">
           <div class="restaurant__category">
-          <img
-            src="${RESTAURANT_IMAGE[restaurant.category]}"
-            alt="${restaurant.category}"
-            class="category-icon"
-          />
+            <img
+              src="${RESTAURANT_IMAGE[restaurant.category]}"
+              alt="${restaurant.category}"
+              class="category-icon"
+            />
+            </div>
+            <div class="restaurant-detail__info-text">
+              <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
+              <span class="restaurant__distance text-body">
+                캠퍼스부터 ${restaurant.distance}분 내
+              </span>
+            </div>
           </div>
-          <div class="restaurant-detail__info-text">
-            <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
-            <span class="restaurant__distance text-body">
-              캠퍼스부터 ${restaurant.distance}분 내
-            </span>
-          </div>
-        </div>
           <div class="icon-container">
             <div class="restaurant__star">
-            <img 
-              src="${getFavoriteIcon(restaurant.favorite)}"
-              alt=""
-              id="restaurant-information-star"
-              class="restaurant-star-icon ${restaurant.favorite && 'favorite'}"
-              data-id="${restaurant.id}"
-            />
+              <img 
+                src="${getFavoriteIcon(restaurant.favorite)}"
+                alt=""
+                id="restaurant-information-star"
+                class="restaurant-star-icon ${restaurant.favorite && 'favorite'}"
+                data-id="${restaurant.id}"
+              />
             </div>
           </div>
         </div>
@@ -87,23 +106,18 @@ function createRestaurantInformationContent(restaurant: Restaurant) {
             : ''
         }
       </div>
-      <div class="button-container">
-        <button
-          type="button"
-          id="restaurant-information-delete-button"
-          class="button button--secondary text-caption"
-          data-id="${restaurant.id}"
-        >
-          삭제하기
-        </button>
-        <button
-          id="restaurant-information-close-button"
-          class="button button--primary text-caption modal-close-button"
-        >
-          닫기
-        </button>
-      </div>
     `;
+  }
+
+  renderContent(restaurant: Restaurant) {
+    const modalContainer = $<HTMLDivElement>('#restaurant-information');
+    modalContainer.dataset.id = String(restaurant.id);
+    console.log(modalContainer.dataset.id);
+
+    const informationContainer = $<HTMLDivElement>('.restaurant-detail__info-container');
+    informationContainer.replaceChildren();
+    informationContainer.insertAdjacentHTML('afterbegin', this.createRestaurantInformationContent(restaurant));
+  }
 }
 
-export { addRestaurantInformationEvents, createRestaurantInformationContent };
+export default new RestaurantInformation();
