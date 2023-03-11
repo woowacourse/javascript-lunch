b83d1699d06ac1e3f32d8ea335c6c0f9fa4eab5d
Suyoung <jsy8492@gmail.com>
2023-03-11 09:02:21 +0900

    refactor: 불필요한 상수 객체 제거

--- src/domain/constants.ts
+++ src/domain/constants.ts
@@ -1,10 +1,1 @@
-export const RESTAURANT_ICON_CATEGORY = {
-  한식: 'korean',
-  중식: 'chinese',
-  일식: 'japanese',
-  양식: 'western',
-  아시안: 'asian',
-  기타: 'etc',
-};
-
 export const LOCAL_STORAGE_KEY = 'restaurants';
