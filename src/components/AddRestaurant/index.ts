b5c9d80fba10cee1fcad6a7840932d71f1ccad46
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-03-11 17:11:23 +0900

    refactor: css클래스 값을 상수 처리

--- src/components/AddRestaurant/index.ts
+++ src/components/AddRestaurant/index.ts
@@ -1,3 +1,4 @@
+import styleClass from "../../constants/styleClass";
 import { categoryOptions, distanceOptions } from "../../domain/restaurant";
 import {
   onClickCancelButton,
@@ -14,45 +15,45 @@ class AddRestaurant extends HTMLElement {
 
   render() {
     this.innerHTML = `
-        <h2 class="modal-title text-title">새로운 음식점</h2>
+        <h2 class="${styleClass.modal.title} ${styleClass.text.title}">새로운 음식점</h2>
         <form id="restaurantForm">
 
-          <div class="form-item form-item--required">
-            <label for="category text-caption">카테고리</label>
+          <div class="${styleClass.form.item} ${styleClass.form.itemRequired}">
+            <label for="category ${styleClass.text.caption}">카테고리</label>
             <select name="category" id="category" required>
               <option value="">선택해 주세요</option>
               ${categoryOptions()}
             </select>
           </div>
 
-          <div class="form-item form-item--required">
-            <label for="name text-caption">이름</label>
+          <div class="${styleClass.form.item} ${styleClass.form.itemRequired}">
+            <label for="name ${styleClass.text.caption}">이름</label>
             <input type="text" name="name" id="name" required>
           </div>
 
-          <div class="form-item form-item--required">
-            <label for="distance text-caption">거리(도보 이동 시간) </label>
+          <div class="${styleClass.form.item} ${styleClass.form.itemRequired}">
+            <label for="distance ${styleClass.text.caption}">거리(도보 이동 시간) </label>
             <select name="distance" id="distance" required>
               <option value="">선택해 주세요</option>
                 ${distanceOptions()}
             </select>
           </div>
 
-          <div class="form-item">
-            <label for="description text-caption">설명</label>
+          <div class="${styleClass.form.item}">
+            <label for="description ${styleClass.text.caption}">설명</label>
             <textarea name="description" id="description" cols="30" rows="5"></textarea>
-            <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
+            <span class="${styleClass.text.help} ${styleClass.text.caption}">메뉴 등 추가 정보를 입력해 주세요.</span>
           </div>
 
-          <div class="form-item">
-            <label for="link text-caption">참고 링크</label>
+          <div class="${styleClass.form.item}">
+            <label for="link ${styleClass.text.caption}">참고 링크</label>
             <input type="text" name="link" id="link">
-            <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
+            <span class="${styleClass.text.help} ${styleClass.text.caption}">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
           </div>
 
           <div class="button-container">
-            <button id="cancelButton" type="button" class="button button--secondary text-caption">취소하기</button>
-            <button type="submit" class="button button--primary text-caption">추가하기</button>
+            <button id="cancelButton" type="button" class="button ${styleClass.button.secondary} ${styleClass.text.caption}">취소하기</button>
+            <button type="submit" class="button ${styleClass.button.primary} ${styleClass.text.caption}">추가하기</button>
           </div>
         </form>
       `;
---
+++ src/constants/styleClass.ts
@@ -0,0 +1,19 @@
+const styleClass = {
+  button: {
+    primary: 'button--primary',
+    secondary: 'button--secondary'
+  },
+  text: {
+    title: 'text-title',
+    caption: 'text-caption',
+    help: 'help-text'
+  },
+  form: {
+    item: 'form-item',
+    itemRequired: 'form-item--required'
+  },
+  modal: {
+    title: 'modal-title'
+  }
+}
+export default styleClass
