d205a4584ac32e33bf4317ce17fe6fdde2a13671
shackstack <kms1061@naver.com>
2023-03-11 20:18:22 +0900

    refactor: addEvent 이벤트 타입 배열로 선언

--- src/types/restaurant.ts
+++ src/types/restaurant.ts
@@ -8,6 +8,11 @@ export type SortingWayType = (typeof SORTINGWAY)[keyof typeof SORTINGWAY];
 
 export type CategoryLowerCaseType = (typeof CATEGORY_LOWER_CASE)[keyof typeof CATEGORY_LOWER_CASE];
 
+export type EventHandler = (event: Event) => void;
+
+const EventTypes = ["click", "submit", "change", "keydown"] as const;
+
+export type EventType = (typeof EventTypes)[number];
 export interface RestaurantInfo {
   name: string;
   category: CategoryType;
--- src/util/addEvent.ts
+++ src/util/addEvent.ts
@@ -1,9 +1,6 @@
-export const addEvent = (
-  $target: Element,
-  eventType: string,
-  selector: string,
-  callback: (event: Event) => void
-): void => {
+import { EventHandler, EventType } from "../types/restaurant";
+
+export const addEvent = ($target: Element, eventType: EventType, selector: string, callback: EventHandler): void => {
   $target.addEventListener(eventType, (event: Event) => {
     if (!(event.target as Element).closest(selector)) return false;
     callback(event);
