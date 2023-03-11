dc77cb1f39312cfaf1e79312636192dcc7f3d7f9
turtle601 <turtle601@naver.com>
2023-03-11 14:51:14 +0900

    feat(Button): Button 컴포넌트 추상화 클래스 구현

---
+++ src/view/components/Button/abstract/Button.ts
@@ -0,0 +1,49 @@
+import { $ } from '../../../../utils/querySelector';
+import { ButtonInfo } from '../type/ButtonType';
+
+class Button {
+  #target;
+
+  constructor($target: Element) {
+    this.#target = $target;
+  }
+
+  template(buttonInfo: ButtonInfo) {
+    return `
+      <button 
+        type="${buttonInfo.buttonType}"
+        class="button ${buttonInfo.buttonStyle} text-caption ${buttonInfo.func}"
+      >
+        ${buttonInfo.buttonText}
+      </button>
+    `;
+  }
+
+  render(buttonInfo: ButtonInfo) {
+    this.#target.innerHTML += this.template(buttonInfo);
+
+    return this;
+  }
+
+  addEvent(eventTarget: Element) {
+    if (!eventTarget) throw new Error('[ERROR]');
+
+    return this;
+  }
+
+  closeModal() {
+    $('.modal')?.classList.remove('modal--open');
+  }
+
+  setEvent() {
+    this.#target.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (e.target instanceof HTMLButtonElement) {
+        this.addEvent(e.target);
+      }
+    });
+  }
+}
+
+export default Button;
