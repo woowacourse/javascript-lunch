a253c3503cbafda1c3ef9d628e349791c8c0b1dc
dladncks1217 <dlaxodud1217@gmail.com>
2023-03-11 16:04:18 +0900

    feat: 음식점 카테고리별 정렬 기능, 음식점 거리순 정렬 기능 추가

--- src/index.js
+++ src/index.js
@@ -10,9 +10,66 @@ const restaurantManager = new RestaurantManager(localStorage);
 const renderListData = restaurantManager.getRestaurantList();
 
 const header = new Header();
-const main = new Main(restaurantManager);
+const main = new Main(restaurantManager, addEvent);
 const addModal = new AddModal(restaurantManager, main);
 
 $('.gnb').innerHTML = header.render();
-$('.restaurant-list').innerHTML = main.render(renderListData);
+$('.restaurant-list').innerHTML = main.render(renderListData, '전체', '이름순', false);
 $('.modal').innerHTML = addModal.render();
+
+function addEvent() {
+  const SORT_DATA = {
+    name: '이름순',
+    distance: '거리순',
+  };
+  const category = $('select#category-filter option:checked').value;
+  const sorts = $('select#sorting-filter option:checked').textContent;
+
+  $('.render-filter').addEventListener('click', (e) => {
+    if (e.target.className === 'render-unselected') {
+      const reRenderData = restaurantManager.filterRestaurantLists(category, sorts);
+      $('.render-selected').className = 'render-unselected';
+      e.target.className = 'render-selected';
+      if (e.target.textContent === '자주 가는 음식점') {
+        $('.restaurant-list').innerHTML = main.render(reRenderData, category, sorts, true);
+        return addEvent();
+      }
+      $('.restaurant-list').innerHTML = main.render(reRenderData, category, sorts, false);
+      return addEvent();
+    }
+  });
+
+  $('#category-filter').addEventListener('change', (e) => {
+    if (e.target.value === '전체') {
+      const renderData = restaurantManager.getRestaurantList();
+      $('.restaurant-list').innerHTML = main.render(
+        renderData,
+        '전체',
+        sorts,
+        $('.render-selected').textContent === '자주 가는 음식점'
+      );
+      return addEvent();
+    }
+    const reRenderData = restaurantManager.filterRestaurantLists(e.target.value, sorts);
+    $('.restaurant-list').innerHTML = main.render(
+      reRenderData,
+      e.target.value,
+      sorts,
+      $('.render-selected').textContent === '자주 가는 음식점'
+    );
+
+    return addEvent();
+  });
+
+  $('#sorting-filter').addEventListener('change', (e) => {
+    const reRenderData = restaurantManager.filterRestaurantLists(category, e.target.value);
+    $('.restaurant-list').innerHTML = main.render(
+      reRenderData,
+      category,
+      SORT_DATA[e.target.value],
+      $('.render-selected').textContent === '자주 가는 음식점'
+    );
+    return addEvent();
+  });
+}
+addEvent();
