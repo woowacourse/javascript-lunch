e3c0a99b8f336c0d04999a407cc67a92d2c53ae2
ashleysyheo <ashleysyheo@gmail.com>
2023-03-11 18:44:59 +0900

    refactor: 음식점 탭 메뉴 컴포넌트 수정

--- src/components/RestaurantTabMenu.ts
+++ src/components/RestaurantTabMenu.ts
@@ -2,34 +2,34 @@ import { TAB_BUTTON_ATTRIBUTE } from '../constants/domAttributes';
 import { createTabButton } from '../template/TabButtonTemplate';
 import { $ } from '../utils/domSelectors';
 
-function addRestaurantTabChangeEvent(onChangeTab: CallableFunction) {
-  const tabs = $<HTMLElement>('.restaurant-navigation');
+class RestaurantTabMenu {
+  private tabMenu: HTMLElement = $<HTMLElement>('.restaurant-navigation');
 
-  tabs.addEventListener('change', (event: Event) => {
-    if (event.target instanceof HTMLInputElement) {
-      onChangeTab(event.target.id);
-    }
-  });
+  addEvent(handleTabChange: CallableFunction) {
+    this.tabMenu.addEventListener('change', (event: Event) => {
+      if (event.target instanceof HTMLInputElement) {
+        handleTabChange(event.target.id);
+      }
+    });
+  }
+
+  render() {
+    this.tabMenu.insertAdjacentHTML(
+      'beforeend',
+      `
+      ${createTabButton(
+        TAB_BUTTON_ATTRIBUTE.ALL_RESTAURANTS_INPUT,
+        TAB_BUTTON_ATTRIBUTE.ALL_RESTAURANTS_LABEL,
+        '모든 음식점'
+      )}
+      ${createTabButton(
+        TAB_BUTTON_ATTRIBUTE.FAVORITE_RESTAURANTS_INPUT,
+        TAB_BUTTON_ATTRIBUTE.FAVORITE_RESTAURANTS_LABEL,
+        '자주 가는 음식점'
+      )}
+      `
+    );
+  }
 }
 
-function renderRestaurantTabMenu() {
-  const tabs = $<HTMLElement>('.restaurant-navigation');
-
-  tabs.insertAdjacentHTML(
-    'beforeend',
-    `
-    ${createTabButton(
-      TAB_BUTTON_ATTRIBUTE.ALL_RESTAURANTS_INPUT,
-      TAB_BUTTON_ATTRIBUTE.ALL_RESTAURANTS_LABEL,
-      '모든 음식점'
-    )}
-    ${createTabButton(
-      TAB_BUTTON_ATTRIBUTE.FAVORITE_RESTAURANTS_INPUT,
-      TAB_BUTTON_ATTRIBUTE.FAVORITE_RESTAURANTS_LABEL,
-      '자주 가는 음식점'
-    )}
-    `
-  );
-}
-
-export { addRestaurantTabChangeEvent, renderRestaurantTabMenu };
+export default new RestaurantTabMenu();
