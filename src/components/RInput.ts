9e8680ffc37260cb076acb827b9d13997b497613
jeongwusi <jeongwusi@gmail.com>
2023-03-12 16:45:27 +0900

    ✨ feat: form과 연동되도록 구현

--- src/components/RInput.ts
+++ src/components/RInput.ts
@@ -3,20 +3,36 @@ import RFormControl from './RFormControl';
 class RInput extends RFormControl {
   override renderTemplate(): string {
     return `
-        <style>
-          input {
-            padding: 8px;
-            margin: 6px 0;
+      <style>
+        input {
+          padding: 8px;
+          margin: 6px 0;
 
-            border: 1px solid var(--grey-200);
-            border-radius: 8px;
+          border: 1px solid var(--grey-200);
+          border-radius: 8px;
 
-            font-size: 16px;
-            width: 100%;
-          }
-        </style>
-        <input>
+          font-size: 16px;
+          width: 100%;
+        }
+      </style>
+      <input>
     `;
+  }
+
+  render(): void {
+    super.render();
+
+    this.shadowRoot
+      ?.querySelector<HTMLInputElement>('input')
+      ?.addEventListener('input', (event) => {
+        if (event.target instanceof HTMLInputElement) {
+          this.internals.setFormValue(event.target.value);
+        }
+      });
+  }
+
+  get value() {
+    return document.querySelector<HTMLInputElement>('input')?.value ?? '';
   }
 }
 
--- src/components/RTextarea.ts
+++ src/components/RTextarea.ts
@@ -1,23 +1,39 @@
-import RComponent from './RComponent';
+import RFormControl from './RFormControl';
 
-class RTextarea extends RComponent {
+class RTextarea extends RFormControl {
   renderTemplate(): string {
     return `
-        <style>
-            textarea {
-                padding: 8px;
-                margin: 6px 0;
+      <style>
+        textarea {
+          padding: 8px;
+          margin: 6px 0;
 
-                border: 1px solid var(--grey-200);
-                border-radius: 8px;
+          border: 1px solid var(--grey-200);
+          border-radius: 8px;
 
-                font-size: 16px;
-                width: 100%;
-                resize: none;
-            }
-        </style>
-        <textarea rows="4"></textarea>
+          font-size: 16px;
+          width: 100%;
+          resize: none;
+        }
+      </style>
+      <textarea rows="4"></textarea>
     `;
+  }
+
+  render(): void {
+    super.render();
+
+    this.shadowRoot
+      ?.querySelector<HTMLTextAreaElement>('textarea')
+      ?.addEventListener('input', (event) => {
+        if (event.target instanceof HTMLTextAreaElement) {
+          this.internals.setFormValue(event.target.value);
+        }
+      });
+  }
+
+  get value() {
+    return document.querySelector<HTMLTextAreaElement>('textarea')?.innerText ?? '';
   }
 }
 
