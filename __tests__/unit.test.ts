b285355559034b0d12a5d91c5e94b555037b7c78
regularPark <srk99279@naver.com>
2023-03-13 01:48:46 +0900

    ♻️ refactor: 타입 변경에 따른 단위 테스트 변경

--- __tests__/unit.test.ts
+++ __tests__/unit.test.ts
@@ -1,28 +1,52 @@
 import { sortByName, sortByDistance } from "../src/utils/Sort";
-import { RestaurantForm } from "../src/types/types";
+import { RestaurantForm } from "../src/global/types";
 
 test("음식점 이름순 정렬하기 테스트", () => {
   const sortedName: RestaurantForm[] = sortByName([
-    { category: "한식", name: "patrick", distance: 5, id: 100, favorite: true },
-    { category: "중식", name: "lego", distance: 5, id: 100, favorite: true },
-    { category: "한식", name: "dori", distance: 5, id: 100, favorite: true },
+    {
+      category: "한식",
+      name: "patrick",
+      distance: 5,
+      id: "100",
+      favorite: true,
+    },
+    { category: "중식", name: "lego", distance: 5, id: "100", favorite: true },
+    { category: "한식", name: "dori", distance: 5, id: "100", favorite: true },
   ]);
   expect(sortedName).toEqual([
-    { category: "한식", name: "dori", distance: 5, id: 100, favorite: true },
-    { category: "중식", name: "lego", distance: 5, id: 100, favorite: true },
-    { category: "한식", name: "patrick", distance: 5, id: 100, favorite: true },
+    { category: "한식", name: "dori", distance: 5, id: "100", favorite: true },
+    { category: "중식", name: "lego", distance: 5, id: "100", favorite: true },
+    {
+      category: "한식",
+      name: "patrick",
+      distance: 5,
+      id: "100",
+      favorite: true,
+    },
   ]);
 });
 
 test("거리순 정렬하기 테스트", () => {
   const sortedDistance: RestaurantForm[] = sortByDistance([
-    { category: "한식", name: "patrick", distance: 5, id: 100, favorite: true },
-    { category: "중식", name: "lego", distance: 15, id: 100, favorite: true },
-    { category: "한식", name: "dori", distance: 10, id: 100, favorite: true },
+    {
+      category: "한식",
+      name: "patrick",
+      distance: 5,
+      id: "100",
+      favorite: true,
+    },
+    { category: "중식", name: "lego", distance: 15, id: "100", favorite: true },
+    { category: "한식", name: "dori", distance: 10, id: "100", favorite: true },
   ]);
   expect(sortedDistance).toEqual([
-    { category: "한식", name: "patrick", distance: 5, id: 100, favorite: true },
-    { category: "한식", name: "dori", distance: 10, id: 100, favorite: true },
-    { category: "중식", name: "lego", distance: 15, id: 100, favorite: true },
+    {
+      category: "한식",
+      name: "patrick",
+      distance: 5,
+      id: "100",
+      favorite: true,
+    },
+    { category: "한식", name: "dori", distance: 10, id: "100", favorite: true },
+    { category: "중식", name: "lego", distance: 15, id: "100", favorite: true },
   ]);
 });
