5b3ff889a85658be3c1bc5759a206c6820e286fa
semnil5202 <semnil5202@gmail.com>
2023-03-12 23:38:08 +0900

    feat: 음식점 상세 모달에서 즐겨찾기 버튼을 눌렀을 때 즐겨찾기 추가 및 해제 기능

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -3,13 +3,15 @@ import { FAVORITE, RESTAURANT_IMAGE } from '../constants/images';
 import { geid, qs } from '../utils/domHelpers';
 
 export default class DetailModal extends Component {
+  details;
+
   constructor($target) {
     super($target);
 
     this.detailRestaurant.subscribe(this.render.bind(this));
 
     this.addEvent('click', (event) => {
-      this.test(event);
+      this.switchEvent(event);
     });
   }
 
@@ -23,6 +25,18 @@ export default class DetailModal extends Component {
       starShape,
     } = this.detailRestaurant.getRestaurantDetail()
   ) {
+    this.starShape = starShape;
+    this.details = {
+      category: category,
+      storeName: storeName,
+      distance: distance,
+      detail: detail || '',
+      link: link || '',
+      starShape: starShape,
+    };
+
+    console.log('render');
+
     return `
     <div class="modal-backdrop"></div>
     <div class="modal-container">
@@ -31,7 +45,7 @@ export default class DetailModal extends Component {
           <img src="${RESTAURANT_IMAGE[category]}" alt="${category}" class="category-icon" />
         </div>
         <div class="favorite__shape">
-          <img src="${FAVORITE[starShape]}" alt="${starShape}" />
+          <img src="${FAVORITE[starShape]}" alt="${starShape}" class="favorite-icon" />
         </div>
       </div>
       <div class="detail-modal-header">
@@ -50,7 +64,7 @@ export default class DetailModal extends Component {
 
       <!-- 링크 -->
       <div class="detail-form-item">
-        <a href="https://${link}" target="_blank">음식점 자세하게 알아보기</a>
+        <a href="${link}" target="_blank">${link}</a>
       </div>
 
       <!-- 취소/추가 버튼 -->
@@ -68,17 +82,81 @@ export default class DetailModal extends Component {
       `;
   }
 
-  test(event) {
+  switchEvent(event) {
     if (event.target.id === 'cancel-detail-modal-button') {
       this.closeDetailModal();
     }
     if (event.target.id === 'delete-detail-modal-button') {
+      this.deleteRestaurantList(event);
+    }
+    if (event.target.className === 'favorite-icon') {
+      this.switchFavorite(event);
     }
   }
 
-  deleteRestaurantList() {}
-
   closeDetailModal() {
     qs('.detail-modal').classList.remove('modal--open');
+  }
+
+  deleteRestaurantList(event) {
+    const deleteStoreName = event.currentTarget.textContent
+      .split('\n')
+      .map((text) => text.trim())
+      .filter((text) => text.length !== 0)[0];
+
+    this.restaurantManager.removeRestaurnat(deleteStoreName);
+    this.favoriteRestaurant.removeRestaurant(deleteStoreName);
+
+    this.closeDetailModal();
+  }
+
+  switchFavorite(event) {
+    if (this.starShape === 'lined') {
+      this.starShape = 'filled';
+
+      this.addRestaurantList(event);
+    } else if (this.starShape === 'filled') {
+      this.starShape = 'lined';
+
+      this.removeRestaurantList(event);
+    }
+  }
+
+  addRestaurantList(event) {
+    const favoriteTexts = event.currentTarget.textContent
+      .split('\n')
+      .map((text) => text.trim())
+      .filter((text) => text.length !== 0);
+
+    const category = event.currentTarget.children[1].children[0].children[0].children[0].alt;
+
+    const addFavoriteData = {
+      category: category,
+      storeName: favoriteTexts[0],
+      distance: favoriteTexts[1].match(/\d+/).join(''),
+      detail: favoriteTexts[2] || '',
+      link: favoriteTexts[3] || '',
+      starShape: 'filled',
+    };
+
+    this.details = addFavoriteData;
+
+    this.restaurantManager.fillRestaurantStarShape(favoriteTexts[0]);
+    this.favoriteRestaurant.addRestaurant(addFavoriteData);
+    this.detailRestaurant.setRestaurantDetail(this.details);
+  }
+
+  removeRestaurantList(event) {
+    const favoriteTexts = event.currentTarget.textContent
+      .split('\n')
+      .filter((text) => text.trim().length !== 0)
+      .map((restaurantText) => restaurantText.trim());
+
+    const storeName = favoriteTexts[0];
+    this.details.starShape = 'lined';
+
+    this.restaurantManager.unfillRestaurantStarShape(storeName);
+    this.favoriteRestaurant.removeRestaurant(storeName);
+    this.detailRestaurant.setRestaurantDetail(this.details);
   }
 }
