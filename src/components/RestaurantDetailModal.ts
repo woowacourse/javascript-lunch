53f8d1a29a36327ad82fce24a398ed6a160fc663
chex <chex417@naver.com>
2023-03-12 23:55:24 +0900

    style: 사용성 관련 리팩터링

--- src/components/RestaurantDetailModal.ts
+++ src/components/RestaurantDetailModal.ts
@@ -19,7 +19,7 @@ export const RestaurantDetailModal = (restaurant: Restaurant) => {
         <div class="restaurant__info">
             <h3 class="restaurant__name text-subtitle">${name}</h3>
             <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
-            <p class="restaurant__description text-body">${description ?? ''}</p>
+            <p class="restaurant__description text-body more-detail">${description ?? ''}</p>
             <a class="restaurant__link text-body" href="${link ?? ''}">${link ?? ''}</a>
         </div>
 
