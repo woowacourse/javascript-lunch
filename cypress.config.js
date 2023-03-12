fa5fd62707f81abde676e55af9c1c90277cdac35
solo5star <solo_5@naver.com>
2023-03-12 23:40:15 +0900

    chore: cypress 설정

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  // experimentalWebKitSupport: true,
+  includeShadowDom: true,
+  component: {
+    devServer: {
+      bundler: 'webpack',
+    },
+    supportFile: 'cypress/support/component.ts',
+  },
+  e2e: {},
+});
