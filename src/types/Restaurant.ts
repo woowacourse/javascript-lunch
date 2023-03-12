90d552d0618d9870bda23ebb2eda0364232dc796
semnil5202 <semnil5202@gmail.com>
2023-03-12 23:39:27 +0900

    refactor: starShape 타입 정의

--- src/types/Restaurant.ts
+++ src/types/Restaurant.ts
@@ -4,9 +4,11 @@ export type Restaurant = {
   distance: number;
   detail?: string;
   link?: string;
-  starShape: string;
+  starShape: StarShape;
 };
 
 export type Category = '전체' | '한식' | '중식' | '일식' | '양식' | '아시안' | '기타';
 
 export type SortBy = 'name' | 'distance';
+
+export type StarShape = 'lined' | 'filled';
