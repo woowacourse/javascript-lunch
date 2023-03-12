fcb14498928af3a4f082315300152a2803aa73b5
jeongwusi <jeongwusi@gmail.com>
2023-03-12 15:19:19 +0900

    ✨ feat: 이름과 이미지를 가진 객체 생성

---
+++ src/domain/Category.ts
@@ -0,0 +1,12 @@
+class Category {
+  #name: string;
+
+  #imageUrl: string;
+
+  constructor(name: string, imageUrl: string) {
+    this.#name = name;
+    this.#imageUrl = imageUrl;
+  }
+}
+
+export default Category;
