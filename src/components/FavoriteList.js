1b911232cf80519fff9d5c03dc6d9c366ee02e01
cruelladevil <dev.timetravel@gmail.com>
2023-03-13 04:16:30 +0900

    feat: FavoriteList 컴포넌트 추가

---
+++ src/components/FavoriteList.js
@@ -0,0 +1,38 @@
+import { restaurantService } from '..';
+import RestaurantItem from './RestaurantItem';
+
+export default class FavoriteList {
+  constructor($root) {
+    this.$root = $root;
+  }
+
+  render() {
+    this.$root.insertAdjacentHTML('beforeend', this.template());
+    this.$ul = this.$root.querySelector('ul');
+
+    restaurantService.getFavoriteRestaurant().forEach((restaurant) => {
+      new RestaurantItem(this.$ul).inject(this.restaurantInfo, restaurant).mount();
+    });
+  }
+
+  template() {
+    return `
+      <ul class="restaurant-list favorite-list"></ul>
+    `;
+  }
+
+  rerender() {
+    this.$ul.remove();
+    this.render();
+  }
+
+  inject(restaurantInfo) {
+    this.restaurantInfo = restaurantInfo;
+
+    return this;
+  }
+
+  mount() {
+    this.render();
+  }
+}
