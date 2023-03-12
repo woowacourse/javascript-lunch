8842669cdb9bf0241aaa1eb96bb6097a60c95eda
afds4567 <afds4567@gmail.com>
2023-03-12 09:15:18 +0900

    refactor: TabMenu update 메서드 내부 기능 분리

--- src/components/TabMenu.ts
+++ src/components/TabMenu.ts
@@ -1,22 +1,12 @@
-class TabMenu {
-  template() {
+export default class TabMenu {
+  public template() {
     return `
       <div id="allRestaurantButton" class="all-restaurant selected" >모든 음식점</div> 
       <div id="likedRestaurantButton" class="liked-restaurant" >자주 가는 음식점</div>
     `;
   }
 
-  allRestuarantClicked() {
-    const event = new CustomEvent('allRestaurantClicked');
-    document.dispatchEvent(event);
-  }
-
-  likedRestaurantClicked() {
-    const event = new CustomEvent('likedRestaurantClicked');
-    document.dispatchEvent(event);
-  }
-
-  bindTabButton(): void {
+  public bindTabButton(): void {
     const allRestaurantButton = document.querySelector('#allRestaurantButton');
     if (allRestaurantButton) {
       allRestaurantButton.addEventListener('click', this.allRestuarantClicked);
@@ -28,19 +18,36 @@ class TabMenu {
     }
   }
 
-  update(allButton: Element, likedButton: Element, navTab: string) {
+  public update(allButton: Element, likedButton: Element, navTab: string) {
     const navigationElement = document.querySelector('.tabMenu-container');
     const filterContainer = document.querySelector('.restaurant-filter-container') as HTMLElement;
-    //선택된 탭 빨간 줄 표시 기능
-    if (navigationElement) {
-      navigationElement.innerHTML = '';
-      navigationElement.appendChild(allButton);
-      navigationElement.appendChild(likedButton);
-    }
-    //자주가는 식당 선택 시 셀렉트 박스 삭제
-    if (navTab === 'liked') {
+
+    const showSelectedTab = () => {
+      navigationElement!.innerHTML = '';
+      navigationElement!.appendChild(allButton);
+      navigationElement!.appendChild(likedButton);
+    };
+
+    const hideFilterContainer = () => {
       filterContainer!.style.display = 'none';
+    };
+
+    if (navigationElement) {
+      showSelectedTab();
+    }
+
+    if (navTab === 'liked') {
+      hideFilterContainer();
     }
   }
+
+  private allRestuarantClicked() {
+    const event = new CustomEvent('allRestaurantClicked');
+    document.dispatchEvent(event);
+  }
+
+  private likedRestaurantClicked() {
+    const event = new CustomEvent('likedRestaurantClicked');
+    document.dispatchEvent(event);
+  }
 }
-export default TabMenu;
