5c18e1f9b2591b7bc9d9faee8e231409ed076c82
jeongwusi <jeongwusi@gmail.com>
2023-03-12 17:49:11 +0900

    ♻️ refactor: 매직넘버 상수화

---
+++ src/utils/constants.ts
@@ -0,0 +1,31 @@
+export const FILTER = {
+  value: {
+    entire: '전체',
+  },
+
+  label: {
+    entire: '전체',
+  },
+};
+
+export const SORT = {
+  value: {
+    name: 'name',
+    distance: 'distance',
+  },
+
+  label: {
+    name: '이름순',
+    distance: '거리순',
+  },
+};
+
+export const DEFAULT_OPTION = {
+  value: '',
+  label: '선택해주세요',
+};
+
+export const RESTAURANT_NAME_LENGTH = {
+  min: 1,
+  max: 20,
+};
