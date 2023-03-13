d1308cbf5c6284c9983c3260993b22ad955d90ad
kangyeongmin <kym6050@hanyang.ac.kr>
2023-03-13 17:28:19 +0900

    test: 즐겨찾기 기능 e2e테스트 구현

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -28,3 +28,26 @@ describe("음식점 추가 바텀시트 테스트", () => {
     cy.get("#restaurantList").contains("캠퍼스부터 5분 내");
   });
 });
+
+describe("즐겨찾기 기능 테스트", () => {
+  beforeEach("beforeEach", () => {
+    cy.visit(TEST_URL);
+    window.localStorage.setItem("restaurants", JSON.stringify(DUMMY_DATA));
+  });
+
+  it("음식점 즐겨찾기 마크를 누르면 자주가는 음식점에서 해당 음식점을 확인할 수 있디.", () => {
+    cy.get("li > #favorite").first().click();
+    cy.get("#favoriteTab").click();
+    cy.get("#restaurantList").contains("찐친");
+    cy.get("#restaurantList").contains("캠퍼스부터 5분 내");
+  });
+
+  it("음식점 상세 바텀시트에서 즐겨찾기 마크를 누르면 자주가는 음식점에서 해당 음식점을 확인할 수 있디.", () => {
+    cy.get(".restaurant__info").first().click();
+    cy.get("#restaurantDetail > .icon-container > #favorite").click();
+    cy.get("#closeButton").click();
+    cy.get("#favoriteTab").click();
+    cy.get("#restaurantList").contains("찐친");
+    cy.get("#restaurantList").contains("캠퍼스부터 5분 내");
+  });
+});
