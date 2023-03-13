f9abf2f0334b807ffb58c036bfb3267be1ae4664
semnil5202 <semnil5202@gmail.com>
2023-03-13 15:34:35 +0900

    refactor: 인스턴스 필드 활용으로 불필요한 로직 및 필드 제거

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -25,7 +25,6 @@ export default class DetailModal extends Component {
       starShape,
     } = this.detailRestaurant.getRestaurantDetail()
   ) {
-    this.starShape = starShape;
     this.details = {
       category: category,
       storeName: storeName,
@@ -34,8 +33,6 @@ export default class DetailModal extends Component {
       link: link || '',
       starShape: starShape,
     };
-
-    console.log('render');
 
     return `
     <div class="modal-backdrop"></div>
@@ -87,10 +84,10 @@ export default class DetailModal extends Component {
       this.closeDetailModal();
     }
     if (event.target.id === 'delete-detail-modal-button') {
-      this.deleteRestaurantList(event);
+      this.deleteRestaurantList();
     }
     if (event.target.className === 'favorite-icon') {
-      this.switchFavorite(event);
+      this.switchFavorite();
     }
   }
 
@@ -98,65 +95,34 @@ export default class DetailModal extends Component {
     qs('.detail-modal').classList.remove('modal--open');
   }
 
-  deleteRestaurantList(event) {
-    const deleteStoreName = event.currentTarget.textContent
-      .split('\n')
-      .map((text) => text.trim())
-      .filter((text) => text.length !== 0)[0];
-
-    this.restaurantManager.removeRestaurnat(deleteStoreName);
-    this.favoriteRestaurant.removeRestaurant(deleteStoreName);
+  deleteRestaurantList() {
+    this.restaurantManager.removeRestaurnat(this.details.storeName);
+    this.favoriteRestaurant.removeRestaurant(this.details.storeName);
 
     this.closeDetailModal();
   }
 
-  switchFavorite(event) {
-    if (this.starShape === 'lined') {
-      this.starShape = 'filled';
-
-      this.addRestaurantList(event);
-    } else if (this.starShape === 'filled') {
-      this.starShape = 'lined';
-
-      this.removeRestaurantList(event);
+  switchFavorite() {
+    if (this.details.starShape === 'lined') {
+      this.addRestaurantList();
+    } else if (this.details.starShape === 'filled') {
+      this.removeRestaurantList();
     }
   }
 
-  addRestaurantList(event) {
-    const favoriteTexts = event.currentTarget.textContent
-      .split('\n')
-      .map((text) => text.trim())
-      .filter((text) => text.length !== 0);
+  addRestaurantList() {
+    this.details.starShape = 'filled';
 
-    const category = event.currentTarget.children[1].children[0].children[0].children[0].alt;
-
-    const addFavoriteData = {
-      category: category,
-      storeName: favoriteTexts[0],
-      distance: favoriteTexts[1].match(/\d+/).join(''),
-      detail: favoriteTexts[2] || '',
-      link: favoriteTexts[3] || '',
-      starShape: 'filled',
-    };
-
-    this.details = addFavoriteData;
-
-    this.restaurantManager.fillRestaurantStarShape(favoriteTexts[0]);
-    this.favoriteRestaurant.addRestaurant(addFavoriteData);
+    this.restaurantManager.fillRestaurantStarShape(this.details.storeName);
+    this.favoriteRestaurant.addRestaurant(this.details);
     this.detailRestaurant.setRestaurantDetail(this.details);
   }
 
-  removeRestaurantList(event) {
-    const favoriteTexts = event.currentTarget.textContent
-      .split('\n')
-      .filter((text) => text.trim().length !== 0)
-      .map((restaurantText) => restaurantText.trim());
-
-    const storeName = favoriteTexts[0];
+  removeRestaurantList() {
     this.details.starShape = 'lined';
 
-    this.restaurantManager.unfillRestaurantStarShape(storeName);
-    this.favoriteRestaurant.removeRestaurant(storeName);
+    this.restaurantManager.unfillRestaurantStarShape(this.details.storeName);
+    this.favoriteRestaurant.removeRestaurant(this.details.storeName);
     this.detailRestaurant.setRestaurantDetail(this.details);
   }
 }
