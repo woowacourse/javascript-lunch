1c53f100d3e5d67f2bd03c334a0ab94ea6fe0868
bassyu <yrkdgus@gmail.com>
2023-03-13 17:35:09 +0900

    feat(constants): 중복되는 옵션들 상수로 정의

--- src/constants.ts
+++ src/constants.ts
@@ -21,3 +21,25 @@ export const CATEGORY_IMAGE_URL: Record<string, string> = {
 
 export const DEFAULT_CATEGORY: Category = CATEGORY_NAME.all;
 export const DEFAULT_SORTING: Sorting = 'name';
+
+export const CATEGORY_OPTIONS = [
+  { value: '한식', content: '한식' },
+  { value: '중식', content: '중식' },
+  { value: '일식', content: '일식' },
+  { value: '양식', content: '양식' },
+  { value: '아시안', content: '아시안' },
+  { value: '기타', content: '기타' },
+];
+
+export const SORTING_OPTIONS = [
+  { value: 'name', content: '이름순' },
+  { value: 'takeMinute', content: '거리순' },
+];
+
+export const TAKE_MINUTE_OPTIONS = [
+  { value: '5', content: '5분 내' },
+  { value: '10', content: '10분 내' },
+  { value: '15', content: '15분 내' },
+  { value: '20', content: '20분 내' },
+  { value: '30', content: '30분 내' },
+];
