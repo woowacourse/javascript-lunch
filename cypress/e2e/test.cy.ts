7a16843cc2c371351566573748f812e020eb01a9
dladncks1217 <dlaxodud1217@gmail.com>
2023-03-13 12:42:51 +0900

    refactor: cypress 테스트코드 방식 수정

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('점심 뭐 먹지 e2e 테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080/');
     cy.viewport(500, 1000);
@@ -18,11 +18,13 @@ describe('template spec', () => {
   it('음식점을 클릭해 상세정보를 확인한 뒤, 삭제하기 버튼을 누르고 삭제 여부를 확인한다.', () => {
     cy.contains('친친').click();
     cy.get('.information-modal--open').contains('삭제하기').click();
-    cy.get('ul.restaurant-list').children('li').should('have.length', 5);
+    cy.get('ul.restaurant-list').children('li').should('not.have.text', '친친');
   });
 
   it('음식점을 즐겨찾기한 뒤, 즐겨찾기 여부를 확인한다.', () => {
-    cy.get('.category-lined').eq(1).click();
+    // cy.get('ul.restaurant-list').children('li').contains('친친').get('.category-lined').click();
+    cy.get('ul.restaurant-list').contains('친친').get('.category-lined').click();
+    // cy.get('.category-lined').eq(1).click();
     cy.get('.render-unselected').click();
     cy.get('.restaurant-list').children().should('contain.text', '친친');
   });
