cfd8ea81fcd2c804d046b08c8b771e593d01cd7f
bassyu <yrkdgus@gmail.com>
2023-03-13 17:36:13 +0900

    refactor(restaurantFilterContainer): 옵션 상수 적용

--- src/component/restaurantFilterContainer.js
+++ src/component/restaurantFilterContainer.js
@@ -1,24 +1,15 @@
+import { CATEGORY_OPTIONS, SORTING_OPTIONS } from '../constants';
 import select from './common/select';
 
 const restaurantFilterContainer = () => {
-  const categoryOptions = [
-    { value: '전체', content: '전체' },
-    { value: '한식', content: '한식' },
-    { value: '중식', content: '중식' },
-    { value: '일식', content: '일식' },
-    { value: '양식', content: '양식' },
-    { value: '아시안', content: '아시안' },
-    { value: '기타', content: '기타' },
-  ];
-  const sortingOptions = [
-    { value: 'name', content: '이름순' },
-    { value: 'takeMinute', content: '거리순' },
-  ];
-
   return `
     <section class="restaurant-filter-container">
-      ${select({ id: 'category-filter', name: 'category', options: categoryOptions })}
-      ${select({ id: 'sorting-filter', name: 'sorting', options: sortingOptions })}
+      ${select({
+        id: 'category-filter',
+        name: 'category',
+        options: [{ value: '전체', content: '전체' }, ...CATEGORY_OPTIONS],
+      })}
+      ${select({ id: 'sorting-filter', name: 'sorting', options: SORTING_OPTIONS })}
     </section>
   `;
 };
