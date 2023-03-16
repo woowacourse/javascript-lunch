8b0118aed4c56779967a14b735b5f269393ffb14
semnil5202 <semnil5202@gmail.com>
2023-03-16 13:04:46 +0900

    refactor: 음식 종류 탭의 하단 라인 Div 클래스 속성 적용

--- src/components/FavoriteTap.js
+++ src/components/FavoriteTap.js
@@ -25,12 +25,12 @@ export default class Favorite extends Component {
     <div class="favorite-item">
       <input type="radio" id="all__restaurant__radio" name="favorite" aria-label="모든 음식점" checked />
       <label class="all__restaurant__label" for="all__restaurant__radio">모든 음식점</label>
-      <div></div>
+      <div class="restaurant__tap__line"></div>
     </div>
     <div class="favorite-item">
       <input type="radio" id="favorite__restaurant__radio" name="favorite" aria-label="자주 가는 음식점" />
       <label class="favorite__restaurant__label" for="favorite__restaurant__radio">자주 가는 음식점</label>
-      <div></div>
+      <div class="restaurant__tap__line"></div>
     </div>
       `;
   }
