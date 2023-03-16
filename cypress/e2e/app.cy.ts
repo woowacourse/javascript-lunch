29280746fa74cab7a6343b67f5d67e5a137f9529
cruelladevil <dev.timetravel@gmail.com>
2023-03-16 22:40:22 +0900

    test: 테스트 추가 및 beforeEach 적용

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,8 +1,10 @@
 describe('점심 뭐 먹지 e2e 테스트', () => {
-  it('새로운 음식점 추가하기', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:8080/');
     cy.viewport(390, 880);
+  });
 
+  it('새로운 음식점 추가하기', () => {
     cy.get('header button').click();
 
     cy.get('form #category').select('한식');
@@ -13,5 +15,18 @@ describe('점심 뭐 먹지 e2e 테스트', () => {
     cy.get('form .button--primary').click();
 
     cy.get('.restaurant-list').contains('마초갈비').should('exist');
+  });
+
+  it('음식점을 클릭하고 삭제하기 버튼을 누르면 음식점이 삭제된다.', () => {
+    cy.get('.restaurant-list').contains('이태리키친').click();
+    cy.get('button').contains('삭제하기').click();
+    cy.get('.restaurant-list').should('not.contain', '이태리키친');
+  });
+
+  it('별을 누르면 자주 가는 음식점을 추가한다.', () => {
+    cy.get('.favorite-icon').first().click();
+    cy.get('nav').contains('자주 가는 음식점').click();
+
+    cy.get('.favorite-list').contains('농민 백암 순대').should('exist');
   });
 });
