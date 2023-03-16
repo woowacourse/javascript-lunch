88397dedfe0610bbf8a67512d8a22540474a8dfc
Patrick <kuyt3122@naver.com>
2023-03-17 07:32:58 +0900

    ♻️ refactor: css를 html 클래스로 묶어서 관리할 수 있도록 개선

--- src/UI/ModalRestaurantDetail.js
+++ src/UI/ModalRestaurantDetail.js
@@ -1,14 +1,20 @@
-import { $ } from "../utils/Dom";
+import { $ } from '../utils/Dom';
 import {
   stringifyJson,
   getRestaurantListFromLocalstorage,
   getSortByFromLocalStorage,
   getFoodCategoryFromLocalStorage,
-  setToLocalStorage
-} from "../utils/LocalStorage";
-import { RESTAURANT_LOCALSTORAGE_KEY, FOODCATEGORY_LOCALSTORAGE_KEY, 
-  SORTBY_LOCALSTORAGE_KEY, FAVORITE_LOCALSTORAGE_KEY, 
-  FAVORITE_VALUE, FAVORITE_ENROLL, FAVORITE_UNENROLL } from "../utils/Constant";
+  setToLocalStorage,
+} from '../utils/LocalStorage';
+import {
+  RESTAURANT_LOCALSTORAGE_KEY,
+  FOODCATEGORY_LOCALSTORAGE_KEY,
+  SORTBY_LOCALSTORAGE_KEY,
+  FAVORITE_LOCALSTORAGE_KEY,
+  FAVORITE_VALUE,
+  FAVORITE_ENROLL,
+  FAVORITE_UNENROLL,
+} from '../utils/Constant';
 
 export default class ModalRestaurantDetail {
   #template = `
@@ -60,21 +66,22 @@ export default class ModalRestaurantDetail {
     this.restaurantRegistry = restaurantRegistry;
   }
   render() {
-    document.body.insertAdjacentHTML("beforeend", this.#template);
+    document.body.insertAdjacentHTML('beforeend', this.#template);
   }
 
   initializeButtonEvents() {
-    $(".button--close").addEventListener("click", this.closeModalDetail);
-    $(".button--delete").addEventListener("click", () => this.deleteButtonInDetail());
+    $('.button--close').addEventListener('click', this.closeModalDetail);
+    $('.button--delete').addEventListener('click', this.deleteButtonInDetail);
   }
 
-  deleteButtonInDetail(){
+  deleteButtonInDetail=()=>{
     this.restaurantList.deleteRestaurantElement();
-    if ($(".favorite-restaurant").style.color === "rgb(236, 74, 10)") {
-      const restaurantAll =getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY) || [];
+    
+    if ($('.favorite-restaurant').classList.contains("active")) {
+      const restaurantAll = getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY) || [];
       this.closeModalDetail();
 
-      $(".restaurant-list").replaceChildren();
+      $('.restaurant-list').replaceChildren();
       this.restaurantRegistry.attachRestaurantToRegistry(restaurantAll);
       return;
     }
@@ -94,76 +101,70 @@ export default class ModalRestaurantDetail {
   changeRestaurantInformation(restaurantInfo) {
     this.restaurantInfo = restaurantInfo;
 
-    this.setRestaurantInformation(restaurantInfo)
+    this.setRestaurantInformation(restaurantInfo);
 
-    $(`.modla--restaurant_image`).addEventListener("click", this.favoriteEvent);
+    $(`.modla--restaurant_image`).addEventListener('click', this.favoriteEvent);
   }
 
-  favoriteEvent = (e) => this.clickModalFavorite(e, this.restaurantInfo);
+  favoriteEvent = e => this.clickModalFavorite(e, this.restaurantInfo);
 
-  setRestaurantInformation(restaurantInfo){
-    this.setRestaurantId(restaurantInfo)
-    this.setRestaurantCategory(restaurantInfo)
-    this.setRestaurantName(restaurantInfo)
-    this.setRestaurantDistance(restaurantInfo)
-    this.setRestaurantDescription(restaurantInfo)
-    this.setRestaurantLink(restaurantInfo)
-    this.setRestaurantFavorite(restaurantInfo)
+  setRestaurantInformation(restaurantInfo) {
+    this.setRestaurantId(restaurantInfo);
+    this.setRestaurantCategory(restaurantInfo);
+    this.setRestaurantName(restaurantInfo);
+    this.setRestaurantDistance(restaurantInfo);
+    this.setRestaurantDescription(restaurantInfo);
+    this.setRestaurantLink(restaurantInfo);
+    this.setRestaurantFavorite(restaurantInfo);
   }
 
-  setRestaurantId(restaurantInfo){
-    $(".modal--detail").id = restaurantInfo.id;
+  setRestaurantId(restaurantInfo) {
+    $('.modal--detail').id = restaurantInfo.id;
   }
 
-  setRestaurantCategory(restaurantInfo){
+  setRestaurantCategory(restaurantInfo) {
     const category = {
-      한식: "./category-korean.png",
-      일식: "./category-japanese.png",
-      양식: "./category-western.png",
-      중식: "./category-chinese.png",
-      아시안: "./category-asian.png",
-      기타: "./category-etc.png",
+      한식: './category-korean.png',
+      일식: './category-japanese.png',
+      양식: './category-western.png',
+      중식: './category-chinese.png',
+      아시안: './category-asian.png',
+      기타: './category-etc.png',
     };
 
-    $(".modal-detail-restaurant__image").setAttribute("src", category[restaurantInfo.category]);
-    $(".modal-detail-restaurant__image").setAttribute("alt", restaurantInfo.category);
+    $('.modal-detail-restaurant__image').setAttribute('src', category[restaurantInfo.category]);
+    $('.modal-detail-restaurant__image').setAttribute('alt', restaurantInfo.category);
   }
 
-  setRestaurantName(restaurantInfo){
-    $(".modal-detail-restaurant__name").textContent = restaurantInfo.name;
+  setRestaurantName(restaurantInfo) {
+    $('.modal-detail-restaurant__name').textContent = restaurantInfo.name;
   }
 
-  setRestaurantDistance(restaurantInfo){
-    $(
-      ".modal-detail-restaurant__distance"
-    ).textContent = `캠퍼스로부터 ${restaurantInfo.distance}분 내`;
+  setRestaurantDistance(restaurantInfo) {
+    $('.modal-detail-restaurant__distance').textContent = `캠퍼스로부터 ${restaurantInfo.distance}분 내`;
   }
 
-  setRestaurantDescription(restaurantInfo){
-    $(".modal-detail-restaurant__description").textContent =
-      restaurantInfo.description;
+  setRestaurantDescription(restaurantInfo) {
+    $('.modal-detail-restaurant__description').textContent = restaurantInfo.description;
   }
 
-  setRestaurantLink(restaurantInfo){
-    $(".modal-detail-restaurant__link").setAttribute(
-      "href",
-      restaurantInfo.link
-    );
-    $(".modal-detail-restaurant__link").textContent = restaurantInfo.link;
+  setRestaurantLink(restaurantInfo) {
+    $('.modal-detail-restaurant__link').setAttribute('href', restaurantInfo.link);
+    $('.modal-detail-restaurant__link').textContent = restaurantInfo.link;
   }
 
-  setRestaurantFavorite(restaurantInfo){
-    this.changeFavoriteImg(FAVORITE_UNENROLL)
+  setRestaurantFavorite(restaurantInfo) {
+    this.changeFavoriteImg(FAVORITE_UNENROLL);
     this.openModalDetail();
 
-    (getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY) ?? []).forEach((val) => {
+    (getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY) ?? []).forEach(val => {
       if (val.id === restaurantInfo.id) {
-        this.changeFavoriteImg(FAVORITE_ENROLL)
+        this.changeFavoriteImg(FAVORITE_ENROLL);
       }
     });
   }
 
-  changeFavoriteImg(favorite){
+  changeFavoriteImg(favorite) {
     $(`.modla--restaurant_favorite`).replaceChildren();
     $(`.modla--restaurant_favorite`).innerHTML += `<img class="modla--restaurant_image" src="${favorite}">`;
   }
@@ -172,63 +173,62 @@ export default class ModalRestaurantDetail {
     e.stopPropagation();
 
     if (this.isFilledOrLined(e, FAVORITE_ENROLL)) {
-      this.ifFavoriteFilled(e, restaurantInfo)
+      this.ifFavoriteFilled(e, restaurantInfo);
       return;
     }
 
     if (this.isFilledOrLined(e, FAVORITE_UNENROLL)) {
-      this.ifFavoriteLined(e, restaurantInfo)
+      this.ifFavoriteLined(e, restaurantInfo);
       return;
     }
   }
 
-  isFilledOrLined(e, favorite){
-    return e.target.getAttribute("src") === favorite
+  isFilledOrLined(e, favorite) {
+    return e.target.getAttribute('src') === favorite;
   }
 
-  getFavoriteList(favorite, restaurantInfo){
-    return getRestaurantListFromLocalstorage(RESTAURANT_LOCALSTORAGE_KEY).map((restaurant) => {
-      if (restaurant.id === restaurantInfo.id)
-        restaurant[FAVORITE_VALUE] = favorite;
+  getFavoriteList(favorite, restaurantInfo) {
+    return getRestaurantListFromLocalstorage(RESTAURANT_LOCALSTORAGE_KEY).map(restaurant => {
+      if (restaurant.id === restaurantInfo.id) restaurant[FAVORITE_VALUE] = favorite;
       return restaurant;
     });
   }
-  
-  ifFavoriteFilled(e, restaurantInfo){
-    const restaurantFavoriteList = this.getFavoriteList(FAVORITE_UNENROLL, restaurantInfo)
-    setToLocalStorage(RESTAURANT_LOCALSTORAGE_KEY, restaurantFavoriteList)
 
-    const res = (getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY)??[]);
-    const deletedRestaurantElementArray = res.filter((val) => val.id !== restaurantInfo.id);
+  ifFavoriteFilled(e, restaurantInfo) {
+    const restaurantFavoriteList = this.getFavoriteList(FAVORITE_UNENROLL, restaurantInfo);
+    setToLocalStorage(RESTAURANT_LOCALSTORAGE_KEY, restaurantFavoriteList);
+
+    const res = getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY) ?? [];
+    const deletedRestaurantElementArray = res.filter(val => val.id !== restaurantInfo.id);
     setToLocalStorage(FAVORITE_LOCALSTORAGE_KEY, deletedRestaurantElementArray);
-    
+
     this.changeFavoriteImageAttribut(e, restaurantInfo, FAVORITE_UNENROLL);
   }
 
-  ifFavoriteLined(e, restaurantInfo){
+  ifFavoriteLined(e, restaurantInfo) {
     const favorite = [];
-    const restaurantFavoriteList = this.getFavoriteList(FAVORITE_ENROLL, restaurantInfo)
-    setToLocalStorage(RESTAURANT_LOCALSTORAGE_KEY, restaurantFavoriteList)
+    const restaurantFavoriteList = this.getFavoriteList(FAVORITE_ENROLL, restaurantInfo);
+    setToLocalStorage(RESTAURANT_LOCALSTORAGE_KEY, restaurantFavoriteList);
 
-    const favoriteList = getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY)??[];
-    if (favoriteList !== null) favoriteList.forEach((val) => favorite.push(val));
+    const favoriteList = getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY) ?? [];
+    if (favoriteList !== null) favoriteList.forEach(val => favorite.push(val));
     favorite.push(restaurantInfo);
     setToLocalStorage(FAVORITE_LOCALSTORAGE_KEY, favorite);
-    
+
     this.changeFavoriteImageAttribut(e, restaurantInfo, FAVORITE_ENROLL);
   }
 
-  changeFavoriteImageAttribut(e, restaurantInfo, favorite){
-    $(`.restaurant_favorite${restaurantInfo.id}`).children[0].setAttribute("src",favorite);
-    e.target.setAttribute("src", favorite);
+  changeFavoriteImageAttribut(e, restaurantInfo, favorite) {
+    $(`.restaurant_favorite${restaurantInfo.id}`).children[0].setAttribute('src', favorite);
+    e.target.setAttribute('src', favorite);
   }
 
   openModalDetail() {
-    $(".modal--detail").style.display = "block";
+    $('.modal--detail').style.display = 'block';
   }
 
   closeModalDetail = () => {
-    $(".modal--detail").style.display = "none";
-    $(".modal--detail").removeAttribute("id");
+    $('.modal--detail').style.display = 'none';
+    $('.modal--detail').removeAttribute('id');
   };
 }
--- src/UI/RestaurantInventory.js
+++ src/UI/RestaurantInventory.js
@@ -1,61 +1,64 @@
-import { $ } from "../utils/Dom";
-import { getRestaurantListFromLocalstorage } from "../utils/LocalStorage";
-import { RESTAURANT_LOCALSTORAGE_KEY, FAVORITE_LOCALSTORAGE_KEY, 
-    FAVORITE_VALUE, FAVORITE_ENROLL } from "../utils/Constant";
+import { $ } from '../utils/Dom';
+import { getRestaurantListFromLocalstorage } from '../utils/LocalStorage';
+import {
+  RESTAURANT_LOCALSTORAGE_KEY,
+  FAVORITE_LOCALSTORAGE_KEY,
+  FAVORITE_VALUE,
+  FAVORITE_ENROLL,
+} from '../utils/Constant';
 
 export default class RestaurantInventory {
   #template = `
     <div class="restaurant-tab">
-        <div class="all-restaurant">
+        <div class="all-restaurant active">
             <span>모든 음식점</span>
         </div>
-        <div class="favorite-restaurant">
+        <div class="favorite-restaurant inactive">
             <span>자주 가는 음식점</span>
         </div>
     </div>
     `;
 
-    constructor(restaurantRegistry){
-        this.restaurantRegistry = restaurantRegistry
-    }
+  constructor(restaurantRegistry) {
+    this.restaurantRegistry = restaurantRegistry;
+  }
 
   render() {
-    $("main").insertAdjacentHTML("afterbegin", this.#template);
+    $('main').insertAdjacentHTML('afterbegin', this.#template);
   }
 
   initializeButtonEvents() {
-    $(".all-restaurant").addEventListener("click", () => {
-        RestaurantInventory.favoriteTabToAllListTab();
-      $(".restaurant-list").replaceChildren();
+    $('.all-restaurant').addEventListener('click', () => {
+      RestaurantInventory.favoriteTabToAllListTab();
+      $('.restaurant-list').replaceChildren();
 
       const restaurantAll = getRestaurantListFromLocalstorage(RESTAURANT_LOCALSTORAGE_KEY);
       this.restaurantRegistry.attachRestaurantToRegistry(restaurantAll);
     });
 
-    $(".favorite-restaurant").addEventListener("click", () => {
-        RestaurantInventory.allListTabToFavoriteTab();
-      $(".restaurant-list").replaceChildren();
+    $('.favorite-restaurant').addEventListener('click', () => {
+      RestaurantInventory.allListTabToFavoriteTab();
+      $('.restaurant-list').replaceChildren();
 
-      const restaurantFavorite= getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY);
-      restaurantFavorite.forEach((restaurant) => (restaurant[FAVORITE_VALUE] = FAVORITE_ENROLL));
+      const restaurantFavorite = getRestaurantListFromLocalstorage(FAVORITE_LOCALSTORAGE_KEY);
+      restaurantFavorite.forEach(restaurant => (restaurant[FAVORITE_VALUE] = FAVORITE_ENROLL));
       this.restaurantRegistry.attachRestaurantToRegistry(restaurantFavorite);
     });
   }
 
-  static favoriteTabToAllListTab=function() {
-    $(".all-restaurant").style.color = "#ec4a0a";
-    $(".all-restaurant").style.borderBottom = "2px solid #ec4a0a";
-    $(".favorite-restaurant").style.color = "#667085";
-    $(".favorite-restaurant").style.borderBottom = "2px solid #667085";
-    $(".restaurant-filter-container").className = "restaurant-filter-container";
-  }
+  static favoriteTabToAllListTab = function () {
+    $('.all-restaurant').classList.remove('inactive');
+    $('.all-restaurant').classList.add('active');
+    $('.favorite-restaurant').classList.remove('active');
+    $('.favorite-restaurant').classList.add('inactive');
+    $('.restaurant-filter-container').className = 'restaurant-filter-container';
+  };
 
-  static allListTabToFavoriteTab=function() {
-    $(".favorite-restaurant").style.color = "#ec4a0a";
-    $(".favorite-restaurant").style.borderBottom = "2px solid #ec4a0a";
-    $(".all-restaurant").style.color = "#667085";
-    $(".all-restaurant").style.borderBottom = "2px solid #667085";
-    $(".restaurant-filter-container").className =
-      "restaurant-filter-container filter-container-close";
-  }
+  static allListTabToFavoriteTab = function () {
+    $('.favorite-restaurant').classList.remove('inactive');
+    $('.favorite-restaurant').classList.add('active');
+    $('.all-restaurant').classList.remove('active');
+    $('.all-restaurant').classList.add('inactive');
+    $('.restaurant-filter-container').className = 'restaurant-filter-container filter-container-close';
+  };
 }
