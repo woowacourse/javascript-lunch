ebff71266430d18f4d4e43077cb3f7782564134f
cruelladevil <dev.timetravel@gmail.com>
2023-03-16 22:07:04 +0900

    refactor: 에러 처리 삭제

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,10 +1,5 @@
 export const getLocalStorage = (key) => {
-  try {
-    return JSON.parse(localStorage.getItem(key));
-  } catch (error) {
-    console.error(`localStorage에서 ${key}를 불러오는데 실패하였습니다.`);
-    console.error(error);
-  }
+  return JSON.parse(localStorage.getItem(key));
 };
 
 export const setLocalStorage = (key, value) => {
