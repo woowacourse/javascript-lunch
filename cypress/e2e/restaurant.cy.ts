4a5106b7df2d0e32ab3dbfce623f778e58af33b9
simhorim <hori5020@gmail.com>
2024-03-05 21:29:26 +0900

    test(restaurant): 선택된 카테고리와 일치 여부 테스트

--- cypress/e2e/restaurant.cy.ts
+++ cypress/e2e/restaurant.cy.ts
@@ -1,7 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    const a = 1;
-    const b = 1;
-    expect(a).to.equal(b);
+import Restaurant from '../../src/Restaurant';
+
+describe('Restaurant 도메인 테스트', () => {
+  it('해당 음식점의 카테고리가 선택된 카테고리와 일치하는 지 확인한다.', () => {
+    const restaurant = new Restaurant({ category: '아시안', name: 'son', distance: 5 });
+
+    expect(restaurant.isMatchedCategory('아시안')).to.be.true;
+    expect(restaurant.isMatchedCategory('중식')).to.be.false;
   });
 });
