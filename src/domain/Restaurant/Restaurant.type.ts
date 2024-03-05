36348bb174d4ee324d0fe1d6cc4b235c4ae6f3d3
손진영 <sm099@naver.com>
2024-03-05 17:17:44 +0900

    feat(Restaurant): 도메인 객체 구현
    레스토랑 들의 정보를 저장하는 도메인 객체 구현
    
    Co-authored-by: ImxYJL <allensain14@gmail.com>

---
+++ src/domain/Restaurant/Restaurant.type.ts
@@ -0,0 +1,17 @@
+export interface RestaurantDetail {
+  category: MenuCategory;
+  name: string;
+  distance: Distance;
+  description?: string;
+  url?: string;
+}
+
+export type MenuCategory =
+  | "한식"
+  | "중식"
+  | "일식"
+  | "양식"
+  | "아시안"
+  | "기타";
+
+export type Distance = 5 | 10 | 15 | 20 | 30;
