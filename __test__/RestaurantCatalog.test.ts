1e428232857bd096a5026f24e6ee809f47129f62
Jeongwoo Park <wahoo9040@naver.com>
2024-03-05 17:57:35 +0900

    test: 카테고리 필터링 및 이름순 정렬 테스트
    Co-Authored-By: MYONG JAEWI <78201530+jaymyong66@users.noreply.github.com>

--- __test__/RestaurantCatalog.test.ts
+++ __test__/RestaurantCatalog.test.ts
@@ -1,42 +1,6 @@
 import RestaurantCatalog from '../src/domain/RestaurantCatalog.ts';
 
-describe('RestaurantCatalog 테스트', () => {
-  const validRestaurantCase = [
-    [
-      [
-        {
-          category: '한식',
-          name: '반포식스',
-          distanceFromCampus: 5,
-        },
-        {
-          category: '양식',
-          name: '빕스',
-          distanceFromCampus: 5,
-        },
-      ],
-      2,
-    ],
-    [
-      [
-        {
-          category: '한식',
-          name: '반포식스',
-          distanceFromCampus: 5,
-        },
-      ],
-      1,
-    ],
-  ];
-
-  test.each(validRestaurantCase)('두개의 음식점을 추가하면 두개의', (inputs, length) => {
-    const newRestaurantCatalog = new RestaurantCatalog();
-    for (const input of inputs) {
-      newRestaurantCatalog.pushNewRestaurant(input);
-    }
-    expect(newRestaurantCatalog.getRestaurants().length).toEqual(length);
-  });
-
+describe('RestaurantCatalog 예외 테스트', () => {
   const invalidRestaurantCase = [
     {
       category: '한식',
@@ -58,4 +22,41 @@ describe('RestaurantCatalog 테스트', () => {
       newRestaurantCatalog.pushNewRestaurant(input);
     }).toThrow('❌');
   });
+});
+
+describe('RestaurantCatalog 기능 테스트', () => {
+  const validRestaurantCase = [
+    {
+      category: '한식',
+      name: '반포식스',
+      distanceFromCampus: 5,
+    },
+    {
+      category: '양식',
+      name: '빕스',
+      distanceFromCampus: 5,
+    },
+  ];
+
+  const newRestaurantCatalog = new RestaurantCatalog();
+
+  validRestaurantCase.forEach((restaurantInfo) => {
+    newRestaurantCatalog.pushNewRestaurant(restaurantInfo);
+  });
+
+  test('두개의 음식점을 추가하면 두개의 음식점이 추가된다.', () => {
+    expect(newRestaurantCatalog.getRestaurants().length).toEqual(2);
+  });
+
+  test('category 필터링 테스트', () => {
+    expect(newRestaurantCatalog.filterByCategory('한식')[0].getInfo().name).toEqual('반포식스');
+  });
+
+  test('이름순으로 정렬', () => {
+    expect(newRestaurantCatalog.sortByName()[0].getInfo().name).toEqual('반포식스');
+  });
+
+  // test('거리순으로 정렬', () => {
+  //   expect(newRestaurantCatalog.getRestaurants().length).toEqual(2);
+  // });
 });
