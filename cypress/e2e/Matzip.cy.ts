2ed6f5e533738e0b51720bbd1e10efd7661c5311
soosoo22 <lllllh00@naver.com>
2024-03-05 19:44:46 +0900

    test: 맛집 클래스 테스트 - 존재하지 않는 카테고리가 들어오면 에러
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>

--- cypress/e2e/Matzip.cy.ts
+++ cypress/e2e/Matzip.cy.ts
@@ -13,4 +13,15 @@ describe('맛집 클래스 테스트', () => {
     // when & then
     expect(() => matzip.add(matzip1)).to.throw();
   });
+  it('존재하지않는 카테고리가 들어오면 에러를 발생시킨다.', () => {
+    // given
+    const matzip = new Matzip([]);
+    const matzip1: Restaurant = {
+      name: '쑤쑤네짜장면',
+      category: '이탈리안' as CategoryType,
+      distance: 5,
+    };
+    // when & then
+    expect(() => matzip.add(matzip1)).to.throw();
+  });
 });
