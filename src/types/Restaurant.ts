9db6c15eb8af7a459b767d56721354192eedfae6
ooherin <555ohr@naver.com>
2024-03-05 16:22:11 +0900

    feat(type): 레스토랑 타입 구현

---
+++ src/types/Restaurant.ts
@@ -0,0 +1,11 @@
+export interface IRestuarant {
+  name: string;
+  distance: Distance;
+  category: Category;
+  description?: string;
+  link?: string;
+}
+
+export type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+
+export type Distance = 5 | 10 | 15 | 20 | 30;
