bfad01e828209d7f2b488a3572f648161d1bd507
Kim Da Eun <midekuna@gmail.com>
2024-03-05 15:57:29 +0900

    feat: 음식점 목록을 확인
    Co-Authored-By: 00kang <70834044+00kang@users.noreply.github.com>

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -11,8 +11,32 @@ interface Restaurant {
 class RestaurantManager {
   #restaurants: Array<Restaurant> = [];
 
+  get restaurants() {
+    return [...this.#restaurants];
+  }
+
   add(restaurant: Restaurant) {
     this.#restaurants.push(restaurant);
+  }
+
+  filterByCategory(category: Category): Restaurant[] {
+    return this.restaurants.filter((restaurant) => restaurant.category === category);
+  }
+
+  sortByName(): Restaurant[] {
+    return this.restaurants.sort((a, b) => {
+      if (a.name > b.name) return 1;
+      if (a.name < b.name) return -1;
+      return 0;
+    });
+  }
+
+  sortByDistance(): Restaurant[] {
+    return this.restaurants.sort((a, b) => {
+      if (a.distance > b.distance) return 1;
+      if (a.distance < b.distance) return -1;
+      return 0;
+    });
   }
 }
 
