3fa5df5e136db2e9bf40f785834c14dea8091c17
jinsil <jinsil378900@gmail.com>
2024-03-05 19:34:00 +0900

    fix(RestaurantInterfaces): 옵션 값 설정

--- src/interface/RestaurantInterfaces.ts
+++ src/interface/RestaurantInterfaces.ts
@@ -1,15 +1,15 @@
-type Category = "한식" | "중식" | "일식" | "아시안" | "양식" | "기타";
+type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
 
 type Distance = 5 | 10 | 15 | 20 | 30;
 
-type Property = "name" | "distance";
+type Property = 'name' | 'distance';
 
 interface Restaurant {
   category: Category;
   name: string;
   distance: Distance;
-  description: string;
-  link: string;
+  description?: string;
+  link?: string;
 }
 
 interface Restaurants {
