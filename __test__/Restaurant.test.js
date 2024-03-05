5961e710eff14ebe46f9f12d07a12724012b5cad
lurgi <wahoo9040@naver.com>
2024-03-05 14:19:38 +0900

    chore: jest prettier eslint 설정
    Co-authored-by: MYONG JAEWI <Jaymyong66@users.noreply.github.com>

---
+++ __test__/Restaurant.test.js
@@ -0,0 +1,5 @@
+describe('DomainTest', () => {
+  test('test', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ src/domain/Restaurant.ts
@@ -0,0 +1,13 @@
+interface IRestaurant {
+  category: '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+  name: string;
+  distanceFromCampus: 5 | 10 | 15 | 20 | 30;
+  description?: string[];
+  link?: string;
+}
+
+class Restaurant {
+  // restaurantInfo;
+}
+
+export default Restaurant;
