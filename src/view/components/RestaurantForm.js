a9dd0f86dcabea720f3e2009902b7910d379c9e9
Kim Da Eun <midekuna@gmail.com>
2024-03-05 20:44:08 +0900

    feat: Form 제출 시 localStorage에 저장
    Co-Authored-By: 00kang <70834044+00kang@users.noreply.github.com>

--- src/view/components/RestaurantForm.js
+++ src/view/components/RestaurantForm.js
@@ -9,10 +9,22 @@ const RestaurantForm = () => {
   restaurantForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const data = new FormData(e.target).entries();
+    const data = new FormData(e.target);
+    const formData = [...data.keys()].reduce((formData, key) => {
+      const value = data.get(key);
+      formData[key] = value;
+      return formData;
+    }, {});
+
+    if (window.localStorage.getItem('restaurants')) {
+      const restaurants = JSON.parse(window.localStorage.getItem('restaurants'));
+      restaurants.push(formData);
+      window.localStorage.setItem('restaurants', JSON.stringify(restaurants));
+    }
+    window.localStorage.setItem('restaurants', JSON.stringify([formData]));
+
     restaurantForm.dispatchEvent(
       new CustomEvent(RESTAURANT_FORM_EVENTS.submit, {
-        detail: { data },
         bubbles: true,
       }),
     );
