58e287ba9f3d44804f138e84b5c099518f7dbc93
Jeongwoo Park <wahoo9040@naver.com>
2024-03-05 16:31:43 +0900

    feat: Restaurant valid 기능 구현
    Co-Authored-By: MYONG JAEWI <78201530+jaymyong66@users.noreply.github.com>

--- src/domain/Restaurant.ts
+++ src/domain/Restaurant.ts
@@ -1,16 +1,37 @@
-interface IRestaurantInfo {
-  category: '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+type ICategory = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+type IDistanceFromCampus = 5 | 10 | 15 | 20 | 30;
+
+export interface IRestaurantInfo {
+  category: ICategory;
   name: string;
-  distanceFromCampus: 5 | 10 | 15 | 20 | 30;
-  description?: string[];
+  distanceFromCampus: IDistanceFromCampus;
+  description?: string;
   link?: string;
 }
+
+const RESTAURANT_CATEGORY: readonly ICategory[] = Object.freeze(['한식', '중식', '일식', '아시안', '양식', '기타']);
+const DISTANCE_FROM_CAMPUS: readonly IDistanceFromCampus[] = Object.freeze([5, 10, 15, 20, 30]);
 
 class Restaurant {
   #restaurantInfo: IRestaurantInfo;
 
   constructor(obj: IRestaurantInfo) {
+    this.#validateRestaurantType(obj);
     this.#restaurantInfo = obj;
+  }
+
+  #validateRestaurantType(obj: IRestaurantInfo) {
+    if (!RESTAURANT_CATEGORY.includes(obj.category)) {
+      throw new Error('❌');
+    }
+
+    if (typeof obj.name !== 'string') {
+      throw new Error('❌');
+    }
+
+    if (!DISTANCE_FROM_CAMPUS.includes(obj.distanceFromCampus)) {
+      throw new Error('❌');
+    }
   }
 }
 
