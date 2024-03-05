9e2e3458daed5dd89c05d29b8adad715cf65be93
anttiey <hanuuny@gmail.com>
2024-03-05 15:23:57 +0900

    feat: 음식점 클래스 추가
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

--- src/type/types.d.ts
+++ src/type/types.d.ts
@@ -1,7 +1,7 @@
-type TCategory = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
-type TDistance = 5 | 10 | 15 | 20 | 30;
+export type TCategory = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+export type TDistance = 5 | 10 | 15 | 20 | 30;
 
-interface IRestaurant {
+export interface IRestaurant {
   category: TCategory;
   name: string;
   distance: TDistance;
