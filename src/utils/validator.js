83b50a030f46dea96db962ec9b0f41c0aa35ca59
hanyoung <phanyoung@naver.com>
2024-03-05 21:50:50 +0900

    feat(validator): 유효성 검증 로직 추가 및 일부 변경 - 입력받은 값이 최대값을 넘으면 에러 발생하는 메서드 추가 - validateInRange 메서드의 입력받은 인자 위치 변경
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,4 +1,10 @@
-export const validateInRange = (value, min, max) => {
-    if (value < min || value > max)
-      throw new Error(`값이 ${min}이상 ${max}이하여야 합니다.`);
+export const validateInRange = (min, max, value) => {
+  if (value < min || value > max)
+    throw new Error(`값이 ${min}이상 ${max}이하여야 합니다.`);
+};
+
+export const validateNumberBelow = (max, value) => {
+  if (value > max) {
+    throw new Error(`값이 ${max}이하여야 합니다.`);
   }
+};
