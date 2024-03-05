69bd69dbe7fdfbfb51ef01196c2d2e5b1ebaa210
jinsil <jinsil378900@gmail.com>
2024-03-05 16:42:33 +0900

    feat(RestaurantInterfaces): 식당 관련 인터페이스 작성

---
+++ src/interface/RestaurantInterfaces.ts
@@ -0,0 +1,20 @@
+type Category = "한식" | "중식" | "일식" | "아시안" | "양식" | "기타";
+
+type Distance = 5 | 10 | 15 | 20 | 30;
+
+type Property = "name" | "distance";
+
+interface Restaurant {
+  category: Category;
+  name: string;
+  distance: Distance;
+  description: string;
+  link: string;
+}
+
+interface Restaurants {
+  restaurants: Restaurant[];
+  addRestaurant: (restaurant: Restaurant) => boolean;
+  filterByCategory: (category: Category) => Restaurant[];
+  sortByProperty: (property: Property) => Restaurant[];
+}
