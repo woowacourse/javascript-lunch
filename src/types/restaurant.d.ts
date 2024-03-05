9e011ed745ab1b1eb4c906fcbd111e10576c55c6
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-05 16:24:00 +0900

    feat: 음식점 타입 정의
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

---
+++ src/types/restaurant.d.ts
@@ -0,0 +1,10 @@
+type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+type Minutes = 5 | 10 | 15 | 20 | 30;
+
+interface Restaurant {
+  category: Category;
+  name: 'string';
+  walkingTimeFromCampus: Minutes;
+  description?: 'string';
+  referenceLink?: 'string';
+}
