a6044285068bcc4a3457252b0f9a784ce8386fcb
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-05 17:59:05 +0900

    test: 거리순으로 음식점 목록 정렬하는 테스트 정의
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- cypress/e2e/Restaurants.cy.js
+++ cypress/e2e/Restaurants.cy.js
@@ -45,4 +45,36 @@ describe('음식점 목록 테스트', () => {
 
     expect(restaurants.filterByCategory('한식')).to.deep.equal(result);
   });
+
+  it('거리순으로 음식점 목록을 정렬할 수 있다.', () => {
+    const restaurants = new Restaurants();
+    const result = [
+      {
+        category: '한식',
+        name: '한식당',
+        walkingTimeFromCampus: 5,
+      },
+      {
+        category: '한식',
+        name: '한식한식당',
+        walkingTimeFromCampus: 5,
+      },
+      {
+        category: '중식',
+        name: '중식당',
+        walkingTimeFromCampus: 20,
+      },
+      {
+        category: '양식',
+        name: '양식당',
+        walkingTimeFromCampus: 30,
+      },
+    ];
+
+    restaurantList.forEach((restaurant) => {
+      restaurants.addRestaurant(restaurant);
+    });
+
+    expect(restaurants.orderByDistance()).to.deep.equal(result);
+  });
 });
