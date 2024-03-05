f0fa97eada9766b6bc8af3889bea65432a845348
anttiey <hanuuny@gmail.com>
2024-03-05 17:17:13 +0900

    test: 음식점 목록을 카테고리로 필터링 기능 테스트 추가
    Co-authored-by: Yuseon Kim <useon@users.noreply.github.com>

---
+++ __tests__/Restaurants.test.ts
@@ -0,0 +1,27 @@
+import Restaurant from '../src/domain/Restaurant';
+import Restaurants from '../src/domain/Restaurants';
+import data from './data';
+
+const { restaurantA, restaurantB, restaurantC, restaurantD, restaurantE, restaurantF } = data;
+
+describe('음식점 목록 기능 단위 테스트', () => {
+  const restaurants = new Restaurants();
+
+  restaurants.addRestaurant(new Restaurant(restaurantA));
+  restaurants.addRestaurant(new Restaurant(restaurantB));
+  restaurants.addRestaurant(new Restaurant(restaurantC));
+  restaurants.addRestaurant(new Restaurant(restaurantD));
+  restaurants.addRestaurant(new Restaurant(restaurantE));
+  restaurants.addRestaurant(new Restaurant(restaurantF));
+
+  test.each([
+    ['한식', new Restaurant(restaurantA)],
+    ['중식', new Restaurant(restaurantB)],
+    ['일식', new Restaurant(restaurantC)],
+    ['아시안', new Restaurant(restaurantD)],
+    ['양식', new Restaurant(restaurantE)],
+    ['기타', new Restaurant(restaurantF)],
+  ])('음식점 목록 중 "%s" 인 카테고리만 필터링 할 수 있다.', (category: string, restaurant: Restaurant) => {
+    expect(restaurants.getFilteredByCategory(category)).toEqual([restaurant]);
+  });
+});
---
+++ __tests__/data.ts
@@ -0,0 +1,37 @@
+const restaurantA = {
+  category: '한식',
+  name: '파슬리네 김치찌개',
+  distance: 5,
+};
+
+const restaurantB = {
+  category: '중식',
+  name: '파슬리네 짜장면',
+  distance: 5,
+};
+
+const restaurantC = {
+  category: '일식',
+  name: '파슬리네 회전초밥',
+  distance: 10,
+};
+
+const restaurantD = {
+  category: '아시안',
+  name: '썬데이네 쌀국수',
+  distance: 5,
+};
+
+const restaurantE = {
+  category: '양식',
+  name: '썬데이네 파스타',
+  distance: 5,
+};
+
+const restaurantF = {
+  category: '기타',
+  name: '썬데이네 반찬가게',
+  distance: 10,
+};
+
+export default { restaurantA, restaurantB, restaurantC, restaurantD, restaurantE, restaurantF };
