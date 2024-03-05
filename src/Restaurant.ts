512876c06030730df3c7eab869fbe366caf66ca4
simhorim <hori5020@gmail.com>
2024-03-05 17:11:11 +0900

    feat(Restaurant): 음식점 클래스 구현

---
+++ src/Restaurant.ts
@@ -0,0 +1,24 @@
+type TCategory = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
+type TDistance = 5 | 10 | 15 | 20 | 30;
+
+interface IRestaurant {
+  category: TCategory;
+  name: string;
+  distance: TDistance;
+  description?: string;
+  referenceLink?: string;
+}
+
+class Restaurant {
+  information;
+
+  constructor(information: IRestaurant) {
+    this.information = information;
+  }
+
+  isMatchedCategory(category: string): boolean {
+    return this.information.category === category;
+  }
+}
+
+export default Restaurant;
