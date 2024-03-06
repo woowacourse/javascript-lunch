e725ba493cfd18c1cd83c9082ee5c7aa6ae053e3
Kim Da Eun <midekuna@gmail.com>
2024-03-06 15:53:30 +0900

    feat: Select 컴포넌트 제작
    Co-Authored-By: 00kang <70834044+00kang@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+// components
 import GNB from './view/components/GNB';
 import Modal from './view/components/Modal';
 import RestaurantForm from './view/components/RestaurantForm';
+import Select from './view/components/select/Select';
 
+// styles
 import './view/styles/global.css';
 import './view/styles/gnb.css';
 import './view/styles/modal.css';
+import './view/styles/Select.css';
 
+// imgs
 import './view/imgs/add-button.png';
 
 GNB();
 RestaurantForm();
 Modal();
+
+window.customElements.define('app-select', Select, { extends: 'select' });
---
+++ src/view/components/select/Select.js
@@ -0,0 +1,48 @@
+export default class Select extends HTMLSelectElement {
+  constructor() {
+    super();
+  }
+
+  static observedAttributes = ['data-options'];
+
+  get options() {
+    if (!this.dataset.options) {
+      return [];
+    }
+    return JSON.parse(this.dataset.options);
+  }
+
+  set options(value) {
+    this.setAttribute('data-options', JSON.stringify(value));
+  }
+
+  attributeChangedCallback() {
+    this.#createOptions();
+  }
+
+  #createOptions() {
+    this.innerHTML = '';
+
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(this.#generateDefaultOption('선택해 주세요'));
+    this.#generateOptions().forEach((option) => fragment.appendChild(option));
+
+    this.appendChild(fragment);
+  }
+
+  #generateDefaultOption(innerText) {
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.innerHTML = innerText;
+    return defaultOption;
+  }
+
+  #generateOptions() {
+    return this.options.map(({ value, option }) => {
+      const element = document.createElement('option');
+      element.value = value;
+      element.innerHTML = option;
+      return element;
+    });
+  }
+}
