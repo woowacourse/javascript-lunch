553eaefaacb7ac4fa0703dafea064ec125d163a0
손진영 <sm099@naver.com>
2024-03-06 15:59:55 +0900

    style(RestaurantItem): 오탈자 수정
    '캠퍼스 부터'를 '캠퍼스로 부터'로 변경

--- src/components/RestaurantItem/RestaurantItem.ts
+++ src/components/RestaurantItem/RestaurantItem.ts
@@ -22,7 +22,7 @@ class RestaurantItem extends BaseComponent {
             </div>
             <div class="restaurant__info">
                 <h3 class="restaurant__name text-subtitle">${this.restaurantDetail.name}</h3>
-                <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurantDetail.distance}분 내</span>
+                <span class="restaurant__distance text-body">캠퍼스로부터 ${this.restaurantDetail.distance}분 내</span>
                 <p class="restaurant__description text-body">${this.restaurantDetail.description}</p>
             </div>
         </li>
