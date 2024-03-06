6f62b3ca2f276aee0574eafb8bac3e02212819a2
jinsil <jinsil378900@gmail.com>
2024-03-06 16:52:27 +0900

    test(RestaurantService.cy): 레스토랑 서비스  정렬 기능 테스트 작성

--- cypress/e2e/RestaurantService.cy.js
+++ cypress/e2e/RestaurantService.cy.js
@@ -39,7 +39,7 @@ describe('레스토랑 추가 기능 테스트', () => {
       category: '한식',
       name: '꺼벙이분식',
       distance: 10,
-      description: '우육면 + 군만두',
+      description: '돈까스 김밥 + 순대',
       link: 'www.naver.com',
     };
 
@@ -71,6 +71,29 @@ describe('레스토랑 카테고리 별 필터링 기능 테스트', () => {
   categories.forEach((category, index) => {
     it(`카테고리가 ${category}인 경우 해당하는 레스토랑 리스트를 반환한다.`, () => {
       const result = RestaurantService.filterByCategory(category, DUMMY);
+
+      expect(result).to.deep.equal(expectedResult[index]);
+    });
+  });
+});
+
+describe('레스토랑 정렬 기능 테스트', () => {
+  const DUMMY = [
+    { category: '한식', name: '배가무닭볶음탕', distance: 15, description: '무가 맛있어요', link: 'www.naver.com' },
+    { category: '한식', name: '용호동낙지', distance: 10, description: '사장님 츤데레', link: 'www.naver.com' },
+    { category: '중식', name: '친친', distance: 5, description: '게살볶음밥 굿', link: 'www.daum.net' },
+  ];
+
+  const properties = ['name', 'distance'];
+
+  const expectedResult = [
+    [DUMMY[0], DUMMY[1], DUMMY[2]],
+    [DUMMY[2], DUMMY[1], DUMMY[0]],
+  ];
+
+  properties.forEach((property, index) => {
+    it(`정렬 기준이 ${property}인 경우 기준에 맞게 정렬된 레스토랑 리스트를 반환한다.`, () => {
+      const result = RestaurantService.sortByProperty(property, DUMMY);
 
       expect(result).to.deep.equal(expectedResult[index]);
     });
