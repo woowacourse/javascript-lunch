c533c2e3aef32553e28800bb8a2e9812c1ffaf79
이태훈 <rhymint@gmail.com>
2024-03-06 16:54:05 +0900

    feat: restaurants 정렬을 위한 SortBy Type 선언 Co-authored-by: chosim-dvlpr <bmj05378@gmail.com>

---
+++ src/constants/sortBy.ts
@@ -0,0 +1,8 @@
+export const SORTBY = {
+  newest: '최신순',
+  oldest: '오래된순',
+  nameAscending: '가게명순▲',
+  nameDescending: '가게명순▼',
+  distanceAscending: '거리순▲',
+  distanceDescending: '거리순▼',
+} as const;
