226038ef537256e6afe3227c72a54f8485d05768
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-06 17:17:03 +0900

    feat: Main 컴포넌트 생성
    html 태그 render 메서드 정의
    
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,9 @@ export default class Header {
   render() {
     /*html*/
     return `
-    <header class="gnb">
       <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
       <button type="button" class="gnb__button" aria-label="음식점 추가">
         <img src="${ICON.추가버튼}" alt="음식점 추가">
-      </button>
-    </header>`;
+    `;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Header from './components/Header';
+import Main from './components/Main';
+
 import './styles/index.css';
 
 const header = new Header();
+const main = new Main();
 
-document.querySelector('body').innerHTML = header.render();
+document.getElementById('header').innerHTML = header.render();
+document.getElementById('main').innerHTML = main.render();
