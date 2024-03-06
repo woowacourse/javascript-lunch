47f712dd979e481bd66948b30a5a406cc605cb00
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-06 19:55:08 +0900

    refactor: 정적 html를 동적 생성 및 Select 컴포넌트 사용
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import RestaurantCreationModal from './components/RestaurantCreationModal';
+import Select from './components/Select';
 
 import './styles/index.css';
 
+const selectDatas = [
+  {
+    id: 'sorting-filter',
+    name: 'sorting',
+    options: {
+      name: '이름순',
+      distance: '거리순',
+    },
+  },
+  {
+    id: 'category-filter',
+    name: 'category',
+    options: {
+      전체: '전체',
+      한식: '한식',
+      중식: '중식',
+      일식: '일식',
+      양식: '양식',
+      아시안: '아시안',
+      기타: '기타',
+    },
+  },
+];
+
 const header = new Header();
 const main = new Main();
+const sortingSelect = new Select(selectDatas[0]);
+const categorySelect = new Select(selectDatas[1]);
+const modal = new RestaurantCreationModal();
 
 document.getElementById('header').innerHTML = header.render();
 document.getElementById('main').innerHTML = main.render();
+document.getElementById('restaurant-filter-container').appendChild(sortingSelect.render());
+document.getElementById('restaurant-filter-container').appendChild(categorySelect.render());
+document.getElementById('restaurant-creation-modal').innerHTML = modal.render();
