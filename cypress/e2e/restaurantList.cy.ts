adc50d7fd63d965febaf569fd48f8c666d585b8f
simhorim <hori5020@gmail.com>
2024-03-06 14:55:51 +0900

    test(RestaurantList): filterByCategory 메서드 테스트 구현

--- cypress/e2e/restaurantList.cy.ts
+++ cypress/e2e/restaurantList.cy.ts
@@ -11,4 +11,14 @@ describe('RestaurantList 도메인 테스트', () => {
 
     expect(RESTAURANT_LIST.restaurants.length).to.equal(3);
   });
+
+  it('음식점 목록을 사용자가 원하는 카테고리로 필터링 할 수 있다.', () => {
+    const RESTAURANT_1 = new Restaurant({ category: '아시안', name: 'cypress', distance: 30 });
+    const RESTAURANT_2 = new Restaurant({ category: '중식', name: 'apple', distance: 15 });
+    const RESTAURANT_3 = new Restaurant({ category: '아시안', name: 'banana', distance: 5 });
+    const RESTAURANT_LIST = new RestaurantList([RESTAURANT_1, RESTAURANT_2, RESTAURANT_3]);
+    const RESULT = RESTAURANT_LIST.filterByCategory('아시안');
+
+    expect(RESULT.length).to.equal(2);
+  });
 });
