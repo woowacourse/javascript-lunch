0c67f448b91d1b5f7b8bb893ea620634eaf75944
simhorim <hori5020@gmail.com>
2024-03-06 15:40:50 +0900

    test(RestaurantList): getSortedByDistance 메서드 기능 테스트

--- cypress/e2e/restaurantList.cy.ts
+++ cypress/e2e/restaurantList.cy.ts
@@ -7,6 +7,7 @@ describe('RestaurantList 도메인 테스트', () => {
     const RESTAURANT_2 = new Restaurant({ category: '중식', name: 'apple', distance: 15 });
     const RESTAURANT_3 = new Restaurant({ category: '일식', name: 'banana', distance: 5 });
     const RESTAURANT_LIST = new RestaurantList([RESTAURANT_1, RESTAURANT_2]);
+
     RESTAURANT_LIST.add(RESTAURANT_3);
 
     expect(RESTAURANT_LIST.restaurants.length).to.equal(3);
@@ -18,6 +19,7 @@ describe('RestaurantList 도메인 테스트', () => {
     const RESTAURANT_3 = new Restaurant({ category: '아시안', name: 'banana', distance: 5 });
     const RESTAURANT_LIST = new RestaurantList([RESTAURANT_1, RESTAURANT_2, RESTAURANT_3]);
     const FILTERED_COUNT = 2;
+
     const RESULT = RESTAURANT_LIST.filterByCategory('아시안');
 
     expect(RESULT.length).to.equal(FILTERED_COUNT);
@@ -29,8 +31,21 @@ describe('RestaurantList 도메인 테스트', () => {
       const RESTAURANT_2 = new Restaurant({ category: '중식', name: 'apple', distance: 15 });
       const RESTAURANT_3 = new Restaurant({ category: '아시안', name: 'banana', distance: 5 });
       const RESTAURANT_LIST = new RestaurantList([RESTAURANT_1, RESTAURANT_2, RESTAURANT_3]);
-      const RESULT = RESTAURANT_LIST.getSortedByName();
       const SORTED_RESTAURANT_LIST = new RestaurantList([RESTAURANT_2, RESTAURANT_3, RESTAURANT_1]);
+
+      const RESULT = RESTAURANT_LIST.getSortedByName();
+
+      expect(RESULT).to.deep.equal(SORTED_RESTAURANT_LIST.restaurants);
+    });
+
+    it('음식점 목록을 거리순으로 정렬할 수 있다.', () => {
+      const RESTAURANT_1 = new Restaurant({ category: '아시안', name: 'cypress', distance: 30 });
+      const RESTAURANT_2 = new Restaurant({ category: '중식', name: 'apple', distance: 15 });
+      const RESTAURANT_3 = new Restaurant({ category: '아시안', name: 'banana', distance: 5 });
+      const RESTAURANT_LIST = new RestaurantList([RESTAURANT_1, RESTAURANT_2, RESTAURANT_3]);
+      const SORTED_RESTAURANT_LIST = new RestaurantList([RESTAURANT_3, RESTAURANT_2, RESTAURANT_1]);
+
+      const RESULT = RESTAURANT_LIST.getSortedByDistance();
 
       expect(RESULT).to.deep.equal(SORTED_RESTAURANT_LIST.restaurants);
     });
