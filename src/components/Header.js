fd0c66967d993148ad09b9cc7d71b0736e4b2a89
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-06 19:55:47 +0900

    feat(Header): 버튼 이벤트 핸들러 메서드 구현
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,24 @@ import ICON from '../icons';
 
 export default class Header {
   constructor() {
-    this.render();
+    this.handleButtonClick();
   }
 
   render() {
     /*html*/
     return `
       <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-      <button type="button" class="gnb__button" aria-label="음식점 추가">
-        <img src="${ICON.추가버튼}" alt="음식점 추가">
+      <button type="button" id="gnb__button" class="gnb__button" aria-label="음식점 추가">
+        <img src="${ICON.추가버튼}" alt="음식점 추가" />
+      </button>
     `;
+  }
+
+  handleButtonClick() {
+    document.getElementById('header').addEventListener('click', (e) => {
+      if (e.target.parentNode.id === 'gnb__button') {
+        document.getElementById('restaurant-creation-modal').classList.add('modal--open');
+      }
+    });
   }
 }
