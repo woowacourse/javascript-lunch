e9c1850c97e2ffa011f12e21302ada88892ecfd2
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-06 16:51:37 +0900

    chore: 사진 아이콘 import
    Co-Authored-By: JEON TAEHEON <109535991+brgndyy@users.noreply.github.com>

--- src/components/header/template.ts
+++ src/components/header/template.ts
@@ -1,2 +1,3 @@
-export const template =
-  '<header class="gnb"><h1 class="gnb__title text-title">점심 뭐 먹지</h1><button type="button" class="gnb__button" aria-label="음식점 추가"><img src="./templates/add-button.png" alt="음식점 추가" /></button></header>';
+import imageSrc from "../../../templates/add-button.png";
+
+export const template = `<header class="gnb"><h1 class="gnb__title text-title">점심 뭐 먹지</h1><button type="button" class="gnb__button" aria-label="음식점 추가"><img src="${imageSrc}" alt="음식점 추가" /></button></header>`;
