af57558031a040634c1f8c31ef9f3c0c0d285300
soosoo22 <lllllh00@naver.com>
2024-03-07 13:34:59 +0900

    feat: 맛집 리스트 추가 기능
    Co-authored-by: JinHo Kim <jinhokim98@users.noreply.github.com>

--- src/matzipApp.ts
+++ src/matzipApp.ts
@@ -3,7 +3,7 @@ import matzipList from './mock/restaurants';
 import DOM from './utils/DOM';
 import { FilterChangeEvent } from './components/FilterContainer';
 import Restaurant from './components/Restaurant';
-import { CategoryType, SortType } from './types';
+import { CategoryType, SortType, Restaurant as RestaurantType } from './types';
 
 const { $, $$ } = DOM;
 
@@ -13,11 +13,15 @@ const matzipApp = {
     this.initList(matzip);
     this.openModal();
     this.listenCategoryChange(matzip);
+    this.listenRestaurantAdd(matzip);
   },
 
   initList(matzip: Matzip) {
     document.addEventListener('DOMContentLoaded', () => {
-      matzip.getRestaurants().forEach((restaurant) => {
+      const initSort = $('#sorting-filter') as HTMLSelectElement;
+      const sortBy = initSort.options[initSort.selectedIndex].value;
+
+      matzip.filterAndSort('전체', sortBy as SortType).forEach((restaurant) => {
         $('.restaurant-list-container')?.appendChild(new Restaurant(restaurant));
       });
     });
@@ -37,6 +41,34 @@ const matzipApp = {
       restaurants.forEach((restaurant) => {
         $('.restaurant-list-container')?.appendChild(new Restaurant(restaurant));
       });
+    });
+  },
+
+  listenRestaurantAdd(matzip: Matzip) {
+    $('#restaurant-form')?.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const fieldValues = Array.from($$('.form-item')).map((item) => {
+        const field = item.children[1];
+        if (field instanceof HTMLSelectElement) {
+          return field.options[field.selectedIndex].value;
+        }
+        if (field instanceof HTMLInputElement) {
+          return field.value;
+        }
+        if (field instanceof HTMLTextAreaElement) {
+          return field.value;
+        }
+      });
+      const newRestaurant: RestaurantType = {
+        category: fieldValues[0] as CategoryType,
+        name: fieldValues[1] as string,
+        distance: Number(fieldValues[2]),
+        introduction: fieldValues[3],
+        link: fieldValues[4],
+      };
+      matzip.add(newRestaurant);
+      $('.modal')?.classList.remove('modal--open');
+      $('.restaurant-list-container')?.appendChild(new Restaurant(newRestaurant));
     });
   },
 
