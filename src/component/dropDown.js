b1286d5ecc6945a848b86015beafc5a884e7988c
jaeml06 <jaeml0630@gmail.com>
2024-03-07 23:57:27 +0900

    fix: 매개변수 변경
    parent 삭제
    required 추가
    innerHTML에서 createElement로 구현
    
    Co-Authored-By: pakxe <pigkill40@naver.com>

--- src/component/dropDown.js
+++ src/component/dropDown.js
@@ -1,17 +1,33 @@
-import { $ } from "../utils/selector.js";
+import { $ } from '../utils/selector.js';
 
-function createDropDown({ id, callback, options, className, parent }) {
-  const element = render({options, id, className});
-  $(parent).innerHTML += element;
-  $(`#${id}`).addEventListener('change', callback);   
+function createDropDown({ id, callback, options, className, required }) {
+  const dropdown = render({ options, id, className, required });
+
+  if (!callback) return dropdown;
+
+  dropdown.addEventListener('change', () => {
+    const selectedOption = dropdown.value; // 선택된 옵션 값
+    callback(selectedOption); // 콜백 함수 호출
+  });
+
+  return dropdown;
 }
 
 // REFACTOR: createElement로 수정해야한다.
-function render({options, id, className}) {
-  return /*html*/ `
-    <select id=${id} class=${className}>
-      ${options.map((option) => `<option value=${option}>${option}</option>`)}
-    </select>`;
+function render({ options, id, className, required }) {
+  const select = document.createElement('select');
+  select.id = id;
+  select.className = className;
+  select.required = required;
+
+  options.forEach((optionValue) => {
+    const option = document.createElement('option');
+    option.value = optionValue;
+    option.textContent = optionValue;
+    select.appendChild(option);
+  });
+
+  return select;
 }
 
-export {createDropDown} ;
+export { createDropDown };
