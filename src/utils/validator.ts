8e32a3debcd6041cb1749326b7851b976ffc4f32
hanyoung <phanyoung@naver.com>
2024-03-07 11:14:38 +0900

    refactor(validator): validator 를 js 에서 ts 로 변경
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,9 +1,9 @@
-export const validateInRange = (min, max, value) => {
+export const validateInRange = (min: number, max: number, value: number) => {
   if (value < min || value > max)
     throw new Error(`값이 ${min}이상 ${max}이하여야 합니다.`);
 };
 
-export const validateNumberBelow = (max, value) => {
+export const validateNumberBelow = (max: number, value: number) => {
   if (value > max) {
     throw new Error(`값이 ${max}이하여야 합니다.`);
   }
