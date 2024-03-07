150667a00ee06d5bcc41b2e9d0bd76f7b57fbba0
simhorim <hori5020@gmail.com>
2024-03-07 15:52:00 +0900

    chore: js lint ignorePatterns 적용

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -1,9 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const getSelectOption = selectOption => {
   return `<option value=${selectOption.value}>${selectOption.text}</option>`;
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const SelectBoxComponent = (select, options) => {
   return `<select name=${select.name} id=${select.id} class="restaurant-filter">
         ${options.map(option => getSelectOption(option)).join('')}
