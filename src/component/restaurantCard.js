98c60f3c9bb443ac8a8aff127e76549ee7cc7f49
jaeml06 <jaeml0630@gmail.com>
2024-03-07 23:55:51 +0900

    refactor: airbnb 스타일 준수
    Co-Authored-By: pakxe <pigkill40@naver.com>

--- src/component/restaurantCard.js
+++ src/component/restaurantCard.js
@@ -1,4 +1,4 @@
-import { KOREAN_CATEGORY } from "../constant/koreanCategory";
+import { KOREAN_CATEGORY } from '../constant/cons';
 
 function createRestaurantCard(restaurant) {
   const restaurantCard = render(restaurant);
@@ -7,34 +7,34 @@ function createRestaurantCard(restaurant) {
 }
 
 function render({ category, name, walkingTime, description = '' }) {
-  const categoryDiv = document.createElement("div");
-  categoryDiv.className = "restaurant__category";
+  const categoryDiv = document.createElement('div');
+  categoryDiv.className = 'restaurant__category';
 
-  categoryDiv.append(createCategoryImage(), createInfoDiv())
+  categoryDiv.append(createCategoryImage(), createInfoDiv());
 
   return categoryDiv;
 }
 
-function createInfoDiv({name, walkingTime, description = ''}) {
-  const infoDiv = document.createElement("div");
-  infoDiv.className = "restaurant__info";
-  const restaurantName = document.createElement("h3");
-  restaurantName.className = "restaurant__name text-subtitle";
+function createInfoDiv({ name, walkingTime, description = '' }) {
+  const infoDiv = document.createElement('div');
+  infoDiv.className = 'restaurant__info';
+  const restaurantName = document.createElement('h3');
+  restaurantName.className = 'restaurant__name text-subtitle';
   restaurantName.textContent = name;
 
-  const restaurantDistance = document.createElement("span");
-  restaurantDistance.className = "restaurant__distance text-body";
+  const restaurantDistance = document.createElement('span');
+  restaurantDistance.className = 'restaurant__distance text-body';
   restaurantDistance.textContent = `캠퍼스부터 ${walkingTime}분 내`;
 
-  const restaurantDescription = document.createElement("p");
-  restaurantDescription.className = "restaurant__description text-body";
+  const restaurantDescription = document.createElement('p');
+  restaurantDescription.className = 'restaurant__description text-body';
   restaurantDescription.textContent = description;
   return infoDiv;
 }
 
 function createCategoryImage(category) {
-  const categoryImg = document.createElement("img");
-  categoryImg.src = `../../templates/category-${KOREAN_CATEGORY.category}.png`;
+  const categoryImg = document.createElement('img');
+  categoryImg.src = `./category-${KOREAN_CATEGORY.category}.png`;
   categoryImg.alt = category;
-  categoryImg.className = "category-icon";
+  categoryImg.className = 'category-icon';
 }
