170321245e6045a2b192e0b3dc49f756da70033e
hwinkr <dnddl8280@naver.com>
2024-03-07 17:13:46 +0900

    feat(RestaurantItem): 레스토랑 리스트 아이템 구현
    Co-authored-by: dle234 <kky1373@gmail.com>

---
+++ src/components/RestaurantItem.js
@@ -0,0 +1,61 @@
+import {
+  category_asian,
+  category_chinese,
+  category_etc,
+  category_japanese,
+  category_korean,
+  category_western,
+} from "../assets/index.js";
+import BaseComponent from "./BaseComponent.js";
+
+class RestaurantItem extends BaseComponent {
+  constructor() {
+    super();
+  }
+
+  #categoryToImg(category) {
+    switch (category) {
+      case "한식":
+        return category_korean;
+      case "중식":
+        return category_chinese;
+      case "아시안":
+        return category_asian;
+      case "일식":
+        return category_japanese;
+      case "양식":
+        return category_western;
+      case "기타":
+        return category_etc;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    const category = this.getAttribute("category");
+    const name = this.getAttribute("name");
+    const distance = this.getAttribute("distance");
+    const description = this.getAttribute("description");
+    const img = this.#categoryToImg(category);
+
+    this.innerHTML = `
+    <li class="restaurant">
+        <div class="restaurant__category">
+            <img src=${img} alt="한식" class="category-icon">
+        </div>
+        <div class="restaurant__info">
+            <h3 class="restaurant__name text-subtitle">${name}</h3>
+            <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+            ${
+              description
+                ? `<p class="restaurant__description text-body">${description}</p>`
+                : ""
+            }
+        </div>
+    </li>
+    `;
+  }
+}
+
+customElements.define("restaurant-item", RestaurantItem);
