d02f73d8c9f05aaf2f32d60bba8fc38f7d9e5c48
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-07 11:24:05 +0900

    feat(Restaurant): 음식점 컴포넌트 분리
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

---
+++ src/components/Restaurant.js
@@ -0,0 +1,19 @@
+import ICON from '../icons';
+
+export default class Restaurant {
+  render({ category, name, walkingTimeFromCampus, description }) {
+    /*html*/
+    return `
+    <li class="restaurant">
+      <div class="restaurant__category">
+        <img src="${ICON[category]}" alt="${category}" class="category-icon">
+      </div>
+      <div class="restaurant__info">
+        <h3 class="restaurant__name text-subtitle">${name}</h3>
+        <span class="restaurant__distance text-body">캠퍼스부터 ${walkingTimeFromCampus}분 내</span>
+        <p class="restaurant__description text-body">${description}</p>
+      </div>
+    </li>
+    `;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import Restaurants from './domains/Restaurants';
+
 import Header from './components/Header';
 import Main from './components/Main';
-import RestaurantCreationModal from './components/RestaurantCreationModal';
 import Select from './components/Select';
-
+import Restaurant from './components/Restaurant';
+import RestaurantCreationModal from './components/RestaurantCreationModal';
 import './styles/index.css';
 
 const selectDatas = [
@@ -29,14 +31,22 @@ const selectDatas = [
   },
 ];
 
+// domain
+const restaurants = new Restaurants(localStorage);
+
+// components
 const header = new Header();
 const main = new Main();
 const sortingSelect = new Select(selectDatas[0]);
 const categorySelect = new Select(selectDatas[1]);
+const restaurant = new Restaurant();
 const modal = new RestaurantCreationModal();
 
 document.getElementById('header').innerHTML = header.render();
 document.getElementById('main').innerHTML = main.render();
 document.getElementById('restaurant-filter-container').appendChild(sortingSelect.render());
 document.getElementById('restaurant-filter-container').appendChild(categorySelect.render());
+restaurants.getStorageData.forEach((restaurantData) => {
+  document.getElementById('restaurant-list').innerHTML += restaurant.render(restaurantData);
+});
 document.getElementById('restaurant-creation-modal').innerHTML = modal.render();
