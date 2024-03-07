0cc7ea7005c87f57ab844abea8c73100d36985c4
bmj0053 <bmj05378@gmail.com>
2024-03-07 15:32:49 +0900

    feat: 더미 데이터 추가
    Co-authored-by: Todari <rhymint@gmail.com>

---
+++ src/constants/dummy.ts
@@ -0,0 +1,37 @@
+const DUMMY = [
+  {
+    category: '한식',
+    name: '피양콩할머니',
+    distance: 10,
+    createdAt: new Date(2024, 1, 2),
+    description: '',
+  },
+  {
+    category: '중식',
+    name: '친친',
+    distance: 10,
+    createdAt: new Date(2024, 1, 3),
+    description: 'Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과 정성으로 정통 어쩌구 저쩌구',
+  },
+  {
+    category: '일식',
+    name: '잇쇼우',
+    distance: 5,
+    createdAt: new Date(2024, 1, 4),
+    description: '잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은 정성을 이길 수 없다!',
+  },
+  {
+    category: '양식',
+    name: '이태리키친',
+    distance: 20,
+    createdAt: new Date(2024, 1, 5),
+  },
+  {
+    category: '아시안',
+    name: '호아빈 삼성점',
+    distance: 15,
+    createdAt: new Date(2024, 1, 6),
+  },
+];
+
+export default DUMMY;
