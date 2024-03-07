bc93ca675314f5e11da577a708530d597d68abbb
simhorim <hori5020@gmail.com>
2024-03-07 11:56:23 +0900

    feat(Restaurant): 이미지 파일 처리

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,8 +1,25 @@
+import korean from '../../templates/category-korean.png';
+import chinese from '../../templates/category-chinese.png';
+import japanese from '../../templates/category-japanese.png';
+import western from '../../templates/category-western.png';
+import asian from '../../templates/category-asian.png';
+import etc from '../../templates/category-etc.png';
+
+const CATEGORY_TRANSLATION = {
+  한식: korean,
+  중식: chinese,
+  일식: japanese,
+  양식: western,
+  아시안: asian,
+  기타: etc,
+};
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const RestaurantComponent = information => {
   return `<li class="restaurant">
     <div class="restaurant__category">
-      <img src="./category-{}.png" alt="${information.category}" class="category-icon" />
+    
+      <img src=${CATEGORY_TRANSLATION[information.category]} alt=${information.category} class="category-icon" />
     </div>
     <div class="restaurant__info">
       <h3 class="restaurant__name text-subtitle">${information.name}</h3>
