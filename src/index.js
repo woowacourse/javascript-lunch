0d168a1e5e322d573f6569392fccf715ac9647dd
hwinkr <dnddl8280@naver.com>
2024-03-07 17:17:39 +0900

    feat(index): 엡 시작을 위한 모듈 import
    Co-authored-by: dle234 <kky1373@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "../templates/style.css";
+import "./components/Header.js";
+import "./components/OptionSelector.js";
+import "./components/RestaurantList.js";
+import "./components/RestaurantItem.js";
+import "./components/RestaurantAddForm.js";
+import "./components/MenuApp.js";
