c94f7932a617cbe456a2b99f3743cfa01ee1618a
greetings1012 <ahnqwerty@gmail.com>
2024-03-07 11:18:55 +0900

    feat: SortType, CompareFunction 타입 추가

--- src/constants/Type.ts
+++ src/constants/Type.ts
@@ -1,4 +1,8 @@
+import Restaurant from '../domain/Restaurant';
+
 type Category = '한식' | '중식' | '일식' | '양식' | '아시안' | '기타';
 type MinutesWalk = 5 | 10 | 15 | 20 | 30;
+type SortType = '이름순' | '거리순';
+type CompareFunction = (a: Restaurant, b: Restaurant) => number;
 
-export { Category, MinutesWalk };
+export { Category, MinutesWalk, SortType, CompareFunction };
