2a6cbf978fcb4680956fd9f222825b39fe150987
hanyoung <phanyoung@naver.com>
2024-03-07 16:11:16 +0900

    chore(Filter): 필터 및 정렬 관련 상수 추가
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/constants/filter.ts
+++ src/constants/filter.ts
@@ -1,14 +1,39 @@
-export const CATEGORY_FILTER = {
-  all: "전체",
-  korean: "한식",
-  chinese: "중식",
-  japanese: "일식",
-  western: "양식",
-  asian: "아시안",
-  etc: "기타",
+import { CategoryFilter, SortFilter } from "../types/Filter";
+
+export const CATEGORY_FILTER: {
+  [key in CategoryFilter]: CategoryFilter;
+} = {
+  all: "all",
+  korean: "korean",
+  chinese: "chinese",
+  japanese: "japanese",
+  western: "western",
+  asian: "asian",
+  etc: "etc",
 };
 
-export const SORT_FILTER = {
-  name: "이름순",
-  timeToReach: "거리순",
+export const KOREAN_CATEGORY_FILTER: {
+  [key: string]: string;
+} = {
+  [CATEGORY_FILTER.all]: "전체",
+  [CATEGORY_FILTER.korean]: "한식",
+  [CATEGORY_FILTER.chinese]: "중식",
+  [CATEGORY_FILTER.japanese]: "일식",
+  [CATEGORY_FILTER.western]: "양식",
+  [CATEGORY_FILTER.asian]: "아시안",
+  [CATEGORY_FILTER.etc]: "기타",
+};
+
+export const SORT_FILTER: {
+  [key in SortFilter]: SortFilter;
+} = {
+  name: "name",
+  timeToReach: "timeToReach",
+};
+
+export const KOREAN_SORT_FILTER: {
+  [key: number]: string;
+} = {
+  [SORT_FILTER.name]: "이름순",
+  [SORT_FILTER.timeToReach]: "거리순",
 };
---
+++ src/types/Filter.ts
@@ -0,0 +1,5 @@
+import { Category } from "./Category";
+
+export type CategoryFilter = Category | "all";
+
+export type SortFilter = "name" | "timeToReach";
