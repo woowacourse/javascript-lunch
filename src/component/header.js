a3a7fe46422e15afd0baebe344de39ab19de93e2
jaeml06 <jaeml0630@gmail.com>
2024-03-08 00:01:43 +0900

    fix:  매개변수 변경
    parent 삭제
    restaurantManager 추가
    
    Co-Authored-By: pakxe <pigkill40@naver.com>

--- src/component/header.js
+++ src/component/header.js
@@ -1,51 +1,58 @@
-import { $ } from '../utils/selector.js'
+import { $ } from '../utils/selector.js';
+import createNewRestaurantModal from '../web/modal/addRestaurantModal.js';
+import modal from './modal.js';
 
-function createHeader({parent, left, right}){
-  const header = $(parent);
+function createHeader({ className, left, right, restaurantManager }) {
+  const header = document.createElement('header');
+  header.className = className;
 
-  const leftElement = item[left];
-  const rightElement = item[right];
-
-  eventHandler[left]?.();
-  eventHandler[right]?.();
+  const leftElement = item[left]();
+  const rightElement = item[right](restaurantManager);
 
   header.append(leftElement, rightElement);
+  return header;
 }
 
 // REFACTOR: 앱 이름 상수로 분리
 const item = {
   logo() {
-    const h1 = document.createElement("h1");
-    h1.className = "gnb__title text-title";
-    h1.textContent = "점심 뭐 먹지";
-    return h1
+    const h1 = document.createElement('h1');
+    h1.className = 'gnb__title text-title';
+    h1.textContent = '점심 뭐 먹지';
+    return h1;
   },
 
-  add() {
-    const button = document.createElement("button");
-  
-    button.type = "button";
-    button.className = "gnb__button";
-    button.setAttribute("aria-label", "음식점 추가");
-  
-    const img = document.createElement("img");
-  
-    img.src = "./add-button.png";
-    img.alt = "음식점 추가";
-  
+  add(restaurantManager) {
+    const button = document.createElement('button');
+
+    button.type = 'button';
+    button.className = 'gnb__button';
+    button.setAttribute('aria-label', '음식점 추가');
+
+    const img = document.createElement('img');
+
+    img.src = './add-button.png';
+    img.alt = '음식점 추가';
+
     button.appendChild(img);
-    
+    eventHandler.add(button, restaurantManager);
+
     return button;
-  }
+  },
 };
 
 const eventHandler = {
-  add(element) {
+  add(element, restaurantManager) {
     element.addEventListener('click', () => {
-      // 모달을 연다.
-      // 모달안에 어떤 내용을 렌더할지도 이곳에 적는다.
-    })
-  }
-}
+      const newRestaurantModalElement =
+        createNewRestaurantModal(restaurantManager);
+      const newRestaurantModal = modal.create(
+        'modal--open',
+        newRestaurantModalElement
+      );
+      document.body.append(newRestaurantModal);
+    });
+  },
+};
 
 export default createHeader;
---
+++ src/web/control.js
@@ -0,0 +1,129 @@
+import { createDropDown } from '../component/dropDown';
+import createHeader from '../component/header';
+import {
+  KOREAN_CATEGORY,
+  categoryFilterList,
+  sortingFilterLsit,
+} from '../constant/cons';
+import { RestaurantManager } from '../domain/RestaurantManager';
+
+export const set = {
+  start() {
+    const data = JSON.parse(localStorage.getItem('restaurants')) || [];
+    const restaurantManager = new RestaurantManager(data);
+
+    this.setMainPage(restaurantManager);
+  },
+
+  setMainPage(restaurantManager) {
+    document.body.insertAdjacentElement(
+      'afterbegin',
+      createHeader({
+        className: 'gnb',
+        left: 'logo',
+        right: 'add',
+        restaurantManager,
+      })
+    );
+
+    const restaurantFilterContainer = document.querySelector(
+      '.restaurant-filter-container'
+    );
+
+    restaurantFilterContainer.appendChild(
+      createDropDown({
+        name: 'categoty',
+        id: 'category-filter',
+        options: categoryFilterList,
+        className: 'restaurant-filter',
+        callback: (category) => {
+          if (category === '전체')
+            return this.updateRestaurantList(
+              restaurantManager.getRestaurants()
+            );
+          this.updateRestaurantList(
+            restaurantManager.filteredRestaurants(category)
+          );
+        },
+      })
+    );
+
+    restaurantFilterContainer.appendChild(
+      createDropDown({
+        name: 'sorting',
+        id: 'sorting-filter',
+        className: 'restaurant-filter',
+        options: sortingFilterLsit,
+        callback: (category) => {
+          if (category === '이름순')
+            this.updateRestaurantList(restaurantManager.sortByAscendingName());
+          if (category === '거리순')
+            this.updateRestaurantList(
+              restaurantManager.sortByAscendingWalkingTime()
+            );
+        },
+      })
+    );
+
+    this.updateRestaurantList(restaurantManager.sortByAscendingName());
+  },
+
+  updateRestaurantList(restaurants) {
+    const restaurantListContainer = document.querySelector(
+      '.restaurant-list-container'
+    );
+    restaurantListContainer.replaceChildren();
+    const restaurantList = document.createElement('ul');
+    restaurantList.className = 'restaurant-list';
+
+    restaurants.map((restaurant) => {
+      const listItem = document.createElement('li');
+      listItem.className = 'restaurant';
+
+      const categoryDiv = document.createElement('div');
+      categoryDiv.className = 'restaurant__category';
+
+      // <img> 요소 생성 및 설정
+      const categoryImg = document.createElement('img');
+      categoryImg.src = `./category-${
+        KOREAN_CATEGORY[restaurant.category]
+      }.png`;
+      categoryImg.alt = restaurant.category;
+      categoryImg.className = 'category-icon';
+
+      // <img>를 카테고리 <div>에 추가
+      categoryDiv.append(categoryImg);
+
+      // 정보 <div> 생성
+      const infoDiv = document.createElement('div');
+      infoDiv.className = 'restaurant__info';
+
+      // <h3> 요소 생성 및 설정
+      const restaurantName = document.createElement('h3');
+      restaurantName.className = 'restaurant__name text-subtitle';
+      restaurantName.textContent = restaurant.name;
+
+      // 거리 <span> 생성 및 설정
+      const restaurantDistance = document.createElement('span');
+      restaurantDistance.className = 'restaurant__distance text-body';
+      restaurantDistance.textContent = `캠퍼스부터 ${restaurant.walkingTime}분 내`;
+
+      // 설명 <p> 생성 및 설정
+      const restaurantDescription = document.createElement('p');
+      restaurantDescription.className = 'restaurant__description text-body';
+      restaurantDescription.textContent = restaurant?.description;
+
+      // 요소들을 infoDiv에 추가
+      infoDiv.append(restaurantName);
+      infoDiv.append(restaurantDistance);
+      infoDiv.append(restaurantDescription);
+
+      // 카테고리 <div>와 정보 <div>를 최상위 <li>에 추가
+      listItem.append(categoryDiv);
+      listItem.append(infoDiv);
+      restaurantList.append(listItem);
+    });
+
+    restaurantListContainer.appendChild(restaurantList);
+  },
+};
