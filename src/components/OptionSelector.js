4db9693c7c2a70262051e2726da534f5d48d80a2
hwinkr <dnddl8280@naver.com>
2024-03-07 17:05:16 +0900

    feat(OptionSelector): 레스토랑 리스트 필터링 옵션 구현
    - 카테고리 별 필터링, 이름 & 거리순 정렬
    
    Co-authored-by: dle234 <kky1373@gmail.com>

---
+++ src/components/OptionSelector.js
@@ -0,0 +1,34 @@
+import BaseComponent from "./BaseComponent.js";
+
+class OptionSelector extends BaseComponent {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const optionsArray = this.getAttribute("options").split(",");
+
+    this.innerHTML = `
+      <select name="category" id="category-filter" class="restaurant-filter">
+        ${optionsArray.map((option) => {
+          return `<option value=${option}>${option}</option>;`;
+        })}
+      </select> 
+    `;
+  }
+
+  setEvent() {
+    const selectType = this.getAttribute("type");
+
+    this.addEventListener("change", (e) => {
+      const selectedValue = e.target.value;
+
+      this.emitEvent("select-change", {
+        type: selectType,
+        option: selectedValue,
+      });
+    });
+  }
+}
+
+customElements.define("option-selector", OptionSelector);
