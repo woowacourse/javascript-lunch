79c4f45ebcb23529a236640155f7283d9f6e14fb
simhorim <hori5020@gmail.com>
2024-03-07 11:56:01 +0900

    feat(index): index.html에 SelectBoxComponent 삽입

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import '../templates/style.css';
 import Restaurant from './Restaurant';
 import RestaurantComponent from './components/Restaurant';
+import SelectBoxComponent from './components/SelectBox';
 
 const $restaurantList = document.querySelector('.restaurant-list');
 
@@ -28,3 +29,27 @@ const restaurants = [
 restaurants.forEach(element => {
   $restaurantList.innerHTML += RestaurantComponent(element.information);
 });
+
+const $restaurantFilterContainer = document.querySelector('.restaurant-filter-container');
+
+const category = [
+  { value: '전체', text: '전체' },
+  { value: '한식', text: '한식' },
+  { value: '중식', text: '중식' },
+  { value: '일식', text: '일식' },
+  { value: '양식', text: '양식' },
+  { value: '아시안', text: '아시안' },
+  { value: '기타', text: '기타' },
+];
+
+const categoryAttribute = { name: 'category', id: 'category-filter' };
+
+const sorting = [
+  { value: 'name', text: '이름순' },
+  { value: 'distance', text: '거리순' },
+];
+
+const sortingAttribute = { name: 'sorting', id: 'sorting-filter' };
+
+$restaurantFilterContainer.innerHTML += SelectBoxComponent(categoryAttribute, category);
+$restaurantFilterContainer.innerHTML += SelectBoxComponent(sortingAttribute, sorting);
