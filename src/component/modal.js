1e46823c01f389702e91b627038dcfaf85b1722f
jaeml06 <jaeml0630@gmail.com>
2024-03-07 23:43:43 +0900

    feat: modal 컴포넌트 구현
    Co-Authored-By: pakxe <pigkill40@naver.com>

--- src/component/modal.js
+++ src/component/modal.js
@@ -1,17 +1,33 @@
-function createModal() {
-  const backdrop =     
-}
+import { $ } from '../utils/selector';
 
-function createBackdrop () {
+const modal = {
+  create(className, content) {
+    const modal = document.createElement('div');
+    modal.className = className;
+
+    const backdrop = createBackdrop();
+    modal.append(backdrop, content);
+
+    return modal;
+  },
+
+  createContainer() {
+    const container = document.createElement('div');
+    container.className = 'modal-container';
+
+    return container;
+  },
+
+  remove(className) {
+    $(`.${className}`).remove();
+  },
+};
+
+function createBackdrop() {
   const backdrop = document.createElement('div');
-  backdrop.className = "modal-backdrop";
+  backdrop.className = 'modal-backdrop';
 
   return backdrop;
 }
 
-export function createModalContainer() {
-  const container = document.createElement('div');
-  container.className = "modal-container";
-
-  return container;
-}
+export default modal;
