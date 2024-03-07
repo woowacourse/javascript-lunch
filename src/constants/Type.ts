820b487214d76080ee449a7cab5f5c91edd6658f
greetings1012 <ahnqwerty@gmail.com>
2024-03-07 18:29:01 +0900

    refactor: category와 minutesWalk를 문자열로 받아오기 위해 코드 수정

--- src/constants/Type.ts
+++ src/constants/Type.ts
@@ -1,8 +1,9 @@
+import { TYPE_SETTING } from './setting';
 import Restaurant from '../domain/Restaurant';
 
-type Category = '한식' | '중식' | '일식' | '양식' | '아시안' | '기타';
-type MinutesWalk = 5 | 10 | 15 | 20 | 30;
-type SortType = '이름순' | '거리순';
+type Category = (typeof TYPE_SETTING.category)[number];
+type MinutesWalk = (typeof TYPE_SETTING.minutesWalk)[number];
+type SortType = (typeof TYPE_SETTING.sortType)[number];
 type CompareFunction = (a: Restaurant, b: Restaurant) => number;
 
 export { Category, MinutesWalk, SortType, CompareFunction };
---
+++ src/constants/setting.ts
@@ -0,0 +1,7 @@
+const TYPE_SETTING = {
+  category: ['한식', '중식', '일식', '양식', '아시안', '기타'] as const,
+  minutesWalk: [5, 10, 15, 20, 30] as const,
+  sortType: ['이름순', '거리순'] as const,
+};
+
+export { TYPE_SETTING };
