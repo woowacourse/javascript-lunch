d87d6fdf2db10d61c428788db44c16b57b5ad2ed
hwinkr <dnddl8280@naver.com>
2024-03-07 16:58:55 +0900

    chore(asset): 이미지 추가
    Co-authored-by: dle234 <kky1373@gmail.com>

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+export { default as add_button } from "../../templates/add-button.png";
+export { default as category_asian } from "../../templates/category-asian.png";
+export { default as category_chinese } from "../../templates/category-chinese.png";
+export { default as category_etc } from "../../templates/category-etc.png";
+export { default as category_japanese } from "../../templates/category-japanese.png";
+export { default as category_korean } from "../../templates/category-korean.png";
+export { default as category_western } from "../../templates/category-western.png";
