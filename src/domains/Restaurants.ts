ec5641bd731f0c85d72751756271ac643ff982f9
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-07 11:02:28 +0900

    refactor(Restaurants): 외부에서 storage를 주입 받아서 사용하도록 변경
    - LocalStorage 사용
    
    Co-authored-by: Yoonkyoungme <dev.yoonkyoung@gmail.com>

--- src/domains/Restaurants.ts
+++ src/domains/Restaurants.ts
@@ -1,29 +1,42 @@
-class Restaurants implements RestaurantsInterface {
-  private restaurants: Restaurant[];
+import initialData from '../data/initialData.json';
 
-  constructor() {
-    this.restaurants = [];
+class Restaurants implements RestaurantsInterface {
+  private storage;
+
+  constructor(storage: Storage) {
+    this.storage = storage;
+    this.initStorage();
+  }
+
+  initStorage() {
+    if (!this.storage.getItem('restaurants')) {
+      this.storage.setItem('restaurants', JSON.stringify(initialData));
+    }
+  }
+
+  get getStorageData() {
+    return JSON.parse(this.storage.getItem('restaurants') || '{}');
   }
 
   filterByCategory(category: Category) {
-    return this.restaurants.filter((restaurant) => restaurant.category === category);
+    return this.getStorageData.filter((restaurant: Restaurant) => restaurant.category === category);
   }
 
   addRestaurant(restaurant: Restaurant) {
     // TODO: 이 값이 있는지 없는지 -> set 고려
 
-    this.restaurants.push(restaurant);
+    this.storage.setItem('restaurants', JSON.stringify(restaurant));
   }
 
   orderByDistance() {
-    return this.restaurants.toSorted((prev: Restaurant, next: Restaurant) => {
+    return this.getStorageData.toSorted((prev: Restaurant, next: Restaurant) => {
       const compareWalkingTime = prev.walkingTimeFromCampus - next.walkingTimeFromCampus;
       return compareWalkingTime || prev.name.localeCompare(next.name);
     });
   }
 
   orderByName() {
-    return this.restaurants.toSorted((prev: Restaurant, next: Restaurant) =>
+    return this.getStorageData.toSorted((prev: Restaurant, next: Restaurant) =>
       prev.name.localeCompare(next.name),
     );
   }
