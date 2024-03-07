4d30ee4c2f56c8ef00ae34ef39105dc59c2046df
Kim Da Eun <midekuna@gmail.com>
2024-03-07 18:24:52 +0900

    feat: 음식점 렌더링 컴포넌트
    Co-Authored-By: 00kang <70834044+00kang@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import WebController from './service/WebController';
 // components
 import GNB from './view/components/GNB';
 import Modal from './view/components/Modal';
 import Select from './view/components/Select';
 import RestaurantForm from './view/components/RestaurantForm';
+import RestaurantItem from './view/components/RestaurantItem';
 
 // styles
 import './view/styles/global.css';
@@ -10,17 +12,22 @@ import './view/styles/GNB.css';
 import './view/styles/Modal.css';
 import './view/styles/Select.css';
 import './view/styles/RestaurantForm.css';
+import './view/styles/RestaurantList.css';
 
 // imgs
 import './view/imgs/add-button.png';
-import WebController from './service/WebController';
-
-// Modal();
+import './view/imgs/category-korean.png';
+import './view/imgs/category-asian.png';
+import './view/imgs/category-chinese.png';
+import './view/imgs/category-japanese.png';
+import './view/imgs/category-western.png';
+import './view/imgs/category-etc.png';
 
 window.customElements.define('app-gnb', GNB);
 window.customElements.define('app-modal', Modal);
 window.customElements.define('app-select', Select, { extends: 'select' });
 window.customElements.define('app-restaurant-form', RestaurantForm, { extends: 'form' });
+window.customElements.define('app-restaurant-item', RestaurantItem, { extends: 'li' });
 
 const controller = new WebController();
 controller.start();
