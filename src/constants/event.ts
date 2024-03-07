ce12ea2ca397bcf402f7f741973040789fb775d0
hanyoung <phanyoung@naver.com>
2024-03-07 21:11:04 +0900

    feat(Modal): 공통으로 사용하는 모달 컴포넌트 추가 - RestaurantHeader 에서 아이콘 클릭 시 모달창 오픈 이벤트 dispatch
    Co-authored-by: pp449 <mma7710@naver.com>

--- src/constants/event.ts
+++ src/constants/event.ts
@@ -1,4 +1,13 @@
-export const EVENT = {
+export const FILTER_EVENT = {
   categoryFilterChanged: "categoryFilterChanged",
   sortFilterChanged: "sortFilterChanged",
+};
+
+export const MODAL_EVENT = {
+  restaurantFormModalAction: "restaurant-form-modal-action",
+};
+
+export const MODAL_EVENT_ACTION = {
+  open: "open",
+  close: "close",
 };
