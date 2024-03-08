48fada427263f04f98ca266e86c605035dcca5ed
jaeml06 <jaeml0630@gmail.com>
2024-03-08 14:57:03 +0900

    fix: 카테고리 선택시 정렬 변경
    Co-Authored-By: pakxe <pigkill40@naver.com>

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -1,18 +1,22 @@
-import { IRestaurant, Category } from "../domain/interface/IRestaurant";
+import { IRestaurant, Category } from '../domain/interface/IRestaurant';
 
 export interface IRestaurantManager {
   add(newRestaurant: IRestaurant): void;
   getRestaurants(): IRestaurant[];
+  getfilterRestaurants(): IRestaurant[];
   sortByAscendingName(): IRestaurant[];
   sortByAscendingWalkingTime(): IRestaurant[];
+  udateFilterRestaurants(): void;
   filteredRestaurants(category: Category): IRestaurant[];
 }
 
 export class RestaurantManager implements IRestaurantManager {
   private restaurants: IRestaurant[];
+  private filterRestaurants: IRestaurant[];
 
   constructor(restaurants: IRestaurant[] = []) {
-    this.restaurants = restaurants;
+    this.restaurants = [...restaurants];
+    this.filterRestaurants = [...restaurants];
   }
 
   add(newRestaurant: IRestaurant): void {
@@ -20,26 +24,37 @@ export class RestaurantManager implements IRestaurantManager {
       ({ name }) => name === newRestaurant.name
     );
     if (hasSameName) {
-      throw new Error("[ERROR]");
+      throw new Error('[ERROR]');
     }
     this.restaurants.push(newRestaurant);
-    localStorage.setItem("restaurants", JSON.stringify(this.restaurants));
+    localStorage.setItem('restaurants', JSON.stringify(this.restaurants));
   }
 
   getRestaurants(): IRestaurant[] {
     return [...this.restaurants];
   }
 
+  getfilterRestaurants(): IRestaurant[] {
+    return [...this.filterRestaurants];
+  }
+
   sortByAscendingName(): IRestaurant[] {
-    return [...this.restaurants].sort((a, b) => {
+    const sortingReataurants = this.filterRestaurants.sort((a, b) => {
       if (a.name < b.name) return -1;
       if (a.name > b.name) return 1;
       return 0;
     });
+
+    this.restaurants.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+    return [...sortingReataurants];
   }
 
   sortByAscendingWalkingTime(): IRestaurant[] {
-    return [...this.restaurants].sort((a, b) => {
+    const sortingReataurants = this.filterRestaurants.sort((a, b) => {
       const aTime = Number(a.walkingTime);
       const bTime = Number(b.walkingTime);
 
@@ -50,11 +65,30 @@ export class RestaurantManager implements IRestaurantManager {
       if (a.name > b.name) return 1;
       return 0;
     });
+
+    this.restaurants.sort((a, b) => {
+      const aTime = Number(a.walkingTime);
+      const bTime = Number(b.walkingTime);
+
+      if (aTime < bTime) return -1;
+      if (aTime > bTime) return 1;
+
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+
+    return sortingReataurants;
+  }
+
+  udateFilterRestaurants() {
+    this.filterRestaurants = [...this.restaurants];
   }
 
   filteredRestaurants(category: Category): IRestaurant[] {
-    return [...this.restaurants].filter(
+    this.filterRestaurants = [...this.restaurants].filter(
       (restaurant) => restaurant.category === category
     );
+    return [...this.filterRestaurants];
   }
 }
