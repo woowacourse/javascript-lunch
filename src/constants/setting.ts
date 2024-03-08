807c6fd0456b19072c6d04d7a2d4114fb3b52693
greetings1012 <ahnqwerty@gmail.com>
2024-03-08 16:11:49 +0900

    refactor: type 이름 명확하게 수정

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -1,13 +1,12 @@
 const TYPE_SETTING = {
   category: ['한식', '중식', '일식', '양식', '아시안', '기타'] as const,
   minutesWalk: [5, 10, 15, 20, 30] as const,
-  sortType: ['이름순', '거리순'] as const,
+  sort: ['이름순', '거리순'] as const,
 };
 
 const ASSETS = {
   imageUrl: {
     버튼_음식점추가: require('/src/assets/image/add-button.png').default,
-
     한식: require('/src/assets/image/category-korean.png').default,
     중식: require('/src/assets/image/category-chinese.png').default,
     일식: require('/src/assets/image/category-japanese.png').default,
