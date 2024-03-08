02c3d91cf048b030bb49013aa73d465a9d26968f
greetings1012 <ahnqwerty@gmail.com>
2024-03-08 09:56:47 +0900

    feat: 이미지 절대 경로 상수화

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -4,4 +4,16 @@ const TYPE_SETTING = {
   sortType: ['이름순', '거리순'] as const,
 };
 
-export { TYPE_SETTING };
+const ASSETS = {
+  imageUrl: {
+    버튼_음식점추가: require('/src/assets/image/add-button.png').default,
+
+    한식: require('/src/assets/image/category-korean.png').default,
+    중식: require('/src/assets/image/category-chinese.png').default,
+    일식: require('/src/assets/image/category-japanese.png').default,
+    양식: require('/src/assets/image/category-western.png').default,
+    아시안: require('/src/assets/image/category-asian.png').default,
+    기타: require('/src/assets/image/category-etc.png').default,
+  },
+};
+export { TYPE_SETTING, ASSETS };
