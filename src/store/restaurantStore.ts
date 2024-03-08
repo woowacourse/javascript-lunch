294e2bd8866c70a9ef11927cb607a4d2a83e9de5
hanyoung <phanyoung@naver.com>
2024-03-08 15:36:07 +0900

    feat(store): 로컬 스토리지를 이용하여 식당 데이터를 저장/조회하는 기능 추가
    Co-authored-by: pp449 <mma7710@naver.com>

---
+++ src/store/restaurantStore.ts
@@ -0,0 +1,30 @@
+import Restaurant, { RestaurantInfo } from "../domain/Restaurant";
+import Restaurants from "../domain/Restaurants";
+import localStore from "./localStorageStore";
+
+const restaurantStore = {
+  getRestaurants(): Restaurants {
+    const restaurantInfos = localStore.getParsedItem("restaurants");
+
+    if (!restaurantInfos) {
+      return new Restaurants([]);
+    }
+
+    try {
+      const restaurantsArray = (restaurantInfos as []).map(
+        (restaurant) => new Restaurant(restaurant)
+      );
+      return new Restaurants(restaurantsArray);
+    } catch {
+      localStore.reset();
+      alert("데이터 건들지 마세요!");
+      return this.getRestaurants();
+    }
+  },
+
+  setRestaurnats(restaurants: Restaurants) {
+    localStore.setItem("restaurants", restaurants.getDetails());
+  },
+};
+
+export default restaurantStore;
