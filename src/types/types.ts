466839860766248d4f8e9674c666e297b22ac057
vi-wolhwa <cerezo00@naver.com>
2024-03-08 14:35:48 +0900

    refactor: 분리된 타입 파일 통합
    Co-authored-by: river <0jenn0@users.noreply.github.com>

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,20 @@
+export type Restaurant = {
+  category: Category;
+  name: string;
+  distance: Distance;
+  description: string;
+  link: string;
+};
+
+const Distance = {
+  Five: 5,
+  Ten: 10,
+  Fifteen: 15,
+  Twenty: 20,
+  Thirty: 30
+} as const;
+export type Distance = (typeof Distance)[keyof typeof Distance];
+
 export const CATEGORY = {
   all: '전체',
   korean: '한식',
@@ -5,12 +22,20 @@ export const CATEGORY = {
   japanese: '일식',
   asian: '아시안',
   western: '양식',
-  others: '기타'
-};
+  etc: '기타'
+} as const;
+export type Category = (typeof CATEGORY)[keyof typeof CATEGORY];
 
 export const SORTING_KEY = {
-  byName: '이름순',
-  byDistance: '거리순'
+  name: '이름순',
+  distance: '거리순'
+} as const;
+
+export type SortingKey = (typeof SORTING_KEY)[keyof typeof SORTING_KEY];
+
+export const DEFAULT = {
+  category: CATEGORY.all,
+  sortingKey: SORTING_KEY.name
 };
 
 export const EVENT = {
