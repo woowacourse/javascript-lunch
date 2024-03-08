b154bc8536accccaf44d4bb8cd36bcd3806071b7
hain-tain <hain7ce@gmail.com>
2024-03-08 13:46:20 +0900

    refactor(RestaurantList): filter 관련 매서드 통합 (sortStandard 받아서 분기)

--- src/domain/RestaurantList.ts
+++ src/domain/RestaurantList.ts
@@ -1,42 +1,56 @@
 import sortLetters from "../utils/sortLetters";
-import Category from "./type/Category";
-import Restaurant from "./type/Restaurant";
 
 class RestaurantList {
-  #restaurants: Map<string, Restaurant> = new Map();
+  static #restaurants: Map<string, Restaurant> = new Map();
+  static #sortCallback: Record<
+    SortStandart,
+    (a: Restaurant, b: Restaurant) => number
+  > = {
+    이름순: (a: Restaurant, b: Restaurant) => sortLetters(a.name, b.name),
+    거리순: (a: Restaurant, b: Restaurant) => a.distance - b.distance,
+  };
 
-  add(restaurant: Restaurant) {
-    if (this.#restaurants.get(restaurant.name)) {
-      throw new Error("name 중복");
-    }
+  static init(restaurants: Restaurant[] = []) {
+    const restaurantEntries: [string, Restaurant][] = restaurants.map(
+      (restaurant) => [restaurant.name, restaurant]
+    );
+    RestaurantList.#restaurants = new Map(restaurantEntries);
+  }
+
+  static getRestaurants() {
+    return Array.from(this.#restaurants.values());
+  }
+
+  static add(restaurant: Restaurant) {
     this.#restaurants.set(restaurant.name, restaurant);
     return this;
   }
 
-  delete(name: string) {
+  static delete(name: string) {
     if (this.#restaurants.get(name) === undefined) {
       throw new Error("name 없음");
     }
     this.#restaurants.delete(name);
   }
 
-  getAscendingByName() {
-    const restaurants = this.getRestaurants();
-    return restaurants.sort((a, b) => sortLetters(a.name, b.name));
+  static getOrderedRestaurant({
+    category,
+    sortStandard,
+  }: {
+    category: CategoryWithEntire;
+    sortStandard: SortStandart;
+  }) {
+    const filteredRestaurant = this.#filterByCategory(category);
+    filteredRestaurant.sort(this.#sortCallback[sortStandard]);
+    return filteredRestaurant;
   }
 
-  getAscendingByDistance() {
+  static #filterByCategory(category: CategoryWithEntire) {
     const restaurants = this.getRestaurants();
-    return restaurants.sort((a, b) => a.distance - b.distance);
-  }
-
-  filterByCategory(category: Category) {
-    const restaurants = this.getRestaurants();
+    if (category === "전체") {
+      return restaurants;
+    }
     return restaurants.filter((restaurant) => restaurant.category === category);
-  }
-
-  getRestaurants() {
-    return Array.from(this.#restaurants.values());
   }
 }
 
