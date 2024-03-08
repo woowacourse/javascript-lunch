cb6f1cbc2b14c7a3daf44bfdddc5c1ac04c8e18e
vi-wolhwa <cerezo00@naver.com>
2024-03-08 14:27:29 +0900

    fix: 컴포넌트 로딩 순서 문제 해결
    Co-authored-by: river <0jenn0@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import '../templates/style.css';
 
 import './component/gnbComponent/GNBComponent.js';
 import './component/selectFilterComponent/SelectFilterComponent.js';
+import './component/restaurantCardComponent/RestaurantCardComponent.js';
 import './component/restaurantListComponent/RestaurantListComponent.js';
 import './component/modalComponent/ModalComponent.js';
-import './component/restaurantCardComponent/RestaurantCardComponent.js';
