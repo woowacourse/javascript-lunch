bbb2fa855fbd14c3c34c55a38d0d32252908ee47
greetings1012 <ahnqwerty@gmail.com>
2024-03-08 16:12:11 +0900

    feat: index.js에 SelectBoxSection.ts import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import './app/style.css';
 
 import './app/modal/AddRestaurantModal/AddRestaurantModal.ts';
-
 import './app/root/LunchHeader/LunchHeader.ts';
-
+import './app/root/SelectBoxSection/SelectBoxSection.ts';
 import './app/root/RestaurantItem/RestaurantItem.ts';
 
 import AppController from './app/controller/AppController.ts';
 
 const appController = new AppController();
-
 appController.initializeApp();
