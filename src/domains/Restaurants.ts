aa8104fc634ee05eade26d2515259f0fd87758ad
hwinkr <dnddl8280@naver.com>
2024-03-08 14:34:42 +0900

    feat(validator): 레스토랑 추가 폼 유효성 검증 구현

--- src/domains/Restaurants.ts
+++ src/domains/Restaurants.ts
@@ -1,4 +1,5 @@
 import { DEFAULT_DATA } from "../constants/MenuApp";
+import restaurantValidator from "../validators/restaurantValidator";
 
 type Category = "한식" | "일식" | "아시안" | "양식" | "중식" | "전체" | "기타";
 type Distance = 5 | 10 | 15 | 20 | 30;
@@ -18,7 +19,48 @@ const getRestaurantFromStorage = () => {
   const storedRestaurants = localStorage.getItem(RESTAURANT_KEY);
   if (!storedRestaurants) return [];
 
-  return JSON.parse(storedRestaurants);
+  return JSON.parse(storedRestaurants) as TRestaurant[];
+};
+
+const isAlreadyExist = (newRestaurantName: string) => {
+  const storedRestaurants = getRestaurantFromStorage();
+  return storedRestaurants.some(
+    ({ name }) => trimAllSpace(newRestaurantName) === trimAllSpace(name)
+  );
+};
+
+const validateRestaurantData = (restaurantInfo: TRestaurant) => {
+  const { name, category, distance, description, link } = restaurantInfo;
+  if (!restaurantValidator.isInRange(name, 0, 10)) {
+    throw new Error("error");
+  }
+  if (!restaurantValidator.isSelected(category)) {
+    throw new Error("error");
+  }
+  if (!restaurantValidator.isSelected(distance)) {
+    throw new Error("error");
+  }
+  if (description && !restaurantValidator.isInRange(description, 0, 300)) {
+    throw new Error("error");
+  }
+  if (link && !restaurantValidator.isValidLink(link)) {
+    throw new Error("error");
+  }
+  if (isAlreadyExist(name)) {
+    throw new Error("error");
+  }
+};
+
+const sortByName = (restaurants: TRestaurant[]) => {
+  return restaurants.sort((a, b) => (a.name < b.name ? -1 : 1));
+};
+
+const sortByDistance = (restaurants: TRestaurant[]) => {
+  return restaurants.sort((a, b) => a.distance - b.distance);
+};
+
+const trimAllSpace = (str: string): string => {
+  return str.replaceAll(" ", "");
 };
 
 export const initResaurantStorage = () => {
@@ -31,20 +73,24 @@ export const initResaurantStorage = () => {
   });
 };
 
+export const add = (restaurantInfo: TRestaurant) => {
+  const storedRestaurants = getRestaurantFromStorage();
+  validateRestaurantData(restaurantInfo);
+
+  localStorage.setItem(
+    RESTAURANT_KEY,
+    JSON.stringify([...storedRestaurants, restaurantInfo])
+  );
+
+  return true;
+};
+
 export const filterByCategory = (category: Category) => {
   const restaurants: TRestaurant[] = getRestaurantFromStorage();
 
   if (category === "전체") return restaurants;
 
   return restaurants.filter((item) => item.category === category);
-};
-
-const sortByName = (restaurants: TRestaurant[]) => {
-  return restaurants.sort((a, b) => (a.name < b.name ? -1 : 1));
-};
-
-const sortByDistance = (restaurants: TRestaurant[]) => {
-  return restaurants.sort((a, b) => a.distance - b.distance);
 };
 
 export const sortByType = (category: Category, type: SortType) => {
@@ -54,22 +100,3 @@ export const sortByType = (category: Category, type: SortType) => {
     ? sortByName(filteredRestaurants)
     : sortByDistance(filteredRestaurants);
 };
-
-export const add = (restaurantInfo: TRestaurant) => {
-  const storedRestaurants = getRestaurantFromStorage();
-  localStorage.setItem(
-    RESTAURANT_KEY,
-    JSON.stringify([...storedRestaurants, restaurantInfo])
-  );
-};
-
-// 지니
-
-// - 항상 어떤 변화가 생기든, 로컬 스토리지에 그 변화를 저장하고 + 그 변화된 데이터를 클래스가 받아오니까
-// -> 멤버변수를 둘 필요가 없다
-// -> 인스턴스를 굳이? 생성해야 할까
-
-// -> restaurantList에서 생성하는 인스턴스
-// -> addForm에서 생성하는 인스턴스
-
-// ->
