23170b0b5f8a78897238b181626d338fa9483c3a
greetings1012 <ahnqwerty@gmail.com>
2024-03-08 10:01:13 +0900

    feat: RestaurantItem.ts 구현

--- src/app/root/RestaurantItem/RestaurantItem.ts
+++ src/app/root/RestaurantItem/RestaurantItem.ts
@@ -1,10 +1,10 @@
-import { RestaurantDataType } from '../../../type/restaurantDataType';
-import { Asset } from '../../../asset/asset';
+import { LocationData } from '../../../constants/Type';
+import { ASSETS } from '../../../constants/setting';
 
-export default class RestaurantItem extends HTMLElement {
-  private restaurantData: RestaurantDataType;
+class RestaurantItem extends HTMLElement {
+  private restaurantData: LocationData;
 
-  constructor(restaurantData: RestaurantDataType) {
+  constructor(restaurantData: LocationData) {
     super();
     this.restaurantData = restaurantData;
   }
@@ -13,57 +13,22 @@ export default class RestaurantItem extends HTMLElement {
     this.render();
   }
 
-  private createCategoryBox(): HTMLDivElement {
-    const categoryBox = document.createElement('div');
-    categoryBox.classList.add('restaurant__category');
-
-    const categoryImg = document.createElement('img');
-    categoryImg.classList.add('category-icon');
-    categoryImg.setAttribute('src', Asset.imageUrl[this.restaurantData.category]);
-    categoryImg.setAttribute('alt', this.restaurantData.category);
-    categoryBox.appendChild(categoryImg);
-
-    return categoryBox;
-  }
-
-  private createInfoBox(): HTMLDivElement {
-    const infoBox = document.createElement('div');
-    infoBox.classList.add('restaurant__info');
-
-    infoBox.appendChild(this.createRestaurantTitle());
-    infoBox.appendChild(this.createRestaurantDistance());
-    infoBox.appendChild(this.createRestaurantDescription());
-
-    return infoBox;
-  }
-
-  private createRestaurantTitle(): HTMLHeadingElement {
-    const title = document.createElement('h3');
-    title.classList.add('restaurant__name', 'text-subtitle');
-    title.textContent = this.restaurantData.name;
-    return title;
-  }
-
-  private createRestaurantDistance(): HTMLSpanElement {
-    const distance = document.createElement('span');
-    distance.classList.add('restaurant__distance', 'text-body');
-    distance.textContent = `캠퍼스부터 ${this.restaurantData.distanceByWalk}분 내`;
-    return distance;
-  }
-
-  private createRestaurantDescription(): HTMLParagraphElement {
-    const description = document.createElement('p');
-    description.classList.add('restaurant__description', 'text-body');
-    description.textContent = this.restaurantData.description ?? '';
-    return description;
-  }
-
   private render() {
-    this.classList.add('restaurant');
-
-    this.appendChild(this.createCategoryBox());
-    this.appendChild(this.createInfoBox());
+    this.innerHTML = `
+      <li class="restaurant">
+        <div class="restaurant__category">
+          <img src="${ASSETS.imageUrl[this.restaurantData.category]}" \
+          alt="${this.restaurantData.category}" class="category-icon" />
+        </div>
+        <div class="restaurant__info">
+          <h3 class="restaurant__name text-subtitle">${this.restaurantData.name}</h3>
+          <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurantData.minutesWalk}분 내</span>
+          <p class="restaurant__description text-body">${this.restaurantData.description}</p>
+        </div>
+      </li>
+    `;
   }
 }
 
 customElements.define('restaurant-item', RestaurantItem);
+export default RestaurantItem;
