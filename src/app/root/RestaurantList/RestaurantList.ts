092e7a7406606523d3e13088b2cb07d26b375e46
greetings1012 <ahnqwerty@gmail.com>
2024-03-08 10:01:28 +0900

    feat: RestaurantList.ts 구현

---
+++ src/app/root/RestaurantList/RestaurantList.ts
@@ -0,0 +1,35 @@
+import RestaurantItem from '../RestaurantItem/RestaurantItem';
+import { LocationData } from '../../../constants/Type';
+
+class RestaurantList extends HTMLElement {
+  private restaurants: LocationData[];
+
+  constructor(restaurants: LocationData[]) {
+    super();
+    this.restaurants = restaurants;
+  }
+
+  connectedCallback() {
+    const restaurantList: HTMLElement[] = this.createRestaurantList();
+    this.render(restaurantList);
+  }
+
+  createRestaurantList() {
+    return this.restaurants.map((restaurantData) => new RestaurantItem(restaurantData));
+  }
+
+  private render(restaurantList: HTMLElement[]) {
+    const listContainer = document.createElement('ul');
+    listContainer.classList.add('restaurant-list');
+
+    restaurantList.forEach((restaurantItem) => {
+      listContainer.appendChild(restaurantItem);
+    });
+
+    this.innerHTML = `<section class="restaurant-list-container"></section>`;
+    this.querySelector('.restaurant-list-container')!.appendChild(listContainer);
+  }
+}
+
+customElements.define('restaurant-list', RestaurantList);
+export default RestaurantList;
