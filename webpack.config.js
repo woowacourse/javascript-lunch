468c3e414422e5e4672aabe8e45fbc949996a99d
Soyeon Choe <soy2302ten@gmail.com>
2024-03-08 14:56:09 +0900

    ci: 개발 셋팅 수정
    Co-Authored-By: badahertz52 <badahertz52@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   resolve: {
-    extensions: [".ts", ".js", ".mjs"],
+    extensions: ['.ts', '.js', '.mjs'],
   },
   module: {
     rules: [
@@ -18,31 +18,34 @@ module.exports = {
         test: /\.(js|mjs|ts)$/i,
         exclude: /node_modules/,
         use: {
-          loader: "ts-loader",
+          loader: 'ts-loader',
         },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]",
+          name: '[name].[ext]',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
   ],
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
     hot: true,
     open: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
 };
