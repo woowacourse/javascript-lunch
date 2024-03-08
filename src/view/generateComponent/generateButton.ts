d70ae3659f38a5084b33197cc529fda39cf8c899
hain-tain <hain7ce@gmail.com>
2024-03-08 13:36:50 +0900

    feat(generateButtom): 버튼 element 생성하는 함수 구현

---
+++ src/view/generateComponent/generateButton.ts
@@ -0,0 +1,21 @@
+import createElementByTag from "./utils/createElementByTag";
+
+const generateButton = ({
+  value,
+  classes = [],
+  onclick = () => {},
+}: {
+  value: string;
+  classes?: string[];
+  onclick: (e: Event) => void;
+}): HTMLButtonElement => {
+  const button = createElementByTag({
+    tag: "button",
+    classes,
+    contents: value,
+  }) as HTMLButtonElement;
+  button.addEventListener("click", onclick);
+  return button;
+};
+
+export default generateButton;
