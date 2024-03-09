234223d212418cdc3715bfc38139f9f7ea9eca23
rbgksqkr <rbgks1937@naver.com>
2024-03-10 01:24:11 +0900

    refactor(SelectBox): ts로 마이그레이션

--- src/components/SelectBox.ts
+++ src/components/SelectBox.ts
@@ -1,10 +1,19 @@
+import type { IDomAttributes, ISelectOption } from '../types/selectBox';
+
+interface ISelectBox {
+  $target: HTMLElement;
+  attributes: IDomAttributes;
+  eventHandler: () => void;
+  options: ISelectOption[];
+}
+
 class SelectBoxComponent {
   $target;
   attributes;
   eventHandler;
   options;
 
-  constructor({ $target, attributes, eventHandler, options }) {
+  constructor({ $target, attributes, eventHandler, options }: ISelectBox) {
     this.$target = $target;
     this.attributes = attributes;
     this.eventHandler = eventHandler;
@@ -13,17 +22,17 @@ class SelectBoxComponent {
     this.setEvent();
   }
 
-  setEvent() {
+  setEvent(): void {
     this.$target.addEventListener('change', this.eventHandler);
   }
 
-  render() {
+  render(): void {
     const { name, id, class: className, required } = this.attributes;
     const selectTag = document.createElement('select');
-    selectTag.name = name;
-    selectTag.id = id;
-    selectTag.classList.add(className);
-    selectTag.required = required ? true : false;
+    selectTag.name = name ?? '';
+    selectTag.id = id ?? '';
+    if (className != null) selectTag.classList.add(className);
+    selectTag.required = required ?? false;
 
     this.options.forEach(option => {
       const optionTag = this.#getOptionTag(option);
@@ -32,7 +41,7 @@ class SelectBoxComponent {
     this.$target.appendChild(selectTag);
   }
 
-  #getOptionTag(selectOption) {
+  #getOptionTag(selectOption: ISelectOption): HTMLOptionElement {
     const optionTag = document.createElement('option');
     optionTag.value = selectOption.value;
     optionTag.textContent = selectOption.text;
