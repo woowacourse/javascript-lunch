01e31d6f1a937054a67fb0b74fe0441b11ac1212
brgndy <brgndy96@gmail.com>
2024-03-09 21:47:37 +0900

    refactor: 기존 handlers였던 파일명 eventHandlers로 변경

--- src/components/header/eventHandlers.ts
+++ src/components/header/eventHandlers.ts
@@ -1,9 +1,9 @@
 const modalOpenHandler = () => {
-  const modal = document.getElementsByClassName("modal")[0];
-  const openButton = document.getElementsByClassName("gnb__button")[0];
+  const modal = document.getElementsByClassName('modal')[0];
+  const openButton = document.getElementsByClassName('gnb__button')[0];
 
-  openButton.addEventListener("click", () => {
-    modal.classList.add("modal--open");
+  openButton.addEventListener('click', () => {
+    modal.classList.add('modal--open');
   });
 };
 export default modalOpenHandler;
