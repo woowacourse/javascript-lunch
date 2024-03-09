e2e74df16b88136bd6949b1394bb0cef50744eb7
brgndy <brgndy96@gmail.com>
2024-03-09 18:01:00 +0900

    feat: 받은 옵션을 통해 공용 Select 컴포넌트를 만들어주는 함수 생성

---
+++ src/uiUtils/generateSelectElement.ts
@@ -0,0 +1,15 @@
+import Select from '../components/composables/Select';
+import { SelectElementDataType } from '../types';
+
+const generateSelectElement = (selectOptionData: SelectElementDataType) => {
+  const { TAG_ID, TAG_NAME, TAG_CLASS_NAME, UI_OPTIONS } = selectOptionData;
+
+  return Select({
+    id: TAG_ID,
+    name: TAG_NAME,
+    className: TAG_CLASS_NAME,
+    options: UI_OPTIONS,
+  });
+};
+
+export default generateSelectElement;
