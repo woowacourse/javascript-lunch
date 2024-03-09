cd4120c2e84a04b6dd002f504a6cfeda4bace1f3
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-09 21:08:09 +0900

    refactor: 앱 내 이미지 경로 참조 방식을 CommonJS에서 ES modules 방식으로 변경

--- src/constants/setting.ts
+++ src/constants/setting.ts
@@ -1,3 +1,11 @@
+import addRestaurantButtonImage from '/src/assets/image/add-restaurant-button.png';
+import categoryKoreanImage from '../assets/image/category-korean.png';
+import categoryChineseImage from '../assets/image/category-chinese.png';
+import categoryJapaneseImage from '../assets/image/category-japanese.png';
+import categoryWesternImage from '../assets/image/category-western.png';
+import categoryAsianImage from '../assets/image/category-asian.png';
+import categoryEtcImage from '../assets/image/category-etc.png';
+
 const TYPE_SETTING = {
   category: ['한식', '중식', '일식', '양식', '아시안', '기타'] as const,
   minutesWalk: [5, 10, 15, 20, 30] as const,
@@ -6,13 +14,13 @@ const TYPE_SETTING = {
 
 const ASSETS = {
   imageUrl: {
-    버튼_음식점추가: require('/src/assets/image/add-button.png').default,
-    한식: require('/src/assets/image/category-korean.png').default,
-    중식: require('/src/assets/image/category-chinese.png').default,
-    일식: require('/src/assets/image/category-japanese.png').default,
-    양식: require('/src/assets/image/category-western.png').default,
-    아시안: require('/src/assets/image/category-asian.png').default,
-    기타: require('/src/assets/image/category-etc.png').default,
+    버튼_음식점추가: addRestaurantButtonImage,
+    한식: categoryKoreanImage,
+    중식: categoryChineseImage,
+    일식: categoryJapaneseImage,
+    양식: categoryWesternImage,
+    아시안: categoryAsianImage,
+    기타: categoryEtcImage,
   },
 };
 export { TYPE_SETTING, ASSETS };
