795e0db46a7f45c9bf1cc723fa1dbea3506a3017
brgndy <brgndy96@gmail.com>
2024-03-09 19:26:51 +0900

    style: prettier 적용

--- src/components/modal/renderHandlers.ts
+++ src/components/modal/renderHandlers.ts
@@ -1,10 +1,10 @@
-import dimmerClickHandler from "./handlers";
-import InputDescription from "./input_description/InputDescription";
-import InputLink from "./input_link/InputLink";
-import InputName from "./input_name/InputName";
-import ModalButton from "./modal_button/ModalButton";
-import SelectCategory from "./select_category/SelectCategory";
-import SelectDistance from "./select_distance/SelectDistance";
+import dimmerClickHandler from './handlers';
+import InputDescription from './input_description/InputDescription';
+import InputLink from './input_link/InputLink';
+import InputName from './input_name/InputName';
+import ModalButton from './modal_button/ModalButton';
+import SelectCategory from './select_category/SelectCategory';
+import SelectDistance from './select_distance/SelectDistance';
 
 const renderModalForm = (form: Element) => {
   SelectCategory(form);
@@ -15,8 +15,8 @@ const renderModalForm = (form: Element) => {
 };
 
 const renderModalContent = () => {
-  const modal = document.getElementsByClassName("modal")[0];
-  const form = document.getElementsByClassName("modal-form")[0];
+  const modal = document.getElementsByClassName('modal')[0];
+  const form = document.getElementsByClassName('modal-form')[0];
 
   renderModalForm(form);
 
