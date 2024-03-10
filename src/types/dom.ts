37c18df48b203cf5c5f2394601257b58222e8bea
rbgksqkr <rbgks1937@naver.com>
2024-03-10 14:14:15 +0900

    chore: selectBox에서 dom 속성을 나타내는 파일 이름으로 수정

--- src/types/dom.ts
+++ src/types/dom.ts
@@ -1,21 +1,26 @@
 interface IDomAttributes {
-  name?: string;
   id?: string;
-  class?: string[];
+  classNames?: string[];
   text?: string;
 }
 
 interface ISelectAttributes extends IDomAttributes {
+  name?: string;
   required?: boolean;
 }
 
 interface IButtonAttributes extends IDomAttributes {
+  name?: string;
   type?: 'submit' | 'reset' | 'button';
 }
 
-interface ISelectOption {
+interface IDomCreation extends IDomAttributes {
+  tagName: string;
+}
+
+interface IOptionAttributes {
   value: string;
   text: string;
 }
 
-export type { IDomAttributes, ISelectAttributes, IButtonAttributes, ISelectOption };
+export type { IDomAttributes, ISelectAttributes, IButtonAttributes, IDomCreation, IOptionAttributes };
