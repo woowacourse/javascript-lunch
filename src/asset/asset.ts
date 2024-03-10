a67f664123576dc2dd92f1d919b79554c2e20281
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-10 14:49:50 +0900

    refactor: 이미지 경로를 상수화하는 Asset 객체를 별도로 분리하여 관리

--- src/asset/asset.ts
+++ src/asset/asset.ts
@@ -1,10 +1,10 @@
-import addRestaurantIcon from './image/add-restaurant-button.png';
-import koreanIcon from './image/category-korean.png';
-import chineseIcon from './image/category-chinese.png';
-import japaneseIcon from './image/category-japanese.png';
-import westernIcon from './image/category-western.png';
-import asianIcon from './image/category-asian.png';
-import etcIcon from './image/category-etc.png';
+import addRestaurantIcon from '/src/asset/image/add-restaurant-button.png';
+import koreanIcon from '../asset/image/category-korean.png';
+import chineseIcon from '../asset/image/category-chinese.png';
+import japaneseIcon from '../asset/image/category-japanese.png';
+import westernIcon from '../asset/image/category-western.png';
+import asianIcon from '../asset/image/category-asian.png';
+import etcIcon from '../asset/image/category-etc.png';
 
 export const Asset = {
   imageUrl: {
