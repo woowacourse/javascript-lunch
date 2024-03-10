0d501f8a16696d64171501087a406fbb2a618f51
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-10 12:39:28 +0900

    refactor: querySelector 래핑 함수에 해당 요소 선택 결과물에 대한 인스턴스 타입 체크 추가

--- src/utils/domSelector.ts
+++ src/utils/domSelector.ts
@@ -1,9 +1,13 @@
-function $<E extends Element = Element>(selector: string, target: Element | Document = document): E | null {
-  return target.querySelector(selector);
+function $<E extends HTMLElement = HTMLElement>(selector: string): E {
+  const element = document.querySelector(selector);
+  if (!(element instanceof HTMLElement)) {
+    throw new Error(`${selector}에 해당하는 요소를 찾을 수 없습니다. 새로고침 후 다시 시도해주세요.`);
+  }
+  return <E>element;
 }
 
-function $$(selector: string, target: Element | Document = document): NodeListOf<Element> {
-  return target.querySelectorAll(selector);
+function $$(selector: string): NodeListOf<HTMLElement> {
+  return document.querySelectorAll(selector);
 }
 
 export { $, $$ };
