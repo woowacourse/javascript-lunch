bc95a8f6c96bf80e9d8771a827fd713df2837152
vi-wolhwa <cerezo00@naver.com>
2024-03-11 00:12:14 +0900

    feat: header title 컴포넌트

---
+++ src/components/independent/HeaderTitle.ts
@@ -0,0 +1,23 @@
+import '../../styles/headerTitle.css';
+
+import Component from '../Component';
+import { EventInfo } from '../../types/component';
+
+class HeaderTitleComponent extends Component {
+  protected setTemplate() {
+    return `
+      <template>
+        <h1 class = "gnb__title text-title">점심 뭐 먹지</h1>
+      </template>`;
+  }
+
+  protected setEvents(): EventInfo[] {
+    return [];
+  }
+
+  protected render() {
+    this.appendChild(this.component);
+  }
+}
+
+customElements.define('header-title', HeaderTitleComponent);
