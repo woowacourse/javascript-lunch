b2765d6e1273ce5a95d36f22c319a22985e32119
brgndy <brgndy96@gmail.com>
2024-03-10 13:12:48 +0900

    feat: description 컴포넌트 공용 컴포넌트로 바인딩

--- src/components/modal/input_description/renderHandlers.ts
+++ src/components/modal/input_description/renderHandlers.ts
@@ -2,6 +2,10 @@ import convertHTMLStringToDOM from '../../../utils/convertHTMLStringToDOM';
 import inputDescriptionTemplate from './inputDescriptionTemplate';
 import generateLabelComponent from '../../../uiUtils/generateLabelComponent';
 import DESCRIPTION_LABEL_COMPONENT_DATA from '../../../constants/DescriptionLabelComponentData';
+import generateTextAreaComponent from '../../../uiUtils/generateTextAreaComponent';
+import DESCRIPTION_TEXT_AREA_COMPONENT_DATA from '../../../constants/DescriptionTextAreaComponentData';
+import generateSpanComponent from '../../../uiUtils/generateSpanComponent';
+import DESCRIPTION_SPAN_COMPONENT_DATA from '../../../constants/DescriptionSpanComponentData';
 
 export const renderBaseDescriptionComponent = (form: Element) => {
   form.appendChild(convertHTMLStringToDOM(inputDescriptionTemplate));
@@ -11,4 +15,6 @@ export const renderDescriptionComponent = () => {
   const formItem = document.getElementsByClassName('form-item')[0];
 
   formItem.appendChild(generateLabelComponent(DESCRIPTION_LABEL_COMPONENT_DATA));
+  formItem.appendChild(generateTextAreaComponent(DESCRIPTION_TEXT_AREA_COMPONENT_DATA));
+  formItem.appendChild(generateSpanComponent(DESCRIPTION_SPAN_COMPONENT_DATA));
 };
