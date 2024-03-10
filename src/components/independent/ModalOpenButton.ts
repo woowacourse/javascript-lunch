5b7391d78e153a4d34df7b02eee8556bdb2ca5d0
vi-wolhwa <cerezo00@naver.com>
2024-03-11 00:13:55 +0900

    feat: modal open 컴포넌트

---
+++ src/components/independent/ModalOpenButton.ts
@@ -0,0 +1,27 @@
+import '../../styles/headerTitle.css';
+
+import ADD_BUTTON_IMAGE from '../../assets/images/add-button.png';
+
+import Component from '../Component';
+import { EventInfo } from '../../types/component';
+
+class ModalOpenButton extends Component {
+  protected setTemplate() {
+    return `
+      <template>
+        <button type="button" class="gnb__button" aria-label="음식점 추가">
+          <img src=${ADD_BUTTON_IMAGE} alt="음식점 추가">
+        </button>
+      </template>`;
+  }
+
+  protected setEvents(): EventInfo[] {
+    return [];
+  }
+
+  protected render() {
+    this.appendChild(this.component);
+  }
+}
+
+customElements.define('modal-open-button', ModalOpenButton);
