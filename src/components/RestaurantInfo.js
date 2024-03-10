1e498c15c4d5ffe8d5469b30f8e1eeb8ad938ffe
useon <yoosk4986@naver.com>
2024-03-10 15:50:59 +0900

    refactor: 제목 단계 순차 기입을 위해 h1 다음을 h3이 아닌 h2로 수정

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -58,7 +58,7 @@ class RestaurantInfo extends Component {
           ${this.displayCategoryIcon(this.#category)}
         </div>
         <div class="restaurant__info">
-          <h3 class="restaurant__name text-subtitle">${this.#name}</h3>
+          <h2 class="restaurant__name text-subtitle">${this.#name}</h3>
           <span class="restaurant__distance text-body">캠퍼스부터 ${this.#distance}분 내</span>
           <p class="restaurant__description text-body">
             ${this.#description !== undefined ? this.#description : ''}
