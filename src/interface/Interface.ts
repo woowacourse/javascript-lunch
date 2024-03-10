61784a0f52b704947cdbc07cf186521c3b0988f4
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-10 14:43:43 +0900

    refactor: 불필요한 인터페이스 삭제

--- src/interface/Interface.ts
+++
@@ -1,9 +0,0 @@
-import type { Category, MinutesWalk } from '../constants/Type';
-
-interface ILocation {
-  getName(): string;
-  getMinutesWalk(): MinutesWalk;
-  isMatchedCategory(category: Category): boolean;
-}
-
-export { ILocation };
