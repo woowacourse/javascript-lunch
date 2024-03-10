16e32fe37538b2bd41c1203fda081cee1e5c15d4
rbgksqkr <rbgks1937@naver.com>
2024-03-11 01:45:26 +0900

    feat(Button): kind 속성으로 add버튼과 close버튼 구분하는 버튼 공통 컴포넌트 구현

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -1,33 +1,82 @@
+import Restaurant from '@/domain/Restaurant';
+import type { IRestaurantList, TCategory, TDistance } from '@/types/restaurant';
+import type RestaurantList from '@/domain/RestaurantList';
 import type { IButtonAttributes } from '../types/dom';
 import dom from '../utils/dom';
+
+interface FormElements extends HTMLFormControlsCollection {
+  category: HTMLInputElement;
+  name: HTMLInputElement;
+  distance: HTMLInputElement;
+  description: HTMLInputElement;
+  link: HTMLInputElement;
+}
 
 interface IButtonProps {
   $target: HTMLElement;
   attributes: IButtonAttributes;
-  eventHandler: () => void;
+  kind: 'close' | 'add';
+  restaurantList?: RestaurantList;
+  renderNewRestaurantList?: (newRestaurants: IRestaurantList) => void;
 }
 
 class Button {
   $target;
   attributes;
-  eventHandler;
+  kind;
+  restaurantList;
+  renderNewRestaurantList;
 
-  constructor({ $target, attributes, eventHandler }: IButtonProps) {
+  constructor({ $target, attributes, kind, restaurantList, renderNewRestaurantList }: IButtonProps) {
     this.$target = $target;
     this.attributes = attributes;
-    this.eventHandler = eventHandler;
+    this.kind = kind;
+    this.restaurantList = restaurantList;
+    this.renderNewRestaurantList = renderNewRestaurantList;
     this.render();
     this.setEvent();
   }
 
   setEvent(): void {
-    this.$target.addEventListener('click', this.eventHandler);
+    if (this.kind === 'add') {
+      dom.getElement('form').addEventListener('submit', e => {
+        e.preventDefault();
+        this.handleAddRestaurant();
+      });
+    } else if (this.kind === 'close') {
+      this.$target.addEventListener('click', () => {
+        this.close();
+      });
+    }
   }
 
   render(): void {
     const { name, id, classNames, type, text } = this.attributes;
     const buttonTag = dom.createButtonTag({ name, id, classNames, type, text });
     this.$target.appendChild(buttonTag);
+  }
+
+  close(): void {
+    dom.getElement('.modal').classList.remove('modal--open');
+    const $form = dom.getElement('form') as HTMLFormElement;
+    $form.reset();
+  }
+
+  handleAddRestaurant(): void {
+    const $restaurantForm = dom.getElement('form') as HTMLFormElement;
+    const elements = $restaurantForm.elements as FormElements;
+
+    const category = elements.category.value as TCategory;
+    const name = elements.name.value;
+    const distance = Number(elements.distance.value) as TDistance;
+    const description = elements.description.value;
+    const referenceLink = elements.link.value;
+    const restaurantInformation = new Restaurant({ category, name, distance, description, referenceLink });
+
+    if (this.restaurantList == null) return;
+    this.restaurantList.add(restaurantInformation);
+    this.renderNewRestaurantList != null && this.renderNewRestaurantList(this.restaurantList.restaurants);
+    this.close();
   }
 }
 
