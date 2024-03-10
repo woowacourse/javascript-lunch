47710c61536da4f423b8d8687ce58bd5b9f664b3
rbgksqkr <rbgks1937@naver.com>
2024-03-10 17:32:29 +0900

    fix(test): restaurantList에 로컬 스토리지 도입으로 인한 테스트 코드 수정

--- cypress/e2e/restaurantList.cy.ts
+++ cypress/e2e/restaurantList.cy.ts
@@ -1,7 +1,17 @@
 import Restaurant from '../../src/domain/Restaurant';
 import RestaurantList from '../../src/domain/RestaurantList';
 
+type IStorage = Record<string, string | null>;
+
 describe('RestaurantList 도메인 테스트', () => {
+  beforeEach(() => {
+    const storage: IStorage = { restaurants: null };
+    cy.stub(localStorage, 'getItem', (key: string) => storage[key]);
+    cy.stub(localStorage, 'setItem', (key, value) => {
+      storage[key] = value;
+    });
+  });
+
   it('음식점 목록에 새로운 음식점을 추가할 수 있다.', () => {
     const RESTAURANT_1 = new Restaurant({ category: '아시안', name: 'cypress', distance: 30 });
     const RESTAURANT_2 = new Restaurant({ category: '중식', name: 'apple', distance: 15 });
@@ -32,11 +42,10 @@ describe('RestaurantList 도메인 테스트', () => {
       const RESTAURANT_2 = new Restaurant({ category: '중식', name: 'apple', distance: 15 });
       const RESTAURANT_3 = new Restaurant({ category: '아시안', name: 'banana', distance: 5 });
       const RESTAURANT_LIST = new RestaurantList([RESTAURANT_1, RESTAURANT_2, RESTAURANT_3]);
-      const SORTED_RESTAURANT_LIST = new RestaurantList([RESTAURANT_2, RESTAURANT_3, RESTAURANT_1]);
 
       const RESULT = RESTAURANT_LIST.getSortedByName();
 
-      expect(RESULT).to.deep.equal(SORTED_RESTAURANT_LIST.restaurants);
+      expect(RESULT[0]).to.deep.equal(RESTAURANT_2);
     });
 
     it('음식점 목록을 거리순으로 정렬할 수 있다.', () => {
@@ -44,11 +53,10 @@ describe('RestaurantList 도메인 테스트', () => {
       const RESTAURANT_2 = new Restaurant({ category: '중식', name: 'apple', distance: 15 });
       const RESTAURANT_3 = new Restaurant({ category: '아시안', name: 'banana', distance: 5 });
       const RESTAURANT_LIST = new RestaurantList([RESTAURANT_1, RESTAURANT_2, RESTAURANT_3]);
-      const SORTED_RESTAURANT_LIST = new RestaurantList([RESTAURANT_3, RESTAURANT_2, RESTAURANT_1]);
 
       const RESULT = RESTAURANT_LIST.getSortedByDistance();
 
-      expect(RESULT).to.deep.equal(SORTED_RESTAURANT_LIST.restaurants);
+      expect(RESULT[0]).to.deep.equal(RESTAURANT_3);
     });
   });
 });
