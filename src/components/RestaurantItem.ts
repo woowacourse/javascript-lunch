c14616c747d0493f43272819668b588f673f324f
hanyoung <phanyoung@naver.com>
2024-03-10 18:18:34 +0900

    chore(RestaurantItem): h1 태그 이후 등장한 h3 태그를 h2 태그로 수정

--- src/components/RestaurantItem.ts
+++ src/components/RestaurantItem.ts
@@ -16,7 +16,7 @@ export default class RestaurantItem extends BaseComponent {
       <li class="restaurant">
         <category-icon category=${category}></category-icon>
         <div class="restaurant__info">
-          <h3 class="restaurant__name text-subtitle">${name}</h3>
+          <h2 class="restaurant__name text-subtitle">${name}</h3>
           <span class="restaurant__distance text-body">캠퍼스부터 ${timeToReach}분 내</span>
           <p class="restaurant__description text-body">${description}</p>
         </div>
