b2acddc444058908f6a608c48f3bed61781e5054
rbgksqkr <rbgks1937@naver.com>
2024-03-10 18:09:12 +0900

    refactor(index): SelectBox 생성하는 코드를 역할별로 메서드 분리

--- src/index.js
+++ src/index.js
@@ -21,57 +21,74 @@ import HomeEventHandler from './eventHandler/HomeEventHandler';
 import ModalEventHandler from './eventHandler/ModalEventHandler';
 import Button from './components/Button';
 
-const init = () => {
-  const $restaurantList = document.querySelector('.restaurant-list');
-  const getDefaultRestaurantList = () => DEFAULT_RESTAURANT_LIST.map(restaurant => new Restaurant(restaurant));
-  const restaurantList = new RestaurantList(getDefaultRestaurantList());
-
-  restaurantList.restaurants.forEach(element => {
-    new RestaurantComponent({ $target: $restaurantList, information: element.information });
-  });
-
-  // 홈화면 select 생성
-  const $restaurantFilterContainer = document.querySelector('.restaurant-filter-container');
-  new SelectBoxComponent({
+const createCategorySelect = ($restaurantFilterContainer, restaurantList) => {
+  return new SelectBoxComponent({
     kind: 'category',
     $target: $restaurantFilterContainer,
     attributes: FILTERED_CATEGORY_ATTRIBUTE,
     options: FILTERED_CATEGORY,
     restaurantList,
   });
+};
 
-  new SelectBoxComponent({
+const createSortingSelect = ($restaurantFilterContainer, restaurantList) => {
+  return new SelectBoxComponent({
     kind: 'sorting',
     $target: $restaurantFilterContainer,
     attributes: SORTING_ATTRIBUTE,
     options: SORTING,
     restaurantList,
   });
+};
 
-  // 모달 form select 생성
+const createHomeSelect = restaurantList => {
+  const $restaurantFilterContainer = document.querySelector('.restaurant-filter-container');
+  createCategorySelect($restaurantFilterContainer, restaurantList);
+  createSortingSelect($restaurantFilterContainer, restaurantList);
+};
+
+const createModalFormSelect = () => {
   const $categoryContainer = document.getElementById('category-container');
-  const $distanceContainer = document.getElementById('distance-container');
   new SelectBoxComponent({
     $target: $categoryContainer,
     attributes: FORM_CATEGORY_ATTRIBUTE,
     options: FORM_CATEGORY,
   });
+
+  const $distanceContainer = document.getElementById('distance-container');
   new SelectBoxComponent({
     $target: $distanceContainer,
     attributes: FORM_DISTANCE_ATTRIBUTE,
     options: FORM_DISTANCE,
   });
+};
 
+const createModalFormButton = () => {
   const $buttonContainer = document.querySelector('.button-container');
+
   new Button({
     $target: $buttonContainer,
     attributes: CLOSE_BUTTON_ATTRIBUTE,
   });
-
   new Button({
     $target: $buttonContainer,
     attributes: ADD_BUTTON_ATTRIBUTE,
   });
+};
+
+const init = () => {
+  const getDefaultRestaurantList = () =>
+    new RestaurantList(DEFAULT_RESTAURANT_LIST.map(restaurant => new Restaurant(restaurant)));
+  const restaurantList = getDefaultRestaurantList();
+
+  const $restaurantList = document.querySelector('.restaurant-list');
+  restaurantList.restaurants.forEach(restaurant => {
+    new RestaurantComponent({ $target: $restaurantList, information: restaurant.information });
+  });
+
+  createHomeSelect(restaurantList);
+  createModalFormSelect();
+  createModalFormButton();
 
   new HomeEventHandler();
   new ModalEventHandler(restaurantList);
