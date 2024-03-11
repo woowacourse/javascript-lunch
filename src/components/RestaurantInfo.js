1b298b21e5f3afb618adfcec73c8bc158c5d67fb
anttiey <hanuuny@gmail.com>
2024-03-11 12:59:06 +0900

    refactor: 조건문 단순화

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -61,7 +61,7 @@ class RestaurantInfo extends Component {
           <h3 class="restaurant__name text-subtitle">${this.#name}</h3>
           <span class="restaurant__distance text-body">캠퍼스부터 ${this.#distance}분 내</span>
           <p class="restaurant__description text-body">
-            ${this.#description !== undefined ? this.#description : ''}
+            ${this.#description || ''}
           </p>
       </div>
       </li>
