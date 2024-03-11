8e41223ec0d94a2e398312063560c21ae110f451
soosoo22 <lllllh00@naver.com>
2024-03-11 21:54:30 +0900

    refactor: try-catch로 에러 감지

--- src/utils/DOM.ts
+++ src/utils/DOM.ts
@@ -1,18 +1,27 @@
 function validateSelector(selector: string) {
   if (selector === '' || selector === undefined) {
-    alert('잘못된 셀렉터 값입니다.');
     throw new Error('잘못된 셀렉터 값입니다.');
   }
 }
 
 const DOM = {
   $: (selector: string) => {
-    validateSelector(selector);
-    return document.querySelector(selector);
+    try {
+      validateSelector(selector);
+      return document.querySelector(selector);
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
+      return;
+    }
   },
   $$: (selector: string) => {
-    validateSelector(selector);
-    return document.querySelectorAll(selector);
+    try {
+      validateSelector(selector);
+      return document.querySelectorAll(selector);
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
+      return [];
+    }
   },
 };
 
