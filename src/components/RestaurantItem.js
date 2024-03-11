3534a1eca85064075b281b4bd2d699204e65c029
hwinkr <dnddl8280@naver.com>
2024-03-11 21:30:04 +0900

    fix(RestaurantItem): d이미지 변수명 카멜 케이스로 변경, h3 태그를 span 태그로 수정

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,10 +1,10 @@
 import {
-  category_asian,
-  category_chinese,
-  category_etc,
-  category_japanese,
-  category_korean,
-  category_western,
+  categoryAsian,
+  categoryChinese,
+  categoryEtc,
+  categoryJapanese,
+  categoryKorean,
+  categoryWestern,
 } from "../assets/index.js";
 import BaseComponent from "./BaseComponent.js";
 
@@ -16,17 +16,17 @@ class RestaurantItem extends BaseComponent {
   #categoryToImg(category) {
     switch (category) {
       case "한식":
-        return category_korean;
+        return categoryKorean;
       case "중식":
-        return category_chinese;
+        return categoryChinese;
       case "아시안":
-        return category_asian;
+        return categoryAsian;
       case "일식":
-        return category_japanese;
+        return categoryJapanese;
       case "양식":
-        return category_western;
+        return categoryWestern;
       case "기타":
-        return category_etc;
+        return categoryEtc;
       default:
         break;
     }
@@ -40,21 +40,21 @@ class RestaurantItem extends BaseComponent {
     const img = this.#categoryToImg(category);
 
     this.innerHTML = `
-    <li class="restaurant">
+      <li class="restaurant">
         <div class="restaurant__category">
-            <img src=${img} alt="한식" class="category-icon">
+          <img src=${img} alt="한식" class="category-icon">
         </div>
         <div class="restaurant__info">
-            <h3 class="restaurant__name text-subtitle">${name}</h3>
-            <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
-            ${
-              description
-                ? `<p class="restaurant__description text-body">${description}</p>`
-                : ""
-            }
+          <span class="restaurant__name text-subtitle">${name}</span>
+          <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+          ${
+            description
+              ? `<p class="restaurant__description text-body">${description}</p>`
+              : ""
+          }
         </div>
-    </li>
-    `;
+      </li>
+      `;
   }
 }
 
