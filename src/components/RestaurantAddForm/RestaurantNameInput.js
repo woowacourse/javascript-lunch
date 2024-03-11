bc6c0a89d38dac499ddb7d882c83603ebf4b589d
hwinkr <dnddl8280@naver.com>
2024-03-11 21:25:34 +0900

    refactor(RestaurantNameInput): 새로운 식당 추가 폼 입력 컴포넌트 리팩토링
    - 에러 메시지의 요소의 클래스 리스트 멤버변수화
    - 에러 메시지 핸들링 함수 추가 분리

--- src/components/RestaurantAddForm/RestaurantNameInput.js
+++ src/components/RestaurantAddForm/RestaurantNameInput.js
@@ -3,6 +3,8 @@ import restaurantValidator from "../../validators/restaurantValidator";
 import BaseComponent from "../BaseComponent";
 
 class RestaurantNameInput extends BaseComponent {
+  #errorMessageClassList = null;
+
   constructor() {
     super();
   }
@@ -12,25 +14,37 @@ class RestaurantNameInput extends BaseComponent {
       <div class="form-item form-item--required" id="name-input">
         <label for="name">이름</label>
         <input type="text" name="name" id="name" class="text-caption">
-        <p class="hidden text-caption error-message" id="error-message">1글자 이상 10글자 이하로 작성해주세요</p>
+        <p class="hidden text-caption error-message" id="name-error-message">1글자 이상 10글자 이하로 작성해주세요</p>
       </div>`;
+
+    this.#errorMessageClassList = $("#name-error-message").classList;
   }
 
   #isValidName(value) {
     return restaurantValidator.isInRange(value, 0, 10);
   }
 
+  #renderErrorMessage() {
+    this.#errorMessageClassList.remove("hidden");
+  }
+
+  #hideErrorMessage() {
+    this.#errorMessageClassList.add("hidden");
+  }
+
+  #handleErrorMessage(value) {
+    this.#isValidName(value)
+      ? this.#hideErrorMessage()
+      : this.#renderErrorMessage();
+  }
+
   setEvent() {
-    document.addEventListener("add-form-submit", (e) => {
-      this.#isValidName($("#name").value)
-        ? $("#error-message").classList.add("hidden")
-        : $("#error-message").classList.remove("hidden");
+    document.addEventListener("add-form-submit", () => {
+      this.#handleErrorMessage($("#name").value);
     });
 
     $("#name").addEventListener("focusout", (e) => {
-      this.#isValidName(e.target.value)
-        ? $("#error-message").classList.add("hidden")
-        : $("#error-message").classList.remove("hidden");
+      this.#handleErrorMessage(e.target.value);
     });
   }
 }
