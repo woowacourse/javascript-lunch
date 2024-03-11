2ffab53733008434d3e4f2feef56040a4e58dd95
Jeongwoo Park <wahoo9040@naver.com>
2024-03-11 12:26:00 +0900

    refactor: beforeEach를 이용한 불필요한 코드 삭제

--- __test__/RestaurantCatalog.test.ts
+++ __test__/RestaurantCatalog.test.ts
@@ -50,10 +50,14 @@ describe('RestaurantCatalog 기능 테스트', () => {
     },
   ];
 
-  const newRestaurantCatalog = new RestaurantCatalog();
+  let newRestaurantCatalog: RestaurantCatalog;
 
-  validRestaurantCase.forEach((restaurantInfo) => {
-    newRestaurantCatalog.pushNewRestaurant(restaurantInfo);
+  beforeEach(() => {
+    newRestaurantCatalog = new RestaurantCatalog();
+
+    validRestaurantCase.forEach((restaurantInfo) => {
+      newRestaurantCatalog.pushNewRestaurant(restaurantInfo);
+    });
   });
 
   test('4개의 음식점을 추가하면 4개의 음식점이 추가된다.', () => {
@@ -69,33 +73,8 @@ describe('RestaurantCatalog 기능 테스트', () => {
   });
 
   test('category 거리순 정렬 테스트 - 오름차순 정렬, 동률일 시 이름의 오름차순으로', () => {
-    expect(RestaurantCatalog.sortByDistance(validRestaurantCase)[0].name).toEqual('깡장시골보리밥');
-  });
-
-  test('category 거리순 정렬 테스트 - 오름차순 정렬', () => {
-    const validRestaurantCaseForDistanceSort: IRestaurantInfo[] = [
-      {
-        category: '한식',
-        name: '반포식스',
-        distanceFromCampus: 20,
-      },
-      {
-        category: '양식',
-        name: '빕스',
-        distanceFromCampus: 5,
-      },
-      {
-        category: '양식',
-        name: '칙바이칙',
-        distanceFromCampus: 10,
-      },
-      {
-        category: '한식',
-        name: '깡장시골보리밥',
-        distanceFromCampus: 15,
-      },
-    ];
-
-    expect(RestaurantCatalog.sortByDistance(validRestaurantCaseForDistanceSort)[0].name).toEqual('빕스');
+    const restaurantsSorted = RestaurantCatalog.sortByDistance(validRestaurantCase);
+    expect(restaurantsSorted[0].name).toEqual('깡장시골보리밥');
+    expect(restaurantsSorted[1].name).toEqual('반포식스');
   });
 });
