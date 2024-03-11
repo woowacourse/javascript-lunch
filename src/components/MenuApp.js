319fdb0474bd7a7eec09d259a74b03c4d81a5fc3
hwinkr <dnddl8280@naver.com>
2024-03-11 21:35:24 +0900

    refactor(MenuApp): 생성자 함수가 호출될 때, localStorage를 초기화 하도록 변경
    - constructor -> connectedCallback 순으로 호출되기 때문에 생성자 함수에서 localStorage를 호출한다면 BaseComponent의 connectedCallback을 오버라이딩 할 필요가 없어짐

--- src/components/MenuApp.js
+++ src/components/MenuApp.js
@@ -1,38 +1,30 @@
-import { CATEGORY_OPTIONS } from "../constants/MenuApp";
-import { initResaurantStorage } from "../domains/Restaurants";
+import { CATEGORIES } from "../constants/menu";
+import { initRestaurantStorage } from "../domains/Restaurants";
 import BaseComponent from "./BaseComponent.js";
 
 class MenuApp extends BaseComponent {
   constructor() {
+    initRestaurantStorage();
     super();
-  }
-
-  connectedCallback() {
-    initResaurantStorage();
-    this.render();
-    this.setEvent();
   }
 
   render() {
     this.innerHTML = `
     <app-header></app-header>
     <main> 
-        <section class="restaurant-filter-container">
-            <option-selector type="category" options=${[
-              "전체",
-              ...CATEGORY_OPTIONS,
-            ]} ></option-selector>
-
-            <option-selector type="sort" options=${[
-              "이름순",
-              "거리순",
-            ]}></option-selector>
-        </section>
-        
-        <restaurant-list></restaurant-list>
-        <restaurant-add-form></restaurant-add-form>
+      <section class="restaurant-filter-container">
+        <option-selector type="category" options=${[
+          "전체",
+          ...CATEGORIES,
+        ]} ></option-selector>
+        <option-selector type="sort" options=${[
+          "이름순",
+          "거리순",
+        ]}></option-selector>
+      </section>
+      <restaurant-list></restaurant-list>
+      <restaurant-add-form></restaurant-add-form>
     </main>
-    
     `;
   }
 }
