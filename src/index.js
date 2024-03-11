df532e61d9e1a5a95056cb890dc7de9dc000c898
Seongjin Hong <hongseongjin.to@gmail.com>
2024-03-11 10:50:07 +0900

    refactor: style 및 컴포넌트 import 구문을 AppController로 이동

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import './app/style.css';
-
-import './app/modal/AddRestaurantModal/AddRestaurantModal.ts';
-import './app/root/NavigationBar/NavigationBar.ts';
-import './app/root/SelectBoxSection/SelectBoxSection.ts';
-import './app/root/RestaurantItem/RestaurantItem.ts';
-
 import AppController from './app/controller/AppController.ts';
 
-const appController = new AppController();
-appController.initializeApp();
+const app = new AppController();
+app.initializeApp();
