26bfd4a7f041cc953665bc5bb22766737d78caa3
cys4585 <dev.youngsu@gmail.com>
2024-03-11 19:50:05 +0900

    refactor: 앱 처음 시작할 때 기본 음식점목록 세팅

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,44 @@ import {
   setRestaurantsToLocalStorage,
 } from "./util";
 
+(function initRestaurants() {
+  if (getRestaurantsFromLocalStorage().length !== 0) {
+    return;
+  }
+
+  setRestaurantsToLocalStorage([
+    {
+      category: "한식",
+      name: "우리김밥",
+      distance: 5,
+    },
+    {
+      category: "일식",
+      name: "너네초밥",
+      distance: 30,
+      description: "정말 맛있는 너네집 초밥!",
+    },
+    {
+      category: "중식",
+      name: "친친",
+      distance: 10,
+      link: "https://map.naver.com",
+    },
+    {
+      category: "아시안",
+      name: "김밥천국",
+      distance: 15,
+      description: "원조 김밥 맛집!",
+      link: "http://map.naver.com",
+    },
+    {
+      category: "한식",
+      name: "비벼비벼비빔밥",
+      distance: 20,
+    },
+  ]);
+})();
+
 const restaurantList = new RestaurantList(getRestaurantsFromLocalStorage());
 
 window.addEventListener("unload", () => {
--- src/util/localStorage.ts
+++ src/util/localStorage.ts
@@ -3,11 +3,10 @@ import { Restaurant } from "../types";
 const RESTAURANTS = "restaurants";
 
 export function getRestaurantsFromLocalStorage(): Restaurant[] {
-  if (!localStorage.getItem(RESTAURANTS)) {
-    localStorage.setItem(RESTAURANTS, JSON.stringify([]));
+  const restaurants = localStorage.getItem(RESTAURANTS);
+  if (restaurants === null) {
+    return [];
   }
-
-  const restaurants = localStorage.getItem(RESTAURANTS) as string;
 
   return JSON.parse(restaurants);
 }
