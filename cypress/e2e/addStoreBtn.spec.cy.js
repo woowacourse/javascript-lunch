8a9206164a9915056acd2d195c0f66e63f3de41b
badahertz52 <badahertz52@gmail.com>
2024-03-11 18:25:22 +0900

    test: 음식점 추가 버튼에 대한 테스트 추가

---
+++ cypress/e2e/addStoreBtn.spec.cy.js
@@ -0,0 +1,18 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/');
+  });
+  it('음식점을 추가하는 버튼이 존재한다.', () => {
+    cy.get('add-store-btn').should('be.visible');
+  });
+
+  it('음식점을 추가하는 버튼을 클릭 하지 않았다면, 입력 폼 모달이 화면에 보이지 않는다.', () => {
+    cy.get('restaurant-form-inner').should('not.be.visible');
+  });
+
+  it('음식점 버튼을 클릭하면, 입력 폼 모달이 화면에 나타난다.', () => {
+    cy.get('add-store-btn').click();
+
+    cy.get('restaurant-form-inner').should('be.visible');
+  });
+});
