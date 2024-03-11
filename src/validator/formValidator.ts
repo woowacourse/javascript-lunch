bea4679950296866e8408ae63b8663c4f0b5d13f
rbgksqkr <rbgks1937@naver.com>
2024-03-11 17:22:26 +0900

    feat(formValidator): 음식점 등록 형식 유효성 검사 로직 추가

---
+++ src/validator/formValidator.ts
@@ -0,0 +1,42 @@
+import type { IRestaurant, TCategory, TDistance } from '@/types/restaurant';
+import dom from '@/utils/dom';
+
+const formValidator = {
+  isValidForm({ category, name, distance, referenceLink }: IRestaurant): boolean {
+    return (
+      this.isValidCategory(category) &&
+      this.isValidName(name) &&
+      this.isValidDistance(distance) &&
+      this.isValidLink(referenceLink)
+    );
+  },
+
+  isValidCategory(category: TCategory): boolean {
+    if (category.trim().length > 0) return true;
+    return false;
+  },
+
+  isValidName(name: string): boolean {
+    if (name.trim().length > 0) return true;
+    return false;
+  },
+
+  isValidDistance(distance: TDistance): boolean {
+    if (distance > 0) return true;
+    return false;
+  },
+
+  isValidLink(link: string | undefined): boolean {
+    if (link === undefined || link.length === 0) return true;
+
+    const $errorLink = dom.getElement('#error-link');
+    if (!(link.startsWith('http://') || link.startsWith('https://'))) {
+      $errorLink.classList.remove('hidden');
+      return false;
+    }
+    $errorLink.classList.add('hidden');
+    return true;
+  },
+};
+
+export default formValidator;
