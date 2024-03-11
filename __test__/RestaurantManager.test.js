5b2dd6ecc87befdd8a101d689933da5d0015e4f6
00kang <kdb070970@gmail.com>
2024-03-11 10:52:46 +0900

    refacotr: 오타 수정

--- __test__/RestaurantManager.test.js
+++ __test__/RestaurantManager.test.js
@@ -1,22 +1,22 @@
 import RestaurantManager from '../src/domain/RestaurantManager';
 
 describe('RestaurantManager 기능 테스트', () => {
-  const RESTARANT_A = {
+  const RESTAURANT_A = {
     category: '분식',
     name: '맛있는 집',
     distance: 15,
   };
-  const RESTARANT_B = {
+  const RESTAURANT_B = {
     category: '기타',
     name: '맛없는 집',
     distance: 10,
   };
-  const RESTARANT_C = {
+  const RESTAURANT_C = {
     category: '분식',
     name: '우리할매 떡볶이',
     distance: 5,
   };
-  const RESTAURANT_INFOS = [RESTARANT_A, RESTARANT_B, RESTARANT_C];
+  const RESTAURANT_INFOS = [RESTAURANT_A, RESTAURANT_B, RESTAURANT_C];
 
   const manager = new RestaurantManager();
   beforeAll(() => {
@@ -34,9 +34,9 @@ describe('RestaurantManager 기능 테스트', () => {
   describe('filteredAndSortedByOptions 기능 테스트', () => {
     test.each([
       { category: '한식', option: 'name', result: [] },
-      { category: '분식', option: 'distance', result: [RESTARANT_C, RESTARANT_A] },
-      { category: '분식', option: 'name', result: [RESTARANT_A, RESTARANT_C] },
-      { category: '전체', option: 'name', result: [RESTARANT_B, RESTARANT_A, RESTARANT_C] },
+      { category: '분식', option: 'distance', result: [RESTAURANT_C, RESTAURANT_A] },
+      { category: '분식', option: 'name', result: [RESTAURANT_A, RESTAURANT_C] },
+      { category: '전체', option: 'name', result: [RESTAURANT_B, RESTAURANT_A, RESTAURANT_C] },
     ])('카테고리와 옵션을 전달받아 정렬한 결과를 반환한다.', ({ category, option, result }) => {
       expect(manager.filteredAndSortedByOptions(category, option)).toEqual(result);
     });
