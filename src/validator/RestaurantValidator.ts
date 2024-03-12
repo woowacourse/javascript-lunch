c3a15e868e728c86ee8172d94bf718a239ac9a54
greetings1012 <ahnqwerty@gmail.com>
2024-03-12 17:24:47 +0900

    refactor: validateUserInput을 category와 minutesWalk 함수로 나누어 관리

--- src/validator/RestaurantValidator.ts
+++ src/validator/RestaurantValidator.ts
@@ -1,14 +1,6 @@
 import { LocationData } from '../constants/typings';
 
 const RestaurantValidator = {
-  validateUserInput(inputData: Object) {
-    const inputLocationData = inputData as LocationData;
-    if (!inputLocationData.category || !inputLocationData.minutesWalk) {
-      throw new Error('값을 입력해주세요.');
-    }
-    this.validateRestaurantName(inputLocationData.name);
-  },
-
   validateRestaurantName(userInput: string) {
     const isNameLengthOutOfRange = userInput.length < 1 || userInput.length > 20;
     if (isNameLengthOutOfRange) {
@@ -18,6 +10,20 @@ const RestaurantValidator = {
     const isNameBlank = [...new Set(userInput)].join('') === ' ';
     if (isNameBlank) {
       throw new Error('공백을 제외하고 1글자 이상 입력해주세요.');
+    }
+  },
+
+  validateRestaurantCategory(inputData: Object) {
+    const inputLocationData = inputData as LocationData;
+    if (!inputLocationData.category) {
+      throw new Error('값을 선택해주세요.');
+    }
+  },
+
+  validateRestaurantMinutesWalk(inputData: Object) {
+    const inputLocationData = inputData as LocationData;
+    if (!inputLocationData.minutesWalk) {
+      throw new Error('값을 선택해주세요.');
     }
   },
 };
