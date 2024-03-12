7055c0b3f8b51a56ee8cf32ad7c52045ec0752c5
anttiey <hanuuny@gmail.com>
2024-03-12 16:07:23 +0900

    fix(Component): 타입스크립트 마이그레이션

---
+++ src/components/Component.ts
@@ -0,0 +1,30 @@
+class Component extends HTMLElement {
+  connectedCallback(): void {
+    if (this.isConnected) {
+      this.render();
+      this.setEvent();
+    }
+  }
+
+  disconnectedCallback(): void {
+    this.removeEvent();
+  }
+
+  render(): void {
+    this.innerHTML = this.template();
+  }
+
+  setEvent(): void {}
+
+  removeEvent(): void {}
+
+  makeCustomEvent(name: string): void {
+    this.dispatchEvent(new CustomEvent(name, { bubbles: true }));
+  }
+
+  template(): string {
+    return ``;
+  }
+}
+
+export default Component;
