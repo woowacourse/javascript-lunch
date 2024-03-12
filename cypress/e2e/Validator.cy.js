43bf6288be55e7a2808548b96cd36956e4a8517b
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-12 20:03:25 +0900

    refactor(test): 테스트 코드 명세 수정

--- cypress/e2e/Validator.cy.js
+++ cypress/e2e/Validator.cy.js
@@ -1,7 +1,7 @@
 import ValidateConditions from '../../src/validators/ValidateConditions.ts';
 
 describe('유효성 검증 테스트', () => {
-  it('배열 안에 값이 포함되어있다면 true를 반환하는 테스트', () => {
+  it('배열 안에 값이 포함되어있는 경우 true를 반환한다.', () => {
     const inputs = [30, '한식'];
     const datas = [
       [5, 10, 15, 20, 30],
@@ -13,7 +13,7 @@ describe('유효성 검증 테스트', () => {
     });
   });
 
-  it('값이 빈 문자열이면 true를 반환하는 테스트', () => {
+  it('값이 빈 문자열인 경우 true를 반환한다.', () => {
     const input = '';
 
     expect(ValidateConditions.isBlank(input)).to.be.true;
