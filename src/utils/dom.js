63df679df25dd9f34b26c4399e13a067b8cb92a1
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-12 17:14:06 +0900

    refactor(dom): null return시 에러 발생 로직 추가

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,1 +1,7 @@
-export const $ = (selector) => document.getElementById(selector);
+export const $ = (selector) => {
+  if (!document.getElementById(selector)) {
+    throw new Error('입력한 셀렉터가 존재하지 않습니다.');
+  }
+
+  return document.getElementById(selector);
+};
