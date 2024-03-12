71dbd66207626843122d5043596c236bf96825ee
anttiey <hanuuny@gmail.com>
2024-03-12 19:45:39 +0900

    fix(RestaurantRepository): 클래스로 변경 후 음식점 목록을 필드로 관리하도록 로직 수정

--- src/domain/RestaurantRepository.ts
+++ src/domain/RestaurantRepository.ts
@@ -1,40 +1,53 @@
-function getRestaurants() {
-  const restaurants = localStorage.getItem('restaurants');
-  return restaurants ? JSON.parse(restaurants) : [];
-}
+class RestaurantRepository {
+  #restaurants;
 
-function getFilteredByCategory(restaurants: IRestaurant[], category: TAllCategory) {
-  return category === '전체' ? restaurants : restaurants.filter((restaurant) => restaurant.category === category);
-}
+  constructor() {
+    this.#restaurants = this.#getRestaurants();
+  }
 
-function getSortedByName(restaurants: IRestaurant[]) {
-  return [...restaurants].sort((a, b) => a.name.localeCompare(b.name));
-}
+  #getRestaurants(): IRestaurant[] {
+    const restaurants = localStorage.getItem('restaurants');
+    return restaurants ? JSON.parse(restaurants) : [];
+  }
 
-function getSortedByDistance(restaurants: IRestaurant[]) {
-  return [...restaurants].sort((a, b) => a.distance - b.distance);
-}
+  #getFilteredByCategory(restaurants: IRestaurant[], category: TAllCategory) {
+    return category === '전체' ? restaurants : restaurants.filter((restaurant) => restaurant.category === category);
+  }
 
-const RestaurantRepository = {
-  addRestaurant(restaurant: IRestaurant) {
-    const restaurants = JSON.parse(localStorage.getItem('restaurants') || '[]');
-    localStorage.setItem('restaurants', JSON.stringify([...restaurants, restaurant]));
-  },
+  #getSortedByName(restaurants: IRestaurant[]) {
+    return [...restaurants].sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  #getSortedByDistance(restaurants: IRestaurant[]) {
+    return [...restaurants].sort((a, b) => a.distance - b.distance);
+  }
+
+  addRestaurant(restaurant: Restaurant) {
+    this.#restaurants = [...this.#restaurants, { key: this.#restaurants.length + 1, ...restaurant }];
+    localStorage.setItem('restaurants', JSON.stringify(this.#restaurants));
+  }
+
+  getRestaurant(key: number) {
+    return this.#restaurants.find((restaurant) => restaurant.key === key);
+  }
 
   removeRestaurant(key: number) {
-    const restaurants = JSON.parse(localStorage.getItem('restaurants') || '[]');
-
-    if (restaurants.length >= key) {
-      localStorage.setItem('restaurants', JSON.stringify(restaurants.slice(key, 1)));
-    }
-  },
+    this.#restaurants = this.#restaurants.slice(key, 1);
+    localStorage.setItem('restaurants', JSON.stringify(this.#restaurants));
+  }
 
   transformRestaurants(category: TAllCategory, sortingOption: TSortingOption) {
-    const restaurants = getRestaurants();
-    const filteredRestaurants = getFilteredByCategory(restaurants, category);
+    const filteredRestaurants = this.#getFilteredByCategory(this.#restaurants, category);
+    return sortingOption === '이름순'
+      ? this.#getSortedByName(filteredRestaurants)
+      : this.#getSortedByDistance(filteredRestaurants);
+  }
 
-    return sortingOption === '이름순' ? getSortedByName(filteredRestaurants) : getSortedByDistance(filteredRestaurants);
-  },
-};
+  toggleFavoriteRestaurant(key: number) {}
 
-export default RestaurantRepository;
+  getFavoriteRestaurants() {
+    return this.#restaurants.filter((restaurant) => restaurant.isFavorite);
+  }
+}
+
+export default new RestaurantRepository();
