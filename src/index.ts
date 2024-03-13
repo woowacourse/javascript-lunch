01edb57cd34b29a4c3acc4bfcb2dae9614425697
rbgksqkr <rbgks1937@naver.com>
2024-03-13 11:04:05 +0900

    chore: 리팩토링 진행한 코드 제거

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,3 @@ import App from './components/App';
 import dom from './utils/dom';
 
 new App(dom.getElement('body'));
-
-// const createModalFormSelect = (restaurantList: RestaurantList): void => {
-//   const $categoryContainer = dom.getElement('#category-container');
-//   new SelectBoxComponent({
-//     $target: $categoryContainer,
-//     props: {
-//       attributes: FORM_CATEGORY_ATTRIBUTE,
-//       options: FORM_CATEGORY,
-//       restaurantList,
-//     },
-//   });
-
-//   const $distanceContainer = dom.getElement('#distance-container');
-//   new SelectBoxComponent({
-//     $target: $distanceContainer,
-//     props: {
-//       attributes: FORM_DISTANCE_ATTRIBUTE,
-//       options: FORM_DISTANCE,
-//       restaurantList,
-//     },
-//   });
-// };
