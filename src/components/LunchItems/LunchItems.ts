c04d5d98cb0e0f34b59f9d453ad631d58854c4a9
이태훈 <rhymint@gmail.com>
2024-03-13 17:31:37 +0900

    fix (LunchItems): merge 충돌이 나던 오류 해결

--- src/components/LunchItems/LunchItems.ts
+++ src/components/LunchItems/LunchItems.ts
@@ -1,17 +1,9 @@
 import './style.css';
-<<<<<<< HEAD
-import '../LunchItem/LunchItem';
-
-import { RestaurantDataProvider } from '../../domain/index';
-
-import { Category, Restaurant, Restaurants, SortBy } from '../../types/index';
-=======
 import LunchItem from '../LunchItem/LunchItem';
 
 import { RestaurantDataProvider } from '../../domain/index';
 
 import { Category, Restaurants, SortBy } from '../../types/index';
->>>>>>> step1
 
 export interface FilterProps {
   category?: Category;
@@ -24,15 +16,6 @@ const LUNCH_ITEMS = `
     </ul>
   </section>
 `;
-<<<<<<< HEAD
-
-const LUNCH_ITEM = (restaurant: Restaurant) => `
-  <lunch-item category="${restaurant.category}" name="${restaurant.name}" distance="${restaurant.distance
-  }" description="${restaurant.description ?? ''}"></lunch-item>
-`;
-
-=======
->>>>>>> step1
 class LunchItems extends HTMLElement {
   connectedCallback() {
     this.render();
@@ -44,22 +27,11 @@ class LunchItems extends HTMLElement {
   }
 
   renderItems(props: FilterProps): void {
-<<<<<<< HEAD
-    const itemHTMLs: string[] = [];
-    this.getRestaurants(props).forEach((restaurant) => {
-      itemHTMLs.push(LUNCH_ITEM(restaurant));
-    });
-    const itemsHTML = this.querySelector('.restaurant-list');
-    if (itemsHTML) {
-      itemsHTML.innerHTML = itemHTMLs.join('');
-    }
-=======
     const container = this.querySelector('.restaurant-list');
     container?.childNodes.forEach((child) => child.remove());
     this.getRestaurants(props).forEach((restaurant) => {
       container?.insertAdjacentElement('beforebegin', new LunchItem(restaurant));
     });
->>>>>>> step1
   }
 
   getRestaurants(props: FilterProps): Restaurants {
