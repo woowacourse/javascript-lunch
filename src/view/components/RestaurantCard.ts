5f99aa4cb135b22e7a44382a9f04e5f34a4cb655
Jeongwoo Park <wahoo9040@naver.com>
2024-03-13 17:39:49 +0900

    feat: 별 UI 구현

--- src/view/components/RestaurantCard.ts
+++ src/view/components/RestaurantCard.ts
@@ -1,5 +1,6 @@
 import { IRestaurantInfo } from '../../domain/Restaurant';
 import restaurantCatalog from '../../domain/RestaurantCatalog';
+import '../../css/likeStar.css';
 
 const IMG_CATEGORY = Object.freeze({
   한식: 'korean',
@@ -32,6 +33,9 @@ class RestaurantCard extends HTMLLIElement {
           <h3 class="restaurant__name text-subtitle">${name}</h3>
           <span class="restaurant__distance text-body">캠퍼스부터 ${distanceFromCampus}분 내</span>
           <p class="restaurant__description text-body">${description}</p>
+          <div class="like-star">
+            <img src="./templates/favorite-icon-lined.png" alt="like-star"/>
+          </div>
         </div>
       `;
   }
