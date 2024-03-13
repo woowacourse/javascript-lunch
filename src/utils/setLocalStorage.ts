966a3b5f422315cd0a3f9e21edde40f100eaac1f
ss0526100 <ss0526100@gmail.com>
2024-03-13 12:03:55 +0900

    fix(setLocalStorage): 미사용 인자 사용

--- src/utils/setLocalStorage.ts
+++ src/utils/setLocalStorage.ts
@@ -1,6 +1,6 @@
 const setLocalStorage = (key: string, item: any) => {
   const stringifiedItem = JSON.stringify(item);
-  localStorage.setItem("restaurants", item);
+  localStorage.setItem(key, stringifiedItem);
 };
 
 export default setLocalStorage;
