6ae4c18b94ea1c3571aac052b56526a5e65c394d
brgndy <brgndy96@gmail.com>
2024-03-14 06:45:28 +0900

    feat: 즐겨찾기 버튼 토글 버튼 기능 추가

--- src/components/restaurantListItem/RestaurantListItem.ts
+++ src/components/restaurantListItem/RestaurantListItem.ts
@@ -1,12 +1,9 @@
 import { RestaurantState } from '../../types';
 
 import generateRestaurantListItemComponent from './renderHandlers';
-import showRestaurantListItemDetail from './showRestaurantListItemDetail';
 
 function RestaurantListItem(restaurant: RestaurantState) {
   const restaurantListItem = generateRestaurantListItemComponent(restaurant);
-
-  showRestaurantListItemDetail();
 
   return restaurantListItem;
 }
---
+++ src/components/restaurantListItem/eventHandlers.ts
@@ -0,0 +1,37 @@
+import RestaurantListStorageService from '../../services/restaurantListStorageService';
+import generateRestaurantListItemComponent from './renderHandlers';
+
+const updateRestaurantListItemUI = (restaurantId: number, newHtml: HTMLElement) => {
+  const existingElement = document.querySelector(`li[data-id="${restaurantId}"]`);
+  if (existingElement && existingElement.parentNode) {
+    existingElement.parentNode.replaceChild(newHtml, existingElement);
+  }
+};
+
+const changeFavoriteState = (restaurantId: number) => {
+  RestaurantListStorageService.patchData(restaurantId);
+  const allRestaurants = RestaurantListStorageService.getData();
+  const targetRestaurant = allRestaurants?.filter((restaurant) => restaurant.id === restaurantId)!;
+  const listComponent = generateRestaurantListItemComponent(targetRestaurant[0]);
+  updateRestaurantListItemUI(restaurantId, listComponent);
+};
+
+const favoriteIconEventPhaseHandler = (event: Event) => {
+  const target = (event.target as Element).closest('.favorited-icon') as HTMLElement;
+  if (target) {
+    const listItem = target.closest('li') as HTMLLIElement;
+    const restaurantId = Number(listItem.dataset.id);
+    changeFavoriteState(restaurantId ?? '');
+  }
+};
+
+const changeFavoriteIconStateHandler = () => {
+  const listContainer = document.querySelector('.restaurant-list') as HTMLLIElement;
+  listContainer.addEventListener('click', (event) => favoriteIconEventPhaseHandler(event));
+};
+
+const changeFavoriteIconState = () => {
+  document.addEventListener('DOMContentLoaded', changeFavoriteIconStateHandler);
+};
+
+export default changeFavoriteIconState;
