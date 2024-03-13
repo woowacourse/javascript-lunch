9d0fb2a510365b057932f5609c2007b22ed9853b
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-13 13:10:43 +0900

    style: like button 스타일 추가

--- src/components/restaurant/like/likeTemplate.ts
+++ src/components/restaurant/like/likeTemplate.ts
@@ -2,7 +2,7 @@ import likeImg from "../../../../templates/favorite-icon-filled.png";
 import unlikeImg from "../../../../templates/favorite-icon-lined.png";
 
 const likeTemplate = (isLike: boolean) => /*html*/ `
-<button type="button" class="like__button" aria-label="즐겨찾기">
+<button type="button" class="restaurant__like-button" aria-label="즐겨찾기">
     ${isLike ? `<img src="${likeImg}" alt="자주 가는 음식점" />` : `<img src="${unlikeImg}" alt="자주 가지 않는 음식점" />`}
 </button>
 `;
