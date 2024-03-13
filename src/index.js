89e98fdd80bc983eb7102e4109b1fd571aa857de
Jeongwoo Park <wahoo9040@naver.com>
2024-03-13 11:50:46 +0900

    feat: like-section UI 추가

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import WebController from './Controller/WebController';
 import Dropdown from './view/components/Dropdown';
 import RestaurantCards from './view/components/RestaurantCards';
 import RestaurantCard from './view/components/RestaurantCard';
+import './view/components/LikeSection';
 
 window.customElements.define('drop-down', Dropdown, { extends: 'select' });
 window.customElements.define('restaurant-cards', RestaurantCards, { extends: 'ul' });
---
+++ src/view/components/LikeSection.ts
@@ -0,0 +1,3 @@
+import '../../css/likeSection.css';
+
+// TODO: likeSection의 로직을 여기서 설정합니다.
