0b84b827bd889761f3cf54da0f0a5c3175a6b8d0
jaeml06 <jaeml0630@gmail.com>
2024-03-14 15:08:37 +0900

    refactor: 자주 가는 음식점 추가 삭제 구현

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -2,33 +2,45 @@ import { ERROR_MESSAGE } from '../constant/error';
 import { Restaurant, Category } from '../interface/Restaurant';
 
 export interface RestaurantManager {
-  add(newRestaurant: Restaurant): void;
+  addRestaurant(newRestaurant: Restaurant): void;
+  addFavoriteRestaurant(favoriteRestaurant: Restaurant): void;
   getRestaurants(): Restaurant[];
   sortByAscendingName(): Restaurant[];
   sortByAscendingWalkingTime(): Restaurant[];
-  filteredRestaurants(category: Category): Restaurant[];
+  filteredRestaurants(): Restaurant[];
+  getUpdatedTotalRsetaurants(): Restaurant[];
+  getUpdatedFavoriteRestaurants(): Restaurant[];
 }
 
 export class RestaurantManager implements RestaurantManager {
-  private restaurants: Restaurant[];
+  private totalRestaurants: Restaurant[];
   private curentSelectedCategory: string;
   private curentSelectedSorting: string;
+  private favoriteRestaurants: Restaurant[];
 
-  constructor(restaurants: Restaurant[] = []) {
-    this.restaurants = [...restaurants];
+  constructor(
+    restaurants: Restaurant[] = [],
+    favoriteRestaurants: Restaurant[] = []
+  ) {
+    this.totalRestaurants = [...restaurants];
+    this.favoriteRestaurants = [...favoriteRestaurants];
     this.curentSelectedCategory = '전체';
     this.curentSelectedSorting = '이름순';
   }
 
-  add(newRestaurant: Restaurant): void {
-    const hasSameName = this.restaurants.some(
+  addRestaurant(newRestaurant: Restaurant): void {
+    const hasSameName = this.totalRestaurants.some(
       ({ name }) => name === newRestaurant.name
     );
-
     if (hasSameName) {
       throw new Error(ERROR_MESSAGE.sameRestaurantName);
     }
-    this.restaurants.push(newRestaurant);
+    this.totalRestaurants.push(newRestaurant);
+    localStorage.setItem('restaurants', JSON.stringify(this.totalRestaurants));
+  }
+
+  addFavoriteRestaurant(favoriteRestaurant: Restaurant): void {
+    this.favoriteRestaurants.push(favoriteRestaurant);
     switch (this.curentSelectedSorting) {
       case '이름순':
         this.sortByAscendingName();
@@ -37,15 +49,16 @@ export class RestaurantManager implements RestaurantManager {
         this.sortByAscendingWalkingTime();
         break;
     }
-    localStorage.setItem('restaurants', JSON.stringify(this.restaurants));
   }
 
   getRestaurants(): Restaurant[] {
-    return [...this.restaurants];
+    return [...this.totalRestaurants];
   }
 
-  sortByAscendingName(): Restaurant[] {
-    const sortingReataurants = this.filteredRestaurants().sort((a, b) => {
+  sortByAscendingName(selectedRestaurants: Restaurant[] = []): Restaurant[] {
+    const sortingReataurants = this.filteredRestaurants(
+      selectedRestaurants
+    ).sort((a, b) => {
       if (a.name < b.name) return -1;
       if (a.name > b.name) return 1;
       return 0;
@@ -54,8 +67,12 @@ export class RestaurantManager implements RestaurantManager {
     return [...sortingReataurants];
   }
 
-  sortByAscendingWalkingTime(): Restaurant[] {
-    const sortingReataurants = this.filteredRestaurants().sort((a, b) => {
+  sortByAscendingWalkingTime(
+    selectedRestaurants: Restaurant[] = []
+  ): Restaurant[] {
+    const sortingReataurants = this.filteredRestaurants(
+      selectedRestaurants
+    ).sort((a, b) => {
       const aTime = Number(a.walkingTime);
       const bTime = Number(b.walkingTime);
 
@@ -78,13 +95,41 @@ export class RestaurantManager implements RestaurantManager {
     this.curentSelectedSorting = curentSelectedSorting;
   }
 
-  filteredRestaurants(): Restaurant[] {
+  filteredRestaurants(selectedRestaurants: Restaurant[] = []): Restaurant[] {
     if (this.curentSelectedCategory === '전체') {
-      return [...this.restaurants];
+      return [...selectedRestaurants];
     }
 
-    return this.restaurants.filter(
+    return selectedRestaurants.filter(
       (restaurant) => restaurant.category === this.curentSelectedCategory
     );
+  }
+
+  getUpdatedTotalRsetaurants(): Restaurant[] {
+    switch (this.curentSelectedSorting) {
+      case '이름순':
+        return this.sortByAscendingName([...this.totalRestaurants]);
+      case '거리순':
+        return this.sortByAscendingWalkingTime([...this.totalRestaurants]);
+    }
+    return [];
+  }
+
+  getUpdatedFavoriteRestaurants(): Restaurant[] {
+    switch (this.curentSelectedSorting) {
+      case '이름순':
+        return this.sortByAscendingName([...this.favoriteRestaurants]);
+      case '거리순':
+        return this.sortByAscendingWalkingTime([...this.favoriteRestaurants]);
+    }
+    return [];
+  }
+
+  removeFavoriteRestaurant(removeRestaurant: Restaurant): Restaurant[] {
+    this.favoriteRestaurants = this.favoriteRestaurants.filter((restaurant) => {
+      return restaurant['name'] !== removeRestaurant['name'];
+    });
+
+    return [...this.favoriteRestaurants];
   }
 }
