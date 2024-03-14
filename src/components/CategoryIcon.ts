e20e9ffc3d07b0e8be3a3e861c58682e7694e662
anttiey <hanuuny@gmail.com>
2024-03-14 19:49:11 +0900

    refactor(CategoryIcon): 카테고리 아이콘 상수화

--- src/components/CategoryIcon.ts
+++ src/components/CategoryIcon.ts
@@ -6,38 +6,28 @@ import asianIcon from '../assets/category-asian.png';
 import westernIcon from '../assets/category-western.png';
 import etcIcon from '../assets/category-etc.png';
 
+const CATEGORY_ICON = {
+  한식: koreanIcon,
+  중식: chineseIcon,
+  일식: japaneseIcon,
+  양식: westernIcon,
+  아시안: asianIcon,
+  기타: etcIcon,
+};
+
 class CategoryIcon extends Component {
   #category: TCategory;
 
   constructor() {
     super();
-    this.#category = (this.getAttribute('category') as TCategory) || '';
-  }
-
-  displayCategoryIcon(category: TCategory) {
-    switch (category) {
-      case '한식':
-        return `<img src=${koreanIcon} alt="한식" class="category-icon" />`;
-      case '중식':
-        return `<img src=${chineseIcon} alt="중식" class="category-icon" />`;
-      case '일식':
-        return `<img src=${japaneseIcon} alt="일식" class="category-icon" />`;
-      case '양식':
-        return `<img src=${westernIcon} alt="양식" class="category-icon" />`;
-      case '아시안':
-        return `<img src=${asianIcon} alt="아시안" class="category-icon" />`;
-      case '기타':
-        return `<img src=${etcIcon} alt="기타" class="category-icon" />`;
-      default:
-        return '';
-    }
+    this.#category = (this.getAttribute('category') as TCategory) ?? '';
   }
 
   template(): string {
     return `
-      <div class="restaurant__category">
-        ${this.displayCategoryIcon(this.#category)}
-      </div>
+      <figure class="restaurant__category">
+        <img src=${CATEGORY_ICON[this.#category]} alt="${this.#category}" class="category-icon" />
+      </figure>
     `;
   }
 }
