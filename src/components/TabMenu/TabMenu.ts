114ea8f1343aae1510283f88a0a56d2784434e34
chysis <chysiss@naver.com>
2024-03-14 20:52:43 +0900

    refactor: 초기 탭 활성화 로직 TabMenu에서 관리

--- src/components/TabMenu/TabMenu.ts
+++ src/components/TabMenu/TabMenu.ts
@@ -1,7 +1,13 @@
+import { DEFAULT_TAB } from '../../constant/constants';
 import { TabMenuProps } from '../../interface/RestaurantInterfaces';
 import { $ } from '../../utils/querySelector';
 
 const TabMenu = (menuList: TabMenuProps[]) => {
+  const initTab = () => {
+    const defaultTab = $(`#${DEFAULT_TAB}`);
+    defaultTab.classList.add('active');
+  };
+
   const render = () => {
     const menuButtons = /* html */ menuList.map(menu => {
       return `<button id=${menu.id} class="button button--tabmenu">${menu.content}</button>`;
@@ -10,6 +16,7 @@ const TabMenu = (menuList: TabMenuProps[]) => {
     const tabContainer = $('.restaurant-tab-container');
 
     tabContainer.insertAdjacentHTML('beforeend', menuButtons.join(''));
+    initTab();
   };
 
   render();
