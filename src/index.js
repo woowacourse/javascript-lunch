c955ecb35cff456ef232dfc3852e269a79c78b5f
greetings1012 <ahnqwerty@gmail.com>
2024-03-14 17:39:56 +0900

    feat: FavoriteNavBar index.js에 import

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './app/modal/AddRestaurantModal/AddRestaurantModal.ts';
 import './app/root/LunchHeader/LunchHeader.ts';
 import './app/root/SelectBoxSection/SelectBoxSection.ts';
 import './app/root/RestaurantItem/RestaurantItem.ts';
+import './app/root/FavoriteNavBar/FavoriteNavBar.ts';
 
 import AppController from './app/controller/AppController.ts';
 
