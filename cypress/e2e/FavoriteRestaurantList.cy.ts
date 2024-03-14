70d1d27966dde75f0cc376f3c4c1e53d4d4a612f
pakxe <pigkill40@naver.com>
2024-03-14 16:13:51 +0900

    test: 즐겨찾는 음식점 목록 테스트 작성

---
+++ cypress/e2e/FavoriteRestaurantList.cy.ts
@@ -0,0 +1,25 @@
+import FavoriteRestaurantList from '../../src/domain/FavoriteRestaurantList';
+
+const favoriteRestaurantList = new FavoriteRestaurantList([]);
+const newRestaurantId = 324;
+
+describe('즐겨찾는 음식점 목록 테스트', () => {
+  it('즐겨 찾는 음식점을 새로 추가한다.', () => {
+    favoriteRestaurantList.add(newRestaurantId);
+
+    expect(favoriteRestaurantList.has(newRestaurantId)).to.eq(true);
+  });
+
+  it('즐겨찾지 않는 음식점으로 설정한다.', () => {
+    favoriteRestaurantList.delete(newRestaurantId);
+
+    expect(favoriteRestaurantList.has(newRestaurantId)).to.eq(false);
+  });
+
+  it('즐겨 찾는 음식점 id 목록을 반환한다.', () => {
+    favoriteRestaurantList.add(1);
+    favoriteRestaurantList.add(2);
+
+    expect(favoriteRestaurantList.getIdList()).to.eql([1, 2]);
+  });
+});
