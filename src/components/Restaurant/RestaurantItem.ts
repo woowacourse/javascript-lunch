9b691e5f1df67ba0461e787ef0c05f15bbc77431
jinsil <jinsil378900@gmail.com>
2024-03-14 11:04:08 +0900

    design: 즐겨찾기 버튼 스타일 추가, common 스타일 분리

--- src/components/Restaurant/RestaurantItem.ts
+++ src/components/Restaurant/RestaurantItem.ts
@@ -9,10 +9,12 @@ const RestaurantItem = (restaurant: Restaurant, index: number) => {
       <img src="./category-${CATEGORY_CONVERTER[restaurant.category]}.png" alt=${restaurant.category} class="category-icon" />
     </div>
     <div class="restaurant__info">   
-      <div>
-        <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
-        <span class="restaurant__distance text-body">캠퍼스부터 ${restaurant.distance}분 내</span>
-        ${FavoriteButton(restaurant.favorite, index)}
+      <div class="restaurant__sub-info">
+        <div>
+          <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
+          <span class="restaurant__distance text-body">캠퍼스부터 ${restaurant.distance}분 내</span>
+        </div>
+        <div>${FavoriteButton(restaurant.favorite, index)}</div>
       </div>
       <p class="restaurant__description text-body">${restaurant.description}</p>
     </div>
