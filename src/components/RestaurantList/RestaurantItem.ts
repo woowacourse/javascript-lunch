24b404ae848d09dbd722f4ce83002fb7e23030f9
skiende74 <skiende74@gmail.com>
2024-03-14 11:52:50 +0900

    fix(RestaurantItem): innerHTML XSS 방지

--- src/components/RestaurantList/RestaurantItem.ts
+++ src/components/RestaurantList/RestaurantItem.ts
@@ -21,18 +21,23 @@ class RestaurantItem extends HTMLLIElement {
   render() {
     this.className = `restaurant ${style.restaurant}`;
     this.innerHTML = `
-    <div is="restaurant-category-icon" category=${this.#category}> </div>
+    <div is="restaurant-category-icon"> </div>
     <div class="restaurant__info ${style.restaurant__info}">
-    <h3 class="restaurant__name text-subtitle ${style.restaurant__name}">${this.#name}</h3>
-    <span class="restaurant__distance text-body  ${style.restaurant__distance}">캠퍼스부터 ${
-      this.#distance
-    }분 내</span>
+    <h3 class="restaurant__name text-subtitle ${style.restaurant__name}"></h3>
+    <span class="restaurant__distance text-body  ${style.restaurant__distance}"></span>
     <p class="restaurant__description text-body ${style.restaurant__description}">
-    ${this.#description ?? ''}
     </p>
     <img is="favorite-icon" style="width:25px; position:absolute; right:10px; top:10px;" />
     </div>
    `;
+
+    this.querySelector('div[is=restaurant-category-icon]')!.setAttribute(
+      'category',
+      this.#category,
+    );
+    this.querySelector('.restaurant__name')!.textContent = `${this.#name}`;
+    this.querySelector('.restaurant__distance')!.textContent = `캠퍼스부터 ${this.#distance}분 내`;
+    this.querySelector('.restaurant__description')!.textContent = `${this.#description ?? ''}`;
   }
 }
 
