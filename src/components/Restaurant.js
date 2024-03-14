dceeb18158cdc9e5d6de51c3b7409c2a6dedb7c7
simhorim <hori5020@gmail.com>
2024-03-14 11:41:15 +0900

    feat(Restaurant): favorite 이미지 소스 및 스타일 추가

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,4 +1,4 @@
-import { CATEGORY_IMG_SRC } from '../constants/filter';
+import { CATEGORY_IMG_SRC, FAVORITE_IMG_SRC } from '../constants/filter';
 
 const RestaurantComponent = {
   render(restaurantList) {
@@ -14,8 +14,15 @@ const RestaurantComponent = {
         <img src=${CATEGORY_IMG_SRC[information.category]} alt=${information.category} class="category-icon" />
       </div>
       <div class="restaurant__info">
-        <h3 class="restaurant__name text-subtitle">${information.name}</h3>
-        <span class="restaurant__distance text-body">캠퍼스부터 ${information.distance}분 내</span>
+        <div class="restaurant__required-info">
+          <div>
+            <h3 class="restaurant__name text-subtitle">${information.name}</h3>
+            <span class="restaurant__distance text-body">캠퍼스부터 ${information.distance}분 내</span>
+          </div>
+          <div>
+            <img src=${FAVORITE_IMG_SRC[information.favorite]} alt=${information.favorite} />
+            </div>
+        </div>
         <p class="restaurant__description text-body">${information.description}</p>
       </div>`;
 
