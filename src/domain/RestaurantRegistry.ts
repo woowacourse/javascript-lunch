1220c4840c3694580d81cf093d53d6a42ef91e76
이태훈 <rhymint@gmail.com>
2024-03-14 15:48:39 +0900

    feat (RestaurantRegistry) : 자주가는 음식점을 등록하는 기능 추가

--- src/domain/RestaurantRegistry.ts
+++ src/domain/RestaurantRegistry.ts
@@ -3,12 +3,18 @@ import { Restaurant } from '../types/Restaurant';
 /**
  * @param {Object} 새로운 가게 등록 후 local에 저장
  */
-const RestaurantRegister = {
+const RestaurantRegistry = {
   registerOneRestaurant(newRestaurant: Restaurant) {
     const allRestaurants: Restaurant[] = JSON.parse(localStorage.getItem('restaurants') ?? '[]');
     allRestaurants.push(newRestaurant);
     localStorage.setItem('restaurants', JSON.stringify(allRestaurants));
   },
+
+  likeOneRestaurant(newRestaurant: Restaurant) {
+    const allRestaurants: Restaurant[] = JSON.parse(localStorage.getItem('liked') ?? '[]');
+    allRestaurants.push(newRestaurant);
+    localStorage.setItem('liked', JSON.stringify(allRestaurants));
+  },
 };
 
-export default RestaurantRegister;
+export default RestaurantRegistry;
