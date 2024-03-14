d5307901a5a4bdde3636623ca35a2fb3a5b74605
greetings1012 <ahnqwerty@gmail.com>
2024-03-14 17:40:22 +0900

    feat: favoriteNavBar event에 bind할 함수 구현

--- src/app/controller/AppController.ts
+++ src/app/controller/AppController.ts
@@ -1,6 +1,6 @@
 import RestaurantService from '../../service/RestaurantService';
 import RestaurantList from '../root/RestaurantList/RestaurantList';
-import { LocationData } from '../../constants/typings';
+import { ILocation } from '../../interface/interface';
 import { Category, Sort } from '../../constants/enums';
 import { $ } from '../../utils/domSelector';
 
@@ -8,18 +8,20 @@ class AppController {
   //TODO: 컴포넌트에 따라 함수 분리하기
   sort: Sort;
   category: Category | '전체';
+  favorite: boolean;
   restaurantService: RestaurantService;
 
   constructor() {
     this.sort = Sort.이름순;
     this.category = '전체';
+    this.favorite = false;
     this.restaurantService = new RestaurantService();
   }
 
   initializeApp() {
     const category = this.category === '전체' ? undefined : this.category;
     const restaurantList: RestaurantList = new RestaurantList(
-      this.restaurantService.getRestaurants(this.sort, category),
+      this.restaurantService.getRestaurants(this.sort, this.favorite, category),
     );
 
     const appElement = document.querySelector('#app');
@@ -35,16 +37,48 @@ class AppController {
       lunchHeader.addEventListener('showAddRestaurantModal', this.showAddRestaurantModal.bind(this));
     }
 
+    const addRestaurantModal = document.querySelector('add-restaurant-modal');
+    if (addRestaurantModal) {
+      addRestaurantModal.addEventListener('submitAddingRestaurant', this.addRestaurant.bind(this));
+    }
+
     const selectBoxSection = document.querySelector('select-box-section');
     if (selectBoxSection) {
       selectBoxSection.addEventListener('changeCategory', this.changeCategory.bind(this));
       selectBoxSection.addEventListener('changeSort', this.changeSort.bind(this));
     }
 
-    const addRestaurantModal = document.querySelector('add-restaurant-modal');
-    if (addRestaurantModal) {
-      addRestaurantModal.addEventListener('submitAddingRestaurant', this.addRestaurant.bind(this));
+    const favoriteNavBar = document.querySelector('favorite-tabnb');
+    if (favoriteNavBar) {
+      favoriteNavBar.addEventListener('showAll', this.showAll.bind(this));
+      favoriteNavBar.addEventListener('showFavorite', this.showFavorite.bind(this));
     }
+  }
+
+  showAll() {
+    this.favorite = false;
+    const showAllButton = $('#show-all');
+    const showFavoriteButton = $('#show-favorite');
+    if (showAllButton && !showAllButton.classList.contains('sel')) {
+      showAllButton.classList.add('sel');
+    }
+    if (showFavoriteButton && showFavoriteButton.classList.contains('sel')) {
+      showFavoriteButton.classList.remove('sel');
+    }
+    this.refreshRestaurantList();
+  }
+
+  showFavorite() {
+    this.favorite = true;
+    const showAllButton = $('#show-all');
+    const showFavoriteButton = $('#show-favorite');
+    if (showFavoriteButton && !showFavoriteButton.classList.contains('sel')) {
+      showFavoriteButton.classList.add('sel');
+    }
+    if (showAllButton && showAllButton.classList.contains('sel')) {
+      showAllButton.classList.remove('sel');
+    }
+    this.refreshRestaurantList();
   }
 
   changeCategory(event: Event) {
@@ -61,7 +95,7 @@ class AppController {
 
   addRestaurant(event: Event) {
     //TODO: 여기에 Validation 연결하기
-    const detail: LocationData = (event as CustomEvent).detail;
+    const detail: ILocation = (event as CustomEvent).detail;
     this.restaurantService.addRestaurant(detail);
     this.refreshRestaurantList();
   }
@@ -72,7 +106,9 @@ class AppController {
     const oldRestaurantList = $('restaurant-list');
     if (appElement && oldRestaurantList) {
       appElement.removeChild(oldRestaurantList);
-      const restaurantList = new RestaurantList(this.restaurantService.getRestaurants(this.sort, category));
+      const restaurantList = new RestaurantList(
+        this.restaurantService.getRestaurants(this.sort, this.favorite, category),
+      );
       appElement.appendChild(restaurantList);
     }
   }
