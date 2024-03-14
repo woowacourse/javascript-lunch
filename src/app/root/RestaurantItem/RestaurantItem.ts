d756b094aa92497f8cca7177c4178f22aa9debc8
greetings1012 <ahnqwerty@gmail.com>
2024-03-14 17:41:25 +0900

    feat: 각 item에 favorite 버튼 구현

--- src/app/root/RestaurantItem/RestaurantItem.ts
+++ src/app/root/RestaurantItem/RestaurantItem.ts
@@ -1,16 +1,25 @@
+import { $ } from '../../../utils/domSelector';
 import { IMAGE } from '../../../assets/assets';
-import { LocationData } from '../../../constants/typings';
+import { ILocation } from '../../../interface/interface';
 
 class RestaurantItem extends HTMLElement {
-  private restaurantData: LocationData;
+  private restaurantData: ILocation;
 
-  constructor(restaurantData: LocationData) {
+  constructor(restaurantData: ILocation) {
     super();
     this.restaurantData = restaurantData;
   }
 
   connectedCallback() {
     this.render();
+    this.addEvent();
+  }
+
+  private addEvent() {
+    const favoriteButton = $('favorite-button');
+    if (favoriteButton) {
+      favoriteButton.addEventListener('click', () => this.dispatchEvent(new CustomEvent('favorite', {})));
+    }
   }
 
   private render() {
@@ -24,6 +33,11 @@ class RestaurantItem extends HTMLElement {
           <h3 class="restaurant__name text-subtitle">${this.restaurantData.name}</h3>
           <span class="restaurant__distance text-body">캠퍼스부터 ${this.restaurantData.minutesWalk}분 내</span>
           <p class="restaurant__description text-body">${this.restaurantData.description}</p>
+        </div>
+        <div>
+          <button type="button" class="favorite-icon" id="favoite-button">
+            <img src="${this.restaurantData.favorite ? IMAGE.url.버튼_즐겨찾기등록됨 : IMAGE.url.버튼_즐겨찾기해제됨}"/>
+          </button>
         </div>
       </li>
     `;
