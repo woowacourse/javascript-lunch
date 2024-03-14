77068968bc7076c172e2afe6051f1c4ac8c268ab
anttiey <hanuuny@gmail.com>
2024-03-14 19:30:45 +0900

    refactor(RestaurantList): 타입 명시 추가

--- src/components/RestaurantList.ts
+++ src/components/RestaurantList.ts
@@ -2,7 +2,7 @@ import Component from './Component';
 import RestaurantRepository from '../domain/RestaurantRepository';
 
 class RestaurantList extends Component {
-  static observedAttributes = ['category', 'sorting', 'type'];
+  static observedAttributes: string[] = ['category', 'sorting', 'type'];
 
   #restaurants: IRestaurant[];
 
@@ -11,15 +11,15 @@ class RestaurantList extends Component {
     this.#restaurants = this.#generateRestaurants();
   }
 
-  attributeChangedCallback() {
+  attributeChangedCallback(): void {
     this.#restaurants = this.#generateRestaurants();
-    this.connectedCallback();
+    this.render();
   }
 
-  #generateRestaurants() {
-    const category = this.getAttribute('category') || '전체';
-    const sorting = this.getAttribute('sorting') || '이름순';
-    const type = this.getAttribute('type') || '모든 음식점';
+  #generateRestaurants(): IRestaurant[] {
+    const category = this.getAttribute('category') ?? '전체';
+    const sorting = this.getAttribute('sorting') ?? '이름순';
+    const type = this.getAttribute('type') ?? '모든 음식점';
 
     return RestaurantRepository.transformRestaurants(
       category as TAllCategory,
@@ -28,11 +28,13 @@ class RestaurantList extends Component {
     );
   }
 
-  template() {
+  template(): string {
     return `
-    <section class="restaurant-list-container">
-      ${this.#restaurants.map((restaurant) => `<restaurant-item key=${restaurant.key}></restaurant-item>`).join('')}
-    </section>
+      <section class="restaurant-list-container">
+        ${this.#restaurants
+          .map((restaurant: IRestaurant) => `<restaurant-item key=${restaurant.key}></restaurant-item>`)
+          .join('')}
+      </section>
     `;
   }
 }
