61a89f6af06e9b847c5ee7dd21ba7e8c08e0e59d
greetings1012 <ahnqwerty@gmail.com>
2024-03-14 17:17:40 +0900

    feat: 즐겨찾기 버튼 png import

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -5,10 +5,14 @@ import restaurantJapaneseIcon from '/src/assets/image/category-japanese.png';
 import restaurantWesternIcon from '/src/assets/image/category-western.png';
 import restaurantAsianIcon from '/src/assets/image/category-asian.png';
 import restaurantEtcIcon from '/src/assets/image/category-etc.png';
+import favoriteIconFilled from '/src/assets/image/favorite-icon-filled.png';
+import favoriteIconLined from '/src/assets/image/favorite-icon-lined.png';
 
 const IMAGE = {
   url: {
     버튼_음식점추가: createRestaurantIcon,
+    버튼_즐겨찾기등록됨: favoriteIconFilled,
+    버튼_즐겨찾기해제됨: favoriteIconLined,
     한식: restaurantKoreanIcon,
     중식: restaurantChineseIcon,
     일식: restaurantJapaneseIcon,
