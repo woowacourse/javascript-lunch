c090e7a0953a5d1bbd8413cf8215844982eee677
Cron <hojoong.choi@woowahan.com>
2024-03-14 10:39:11 +0900

    feat: 템플릿 로드 작업

---
+++ src/resources.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+import "./images/add-button.png";
+import "./images/category-asian.png";
+import "./images/category-chinese.png";
+import "./images/category-etc.png";
+import "./images/category-japanese.png";
+import "./images/category-korean.png";
+import "./images/category-western.png";
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface Restaurant {
+  category: string;
+  name: string;
+  distance: number;
+  description: string;
+}
