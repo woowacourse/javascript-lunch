57728c99adf1045fd94b993f4b4a64b3ff04c406
Largopie <106071687+Largopie@users.noreply.github.com>
2024-03-15 17:24:35 +0900

    refactor(dom): util함수 예외 처리 console로 출력하도록 수정

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,6 +1,6 @@
 export const $ = (selector) => {
   if (!document.getElementById(selector)) {
-    throw new Error('입력한 셀렉터가 존재하지 않습니다.');
+    console.log(`${selector}가 존재하지 않습니다!`);
   }
 
   return document.getElementById(selector);
