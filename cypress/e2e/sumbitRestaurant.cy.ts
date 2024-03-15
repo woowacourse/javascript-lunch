e1cfd556a84c11bea441d98710af1ca1db78f646
Jeongwoo Park <wahoo9040@naver.com>
2024-03-15 14:56:18 +0900

    test: cypress 음식점 제출 테스트

---
+++ cypress/e2e/sumbitRestaurant.cy.ts
@@ -0,0 +1,25 @@
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+
+    cy.get('.restaurant-list')
+      .children()
+      .its('length')
+      .then((count: number) => {
+        cy.get('#add-restaurant-button').click();
+        cy.get('#add-category-select').invoke('val', '한식');
+        cy.get('#name').type('평가옥');
+        cy.get('#add-distance-select').invoke('val', '15');
+        cy.get('#description').type('어복쟁반도 맛있지만 만두전골');
+        cy.get('#link').type(
+          'https://map.naver.com/p/entry/place/12943462?lng=127.0528376&lat=37.5070417&placePath=%2Fhome&entry=plt&searchType=place&c=15.00,0,0,0,dh',
+        );
+        cy.get('.button-container').children().eq(1).click();
+
+        cy.get('.restaurant-list')
+          .children()
+          .its('length')
+          .should('eq', count + 1);
+      });
+  });
+});
