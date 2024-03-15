6bc56cad04c3eb662891bf701516e25ccaea3027
Hyelim Choi <HyelimChoi01@gmail.com>
2024-03-15 20:39:41 +0900

    chore: 폴더 이름 에러 이슈로 생긴 파일 삭제

--- src/components/restaurant_list/RestaurantList.ts
+++
@@ -1,9 +0,0 @@
-import RestaurantListStorageService from "../../services/restaurantListStorageService";
-
-import render from "./renderHandlers";
-
-function RestaurantList() {
-  const filterData = RestaurantListStorageService.getfilteredData();
-  render(filterData);
-}
-export default RestaurantList;
