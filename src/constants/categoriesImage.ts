5ae8e19b027ba8470d5967e042c00a5b1a99fa60
bmj0053 <bmj05378@gmail.com>
2024-03-15 19:10:26 +0900

    feat: CATEGORY_IMG 타입 추가

--- src/constants/categoriesImage.ts
+++ src/constants/categoriesImage.ts
@@ -1,6 +1,15 @@
 import { ASIAN, CHINESE, ETC, JAPANESE, KOREAN, WESTERN } from '../imgs/index';
 
-export const CATEGORY_IMG = {
+type CategoryImageType = {
+  한식: typeof KOREAN;
+  중식: typeof CHINESE;
+  일식: typeof JAPANESE;
+  아시안: typeof ASIAN;
+  양식: typeof WESTERN;
+  기타: typeof ETC;
+};
+
+export const CATEGORY_IMG: CategoryImageType = {
   한식: KOREAN,
   중식: CHINESE,
   일식: JAPANESE,
