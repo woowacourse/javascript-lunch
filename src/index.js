f47590ee118982e30b56ce0e50708293e43eb170
jayming66 <mjw8941@gmail.com>
2024-03-15 09:35:49 +0900

    fix: FormModal import 제거

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import '../style.css';
 
 import RestaurantList from './components/RestaurantList';
 import RestaurantCard from './components/RestaurantCard';
-import FormModal from './components/FormModal';
 import Dropdown from './components/Dropdown/Dropdown';
 
 window.customElements.define('drop-down', Dropdown, { extends: 'select' });
 window.customElements.define('restaurant-list', RestaurantList, { extends: 'ul' });
 window.customElements.define('restaurant-item', RestaurantCard, { extends: 'li' });
-window.customElements.define('form-modal', FormModal, { extends: 'div' });
 
 new App().run();
