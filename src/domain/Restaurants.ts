d789ce50cc240fe3e700490cce1ea4c83f9ca5fc
Lee sang Yeop <mma7710@naver.com>
2024-03-15 17:11:33 +0900

    refactor(Restaurant): 레스토랑 클래스 내부의 #(private 필드)을 private(ts 접근제어)으로 변경

--- src/domain/Restaurants.ts
+++ src/domain/Restaurants.ts
@@ -1,24 +1,30 @@
 import Restaurant, { RestaurantInfo } from "./Restaurant";
 
 class Restaurants {
-  #restaurants: Restaurant[] = [];
+  protected restaurants: Restaurant[] = [];
 
   constructor(restaurants: Restaurant[]) {
-    this.#validateUniqueName(restaurants);
-    this.#restaurants = restaurants;
+    this.validateUniqueName(restaurants);
+    this.restaurants = restaurants;
   }
 
   add(restaurant: Restaurant): void {
-    this.#validateUniqueName([...this.#restaurants, restaurant]);
+    this.validateUniqueName([...this.restaurants, restaurant]);
 
-    this.#restaurants = [...this.#restaurants, restaurant];
+    this.restaurants = [...this.restaurants, restaurant];
+  }
+
+  remove(restaurant: Restaurant): void {
+    this.restaurants = this.restaurants.filter(
+      (item) => JSON.stringify(item) !== JSON.stringify(restaurant)
+    );
   }
 
   getDetails(): RestaurantInfo[] {
-    return this.#restaurants.map((restaurant) => restaurant.getInfo());
+    return this.restaurants.map((restaurant) => restaurant.getInfo());
   }
 
-  #validateUniqueName(restaurants: Restaurant[]) {
+  private validateUniqueName(restaurants: Restaurant[]) {
     const names = restaurants.map((restaurant) => restaurant.getName());
 
     if (new Set(names).size !== names.length) {
