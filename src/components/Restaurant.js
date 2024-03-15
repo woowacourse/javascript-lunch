059cd3f0cc0abbc8df836d15c309d3b7b4c4f180
simhorim <hori5020@gmail.com>
2024-03-16 02:38:02 +0900

    refactor(Restaurant): innerHTML 방식을 노드 생성과 추가 방식으로 수정

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -7,27 +7,122 @@ const RestaurantComponent = {
       $restaurantList.appendChild(this.mounted(element.information));
     });
   },
+
   mounted(information) {
-    const $li = document.createElement('li');
-    $li.classList.add('restaurant');
+    const $restaurant = document.createElement('li');
+    const $restaurantCategory = this.createRestaurantCategory(information);
+    const $restaurantInfo = this.createRestaurantInfo(information);
 
-    $li.innerHTML = /* html */ `<div class="restaurant__category">
-        <img src=${CATEGORY_IMG_SRC[information.category]} alt=${information.category} class="category-icon" />
-      </div>
-      <div class="restaurant__info">
-        <div class="restaurant__required-info">
-          <div>
-            <h3 class="restaurant__name text-subtitle">${information.name}</h3>
-            <span class="restaurant__distance text-body">캠퍼스부터 ${information.distance}분 내</span>
-          </div>
-          <div>
-            <img src=${FAVORITE_IMG_SRC[information.favorite]} alt=${information.favorite} class="favorite-icon" />
-            </div>
-        </div>
-        <p class="restaurant__description text-body">${information.description}</p>
-      </div>`;
+    $restaurant.classList.add('restaurant');
 
-    return $li;
+    $restaurant.appendChild($restaurantCategory);
+    $restaurant.appendChild($restaurantInfo);
+
+    return $restaurant;
+  },
+
+  createRestaurantCategory(information) {
+    const $restaurantCategory = document.createElement('div');
+    const $categoryIcon = this.createCategoryIcon(information);
+
+    $restaurantCategory.classList.add('restaurant__category');
+
+    $restaurantCategory.appendChild($categoryIcon);
+
+    return $restaurantCategory;
+  },
+
+  createCategoryIcon(information) {
+    const $categoryIcon = document.createElement('img');
+
+    $categoryIcon.setAttribute('src', `${CATEGORY_IMG_SRC[information.category]}`);
+    $categoryIcon.setAttribute('alt', `${information.category}`);
+    $categoryIcon.classList.add('category-icon');
+
+    return $categoryIcon;
+  },
+
+  createRestaurantInfo(information) {
+    const $restaurantInfo = document.createElement('div');
+    const $restaurantRequiredInfo = this.createRestaurantRequiredInfo(information);
+    const $restaurantDescription = this.createRestaurantDescription(information);
+
+    $restaurantInfo.classList.add('restaurant__info');
+
+    $restaurantInfo.appendChild($restaurantRequiredInfo);
+    $restaurantInfo.appendChild($restaurantDescription);
+
+    return $restaurantInfo;
+  },
+
+  createRestaurantRequiredInfo(information) {
+    const $restaurantRequiredInfo = document.createElement('div');
+    const $restaurantTitleContainer = this.createRestaurantTitleContainer(information);
+    const $favoriteContainer = this.createFavoriteContainer(information);
+
+    $restaurantRequiredInfo.classList.add('restaurant__required-info');
+
+    $restaurantRequiredInfo.appendChild($restaurantTitleContainer);
+    $restaurantRequiredInfo.appendChild($favoriteContainer);
+
+    return $restaurantRequiredInfo;
+  },
+
+  createRestaurantTitleContainer(information) {
+    const $restaurantTitleContainer = document.createElement('div');
+    const $restaurantName = this.createRestaurantName(information);
+    const $restaurantDistance = this.createRestaurantDistance(information);
+
+    $restaurantTitleContainer.appendChild($restaurantName);
+    $restaurantTitleContainer.appendChild($restaurantDistance);
+
+    return $restaurantTitleContainer;
+  },
+
+  createRestaurantName(information) {
+    const $restaurantName = document.createElement('h3');
+
+    $restaurantName.classList.add('restaurant__name', 'text-subtitle');
+    $restaurantName.textContent = `${information.name}`;
+
+    return $restaurantName;
+  },
+
+  createRestaurantDistance(information) {
+    const $restaurantDistance = document.createElement('span');
+
+    $restaurantDistance.classList.add('restaurant__distance', 'text-body');
+    $restaurantDistance.textContent = `캠퍼스부터 ${information.distance}분 내`;
+
+    return $restaurantDistance;
+  },
+
+  createFavoriteContainer(information) {
+    const $favoriteContainer = document.createElement('div');
+    const $favoriteIcon = this.createFavoriteIcon(information);
+
+    $favoriteContainer.appendChild($favoriteIcon);
+
+    return $favoriteContainer;
+  },
+
+  createFavoriteIcon(information) {
+    const $favoriteIcon = document.createElement('img');
+
+    $favoriteIcon.setAttribute('src', `${FAVORITE_IMG_SRC[information.favorite]}`);
+    $favoriteIcon.setAttribute('alt', `${information.favorite}`);
+    $favoriteIcon.classList.add('favorite-icon');
+
+    return $favoriteIcon;
+  },
+
+  createRestaurantDescription(information) {
+    const $restaurantDescription = document.createElement('p');
+
+    $restaurantDescription.classList.add('restaurant__description', 'text-body');
+    $restaurantDescription.textContent = `${information.description}`;
+
+    return $restaurantDescription;
   },
 };
 
