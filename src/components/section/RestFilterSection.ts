90411ff5fe28ecfed766bf5b4671e4a8d8c870c7
vi-wolhwa <cerezo00@naver.com>
2024-03-16 18:30:58 +0900

    docs: RestFilterSection 주석

--- src/components/section/RestFilterSection.ts
+++ src/components/section/RestFilterSection.ts
@@ -2,15 +2,19 @@ import { FILTER_OPTIONS } from '../../constants/filter';
 import { createSelect } from '../unit/Select';
 
 export const createRestFilterSection = (): HTMLElement => {
+  /* section */
   const $section = document.createElement('section');
   $section.classList.add('rest-filter-section');
 
+  /* 카테고리 필터 */
   const $categoryFilter = createSelect({
     optionData: FILTER_OPTIONS.category
   });
 
+  /* 정렬 필터 */
   const $sortingFilter = createSelect({ optionData: FILTER_OPTIONS.sortingKey });
 
+  /* 컴포넌트 조립 */
   $section.appendChild($categoryFilter);
   $section.appendChild($sortingFilter);
 
