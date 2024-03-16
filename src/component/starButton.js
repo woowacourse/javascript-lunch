bd4fc4197a97831e5bd2617ff5f9cdedb98ff08a
jaeml06 <jaeml0630@gmail.com>
2024-03-16 17:55:06 +0900

    refactor: 별 버튼 컴포넌트 구현

---
+++ src/component/starButton.js
@@ -0,0 +1,21 @@
+export function createStarButton(name, favoriteRestaurantNames, starCallback) {
+  const starButton = render(name, favoriteRestaurantNames);
+  if (starCallback)
+    starButton.addEventListener('click', (event) => {
+      starCallback(event);
+    });
+  return starButton;
+}
+
+function render(name, favoriteRestaurantNames) {
+  const star = document.createElement('img');
+  if (favoriteRestaurantNames.includes(name)) {
+    star.className = 'star filled';
+    star.src = './favorite-icon-filled.png';
+  } else {
+    star.className = 'star lined';
+    star.src = './favorite-icon-lined.png';
+  }
+  star.alt = '추천별';
+  return star;
+}
