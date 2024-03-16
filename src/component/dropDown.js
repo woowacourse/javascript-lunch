5b3548e0d28b23e90eb87f6c3dc8da0fcf50e66e
pakxe <pigkill40@naver.com>
2024-03-17 02:28:46 +0900

    feat: 선택된 옵션의 경우 선택되었다고 클래스를 추가하는 기능 구현

--- src/component/dropDown.js
+++ src/component/dropDown.js
@@ -1,28 +1,35 @@
-import { $ } from '../utils/selector.js';
-
 function createDropDown({ id, callback, options, className, required, cover }) {
-  const dropdown = render({ options, id, className, required ,cover});
+  const dropdown = render({ options, id, className, required, cover });
 
   if (!callback) return dropdown;
 
-  dropdown.addEventListener('change', () => {
-    const selectedOption = dropdown.value; // 선택된 옵션 값
-    callback(selectedOption); // 콜백 함수 호출
+  dropdown.addEventListener('change', (event) => {
+    const $previouslySelectedOption =
+      dropdown.querySelector('.selected__option');
+    $previouslySelectedOption.classList.remove('selected__option');
+
+    const selectedOptionText = dropdown.value;
+    const $selectedOption = event.target.querySelector(
+      `[value=${selectedOptionText}]`
+    );
+    $selectedOption.classList.add('selected__option');
+
+    callback(selectedOptionText); // 콜백 함수 호출
   });
 
   return dropdown;
 }
 
 // REFACTOR: createElement로 수정해야한다.
-function render({ options, id, className, required,  cover}) {
+function render({ options, id, className, required, cover }) {
   const select = document.createElement('select');
   select.id = id;
   select.className = className;
   select.required = required;
 
-  if(!!cover) {
+  if (!!cover) {
     const option = document.createElement('option');
-    option.value = ''
+    option.value = '';
     option.textContent = cover;
     select.appendChild(option);
   }
@@ -32,8 +39,11 @@ function render({ options, id, className, required,  cover}) {
 }
 
 function createSelectOptions(select, options) {
-  options.forEach((optionValue) => {
+  options.forEach((optionValue, i) => {
     const option = document.createElement('option');
+
+    if (i === 0) option.classList.add('selected__option');
+
     option.value = optionValue;
     option.textContent = optionValue;
     select.appendChild(option);
