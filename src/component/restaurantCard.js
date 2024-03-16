6cb200e44ca4f929cb8c294f7ed4e163ae0bc6e1
jaeml06 <jaeml0630@gmail.com>
2024-03-16 18:08:15 +0900

    refactor: RestaurantCard 매개변수 변경

--- src/component/restaurantCard.js
+++ src/component/restaurantCard.js
@@ -1,33 +1,39 @@
-import { KOREAN_CATEGORY } from "../constant/select";
+import { KOREAN_CATEGORY } from '../constant/select';
+import { createCategoryImage } from './categoryImage';
+import { createStarButton } from './starButton';
 
 function createRestaurantCard(restaurant, favoriteRestaurantNames = []) {
   const restaurantCard = render(restaurant, favoriteRestaurantNames);
   return restaurantCard;
 }
 
-function render({ category, name, walkingTime, description = '' }, favoriteRestaurantNames) {
-  const categoryDiv = document.createElement('div');
-  categoryDiv.className = 'restaurant__category';
-  categoryDiv.append(createCategoryImage(category))
+function render(
+  { category, name, walkingTime, description = '' },
+  favoriteRestaurantNames
+) {
+  const categoryDiv = createCategoryImage(category);
   const fragment = new DocumentFragment();
-  fragment.append(categoryDiv, createInfoDiv({ name, walkingTime, description }, favoriteRestaurantNames));
+  fragment.append(
+    categoryDiv,
+    createInfoDiv({ name, walkingTime, description }, favoriteRestaurantNames)
+  );
   return fragment;
 }
 
-function createInfoDiv({ name, walkingTime, description = '' }, favoriteRestaurantNames) {
+function createInfoDiv(
+  { name, walkingTime, description = '' },
+  favoriteRestaurantNames
+) {
   const infoDiv = document.createElement('div');
   infoDiv.className = 'restaurant__info';
 
   const infoHeader = document.createElement('div');
-  infoHeader.className = 'restaurant__info__header'
+  infoHeader.className = 'restaurant__info__header';
 
   const infoHeaderTextArea = document.createElement('div');
   infoHeaderTextArea.className = 'restaurant__info';
-  
-  const star = document.createElement('img');
-  star.className = 'star lined'
-  star.src = favoriteRestaurantNames.includes(name) ? './favorite-icon-filled.png' : './favorite-icon-lined.png' 
-  star.alt = '추천별'
+
+  const star = createStarButton(name, favoriteRestaurantNames);
 
   const restaurantName = document.createElement('h3');
   restaurantName.className = 'restaurant__name text-subtitle';
@@ -40,7 +46,7 @@ function createInfoDiv({ name, walkingTime, description = '' }, favoriteRestaura
   const restaurantDescription = document.createElement('p');
   restaurantDescription.className = 'restaurant__description text-body';
   restaurantDescription.textContent = description;
-  
+
   infoHeaderTextArea.append(restaurantName, restaurantDistance);
   infoHeader.append(infoHeaderTextArea, star);
   infoDiv.append(infoHeader, restaurantDescription);
@@ -48,15 +54,4 @@ function createInfoDiv({ name, walkingTime, description = '' }, favoriteRestaura
   return infoDiv;
 }
 
-function createCategoryImage(category) {
-  const categoryImg = document.createElement('img');
-  categoryImg.src = `./category-${KOREAN_CATEGORY[category]}.png`;
-  categoryImg.alt = category;
-  categoryImg.className = 'category-icon';
-
-  return categoryImg;
-}
-
 export default createRestaurantCard;
-
-
