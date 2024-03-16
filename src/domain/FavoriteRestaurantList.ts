4c327b30955e8104699da9eea7920831df0e7492
pakxe <pigkill40@naver.com>
2024-03-17 03:04:25 +0900

    feat: 즐겨찾기를 토글하는 로직 구현

--- src/domain/FavoriteRestaurantList.ts
+++ src/domain/FavoriteRestaurantList.ts
@@ -1,38 +1,54 @@
 interface FavoriteRestaurantList {
-  add(restaurantId: number): void;
-  has(restaurantId: number): void;
-  delete(restaurantId: number): void;
+  addRestaurant(restaurantId: number): void;
+  hasRestaurant(restaurantId: number): boolean;
+  deleteRestaurant(restaurantId: number): void;
   getIdList(): number[];
+  toggleRestaurant(restaurantId: number): void;
 }
 
 class FavoriteRestaurantList implements FavoriteRestaurantList {
   private favoriteRestaurantList: Set<number>;
 
-  constructor(idList: number[]) {
-    this.favoriteRestaurantList = new Set([...idList]);
+  constructor(idList: string[]) {
+    this.favoriteRestaurantList = new Set([...idList.map(Number)]);
   }
 
-  add(restaurantId: number) {
+  addRestaurant(restaurantId: number) {
     if (this.favoriteRestaurantList.has(restaurantId))
       throw new Error('이미 즐겨찾기 한 음식점입니다.');
 
     this.favoriteRestaurantList.add(restaurantId);
+
+    localStorage.setItem(
+      'favoriteRestaurantList',
+      JSON.stringify([...this.favoriteRestaurantList])
+    );
   }
 
-  has(restaurantId: number) {
+  hasRestaurant = (restaurantId: number): boolean => {
     return this.favoriteRestaurantList.has(restaurantId);
-  }
+  };
 
-  delete(restaurantId: number): void {
+  deleteRestaurant(restaurantId: number): void {
     if (!this.favoriteRestaurantList.has(restaurantId))
       throw new Error('즐겨찾는 음식점 목록에 없는 음식점은 지울 수 없습니다.');
 
     this.favoriteRestaurantList.delete(restaurantId);
+
+    localStorage.setItem(
+      'favoriteRestaurantList',
+      JSON.stringify([...this.favoriteRestaurantList])
+    );
   }
 
   getIdList(): number[] {
     return [...this.favoriteRestaurantList];
   }
+
+  toggleRestaurant = (restaurantId: number): void => {
+    if (this.hasRestaurant(restaurantId)) this.deleteRestaurant(restaurantId);
+    else this.addRestaurant(restaurantId);
+  };
 }
 
 export default FavoriteRestaurantList;
