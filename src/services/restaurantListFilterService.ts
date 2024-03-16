ede32758b6b41e5a98f73425ab7f7960f29e45ee
brgndy <brgndy96@gmail.com>
2024-03-17 08:42:50 +0900

    feat: filter sort 필터 관련 서비스레이어 생성

---
+++ src/services/restaurantListFilterService.ts
@@ -0,0 +1,29 @@
+// RestaurantListFilterService.ts
+import { RestaurantState } from '../types/index.d';
+import restaurantListHelper from '../helpers/RestaurantListHelper';
+import filterState from '../store/FilterStateStore';
+
+const RestaurantListFilterService = (function () {
+  function getDataFromFilterCategoryValue(data: RestaurantState[]) {
+    const filterDataByCategory = restaurantListHelper.filterByCategory(filterState.getFilterInfo().filter, data);
+    return filterDataByCategory || [];
+  }
+
+  function getDataFromNameOrDistanceSelectValue(data: RestaurantState[]) {
+    const sortedData = restaurantListHelper.sortBySelectedValue(filterState.getFilterInfo().sort, data);
+    return sortedData || [];
+  }
+
+  function getFilteredData(data: RestaurantState[]) {
+    const filteredData = getDataFromFilterCategoryValue(data);
+    return getDataFromNameOrDistanceSelectValue(filteredData);
+  }
+
+  return {
+    getDataFromFilterCategoryValue,
+    getDataFromNameOrDistanceSelectValue,
+    getFilteredData,
+  };
+})();
+
+export default RestaurantListFilterService;
