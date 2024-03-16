2f3173c9c68f1a5431e20a5651e457f0244901b3
vi-wolhwa <cerezo00@naver.com>
2024-03-16 14:37:35 +0900

    refactor:  HTML요소 네이밍 컨벤션 ($ prefix)

--- src/components/AppHeader.ts
+++ src/components/AppHeader.ts
@@ -1,26 +1,23 @@
+import { createImageButton } from './ImageButton';
+
 interface Props {
   titleTextContent: string;
   buttonImageSource: string;
 }
 
 export const createAppHeader = ({ titleTextContent, buttonImageSource }: Props): HTMLElement => {
-  const header = document.createElement('header');
-  const title = document.createElement('h1');
-  const button = document.createElement('button');
-  const buttonImage = document.createElement('img');
+  const $header = document.createElement('header');
+  const $title = document.createElement('h1');
+  const $imageButton = createImageButton({ imageSource: buttonImageSource });
 
-  header.classList.add('app-header');
-  title.classList.add('app-header__title', 'text-title');
-  title.textContent = titleTextContent;
-  button.classList.add('app-header__button');
-  buttonImage.src = buttonImageSource;
-  buttonImage.alt = '음식점 추가';
+  $header.classList.add('app-header');
+  $title.classList.add('app-header__title', 'text-title');
+  $title.textContent = titleTextContent;
 
-  button.appendChild(buttonImage);
-  header.appendChild(title);
-  header.appendChild(button);
+  $header.appendChild($title);
+  $header.appendChild($imageButton);
 
-  console.log(header);
+  console.log($header);
 
-  return header;
+  return $header;
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createAppHeader } from './components/AppHeader.ts';
 import { IMAGE_MAP } from './constants/imageMap';
 import { $ } from './utils/dom';
 
-const app = $('#app');
-const appHeader = createAppHeader({ titleTextContent: '점심 뭐 먹지', buttonImageSource: IMAGE_MAP.addButton });
+const $app = $('#app');
+const $appHeader = createAppHeader({ titleTextContent: '점심 뭐 먹지', buttonImageSource: IMAGE_MAP.addButton });
 
-app.appendChild(appHeader);
+$app.appendChild($appHeader);
