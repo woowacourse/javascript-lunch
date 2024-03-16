0d7d5fe2d0e5745f97b131abbeb32a7c01487625
jaeml06 <jaeml0630@gmail.com>
2024-03-16 18:19:10 +0900

    refactor: tabBar 컨밴션 준수

--- src/component/tabBar.js
+++ src/component/tabBar.js
@@ -1,24 +1,24 @@
-import { $ } from "../utils/selector";
+import { $ } from '../utils/selector';
 
-export function createtabBar(tabs = []){
-    const tabBarsDiv = document.createElement('div');
-    tabBarsDiv.className = 'tab__bar';
-    tabBarsDiv.append(...render(tabs));
-    return tabBarsDiv;
+export function createtabBar(tabs = []) {
+  const tabBarsDiv = document.createElement('div');
+  tabBarsDiv.className = 'tab__bar';
+  tabBarsDiv.append(...render(tabs));
+  return tabBarsDiv;
 }
 
-function render(tabs){
-    return tabs.map(({className, text, callback}) => {
-        const tabItem = document.createElement('div');
-        tabItem.className = className;
-        tabItem.textContent = text;
-        tabItem.addEventListener('click', (event) => {
-            callback();
-            if(tabItem.className.includes('tab__bar__item')){
-                $('.tab__bar__item__checked').className = 'tab__bar__item';
-                tabItem.className = 'tab__bar__item__checked';
-            }
-        })
-        return tabItem;
-        }); 
+function render(tabs) {
+  return tabs.map(({ className, text, callback }) => {
+    const tabItem = document.createElement('div');
+    tabItem.className = className;
+    tabItem.textContent = text;
+    tabItem.addEventListener('click', (event) => {
+      callback();
+      if (tabItem.className.includes('tab__bar__item')) {
+        $('.tab__bar__item__checked').className = 'tab__bar__item';
+        tabItem.className = 'tab__bar__item__checked';
+      }
+    });
+    return tabItem;
+  });
 }
