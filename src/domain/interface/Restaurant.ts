a5301c29bc99c926797b294e097505f35568d269
pakxe <pigkill40@naver.com>
2024-03-17 03:04:03 +0900

    rename: 인터페이스 이름 수정

--- src/domain/interface/Restaurant.ts
+++ src/domain/interface/Restaurant.ts
@@ -1,7 +1,8 @@
 export type Category = '한식' | '중식' | '일식' | '아시안' | '양식' | '기타';
 export type WalkingTime = 5 | 10 | 15 | 20 | 30;
 
-export interface IRestaurant {
+export interface Restaurant {
+  id: number;
   category: Category;
   name: string;
   walkingTime: WalkingTime;
