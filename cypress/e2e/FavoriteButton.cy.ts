4b5a682fe3cba9859804639116771b131d9e3310
ooherin <555ohr@naver.com>
2024-03-16 23:41:36 +0900

    test(e2e): 즐겨찾기 동작 e2e 테스트 추가

--- cypress/e2e/FavoriteButton.cy.ts
+++ cypress/e2e/FavoriteButton.cy.ts
@@ -3,8 +3,54 @@ describe('즐겨찾기 버튼 작동 테스트', () => {
     cy.visit('http://localhost:8080/');
   });
 
-  it('메인 페이지에서 음식점의 즐겨찾기 버튼을 누르면 별의 색이 변하고 isFavorite 속성이 바뀐다.', () => {});
-  it('디테일 페이지에서 음식점의 즐겨찾기 버튼을 누르면 별의 색이 변하고 isFavorite 속성이 바뀐다.', () => {});
-  it('디테일 페이지에서 음식점의 즐겨찾기 버튼을 누르면 메인 페이지의 별도 바뀐다.', () => {});
-  it('자주 가는 음식점에서 즐겨찾기를 해제하면 해당 음식점은 리스트에서 사라진다.');
+  const filledStarImg = 'http://localhost:8080/favorite-icon-filled.png';
+  const nofilledStarImg = 'http://localhost:8080/favorite-icon-lined.png';
+
+  it('메인 페이지에서 음식점의 즐겨찾기 버튼을 누르면 별의 색이 빨갛게 변한다.', () => {
+    cy.get('.favorite-button').first().click();
+    cy.get('.favorite-button')
+      .first()
+      .then(($restaurant) => {
+        const $star = $restaurant.find('.star');
+        if ($star.length > 0) {
+          expect($star.attr('src')).to.equal(filledStarImg);
+        }
+      });
+  });
+  it('메인 페이지에서 음식점의 즐겨찾기 버튼을 해제하면 별이 빈별로 변한다.', () => {
+    cy.get('.favorite-button').first().click();
+    cy.get('.favorite-button').first().click();
+    cy.get('.favorite-button')
+      .first()
+      .then(($restaurant) => {
+        const $star = $restaurant.find('.star');
+        expect($star.attr('src')).to.equal(nofilledStarImg);
+      });
+  });
+  it('디테일 페이지에서 음식점의 즐겨찾기 버튼을 누르면 메인 페이지의 별도 바뀐다.', () => {
+    cy.get('.restaurant').first().click();
+    cy.get('.restaurant-detail').find('.star').click();
+
+    cy.get('.restaurant-detail').then(($restaurant) => {
+      const $star = $restaurant.find('.star');
+      expect($star.attr('src')).to.equal(filledStarImg);
+    });
+
+    cy.get('.favorite-button')
+      .first()
+      .then(($restaurant) => {
+        const $star = $restaurant.find('.star');
+        if ($star.length > 0) {
+          expect($star.attr('src')).to.equal(filledStarImg);
+        }
+      });
+  });
+
+  it('자주 가는 음식점에서 즐겨찾기를 해제하면 해당 음식점은 리스트에서 사라진다.', () => {
+    cy.get('.favorite-button').first().click();
+    cy.get('#favorite-tab').click();
+    cy.get('.favorite-button').first().click();
+
+    cy.get('.restaurant').should('not.exist');
+  });
 });
