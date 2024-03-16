56a83404b33d2dd7ad5402a088a39993dd6d8daf
hanyoung <phanyoung@naver.com>
2024-03-16 17:15:13 +0900

    refactor(RestaurantForm): RestaurantForm의 닫기 버튼의 id를 변경

--- src/components/RestaurantForm.ts
+++ src/components/RestaurantForm.ts
@@ -65,7 +65,7 @@ export default class RestaurantForm extends EventComponent {
       </form-item>
 
       <div class="button-container">
-        <button id="close-button" type="button" class="button button--secondary text-caption">취소하기</button>
+        <button id="restaurant-form-close-button" type="button" class="button button--secondary text-caption">취소하기</button>
         <button class="button button--primary text-caption">추가하기</button>
       </div>
     </form>
@@ -75,7 +75,10 @@ export default class RestaurantForm extends EventComponent {
   protected setEvent() {
     this.addEventListener("submit", this.handleSubmit);
 
-    $("#close-button")?.addEventListener("click", this.handleCloseButtonClick);
+    $("#restaurant-form-close-button")?.addEventListener(
+      "click",
+      this.handleCloseButtonClick
+    );
   }
 
   private handleCloseButtonClick() {
