d3491ebda2039760541d44f1a882373e6aeac5c7
pakxe <pigkill40@naver.com>
2024-03-16 23:38:42 +0900

    feat: 리스트 길이가 0이면 음식점이 존재하지 않는다고 안내 구현

--- src/component/restaurantList.js
+++ src/component/restaurantList.js
@@ -1,25 +1,64 @@
+import { toggleStarButton } from './iconButtons/starButton.js';
+import { openModal } from './modal/modal.js';
 import createRestaurantCard from './restaurantCard.js';
 
-function renderRestaurantList(restaurantList) {
+function createRestaurantList({ restaurantList, toggleFavorite, hasFavorite }) {
   const restaurantListContainer = document.querySelector(
     '.restaurant-list-container'
   );
 
   restaurantListContainer.replaceChildren();
 
-  const restaurantList = document.createElement('ul');
-  restaurantList.className = 'restaurant-list';
+  if (restaurantList.length === 0) {
+    const alertElement = document.createElement('div');
+    alertElement.textContent = '음식점이 존재하지 않습니다.';
+
+    restaurantListContainer.append(alertElement);
+    return;
+  }
+
+  const restaurantListWrapper = document.createElement('ul');
+  restaurantListWrapper.className = 'restaurant-list';
 
   restaurantList.map((restaurant) => {
     const listItem = document.createElement('li');
+    listItem.id = restaurant.id;
     listItem.className = 'restaurant';
 
-    const categoryDiv = createRestaurantCard(restaurant);
-    listItem.append(categoryDiv);
-    restaurantList.append(listItem);
+    createRestaurantCard({
+      baseComponent: listItem,
+      restaurant,
+      hasFavorite,
+    });
+
+    const handleFavorite = (restaurantId) => {
+      toggleStarButton(restaurantId);
+      toggleFavorite(restaurantId);
+    };
+
+    listItem.addEventListener('starButtonClick', (event) => {
+      const restaurantId = Number(event.currentTarget.id);
+
+      handleFavorite(restaurantId);
+    });
+
+    listItem.addEventListener('click', (event) => {
+      const isStarButton = [...event.target.classList].some((className) =>
+        className.startsWith('star__button')
+      );
+
+      if (!isStarButton)
+        openModal('restaurantDetail', {
+          restaurant,
+          hasFavorite,
+          handleFavorite,
+        });
+    });
+
+    restaurantListWrapper.append(listItem);
   });
 
-  restaurantListContainer.appendChild(restaurantList);
+  restaurantListContainer.appendChild(restaurantListWrapper);
 }
 
-export default renderRestaurantList;
+export default createRestaurantList;
