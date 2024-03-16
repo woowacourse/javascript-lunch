0628597fd6bd854f743436e1491772a5a586ff25
vi-wolhwa <cerezo00@naver.com>
2024-03-16 16:48:10 +0900

    rename: 컴포넌트 폴더 구조 변경

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './resources.ts';
 
-import { createAppHeader } from './components/container/AppHeader.ts';
 import { $ } from './utils/dom';
+import { createAppHeader } from './components/header/AppHeader.js';
+import { createRestViewer } from './components/main/RestListMain';
 
 const $app = $('#app');
 const $appHeader = createAppHeader();
+const $restListMain = createRestViewer();
 
 $app.appendChild($appHeader);
+$app.appendChild($restListMain);
