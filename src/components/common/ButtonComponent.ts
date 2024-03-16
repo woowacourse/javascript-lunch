8fbdf6effe7f21eff6435114c8bf85760f10e2b7
river <jenn0.6n@gmail.com>
2024-03-16 23:02:04 +0900

    Refactor: ButtonComponent innerHtml -> createElement로 수정

--- src/components/common/ButtonComponent.ts
+++ src/components/common/ButtonComponent.ts
@@ -1,10 +1,35 @@
-export default class ButtonComponent {
-  getTemplate = (text: string, variant: 'primary' | 'secondary', type: string) => {
-    const template = document.createElement('template');
-    template.innerHTML = /*html*/ `
-    <button type=${type} class="button button--${variant} text-caption">${text}</button> `;
-    const node = template.content.cloneNode(true) as DocumentFragment;
+// export default class ButtonComponent {
+//   getTemplate = (text: string, variant: 'primary' | 'secondary', type: string) => {
+//     const template = document.createElement('template');
+//     template.innerHTML = /*html*/ `
+//     <button type=${type} class="button button--${variant} text-caption">${text}</button> `;
+//     const node = template.content.cloneNode(true) as DocumentFragment;
 
-    return node;
+//     return node;
+//   };
+// }
+
+type Props = {
+  id: string;
+  text: string;
+  variant: 'primary' | 'secondary';
+  type?: 'button';
+};
+
+const ButtonComponent = ({ id, text, variant, type }: Props) => {
+  const button = document.createElement('button');
+  if (type) {
+    button.type = type;
+  }
+  button.classList.add('button', `button--${variant}`, 'text-caption');
+  button.textContent = text;
+  button.id = id;
+
+  const create = () => button;
+
+  return {
+    create
   };
-}
+};
+
+export default ButtonComponent;
