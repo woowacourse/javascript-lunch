2b875f531d8d318851b66af2e9b9a7657c7ccd58
river <jenn0.6n@gmail.com>
2024-03-16 20:45:31 +0900

    Feat: 카테고리 아이콘 컴포넌트 내부에 button 태그 div로 수정
    - 클릭될일 없으므로 div로 수정함

--- src/components/CategoryIconComponent.ts
+++ src/components/CategoryIconComponent.ts
@@ -1,23 +1,46 @@
 import { CATEGORY } from '../constants/constants';
 import { CategoryValues } from '../types/types';
 
-export function CategoryIconComponent() {
-  const getTemplate = (category: CategoryValues) => {
-    const key = Object.keys(CATEGORY).find(
-      (key) => CATEGORY[key as keyof typeof CATEGORY] === category
-    );
+// export function CategoryIconComponent() {
+//   const getTemplate = (category: CategoryValues) => {
+//     const key = Object.keys(CATEGORY).find(
+//       (key) => CATEGORY[key as keyof typeof CATEGORY] === category
+//     );
 
-    return `
-          <button type="button" class="gnb__button" aria-label="음식점 추가">
-          <img src="../templates/category-${key}.png" alt="음식점 추가" />
-          </button>
-        `;
-  };
+//     return `
+//           <button type="button" class="gnb__button" aria-label="음식점 추가">
+//           <img src="../templates/category-${key}.png" alt="음식점 추가" />
+//           </button>
+//         `;
+//   };
 
-  const handleClick = () => {};
+//   const handleClick = () => {};
+
+//   return {
+//     getTemplate,
+//     handleClick
+//   };
+// }
+
+const CategoryIconComponent = (category: CategoryValues) => {
+  const key = Object.keys(CATEGORY).find(
+    (key) => CATEGORY[key as keyof typeof CATEGORY] === category
+  );
+
+  const div = document.createElement('div');
+  div.classList.add('restaurant__category');
+
+  const img = document.createElement('img');
+  img.src = `../templates/category-${key}.png`;
+  img.alt = '${category} 카테고리 아이콘';
+
+  div.appendChild(img);
+
+  const create = () => div;
 
   return {
-    getTemplate,
-    handleClick
+    create
   };
-}
+};
+
+export default CategoryIconComponent;
