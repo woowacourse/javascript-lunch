5726c05e238af5ed57e2ab8741b13d526c020ec4
Lee sang Yeop <mma7710@naver.com>
2024-03-17 18:06:25 +0900

    refactor(modalId): 모달 id 값을 상수로 관리하도록 리팩토링

--- src/LunchApp.ts
+++ src/LunchApp.ts
@@ -4,22 +4,31 @@ import ModalBox from "./components/common/ModalBox";
 import RestaurantHeader from "./components/RestaurantHeader";
 import RestaurantList from "./components/RestaurantList";
 import RestaurantForm from "./components/RestaurantForm";
+import HeaderTab from "./components/HeaderTab";
+import RestaurantDetail from "./components/RestaurantDetail";
+import MODAL_ID from "./constants/modalId";
 
 customElements.define("restaurant-header", RestaurantHeader);
+customElements.define("header-tab", HeaderTab);
 customElements.define("restaurant-list", RestaurantList);
 customElements.define("select-box", SelectBox);
 customElements.define("modal-box", ModalBox);
 customElements.define("restaurant-form", RestaurantForm);
+customElements.define("restaurant-detail", RestaurantDetail);
 
 export default class LunchApp extends BaseComponent {
   getTemplate() {
     return /* html */ `
       <div>
         <restaurant-header></restaurant-header> 
+        <header-tab></header-tab>
         <filter-bar></filter-bar>
         <restaurant-list></restaurant-list>
-        <modal-box modal-id="restaurant-form-modal">
+        <modal-box modal-id=${MODAL_ID.restaurantFormModal}>
           <restaurant-form></restaurant-form>
+        </modal-box>
+        <modal-box modal-id=${MODAL_ID.restaurantDetailModal}>
+          <restaurant-detail></restaurant-detail>
         </modal-box>
       </div>
       `;
