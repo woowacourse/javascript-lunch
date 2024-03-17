89ca6b60d7abe5e68d6ce803927f2b96d7932ef7
이태훈 <rhymint@gmail.com>
2024-03-17 15:04:33 +0900

    feat (RestaurantRegistry): 삭제하기 버튼 누른 경우, 아이템이 삭제되는 기능 추가

--- src/components/LunchItemModal/LunchItemModal.ts
+++ src/components/LunchItemModal/LunchItemModal.ts
@@ -5,6 +5,11 @@ import LunchRestaurantTypeIcon from '../LunchRestaurantTypeIcon/LunchRestaurantT
 import LunchFavoriteIcon from '../LunchFavoriteIcon/LunchFavoriteIcon';
 import { CATEGORY_IMG } from '../LunchItem/LunchItem';
 import LunchButton from '../LunchButton/LunchButton';
+import FavoriteRestaurantsRegistry from '../../domain/FavoriteRestaurantsRegistry';
+import { RestaurantRegistry } from '../../domain';
+import LunchItems from '../LunchItems/LunchItems';
+import LunchItemFilter from '../LunchItemFilter/LunchItemFilter';
+import LunchTab from '../LunchTab/LunchTab';
 
 const LUNCH_ITEM_MODAL_CONTENT_HTML = (restaurant: Restaurant) => `
 <div class="restaurant__modal-container">
@@ -17,6 +22,8 @@ const LUNCH_ITEM_MODAL_CONTENT_HTML = (restaurant: Restaurant) => `
 <div class="button-container"></div>`;
 
 class LunchItemModal extends HTMLElement {
+  #restaurant: Restaurant = { name: '', category: '기타', distance: 5, createdAt: Date.now() };
+
   constructor() {
     super();
     this.appendChild(new LunchModal());
@@ -28,35 +35,41 @@ class LunchItemModal extends HTMLElement {
       if (!(event instanceof CustomEvent)) return;
       const modal = this.querySelector('.modal') as LunchModal;
       modal.handleModalOpen();
-      this.createContent(event.detail.info);
+      this.#restaurant = event.detail.info;
+      this.createContent();
     });
   }
 
-  createContent(restaurant: Restaurant) {
+  createContent() {
     const container = this.querySelector('.modal-container');
     if (container) {
-      container.innerHTML = LUNCH_ITEM_MODAL_CONTENT_HTML(restaurant);
+      container.innerHTML = LUNCH_ITEM_MODAL_CONTENT_HTML(this.#restaurant);
     }
 
-    this.createTitle(restaurant);
+    this.createTitle();
     this.createButtons();
   }
 
-  createTitle(restaurant: Restaurant) {
+  createTitle() {
     const title = this.querySelector('.restaurant__category-favorite');
     title?.appendChild(
       new LunchRestaurantTypeIcon({
-        imgSrc: CATEGORY_IMG[restaurant.category],
-        alt: restaurant.category,
+        imgSrc: CATEGORY_IMG[this.#restaurant.category],
+        alt: this.#restaurant.category,
       }),
     );
-    title?.appendChild(new LunchFavoriteIcon(restaurant));
+    title?.appendChild(new LunchFavoriteIcon(this.#restaurant));
   }
 
   createButtons() {
     const buttonContainer = this.querySelector('.button-container');
     buttonContainer?.appendChild(
-      new LunchButton({ color: 'secondary', type: 'button', text: '삭제하기' }),
+      new LunchButton({
+        color: 'secondary',
+        type: 'button',
+        text: '삭제하기',
+        onClick: this.handleDeleteItem.bind(this),
+      }),
     );
     buttonContainer?.appendChild(
       new LunchButton({
@@ -66,6 +79,22 @@ class LunchItemModal extends HTMLElement {
         onClick: this.handleModalOpen.bind(this),
       }),
     );
+  }
+
+  handleDeleteItem() {
+    RestaurantRegistry.deleteOneRestaurant(this.#restaurant);
+
+    const items = document.querySelector('lunch-items') as LunchItems;
+    const tab = (document.querySelector('.lunch-tab') as LunchTab).nowSelected;
+    const filter = document.querySelector('lunch-item-filter') as LunchItemFilter;
+    const dropdowns = filter.querySelectorAll('select');
+    if (tab === 'favorite-restaurants') {
+      items.renderItems({ database: 'liked' });
+    } else {
+      items.renderItems({ category: dropdowns[0].value, sortBy: dropdowns[1].value });
+    }
+
+    this.handleModalOpen();
   }
 
   handleModalOpen() {
--- src/domain/RestaurantRegistry.ts
+++ src/domain/RestaurantRegistry.ts
@@ -1,4 +1,5 @@
 import { Restaurant } from '../types/Restaurant';
+import FavoriteRestaurantsRegistry from './FavoriteRestaurantsRegistry';
 
 /**
  * @param {Object} 새로운 가게 등록 후 local에 저장
@@ -10,10 +11,19 @@ const RestaurantRegistry = {
     localStorage.setItem('restaurants', JSON.stringify(allRestaurants));
   },
 
-  likeOneRestaurant(newRestaurant: Restaurant) {
-    const allRestaurants: Restaurant[] = JSON.parse(localStorage.getItem('liked') ?? '[]');
-    allRestaurants.push(newRestaurant);
-    localStorage.setItem('liked', JSON.stringify(allRestaurants));
+  deleteOneRestaurant(restaurant: Restaurant) {
+    if (this.hasOneRestaurant(restaurant)) {
+      const allRestaurants: Restaurant[] = JSON.parse(localStorage.getItem('restaurants') ?? '[]');
+      const removedRestaurants = allRestaurants.filter((value) => value.name !== restaurant.name);
+      localStorage.setItem('restaurants', JSON.stringify(removedRestaurants));
+    }
+
+    FavoriteRestaurantsRegistry.unlikeOneRestaurant(restaurant);
+  },
+
+  hasOneRestaurant(restaurant: Restaurant) {
+    const allRestaurants: Restaurant[] = JSON.parse(localStorage.getItem('restaurants') ?? '[]');
+    return allRestaurants.some((value) => value.name === restaurant.name);
   },
 };
 
