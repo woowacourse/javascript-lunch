23d71f2d23bc32810493f09d59c569c18beebb71
rbgksqkr <rbgks1937@naver.com>
2024-03-17 15:22:33 +0900

    refactor(RestaurantForm): HTML로 분리했던 UI 부분을 가져와 컴포넌트 응집성 향상

--- src/components/RestaurantForm.ts
+++ src/components/RestaurantForm.ts
@@ -6,21 +6,10 @@ import Button from './button/Button';
 import Component from './core/Component';
 import Dropdown from './Dropdown';
 
-import {
-  ADD_BUTTON_ATTRIBUTE,
-  CLOSE_BUTTON_ATTRIBUTE,
-  FORM_CATEGORY,
-  FORM_CATEGORY_ATTRIBUTE,
-  FORM_DISTANCE,
-  FORM_DISTANCE_ATTRIBUTE,
-} from '@/constants/filter';
+import { ADD_BUTTON_ATTRIBUTE, CLOSE_BUTTON_ATTRIBUTE } from '@/constants/button';
+import { FORM_CATEGORY, FORM_CATEGORY_ATTRIBUTE, FORM_DISTANCE, FORM_DISTANCE_ATTRIBUTE } from '@/constants/filter';
 import dom from '@/utils/dom';
 import formValidator from '@/validator/formValidator';
-
-interface IRestaurantForm {
-  $target: HTMLElement;
-  props: IRestaurantFormProps;
-}
 
 interface IRestaurantFormProps {
   restaurantList: RestaurantList;
@@ -28,20 +17,67 @@ interface IRestaurantFormProps {
 }
 
 class RestaurantForm extends Component<IRestaurantFormProps> {
-  constructor({ $target, props }: IRestaurantForm) {
-    super($target, props);
+  template() {
+    return /* html */ `
+    <form>
+      <!-- 카테고리 -->
+      <div id="category-container" class="form-item form-item--required">
+        <label for="category text-caption">카테고리</label>
+      </div>
+
+      <!-- 음식점 이름 -->
+      <div class="form-item form-item--required">
+        <label for="name text-caption">이름</label>
+        <input type="text" name="name" id="name" required />
+      </div>
+
+      <!-- 거리 -->
+      <div id="distance-container" class="form-item form-item--required">
+        <label for="distance text-caption">거리(도보 이동 시간) </label>
+      </div>
+
+      <!-- 설명 -->
+      <div class="form-item">
+        <label for="description text-caption">설명</label>
+        <textarea name="description" id="description" cols="30" rows="5"></textarea>
+        <span class="help-text text-caption">메뉴 등 추가 정보를 입력해 주세요.</span>
+      </div>
+
+      <!-- 링크 -->
+      <div class="form-item">
+        <label for="link text-caption">참고 링크</label>
+        <input type="text" name="link" id="link" placeholder="https://github.com/rbgksqkr" />
+        <span class="help-text text-caption">매장 정보를 확인할 수 있는 링크를 입력해 주세요.</span>
+        <span id="error-link" class="text-caption error-text hidden">http:// 또는 https:// 로 시작해야 합니다.</span>
+      </div>
+
+      <!-- 취소/추가 버튼 -->
+      <div class="button-container"></div>
+  </form>`;
   }
 
   render() {
+    this.$target.innerHTML += this.template();
     this.createModalFormSelect(this.props.restaurantList);
     this.createModalFormButton(this.props.restaurantList);
   }
 
   setEvent() {
-    const { category, name, distance, link, $addButton } = this.getFormInputTag();
+    const restaurantInputs = this.getFormInputTag();
     this.$target.addEventListener('input', () => {
-      this.handleFormInput({ category, name, distance, link, $addButton });
+      this.handleFormInput(restaurantInputs);
     });
+  }
+
+  createModalFormSelect(restaurantList: RestaurantList) {
+    this.createCategoryDropdown(restaurantList);
+    this.createDistanceDropdown(restaurantList);
+  }
+
+  createModalFormButton(restaurantList: RestaurantList) {
+    const $buttonContainer = dom.getElement('.button-container');
+    this.createModalCloseButton($buttonContainer);
+    this.createModalAddButton($buttonContainer, restaurantList);
   }
 
   createCategoryDropdown(restaurantList: RestaurantList) {
@@ -59,6 +95,7 @@ class RestaurantForm extends Component<IRestaurantFormProps> {
 
   createDistanceDropdown(restaurantList: RestaurantList) {
     const $distanceContainer = dom.getElement('#distance-container');
+
     new Dropdown({
       $target: $distanceContainer,
       props: {
@@ -69,31 +106,20 @@ class RestaurantForm extends Component<IRestaurantFormProps> {
     });
   }
 
-  createModalFormSelect(restaurantList: RestaurantList) {
-    this.createCategoryDropdown(restaurantList);
-    this.createDistanceDropdown(restaurantList);
+  createModalCloseButton($buttonContainer: HTMLElement) {
+    new Button($buttonContainer, {
+      kind: 'close',
+      attributes: CLOSE_BUTTON_ATTRIBUTE,
+      handleCloseModal: this.props.handleCloseModal,
+    });
   }
 
-  createModalFormButton(restaurantList: RestaurantList) {
-    const $buttonContainer = dom.getElement('.button-container');
-
-    new Button({
-      $target: $buttonContainer,
-      props: {
-        kind: 'close',
-        attributes: CLOSE_BUTTON_ATTRIBUTE,
-        handleCloseModal: this.props.handleCloseModal,
-      },
-    });
-
-    new Button({
-      $target: $buttonContainer,
-      props: {
-        kind: 'add',
-        attributes: ADD_BUTTON_ATTRIBUTE,
-        restaurantList,
-        handleCloseModal: this.props.handleCloseModal,
-      },
+  createModalAddButton($buttonContainer: HTMLElement, restaurantList: RestaurantList) {
+    new Button($buttonContainer, {
+      kind: 'add',
+      attributes: ADD_BUTTON_ATTRIBUTE,
+      restaurantList,
+      handleCloseModal: this.props.handleCloseModal,
     });
   }
 
