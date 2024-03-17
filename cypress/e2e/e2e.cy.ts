d979654718124125a1085b5064f17f8de15eb992
rbgksqkr <rbgks1937@naver.com>
2024-03-18 02:31:10 +0900

    test(e2e): 필터링된 상태에서 음식점 등록 시, 필터링된 카테고리에 맞게 저장

--- cypress/e2e/e2e.cy.ts
+++ cypress/e2e/e2e.cy.ts
@@ -24,11 +24,26 @@ describe('필터링 테스트', () => {
     cy.viewport('macbook-13');
   });
 
-  it('카테고리를 일식을 클릭하면 일식만 나오고, 다시 전체를 클릭하면 전체 음식점 목록이 나온다.', () => {
+  it('카테고리를 일식을 클릭하면 일식만 나오고, 다시 전체를 클릭하면 전체 음식점 목록이 출력된다.', () => {
     cy.get('#category-filter').select('일식').should('have.value', '일식');
     cy.get('.restaurant-list').get('[alt="일식"]');
 
     cy.get('#category-filter').select('전체').should('have.value', '전체');
     cy.get('.restaurant-list').find('li').should('have.length', 6);
+  });
+
+  it('일식으로 필터링 후 일식 음식점을 등록하면, 일식 카테고리에 추가된 상태로 음식점 목록이 출력된다.', () => {
+    cy.get('#category-filter').select('일식').should('have.value', '일식');
+    cy.get('.restaurant-list').get('[alt="일식"]');
+
+    cy.get('.gnb__button').click();
+    cy.get('#category').select('일식').should('have.value', '일식');
+    cy.get('#name').type('e2e 테스트!!!!!!');
+    cy.get('#distance').select('10분 내').should('have.value', '10');
+    cy.get('#description').type('안녕하세요 리뷰어님! 잘부탁드립니다!!!');
+    cy.get('#link').type('https://github.com/rbgksqkr');
+    cy.get('#button-add').click();
+
+    cy.get('.restaurant-list').find('li').should('have.length', 2);
   });
 });
