77b811022b1539bd2d2b5bf703db139a4c0fff76
hwinkr <dnddl8280@naver.com>
2024-03-17 11:51:10 +0900

    refactor(MenuApp): html 구조 수정

--- src/components/MenuApp.ts
+++ src/components/MenuApp.ts
@@ -1,7 +1,6 @@
-import { MENU_APP_EVENTS } from "../constants/event";
-import { CATEGORIES, SORT_TYPE } from "../constants/menu";
-import { initRestaurantStorage } from "../domains/Restaurants";
 import BaseComponent from "./BaseComponent";
+import { MENU_APP_EVENTS } from "../constants/event";
+import { initRestaurantStorage } from "../domains/Restaurants";
 
 class MenuApp extends BaseComponent {
   constructor() {
@@ -11,21 +10,22 @@ class MenuApp extends BaseComponent {
 
   render() {
     this.innerHTML = /*html*/ `
-    <app-header></app-header>
-    <main> 
-      <section class="restaurant-filter-container">
-        <option-selector type="category" options=${[
-          ...Object.values(CATEGORIES),
-        ]} ></option-selector>
-        <option-selector type="sort" options=${[
-          ...Object.values(SORT_TYPE),
-        ]}></option-selector>
-      </section>
-      <restaurant-list></restaurant-list>
-    </main>
-    <modal-wrapper id='add-form' open-type=${MENU_APP_EVENTS.openAddForm}>
-      <restaurant-add-form></restaurant-add-form>
-    </modal-wrapper>
+      <app-header></app-header>
+      <restaurant-tab-container class="flex justify-between tab-container"></restaurant-tab-container>
+      <main> 
+        <option-selector-container></option-selector-container>
+        <restaurant-list></restaurant-list>
+      </main>
+      <modal-wrapper 
+        open-type=${MENU_APP_EVENTS.openAddForm} 
+        id='add-form'>
+        <restaurant-add-form></restaurant-add-form>
+      </modal-wrapper>
+      <modal-wrapper 
+        open-type=${MENU_APP_EVENTS.openRestaurantDetail} 
+        id="restaurant-detail" >
+        <restaurant-detail></restaurant-detail>
+      </modal-wrapper>
     `;
   }
 }
