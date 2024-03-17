11445ff27131e056a8511283ad8d4af864e67956
Kim Da Eun <midekuna@gmail.com>
2024-03-17 23:18:39 +0900

    chore: private을 #로 변경

--- src/domain/RestaurantManager.ts
+++ src/domain/RestaurantManager.ts
@@ -3,29 +3,29 @@ import { filterByBookmark, filterByCategory, sortByOption } from './RestaurantFi
 import { FilterOptions, Restaurant } from './types';
 
 export default class RestaurantManager {
-  private restaurants: Restaurant[] = [];
+  #restaurants: Restaurant[] = [];
 
   constructor(restaurants: Restaurant[] = []) {
-    this.restaurants = restaurants;
+    this.#restaurants = [...restaurants];
   }
 
   add(restaurant: Omit<Restaurant, 'id' | 'isBookmark'>): void {
-    this.restaurants.push({ id: getUniqueID(), isBookmark: false, ...restaurant });
+    this.#restaurants.push({ id: getUniqueID(), isBookmark: false, ...restaurant });
   }
 
   delete(id: number): void {
-    this.restaurants = this.restaurants.filter((restaurant) => restaurant.id !== id);
+    this.#restaurants = this.#restaurants.filter((restaurant) => restaurant.id !== id);
   }
 
   update(updatedRestaurant: Restaurant): void {
-    const index = this.restaurants.findIndex((restaurant) => restaurant.id === updatedRestaurant.id);
+    const index = this.#restaurants.findIndex((restaurant) => restaurant.id === updatedRestaurant.id);
     if (index !== -1) {
-      this.restaurants[index] = updatedRestaurant;
+      this.#restaurants[index] = updatedRestaurant;
     }
   }
 
   getFilteredList(options: FilterOptions, isBookmark: boolean): Restaurant[] {
-    const restaurants = filterByBookmark(this.restaurants, isBookmark);
+    const restaurants = filterByBookmark(this.#restaurants, isBookmark);
     const filteredRestaurants = filterByCategory(restaurants, options.category);
     return sortByOption(filteredRestaurants, options.sort);
   }
