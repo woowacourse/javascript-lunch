4774765497bd6f7a2340a215181917dee952f3eb
hwinkr <dnddl8280@naver.com>
2024-03-17 12:37:55 +0900

    chore(event constants): 점심 뭐 먹지에서 발생하는 커스텀 이벤트 상수화

---
+++ src/constants/event.ts
@@ -0,0 +1,10 @@
+export const MENU_APP_EVENTS = {
+  renderRestaurants: "render-restaurants",
+  selectChange: "select-change",
+  openAddForm: "open-add-form",
+  openRestaurantDetail: "open-restaurant-detail",
+  restaurantFormSubmit: "restaurant-form-submit",
+  closeModal: "close-modal",
+  toggleFavoriteIcon: "toggle-favorite-icon",
+  changeTabState: "change-tab-state",
+} as const;
