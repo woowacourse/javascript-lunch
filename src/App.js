208e2101b669010c6c8ed1be434cf756b327c744
chysis <chysiss@naver.com>
2024-03-17 19:10:12 +0900

    feat: App 클래스로 구현 및 리스트가 비어있는 경우 안내 문구 처리

--- src/App.js
+++ src/App.js
@@ -7,39 +7,23 @@ import RestaurantList from './components/RestaurantList/RestaurantList';
 import createTabMenu from './components/TabMenu/TabMenu';
 import { DEFAULT_TAB, TAB_MENUS } from './constant/constants';
 import { FILTER_DROPDOWN_PROPS, SORT_DROPDOWN_PROPS } from './constant/options';
-import { $ } from './utils/querySelector';
+import { $, $$ } from './utils/querySelector';
 
-const App = {
+class App {
+  #addRestaurantModal = new AddRestaurantModal();
+  #restaurantDetailModal = new RestaurantDetailModal();
+  #restaurantList = new RestaurantList();
+
   initApp() {
-    const addRestaurantModal = new AddRestaurantModal();
-    addRestaurantModal.setEvents();
-
-    const restaurantDetailModal = new RestaurantDetailModal();
-
-    const restaurantList = new RestaurantList();
-
-    createHeader({ title: '점심 뭐 먹지', buttonEvent: () => addRestaurantModal.toggle() });
+    createHeader({ title: '점심 뭐 먹지', buttonEvent: () => this.#addRestaurantModal.toggle() });
     createTabMenu({ tabs: TAB_MENUS, defaultTab: DEFAULT_TAB });
+    this.#addRestaurantModal.setEvents();
+    this.setTabEvents();
     this.renderFilterDropdown();
 
-    const restaurantUl = document.createElement('ul');
-    restaurantUl.classList.add('restaurant-list');
-
-    restaurantList.list.map(restaurantItem => {
-      restaurantUl.append(
-        new createRestaurantItem({
-          restaurant: restaurantItem,
-          onClick: () => {
-            restaurantDetailModal.restaurant = restaurantItem;
-            restaurantDetailModal.toggle();
-          },
-        }).element,
-      );
-    });
-
-    const restaurantListContainer = $('.restaurant-list-container');
-    restaurantListContainer.append(restaurantUl);
-  },
+    if (this.#restaurantList.list.length > 0) this.render();
+    else this.renderEmptyListMessage();
+  }
 
   renderFilterDropdown() {
     const filterContainer = $('.restaurant-filter-container');
@@ -49,7 +33,58 @@ const App = {
 
     filterContainer.insertAdjacentHTML('beforeend', filterDropdown);
     filterContainer.insertAdjacentHTML('beforeend', sortDropdown);
-  },
-};
+  }
+
+  setTabEvents() {
+    const tabButtons = Array.from($$('.button--tabmenu'));
+    tabButtons.forEach(tabButton => {
+      tabButton.addEventListener('click', e => {
+        e.preventDefault();
+        if (e.target.classList.contains('active')) return;
+
+        tabButtons.forEach(button => button.classList.remove('active'));
+        e.target.classList.add('active');
+
+        this.render();
+      });
+    });
+  }
+
+  render() {
+    if (this.#restaurantList.list.length === 0) {
+      this.renderEmptyListMessage();
+      return;
+    }
+
+    const restaurantUl = document.createElement('ul');
+    restaurantUl.classList.add('restaurant-list');
+
+    this.#restaurantList.list.map(restaurantItem => {
+      restaurantUl.append(
+        new createRestaurantItem({
+          restaurant: restaurantItem,
+          onClick: () => {
+            this.#restaurantDetailModal.restaurant = restaurantItem;
+            this.#restaurantDetailModal.toggle();
+          },
+        }).element,
+      );
+    });
+
+    const restaurantListContainer = $('.restaurant-list-container');
+    restaurantListContainer.replaceChildren();
+    restaurantListContainer.append(restaurantUl);
+  }
+
+  renderEmptyListMessage() {
+    const messageElement = document.createElement('h3');
+    messageElement.classList.add('empty--message');
+    messageElement.textContent = '음식점 목록이 비어있습니다. 새로 추가해보세요!';
+
+    const restaurantListContainer = $('.restaurant-list-container');
+    restaurantListContainer.replaceChildren();
+    restaurantListContainer.append(messageElement);
+  }
+}
 
 export default App;
