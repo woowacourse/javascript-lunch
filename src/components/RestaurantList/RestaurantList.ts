736ebf3359f11a3b4876b76529bcd24c5383c3f5
chysis <chysiss@naver.com>
2024-03-17 22:19:12 +0900

    refactor: localStorage에서 값을 얻어오는 로직 수정

--- src/components/RestaurantList/RestaurantList.ts
+++ src/components/RestaurantList/RestaurantList.ts
@@ -2,30 +2,33 @@ import { LOCAL_STORAGE_KEY } from '../../constant/constants';
 import { DUMMY } from '../../constant/dummy';
 import RestaurantEntity from '../../domain/entities/RestaurantEntity';
 import { Restaurant } from '../../interface/RestaurantInterfaces';
-
 class RestaurantList {
-  #restaurantList;
+  #restaurantList: Restaurant[];
 
   constructor() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(DUMMY));
 
+    const storageData = this.fetchData();
+    this.#restaurantList = Array.from(storageData, item => new RestaurantEntity({ restaurant: item }));
+  }
+
+  fetchData(): Restaurant[] {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]');
+  }
+
+  updateData() {
     const storageData = this.fetchData();
     if (storageData) {
       this.#restaurantList = Array.from(storageData, item => new RestaurantEntity({ restaurant: item }));
     }
   }
 
-  fetchData(): Restaurant[] {
-    const storage = localStorage.getItem(LOCAL_STORAGE_KEY);
-    return storage ? JSON.parse(storage) : [];
-  }
-
-  updateData() {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.#restaurantList));
-  }
-
   get list() {
     return this.#restaurantList;
+  }
+
+  set list(listData: Restaurant[]) {
+    this.#restaurantList = listData;
   }
 }
 
