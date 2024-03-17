412c0975b5a980e6eadf296a116a7c12c0f6ca54
chysis <chysiss@naver.com>
2024-03-17 17:48:23 +0900

    refactor: 파일 경로 수정 및 entity 객체 이름 변경

--- src/domain/services/RestaurantService.ts
+++ src/domain/services/RestaurantService.ts
@@ -1,7 +1,19 @@
-import { LOCAL_STORAGE_KEY } from '../constant/constants';
-import { FilteringCategory, SortingProperty, Restaurant, Restaurants } from '../interface/RestaurantInterfaces';
+import { LOCAL_STORAGE_KEY } from '../../constant/constants';
+import { FilteringCategory, SortingProperty, Restaurant, Restaurants } from '../../interface/RestaurantInterfaces';
 
 const RestaurantService: Restaurants = {
+  createRestaurant(formData: Restaurant) {
+    return {
+      id: formData.id,
+      category: formData.category,
+      name: formData.name,
+      distance: formData.distance,
+      description: formData.description,
+      link: formData.link,
+      isFavorite: false,
+    };
+  },
+
   addRestaurant(restaurant: Restaurant, restaurantList: Restaurant[]) {
     const existingRestaurant = restaurantList.find(
       item => item.category === restaurant.category && item.name === restaurant.name,
@@ -19,7 +31,13 @@ const RestaurantService: Restaurants = {
   },
 
   sortByProperty(property: SortingProperty, restaurantList: Restaurant[]) {
-    return restaurantList.sort((first, second) => (first[property] > second[property] ? 1 : -1));
+    // return restaurantList.sort((first, second) => {
+    //   if (first[property] === second[property]) return first.id > second.id ? 1 : -1;
+    //   else return first[property] > second[property] ? 1 : -1;
+    // });
+    return restaurantList.sort((first, second) => {
+      return first[property] > second[property] ? 1 : -1;
+    });
   },
 
   filterFavorite(restaurantList: Restaurant[]) {
