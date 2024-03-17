100e85cbc3efe91ea5dae76b0aead5ad18298cb7
Soyeon Choe <soy2302ten@gmail.com>
2024-03-17 23:30:14 +0900

    refactor: if문 분기처리 간결하게 수정

--- src/utils/closeModal.ts
+++ src/utils/closeModal.ts
@@ -2,15 +2,17 @@ export default function closeModal() {
   const modalEl = document
     .querySelector('custom-modal')
     ?.shadowRoot?.querySelector('.modal');
-
   if (modalEl) {
     modalEl.classList.toggle('open');
-    const childSlotEl = document.querySelector('[slot="child"]');
-    if (childSlotEl) {
-      childSlotEl.innerHTML = '';
-    }
+  }
 
-    const bodyEl = document.querySelector('body');
-    if (bodyEl) bodyEl.style.overflowY = 'scroll';
+  const childSlotEl = document.querySelector('[slot="child"]');
+  if (childSlotEl) {
+    childSlotEl.innerHTML = '';
+  }
+
+  const bodyEl = document.querySelector('body');
+  if (bodyEl) {
+    bodyEl.style.overflowY = 'scroll';
   }
 }
