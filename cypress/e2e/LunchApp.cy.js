4ff58d252494283043a2205817b74671b99a9bff
hanyoung <phanyoung@naver.com>
2024-03-17 20:49:38 +0900

    test(LunchApp.cy): 방문 시 기본 동작 확인 테스트 추가

--- cypress/e2e/LunchApp.cy.js
+++ cypress/e2e/LunchApp.cy.js
@@ -5,4 +5,22 @@ describe("LunchApp(점심 뭐 먹지) E2E 테스트", () => {
     cy.visit("http://localhost:8080/");
     localStorage.setItem("restaurants", JSON.stringify(RESTAURANTS_INFOS));
   });
+
+  it("처음 방문했을 때 기본 요소(header와 main 태그)가 존재한다.", () => {
+    cy.get("header").should("be.visible");
+    cy.get("main").should("be.visible");
+  });
+
+  it("처음 방문했을 때 header에 '점심 뭐 먹지'라는 텍스트가 존재한다.", () => {
+    cy.get("header").contains("점심 뭐 먹지");
+  });
+
+  it("처음 방문했을 때 음식점 등록 모달과 음식점 상세 정보 모달이 보이지 않는다.", () => {
+    cy.get("restaurant-form").should("not.be.visible");
+    cy.get("restaurant-detail").should("not.be.visible");
+  });
+
+  it("처음 방문했을 때 로컬 스토리지에 저장된 데이터의 수 만큼 restaurant item이 존재한다.", () => {
+    cy.get("restaurant-item").should("have.length", RESTAURANTS_INFOS.length);
+  });
 });
