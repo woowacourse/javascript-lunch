ee38f1b059d199e747c36fa8b89f7eb4f80bd45c
pakxe <pigkill40@naver.com>
2024-03-17 21:38:49 +0900

    test: 이름순, 거리순에 대해 올바르게 정렬되는지 테스트 작성

--- cypress/e2e/e2eTest.cy.ts
+++ cypress/e2e/e2eTest.cy.ts
@@ -62,17 +62,16 @@ const dummyRestaurantData = [
 //   cy.clearLocalStorage(); // Cypress에서 제공하는 clearLocalStorage() 메서드를 사용하여 로컬 스토리지를 초기화합니다.
 // });
 describe('점심 뭐 먹지 E2E테스트', () => {
+  beforeEach(() => {
+    localStorage.setItem('restaurantList', JSON.stringify(dummyRestaurantData));
+
+    cy.visit('/');
+  });
+
   const testCategoryList = ['한식', '아시안'];
 
   testCategoryList.forEach((targetCategory) => {
     it(`${targetCategory} 카테고리를 선택하면 해당하는 카테고리의 음식점 목록만 보여준다.`, () => {
-      localStorage.setItem(
-        'restaurantList',
-        JSON.stringify(dummyRestaurantData)
-      );
-
-      cy.visit('http://localhost:8080/');
-
       cy.get('select#category-filter').select(targetCategory);
 
       cy.get('.restaurant-list-container')
@@ -81,6 +80,35 @@ describe('점심 뭐 먹지 E2E테스트', () => {
           const category = img.attr('alt');
           expect(category).to.eq(targetCategory);
         });
+    });
+  });
+
+  it('거리순으로 정렬하면 음식점 목록을 거리순 오름차순으로 정렬해 보여준다.', () => {
+    const sortedByWalkingTime = dummyRestaurantData
+      .map(({ walkingTime }) => walkingTime)
+      .sort((a, b) => a - b);
+    const sorting = '거리순';
+
+    cy.get('select#sorting-filter').select(sorting);
+    cy.get('.restaurant__distance').each((distanceElement, i) => {
+      const distance = parseInt(distanceElement.text().match(/\d+/)[0], 10);
+      expect(distance).to.eq(sortedByWalkingTime[i]);
+    });
+  });
+
+  it('이름순으로 정렬하면 음식점 목록을 거리순 오름차순으로 정렬해 보여준다.', () => {
+    const sortedByName = dummyRestaurantData
+      .map(({ name }) => name)
+      .sort((a, b) => {
+        if (a > b) return 1;
+        else return -1;
+      });
+    const sorting = '이름순';
+
+    cy.get('select#sorting-filter').select(sorting);
+    cy.get('.restaurant__name').each((nameElement, i) => {
+      const name = nameElement.text();
+      expect(name).to.eq(sortedByName[i]);
     });
   });
 });
