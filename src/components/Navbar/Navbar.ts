b7a505344d6edba6c7cd4e18ae04174958d5de05
jinsil <jinsil378900@gmail.com>
2024-03-17 13:24:46 +0900

    feat: 내브바 컴포넌트화

---
+++ src/components/Navbar/Navbar.ts
@@ -0,0 +1,37 @@
+interface Props {
+  firstTitle: string;
+  secondTitle: string;
+  onClick?: (value: boolean) => void;
+}
+
+const createNavbar = ({ firstTitle, secondTitle, onClick }: Props) => {
+  const container = document.createElement('div');
+  const allRestaurantNav = document.createElement('div');
+  const favoriteRestaurantNav = document.createElement('div');
+
+  container.classList.add('restaurant-nav-container');
+  allRestaurantNav.textContent = firstTitle;
+  allRestaurantNav.id = 'all-restaurant';
+  allRestaurantNav.classList.add('restaurant-nav', 'text-body', 'selected');
+  favoriteRestaurantNav.textContent = secondTitle;
+  favoriteRestaurantNav.id = 'favorite-restaurant';
+  favoriteRestaurantNav.classList.add('restaurant-nav', 'text-body');
+
+  container.appendChild(allRestaurantNav);
+  container.appendChild(favoriteRestaurantNav);
+
+  if (onClick) {
+    container.addEventListener('click', event => {
+      allRestaurantNav.classList.remove('selected');
+      favoriteRestaurantNav.classList.remove('selected');
+
+      if (event.target === allRestaurantNav) onClick(true);
+      else onClick(false);
+      (event.target as HTMLDivElement).classList.add('selected');
+    });
+  }
+
+  return container;
+};
+
+export default createNavbar;
