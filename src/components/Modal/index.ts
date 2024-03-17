2d2b05e593105029af05e15e54f79a1a08e32203
hwinkr <dnddl8280@naver.com>
2024-03-17 12:04:05 +0900

    feat(Modal): 재사용할 수 있는 모달 컴포넌트 구현

---
+++ src/components/Modal/index.ts
@@ -0,0 +1,63 @@
+import BaseComponent from "../BaseComponent";
+import { MENU_APP_EVENTS } from "../../constants/event";
+
+import { $ } from "../../utils/dom";
+import { MenuAppEvent } from "../../types/event";
+
+class Modal extends BaseComponent {
+  private modalId: string | null = null;
+
+  constructor() {
+    super();
+  }
+
+  private getModalTemplate(): string {
+    this.modalId = this.getAttribute("id") ?? "";
+    const children = this.innerHTML;
+
+    return /*html*/ `
+      <div class="modal" id=${this.modalId}-wrapper>
+        <div class="modal-backdrop" id=${this.modalId}-backdrop></div>
+        ${children}
+      </div>
+    `;
+  }
+
+  private showModal() {
+    document.body.classList.add("stop-scroll");
+    $(`#${this.modalId}-wrapper`)!.classList.add("modal--open");
+  }
+
+  private hideModal() {
+    document.body.classList.remove("stop-scroll");
+    $(`#${this.modalId}-wrapper`)!.classList.remove("modal--open");
+  }
+
+  render() {
+    this.innerHTML = this.getModalTemplate();
+  }
+
+  checkEventType(type: string): type is MenuAppEvent {
+    return ["open-add-form", "open-restaurant-detail"].includes(type);
+  }
+
+  setEvent() {
+    $<HTMLDivElement>(`#${this.modalId}-backdrop`)!.addEventListener("click", (event) => {
+      if (event.target === event.currentTarget) {
+        this.hideModal();
+      }
+    });
+
+    const openType = this.getAttribute("open-type") ?? "";
+    this.checkEventType(openType) &&
+      document.addEventListener(openType, () => {
+        this.showModal();
+      });
+
+    document.addEventListener(MENU_APP_EVENTS.closeModal, () => {
+      this.hideModal();
+    });
+  }
+}
+
+customElements.define("modal-wrapper", Modal);
