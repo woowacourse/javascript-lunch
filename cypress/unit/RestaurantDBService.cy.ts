2181864035e6481dc559e35279aee01de2429fee
ooherin <555ohr@naver.com>
2024-03-17 12:03:34 +0900

    fix(annotation): 필요없는 주석 삭제

--- cypress/unit/RestaurantDBService.cy.ts
+++ cypress/unit/RestaurantDBService.cy.ts
@@ -4,22 +4,6 @@ import { IRestaurant } from '../../src/types/Restaurant';
 import restaurantListMock from '@/mock/restaurantList.mock';
 import RestaurantCollection from '@/domains/entities/RestaurantCollection';
 describe('음식점 업데이트 테스트 서비스', () => {
-  const RESTAURANTS_DB_TEST_KEY = 'restaurants_test';
-
-  // let restaurants: IRestaurant[] = [];
-
-  // beforeEach(() => {
-  //   cy.stub(localStorage, 'getItem', () => localStorage.getItem(RESTAURANTS_DB_TEST_KEY));
-  //   cy.stub(localStorage, 'setItem', (restaurantJSON) => {
-  //     // JSON.parse(restaurantJSON);
-  //     localStorage.setItem(RESTAURANTS_DB_TEST_KEY, restaurantJSON);
-  //   });
-  //   cy.stub(localStorage, 'removeItem', () => {
-  //     localStorage.removeItem(RESTAURANTS_DB_TEST_KEY);
-  //     // restaurants = JSON.parse('[]');
-  //   });
-  // });
-
   it('새로운 음식점을 추가했을 때, 로컬 스토리지에 새로운 음식점이 추가된 음식점 목록이 가져와진다.', () => {
     const NEW_RESTAURANT1: IRestaurant = {
       id: 1,
@@ -36,34 +20,6 @@ describe('음식점 업데이트 테스트 서비스', () => {
 
     expect(restaurantDBService.update().get().length).to.equal(restaurantListMock.length + 1);
   });
-
-  // it('음식점을 삭제했을 떄, DB서비스에서 업데이트한 데이터에 반영이 된다.', () => {
-  //   const NEW_RESTAURANT1: IRestaurant = {
-  //     id: 1,
-  //     name: '리안네 볶음밥',
-  //     distance: 10,
-  //     category: '중식',
-  //     description: '게살볶음밥',
-  //     isFavorite: false,
-  //   };
-  //   const NEW_RESTAURANT2: IRestaurant = {
-  //     id: 2,
-  //     name: '꺼벙이 김밥',
-  //     distance: 10,
-  //     category: '한식',
-  //     description: '김밥 최저',
-  //     isFavorite: false,
-  //   };
-
-  //   const restaurantUpdateService = new RestaurantUpdateService();
-  //   const restaurantDBService = new RestaurantDBService();
-  //   restaurantDBService.set(new RestaurantCollection([NEW_RESTAURANT1, NEW_RESTAURANT2]));
-
-  //   restaurantUpdateService.deleteRestaurant(2);
-
-  //   expect(restaurantDBService.update().get().length).to.equal(1);
-  //   expect(restaurantDBService.update().get()).to.deep.equal([NEW_RESTAURANT1]);
-  // });
 
   it('RestaurantDBService의 get 메서드를 썼을 때 저장된 음식점 목록이 잘 가져와진다.', () => {
     const restaurantDBService = new RestaurantDBService();
