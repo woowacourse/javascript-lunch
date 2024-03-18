76848e3a122bfa607644d4e920282d14b2073344
jayming66 <mjw8941@gmail.com>
2024-03-18 17:42:31 +0900

    feat: 레스토랑 상세 모달 - 설명문 전체 보기, 링크 표시

--- src/components/Modal/RestaurantDetailModal.ts
+++ src/components/Modal/RestaurantDetailModal.ts
@@ -88,8 +88,8 @@ class RestaurantDetailModal extends HTMLDivElement {
       <div class="restaurant__info">
         <h3 class="restaurant__name text-subtitle">${restaurant.name}</h3>
         <span class="restaurant__distance text-body">캠퍼스부터 ${restaurant.distanceFromCampus}분 내</span>
-        <p class="restaurant__description text-body">${restaurant.description}</p>
-        <a src="${restaurant.link}" >
+        <p class="restaurant__description-full text-body">${restaurant.description}</p>
+        <a href="${restaurant.link}" class="restaurant__link">${restaurant.link}</a>
       </div>
     </div>`;
   }
