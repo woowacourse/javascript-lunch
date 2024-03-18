bdac4e632d9e08fad7164195c8318540bed45375
hain-tain <hain7ce@gmail.com>
2024-03-18 14:13:01 +0900

    feat(AllRestaurantList): AllRestaurantList 상태와 로컬스토리지를 업데이트 해주는 함수들 생성 - getRestaurant: 레스토랑 이름을 받아 정보를 담은 객체 리턴해주는 함수 - postRestaurant: 레스토랑 정보 객체를 받아 상태에 추가하고 로컬스토리지에 등록해주는 함수 - deleteRestaurant: 레스토랑 이름을 받아 해당 레스토랑 정보를 삭제하고 로컬스토리지에 반영해주는 함수 - patchRestaurantFavorites: 레스토랑 이름을 받아 favorites 상태를 변경하고 로컬스토리지에 반영해주는 함수

--- src/domain/AllRestaurantList.ts
+++ src/domain/AllRestaurantList.ts
@@ -1,5 +1,30 @@
 import RestaurantList from "./RestaurantList";
+import setLocalStorageItem from "../utils/setLocalStorageItem";
 
 const AllRestaurantList = new RestaurantList();
 
 export default AllRestaurantList;
+
+const updateLocalStorage = () => {
+  setLocalStorageItem(
+    "allRestaurants",
+    AllRestaurantList.convertedRestaurants()
+  );
+};
+
+export const getRestaurant = (restaurantName: string) => {
+  return AllRestaurantList.bringRestaurantInfo(restaurantName);
+};
+export const postRestaurant = (restaurant: Restaurant) => {
+  AllRestaurantList.add(restaurant);
+  updateLocalStorage();
+};
+
+export const deleteRestaurant = (restaurantName: string) => {
+  AllRestaurantList.delete(restaurantName);
+  updateLocalStorage();
+};
+export const patchRestaurantFavorites = (restaurantName: string) => {
+  AllRestaurantList.updateFavorites(restaurantName);
+  updateLocalStorage();
+};
