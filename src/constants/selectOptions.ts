b5393fed682ec4750f56392eb24ad941cc3f6b6a
hain-tain <hain7ce@gmail.com>
2024-03-18 14:18:13 +0900

    fix(selectOptions): CATEGORY 와 DISTANCE 상수를 활용하는 방향으로 CATEGORY_WITH_ENTIRE, isCategory, isDistance 변경

--- src/constants/selectOptions.ts
+++ src/constants/selectOptions.ts
@@ -1,13 +1,3 @@
-export const CATEGORY_WITH_ENTIRE: CategoryWithEntire[] = [
-  "전체",
-  "한식",
-  "중식",
-  "일식",
-  "아시안",
-  "양식",
-  "기타",
-];
-
 export const SORT_STANDARD: SortStandard[] = ["이름순", "거리순"];
 
 export const CATEGORY: Category[] = [
@@ -19,14 +9,16 @@ export const CATEGORY: Category[] = [
   "기타",
 ];
 
+export const CATEGORY_WITH_ENTIRE: CategoryWithEntire[] = ["전체", ...CATEGORY];
+
 export const DISTANCE: Distance[] = [5, 10, 15, 20, 30];
 
 export const DEFAULT_UNSELECTED_OPTION = "선택해주세요";
 
 export const isCategory = (value: string): value is Category => {
-  return ["한식", "중식", "일식", "아시안", "양식", "기타"].includes(value);
+  return CATEGORY.includes(value as Category);
 };
 
 export const isDistance = (value: number): value is Distance => {
-  return [5, 10, 15, 20, 30].includes(value);
+  return DISTANCE.includes(value as Distance);
 };
