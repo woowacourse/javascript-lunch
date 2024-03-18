fe27e75757e1943e55e46e373380af3b521bc01a
hain-tain <hain7ce@gmail.com>
2024-03-18 14:16:05 +0900

    fix(formItemTexts): url 을 link 로 변경

--- src/constants/formItemTexts.ts
+++ src/constants/formItemTexts.ts
@@ -5,9 +5,10 @@ const FORM_ITEM_TEXTS = {
   distanceTitle: "거리(도보 이동 시간)",
   descriptionTitle: "설명",
   descriptionDescription: "메뉴 등 추가 정보를 입력해 주세요",
-  urlTitle: "참고 링크",
-  urlDescription: "매장 정보를 확인할 수있는 링크를 입력해 주세요.",
+  linkTitle: "참고 링크",
+  linkDescription: "매장 정보를 확인할 수있는 링크를 입력해 주세요.",
   cancelButton: "취소하기",
   addButton: "추가하기",
 } as const;
+
 export default FORM_ITEM_TEXTS;
