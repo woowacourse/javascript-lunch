0d155682b3acf84fbaf65954c00245ae3b2409da
hain-tain <hain7ce@gmail.com>
2024-03-18 14:23:32 +0900

    test(toggleFavorites): 자주 가는 음식점 탭 목록 확인 및 자주 가는 음식점 추가/취소 테스트 작성

---
+++ cypress/e2e/toggleFavorites.cy.ts
@@ -0,0 +1,77 @@
+describe("자주 가는 음식점을 추가하고 목록으로 확인할 수 있다.", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  const currentFavoriteImage = (isFavorite: boolean) =>
+    isFavorite
+      ? "http://localhost:8080/favorite-icon-filled.png"
+      : "http://localhost:8080/favorite-icon-lined.png";
+
+  const clickFavoriteButton = (
+    favoriteButton: Cypress.Chainable<JQuery<HTMLElement>>,
+    isFavorites: boolean
+  ) => {
+    favoriteButton.should(
+      "have.attr",
+      "src",
+      currentFavoriteImage(isFavorites)
+    );
+    favoriteButton.click();
+    favoriteButton.should(
+      "have.attr",
+      "src",
+      currentFavoriteImage(!isFavorites)
+    );
+  };
+
+  it("음식점 목록에서 자주 가는 음식점을 추가할 수 있다.", () => {
+    const favoriteButton = cy.get(
+      "li[name=도쿄라면]>button.button--favorites>img"
+    );
+
+    clickFavoriteButton(favoriteButton, false);
+  });
+
+  it("음식점 목록에서 자주 가는 음식점을 취소할 수 있다.", () => {
+    const favoriteButton = cy.get(
+      "li[name=스페인타파스]>button.button--favorites>img"
+    );
+
+    clickFavoriteButton(favoriteButton, false);
+    clickFavoriteButton(favoriteButton, true);
+  });
+
+  it("음식점 상세 모달에서 자주 가는 음식점으로 추가할 수 있다.", () => {
+    cy.get("li[name=프렌치빌").click();
+
+    const favoriteButton = cy.get(
+      "div.restaurant__detail[name=프렌치빌]>div>button>img"
+    );
+
+    clickFavoriteButton(favoriteButton, false);
+  });
+
+  it("음식점 상세 모달에서 자주 가는 음식점을 취소할 수 있다.", () => {
+    cy.get("li[name=방콕맛집").click();
+
+    const favoriteButton = cy.get(
+      "div.restaurant__detail[name=방콕맛집]>div>button>img"
+    );
+
+    clickFavoriteButton(favoriteButton, false);
+    clickFavoriteButton(favoriteButton, true);
+  });
+
+  it("자주 가는 음식점 탭에서 추가한 음식점 목록을 확인할 수 있다.", () => {
+    const favoriteButton = cy.get(
+      "li[name=도쿄라면]>button.button--favorites>img"
+    );
+
+    clickFavoriteButton(favoriteButton, false);
+
+    cy.get("div#tab>button").last().click();
+
+    cy.get("ul.restaurant-list").contains("도쿄라면");
+  });
+});
