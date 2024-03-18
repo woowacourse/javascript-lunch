261116688ce45ab9a33f42fd48bde4696c0915bc
jinsil <jinsil378900@gmail.com>
2024-03-18 15:49:37 +0900

    test: 식당 추가, 필터링

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,11 +1,13 @@
 import restaurantList from '../fixtures/RestaurantList.json';
 
 describe('점심 뭐 먹지 E2E 테스트', () => {
+  // 초기 설정
   beforeEach(() => {
     window.localStorage.setItem('restaurantList', JSON.stringify(restaurantList));
     cy.visit('http://localhost:8080');
   });
 
+  // 처음 렌더링 됐을 때
   it('처음 화면이 뜨면 헤더와 식당 추가 버튼 보인다.', () => {
     cy.get('.gnb').should('be.visible');
     cy.get('.gnb__button').should('be.visible');
@@ -23,5 +25,36 @@ describe('점심 뭐 먹지 E2E 테스트', () => {
 
   it('처음 화면이 뜨면 레스토랑 리스트가 보인다.', () => {
     cy.get('.restaurant').should('have.length', restaurantList.length);
+  });
+
+  // 식당 추가
+  it('카테고리, 이름, 거리 등을 입력한 후 추가하기 버튼을 누르면 식당이 추가된다.', () => {
+    cy.get('.gnb__button').click();
+
+    cy.get('.modal').should('have.class', 'modal--open');
+
+    cy.get('#category').select('한식');
+    cy.get('#name').type('용호동낙지');
+    cy.get('#distance').select('10');
+    cy.get('#description').type('사장님 츤데레');
+    cy.get('#link').type('www.naver.com');
+
+    cy.get('.form-add-restaurant').submit();
+
+    cy.on('window:alert', alertText => {
+      expect(alertText).to.equal('추가되었습니다.');
+    });
+
+    cy.get('.modal').should('not.be.visible');
+  });
+
+  // 식당 필터링/정렬
+  it('식당을 카테고리 별로 필터링 하고 이름순, 거리순으로 정렬할 수 있다.', () => {
+    cy.get('#category-filter').select('한식');
+    cy.get('#sorting-filter').select('distance');
+
+    cy.get('.restaurant').should('have.length', 2);
+    cy.get('.restaurant__name').eq(0).should('contain', '꺼벙이분식');
+    cy.get('.restaurant__name').eq(1).should('contain', '배가무닭볶음탕');
   });
 });
