68ab3216304b9532bc4e60c92675f8a1a8b564c4
김진호 <rlawlsgh1227@gmail.com>
2024-03-18 17:07:07 +0900

    fix: 리스트에서 빈 화면에서 추가됐을 때, 빈 상태로 됐을 때 fallback이 나타나고 없어지도록 수정

--- src/components/listContainer/ListContainer.ts
+++ src/components/listContainer/ListContainer.ts
@@ -6,17 +6,19 @@ const { insertElementsInTarget } = DOM;
 
 class ListContainer extends HTMLUListElement {
   private restaurants: Restaurant[];
+  private fallbackUI: HTMLDivElement | null;
 
   constructor(restaurants: Restaurant[]) {
     super();
     this.id = 'restaurant-lists';
     this.className = 'restaurant-lists';
+    this.fallbackUI = null;
     this.restaurants = this.createRestaurants(restaurants);
   }
 
   createRestaurants(restaurants: Restaurant[]) {
     if (restaurants.length === 0) {
-      this.createFallback();
+      this.fallbackUI = this.createFallback();
       return [];
     }
     insertElementsInTarget(this, restaurants);    
@@ -25,6 +27,7 @@ class ListContainer extends HTMLUListElement {
 
   createFallback() {
     const nothingContainer = document.createElement('div');
+    nothingContainer.id = 'nothing__restaurant';
     nothingContainer.classList.add('nothing__restaurant');
 
     const nothingMent = document.createElement('p');
@@ -33,18 +36,33 @@ class ListContainer extends HTMLUListElement {
 
     nothingContainer.appendChild(nothingMent);
     this.appendChild(nothingContainer);
+    return nothingContainer;
   }
 
   addRestaurants(restaurant: Restaurant) {
     this.appendChild(restaurant);
+    if (this.restaurants.length === 0 && this.fallbackUI !== null) {
+      this.removeChild(this.fallbackUI);
+    }
     this.restaurants.push(restaurant);
   }
 
   deleteRestaurant(targetId: string) {    
     const targetNode = this.restaurants.find((restaurant) => restaurant.id === `restaurant-list${targetId}`);    
+    const targetNodeIndex = this.restaurants.findIndex((restaurant) => restaurant.id === `restaurant-list${targetId}`);
+    
     if (targetNode === undefined) return;
-
+    
     this.removeChild(targetNode);
+    this.restaurants.splice(targetNodeIndex, 1);
+
+    if (this.restaurants.length === 0 && this.fallbackUI === null) {
+      this.fallbackUI = this.createFallback();
+    }
+  }
+
+  get restaurantCount() {
+    return this.restaurants.length;
   }
 }
 
