223b03b47bccdee1c420db1b9353077e49660aee
greetings1012 <ahnqwerty@gmail.com>
2024-03-18 14:17:55 +0900

    refactor: validator 나누어 구현

--- src/validator/RestaurantValidator.ts
+++ src/validator/RestaurantValidator.ts
@@ -1,35 +1,43 @@
-import RestaurantService from '../service/RestaurantService';
+import { Category, MinutesWalk } from '../constants/enums';
 import { ILocation } from '../interface/interface';
+import Restaurant from '../domain/Restaurant';
 
 const RestaurantValidator = {
-  validateRestaurantName(userInput: string) {
-    const isNameLengthOutOfRange = userInput.length < 1 || userInput.length > 20;
+  validateUserInput(userInput: ILocation) {
+    this.validateRestaurantName(userInput.name);
+    this.validateRestaurantCategory(userInput.category);
+    this.validateRestaurantMinutesWalk(userInput.minutesWalk);
+  },
+
+  validateRestaurantName(inputData: string) {
+    const isNameLengthOutOfRange = inputData.length < 1 || inputData.length > 20;
     if (isNameLengthOutOfRange) {
       throw new Error('1글자 이상 20글자 이하로 입력해주세요.');
     }
 
-    const isNameBlank = [...new Set(userInput)].join('') === ' ';
+    const isNameBlank = [...new Set(inputData)].join('') === ' ';
     if (isNameBlank) {
       throw new Error('공백을 제외하고 1글자 이상 입력해주세요.');
     }
   },
 
-  validateRestaurantCategory(inputData: ILocation) {
-    if (!inputData.category) {
-      throw new Error('값을 선택해주세요.');
+  validateRestaurantCategory(inputData: Category) {
+    if (!inputData) {
+      throw new Error('카테고리를 선택해주세요.');
     }
   },
 
-  validateRestaurantMinutesWalk(inputData: Object) {
-    const inputLocationData = inputData as ILocation;
-    if (!inputLocationData.minutesWalk) {
-      throw new Error('값을 선택해주세요.');
+  validateRestaurantMinutesWalk(inputData: MinutesWalk) {
+    if (!inputData) {
+      throw new Error('거리(도보 이동 시간)을 선택해주세요.');
     }
   },
 
-  validateRestaurantOverlapping(inputData: Object) {
-    const inputLocationData = inputData as ILocation;
-    //TODO: 코드 완성하기
+  validateRestaurantOverlapping(restaurants: Restaurant[], inputData: ILocation) {
+    restaurants.forEach((restaurant) => {
+      if (restaurant.getName() === inputData.name)
+        throw new Error('이미 존재하는 음식점 이름입니다. 다른 음식점을 입력해주세요.');
+    });
   },
 };
 
