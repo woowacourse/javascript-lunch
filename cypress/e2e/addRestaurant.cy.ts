2551776f059f7b739ef6be8c3a21fa649d15c172
Kim Da Eun <midekuna@gmail.com>
2024-03-18 14:41:22 +0900

    refactor: fillOutForm 함수를 커스텀 커맨드로 변경

--- cypress/e2e/addRestaurant.cy.ts
+++ cypress/e2e/addRestaurant.cy.ts
@@ -1,12 +1,4 @@
 describe('음식점 추가 테스트', () => {
-  const fillOutForm = ({ name, category, distance, description, link }) => {
-    cy.get('[data-cy=category]').find('select').select(category);
-    cy.get('[data-cy=name]').find('input').type(name);
-    cy.get('[data-cy=distances]').find('select').select(distance);
-    if (description) cy.get('[data-cy="description"]').find('textarea').type(description);
-    if (link) cy.get('[data-cy="link"]').find('input').type(link);
-  };
-
   beforeEach(() => {
     cy.visit('');
 
@@ -21,7 +13,7 @@ describe('음식점 추가 테스트', () => {
 
   it('취소하기 버튼을 눌러 모달을 닫을 수 있다.', () => {
     cy.fixture('dummyRestaurant').then((dummyRestaurant) => {
-      fillOutForm(dummyRestaurant);
+      cy.fillOutRestaurantForm(dummyRestaurant);
     });
 
     cy.get('[data-cy="reset"]').click();
@@ -30,7 +22,7 @@ describe('음식점 추가 테스트', () => {
 
   it('카테고리, 이름, 거리, 설명, 링크를 입력한 후 제출하면 모달이 닫히고 음식점 리스트가 업데이트된다.', () => {
     cy.fixture('dummyRestaurant').then((dummyRestaurant) => {
-      fillOutForm(dummyRestaurant);
+      cy.fillOutRestaurantForm(dummyRestaurant);
     });
 
     cy.get('[data-cy="submit"]').click();
---
+++ cypress/support/commands.ts
@@ -0,0 +1,15 @@
+interface RestaurantFormProps {
+  name: string;
+  category: string;
+  distance: string;
+  description?: string;
+  link?: string;
+}
+
+Cypress.Commands.add('fillOutRestaurantForm', ({ name, category, distance, description, link }) => {
+  cy.get('[data-cy=category]').find('select').select(category);
+  cy.get('[data-cy=name]').find('input').type(name);
+  cy.get('[data-cy=distances]').find('select').select(distance);
+  if (description) cy.get('[data-cy="description"]').find('textarea').type(description);
+  if (link) cy.get('[data-cy="link"]').find('input').type(link);
+});
