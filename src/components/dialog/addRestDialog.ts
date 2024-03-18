e65c0ee91625a5665ae5d7ff7beaeaa5b93578da
vi-wolhwa <cerezo00@naver.com>
2024-03-18 16:10:37 +0900

    docs: addRestDialog 주석

--- src/components/dialog/addRestDialog.ts
+++ src/components/dialog/addRestDialog.ts
@@ -13,48 +13,57 @@ export const createAddRestDialog = (): HTMLDialogElement => {
   const $container = document.createElement('div');
   $container.classList.add('modal-container');
 
-  const $form = document.createElement('form');
+  const $buttonContainer = document.createElement('div');
+  $buttonContainer.classList.add('button-container');
 
+  /* 타이틀 */
   const $title = createH2({ h2: { class: 'modal-title text-title' }, text: '새로운 음식점' });
 
+  /* Form */
+  const $form = document.createElement('form');
+
+  /* 카테고리 */
   const $category = createFormItem({
     label: { text: '카테고리', for: 'category', class: 'text-caption' },
     element: createSelect({ select: { id: 'category', name: 'category' }, optionData: FORM_OPTIONS.category }),
     required: true
   });
 
+  /* 이름 */
   const $name = createFormItem({
     label: { text: '이름', for: 'name', class: 'text-caption' },
     element: createInput({ input: { id: 'name', name: 'name', type: 'text' } }),
     required: true
   });
 
+  /* 거리 */
   const $distance = createFormItem({
     label: { text: '거리(도보 이동 시간)', for: 'distance', class: 'text-caption' },
     element: createSelect({ select: { id: 'distance', name: 'distance' }, optionData: FORM_OPTIONS.distance }),
     required: true
   });
 
+  /* 설명 */
   const $description = createFormItem({
     label: { text: '설명', for: 'description', class: 'text-caption' },
     element: createTextarea({ textarea: { id: 'description', name: 'description', cols: '30', rows: '5' } }),
     description: '메뉴 등 추가 정보를 입력해 주세요.'
   });
 
+  /* 참고 링크 */
   const $link = createFormItem({
     label: { text: '참고 링크', for: 'link', class: 'text-caption' },
     element: createInput({ input: { id: 'link', name: 'link', type: 'text' } }),
     description: '매장 정보를 확인할 수 있는 링크를 입력해 주세요.'
   });
 
-  const $buttonContainer = document.createElement('div');
-  $buttonContainer.classList.add('button-container');
-
+  /* 취소 버튼 */
   const $cancel = createButton({
     button: { class: 'button button--secondary text-caption', type: 'reset' },
     text: '취소하기'
   });
 
+  /* 추가 버튼 */
   const $submit = createButton({
     button: { class: 'button button--primary text-caption', type: 'submit' },
     text: '추가하기'
