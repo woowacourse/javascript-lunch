bcaee774364044e145cbb5895c57d0341cfc254a
useon <yoosk4986@naver.com>
2024-03-18 17:04:26 +0900

    style: 레스토랑 정보 모달 스타일 추가 및 레스토랑 아이템 스타일 추가

--- src/components/RestaurantDetail.ts
+++ src/components/RestaurantDetail.ts
@@ -5,16 +5,19 @@ const RestaurantDetail = (detail: IRestaurant) => {
   return `
       <div class="restaurant-detail-content">
         <div class="restaurant-detail-icon-favorite-wrapper">
-          ${displayCategoryIcon(detail.category)}
-          <img src=${detail.favorite ? favoriteFilledIcon : favoriteLinedIcon} alt="즐겨찾기" class="favorite-icon" />
+          <img src=${displayCategoryIcon(detail.category)} alt="카테고리" class="detail-category-icon" />
+          <img src=${
+            detail.favorite ? favoriteFilledIcon : favoriteLinedIcon
+          } alt="즐겨찾기" class="detail-favorite-icon" />
         </div>
-        <p>캠퍼스부터 ${detail.distance}분 내</p>
-        <p>${detail.description}</p>
-        <p>${detail.reference}</p>
+        <p class="detail-restaurant-name text-subtitle">${detail.name}</p>
+        <p class="text-body detail-restaurant-distance">캠퍼스부터 ${detail.distance}분 내</p>
+        <p class="text-body detail-restaurant-discription">${detail.description}</p>
+        <p class="detail-restaurant-reference">${detail.reference}</p>
       </div>
-      <div class="restaurant-detail-button">
-        <button>삭제하기</button>
-        <button>닫기</button>
+      <div class="restaurant-detail-button button-container">
+        <button class="button button--secondary">삭제하기</button>
+        <button class="button button--primary">닫기</button>
       </div>
   `;
 };
