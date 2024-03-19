308839e793a01d7a339d337c3e3996cc91c6c1cc
hanyoung <phanyoung@naver.com>
2024-03-19 22:31:44 +0900

    refactor(LunchApp.cy.js): 비슷한 의미의 테스트 케이스 묶고, 중복된 의미의 표현을 삭제

--- cypress/e2e/LunchApp.cy.js
+++ cypress/e2e/LunchApp.cy.js
@@ -8,21 +8,18 @@ describe("LunchApp(점심 뭐 먹지) E2E 테스트", () => {
   });
 
   context("첫 방문 시 동작 확인", () => {
-    it("처음 방문했을 때 기본 요소(header와 main 태그)가 존재한다.", () => {
+    it("웹사이트가 정상적으로 로드된다.", () => {
       cy.get("header").should("be.visible");
       cy.get("main").should("be.visible");
-    });
-
-    it("처음 방문했을 때 header에 '점심 뭐 먹지'라는 텍스트가 존재한다.", () => {
       cy.get("header").contains("점심 뭐 먹지");
     });
 
-    it("처음 방문했을 때 음식점 등록 모달과 음식점 상세 정보 모달이 보이지 않는다.", () => {
+    it("음식점 등록 모달과 음식점 상세 정보 모달이 보이지 않는다.", () => {
       cy.get("restaurant-form").should("not.be.visible");
       cy.get("restaurant-detail").should("not.be.visible");
     });
 
-    it("처음 방문했을 때 로컬 스토리지에 저장된 데이터의 수 만큼 restaurant item이 존재한다.", () => {
+    it("로컬 스토리지에 저장된 데이터의 수 만큼 restaurant item이 존재한다.", () => {
       cy.get("restaurant-item").should("have.length", RESTAURANTS_INFOS.length);
     });
   });
