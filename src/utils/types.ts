3fb961a775082cef075a9eed303037ad0f15c7f5
hwinkr <dnddl8280@naver.com>
2024-03-19 21:00:01 +0900

    refactor(type utils): 여러 상황에서 범용적으로 사용할 수 있도록 함수명 수정

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,7 @@
-export const isRestaurantItemType = <T extends object>(item: object, candidates: string[]): item is T => {
+export const isObjectWithKeys = <T extends object>(item: object, candidates: string[]): item is T => {
   return Object.keys(item).every((key) => candidates.includes(key));
 };
 
-export const isValidOption = <T extends string>(options: T[], value: string): value is T => {
-  return options.includes(value as T);
+export const isArrayElement = <T extends string>(options: string[], target: string): target is T => {
+  return options.includes(target);
 };
