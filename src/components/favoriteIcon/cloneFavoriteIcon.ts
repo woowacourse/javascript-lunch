91feead6f65a7d4fe9e477eb01f9c421b50c21c0
김진호 <rlawlsgh1227@gmail.com>
2024-03-21 02:37:17 +0900

    fix: 함수명 바꿔주면서 cloneFavoriteIcon도 같이 변경

--- src/components/favoriteIcon/cloneFavoriteIcon.ts
+++ src/components/favoriteIcon/cloneFavoriteIcon.ts
@@ -3,7 +3,7 @@ import FavoriteIcon, { ChangeStateProps } from './FavoriteIcon';
 function cloneFavoriteIcon(origin: FavoriteIcon, active: boolean, changeState: ChangeStateProps) {
   const clone = new FavoriteIcon({ active, isChild: true, changeState });
 
-  clone.showStarIcon = origin.showStarIcon;
+  clone.getStarIconSource = origin.getStarIconSource;
   clone.listenChangeState = origin.listenChangeState;
   return clone;
 }
