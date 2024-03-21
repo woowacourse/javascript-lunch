ce8f1cd07893f4ef43a0f9f1949e32582de15fdf
hain-tain <hain7ce@gmail.com>
2024-03-22 02:18:31 +0900

    test: commands 에 음식점 추가 테스트를 위한  addRestaurant 구현

--- cypress/e2e/addRestaurant.cy.ts
+++ cypress/e2e/addRestaurant.cy.ts
@@ -4,37 +4,8 @@ describe("음식점 목록에 새로운 음식점을 추가할 수 있다", () =
     cy.get("#add-restaurant-button").click();
   });
 
-  const addRestaurant = ({
-    nameValue,
-    categoryValue,
-    distanceValue,
-    descriptionValue = "",
-    linkValue = "",
-  }) => {
-    const form = cy.get(
-      "#adding-restaurant-modal-container>div.modal>div.modal-container>form"
-    );
-
-    const category = form.get("div.form-item>select[name=category]");
-    category.select(categoryValue, { force: true });
-
-    const name = form.get("div.form-item>input[name=name]");
-    name.type(nameValue);
-
-    const distance = form.get("div.form-item>select[name=distance]");
-    distance.select(distanceValue);
-
-    const description = form.get("div.form-item>textarea[name=description]");
-    descriptionValue && description.type(descriptionValue);
-
-    const link = form.get("div.form-item>input[name=link]");
-    linkValue && link.type(linkValue);
-
-    form.get("div.button-container>button.button--primary").click();
-  };
-
-  it("음식점의 카테고리, 이름, 거리(도보 이동 시간), 설명, 참고 링크를 입력해서 추가할 수 있다.", () => {
-    addRestaurant({
+  it("사용자가 음식 추가 모달에 카테고리, 이름, 거리(도보 이동 시간), 설명, 참고 링크를 입력하여 제출하면 새 음식점이 목록에 추가된다.", () => {
+    cy.addRestaurant({
       nameValue: "소코아",
       categoryValue: "일식",
       distanceValue: "10",
@@ -45,7 +16,7 @@ describe("음식점 목록에 새로운 음식점을 추가할 수 있다", () =
     cy.get("ul.restaurant-list").contains("소코아");
   });
 
-  it("카테고리, 이름, 거리는 입력 필수이며, 입력하지 않으면 음식점을 추가할 수 없다.", () => {
+  it("사용자가 음식점을 추가하기 위한 필수요소(카테고리, 이름, 거리) 중 하나라도 입력하지 않으면 음식점이 추가되지 않는다.", () => {
     const form = cy.get(
       "#adding-restaurant-modal-container>div.modal>div.modal-container>form"
     );
@@ -70,8 +41,8 @@ describe("음식점 목록에 새로운 음식점을 추가할 수 있다", () =
     );
   });
 
-  it("설명, 참고 링크는 옵션. 입력하지 않아도 음식점을 추가할 수 있다.", () => {
-    addRestaurant({
+  it("사용자가 음식점을 추가할 때 옵션 요소(설명, 참고 링크)는 입력하지 않고 제출해도 음식점이 추가된다.", () => {
+    cy.addRestaurant({
       nameValue: "얌샘김밥",
       categoryValue: "한식",
       distanceValue: "5",
@@ -80,8 +51,8 @@ describe("음식점 목록에 새로운 음식점을 추가할 수 있다", () =
     cy.get("ul.restaurant-list").contains("얌샘김밥");
   });
 
-  it("새로고침해도 추가한 음식점 정보들이 유지되어야 한다.", () => {
-    addRestaurant({
+  it("새로고침해도 추가한 음식점 정보들이 유지된다.", () => {
+    cy.addRestaurant({
       nameValue: "토마토파스타",
       categoryValue: "양식",
       distanceValue: "15",
