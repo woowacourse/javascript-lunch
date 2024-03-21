f8bbdd709ca6acd6d2a4ea485d5a542d956ddfe0
skiende74 <skiende74@gmail.com>
2024-03-22 02:23:07 +0900

    refactor(NewRestaurantForm): 버튼박스 렌더. 메소드로뽑아냄

--- src/components/NewRestaurantModal/NewRestaurantForm.ts
+++ src/components/NewRestaurantModal/NewRestaurantForm.ts
@@ -77,12 +77,15 @@ class NewRestaurantForm extends HTMLFormElement {
       helpText: '매장 정보를 확인할 수 있는 링크를 입력해 주세요.',
     });
 
+    this.#renderButtonBox();
+  }
+
+  #renderButtonBox() {
     const $buttonBox = dom.getElement<HTMLElement>(this, '.button-container');
-    $buttonBox.append(
-      new BasicButton('secondary', '취소하기', 'reset', () => {
-        (this.parentElement?.parentElement as BasicModal).closeModal();
-      }),
-    );
+    const closeModal = () => {
+      (this.parentElement?.parentElement as BasicModal).closeModal();
+    };
+    $buttonBox.append(new BasicButton('secondary', '취소하기', 'reset', closeModal));
     $buttonBox.append(new BasicButton('primary', '추가하기', 'submit', () => {}));
   }
 }
