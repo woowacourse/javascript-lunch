23050f95462af427ec44b7cca54f6f7ed1e69c2c
simhorim <hori5020@gmail.com>
2024-03-22 17:56:30 +0900

    feat(RestaurantStorage): setMatchedRestaurant, getMatchedRestaurant 메서드 기능 구현

--- src/domain/RestaurantsStorage.ts
+++ src/domain/RestaurantsStorage.ts
@@ -1,5 +1,5 @@
 import { STORAGE_KEY } from '../constants/config';
-import type { IRestaurantList } from '../types/restaurant';
+import type { IRestaurantList, TRestaurantInstance } from '../types/restaurant';
 import Restaurant from './Restaurant';
 
 const RestaurantsStorage = {
@@ -14,6 +14,28 @@ const RestaurantsStorage = {
       return parsedRestaurantList.map(parsedRestaurant => new Restaurant(parsedRestaurant.information));
     }
     return Array(0);
+  },
+
+  setMatchedRestaurant(restaurant: TRestaurantInstance): void {
+    const localStorageRestaurants = this.getRestaurants();
+
+    localStorageRestaurants.forEach((localStorageRestaurant, index) => {
+      if (localStorageRestaurant.information.name === restaurant.information.name) {
+        localStorageRestaurants[index] = restaurant;
+      }
+    });
+
+    this.setRestaurants(localStorageRestaurants);
+  },
+
+  getMatchedRestaurant(name: string): TRestaurantInstance | undefined {
+    const localStorageRestaurants = this.getRestaurants();
+
+    const matchedRestaurant = localStorageRestaurants.find(
+      localStorageRestaurant => localStorageRestaurant.information.name === name,
+    );
+
+    return matchedRestaurant;
   },
 };
 
