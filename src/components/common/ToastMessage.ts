de359753fdd7c2b5243dd1024e26943a3641b6a8
river <jenn0.6n@gmail.com>
2024-03-23 15:24:26 +0900

    Feat: 토스트 메시지 컴포넌트 구현

---
+++ src/components/common/ToastMessage.ts
@@ -0,0 +1,56 @@
+import { $ } from '../../util/dom';
+
+type Props = {
+  message: string;
+};
+
+const ToastMessage = () => {
+  let toastTimeout: NodeJS.Timeout;
+
+  const create = () => {
+    const toast = document.createElement('div');
+    toast.classList.add('toast');
+
+    const toastContainer = document.createElement('div');
+    toastContainer.classList.add('toast-container', 'text-subtitle');
+
+    toast.appendChild(toastContainer);
+    return toast;
+  };
+
+  const showToastMessage = () => {
+    const toastMessage = $('.toast');
+    toastMessage?.classList.add('toast--open');
+  };
+
+  const closeToastMessage = () => {
+    toastTimeout = setTimeout(() => {
+      const toastMessage = $('.toast');
+      toastMessage?.classList.remove('toast--open');
+    }, 1000);
+  };
+
+  const render = ({ message }: Props) => {
+    const toastMessage = $('.toast-container');
+    if (toastMessage) {
+      toastMessage.textContent = message;
+    }
+
+    if (toastMessage?.classList.contains('toast--open')) {
+      closeToastMessage();
+      return;
+    }
+
+    showToastMessage();
+    closeToastMessage();
+
+    return;
+  };
+
+  return {
+    create,
+    render
+  };
+};
+
+export default ToastMessage;
