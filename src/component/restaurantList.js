64243e5ca6bfe2e2ec181cac10f90c1bec7b25d9
pakxe <pigkill40@naver.com>
2024-03-24 17:16:34 +0900

    refactor: 로직을 함수로 분리

--- src/component/restaurantList.js
+++ src/component/restaurantList.js
@@ -2,26 +2,23 @@ import { toggleStarButton } from './iconButtons/starButton.js';
 import { openModal } from './modal/modal.js';
 import createRestaurantCard from './restaurantCard.js';
 
-function createRestaurantList({ restaurantList, toggleFavorite, hasFavorite }) {
+function handleFavorite({ restaurantId, toggleFavorite }) {
+  toggleStarButton(restaurantId);
+  toggleFavorite(restaurantId);
+}
 
-  const restaurantListContainer = document.querySelector(
-    '.restaurant-list-container'
-  );
+function clearRestaurantListContainer() {
+  const alertElement = document.createElement('div');
+  alertElement.textContent = '음식점이 존재하지 않습니다.';
 
-  restaurantListContainer.replaceChildren();
+  restaurantListContainer.append(alertElement);
+}
 
-  if (restaurantList.length === 0) {
-    const alertElement = document.createElement('div');
-    alertElement.textContent = '음식점이 존재하지 않습니다.';
-
-    restaurantListContainer.append(alertElement);
-    return;
-  }
-
+function renderRestaurantList({ restaurantList, hasFavorite }) {
   const restaurantListWrapper = document.createElement('ul');
   restaurantListWrapper.className = 'restaurant-list';
 
-  restaurantList.map((restaurant) => {
+  restaurantList.forEach((restaurant) => {
     const listItem = document.createElement('li');
     listItem.id = restaurant.id;
     listItem.className = 'restaurant';
@@ -32,35 +29,50 @@ function createRestaurantList({ restaurantList, toggleFavorite, hasFavorite }) {
       hasFavorite,
     });
 
-    const handleFavorite = (restaurantId) => {
-      toggleStarButton(restaurantId);
-      toggleFavorite(restaurantId);
-    };
+    restaurantListWrapper.append(listItem);
+  });
 
-    listItem.addEventListener('starButtonClick', (event) => {
+  return restaurantListWrapper;
+}
+
+function createRestaurantList({ restaurantList, toggleFavorite, hasFavorite }) {
+  const restaurantListContainer = document.querySelector(
+    '.restaurant-list-container'
+  );
+
+  restaurantListContainer.replaceChildren();
+
+  if (restaurantList.length === 0) return clearRestaurantListContainer();
+
+  const restaurantListWrapper = renderRestaurantList({
+    restaurantList,
+    hasFavorite,
+  });
+
+  const restaurantItemList = restaurantListWrapper.querySelectorAll('li');
+
+  restaurantItemList.forEach((restaurantItem, i) => {
+    restaurantItem.addEventListener('starButtonClick', (event) => {
       const restaurantId = Number(event.currentTarget.id);
 
-      handleFavorite(restaurantId);
+      handleFavorite({ restaurantId, toggleFavorite });
     });
 
-    listItem.addEventListener('click', (event) => {
+    restaurantItem.addEventListener('click', (event) => {
       const isStarButton = [...event.target.classList].some((className) =>
         className.startsWith('star__button')
       );
 
       if (!isStarButton)
         openModal('restaurantDetail', {
-          restaurant,
+          restaurant: restaurantList[i],
           hasFavorite,
           handleFavorite,
         });
     });
-
-    restaurantListWrapper.append(listItem);
   });
 
   restaurantListContainer.appendChild(restaurantListWrapper);
 }
 
 export default createRestaurantList;
-
