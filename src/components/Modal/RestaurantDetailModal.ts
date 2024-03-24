f64d812a310c6b787cea23f01b233c5ed02ee5f5
chysis <chysiss@naver.com>
2024-03-25 00:02:27 +0900

    refactor: 즐겨찾기 버튼 컴포넌트 적용 및 메서드 인자 수정

--- src/components/Modal/RestaurantDetailModal.ts
+++ src/components/Modal/RestaurantDetailModal.ts
@@ -1,129 +1,91 @@
 import { CATEGORY_CONVERTER } from '../../constant/constants';
 import { CLOSE_BUTTON_PROPS, DELETE_BUTTON_PROPS } from '../../constant/options';
-import { $ } from '../../utils/querySelector';
-import RestaurantEntity from '../../domain/entities/RestaurantEntity';
-import createButton from '../Common/Button';
+import { Restaurant } from '../../interface/RestaurantInterfaces';
 import Modal from './Modal';
+import createButton from '../Common/Button';
+import createFavoriteIcon from '../FavoriteIcon/FavoriteIcon';
 
 const restaurantDetailLayout = document.createElement('div');
 restaurantDetailLayout.classList.add('modal-detail-container');
 
-const render = (restaurant: RestaurantEntity) => {
-  const favorite = document.createElement('div');
-  favorite.classList.add('restaurant__favorite');
-  favorite.setAttribute('id', 'detail-favorite');
-
-  const starredIcon = document.createElement('img');
-  starredIcon.setAttribute('src', 'favorite-icon-filled.png');
-  starredIcon.classList.add('favorite');
-  favorite.appendChild(starredIcon);
-
-  const unstarredIcon = document.createElement('img');
-  unstarredIcon.setAttribute('src', 'favorite-icon-lined.png');
-  unstarredIcon.classList.add('favorite');
-  favorite.appendChild(unstarredIcon);
-
-  if (restaurant.isFavorite) {
-    unstarredIcon.classList.add('hidden');
-  } else {
-    starredIcon.classList.add('hidden');
-  }
-
-  restaurantDetailLayout.appendChild(favorite);
-
-  const category = document.createElement('div');
-  category.classList.add('restaurant__category');
-
-  const categoryIcon = document.createElement('img');
-  categoryIcon.setAttribute('src', `./category-${CATEGORY_CONVERTER[restaurant.category]}.png`);
-  categoryIcon.classList.add('category-icon');
-  category.appendChild(categoryIcon);
-
-  restaurantDetailLayout.appendChild(category);
-
-  const info = document.createElement('div');
-  info.classList.add('restaurant__info');
-
-  const infoName = document.createElement('h3');
-  infoName.classList.add('restaurant__name', 'text-subtitle');
-  infoName.textContent = restaurant.name;
-  info.appendChild(infoName);
-
-  const infoDistance = document.createElement('span');
-  infoDistance.classList.add('restaurant__distance', 'text-body');
-  infoDistance.textContent = `캠퍼스부터 ${restaurant.distance}분 내`;
-  info.appendChild(infoDistance);
-
-  if (restaurant.description) {
-    const infoDescription = document.createElement('p');
-    infoDescription.classList.add('restaurant__description', 'text-body');
-    infoDescription.textContent = restaurant.description;
-    info.appendChild(infoDescription);
-  }
-
-  if (restaurant.link) {
-    const infoLink = document.createElement('a');
-    infoLink.classList.add('restaurant__link', 'text-body');
-    infoLink.setAttribute('href', restaurant.link);
-    infoLink.textContent = restaurant.link;
-    info.appendChild(infoLink);
-  }
-
-  restaurantDetailLayout.appendChild(info);
-
-  const buttonContainer = document.createElement('div');
-  buttonContainer.classList.add('button-container');
-
-  const deleteButton = createButton(DELETE_BUTTON_PROPS);
-  const closeButton = createButton(CLOSE_BUTTON_PROPS);
-  buttonContainer.innerHTML += deleteButton;
-  buttonContainer.innerHTML += closeButton;
-
-  restaurantDetailLayout.appendChild(buttonContainer);
-};
-
 interface SetProps {
-  restaurant: RestaurantEntity;
-  reRender: () => void;
-  onUpdate: () => void;
+  restaurant: Restaurant;
   onDelete: (id: string) => void;
+  onToggle: (id: string) => void;
+  onClose: () => void;
 }
 
 class RestaurantDetailModal extends Modal {
-  #restaurant?: RestaurantEntity;
-
   constructor() {
     super({ child: restaurantDetailLayout });
   }
 
-  set restaurant({ restaurant, reRender, onUpdate, onDelete }: SetProps) {
-    this.#restaurant = restaurant;
+  draw({ restaurant, onDelete, onToggle, onClose }: SetProps) {
     restaurantDetailLayout.replaceChildren();
-    render(this.#restaurant);
-    this.setEvents(reRender, onUpdate, onDelete);
-  }
 
-  setEvents(reRender: () => void, onUpdate: () => void, onDelete: (id: string) => void) {
-    const favorite = $('#detail-favorite');
-    const deleteButton = $('#delete');
-    const closeButton = $('#close');
+    const favorite = createFavoriteIcon({ isFavorite: restaurant.isFavorite, id: 'detail-favorite' });
+    restaurantDetailLayout.appendChild(favorite);
+
+    const category = document.createElement('div');
+    category.classList.add('restaurant__category');
+
+    const categoryIcon = document.createElement('img');
+    categoryIcon.setAttribute('src', `./category-${CATEGORY_CONVERTER[restaurant.category]}.png`);
+    categoryIcon.classList.add('category-icon');
+    category.appendChild(categoryIcon);
+
+    restaurantDetailLayout.appendChild(category);
+
+    const info = document.createElement('div');
+    info.classList.add('restaurant__info');
+
+    const infoName = document.createElement('h3');
+    infoName.classList.add('restaurant__name', 'text-subtitle');
+    infoName.textContent = restaurant.name;
+    info.appendChild(infoName);
+
+    const infoDistance = document.createElement('span');
+    infoDistance.classList.add('restaurant__distance', 'text-body');
+    infoDistance.textContent = `캠퍼스부터 ${restaurant.distance}분 내`;
+    info.appendChild(infoDistance);
+
+    if (restaurant.description) {
+      const infoDescription = document.createElement('p');
+      infoDescription.classList.add('restaurant__description-detail', 'text-body');
+      infoDescription.textContent = restaurant.description;
+      info.appendChild(infoDescription);
+    }
+
+    if (restaurant.link) {
+      const infoLink = document.createElement('a');
+      infoLink.classList.add('restaurant__link', 'text-body');
+      infoLink.setAttribute('href', restaurant.link);
+      infoLink.textContent = restaurant.link;
+      info.appendChild(infoLink);
+    }
+
+    restaurantDetailLayout.appendChild(info);
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('button-container');
+
+    const deleteButton = createButton(DELETE_BUTTON_PROPS);
+    const closeButton = createButton(CLOSE_BUTTON_PROPS);
+    buttonContainer.append(deleteButton);
+    buttonContainer.append(closeButton);
+
+    restaurantDetailLayout.appendChild(buttonContainer);
 
     favorite.addEventListener('click', () => {
-      this.#restaurant?.toggleFavorite();
-      restaurantDetailLayout.replaceChildren();
-      if (this.#restaurant) render(this.#restaurant);
-      onUpdate();
-      this.setEvents(reRender, onUpdate, onDelete);
+      onToggle(restaurant.id);
     });
 
     deleteButton.addEventListener('click', () => {
-      onDelete(this.#restaurant!.id);
+      onDelete(restaurant.id);
     });
 
     closeButton.addEventListener('click', () => {
-      this.toggle.bind(this)();
-      onUpdate();
-      reRender();
+      onClose();
     });
   }
 }
--- src/components/Restaurant/RestaurantItem.ts
+++ src/components/Restaurant/RestaurantItem.ts
@@ -1,95 +1,60 @@
 import { CATEGORY_CONVERTER } from '../../constant/constants';
-import RestaurantEntity from '../../domain/entities/RestaurantEntity';
+import { Restaurant } from '../../interface/RestaurantInterfaces';
+import createFavoriteIcon from '../FavoriteIcon/FavoriteIcon';
 
 interface Props {
-  restaurant: RestaurantEntity;
-  onClick?: () => void;
-  onToggle: () => void;
+  restaurant: Restaurant;
+  onClick: () => void;
+  onToggle: (id: string) => void;
 }
 
-class createRestaurantItem {
-  #itemContainer = document.createElement('li');
-  #starredIcon = document.createElement('img');
-  #unstarredIcon = document.createElement('img');
+const createRestaurantItem = ({ restaurant, onClick, onToggle }: Props) => {
+  const itemContainer = document.createElement('li');
+  itemContainer.classList.add('restaurant');
 
-  constructor({ restaurant, onClick, onToggle }: Props) {
-    this.render(restaurant, onToggle);
+  const favorite = createFavoriteIcon({ isFavorite: restaurant.isFavorite });
 
-    if (onClick) {
-      this.#itemContainer.addEventListener('click', onClick.bind(this));
-    }
+  const category = document.createElement('div');
+  category.classList.add('restaurant__category');
+
+  const categoryIcon = document.createElement('img');
+  categoryIcon.setAttribute('src', `./category-${CATEGORY_CONVERTER[restaurant.category]}.png`);
+  categoryIcon.classList.add('category-icon');
+  category.appendChild(categoryIcon);
+
+  const info = document.createElement('div');
+  info.classList.add('restaurant__info');
+
+  const infoName = document.createElement('h3');
+  infoName.classList.add('restaurant__name', 'text-subtitle');
+  infoName.textContent = restaurant.name;
+  info.appendChild(infoName);
+
+  const infoDistance = document.createElement('span');
+  infoDistance.classList.add('restaurant__distance', 'text-body');
+  infoDistance.textContent = `캠퍼스부터 ${restaurant.distance}분 내`;
+  info.appendChild(infoDistance);
+
+  if (restaurant.description) {
+    const infoDescription = document.createElement('p');
+    infoDescription.classList.add('restaurant__description', 'text-body');
+    infoDescription.textContent = restaurant.description;
+    info.appendChild(infoDescription);
   }
 
-  render(restaurant: RestaurantEntity, onToggle: () => void) {
-    this.#itemContainer.classList.add('restaurant');
+  itemContainer.appendChild(favorite);
+  itemContainer.appendChild(category);
+  itemContainer.appendChild(info);
 
-    const favorite = document.createElement('div');
-    favorite.classList.add('restaurant__favorite');
+  itemContainer.addEventListener('click', () => {
+    onClick();
+  });
 
-    this.#starredIcon.setAttribute('src', 'favorite-icon-filled.png');
-    this.#starredIcon.classList.add('favorite');
-    favorite.appendChild(this.#starredIcon);
+  favorite.addEventListener('click', () => {
+    onToggle(restaurant.id);
+  });
 
-    this.#unstarredIcon.setAttribute('src', 'favorite-icon-lined.png');
-    this.#unstarredIcon.classList.add('favorite');
-    favorite.appendChild(this.#unstarredIcon);
-
-    if (restaurant.isFavorite) this.#unstarredIcon.classList.add('hidden');
-    else this.#starredIcon.classList.add('hidden');
-
-    const category = document.createElement('div');
-    category.classList.add('restaurant__category');
-
-    const categoryIcon = document.createElement('img');
-    categoryIcon.setAttribute('src', `./category-${CATEGORY_CONVERTER[restaurant.category]}.png`);
-    categoryIcon.classList.add('category-icon');
-    category.appendChild(categoryIcon);
-
-    const info = document.createElement('div');
-    info.classList.add('restaurant__info');
-
-    const infoName = document.createElement('h3');
-    infoName.classList.add('restaurant__name', 'text-subtitle');
-    infoName.textContent = restaurant.name;
-    info.appendChild(infoName);
-
-    const infoDistance = document.createElement('span');
-    infoDistance.classList.add('restaurant__distance', 'text-body');
-    infoDistance.textContent = `캠퍼스부터 ${restaurant.distance}분 내`;
-    info.appendChild(infoDistance);
-
-    if (restaurant.description) {
-      const infoDescription = document.createElement('p');
-      infoDescription.classList.add('restaurant__description', 'text-body');
-      infoDescription.textContent = restaurant.description;
-      info.appendChild(infoDescription);
-    }
-
-    this.#itemContainer.appendChild(favorite);
-    this.#itemContainer.appendChild(category);
-    this.#itemContainer.appendChild(info);
-
-    favorite.addEventListener('click', e => {
-      e.stopPropagation();
-      this.favoriteToggle.bind(this)(restaurant);
-      onToggle();
-    });
-  }
-
-  get element() {
-    return this.#itemContainer;
-  }
-
-  favoriteToggle(restaurant: RestaurantEntity) {
-    if (this.#starredIcon.classList.contains('hidden')) {
-      this.#starredIcon.classList.remove('hidden');
-      this.#unstarredIcon.classList.add('hidden');
-    } else {
-      this.#starredIcon.classList.add('hidden');
-      this.#unstarredIcon.classList.remove('hidden');
-    }
-    restaurant.toggleFavorite();
-  }
-}
+  return itemContainer;
+};
 
 export default createRestaurantItem;
