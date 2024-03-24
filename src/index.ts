06334c46e04f0c114595fae6b3751c96ca5a4d04
ss0526100 <ss0526100@gmail.com>
2024-03-24 21:57:36 +0900

    feat: main 내부의 함수를 주입받도력 변경

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,15 @@ import "./image/add-button.png";
 
 import MainController from "./controller/MainController";
 
-MainController.start();
+const mainController = new MainController();
+
+const body = document.querySelector("body");
+
+const titleElement = document.querySelector(".gnb__title") as HTMLElement;
+const addButtonElement = document.getElementById(
+  "add-restaurant-button"
+) as HTMLButtonElement;
+
+body?.append(mainController.element);
+
+mainController.attachEventToHeader(titleElement, addButtonElement);
