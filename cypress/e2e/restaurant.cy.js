b9a9973bbcad9852d5a692bce90fe5c366407706
ImxYJL <allensain14@gmail.com>
2024-03-25 10:15:12 +0900

    test: 테스트 코드 리팩토링 - given, when, then 주석 추가, 테스트 명세 업데이트

--- cypress/e2e/restaurant.cy.js
+++ cypress/e2e/restaurant.cy.js
@@ -1,5 +1,5 @@
 describe("간단한 음식점 홈페이지 동작 테스트", () => {
-  const newRestaurant = {
+  const restaurant = {
     category: "양식",
     name: "양식당",
     distance: "5분 내",
@@ -31,36 +31,28 @@ describe("간단한 음식점 홈페이지 동작 테스트", () => {
     },
   ];
 
-  const addOneRestaurantItem = () => {
+  const fillAndSubmitRestaurantForm = (restaurant) => {
     cy.get("#gnb-button").click();
 
-    const newRestaurant = {
-      category: "양식",
-      name: "양식당",
-      distance: "5분 내",
-      description: "맛있는 스테이크",
-      url: "http://example4.com",
-    };
-
-    cy.get("#category").select(newRestaurant.category);
-    cy.get("#name").type(newRestaurant.name);
-    cy.get("#distance").select(newRestaurant.distance);
-    cy.get("#description").type(newRestaurant.description);
-    cy.get("#url").type(newRestaurant.url);
+    cy.get("#category").select(restaurant.category);
+    cy.get("#name").type(restaurant.name);
+    cy.get("#distance").select(restaurant.distance);
+    cy.get("#description").type(restaurant.description);
+    cy.get("#url").type(restaurant.url);
 
     cy.get("#restaurant-add-form").submit();
   };
 
-  const addThreeRestaurantItems = () => {
-    restaurants.forEach((restaurant) => {
-      cy.get("#gnb-button").click();
-      cy.get("#category").select(restaurant.category);
-      cy.get("#name").type(restaurant.name);
-      cy.get("#distance").select(restaurant.distance);
-      cy.get("#description").type(restaurant.description);
-      cy.get("#url").type(restaurant.url);
-      cy.get("form#restaurant-add-form").submit();
-    });
+  const addOneRestaurantItem = () => {
+    cy.get("#gnb-button").click();
+
+    cy.get("#category").select(restaurant.category);
+    cy.get("#name").type(restaurant.name);
+    cy.get("#distance").select(restaurant.distance);
+    cy.get("#description").type(restaurant.description);
+    cy.get("#url").type(restaurant.url);
+
+    cy.get("#restaurant-add-form").submit();
   };
 
   beforeEach(() => {
@@ -69,52 +61,55 @@ describe("간단한 음식점 홈페이지 동작 테스트", () => {
 
   it("GNB 버튼을 눌렀을 때 음식점 추가 모달 등장", () => {
     cy.get("#gnb-button").click();
+
     cy.get("restaurant-add-modal").should("have.class", "modal--open");
   });
 
-  it("음식점 추가 모달에서 취소하기를 눌렀을 때 모달 닫기", () => {
+  it("음식점 추가 모달에서 취소하기를 누르면 모달 닫힘", () => {
     cy.get("#gnb-button").click();
+
     cy.get("#modal-cancel-button").click();
 
     cy.get("restaurant-add-modal").should("not.have.class", "modal--open");
   });
 
-  it("음식점 추가 모달에서 음식점 정보를 추가했을 때 모달 닫고 전체 리스트에 정보 추가", () => {
-    cy.get("#gnb-button").click();
-
-    cy.get("#category").select(newRestaurant.category);
-    cy.get("#name").type(newRestaurant.name);
-    cy.get("#distance").select(newRestaurant.distance);
-    cy.get("#description").type(newRestaurant.description);
-    cy.get("#url").type(newRestaurant.url);
-
-    cy.get("#restaurant-add-form").submit();
+  it("음식점 추가 모달에서 음식점 정보를 추가하면 모달이 닫히고, 전체 리스트에 추가한 정보 렌더링", () => {
+    fillAndSubmitRestaurantForm(restaurant);
 
     cy.get("restaurant-add-modal").should("not.have.class", "modal--open");
-    cy.get(".restaurant-list-container").should("contain", newRestaurant.name);
+    cy.get(".restaurant-list-container").should("contain", restaurant.name);
   });
 
-  it("'중식' 카테고리 필터", () => {
-    addThreeRestaurantItems();
+  it("'중식' 카테고리를 선택한 경우 중식에 속하는 음식점만 렌더링", () => {
+    // given
+    restaurants.forEach((restaurant) =>
+      fillAndSubmitRestaurantForm(restaurant)
+    );
 
+    // when
     cy.get("#category-filter").select("중식");
 
+    // then
     cy.get(".restaurant-list-container")
+      .find(".restaurant")
       .should("have.length", 1)
-      .contains("중식당1")
-      .should("exist");
+      .and("contain", "중식당1");
   });
 
-  it("이름순 정렬", () => {
-    addThreeRestaurantItems();
+  it("이름순 정렬을 선택한 경우 이름순으로 정렬한 음식점 정보들을 렌더링", () => {
+    // given
+    restaurants.forEach((restaurant) =>
+      fillAndSubmitRestaurantForm(restaurant)
+    );
 
-    cy.get("#sorting-filter").select("이름순");
-
-    const sortedRestaurants = restaurants.sort((a, b) =>
+    const sortedRestaurants = [...restaurants].sort((a, b) =>
       a.name.localeCompare(b.name)
     );
 
-    // NOTE: 페이지에 표시된 각 레스토랑의 이름이 정렬된 순서와 일치하는지 확인
+    // when
+    cy.get("#sorting-filter").select("이름순");
+
+    // then
     sortedRestaurants.forEach((restaurant, index) => {
       cy.get(".restaurant-list-container .restaurant")
         .eq(index)
@@ -122,19 +117,15 @@ describe("간단한 음식점 홈페이지 동작 테스트", () => {
     });
   });
 
-  it("거리순 정렬", () => {
-    addThreeRestaurantItems();
-
-    cy.get("#sorting-filter").select("거리순");
-
-    // NOTE: 거리 정보를 숫자로 변환하는 함수
+  it("거리순 정렬을 선택한 경우 거리순으로 정렬한 음식점 정보들을 렌더링", () => {
+    // given
     const distanceToNumber = (distanceStr) => {
-      const numStr = distanceStr.trim().split(" ")[0]; // "30분 내"에서 숫자 부분만 추출
+      const numStr = distanceStr.trim().split(" ")[0];
       return Number(numStr);
     };
 
-    cy.get(".restaurant-list-container .restaurant").then(($restaurants) => {
-      const restaurants = $restaurants
+    const getSortedRestaurantsByDistance = ($restaurants) => {
+      return $restaurants
         .toArray()
         .map((el) => {
           const distance = Cypress.$(el).find(".restaurant__distance").text();
@@ -143,34 +134,51 @@ describe("간단한 음식점 홈페이지 동작 테스트", () => {
         .sort(
           (a, b) => distanceToNumber(a.distance) - distanceToNumber(b.distance)
         );
+    };
 
-      restaurants.forEach((restaurant, index) => {
+    restaurants.forEach((restaurant) =>
+      fillAndSubmitRestaurantForm(restaurant)
+    );
+
+    // when
+    cy.get("#sorting-filter").select("거리순");
+
+    // then
+    cy.get(".restaurant-list-container .restaurant").then(($restaurants) => {
+      const sortedRestaurants = getSortedRestaurantsByDistance($restaurants);
+
+      sortedRestaurants.forEach((restaurant) => {
         cy.wrap(restaurant.el)
           .invoke("text")
           .then((text) => {
-            expect(text).to.contain(restaurant.distance.trim()); // 공백 제거 추가
+            expect(text).to.contain(restaurant.distance.trim());
           });
       });
     });
   });
 
   it("즐겨찾기 버튼을 눌렀을 때 버튼 활성화", () => {
+    // given
     addOneRestaurantItem();
 
-    cy.get(`#${newRestaurant.name}-favorite-button`).click();
+    // when
+    cy.get(`#${restaurant.name}-favorite-button`).click();
 
-    // NOTE: 버튼의 이미지 소스(src)에 'filled' 문자열이 포함되어 있는지 확인
-    cy.get(`#${newRestaurant.name}-favorite-button`)
+    // then
+    cy.get(`#${restaurant.name}-favorite-button`)
       .invoke("attr", "src")
       .should("include", "filled");
   });
 
-  it("음식점 아이템을 눌렀을 때 해당 음식점의 정보 모달 등장", () => {
+  it("음식점 아이템을 클릭하면 해당 음식점의 정보 모달 등장", () => {
+    // given
     addOneRestaurantItem();
 
+    // when
     cy.get("li.restaurant").click();
 
+    // then
     cy.get("restaurant-info-modal").should("have.class", "modal--open");
-    cy.get(".restaurant__name").should("contain", newRestaurant.name);
+    cy.get(".restaurant__name").should("contain", restaurant.name);
   });
 });
