5b18a84b3a167e7a0ca4e781ea3aedaae26a02d7
sooyeoniya <tndus1306403@naver.com>
2025-03-04 17:45:25 +0900

    feat(LinkInput): LinkInput 컴포넌트 생성
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

---
+++ src/components/linkInput/LinkInput.js
@@ -0,0 +1,25 @@
+export default function LinkInput() {
+  const $linkFormItem = document.createElement("div");
+  $linkFormItem.className = "form-item";
+
+  const $linkLabel = document.createElement("label");
+  $linkLabel.setAttribute("for", "link text-caption");
+  // TODO: label text 데이터로 변경
+  $linkLabel.textContent = "참고 링크";
+
+  const $linkInput = document.createElement("input");
+  $linkInput.type = "text";
+  $linkInput.setAttribute("name", "link");
+  $linkInput.id = "link";
+
+  const $linkHelpText = document.createElement("span");
+  $linkHelpText.className = "help-text text-caption";
+  $linkHelpText.textContent =
+    "매장 정보를 확인할 수 있는 링크를 입력해 주세요.";
+
+  $linkFormItem.appendChild($linkLabel);
+  $linkFormItem.appendChild($linkInput);
+  $linkFormItem.appendChild($linkHelpText);
+
+  return $linkFormItem;
+}
