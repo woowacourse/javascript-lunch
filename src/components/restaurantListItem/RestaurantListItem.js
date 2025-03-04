6424f6572fcf0dbacbf13ab27976ab29fdef2b3d
sooyeoniya <tndus1306403@naver.com>
2025-03-04 15:40:04 +0900

    feat(RestaurantListItem): RestaurantListItem 컴포넌트 생성
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

---
+++ src/components/restaurantListItem/RestaurantListItem.js
@@ -0,0 +1,43 @@
+export default function RestaurantListItem({
+  category,
+  name,
+  distance,
+  description,
+}) {
+  const $item = document.createElement("li");
+  $item.className = "restaurant";
+
+  const $category = document.createElement("div");
+  $category.className = "restaurant__category";
+
+  const $categoryImg = document.createElement("img");
+  $categoryImg.className = "category-icon";
+  // TODO: 카테고리 상수화
+  $categoryImg.src = "/assets/category-korean.png";
+  $categoryImg.setAttribute("alt", category);
+
+  const $info = document.createElement("div");
+  $info.className = "restaurant__info";
+
+  const $name = document.createElement("h3");
+  $name.className = "restaurant__name text-subtitle";
+  $name.textContent = name;
+
+  const $distance = document.createElement("span");
+  $distance.className = "restaurant__distance text-body";
+  $distance.textContent = `캠퍼스부터 ${distance}분 내`;
+
+  const $description = document.createElement("p");
+  $description.className = "restaurant__description text-body";
+  $description.textContent = description;
+
+  $item.appendChild($category);
+  $item.appendChild($info);
+
+  $category.appendChild($categoryImg);
+  $info.appendChild($name);
+  $info.appendChild($distance);
+  $info.appendChild($description);
+
+  return $item;
+}
