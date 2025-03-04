816194bd9d070dbc0f18027a7d76fed23812afc4
wo-o29 <woogur29@gmail.com>
2025-03-04 17:31:02 +0900

    ​feat: restaurantForm 컴포넌트 추가
    Co-authored-by: spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>

---
+++ src/components/restaurant/form/form.js
@@ -0,0 +1,67 @@
+import createButton from "../../button/button.js";
+import createDropdownBox from "../../dropdown/dropdown.js";
+import createInputBox from "../../input/input.js";
+import createTextAreaBox from "../../textarea/textarea.js";
+import {
+  FOOD_CATEGORY,
+  RESTAURANT_DISTANCE,
+} from "../../../settings/settings.js";
+
+export default function createRestaurantForm() {
+  const restaurantAddForm = createElement("form");
+
+  restaurantAddForm.append(
+    createDropdownBox({
+      labelText: "카테고리",
+      dropdownList: FOOD_CATEGORY,
+      required: true,
+    }),
+    createInputBox({
+      labelText: "이름",
+      required: true,
+      type: "text",
+      id: "name",
+    }),
+    createDropdownBox({
+      labelText: "거리(도보 이동 시간)",
+      dropdownList: RESTAURANT_DISTANCE,
+      required: true,
+    }),
+    createTextAreaBox({
+      labelText: "설명",
+      id: "description",
+      textCaption: "메뉴 등 추가 정보를 입력해 주세요.",
+      required: true,
+    }),
+    createInputBox({
+      labelText: "참고 링크",
+      required: true,
+      type: "text",
+      id: "link",
+      textCaption: "메장 정보를 확인할 수 있는 링크를 입력해 주세요.",
+    })
+  );
+
+  const buttonContainer = createElement("div", {
+    className: "button-container",
+  });
+
+  buttonContainer.append(
+    createButton({
+      type: "button",
+      className: ["button", "button--secondary", "text-caption"],
+      textContent: "취소하기",
+      onclick: () => console.log("취소"),
+    }),
+    createButton({
+      type: "submit",
+      className: ["button", "button--primary", "text-caption"],
+      textContent: "추가하기",
+      onclick: () => console.log("추가"),
+    })
+  );
+
+  restaurantAddForm.appendChild(buttonContainer);
+
+  return restaurantAddForm;
+}
