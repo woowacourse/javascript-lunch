78676d8e6a7aa7ae38c47b41f4338ddab787809c
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-04 16:12:45 +0900

    ✅ test: 헤더 컴포넌트 생성 테스트 작성
    Co-authored-by: Db0111 <Db0111@users.noreply.github.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
-describe("Test Group", () => {
-  it("Test for the Test", () => {
-    expect(false).to.be.true;
+describe("컴포넌트 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5500/");
+  });
+  it("헤더에서 점심 뭐먹지가 잘 렌더링 되는지 확인", () => {
+    cy.get(".gnb").should("exist");
+    cy.get(".gnb").contains("점심 뭐 먹지");
   });
 });
