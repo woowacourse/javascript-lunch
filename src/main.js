b9fdbcbc40e7ac53301620abc43b0cd474b77daa
ohgus <anakin1108@naver.com>
2025-03-04 15:49:07 +0900

    feat: 음식점 등록
    Co-authored-by: thgml05 <thgml05@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,11 +1,36 @@
 import querySelector from "./utils/querySelector.js";
 
+const storeList = [];
+
 addEventListener("load", () => {
   querySelector.gnbButton().addEventListener("click", () => {
     querySelector.modal().classList.add("modal--open");
   });
 
-  querySelector.modalCancelButton().addEventListener("click", () => {
-    querySelector.modal().classList.remove("modal--open");
-  });
+  querySelector.modalForm().addEventListener("submit", (e) => updateStore(e));
+
+  querySelector.modalCancelButton().addEventListener("click", closeModal);
 });
+
+const updateStore = (e) => {
+  e.preventDefault();
+
+  const store = {
+    category: e.target[0].value,
+    name: e.target[1].value,
+    dist: e.target[2].value,
+    description: e.target[3].value,
+    link: e.target[4].value,
+  };
+  storeList.push(store);
+
+  closeModal();
+
+  Array.from(e.target).forEach((item) => {
+    item.value = "";
+  });
+};
+
+const closeModal = () => {
+  querySelector.modal().classList.remove("modal--open");
+};
--- src/utils/querySelector.js
+++ src/utils/querySelector.js
@@ -1,6 +1,8 @@
 const querySelector = {
   gnbButton: () => document.querySelector(".gnb__button"),
   modal: () => document.querySelector(".modal"),
+  modalForm: () => document.querySelector(".modal-form"),
+  addButton: () => document.querySelector("#add-button"),
   modalCancelButton: () => document.querySelector("#cancel-button"),
 };
 
