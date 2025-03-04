e672a98d3786da437014dc3dc76eb17367b1d1e5
yeji0214 <1225yj50@naver.com>
2025-03-04 15:51:11 +0900

    test: 헤더 컴포넌트 생성 테스트
    Co-authored-by: seo_dev <jin123457@users.noreply.github.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe("Test Group", () => {
-  it("Test for the Test", () => {
-    expect(false).to.be.true;
+describe("컴포넌트 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  })
+  it("헤더 정상 생성", () => {
+    cy.get(".gnb").contains("점심 뭐 먹지");
   });
 });
