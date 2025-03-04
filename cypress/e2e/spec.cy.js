eb842e2bcdb6e65d1a9c806d213e5483b773f5b9
hanheel <hanhee630630@gmail.com>
2025-03-04 15:55:14 +0900

    feat : 헤더 컴포넌트화

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
 describe("Test Group", () => {
-  it("Test for the Test", () => {
-    expect(false).to.be.true;
+  it("헤더 컴포넌트를 정상적으로 렌더링 한다", () => {
+    //given
+    //when
+    //then
+    cy.visit("http://localhost:5173/");
+    cy.get(".gnb__title").should("exist");
+    cy.get(".gnb__title").contains("점심 뭐 먹지");
   });
 });
