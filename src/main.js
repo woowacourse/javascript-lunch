61cbbfdef7656b1bca44c8a2b570271e2f37cfe2
minji2219 <aabb0219@naver.com>
2025-03-04 18:04:51 +0900

    feat: 리스트 렌더링 함수 작성

--- src/main.js
+++ src/main.js
@@ -36,6 +36,40 @@ $("#register-button").addEventListener("click", (e) => {
   const restaurant = new Restaurant(getInfo());
   // 생성된 객체를 list에 넣기
   restaurantList.push(restaurant);
-
+  //모달창 닫기기
   $(".modal").classList.remove("modal--open");
+  //화면에 추가된 리스트를 렌더링
+  renderRestaurants(restaurantList);
 });
+
+const renderRestaurants = (restaurantList) => {
+  const ulTag = $(".restaurant-list");
+  restaurantList.forEach((restaurant) => {
+    ulTag.appendChild(createListHtml(restaurant));
+  });
+};
+const createListHtml = (restaurant) => {
+  const { category, name, distance, description, link } = restaurant.info;
+  const listTag = document.createElement("li");
+  listTag.classList.add("restaurant");
+  listTag.innerHTML = `     
+  <div class="restaurant__category">
+    <img
+      src="./category-korean.png"
+      alt=${category}
+      class="category-icon"
+    />
+  </div>
+  <div class="restaurant__info">
+    <h3 class="restaurant__name text-subtitle">${name}</h3>
+    <span class="restaurant__distance text-body"
+      >캠퍼스부터 ${distance}분 내</span
+    >
+    <p class="restaurant__description text-body">
+     ${description}
+    </p>
+  </div>
+         
+`;
+  return listTag;
+};
