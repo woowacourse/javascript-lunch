71aabdd08bbfbaee78f4c4970070b6e8222a842e
sooyeoniya <tndus1306403@naver.com>
2025-03-04 15:56:07 +0900

    feat(RestaurantList): RestaurantList 컴포넌트 생성
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

---
+++ src/components/restaurantList/RestaurantList.js
@@ -0,0 +1,10 @@
+import RestaurantListItem from "../restaurantListItem/RestaurantListItem.js";
+
+export default function RestaurantList(itemsData) {
+  const $list = document.createElement("ul");
+  $list.className = "restaurant-list";
+
+  itemsData.forEach((data) => $list.appendChild(RestaurantListItem(data)));
+
+  return $list;
+}
