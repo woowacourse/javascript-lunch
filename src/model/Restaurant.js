37f6c694c8621ed07789520dd14a9ecdf8363265
minji2219 <aabb0219@naver.com>
2025-03-04 16:06:12 +0900

    feat: 식당 목데이터 생성 및 식당 controller 구현

--- src/model/Restaurant.js
+++ src/model/Restaurant.js
@@ -1,9 +1,11 @@
 class Restaurant {
+  //이미지지
   // 카테 고리
   // 가게 이름
   // 거리
   // 설명(선택사항)
   // 참고링크(선택사항)
+
   #info;
   constructor({ category, name, distance, description, link }) {
     this.#info = { category, name, distance, description, link };
