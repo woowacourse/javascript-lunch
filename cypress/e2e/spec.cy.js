6b7f39e52a9f36a6f7f0c7c6066585961469ba43
bunju20 <ghkd2271@naver.com>
2025-03-04 17:30:40 +0900

    test: 헤더 테스트 코드 추가 - 헤더 타이틀 테스트 - 헤더 모달 버튼 테스트

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe("Test Group", () => {
-  it("Test for the Test", () => {
-    expect(false).to.be.true;
+describe("헤더 테스트", () => {
+  it("헤더에 '점심 뭐 먹지' 텍스트가 표시되는지 확인한다.", () => {
+    cy.visit("http://localhost:5173/");
+
+    cy.get(".text-title").should("contain", "점심 뭐 먹지");
+  });
+  it("헤더에 모달 버튼이 표시되는지 확인한다.", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("#gnb-button").should("exist");
   });
 });
