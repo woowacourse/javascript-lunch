ade5c43e0070c04022b120231c66a0be9c295622
yeongipark <yeongi0111@naver.com>
2025-03-04 17:39:02 +0900

    feat: 음식점 정보 아이템 컴포넌트 분리

---
+++ src/components/RestaurantItem.js
@@ -0,0 +1,17 @@
+import createElement from "../util/createElement";
+
+export function RestaurantItem({ src, alt, name, distance, description }) {
+  const $restaurantItem = createElement({ tag: "li", className: "restaurant" });
+
+  $restaurantItem.innerHTML = `
+        <div class="restaurant__category">
+            <img src=${src} alt=${alt} class="category-icon">
+          </div>
+          <div class="restaurant__info">
+            <h3 class="restaurant__name text-subtitle">${name}</h3>
+            <span class="restaurant__distance text-body">${distance}</span>
+            <p class="restaurant__description text-body">${description}</p>
+        </div>`;
+
+  return $restaurantItem;
+}
