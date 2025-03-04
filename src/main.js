31c67d12b38773592aa6fa8bd85906ac56353754
H0ngJu <hongju4385@gmail.com>
2025-03-04 17:50:11 +0900

    feat: restaurantList 출력
    Co-authored-by: Claire <eunsoA@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { DOM } from './dom.js';
 import { createHeader } from './components/header.js';
+import { createRestaurantItem } from './components/restaurantItem.js';
+import { RestaurantList } from './restaurantList.js';
 
 createHeader();
+
+const createRestaurantList = (restaurantList) => {
+  restaurantList.forEach((restaurant) => {
+    const restaurantItem = createRestaurantItem(restaurant);
+    DOM.RESTAURANT_LIST.appendChild(restaurantItem);
+  });
+};
+
+createRestaurantList(RestaurantList);
