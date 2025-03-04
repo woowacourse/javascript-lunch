9331c33aff21e4b749a32b2b514a93d324ec8cf1
ohgus <anakin1108@naver.com>
2025-03-04 15:04:58 +0900

    feat: 모달 열고 닫기 구현

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
-import image from "../templates/favorite-icon-filled.png";
-
-console.log("npm run dev 명령어를 통해 점심 뭐 먹지 미션을 시작하세요");
-console.log(
-  "%c ___       ___  ___  ________   ________  ___  ___     \n" +
-    "|\\  \\     |\\  \\|\\  \\|\\   ___  \\|\\   ____\\|\\  \\|\\  \\    \n" +
-    "\\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\___|\\ \\  \\\\\\  \\   \n" +
-    " \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\    \\ \\   __  \\  \n" +
-    "  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\____\\ \\  \\ \\  \\ \n" +
-    "   \\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\ \\__\\ \\__\\\n" +
-    "    \\|_______|\\|_______|\\|__| \\|__|\\|_______|\\|__|\\|__|",
-  "color: #d81b60; font-size: 14px; font-weight: bold;"
-);
+import querySelector from "./utils/querySelector.js";
 
 addEventListener("load", () => {
-  const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = image;
+  querySelector.gnbButton().addEventListener("click", () => {
+    querySelector.modal().classList.add("modal--open");
+  });
 
-  if (app) {
-    app.appendChild(buttonImage);
-  }
+  querySelector.modalCancelButton().addEventListener("click", () => {
+    querySelector.modal().classList.remove("modal--open");
+  });
 });
---
+++ src/utils/querySelector.js
@@ -0,0 +1,7 @@
+const querySelector = {
+  gnbButton: () => document.querySelector(".gnb__button"),
+  modal: () => document.querySelector(".modal"),
+  modalCancelButton: () => document.querySelector("#cancel-button"),
+};
+
+export default querySelector;
