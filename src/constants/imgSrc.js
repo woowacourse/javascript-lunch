c21d7b194d911ed8460f0122d5ca5948a4ad3450
ohgus <anakin1108@naver.com>
2025-03-04 17:38:55 +0900

    feat: store 컴포넌트 추가
    Co-authored-by: thgml05 <thgml05@users.noreply.github.com>

---
+++ src/constants/imgSrc.js
@@ -0,0 +1,10 @@
+const IMG_SRC = {
+  한식: "./public/category-korean.png",
+  중식: "./public/category-chinese.png",
+  일식: "./public/category-japanese.png",
+  양식: "./public/category-western.png",
+  아시안: "./public/category-asian.png",
+  기타: "./public/category-etc.png",
+};
+
+export default IMG_SRC;
