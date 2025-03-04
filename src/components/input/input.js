133eceb793ad93b59412c37f4c989ed4da45ec13
wo-o29 <woogur29@gmail.com>
2025-03-04 16:55:15 +0900

    ​feat: inputBox 컴포넌트 추가
    Co-authored-by: spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>

---
+++ src/components/input/input.js
@@ -0,0 +1,22 @@
+export default function createInputBox({
+  labelText,
+  required,
+  type,
+  id,
+  textCaption = "",
+}) {
+  const inputBox = createElement("div", {
+    className: ["form-item", `${required && "form-item--required"}`],
+  });
+
+  inputBox.innerHTML = `
+    <label for="${id}" text-caption>${labelText}</label>
+    <input type="${type}" name="${id}" id="${id}" ${required && "required"} />
+    ${
+      textCaption &&
+      `<span class="help-text text-caption">${textCaption}</span>`
+    }
+  `;
+
+  return inputBox;
+}
