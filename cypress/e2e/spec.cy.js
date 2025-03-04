66c734e98d3ea2adf7ea0620be9846ef5620190b
yeongipark <yeongi0111@naver.com>
2025-03-04 16:06:14 +0900

    test: 음식점 목록 페이지 접속하면 헤더가 보임

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
 describe("Test Group", () => {
-  it("Test for the Test", () => {
-    expect(false).to.be.true;
+  it("사용자가 음식점 목록 페이지에 접속한다.", () => {
+    cy.visit("http://localhost:5173");
+    cy.get(".gnb").should("be.visible");
   });
 });
+
+// describe("컴포넌트 테스트", () => {
+//   beforeEach(() => {
+//     cy.visit("http://localhost:5173");
+//   });
+
+//   it("헤더에서 점심 뭐 먹지가 잘 렌더링되는지 확인한다.", () => {
+//     cy.get(".gnb").should("exist");
+//     cy.get(".gnb").contains("점심 뭐 먹지");
+//   });
+// });
