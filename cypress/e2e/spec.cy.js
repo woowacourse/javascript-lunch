3baff2df63250f7b694cff015bd3eee04e71499d
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-04 17:44:50 +0900

    test: Body 테스트 코드 추가 - 가게 아이콘이 표시되는지 확인 - 가게 이름이 표시되는지 확인 - 캠퍼스 소요 시간이 표시되는지 확인 - 가게 설명이 표시되는지 확인

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,27 @@
 describe("헤더 테스트", () => {
-  it("헤더에 '점심 뭐 먹지' 텍스트가 표시되는지 확인한다.", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173/");
-
+  });
+  it("헤더에 '점심 뭐 먹지' 텍스트가 표시되는지 확인한다.", () => {
     cy.get(".text-title").should("contain", "점심 뭐 먹지");
   });
   it("헤더에 모달 버튼이 표시되는지 확인한다.", () => {
-    cy.visit("http://localhost:5173/");
     cy.get("#gnb-button").should("exist");
+  });
+
+  it("Body에 레스토랑 아이콘이 표시되는지 확인한다.", () => {
+    cy.get(".restaurant__category").should("exist");
+  });
+
+  it("Body에 레스토랑 title이 표시되는지 확인한다.", () => {
+    cy.get(".restaurant__name").should("exist");
+  });
+
+  it("Body에 레스토랑 소요시간이 표시되는지 확인한다.", () => {
+    cy.get(".restaurant__distance").should("exist");
+  });
+
+  it("Body에 레스토랑 설명이 표시되는지 확인한다.", () => {
+    cy.get(".restaurant__description").should("exist");
   });
 });
