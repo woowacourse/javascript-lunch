1c13226f023f45d8e1f2951b46956743a88ca98b
hwanheejung <hwanheejung117@gmail.com>
2025-03-04 18:03:36 +0900

    feat: Modal 컴포넌트 분리

---
+++ src/components/Modal.js
@@ -0,0 +1,13 @@
+const Modal = () => {
+  return /* html */ `
+    <div class="modal modal--open">
+      <div class="modal-backdrop"></div>
+      <div id="modal-container" class="modal-container">
+        
+      </div>
+    </div>
+    
+    `;
+};
+
+export default Modal;
--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@ import image from "../public/icons/favorite-icon-filled.png";
 import Header from "./components/Header.js";
 import RestaurantList from "./components/RestaurantList.js";
 import { restaurants } from "./database/restaurants.js";
+import AddRestaurantModal from "./components/AddRestaurantModal.js";
+import Modal from "./components/Modal.js";
 
 addEventListener("load", () => {
   const app = document.querySelector("#app");
   const main = document.querySelector("main");
+  const $modal = Modal();
 
   const $header = Header();
   const $restaurantList = RestaurantList(restaurants);
+  const $addRestaurantModal = AddRestaurantModal();
 
   app.insertAdjacentHTML("afterbegin", $header);
   main.insertAdjacentHTML("afterbegin", $restaurantList);
+
+  app.insertAdjacentHTML("afterbegin", $modal);
+  const modal = document.querySelector("#modal-container");
+  modal.insertAdjacentHTML("afterbegin", $addRestaurantModal);
 });
