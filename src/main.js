870742a147f670818d17fe99e1f07a2b1a4ab26e
Jeongeun Lee <leeju1009@gmail.com>
2025-03-04 17:24:26 +0900

    feat: 동적으로 GNB 헤더 생성 및 페이지 로드시 추가
    - createHeader 함수 추가하여 헤더 동적 생성
    - window load 이벤트를 사용해 페이지 로드시 헤더 자동 삽입
    - 기존 정적인 HTML 대신 JavaScript로 GNB 구조 추가
    
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
-import image from "../templates/favorite-icon-filled.png";
+addEventListener('load', () => {
+  const app = document.querySelector('#app');
 
-console.log("npm run dev 명령어를 통해 점심 뭐 먹지 미션을 시작하세요");
-console.log(
-  "%c ___       ___  ___  ________   ________  ___  ___     \n" +
-    "|\\  \\     |\\  \\|\\  \\|\\   ___  \\|\\   ____\\|\\  \\|\\  \\    \n" +
-    "\\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\___|\\ \\  \\\\\\  \\   \n" +
-    " \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\    \\ \\   __  \\  \n" +
-    "  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\____\\ \\  \\ \\  \\ \n" +
-    "   \\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\ \\__\\ \\__\\\n" +
-    "    \\|_______|\\|_______|\\|__| \\|__|\\|_______|\\|__|\\|__|",
-  "color: #d81b60; font-size: 14px; font-weight: bold;"
-);
-
-addEventListener("load", () => {
-  const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = image;
-
-  if (app) {
-    app.appendChild(buttonImage);
-  }
+  const header = createHeader({ title: '점심 뭐 먹지' });
+  app.prepend(header);
 });
+
+function createHeader({ title }) {
+  const header = document.createElement('header');
+
+  header.innerHTML = `<h1 class="gnb__title text-title">${title}</h1>
+    <button type="button" class="gnb__button" aria-label="음식점 추가">
+      <img src="./add-button.png" alt="음식점 추가" />
+    </button>`;
+  header.classList.add('gnb');
+
+  return header;
+}
