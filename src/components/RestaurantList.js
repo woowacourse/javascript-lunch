777461fa7107bd80d57212a810a17187e2843175
youdame <112458620+youdame@users.noreply.github.com>
2025-03-04 17:50:58 +0900

    feat: main에 레스토랑 리스트 추가

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,3 +1,22 @@
-function RestaurantList() {}
+import restaurants from '../../public/data/restaurants.json';
+import createDOMElement from '../util/createDomElement.js';
+import RestaurantIcon from './RestaurantIcon.js';
+import RestaurantItem from './RestaurantItem.js';
+
+function RestaurantList() {
+  return createDOMElement({
+    tag: 'section',
+    class: 'restaurant-list-container',
+    children: [
+      createDOMElement({
+        tag: 'ul',
+        class: 'restaurant-list',
+        children: restaurants.map((restaurant) =>
+          RestaurantItem({ ...restaurant, icon: RestaurantIcon(restaurant.icon) }),
+        ),
+      }),
+    ],
+  });
+}
 
 export default RestaurantList;
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Header from './components/Header.js';
 import PlusButton from './components/PlusButton.js';
 import RestaurantIcon from './components/RestaurantIcon.js';
 import RestaurantItem from './components/RestaurantItem.js';
+import RestaurantList from './components/RestaurantList.js';
 import createDOMElement from './util/createDomElement.js';
-import restaurants from '../public/data/restaurants.json';
 
 addEventListener('load', () => {
   const body = document.querySelector('body');
@@ -11,9 +11,7 @@ addEventListener('load', () => {
   const header = Header({ title: '점심 뭐 먹지', right: PlusButton() });
   body.prepend(header);
 
-  const restaurantList = document.querySelector('.restaurant-list');
+  const main = document.querySelector('main');
 
-  restaurants.forEach((restaurant) => {
-    restaurantList.appendChild(RestaurantItem({ ...restaurant, icon: RestaurantIcon(restaurant.icon) }));
-  });
+  main.appendChild(RestaurantList());
 });
