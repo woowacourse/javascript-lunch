6eef41116f86f0f13dbfb25d2f9028bb2ad88fc0
hwanheejung <hwanheejung117@gmail.com>
2025-03-04 17:06:06 +0900

    feat: Header component 분리

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return /*html*/ `
+    <header class="gnb">
+        <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+        <button type="button" class="gnb__button" aria-label="음식점 추가">
+            <img src="./icons/add-button.png" alt="음식점 추가">
+        </button>
+    </header>
+  `;
+};
+
+export default Header;
--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
-import image from "../templates/favorite-icon-filled.png";
-
-console.log("npm run dev 명령어를 통해 점심 뭐 먹지 미션을 시작하세요");
-console.log(
-  "%c ___       ___  ___  ________   ________  ___  ___     \n" +
-    "|\\  \\     |\\  \\|\\  \\|\\   ___  \\|\\   ____\\|\\  \\|\\  \\    \n" +
-    "\\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\___|\\ \\  \\\\\\  \\   \n" +
-    " \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\    \\ \\   __  \\  \n" +
-    "  \\ \\  \\____\\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\____\\ \\  \\ \\  \\ \n" +
-    "   \\ \\_______\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\ \\__\\ \\__\\\n" +
-    "    \\|_______|\\|_______|\\|__| \\|__|\\|_______|\\|__|\\|__|",
-  "color: #d81b60; font-size: 14px; font-weight: bold;"
-);
+import image from "../public/icons/favorite-icon-filled.png";
+import Header from "./components/Header";
 
 addEventListener("load", () => {
   const app = document.querySelector("#app");
-  const buttonImage = document.createElement("img");
-  buttonImage.src = image;
+  const $header = Header();
 
-  if (app) {
-    app.appendChild(buttonImage);
-  }
+  app.insertAdjacentHTML("afterbegin", $header);
 });
