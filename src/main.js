fb1b7e57c5f812f5de8a88a9eb1cf5f47523d9a7
ahhyun <wndkgus0225@naver.com>
2025-03-04 21:09:39 +0900

    feat: GNB header 동적으로 렌더링
    Co-authored-by: ShinjungOh <97431021+ShinjungOh@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,17 +1,36 @@
-const addRestaurantButton = document.querySelector(".gnb__button");
-const addNewRestaurantModal = document.querySelector("dialog");
-const closeModalButton = document.getElementById("cancel-dialog-btn");
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body");
+  const header = createHeader({ title: "점심 뭐 먹지" });
+  body.prepend(header);
 
-addRestaurantButton.addEventListener("click", () => {
-  addNewRestaurantModal.showModal();
-});
+  const addRestaurantButton = header.querySelector(".gnb__button");
+  const addNewRestaurantModal = document.querySelector("dialog");
+  const closeModalButton = document.getElementById("cancel-dialog-btn");
 
-addNewRestaurantModal.addEventListener("click", (event) => {
-  if (event.target === addNewRestaurantModal) {
+  addRestaurantButton.addEventListener("click", () => {
+    addNewRestaurantModal.showModal();
+  });
+
+  addNewRestaurantModal.addEventListener("click", (event) => {
+    if (event.target === addNewRestaurantModal) {
+      addNewRestaurantModal.close();
+    }
+  });
+
+  closeModalButton.addEventListener("click", () => {
     addNewRestaurantModal.close();
-  }
+  });
 });
 
-closeModalButton.addEventListener("click", () => {
-  addNewRestaurantModal.close();
-});
+const createHeader = ({ title }) => {
+  const header = document.createElement("header");
+
+  header.innerHTML = `
+      <h1 class="gnb__title text-title">${title}</h1>
+      <button type="button" class="gnb__button" aria-label="음식점 추가">
+        <img src="../images/add-button.png" alt="음식점 추가" />
+      </button>`;
+  header.classList.add("gnb");
+
+  return header;
+};
