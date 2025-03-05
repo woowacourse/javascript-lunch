1a78dbcde8fda89601057607e9f0d4cb09ee6165
keemsebeen <sebin0580@gmail.com>
2025-03-05 19:49:03 +0900

    chore: js 확장자 명시
    Co-Authored-By: dev-dino22 <141295691+dev-dino22@users.noreply.github.com>

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,5 +1,5 @@
-import Component from "../../core/Component";
-import { styleStr } from "../../utils/styleStr";
+import Component from "../../core/Component.js";
+import { styleStr } from "../../utils/styleStr.js";
 
 export default class Icon extends Component {
   setDefaultProps() {
