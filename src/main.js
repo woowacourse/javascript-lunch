cb24da2c872bcb4a3e5cb0cb3a5aaacbf10cd8f1
마 <126178440+shuyeon@users.noreply.github.com>
2025-03-05 17:48:34 +0900

    feat : 아이콘 버튼 컴포넌트 구현

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import AddLunchModal from "./component/AddLunchModal/AddLunchModal.js";
 import { DOM } from "./utils/dom.js";
 import TextButton from "./component/TextButton/TextButton.js";
 
-IconButton.init(AddLunchModal.open, DOM.$gnbButton);
+IconButton.init(
+  {
+    src: "./templates/add-button.png",
+    onClick: () => AddLunchModal.open(),
+    label: "음식점 추가",
+  },
+  DOM.$header
+);
 TextButton.init(AddLunchModal.close, DOM.$cancelButton);
--- src/utils/dom.js
+++ src/utils/dom.js
@@ -4,4 +4,5 @@ export const DOM = {
   $gnbButton: $(".gnb__button"),
   $cancelButton: $("#cancel__button"),
   $addButton: $("#add__button"),
+  $header: $(".gnb"),
 };
