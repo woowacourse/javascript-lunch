6f92d5fefa0d0265254c34979d255bbc7f3a4dad
mlnwns <mlnwns@naver.com>
2025-03-05 13:39:30 +0900

    feat: restaurantItem 컴포넌트 구현
    Co-authored-by: 장정안 <ExceptAnyone@users.noreply.github.com>

--- src/components/restaurantItem.js
+++ src/components/restaurantItem.js
@@ -1,21 +1,32 @@
-const restaurantItem = (props) => {
-    const {title, subtitle, description} = props
+import text from "./@common/text";
 
-    return `
-    <div class="restaurant__info">
-      ${text({ tag: "h3", size: "large", children: title })}
-      ${text({
-        tag: "span",
-        size: "medium",
-        color: "orange",
-        children: subtitle,
-      })}
-      ${text({tag: "p" size: "medium", children: "4.6"})}
-      <p class="restaurant__description text-body">
-      ${description}
-      </p>
+const restaurantItem = (props) => {
+  const { category, categoryAlt, title, distance, description } = props;
+
+  return `
+    <li class="restaurant">
+      <div class="restaurant__category">
+        <img
+          src="/category-${category}.png"
+          alt="${categoryAlt}"
+          class="category-icon"
+        />
       </div>
-    `
+      
+      <div class="restaurant__info">
+        ${text({ tag: "h3", size: "large", children: title })}
+        ${text({
+          tag: "span",
+          size: "medium",
+          color: "orange",
+          children: `캠퍼스부터 ${distance}분 내`,
+        })}
+        <p class="restaurant__description text-body">
+          ${description}
+        </p>
+      </div>
+    </li>
+  `;
 };
 
 export default restaurantItem;
