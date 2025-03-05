bccaa84089428d5522acc66d6971f5828c9ef67c
ha-kuku <hakukudh@gmail.com>
2025-03-05 20:43:23 +0900

    feat: 인풋 박스를 생성하는 함수 구현
    Co-authored-by: rosielsh <tnghk9611@naver.com>

---
+++ src/components/createInputBox.js
@@ -0,0 +1,24 @@
+import createElement from '../utils/createElement.js';
+
+function createInputBox({ label, isRequired, type, helpText = '' }) {
+  const inputBoxDiv = createElement('div', `form-item ${isRequired && 'form-item--required'}`);
+  const inputLabel = createElement('label', null, label, { for: `${type} text-caption` });
+  const input = createElement('input', null, null, {
+    type: 'text',
+    name: type,
+    id: type,
+    required: isRequired,
+  });
+
+  const helpTextSpan = createElement('span', 'help-text text-caption', helpText);
+
+  inputBoxDiv.appendChild(inputLabel, input);
+
+  if (helpText !== '') {
+    inputBoxDiv.appendChild(helpTextSpan);
+  }
+
+  return inputBoxDiv;
+}
+
+export default createInputBox;
