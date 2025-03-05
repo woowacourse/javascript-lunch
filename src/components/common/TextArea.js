70f0979e6657043cac9fffdfb52713882522b259
keemsebeen <sebin0580@gmail.com>
2025-03-05 22:20:41 +0900

    refactor: TextArea 컴포넌트 props 수정, event 조건 추가
    Co-Authored-By: dev-dino22 <141295691+dev-dino22@users.noreply.github.com>

--- src/components/common/TextArea.js
+++ src/components/common/TextArea.js
@@ -15,28 +15,30 @@ export default class TextArea extends Component {
   }
 
   setEvent() {
-    const { onChange, id } = this.props;
+    if (!this.props) return;
+    const { onInput, id } = this.props;
     const textAreaContainer = document.querySelector(`#${id}`);
-    textAreaContainer.addEventListener("input", (e) => {
-      e.preventDefault();
-      onInput(e);
-    });
+    if (textAreaContainer) {
+      textAreaContainer.addEventListener("input", (e) => {
+        e.preventDefault();
+        onInput(e);
+      });
+    }
   }
 
   template() {
     const { placeHolder, maxLength, rows, classList, styles, id } = this.props;
     return `
       <textarea 
+        autofocus
         id="${id}"  
         name="${id}"
-        autofocus
-        placeHolder="${placeHolder}">
         rows="${rows}"
         maxlength="${maxLength}"
+        placeholder="${placeHolder}"
         class="${classList.join(" ")} w-full border border-slate-400 px-8 py-8"
         style="${styleStr(styles)}"
-      >
-      </textarea>
+      ></textarea>
     `;
   }
 }
