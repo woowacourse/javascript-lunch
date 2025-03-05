d9aa436ea30c9ab3b6cd3cdfe6bab686e3606ba5
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-05 17:26:40 +0900

    ✅ test: 레스토랑 목록 컴포넌트 테스트케이스 작성
    Co-authored-by: Db0111 <Db0111@users.noreply.github.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,31 @@
-describe("컴포넌트 테스트", () => {
+import { RestaurantData } from "../../src/RestaurantData.js";
+
+describe("헤더 컴포넌트 테스트", () => {
   beforeEach(() => {
-    cy.visit("http://127.0.0.1:5500/");
+    cy.visit("http://localhost:5173/");
   });
-  it("헤더에서 점심 뭐먹지가 잘 렌더링 되는지 확인", () => {
+
+  it("헤더가 렌더링 된다.", () => {
     cy.get(".gnb").should("exist");
-    cy.get(".gnb").contains("점심 뭐 먹지");
+  });
+  it("헤더의 타이틀이 렌더링 된다.", () => {
+    cy.get(".gnb__title").should("exist");
+    cy.get(".gnb__title").contains("점심 뭐 먹지");
+  });
+  it("헤더의 버튼이 렌더링 된다.", () => {
+    cy.get(".gnb__button").should("exist");
+  });
+});
+
+describe("레스트랑 목록 컴포넌트 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/");
+  });
+  it("레스토랑이 랜더링 된다.", () => {
+    cy.get(".restaurant__category").should("exist");
+  });
+  it("레스토랑 갯수만큼 랜더링 된다.", () => {
+    const length = RestaurantData.length;
+    cy.get(".restaurant__category").should("have.length", length);
   });
 });
