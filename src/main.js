22240fe0649623867e33f53cecb621e65e0f602b
mlnwns <mlnwns@naver.com>
2025-03-05 13:40:00 +0900

    feat: restaurantItem 매핑 및 main함수에서 연결
    Co-authored-by: 장정안 <ExceptAnyone@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-// import text from "./components/@common/text.js";
+import { $ } from "./utils/domHelpers.js";
 import restaurantList from "./views/restaurantList.js";
 
-const app = document.querySelector("#app");
+const app = $("#app");
 
-console.log("app", app);
 app.append(restaurantList());
+
+const modalOpenButton = $(".gnb__button");
+const modalContainer = $("#restaurant-modal");
+const closeButton = $(".button--secondary");
+const modalBackdrop = $(".modal-backdrop");
+
+const toggleModal = () => {
+  modalContainer.classList.toggle("modal--open");
+};
+
+modalOpenButton.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+modalBackdrop.addEventListener("click", toggleModal);
