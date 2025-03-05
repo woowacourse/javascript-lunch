4a9a58ef38375c5ce97a5f61955fac7e78a2caeb
ha-kuku <hakukudh@gmail.com>
2025-03-05 20:13:32 +0900

    feat: 모달창 생성하는 함수 구현
    Co-authored-by: rosielsh <tnghk9611@naver.com>

---
+++ src/components/createModal.js
@@ -0,0 +1,14 @@
+import createElement from '../utils/createElement.js';
+
+function createModal(children) {
+  const modalContainer = createElement('div', 'modal');
+  const modalBackdrop = createElement('div', 'modal-backdrop');
+  const modalDiv = createElement('div', 'modal-container');
+
+  modalDiv.appendChild(children);
+  modalContainer.appendChild(modalBackdrop, modalDiv);
+
+  return modalContainer;
+}
+
+export default createModal;
