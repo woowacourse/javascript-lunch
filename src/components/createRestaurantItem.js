bfd873be3ddc070aa7d80a1c87878fe76b4539bc
ha-kuku <hakukudh@gmail.com>
2025-03-05 20:05:32 +0900

    feat: 식당 아이템을 생성하는 함수 구현
    Co-authored-by: rosielsh <tnghk9611@naver.com>

---
+++ src/components/createRestaurantItem.js
@@ -0,0 +1,22 @@
+import createElement from '../utils/createElement.js';
+
+function createRestaurantItem(data) {
+  const restaurantItem = createElement('li', 'restaurant');
+  const categoryDiv = createElement('div', 'restaurant__category');
+  const categoryImg = createElement('img', 'category-icon', null, {
+    src: data.categoryImgSrc,
+    alt: data.categoryAlt,
+  });
+  const infoDiv = createElement('div', 'restaurant__info');
+  const nameHeading = createElement('h3', 'restaurant__name text-subtitle', data.name);
+  const distanceSpan = createElement('span', 'restaurant__distance text-body', data.distance);
+  const descriptionPara = createElement('p', 'restaurant__description text-body', data.description);
+
+  categoryDiv.appendChild(categoryImg);
+  infoDiv.append(nameHeading, distanceSpan, descriptionPara);
+  restaurantItem.append(categoryDiv, infoDiv);
+
+  return restaurantItem;
+}
+
+export default createRestaurantItem;
