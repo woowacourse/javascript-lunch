a02160db1d143c84b8f12b07c0e775f5c472e716
mlnwns <mlnwns@naver.com>
2025-03-05 17:49:30 +0900

    feat: 새로운 음식점 추가 기능 구현
    Co-authored-by: 장정안 <ExceptAnyone@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,19 +1,43 @@
 import { $ } from "./utils/domHelpers.js";
 import restaurantList from "./views/restaurantList.js";
+import { restaurants } from "./constants/restaurantData.js";
 
-const app = $("#app");
+const $app = $("#app");
 
-app.append(restaurantList());
+$app.append(restaurantList());
 
-const modalOpenButton = $(".gnb__button");
-const modalContainer = $("#restaurant-modal");
-const closeButton = $(".button--secondary");
-const modalBackdrop = $(".modal-backdrop");
+const $modalOpenButton = $(".gnb__button");
+const $modalContainer = $("#restaurant-modal");
+const $closeButton = $(".button--secondary");
+const $modalBackdrop = $(".modal-backdrop");
+const $addButton = $("#add-button");
 
 const toggleModal = () => {
-  modalContainer.classList.toggle("modal--open");
+  $modalContainer.classList.toggle("modal--open");
 };
 
-modalOpenButton.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-modalBackdrop.addEventListener("click", toggleModal);
+$modalOpenButton.addEventListener("click", toggleModal);
+$closeButton.addEventListener("click", toggleModal);
+$modalBackdrop.addEventListener("click", toggleModal);
+$addButton.addEventListener("click", toggleModal);
+
+const $category = $("#category");
+const $name = $("#name");
+const $distance = $("#distance");
+const $description = $("#description");
+const $link = $("#link");
+
+$addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const newRestaurant = {
+    category: $category.value,
+    title: $name.value,
+    distance: $distance.value,
+    description: $description.value,
+    link: $link.value,
+  };
+
+  restaurants.push(newRestaurant);
+  restaurantList();
+});
