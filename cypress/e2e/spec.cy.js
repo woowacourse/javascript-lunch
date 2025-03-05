471b34b7b05c23fea140e3225a5027c670e17c4c
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-05 18:35:21 +0900

    ✅ test: 모달 닫힘 테스트 작성
    Co-authored-by: Db0111 <Db0111@users.noreply.github.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -38,4 +38,9 @@ describe("모달 테스트", () => {
     cy.get(".gnb__button").click();
     cy.get(".modal.modal--open").should("exist");
   });
+  it("취소하기 버튼을 눌렀을 때 모달이 정상적으로 닫힌다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get(".button.button--secondary.text-caption").click();
+    cy.get(".modal").should("be.hidden");
+  });
 });
