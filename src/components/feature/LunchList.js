b50732e71b6e8eb924fd27988c38a2c724cc455d
keemsebeen <sebin0580@gmail.com>
2025-03-05 22:24:19 +0900

    feat: LunchList 컴포넌트 item 유무에 따른 조건부 렌더링 로직 추가
    Co-Authored-By: dev-dino22 <141295691+dev-dino22@users.noreply.github.com>

--- src/components/feature/LunchList.js
+++ src/components/feature/LunchList.js
@@ -1,4 +1,5 @@
 import Component from "../../core/Component.js";
+import LunchItem from "./LunchItem.js";
 
 export default class LunchList extends Component {
   setDefaultProps() {
@@ -7,13 +8,46 @@ export default class LunchList extends Component {
     };
   }
 
-  template() {
-    const { lunchList } = this.props;
+  initState() {
+    this.state = {
+      items: [],
+    };
+  }
 
+  setProps(newProps) {
+    if (newProps.lunchList) {
+      this.setState({
+        items: newProps.lunchList,
+      });
+    }
+  }
+
+  // addLunchItem(lunchItemData) {
+  //   const newLunchItem = new LunchItem();
+  //   newLunchItem.setProps(lunchItemData);
+
+  //   this.setState({
+  //     items: [...this.state.items, newLunchItem],
+  //   });
+  // }
+
+  renderItems() {
+    return this.state.items
+      .map((item) => {
+        return item.template();
+      })
+      .join("");
+  }
+
+  template() {
     return `
-      <section class="w-full h-full flex flex-col justify-center items-center my-16">
+      <section class="w-full h-full flex flex-col justify-center items-center my-16 overflow-y">
         <ul id="restaurant-list">
-        ${lunchList.map((lunch) => lunch.template()).join("")}
+        ${
+          this.state.items.length >= 1
+            ? this.renderItems()
+            : '<p class="text-xl">아직 추가된 음식점이 없습니다.</p>'
+        }
         </ul>
       </section>
     `;
