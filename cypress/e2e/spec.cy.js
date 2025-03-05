8a4f0942065be451da1b9ceceb3cf79984c6caf6
hanheel <hanhee630630@gmail.com>
2025-03-05 17:10:24 +0900

    [feat] : 모달 컴포넌트 제작 및 제어

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe("Test Group", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/");
+    cy.viewport(1920, 1080);
   });
   it("헤더 컴포넌트를 정상적으로 렌더링 한다", () => {
     //given
@@ -31,5 +32,19 @@ describe("Test Group", () => {
     //when
     //then
     cy.get(".restaurant-list li").should("exist").and("be.visible");
+  });
+  it("모달 컴포넌트를 정상적으로 렌더링 한다", () => {
+    //then
+    cy.get(".modal").should("exist");
+  });
+  it("음식점 추가 버튼을 클릭했을 때 모달 컴포넌트가 렌더링 된다", () => {
+    cy.get(".gnb__button").click();
+    cy.get(".modal").should("exist");
+  });
+  it("모달 배경화면을 클릭했을 때 모달 컴포넌트가 사라진다", () => {
+    //then
+    cy.get(".gnb__button").click();
+    cy.get(".modal-backdrop").click({ force: true });
+    cy.get(".modal--open").should("not.exist");
   });
 });
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { FoodItem } from "./component/FoodItem.js";
 import { FoodList } from "./component/FoodList.js";
 import { HeaderComponent } from "./component/HeaderComponent.js";
+import { Modal } from "./component/Modal.js";
 import { foodItems } from "./mock/foodItems.js";
 
 addEventListener("load", () => {
@@ -8,4 +9,22 @@ addEventListener("load", () => {
   body.appendChild(HeaderComponent({ title: "점심 뭐 먹지?" }));
 
   body.appendChild(FoodList({ foodItems: foodItems }));
+  body.appendChild(Modal());
+
+  init();
 });
+
+function init() {
+  document.querySelector(".gnb__button").addEventListener("click", () => {
+    const modalOpen = document.querySelector(".modal--open");
+    const modal = document.querySelector(".modal");
+    if (modalOpen) {
+      modal.classList.remove("modal--open");
+    }
+    modal.classList.add("modal--open");
+  });
+
+  document.querySelector(".modal-backdrop").addEventListener("click", () => {
+    document.querySelector(".modal").classList.remove("modal--open");
+  });
+}
