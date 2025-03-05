3e003898f57ae48b04a154470b1e243aaa342bb3
ha-kuku <hakukudh@gmail.com>
2025-03-05 20:04:17 +0900

    feat: 태그를 생성하는 유틸 함수 구현
    Co-authored-by: rosielsh <tnghk9611@naver.com>

---
+++ src/utils/createElement.js
@@ -0,0 +1,14 @@
+function createElement(tag, className, textContent, attributes = {}) {
+  const element = document.createElement(tag);
+
+  if (className) element.className = className;
+  if (textContent) element.textContent = textContent;
+
+  Object.entries(attributes).forEach(([key, value]) => {
+    element[key] = value;
+  });
+
+  return element;
+}
+
+export default createElement;
