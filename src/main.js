b1910ecae7e5e54d9049d329970c9e6498a26fd5
minji2219 <aabb0219@naver.com>
2025-03-05 17:43:52 +0900

    rename : 식당 목 데이터 파일 분리

--- src/main.js
+++ src/main.js
@@ -5,85 +5,16 @@ import { FOOD_CATEGORY } from "./constants/foodCategory";
 import { WALK_TIME_MINUTES } from "./constants/walkTimeMinutes";
 import header from "./components/header";
 import restaurantCard from "./components/restaurantCard";
-
-const mockRestaurants = [
-  new Restaurant({
-    category: "한식",
-    name: "피양콩할마니",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "중식",
-    name: "친친",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "일식",
-    name: "잇쇼우",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "일식",
-    name: "잇쇼우",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "일식",
-    name: "잇쇼우",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "일식",
-    name: "잇쇼우",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "일식",
-    name: "잇쇼우",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "일식",
-    name: "잇쇼우",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-  new Restaurant({
-    category: "일식",
-    name: "잇쇼우",
-    distance: WALK_TIME_MINUTES[0],
-    description: "설명입니다",
-    link: "http//localhost:30000",
-  }),
-];
+import { mockRestaurants } from "./mockRestaurant";
 
 const restaurantList = [...mockRestaurants];
 
 $("#register-button").addEventListener("click", (e) => {
   e.preventDefault();
-  //받은 info로 restaurant 객체를 생성하고
   const restaurant = new Restaurant(getInfo());
-  console.log(restaurant);
-  // 생성된 객체를 list에 넣기
   restaurantList.push(restaurant);
-  //모달창 닫기기
+
   $(".modal").classList.remove("modal--open");
-  //화면에 추가된 리스트를 렌더링
   renderRestaurants(restaurantList);
 });
 
---
+++ src/mockRestaurant.js
@@ -0,0 +1,68 @@
+import { WALK_TIME_MINUTES } from "./constants/walkTimeMinutes";
+import Restaurant from "./model/Restaurant";
+
+export const mockRestaurants = [
+  new Restaurant({
+    category: "한식",
+    name: "피양콩할마니",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "중식",
+    name: "친친",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "일식",
+    name: "잇쇼우",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "일식",
+    name: "잇쇼우",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "일식",
+    name: "잇쇼우",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "일식",
+    name: "잇쇼우",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "일식",
+    name: "잇쇼우",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "일식",
+    name: "잇쇼우",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+  new Restaurant({
+    category: "일식",
+    name: "잇쇼우",
+    distance: WALK_TIME_MINUTES[0],
+    description: "설명입니다",
+    link: "http//localhost:30000",
+  }),
+];
