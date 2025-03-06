9a4ed029e7657cf171069389439d399f3dc1196d
yeji0214 <1225yj50@naver.com>
2025-03-06 16:47:25 +0900

    feat: 음식점 목록 추가 기능 구현
    Co-authored-by: seo_dev <jin123457@users.noreply.github.com>

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,5 +1,36 @@
 import $form from "./form";
 import $inputItem from "./input-item";
+import $restaurantItem from "./restaurant-item";
+
+const getCategoryIcon = (category) => {
+  switch (category) {
+    case "한식":
+      return "../images/category-korean.png";
+    case "중식":
+      return "../images/category-chinese.png";
+    case "일식":
+      return "../images/category-japanese.png";
+    case "양식":
+      return "../images/category-western.png";
+    case "아시안":
+      return "../images/category-asian.png";
+    default:
+      return "../images/category-etc.png";
+  }
+};
+
+const addRestaurant = (data) => {
+  handleModalClose();
+  const categoryIcon = getCategoryIcon(data.category);
+  const newRestaurant = {
+      categoryIcon,
+      categoryTitle: data.category,
+      name: data.name,
+      distance: `캠퍼스부터 ${data.distance}분 내`,
+      description: data.description
+  };
+  document.querySelector(".restaurant-list").appendChild($restaurantItem(newRestaurant));
+};
 
 export const handleModalClose = () => {
   document.querySelector(".modal").classList.remove("modal--open");
@@ -9,8 +40,11 @@ export const handleAddRestaurant = (e) => {
     e.preventDefault();
 
     try {
+      const form = document.getElementById("add-restaurant-form");
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData);
         validateForm(['category', 'name', 'distance']);
-        addRestaurant();
+        addRestaurant(data);
     } catch (error) {
         alert(error.message);
     }
@@ -25,7 +59,7 @@ const validateForm = (requiredFields) => {
         throw new Error(`${labelText}(은)는 필수 값입니다.`);
       }
     });
-    
+
 }
 
 const $modal = (form) => {
