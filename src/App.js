3d7bc6ccc972a6f411a527227936291b555fcb61
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-06 16:21:47 +0900

    ✨ feat: App 컨트롤러 추가
    Co-authored-by: Db0111 <Db0111@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Header from "./Header.js";
+import Restaurant from "./Restaurant.js";
+import { RestaurantData } from "./RestaurantData.js";
+import { categoryValue } from "./optionValue.js";
+import Component from "./Component.js";
+class App extends Component {
+  constructor($target) {
+    super($target);
+    document.addEventListener("restaurantUpdated", () => this.render());
+  }
+
+  render() {
+    document.querySelector(".restaurant-list").innerHTML = "";
+    new Header(document.querySelector(".gnb"));
+
+    RestaurantData.forEach((data) => {
+      const restaurantItem = document.createElement("li");
+      restaurantItem.classList.add("restaurant");
+      console.log(document.querySelector(".restaurant-list"));
+      document.querySelector(".restaurant-list").appendChild(restaurantItem);
+
+      new Restaurant(restaurantItem, data);
+    });
+  }
+}
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,14 +1,2 @@
-import Header from "./Header.js";
-import Restaurant from "./Restaurant.js";
-import { RestaurantData } from "./RestaurantData.js";
-import { categoryValue } from "./optionValue.js";
-
-new Header(document.querySelector(".gnb"));
-
-RestaurantData.forEach((data) => {
-  const restaurantItem = document.createElement("li");
-  restaurantItem.classList.add("restaurant");
-  document.querySelector(".restaurant-list").appendChild(restaurantItem);
-
-  new Restaurant(restaurantItem, data);
-});
+import App from "./App.js";
+new App(document.getElementById("app"));
