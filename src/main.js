7f7f13f1893388bbc24eb98c63017d614a4b638a
ha-kuku <hakukudh@gmail.com>
2025-03-06 17:48:49 +0900

    feat: 취소 버튼을 누르면 모달이 닫히는 기능 구현
    Co-authored-by: rosielsh <tnghk9611@naver.com>

--- src/main.js
+++ src/main.js
@@ -14,78 +14,79 @@ import createButton from './components/createButton.js';
 import createElement from './utils/createElement.js';
 import updateRestaurantList from './components/RestaurantList/updateRestaurantList.js';
 
-const main = document.getElementsByTagName('main')[0];
-const filterContainer = createSectionContainer('restaurant-list-container');
-const restaurantList = createRestaurantList(restaurantItems);
+const $main = document.getElementsByTagName('main')[0];
+const $filterContainer = createSectionContainer('restaurant-list-container');
+const $restaurantList = createRestaurantList(restaurantItems);
 
-filterContainer.appendChild(restaurantList);
+$filterContainer.appendChild($restaurantList);
 
-const modalTitle = createElement('h2', 'modal-title text-title', '새로운 음식점');
-const enrollForm = createElement('form');
+const $modalTitle = createElement('h2', 'modal-title text-title', '새로운 음식점');
+const $enrollForm = createElement('form');
 
-const categoryBox = createSelectBox({
+const $categoryBox = createSelectBox({
   options: categoryOptions,
   isRequired: true,
   type: 'category',
 });
-const nameInputBox = createInputBox({ label: '이름', isRequired: true, type: 'name' });
-const distanceBox = createSelectBox({
+const $$nameInputBox = createInputBox({ label: '이름', isRequired: true, type: 'name' });
+const $distanceBox = createSelectBox({
   options: distanceOptions,
   isRequired: true,
   type: 'distance',
 });
-const descriptionTextArea = createTextArea({
+const $descriptionTextArea = createTextArea({
   label: '설명',
   type: 'description',
   helpText: '메뉴 등 추가 정보를 입력해 주세요.',
 });
-const linkInputBox = createInputBox({
+const $$linkInputBox = createInputBox({
   label: '참고 링크',
   isRequired: false,
   type: 'link',
   helpText: '매장 정보를 확인할 수 있는 링크를 입력해 주세요.',
 });
 
-const buttonContainer = createSectionContainer('button-container');
-const cancelButton = createButton({
+const $buttonContainer = createSectionContainer('button-container');
+const $cancelButton = createButton({
   className: 'button--secondary',
   textContent: '취소하기',
   buttonType: 'button',
 });
-const enrollButton = createButton({
+const $enrollButton = createButton({
   className: 'button--primary',
   textContent: '등록하기',
 });
 
-buttonContainer.append(cancelButton, enrollButton);
+$buttonContainer.append($cancelButton, $enrollButton);
 
-enrollForm.append(
-  categoryBox,
-  nameInputBox,
-  distanceBox,
-  descriptionTextArea,
-  linkInputBox,
-  buttonContainer
+$enrollForm.append(
+  $categoryBox,
+  $$nameInputBox,
+  $distanceBox,
+  $descriptionTextArea,
+  $$linkInputBox,
+  $buttonContainer
 );
 
 const fragment = new DocumentFragment();
-fragment.append(modalTitle, enrollForm);
+fragment.append($modalTitle, $enrollForm);
 
-const enrollRestaurantModal = createModal(fragment);
-main.append(filterContainer, enrollRestaurantModal);
+const $enrollRestaurantModal = createModal(fragment);
+$main.append($filterContainer, $enrollRestaurantModal);
 
-const modal = document.querySelector('.modal');
-const openModalButton = document.querySelector('.gnb__button');
+const $modal = document.querySelector('.modal');
+const $openModalButton = document.querySelector('.gnb__button');
 
-openModalButton.addEventListener('click', () => {
-  modal.classList.add('modal--open');
+$openModalButton.addEventListener('click', () => {
+  $modal.classList.add('modal--open');
 });
 
-const categorySelectBox = document.querySelector('select#category');
-const nameInput = document.querySelector('input#name');
-const distanceSelectBox = document.querySelector('select#distance');
-const descriptionTextarea = document.querySelector('textarea#description');
-const linkInput = document.querySelector('input#link');
+const $categorySelectBox = document.querySelector('select#category');
+const $nameInput = document.querySelector('input#name');
+const $distanceSelectBox = document.querySelector('select#distance');
+const $descriptionTextarea = document.querySelector('textarea#description');
+const $linkInput = document.querySelector('input#link');
+const $backDrop = document.querySelector('.modal-backdrop');
 
 const restaurantInput = {
   category: null,
@@ -95,29 +96,29 @@ const restaurantInput = {
   link: null,
 };
 
-categorySelectBox.addEventListener('change', (event) => {
+$categorySelectBox.addEventListener('change', (event) => {
   restaurantInput.category = event.target.value;
 });
 
-nameInput.addEventListener('change', (event) => {
+$nameInput.addEventListener('change', (event) => {
   restaurantInput.name = event.target.value;
 });
 
-distanceSelectBox.addEventListener('change', (event) => {
+$distanceSelectBox.addEventListener('change', (event) => {
   restaurantInput.distance = event.target.value;
 });
 
-descriptionTextArea.addEventListener('change', (event) => {
+$descriptionTextArea.addEventListener('change', (event) => {
   restaurantInput.description = event.target.value;
 });
 
-linkInput.addEventListener('change', (event) => {
+$linkInput.addEventListener('change', (event) => {
   restaurantInput.link = event.target.value;
 });
 
-const $enrollForm = document.querySelector('form');
+const $$enrollForm = document.querySelector('form');
 
-$enrollForm.addEventListener('submit', (event) => {
+$$enrollForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   if (!restaurantInput.category || !restaurantInput.name || !restaurantInput.distance) {
@@ -127,5 +128,13 @@ $enrollForm.addEventListener('submit', (event) => {
 
   restaurantInput.categoryImgSrc = `./category-${categoryImages[restaurantInput.category]}.png`;
   updateRestaurantList(restaurantInput);
-  modal.classList.remove('modal--open');
+  $modal.classList.remove('modal--open');
+});
+
+$cancelButton.addEventListener('click', () => {
+  $modal.classList.remove('modal--open');
+});
+
+$backDrop.addEventListener('click', () => {
+  $modal.classList.remove('modal--open');
 });
