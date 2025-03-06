da823e82faaab6949f2d6478c180c3f04b6d1237
mlnwns <mlnwns@naver.com>
2025-03-06 15:55:43 +0900

    feat: input 컴포넌트를 사용하여 렌더링 구현
    Co-authored-by: 장정안 <ExceptAnyone@users.noreply.github.com>

---
+++ src/views/linkInput.js
@@ -0,0 +1,18 @@
+import { $ } from "../utils/domHelpers";
+import input from "../components/@common/input";
+
+const linkInput = () => {
+  const $linkInputContainer = $(".link-input");
+
+  $linkInputContainer.innerHTML = `
+    ${input({
+      id: "link",
+      labelText: "참고 링크",
+      spanText: "매장 정보를 확인할 수 있는 링크를 입력해 주세요.",
+    })}
+  `;
+
+  return $linkInputContainer;
+};
+
+export default linkInput;
---
+++ src/views/nameInput.js
@@ -0,0 +1,18 @@
+import input from "../components/@common/input";
+import { $ } from "../utils/domHelpers";
+
+const nameInput = () => {
+  const $inputContainer = $(".name-input");
+
+  $inputContainer.innerHTML = `
+    ${input({
+      id: "name",
+      labelText: "이름",
+      isRequired: true,
+    })}
+`;
+
+  return $inputContainer;
+};
+
+export default nameInput;
