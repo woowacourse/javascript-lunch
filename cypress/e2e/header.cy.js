53fc34e3fee90cc167ad9f18c2e5205f64ee39df
hoyyChoi <hoyeon8165@gmail.com>
2025-03-06 11:28:02 +0900

    test: header 컴포넌트 동적인자 테스트 수정

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,6 +1,15 @@
+import { HEADER_CONTENTS } from "../../src/contants.js";
+
 describe("header 태그 E2E 테스트", () => {
-  it("header 태그가 렌더링이 되었는지 확인한다.", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5500");
+  });
+
+  it("header 태그가 렌더링이 되었는지 확인한다.", () => {
     cy.get("header").should("exist");
+  });
+
+  it("header 컴포넌트 인자가 화면에 표시 되었는지 확인한다.", () => {
+    cy.get("header").should("contain", HEADER_CONTENTS.TITLE);
   });
 });
