b739431218f4e47be9b639f7a2c565b651b15abe
마 <126178440+shuyeon@users.noreply.github.com>
2025-03-06 19:56:32 +0900

    refactor : 상수 분리

---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+export const CATEGORY_ICON = {
+  한식: "./templates/category-korean.png",
+  중식: "./templates/category-chinese.png",
+  일식: "./templates/category-japanese.png",
+  양식: "./templates/category-western.png",
+  아시안: "./templates/category-asian.png",
+  기타: "./templates/category-etc.png",
+};
