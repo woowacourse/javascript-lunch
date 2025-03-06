f678484e52622e4d8f0854e159806922ecc41374
mlnwns <mlnwns@naver.com>
2025-03-06 11:11:57 +0900

    chore: cypress 화면 크기 설정

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    viewportWidth: 1600,
+    viewportHeight: 900,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
