436c9c25e506215eb377ba3d0e6a9169b822d989
ahhyun <wndkgus0225@naver.com>
2025-03-06 14:57:38 +0900

    feat: 사용자 입력값 유효성 검사 및 오류 처리
    Co-authored-by: ShinjungOh <97431021+ShinjungOh@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -32,14 +32,38 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    const nameInput = document.getElementById("name");
+    if (nameInput.value.length > 20) {
+      alert("이름은 최대 20글자까지 가능합니다.");
+      return;
+    }
+
+    const restaurantsNameList = restaurantsData.map((restaurant) => {
+      return restaurant.name;
+    });
+
+    if (restaurantsNameList.includes(nameInput.value)) {
+      alert("기존에 있는 식당과 중복된 이름입니다.");
+      return;
+    }
+
+    const descriptionInput = document.getElementById("description");
+    if (descriptionInput.value.length > 500) {
+      alert("설명은 최대 500글자까지 가능합니다.");
+      return;
+    }
+
     // TODO
     const newRestaurant = {
       category: document.getElementById("category").value,
-      name: document.getElementById("name").value,
+      name: nameInput.value,
       distance: document.getElementById("distance").value + "분 내",
-      description: document.getElementById("description").value,
+      description: descriptionInput.value,
       link: document.getElementById("link").value,
     };
+
+    // TODO 메시지를 상수로 빼두자
+    // constants 폴더에
 
     const restaurantItem = createRestaurantItem(newRestaurant);
     restaurantList.appendChild(restaurantItem);
@@ -74,6 +98,7 @@ const createRestaurantItem = ({
   const li = document.createElement("li");
   li.classList.add("restaurant");
 
+  // TODO 얘도 상수
   const imageSrcByRestaurantsCategory = {
     한식: "./images/category-korean.png",
     중식: "./images/category-chinese.png",
