13bf9e43aa9d4f48fd6905c09d302aa3c2a4d3bf
hanheel <hanhee630630@gmail.com>
2025-03-06 11:15:35 +0900

    [feat] : foodListPage 분리

--- src/main.js
+++ src/main.js
@@ -4,14 +4,10 @@ import { FoodList } from "./component/FoodList.js";
 import { HeaderComponent } from "./component/HeaderComponent.js";
 import { Modal } from "./component/Modal.js";
 import { foodItems } from "./mock/foodItems.js";
+import { FoodListPage } from "./pages/FoodListPage.js";
 
 addEventListener("load", () => {
-  const body = document.querySelector("body");
-  body.appendChild(HeaderComponent({ title: "점심 뭐 먹지?" }));
-
-  body.appendChild(FoodList({ foodItems: foodItems }));
-  Modal({ form: FoodForm() });
-
+  FoodListPage();
   init();
 });
 
