a0e0be8a294ca6c2d5e02e387468896b58b08856
hoyyChoi <hoyeon8165@gmail.com>
2025-03-06 11:19:35 +0900

    refactor: header 컴포넌트 리팩토링

---
+++ src/contants.js
@@ -0,0 +1,4 @@
+export const HEADER_CONTENTS = {
+  TITLE: "점심 뭐 먹지",
+  LABEL: "음식점 추가",
+};
--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
-function renderHeader() {
-  const headerElement = document.createElement("header");
-  headerElement.classList.add("gnb");
-  headerElement.innerHTML = `
-  <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-  <button type="button" class="gnb__button" aria-label="음식점 추가">
-    <img src="./public/add-button.png" alt="음식점 추가" />
-  </button>
-  `;
-  return headerElement;
-}
+import Header from "./components/Header.js";
+import { HEADER_CONTENTS } from "./contants.js";
 
 function renderContents() {
   const app = document.getElementById("app");
-  app.prepend(renderHeader());
+
+  app.prepend(Header(HEADER_CONTENTS));
 }
 
 renderContents();
