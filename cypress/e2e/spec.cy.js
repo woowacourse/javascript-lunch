c84fbd46064330e524d650a338816fbc426dfcbb
mlnwns <mlnwns@naver.com>
2025-03-06 11:06:05 +0900

    test: 음식점 추가 모달 화면 렌더링 테스트
    Co-authored-by: 장정안 <ExceptAnyone@users.noreply.github.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
-describe("Test Group", () => {
-  it("Test for the Test", () => {
-    expect(false).to.be.true;
+describe("음식점 추가 모달 기능 테스트", () => {
+  it("목록 추가 아이콘 클릭 시 음식점 추가 모달이 화면에 보인다.", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get(".modal-container").should("not.be.visible");
+    cy.get(".gnb__button > img").click();
+    cy.get(".modal-container").should("be.visible");
   });
 });
