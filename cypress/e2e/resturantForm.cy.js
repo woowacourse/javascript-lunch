9990966e01e84a02d63b3d74dc384495dc9f7ca1
wo-o29 <woogur29@gmail.com>
2025-03-06 10:58:53 +0900

    test: 음식점 이름 경고창 나오는 시나리오 추가
    Co-authored-by: spoyodevelop <73924592+spoyodevelop@users.noreply.github.com>

--- cypress/e2e/resturantForm.cy.js
+++ cypress/e2e/resturantForm.cy.js
@@ -1,4 +1,6 @@
-it("음식점 추가 테스트", () => {
+import { ERROR_MESSAGE } from "../../src/settings/settings.js";
+
+it("모달 열기 닫기 테스트, 음식점 추가(카테고리, 이름, 거리 입력)", () => {
   cy.visit("http://localhost:5173/");
 
   // 모달 띄우기
@@ -22,5 +24,29 @@ it("음식점 추가 테스트", () => {
 
   // 폼 제출
   cy.get(".restaurant-add-form").submit();
-  cy.get(".restaurant-list").should("have.length", 1);
+  cy.get(".restaurant").should("have.length", 1);
+});
+
+it("모달 열기 테스트, 음식점 추가(음식점 이름을 15자를 입력하여 경고창을 발생시킨다.)", () => {
+  cy.visit("http://localhost:5173/");
+
+  // 모달 띄우기
+  cy.get(".gnb__button").click();
+  cy.get(".modal").should("be.visible");
+  cy.get(".restaurant-add-form").should("exist");
+
+  cy.get("#category").select("한식");
+  cy.get("#name").type("대충이름이긴한식부페같은것마아앙");
+  cy.get("#distance").select("5");
+
+  cy.get(".restaurant-add-form").submit();
+
+  cy.on("window:alert", (alertText) => {
+    expect(alertText).to.equal(ERROR_MESSAGE.INVALID_RESTAURANT_NAME_LENGTH);
+  });
+
+  cy.get("#name").clear();
+  cy.get("#name").type("더휴웨딩몰");
+  cy.get(".restaurant-add-form").submit();
+  cy.get(".restaurant").should("have.length", 1);
 });
