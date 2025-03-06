89abfa1d5bb8b23e09a4491e3c77e7c2a1a5c23b
mlnwns <mlnwns@naver.com>
2025-03-06 16:01:03 +0900

    feat: main.js에서 전체 컴포넌트 렌더링
    Co-authored-by: 장정안 <ExceptAnyone@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
 import { $ } from "./utils/domHelpers.js";
 import restaurantList from "./views/restaurantList.js";
 import { restaurants } from "./constants/restaurantData.js";
+import buttonContainer from "./views/buttonContainer.js";
+import nameInput from "./views/nameInput";
+import linkInput from "./views/linkInput";
+import distanceSelect from "./views/distanceSelect.js";
+import categorySelect from "./views/categorySelect.js";
 
-const $app = $("#app");
-
-$app.append(restaurantList());
+restaurantList();
+buttonContainer();
+nameInput();
+linkInput();
+distanceSelect();
+categorySelect();
 
 const $modalOpenButton = $(".gnb__button");
 const $modalContainer = $("#restaurant-modal");
 const $closeButton = $(".button--secondary");
 const $modalBackdrop = $(".modal-backdrop");
-const $addButton = $("#add-button");
 
 const toggleModal = () => {
   $modalContainer.classList.toggle("modal--open");
@@ -19,25 +26,10 @@ const toggleModal = () => {
 $modalOpenButton.addEventListener("click", toggleModal);
 $closeButton.addEventListener("click", toggleModal);
 $modalBackdrop.addEventListener("click", toggleModal);
-$addButton.addEventListener("click", toggleModal);
+// $addButton.addEventListener("click", toggleModal);
 
-const $category = $("#category");
-const $name = $("#name");
-const $distance = $("#distance");
-const $description = $("#description");
-const $link = $("#link");
-
-$addButton.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  const newRestaurant = {
-    category: $category.value,
-    title: $name.value,
-    distance: $distance.value,
-    description: $description.value,
-    link: $link.value,
-  };
-
-  restaurants.push(newRestaurant);
-  restaurantList();
-});
+// const $category = $("#category");
+// const $name = $("#name");
+// const $distance = $("#distance");
+// const $description = $("#description");
+// const $link = $("#link");
