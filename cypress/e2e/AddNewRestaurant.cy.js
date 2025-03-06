db1be4f67f794b3f5d8c4b96cb4bc8200876bdc9
sooyeoniya <tndus1306403@naver.com>
2025-03-06 11:15:47 +0900

    test: 바텀 시트 닫는 기능 E2E 테스트 구현
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

--- cypress/e2e/AddNewRestaurant.cy.js
+++ cypress/e2e/AddNewRestaurant.cy.js
@@ -1,22 +1,18 @@
-describe("E2E 테스트", () => {
+describe("새로운 음식점 추가 플로우 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5174/");
     cy.viewport(1536, 960);
   });
 
-  it("새로운 음식점 추가 플로우 테스트", () => {
+  it("음식점 추가 버튼을 누르면 바텀 시트가 열리고, 새로운 음식점 정보를 입력하여 제출 시 목록에 해당 음식점 정보가 추가된다.", () => {
     cy.get(".gnb__button").click();
-
     cy.get(".form-item").should("have.length", 5);
 
     cy.contains("label", "카테고리").should("exist").and("be.visible");
-
     cy.contains("label", "이름").should("exist").and("be.visible");
-
     cy.contains("label", "거리(도보 이동 시간)")
       .should("exist")
       .and("be.visible");
-
     cy.contains("label", "설명").should("exist").and("be.visible");
     cy.contains("label", "참고 링크").should("exist").and("be.visible");
 
@@ -47,16 +43,10 @@ describe("E2E 테스트", () => {
       .should("have.value", "링크 테스트용 텍스트");
 
     cy.contains("button", "추가하기").should("exist").and("be.visible").click();
-    cy.get(".modal").should("not.have.class", ".modal--option");
+    cy.get(".modal").should("not.have.class", "modal--open");
 
     cy.contains("h3", "얌생 김밥").should("exist").and("be.visible");
     cy.contains("span", "캠퍼스부터 10분 내").should("exist").and("be.visible");
     cy.contains("p", "설명 테스트용 텍스트").should("exist").and("be.visible");
   });
-
-  // it("모달 취소 버튼 테스트", () => {
-  //   cy.contains("button", "취소하기").should("exist").and("be.visible");
-  // });
-
-  // it("모달 백드롭 테스트", () => {});
 });
