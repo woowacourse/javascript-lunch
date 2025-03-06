8b7cd1f7335c1de87058b326f709fe03222f3d10
keemsebeen <sebin0580@gmail.com>
2025-03-06 21:50:42 +0900

    feat: 카테고리에 따른 이미지 렌더링 함수 추가
    Co-Authored-By: dev-dino22 <141295691+dev-dino22@users.noreply.github.com>

---
+++ src/utils/match.js
@@ -0,0 +1,12 @@
+const category = {
+  한식: "category-korean",
+  중식: "category-chinese",
+  일식: "category-japanese",
+  아시안: "category-asian",
+  양식: "category-western",
+  기타: "category-etc",
+};
+
+export const match = (str) => {
+  return category[str];
+};
