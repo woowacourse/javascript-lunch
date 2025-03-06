40c3bd956e310c6f1f7398843205656c58435f74
hoyyChoi <hoyeon8165@gmail.com>
2025-03-06 13:06:55 +0900

    test: list-item 컴포넌트 내부 클래스 유무 테스트

--- cypress/e2e/listItem.cy.js
+++ cypress/e2e/listItem.cy.js
@@ -6,4 +6,8 @@ describe("list-item(li) 태그 E2E 테스트", () => {
   it("list-item(li) 태그가 렌더링이 되었는지 확인한다.", () => {
     cy.get("li.restaurant").should("exist");
   });
+
+  it("list-item(li) 컴포넌트 내부에 restaurant__info 클래스가 있는지 확인한다.", () => {
+    cy.get("li .restaurant__info").should("exist");
+  });
 });
