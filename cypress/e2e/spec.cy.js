91a877ae764fb034d8a5675414220132957cf121
mlnwns <mlnwns@naver.com>
2025-03-06 11:28:35 +0900

    test: 음식점 추가 모달 내 추가하기 버튼 클릭 시 음식점 추가

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,24 @@
 describe("음식점 추가 모달 기능 테스트", () => {
-  it("목록 추가 아이콘 클릭 시 음식점 추가 모달이 화면에 보인다.", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173/");
+  });
+  it("목록 추가 아이콘 클릭 시 음식점 추가 모달이 화면에 보인다.", () => {
     cy.get(".modal-container").should("not.be.visible");
     cy.get(".gnb__button > img").click();
     cy.get(".modal-container").should("be.visible");
+  });
+
+  it("음식점 추가 모달에서 추가하기 버튼 클릭 시 restaurantList에 추가된다.", () => {
+    cy.get(".restaurant").then(($items) => {
+      const initialLength = $items.length;
+
+      cy.get(".gnb__button > img").click();
+      cy.get("#category").select("한식");
+      cy.get("#name").type("테스트음식점");
+      cy.get("#distance").select("10");
+      cy.get("#add-button").click();
+      cy.get(".restaurant-list").should("contain", "테스트음식점");
+      cy.get(".restaurant").should("have.length", initialLength + 1);
+    });
   });
 });
