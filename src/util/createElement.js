c0486743ea7969d1002130bc6010f753bcef0f95
yeongipark <yeongi0111@naver.com>
2025-03-06 10:41:38 +0900

    refactor: 엘리먼트 속성 추가 로직 변경

--- src/util/createElement.js
+++ src/util/createElement.js
@@ -1,5 +1,11 @@
-export default function createElement({ tag, className }) {
+export default function createElement({ tag, classNames = [], ...attributes }) {
   const $element = document.createElement(tag);
-  $element.classList.add(className);
+
+  classNames.forEach((className) => $element.classList.add(className));
+
+  Object.entries(attributes).forEach(([key, value]) => {
+    $element.setAttribute(key, value);
+  });
+
   return $element;
 }
