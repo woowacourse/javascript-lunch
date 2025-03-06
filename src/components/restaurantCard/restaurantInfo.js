606943eae1052a879df464b7717af4c1e3f754e4
minji2219 <aabb0219@naver.com>
2025-03-06 21:05:38 +0900

    feat : description 컴포넌트 생성

--- src/components/restaurantCard/restaurantInfo.js
+++ src/components/restaurantCard/restaurantInfo.js
@@ -1,19 +1,16 @@
 import Title from "../common/title";
+import Description from "./description";
 import Distance from "./distance";
 
 const RestaurantInfo = ({ name, distance, description }) => {
   const restaurantInfo = document.createElement("div");
   restaurantInfo.classList.add("restaurant__info");
 
-  // restaurantInfo.innerHTML = `
-  // <p class="restaurant__description text-body">
-  //   ${description}
-  // </p>`;
-
   restaurantInfo.appendChild(
     Title(name, "h3", "restaurant__name", "text-subtitle")
   );
   restaurantInfo.appendChild(Distance(distance));
+  restaurantInfo.appendChild(Description(description, true));
 
   return restaurantInfo;
 };
