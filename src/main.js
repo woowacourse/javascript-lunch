831c2e9b20f66b1e3f4d51e8576f592f59d32bdb
hoyyChoi <hoyeon8165@gmail.com>
2025-03-06 11:09:59 +0900

    feat: header 태그 컴포넌트 제작

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-const app = document.getElementById("app");
+function renderHeader() {
+  const headerElement = document.createElement("header");
+  headerElement.classList.add("gnb");
+  headerElement.innerHTML = `
+  <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
+  <button type="button" class="gnb__button" aria-label="음식점 추가">
+    <img src="./public/add-button.png" alt="음식점 추가" />
+  </button>
+  `;
+  return headerElement;
+}
 
-app.insertAdjacentHTML(
-  "beforebegin",
-  `<header class="gnb">
-    <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
-    <button type="button" class="gnb__button" aria-label="음식점 추가">
-      <img src="./public/add-button.png" alt="음식점 추가" />
-    </button>
-  </header>`,
-);
+function renderContents() {
+  const app = document.getElementById("app");
+  app.prepend(renderHeader());
+}
+
+renderContents();
