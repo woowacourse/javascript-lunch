a42df7fa707a1bffa18825a9936b1754c2cc5504
hoyyChoi <hoyeon8165@gmail.com>
2025-03-06 13:46:23 +0900

    feat: select-form 컴포넌트 제작

--- README.md
+++ README.md
@@ -84,11 +84,11 @@
 
 - [ ] `select-form` (label + select.category + notice(옵션) -> 묶고 있는 div)
 
-  - [ ] (템플릿 코드 기반) `select-form` 태그를 바탕으로 테스트 작성
-  - [ ] (템플릿 코드 기반) `select`, `label`, `notice` 태그가 있는지 테스트 작성
-  - [ ] (프로덕트 코드) `select-form` 태그 삭제 -> 테스트 실패
-  - [ ] (테스트 실패를 성공시키기 위해) js를 이용해 동적으로 `select-form` 태그 제작 -> 테스트 성공
-  - [ ] `select-form` 태그를 컴포넌트로 제작
+  - [x] (템플릿 코드 기반) `select-form` 태그를 바탕으로 테스트 작성
+  - [x] (템플릿 코드 기반) `select`, `label`, `notice` 태그가 있는지 테스트 작성
+  - [x] (프로덕트 코드) `select-form` 태그 삭제 -> 테스트 실패
+  - [x] (테스트 실패를 성공시키기 위해) js를 이용해 동적으로 `select-form` 태그 제작 -> 테스트 성공
+  - [x] `select-form` 태그를 컴포넌트로 제작
   - [ ] `select-form` 컴포넌트 리팩토링 (동적 인자 전달/컴포넌트 이동 및 분리)
     - [ ] 동적 인자 -> `select`에 들어갈 리스트들 / label에 들어갈 이름
     - [ ] `select-form` 테스트 코드 수정
--- src/main.js
+++ src/main.js
@@ -3,12 +3,34 @@ import List from "./components/List.js";
 import ListItem from "./components/ListItem.js";
 import { HEADER_CONTENTS, LIST_ITEM_CONTENTS } from "./contants.js";
 
+function SelectForm() {
+  const formItemElement = document.createElement("div");
+  formItemElement.className = "form-item form-item--required";
+
+  formItemElement.innerHTML = `
+    <label for="category text-caption">카테고리</label>
+    <select name="category" id="category" required>
+      <option value="">선택해 주세요</option>
+      <option value="한식">한식</option>
+      <option value="중식">중식</option>
+      <option value="일식">일식</option>
+      <option value="양식">양식</option>
+      <option value="아시안">아시안</option>
+      <option value="기타">기타</option>
+    </select>
+  `;
+
+  return formItemElement;
+}
+
 function renderContents() {
   const app = document.getElementById("app");
   const listContainerElement = app.querySelector(".restaurant-list-container");
+  const modalFormElement = app.querySelector(".modal form");
   const listElement = List(LIST_ITEM_CONTENTS);
   app.prepend(Header(HEADER_CONTENTS));
   listContainerElement.appendChild(listElement);
+  modalFormElement.prepend(SelectForm());
 }
 
 renderContents();
