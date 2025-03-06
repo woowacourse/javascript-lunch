8962e306181ae58a55e20f27564af1a88e3f0e44
hwanheejung <hwanheejung117@gmail.com>
2025-03-06 17:50:20 +0900

    refactor: css 파일 분리

--- src/components/modal/AddRestaurantModal/RestaurantName.js
+++ src/components/modal/AddRestaurantModal/RestaurantName.js
@@ -8,7 +8,7 @@ const RestaurantName = () => {
   const MAX_RESTAURANT_NAME = 15;
 
   const contents = /*html*/ `
-  <input type="text" name="name" id="name" required maxlength="${MAX_RESTAURANT_NAME}" data-testid="restaurant-name"/>
+    <input type="text" name="name" id="name" required maxlength="${MAX_RESTAURANT_NAME}" data-testid="restaurant-name"/>
   `;
 
   return FormFieldContainer({ contents, required, label, name });
