455f4786af517f0e1bdc6cb7dbbe6f985cb713d1
minji2219 <aabb0219@naver.com>
2025-03-06 17:02:36 +0900

    refactor : renderRestaurants 함수를 도메인 로직으로 분리리

---
+++ src/domain/renderRestaurant.js
@@ -0,0 +1,11 @@
+import RestaurantCard from "../components/restaurantCard";
+import { $ } from "../utils/dom";
+
+const renderRestaurants = (restaurantList) => {
+  const ulTag = $(".restaurant-list");
+  restaurantList.forEach((restaurant) => {
+    ulTag.appendChild(RestaurantCard(restaurant));
+  });
+};
+
+export default renderRestaurants;
