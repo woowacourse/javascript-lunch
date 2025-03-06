73b0ebef772e1410376f39717ddf3ede13c72357
yeongipark <yeongi0111@naver.com>
2025-03-06 16:06:46 +0900

    feat: 버튼 컴포넌트에 클릭 이벤트 추가

--- src/components/util/Button.js
+++ src/components/util/Button.js
@@ -1,12 +1,13 @@
 import createElement from "../../util/createElement.js";
 
-export default function Button({ variant, type, text }) {
+export default function Button({ variant, type, text, onClick }) {
   const $button = createElement({
     tag: "button",
     classNames: ["button", `button--${variant}`, "text-caption"],
     type,
   });
   $button.textContent = text;
+  $button.onclick = onClick;
 
   return $button;
 }
