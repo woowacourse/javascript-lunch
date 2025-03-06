579095907129a32f23d06e1e76a71b661cda10cf
bunju20 <ghkd2271@naver.com>
2025-03-06 16:52:35 +0900

    test: 설명 입력 검증 테스트 구현

--- __tests__/validation.test.js
+++ __tests__/validation.test.js
@@ -1,17 +1,35 @@
 describe("모달 사용자 입력 테스트", () => {
-  beforeEach(() => {});
-  it("이름의 입력 값은 공백일 수 없다.", () => {
-    // given
-    const input = "";
+  describe("이름 입력 테스트", () => {
+    it("이름의 입력 값은 공백일 수 없다.", () => {
+      // given
+      const input = "";
 
-    // then
-    expect(() => validateInputValue(input)).toThrow();
+      // then
+      expect(() => validateNameInput(input)).toThrow();
+    });
+    it("이름의 입력 값은 30자를 넘길 수 없다.", () => {
+      // given
+      const input = "a".repeat(31);
+
+      // then
+      expect(() => validateNameInput(input)).toThrow();
+    });
   });
-  it("이름의 입력 값은 30자를 넘길 수 없다.", () => {
-    // given
-    const input = "a".repeat(31);
 
-    // then
-    expect(() => validateInputValue(input)).toThrow();
+  describe("설명 입력 테스트", () => {
+    it("설명의 입력 값은 공백일 수 없다.", () => {
+      // given
+      const input = "";
+
+      // then
+      expect(() => validateDescriptiontInput(input)).toThrow();
+    });
+    it("설명의 입력 값은 1500자를 넘길 수 없다.", () => {
+      // given
+      const input = "a".repeat(1501);
+
+      // then
+      expect(() => validateDescriptiontInput(input)).toThrow();
+    });
   });
 });
--- src/validation/validator.js
+++ src/validation/validator.js
@@ -1,4 +1,4 @@
-export const validateInputValue = (rawInput) => {
+export const validateNameInput = (rawInput) => {
   const input = rawInput.trim();
   if (input === "") {
     throw new Error("이름은 공백일 수 없습니다.");
@@ -6,5 +6,16 @@ export const validateInputValue = (rawInput) => {
 
   if (input.length > 30) {
     throw new Error("이름은 30자를 넘길 수 없습니다.");
+  }
+};
+
+export const validateDescriptiontInput = (rawInput) => {
+  const input = rawInput.trim();
+  if (input === "") {
+    throw new Error("설명은 공백일 수 없습니다.");
+  }
+
+  if (input.length > 1500) {
+    throw new Error("설명은 1500자를 넘길 수 없습니다.");
   }
 };
