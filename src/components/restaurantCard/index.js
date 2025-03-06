e0f07ab076feef9fef5cdc4ebd70190d4409f68d
minji2219 <aabb0219@naver.com>
2025-03-06 20:00:34 +0900

    feat : categoryImage 컴포넌트 구현

--- src/components/restaurantCard/index.js
+++ src/components/restaurantCard/index.js
@@ -1,22 +1,12 @@
-import { FOOD_CATEGORY } from "../../constants/foodCategory";
-import Image from "../common/image";
+import CategoryImage from "./categoryImage";
 import RestaurantInfo from "./restaurantInfo";
 
 const RestaurantCard = (restaurant) => {
-  const { category, name, distance, description, link } = restaurant.info;
+  const { category, name, distance, description } = restaurant.info;
   const restaurantCard = document.createElement("li");
   restaurantCard.classList.add("restaurant");
 
-  // 카테고리 이미지 추가
-  const categoryImageContainer = document.createElement("div");
-  categoryImageContainer.classList.add("restaurant__category");
-  restaurantCard.prepend(categoryImageContainer);
-
-  const src = `./category-${FOOD_CATEGORY[category]}.png`;
-
-  categoryImageContainer.appendChild(Image(src, category, "category-icon"));
-
-  // 식당 정보 추가
+  restaurantCard.prepend(CategoryImage(category));
   restaurantCard.appendChild(RestaurantInfo({ name, distance, description }));
 
   return restaurantCard;
