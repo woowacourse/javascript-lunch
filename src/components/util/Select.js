f5d17de687c11ec6e14b9788e810d6f6231b5726
yeongipark <yeongi0111@naver.com>
2025-03-06 21:41:57 +0900

    fix: select 태그 기본값 설정

--- src/components/util/Select.js
+++ src/components/util/Select.js
@@ -22,6 +22,12 @@ export default function Select({
 
 function createOptions(options) {
   const $fragment = document.createDocumentFragment();
+  const $defaultOption = createElement({
+    tag: "option",
+    value: "",
+  });
+  $defaultOption.textContent = "선택해 주세요.";
+  $fragment.appendChild($defaultOption);
 
   options.forEach((option) => {
     const $option = createElement({ tag: "option", value: option });
