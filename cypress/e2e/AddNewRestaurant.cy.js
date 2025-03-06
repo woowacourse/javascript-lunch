3b1ffefabaaea2bdfb0fef017a42c20614e4caed
sooyeoniya <tndus1306403@naver.com>
2025-03-06 21:03:32 +0900

    test: 필수 정보 입력하지 않았을 경우 음식점 추가되지 않는 플로우 테스트
    Co-authored-by: Yugyeong Kim <kimyou1102@users.noreply.github.com>

--- cypress/e2e/AddNewRestaurant.cy.js
+++ cypress/e2e/AddNewRestaurant.cy.js
@@ -4,7 +4,7 @@ describe("새로운 음식점 추가 플로우 테스트", () => {
     cy.viewport(1536, 960);
   });
 
-  it("음식점 추가 버튼을 누르면 바텀 시트가 열리고, 새로운 음식점 정보를 입력하여 제출 시 목록에 해당 음식점 정보가 추가된다.", () => {
+  const openFormAndCheckLabels = () => {
     cy.get(".gnb__button").click();
     cy.get(".form-item").should("have.length", 5);
 
@@ -15,6 +15,10 @@ describe("새로운 음식점 추가 플로우 테스트", () => {
       .and("be.visible");
     cy.contains("label", "설명").should("exist").and("be.visible");
     cy.contains("label", "참고 링크").should("exist").and("be.visible");
+  };
+
+  it("음식점 추가 버튼을 누르면 바텀 시트가 열리고, 새로운 음식점 정보를 입력하여 제출 시 목록에 해당 음식점 정보가 추가된다.", () => {
+    openFormAndCheckLabels();
 
     const category = "한식";
     cy.get('select[name="category"]')
@@ -48,5 +52,32 @@ describe("새로운 음식점 추가 플로우 테스트", () => {
     cy.contains("h3", "얌생 김밥").should("exist").and("be.visible");
     cy.contains("span", "캠퍼스부터 10분 내").should("exist").and("be.visible");
     cy.contains("p", "설명 테스트용 텍스트").should("exist").and("be.visible");
+  });
+
+  it("음식점 추가 버튼을 누르면 바텀 시트가 열리고, 필수 입력 정보를 입력하지 않을 시 모달창이 닫히지 않고 해당 음식점이 추가가 되지 않는다.", () => {
+    openFormAndCheckLabels();
+
+    const category = "한식";
+    cy.get('select[name="category"]')
+      .should("be.visible")
+      .select(category)
+      .should("have.value", category);
+
+    cy.get('input[name="name"]')
+      .should("be.visible")
+      .type("얌생 김밥")
+      .should("have.value", "얌생 김밥");
+
+    cy.get('textarea[name="description"]')
+      .should("be.visible")
+      .type("설명 테스트용 텍스트")
+      .should("have.value", "설명 테스트용 텍스트");
+
+    cy.contains("button", "추가하기").should("exist").and("be.visible").click();
+    cy.get(".modal").should("have.class", "modal--open");
+
+    cy.contains("h3", "얌생 김밥").should("not.exist");
+    cy.contains("span", "캠퍼스부터 10분 내").should("not.exist");
+    cy.contains("p", "설명 테스트용 텍스트").should("not.exist");
   });
 });
