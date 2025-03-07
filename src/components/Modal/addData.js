ec4dc63eea0cfca70cdb24dd296a0d7a5dfe491b
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-07 15:59:28 +0900

    🐛 fix: 파일 경로 변경

--- src/components/Modal/addData.js
+++ src/components/Modal/addData.js
@@ -8,7 +8,7 @@ const addData = () => {
     name: submittedData.name,
     distance: Number(submittedData.distance),
     description: submittedData.description,
-    imgSrc: `../../../public/templates/category-${submittedData.category}.png`,
+    imgSrc: `../../../public/category-${submittedData.category}.png`,
     imgAlt: `${categoryValue[submittedData.category]}`,
   };
   RestaurantData.push(information);
