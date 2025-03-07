1f21b9177fa39c80f5ca47a0691237cb4497899d
ahhyun <wndkgus0225@naver.com>
2025-03-07 15:36:11 +0900

    test: cypress로 e2e 테스트 작성
    Co-authored-by: ShinjungOh <97431021+ShinjungOh@users.noreply.github.com>

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,29 +1,52 @@
 describe("The Home Page", () => {
-  it("successfully loads", () => {
+  it("페이지가 정상적으로 로드된다.", () => {
     cy.visit("http://localhost:5173/");
   });
 });
 
-describe("Test Group", () => {
-  it("adds todos", () => {
-    cy.visit("http://localhost:5173/");
-    cy.get("#nickname").type(nickname);
-    cy.contains("추가").click();
-    cy.get(".nickname-list").children().last().should("contain.text", nickname);
+describe("Modal Test", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+  });
+
+  it("헤더에서 버튼을 눌러 모달창을 띄운다.", () => {
+    cy.get(".gnb__button").click();
+  });
+
+  it("모달창을 열고 아무것도 입력하지 않은 상태로 취소하기 버튼으로 닫으면 모달이 닫힌다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get("#cancel-dialog-btn").click();
+    cy.get("#add-restaurant-dialog").should("not.be.visible");
+  });
+
+  it("모달의 모든 입력창에 값을 작성해서 추가하기 버튼을 누르면 새 식당이 등록되고, 목록에 식당이 보여진다", () => {
+    cy.get(".gnb__button").click();
+    cy.get("#category").select("한식", { force: true });
+    cy.get("#name").type("한식당");
+    cy.get("#distance").select("5");
+    cy.get("#description").type("맛있는 한식당입니다.");
+    cy.get("#link").type("https://www.google.com/");
+    cy.get("#add-restaurant-btn").click();
+    cy.get(".restaurant-list").contains("한식당").should("be.visible");
+    cy.get("#add-restaurant-dialog").should("not.be.visible");
+  });
+
+  it("모달의 필수 입력창에 값을 작성해서 추가하기 버튼을 누르면 새 식당이 등록되고, 목록에 식당이 보여진다", () => {
+    cy.get(".gnb__button").click();
+    cy.get("#category").select("한식", { force: true });
+    cy.get("#name").type("한식당");
+    cy.get("#distance").select("5");
+    cy.get("#add-restaurant-btn").click();
+    cy.get(".restaurant-list").contains("한식당").should("be.visible");
+    cy.get("#add-restaurant-dialog").should("not.be.visible");
+  });
+
+  it("필수 사항을 입력하지 않고 추가하기 버튼을 클릭하면 에러가 발생한다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get("#category").select("", { force: true });
+    cy.get("#name").type("한식당");
+    cy.get("#distance").select("");
+    cy.get("#add-restaurant-btn").click();
+    cy.get("#add-restaurant-dialog").should("exist");
   });
 });
-
-// // 보임
-// cy.get('.login').should('be.visible');
-
-// // 'form-horizontal' 클래스가 있음
-// cy.get('form').should('have.class', 'form-horizontal');
-
-// // value가 'Jane'이 아님
-// cy.get('input').should('not.have.value', 'Jane');
-
-// // 텍스트가 '10'
-// cy.get('.value').should('have.text', '10');
-
-// // disabled 속성이 있음(true)
-// cy.get('.btn-inc').should('have.attr', 'disabled');
