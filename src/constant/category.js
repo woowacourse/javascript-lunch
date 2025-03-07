8d0001e203f8d5abef6301996a500aaca0d8cee4
H0ngJu <hongju4385@gmail.com>
2025-03-07 16:19:07 +0900

    chore: img 링크 수정

--- src/constant/category.js
+++ src/constant/category.js
@@ -1,26 +1,26 @@
 const CATEGORY = Object.freeze({
   KOREAN: {
-    SRC: 'https://h0ngju.github.io/javascript-lunch/public/assets/category-korean.png',
+    SRC: '../../public/assets/category-korean.png',
     ALT: '한식',
   },
   CHINESE: {
-    SRC: 'https://h0ngju.github.io/javascript-lunch/public/assets/category-chinese.png',
+    SRC: '../../public/assets/category-chinese.png',
     ALT: '중식',
   },
   JAPANESE: {
-    SRC: 'https://h0ngju.github.io/javascript-lunch/public/assets/category-japanese.png',
+    SRC: '../../public/assets/category-japanese.png',
     ALT: '일식',
   },
   WESTERN: {
-    SRC: 'https://h0ngju.github.io/javascript-lunch/public/assets/category-western.png',
+    SRC: '../../public/assets/category-western.png',
     ALT: '양식',
   },
   ASIAN: {
-    SRC: 'https://h0ngju.github.io/javascript-lunch/public/assets/category-asian.png',
+    SRC: '../../public/assets/category-asian.png',
     ALT: '아시안',
   },
   ETC: {
-    SRC: 'https://h0ngju.github.io/javascript-lunch/public/assets/category-etc.png',
+    SRC: '../../public/assets/category-etc.png',
     ALT: '기타',
   },
 });
