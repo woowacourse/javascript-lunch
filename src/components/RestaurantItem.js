1d0a8e4dd6837292342a947c59e94c7f65a7983b
Cron <hojoong.choi@woowahan.com>
2025-03-07 11:32:25 +0900

    refactor: 레스토랑 아이템

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,28 +1,35 @@
-export function createRestaurantItem() {
+const CATEGORY_IMAGE = {
+  한식: "./category-korean.png",
+  중식: "./category-chinese.png",
+  일식: "./category-japanese.png",
+  양식: "./category-western.png",
+  아시안: "./category-asian.png",
+  기타: "./category-etc.png",
+};
+
+export function createRestaurantItem({
+  category,
+  restaurantName,
+  distance,
+  description,
+}) {
   const restaurantItem = document.createElement("li");
   restaurantItem.classList.add("restaurant");
   restaurantItem.innerHTML = /*html*/ `
     <div class="restaurant__category">
       <img
-        src="./category-korean.png"
-        alt="한식"
+        src="${CATEGORY_IMAGE[category] || "./category-etc.png"}"
+        alt="${category}"
         class="category-icon"
       />
     </div>
     <div class="restaurant__info">
-      <h3 class="restaurant__name text-subtitle">피양콩할마니</h3>
+      <h3 class="restaurant__name text-subtitle">${restaurantName}</h3>
       <span class="restaurant__distance text-body"
-        >캠퍼스부터 10분 내</span
+        >캠퍼스부터 ${distance}분 내</span
       >
       <p class="restaurant__description text-body">
-        평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩
-        할마니. 두부를 빼지 않은 되비지를 맛볼 수 있는 곳으로, ‘피양’은
-        평안도 사투리로 ‘평양’을 의미한다. 딸과 함께 운영하는 이곳에선
-        맷돌로 직접 간 콩만을 사용하며, 일체의 조미료를 넣지 않은
-        건강식을 선보인다. 콩비지와 피양 만두가 이곳의 대표 메뉴지만,
-        할머니가 옛날 방식을 고수하며 만들어내는 비지전골 또한 이 집의
-        역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은
-        만큼 덜어 먹을 수 있게 준비돼 있다.
+        ${description}
       </p>
     </div>
   `;
--- src/main.js
+++ src/main.js
@@ -10,6 +10,52 @@ addEventListener("load", () => {
   body.prepend(header);
 
   //////// 다음 컴포넌트
-  const restaurantItem = createRestaurantItem();
-  restaurantList.append(restaurantItem);
+  restaurantList.append(
+    createRestaurantItem({
+      category: "한식",
+      restaurantName: "피양콩할마니",
+      distance: 10,
+      description: "평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩",
+    })
+  );
+  restaurantList.append(
+    createRestaurantItem({
+      category: "중식",
+      restaurantName: "친친",
+      distance: 5,
+      description: "Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은",
+    })
+  );
+  restaurantList.append(
+    createRestaurantItem({
+      category: "일식",
+      restaurantName: "잇쇼우",
+      distance: 10,
+      description: "잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다",
+    })
+  );
+  restaurantList.append(
+    createRestaurantItem({
+      category: "양식",
+      restaurantName: "파스타",
+      distance: 10,
+      description: "파스타",
+    })
+  );
+  restaurantList.append(
+    createRestaurantItem({
+      category: "기타",
+      restaurantName: "도넛",
+      distance: 10,
+      description: "도넛",
+    })
+  );
+  restaurantList.append(
+    createRestaurantItem({
+      category: "아시안",
+      restaurantName: "베트남쌀국수",
+      distance: 10,
+      description: "쌀국수",
+    })
+  );
 });
