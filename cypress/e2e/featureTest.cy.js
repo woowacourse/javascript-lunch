61b323e0245ab37f9b3e2020b32f8f1945c2de59
Jeongeun Lee <leeju1009@gmail.com>
2025-03-07 14:35:56 +0900

    test: modal이 열리는지 확인하는 로직 수정
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- cypress/e2e/featureTest.cy.js
+++ cypress/e2e/featureTest.cy.js
@@ -5,7 +5,7 @@ describe('기능 테스트', () => {
 
   it('사용자가 음식점 추가 모달을 열어, 모든 필드를 입력 후 음식점을 추가하는 시나리오 테스트', () => {
     cy.get('.gnb__button').click();
-    cy.get('.modal').should('exist');
+    cy.get('.modal--open').should('exist');
 
     cy.get('#category').select('한식', { force: true });
     cy.get('#name').type('꺼벙이');
@@ -19,7 +19,7 @@ describe('기능 테스트', () => {
 
   it('사용자가 음식점 추가 모달을 열어, 필수 입력값만 입력 후 추가하기 시나리오 테스트', () => {
     cy.get('.gnb__button').click();
-    cy.get('.modal').should('exist');
+    cy.get('.modal--open').should('exist');
 
     cy.get('#category').select('한식', { force: true });
     cy.get('#name').type('꺼벙이');
