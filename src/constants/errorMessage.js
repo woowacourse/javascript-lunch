bb3f979a82f47a8c9ff69cd8625051fbf0a8c917
yeongipark <yeongi0111@naver.com>
2025-03-07 15:06:36 +0900

    refactor: 객체로 선언한 상수를 동결

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,7 +1,7 @@
-const ERROR_MASSAGE = {
+const ERROR_MASSAGE = Object.freeze({
   category: "카테고리를 선택해 주세요",
   distance: "거리를 선택해 주세요",
   name: "식당 이름은 2글자 이상 입력해 주세요",
-};
+});
 
 export default ERROR_MASSAGE;
