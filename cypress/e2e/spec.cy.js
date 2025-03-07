fef3e319e004020e0cb5206f5831a98734a2062c
ahhyun <wndkgus0225@naver.com>
2025-03-07 14:50:02 +0900

    refactor: 이미지 경로 변경
    (cat coauthor.txt)

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,29 @@
-describe("Test Group", () => {
-  it("Test for the Test", () => {
-    expect(false).to.be.true;
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("http://localhost:5173/");
   });
 });
+
+describe("Test Group", () => {
+  it("adds todos", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("#nickname").type(nickname);
+    cy.contains("추가").click();
+    cy.get(".nickname-list").children().last().should("contain.text", nickname);
+  });
+});
+
+// // 보임
+// cy.get('.login').should('be.visible');
+
+// // 'form-horizontal' 클래스가 있음
+// cy.get('form').should('have.class', 'form-horizontal');
+
+// // value가 'Jane'이 아님
+// cy.get('input').should('not.have.value', 'Jane');
+
+// // 텍스트가 '10'
+// cy.get('.value').should('have.text', '10');
+
+// // disabled 속성이 있음(true)
+// cy.get('.btn-inc').should('have.attr', 'disabled');
--- src/restaurantsData.js
+++ src/restaurantsData.js
@@ -5,7 +5,6 @@ export const restaurantsData = [
     distance: "캠퍼스부터 10분 내",
     description:
       "평양 출신의 할머니가 수십 년간 운영해온 비지 전문점. 두부를 빼지 않은 되비지를 맛볼 수 있는 곳.",
-    imgSrc: "./images/category-korean.png",
   },
   {
     category: "중식",
@@ -13,8 +12,6 @@ export const restaurantsData = [
     distance: "캠퍼스부터 5분 내",
     description:
       "Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과 정성으로 정통 중식의 세계를 펼쳐갑니다",
-    imgSrc: "../images/category-chinese.png",
-    alt: "중식",
   },
   {
     category: "일식",
@@ -22,7 +19,6 @@ export const restaurantsData = [
     distance: "캠퍼스부터 10분 내",
     description:
       "정통 자가제면 사누끼 우동을 제공하는 일식당. 기술은 정성을 이길 수 없다는 신념으로 모든 음식에 최선을 다합니다.",
-    imgSrc: "./images/category-japanese.png",
   },
   {
     category: "양식",
@@ -30,7 +26,6 @@ export const restaurantsData = [
     distance: "캠퍼스부터 20분 내",
     description:
       "늘 변화를 추구하는 이태리키친. 현대적인 감각의 양식 메뉴를 선보입니다.",
-    imgSrc: "./images/category-western.png",
   },
   {
     category: "아시안",
@@ -38,7 +33,6 @@ export const restaurantsData = [
     distance: "캠퍼스부터 15분 내",
     description:
       "푸짐한 양과 일품 국물이 매력인 쌀국수 전문점. 다양한 아시안 요리를 즐길 수 있습니다.",
-    imgSrc: "./images/category-asian.png",
   },
   {
     category: "기타",
@@ -46,6 +40,5 @@ export const restaurantsData = [
     distance: "캠퍼스부터 5분 내",
     description:
       "멕시칸 캐주얼 그릴. 다양한 메뉴와 분위기를 즐길 수 있는 곳입니다.",
-    imgSrc: "./images/category-etc.png",
   },
 ];
