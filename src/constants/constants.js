7dad116b0a997902b2ad11068c4f9e19d0a3607a
ahhyun <wndkgus0225@naver.com>
2025-03-07 14:41:32 +0900

    refactor: 이미지 절대 경로 변경

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,10 +5,10 @@ export const ERROR_MESSAGE = {
 };
 
 export const IMAGE_SRC_BY_RESTAURANTS_CATEGORY = {
-  한식: "./images/category-korean.png",
-  중식: "./images/category-chinese.png",
-  일식: "./images/category-japanese.png",
-  양식: "./images/category-western.png",
-  아시안: "./images/category-asian.png",
-  기타: "./images/category-etc.png",
+  한식: "images/category-korean.png",
+  중식: "images/category-chinese.png",
+  일식: "images/category-japanese.png",
+  양식: "images/category-western.png",
+  아시안: "images/category-asian.png",
+  기타: "images/category-etc.png",
 };
