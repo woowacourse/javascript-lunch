6dc4e1d60a4439ecdc5e3b2d88babbc3887a7229
yeongipark <yeongi0111@naver.com>
2025-03-07 14:41:30 +0900

    test: 모달창이 열리는지 테스트

---
+++ cypress/e2e/modal.cy.js
@@ -0,0 +1,11 @@
+describe("모달창 e2e 테스트", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+    cy.viewport(1280, 720);
+  });
+
+  it("음식점 목록에서 우측 상단의 추가 버튼을 누르면 모달창이 보인다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get(".modal-container").should("be.visible");
+  });
+});
