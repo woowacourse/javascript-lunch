bf788fe14a6ae456039d8c7a6516aafa0f655c68
Jeongeun Lee <leeju1009@gmail.com>
2025-03-07 13:56:43 +0900

    feat: 모달창 UI 구현
    Co-authored-by: Youjeong Jeong <jeongyou@users.noreply.github.com>

--- src/constants.js
+++ src/constants.js
@@ -1,54 +1,102 @@
 export const CATEGORY = {
-  KOREAN: '한식',
-  CHINESE: '중식',
-  JAPANESE: '일식',
-  WESTERN: '양식',
-  ASIAN: '아시안',
-  ETC: '기타',
+  label: '카테고리',
+  name: 'category',
+  lists: new Map([
+    ['KOREAN', '한식'],
+    ['CHINESE', '중식'],
+    ['JAPANESE', '일식'],
+    ['WESTERN', '양식'],
+    ['ASIAN', '아시안'],
+    ['ETC', '기타'],
+  ]),
+};
+
+export const DISTANCE = {
+  label: '거리(도보 이동 시간)',
+  name: 'distance',
+  lists: new Map([
+    ['FIVE_MIN', '5분 내'],
+    ['TEN_MIN', '10분 내'],
+    ['FIFTEEN_MIN', '15분 내'],
+    ['TWENTY_MIN', '20분 내'],
+    ['THIRTY_MIN', '30분 내'],
+  ]),
+};
+
+export const NAME = {
+  label: '이름',
+  name: 'name',
+  helpText: '',
+  required: 'form-item--required',
+};
+
+export const LINK = {
+  label: '참고 링크',
+  name: 'link',
+  helpText: '매장 정보를 확인할 수 있는 링크를 입력해 주세요.',
+  required: '',
+};
+
+export const DESCRIPTION = {
+  label: '설명',
+  name: 'description',
+  helpText: '메뉴 등 추가 정보를 입력해 주세요.',
+};
+
+export const CANCEL_BUTTON = {
+  type: 'button',
+  className: 'button--secondary',
+  content: '취소하기',
+};
+
+export const ADD_BUTTON = {
+  type: 'submit',
+  className: 'button--primary',
+  content: '추가하기',
 };
 
 export const RESTAURANTS = [
   {
-    category: `${CATEGORY.KOREAN}`,
+    category: `${CATEGORY.lists.get('KOREAN')}`,
     name: '피양콩할마니',
-    distance: 10,
+    distance: '10분 내',
     description:
       '평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩 할마니. 두부를 빼지 않은 되비지를 맛볼 수 있는 곳으로, ‘피양’은 평안도 사투리로 ‘평양’을 의미한다. 딸과 함께 운영하는 이곳에선 맷돌로 직접 간 콩만을 사용하며, 일체의 조건강식을 선보인다. 콩비이곳의 대표메뉴지만, 할머니가 옛날만들어내는 비지전골 또한느낄 수 있는 특별한메뉴다. 반찬은 손님들이덜어 먹을 수 있게 준비돼 있다.',
     link: '',
   },
   {
-    category: `${CATEGORY.CHINESE}`,
+    category: `${CATEGORY.lists.get('CHINESE')}`,
     name: '친친',
-    distance: 5,
+    distance: '5분 내',
     description: 'Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과 정성으로 정통 중식의 세계를 펼쳐갑니다',
     link: '',
   },
   {
-    category: `${CATEGORY.JAPANESE}`,
+    category: `${CATEGORY.lists.get('JAPANESE')}`,
     name: '잇쇼우',
-    distance: 10,
+    distance: '10분 내',
     description:
       '잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은 정성을 이길 수 없다는 신념으로 모든 음식에 최선을 다하는 잇쇼우는 고객 한분 한분께 최선을 다하겠습니다',
     link: '',
   },
   {
-    category: `${CATEGORY.WESTERN}`,
+    category: `${CATEGORY.lists.get('WESTERN')}`,
     name: '이태리키친',
-    distance: 20,
+    distance: '20분 내',
     description: '늘 변화를 추구하는 이태리키친입니다.',
     link: '',
   },
   {
-    category: `${CATEGORY.ASIAN}`,
+    category: `${CATEGORY.lists.get('ASIAN')}`,
     name: '호아빈 삼성점',
-    distance: 15,
+    distance: '15분 내',
     description: '푸짐한 양에 국물이 일품인 쌀국수',
     link: '',
   },
   {
-    category: `${CATEGORY.ETC}`,
+    category: `${CATEGORY.lists.get('ETC')}`,
     name: '도스타코스 선릉점',
-    distance: 5,
+    distance: '5분 내',
     description: '멕시칸 캐주얼 그릴',
     link: '',
   },
--- src/lunchUI.js
+++ src/lunchUI.js
@@ -20,7 +20,7 @@ function createRestaurantItem({ category, name, distance, description, link }) {
               </div>
               <div class="restaurant__info">
                 <h3 class="restaurant__name text-subtitle">${name}</h3>
-                <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+                <span class="restaurant__distance text-body">캠퍼스부터 ${distance}</span>
                 <p class="restaurant__description text-body">
                   ${description}
                 </p>
@@ -31,9 +31,65 @@ function createRestaurantItem({ category, name, distance, description, link }) {
   return item;
 }
 
+function createModal() {
+  const modal = `<div class="modal">
+      <div class="modal-backdrop"></div>
+      <div class="modal-container">
+        <h2 class="modal-title text-title">새로운 음식점</h2>
+        <form id="new-restaurant-form"></form>
+      </div>
+    </div>`;
+
+  return modal;
+}
+
+function createSelect(fieldName) {
+  const select = `<div class="form-item form-item--required">
+    <label for="${fieldName.name} text-caption">${fieldName.label}</label>
+    <select name="${fieldName.name}" id="${fieldName.name}" required>
+      <option value="">선택해 주세요</option>
+      ${Array.from(fieldName.lists.values()).map((list) => {
+        return `<option value="${list}">${list}</option>`;
+      })}
+    </select>
+  </div>`;
+
+  return select;
+}
+
+function createInput(fieldName) {
+  const input = `<div class="form-item ${fieldName.required}">
+    <label for="${fieldName.name} text-caption">${fieldName.label}</label>
+    <input type="text" name="${fieldName.name}" id="${fieldName.name}">
+    <span class="help-text text-caption">${fieldName.helpText}</span>
+  </div>`;
+
+  return input;
+}
+
+function createTextarea(fieldName) {
+  const textarea = `<div class="form-item">
+      <label for="${fieldName.name} text-caption">${fieldName.label}</label>
+      <textarea name="${fieldName.name}" id="${fieldName.name}" cols="30" rows="5"></textarea>
+      <span class="help-text text-caption">${fieldName.helpText}</span>
+    </div>`;
+
+  return textarea;
+}
+
+function createButton(fieldName) {
+  const button = `<button type="${fieldName.type}" class="button ${fieldName.className} text-caption">${fieldName.content}</button>`;
+  return button;
+}
+
 const lunchUI = {
   createHeader,
   createRestaurantItem,
+  createModal,
+  createSelect,
+  createInput,
+  createTextarea,
+  createButton,
 };
 
 export default lunchUI;
