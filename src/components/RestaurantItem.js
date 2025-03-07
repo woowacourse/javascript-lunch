fddc8929559d8bc514545c16780dc5306cd3f279
ha-kuku <hakukudh@gmail.com>
2025-03-07 16:12:19 +0900

    refactor: 관심사에 따른 함수 분리
    Co-authored-by: rosielsh <tnghk9611@naver.com>

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,13 +1,10 @@
 import createElement from '../utils/createElement.js';
 
-function createRestaurantItem(data) {
-  const restaurantItem = createElement('li', 'restaurant');
-  const categoryDiv = createElement('div', 'restaurant__category');
+function createTags(data) {
   const categoryImg = createElement('img', 'category-icon', null, {
     src: data.categoryImgSrc,
     alt: data.category,
   });
-  const infoDiv = createElement('div', 'restaurant__info');
   const nameHeading = createElement('h3', 'restaurant__name text-subtitle', data.name);
   const distanceSpan = createElement(
     'span',
@@ -16,7 +13,17 @@ function createRestaurantItem(data) {
   );
   const descriptionPara = createElement('p', 'restaurant__description text-body', data.description);
 
-  categoryDiv.appendChild(categoryImg);
+  return { categoryImg, nameHeading, distanceSpan, descriptionPara };
+}
+
+function createRestaurantItem(data) {
+  const restaurantItem = createElement('li', 'restaurant');
+  const categoryDiv = createElement('div', 'restaurant__category');
+  const infoDiv = createElement('div', 'restaurant__info');
+
+  const { categoryImg, nameHeading, distanceSpan, descriptionPara } = createTags(data);
+
+  categoryDiv.append(categoryImg);
   infoDiv.append(nameHeading, distanceSpan, descriptionPara);
   restaurantItem.append(categoryDiv, infoDiv);
 
