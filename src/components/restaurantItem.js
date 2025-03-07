ac26fb9c55ac7fe90e8ea4b6d171b0d1967bacd2
장정안 <wjddks96@naver.com>
2025-03-07 16:17:53 +0900

    fix: 배포된 주소로 이미지가 안뜨는 현상 해결 -> 이미지 경로 변경

--- src/components/restaurantItem.js
+++ src/components/restaurantItem.js
@@ -1,4 +1,19 @@
 import text from "./@common/text";
+import koreanIcon from "../../public/category-korean.png";
+import chineseIcon from "../../public/category-chinese.png";
+import japaneseIcon from "../../public/category-japanese.png";
+import westernIcon from "../../public/category-western.png";
+import asianIcon from "../../public/category-asian.png";
+import etcIcon from "../../public/category-etc.png";
+
+const categoryIcons = {
+  korean: koreanIcon,
+  chinese: chineseIcon,
+  japanese: japaneseIcon,
+  western: westernIcon,
+  asian: asianIcon,
+  etc: etcIcon,
+};
 
 const restaurantItem = (props) => {
   const { category, categoryAlt, title, distance, description } = props;
@@ -7,7 +22,7 @@ const restaurantItem = (props) => {
     <li class="restaurant">
       <div class="restaurant__category">
         <img
-          src="/category-${category}.png"
+          src="${categoryIcons[category]}"
           alt="${categoryAlt}"
           class="category-icon"
         />
