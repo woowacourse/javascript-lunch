eafe4c0912ea14a054c6e576f3ff4419a5484a7f
H0ngJu <hongju4385@gmail.com>
2025-03-07 15:16:43 +0900

    test: 새로고침 테스트 추가
    Co-authored-by: Claire <eunsoA@users.noreply.github.com>

--- cypress/e2e/init.cy.js
+++ cypress/e2e/init.cy.js
@@ -1,3 +1,4 @@
+import { TEST_CONSTANT } from './constants/testConstant';
 import { VISIT_URL_CONSTANT } from './constants/visitUrl';
 
 describe('초기 음식점 목록 페이지를 디자인 화면과 같이 구성한다.', () => {
@@ -15,5 +16,20 @@ describe('초기 음식점 목록 페이지를 디자인 화면과 같이 구성
 
   it('리스트 안에 아이템이 있어야 한다.', () => {
     cy.get('.restaurant').should('be.visible');
+  });
+});
+
+describe('새로고침 시 이전에 추가한 새로운 음식점 정보는 초기화된다.', () => {
+  it('새로고침 시 이전에 추가한 새로운 음식점 정보는 초기화된다.', () => {
+    cy.visit(VISIT_URL_CONSTANT.LINK);
+    cy.get('.gnb__button').click();
+    cy.get('#category').select(TEST_CONSTANT.CATEGORY);
+    cy.get('#name').type(TEST_CONSTANT.NAME);
+    cy.get('#distance').select(TEST_CONSTANT.DISTANCE);
+
+    cy.get('button').contains('추가하기').click();
+    cy.get('li').contains(TEST_CONSTANT.NAME);
+    cy.reload();
+    cy.get('li').should('not.contain', TEST_CONSTANT.NAME);
   });
 });
