4786ed8a072cf71b10982eec6090910561bca2eb
H0ngJu <hongju4385@gmail.com>
2025-03-07 15:06:31 +0900

    test: e2e 테스트를 위한 상수 선언
    Co-authored-by: Claire <eunsoA@users.noreply.github.com>

---
+++ cypress/e2e/constants/testConstant.js
@@ -0,0 +1,7 @@
+export const TEST_CONSTANT = Object.freeze({
+  CATEGORY: '한식',
+  NAME: '테스트 식당',
+  DISTANCE: '5분 내',
+  DESCRIPTION: '테스트 식당에 대한 설명 입니다.',
+  LINK: 'https://test.com',
+});
---
+++ cypress/e2e/constants/visitUrl.js
@@ -0,0 +1,3 @@
+export const VISIT_URL_CONSTANT = Object.freeze({
+  LINK: 'http://localhost:5173/',
+});
