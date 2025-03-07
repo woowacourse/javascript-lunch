febea8d57f50a60b80137a547acf096308381b32
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-07 15:44:15 +0900

    🐛 fix: 파일 경로 수정
    Co-authored-by: Db0111 <Db0111@users.noreply.github.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
     return `
     <h1 class="gnb__title text-title">점심 뭐 먹지</h1>
       <button type="button" class="gnb__button" aria-label="음식점 추가">
-        <img src="../templates/add-button.png" alt="음식점 추가" />
+        <img src="../../templates/add-button.png" alt="음식점 추가" />
       </button>`;
   }
 
