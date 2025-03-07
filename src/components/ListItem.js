7826b8e6243f240d8ceb869d09d4d726dc3ccb79
hoyyChoi <hoyeon8165@gmail.com>
2025-03-07 13:53:09 +0900

    feat: ListItem 이미지 동적 할당

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,18 @@
+const CATRGORY_IMAGE_PATH = {
+  한식: "./public/category-korean.png",
+  중식: "./public/category-chinese.png",
+  일식: "./public/category-japanese.png",
+  양식: "./public/category-western.png",
+  아시안: "./public/category-asian.png",
+  기타: "./public/category-etc.png",
+};
+
 function ListItem({ category, name, distance, description }) {
   const listElement = document.createElement("li");
   listElement.classList.add("restaurant");
   listElement.innerHTML = `
     <div class="restaurant__category">
-      <img src="./public/category-korean.png" alt=${category} class="category-icon" />
+      <img src=${CATRGORY_IMAGE_PATH[category]} alt=${category} class="category-icon" />
     </div>
     <div class="restaurant__info">
       <h3 class="restaurant__name text-subtitle">${name}</h3>
