c5b29b9348f23050273eabcf51afb561139bd336
Jeongeun Lee <leeju1009@gmail.com>
2025-03-07 17:13:18 +0900

    fix: 사진이 배포 안되는 문제 수정

--- src/constants.js
+++ src/constants.js
@@ -58,10 +58,10 @@ export const ADD_BUTTON = {
 };
 
 export const IMAGE = new Map([
-  ['한식', '/category-korean.png'],
-  ['중식', '/category-chinese.png'],
-  ['일식', '/category-japanese.png'],
-  ['양식', '/category-western.png'],
-  ['아시안', '/category-asian.png'],
-  ['기타', '/category-etc.png'],
+  ['한식', 'category-korean.png'],
+  ['중식', 'category-chinese.png'],
+  ['일식', 'category-japanese.png'],
+  ['양식', 'category-western.png'],
+  ['아시안', 'category-asian.png'],
+  ['기타', 'category-etc.png'],
 ]);
