ec6fde83378fad57d827f8507896d15d3b767688
마 <126178440+shuyeon@users.noreply.github.com>
2025-03-07 18:21:35 +0900

    fix: state 내 이미지 경로 변경

--- src/state.js
+++ src/state.js
@@ -1,7 +1,7 @@
 const state = {
   restaurantList: [
     {
-      src: "./templates/category-korean.png",
+      src: "./category-korean.png",
       name: "피양콩할마니",
       distance: "10",
       description: `평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩
@@ -15,7 +15,7 @@ const state = {
       label: "한식",
     },
     {
-      src: "./templates/category-chinese.png",
+      src: "./category-chinese.png",
       name: "친친",
       distance: "5",
       description: `Since 2004 편리한 교통과 주차, 그리고 관록만큼 깊은 맛과
@@ -23,7 +23,7 @@ const state = {
       label: "중식",
     },
     {
-      src: "./templates/category-japanese.png",
+      src: "./category-japanese.png",
       name: "잇쇼우",
       distance: "10",
       description: `잇쇼우는 정통 자가제면 사누끼 우동이 대표메뉴입니다. 기술은
@@ -32,21 +32,21 @@ const state = {
       label: "일식",
     },
     {
-      src: "./templates/category-western.png",
+      src: "./category-western.png",
       name: "이태리키친",
       distance: "20",
       description: `늘 변화를 추구하는 이태리키친입니다.`,
       label: "양식",
     },
     {
-      src: "./templates/category-asian.png",
+      src: "./category-asian.png",
       name: "호야빈 삼성점",
       distance: "15",
       description: `푸짐한 양에 국물이 일품인 쌀국수`,
       label: "아시안",
     },
     {
-      src: "./templates/category-etc.png",
+      src: "./category-etc.png",
       name: "도스타코스 선릉점",
       distance: "5",
       description: `멕시칸 캐주얼 그릴`,
