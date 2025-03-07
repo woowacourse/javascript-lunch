bf18a1aa458148db83083a89210d5b3dcca1c7dc
hoyyChoi <hoyeon8165@gmail.com>
2025-03-07 15:04:41 +0900

    test: 헤더의 모달버튼 모달 활성화 E2E 테스트

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -12,4 +12,12 @@ describe("header 태그 E2E 테스트", () => {
   it("header 컴포넌트 인자가 화면에 표시 되었는지 확인한다.", () => {
     cy.get("header").should("contain", HEADER_CONTENTS.TITLE);
   });
+
+  it("header태그의 모달버튼을 누를시, 모달이 활성화된다.", () => {
+    cy.get("header button").click();
+
+    setTimeout(() => {
+      cy.get(".modal--open").should("exsit");
+    }, 1000);
+  });
 });
