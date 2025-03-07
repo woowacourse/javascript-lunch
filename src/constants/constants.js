325c4159ec9a42270ccdc204ef617589cd64742e
마 <126178440+shuyeon@users.noreply.github.com>
2025-03-07 15:50:21 +0900

    feat : 입력값 검증 함수 구현

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,3 +6,12 @@ export const CATEGORY_ICON = {
   아시안: "./templates/category-asian.png",
   기타: "./templates/category-etc.png",
 };
+
+export const RESTAURANT_NAME_LENGTH_MAX = 30;
+export const DESCRIPTION_LENGTH_MAX = 200;
+
+export const ERROR_MESSAGE = {
+  NAME_LENGTH_MAX: `가게 이름은 ${RESTAURANT_NAME_LENGTH_MAX}자를 넘을 수 없습니다.`,
+  DESCRIPTION_MAX: `설명은 ${DESCRIPTION_LENGTH_MAX}자를 넘을 수 없습니다.`,
+  LINK: "유효하지 않은 링크입니다.",
+};
