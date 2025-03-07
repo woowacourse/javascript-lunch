55eebd9749324d731ea6456ab9076deb110c13d5
H0ngJu <hongju4385@gmail.com>
2025-03-07 15:08:47 +0900

    style: 거리 텍스트 수정
    Co-authored-by: Claire <eunsoA@users.noreply.github.com>

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -3,7 +3,7 @@ import CATEGORY from '../constant/category.js';
 const RESTAURANT_INFO_TEMPLATE = (name, distance, description) => {
   return `
     <h3 class="restaurant__name text-subtitle">${name}</h3>
-    <span class="restaurant__distance text-body">캠퍼스부터 ${distance}</span>
+    <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
     <p class="restaurant__description text-body">${description}</p>
 `;
 };
