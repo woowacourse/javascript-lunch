3aaddad1641024fd03558b0beca84691c6f4687d
Jeongeun Lee <leeju1009@gmail.com>
2025-03-07 14:34:36 +0900

    test: 음식점 추가 모달을 연 후, 음식점 추가 중 취소하기 버튼을 클릭하여 음식점 추가를 취소하는 시나리오 테스트 추가

--- cypress/e2e/featureTest.cy.js
+++ cypress/e2e/featureTest.cy.js
@@ -28,4 +28,19 @@ describe('기능 테스트', () => {
     cy.get('.button--primary').click();
     cy.get('.restaurant-list').should('contain', '꺼벙이');
   });
+
+  it('사용자가 음식점 추가 모달을 연 후, 음식점 추가 중 취소하기 버튼을 클릭하여 음식점 추가를 취소하는 시나리오 테스트', () => {
+    cy.get('.gnb__button').click();
+    cy.get('.modal--open').should('exist');
+
+    cy.get('#category').select('한식', { force: true });
+    cy.get('#name').type('꺼벙이');
+    cy.get('#distance').select('15분 내', { force: true });
+    cy.get('#description').type('꺼벙이 분식');
+    cy.get('#link').type('https://naver.com');
+
+    cy.get('.button--secondary').click();
+    cy.get('.modal--open').should('not.exist');
+    cy.get('.restaurant-list').should('not.contain', '꺼벙이');
+  });
 });
