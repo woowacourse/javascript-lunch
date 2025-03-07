3a95c845584972822330d4b952c6550daba7714d
guesung <gueit214@naver.com>
2025-03-07 17:40:33 +0900

    refactor: 들여쓰기 수정

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,9 +4,9 @@ export default class Button extends Component {
   template() {
     return `
      <button
-      type=${this.props.type}
-      class="button ${this.props.class} text-caption"
-      id=${this.props.id}
+        type=${this.props.type}
+        class="button ${this.props.class} text-caption"
+        id=${this.props.id}
       >
       ${this.props.message}
     </button>`;
