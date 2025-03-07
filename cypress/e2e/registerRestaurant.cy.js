5b08ea22d29863edc8f8a088fb92be827308c1fd
yeongipark <yeongi0111@naver.com>
2025-03-07 14:40:40 +0900

    test: 음식점 등록시 필드값을 제대로 채우지 않으면 경고창이 뜸

--- cypress/e2e/registerRestaurant.cy.js
+++ cypress/e2e/registerRestaurant.cy.js
@@ -18,3 +18,48 @@ describe("정상적으로 음식점을 등록한다.", () => {
     cy.get("body").contains("더휴");
   });
 });
+
+describe("필드 값을 제대로 채우지 못하면 경고 문구가 발생한다.", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+    cy.viewport(1280, 720);
+  });
+
+  it("카테고리를 입력하지 않고 제출하기 버튼을 누르면 경고 문구가 발생한다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get(".button--primary").click();
+    cy.get("#category").then(($select) => {
+      expect($select[0].checkValidity()).to.be.false;
+    });
+  });
+
+  it("이름을 입력하지 않고 제출하기 버튼을 누르면 경고 문구가 발생한다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get("#category").select("한식");
+    cy.get(".button--primary").click();
+    cy.get("#name").then(($input) => {
+      expect($input[0].checkValidity()).to.be.false;
+    });
+  });
+
+  it("거리를 입력하지 않고 제출하기 버튼을 누르면 경고 문구가 발생한다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get("#category").select("한식");
+    cy.get("#name").type("더휴");
+    cy.get(".button--primary").click();
+    cy.get("#distance").then(($select) => {
+      expect($select[0].checkValidity()).to.be.false;
+    });
+  });
+
+  it("두 글자 이하를 입력했을 때 alert 창으로 경고 문구가 발생한다.", () => {
+    cy.get(".gnb__button").click();
+    cy.get("#category").select("한식");
+    cy.get("#name").type("더");
+    cy.get("#distance").select("5분 내");
+    cy.get(".button--primary").click();
+    cy.on("window:alert", (text) => {
+      expect(text).to.equal("식당 이름은 2글자 이상 입력해 주세요");
+    });
+  });
+});
