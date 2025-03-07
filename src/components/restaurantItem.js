f206e8342c29fcb36a3e53d8d6cf7c37e15499a9
mlnwns <mlnwns@naver.com>
2025-03-07 17:10:28 +0900

    fix: 이미지 경로 주소 변경

--- src/components/restaurantItem.js
+++ src/components/restaurantItem.js
@@ -1,18 +1,29 @@
 import text from "./@common/text";
-
+import koreanIcon from "../../public/category-korean.png";
+import chineseIcon from "../../public/category-chinese.png";
+import japaneseIcon from "../../public/category-japanese.png";
+import westernIcon from "../../public/category-western.png";
+import asianIcon from "../../public/category-asian.png";
+import etcIcon from "../../public/category-etc.png";
+const categoryIcons = {
+  korean: koreanIcon,
+  chinese: chineseIcon,
+  japanese: japaneseIcon,
+  western: westernIcon,
+  asian: asianIcon,
+  etc: etcIcon,
+};
 const restaurantItem = (props) => {
   const { category, categoryAlt, title, distance, description } = props;
-
   return `
     <li class="restaurant">
       <div class="restaurant__category">
         <img
-          src="/category-${category}.png"
+          src="${categoryIcons[category]}"
           alt="${categoryAlt}"
           class="category-icon"
         />
       </div>
-      
       <div class="restaurant__info">
         ${text({ tag: "h3", size: "large", children: title })}
         ${text({
@@ -28,5 +39,4 @@ const restaurantItem = (props) => {
     </li>
   `;
 };
-
 export default restaurantItem;
