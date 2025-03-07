6f1ef785e054f03c4bc23cd35f01838fe9b2820e
Cron <hojoong.choi@woowahan.com>
2025-03-07 11:24:02 +0900

    feat: 레스토랑 아이템 컴포넌트

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export function createHeader({ title }) {
+  const header = document.createElement("header");
+
+  header.innerHTML = /*html*/ `
+    <h1 class="gnb__title text-title">${title}</h1>
+    <button type="button" class="gnb__button" aria-label="음식점 추가">
+      <img src="./add-button.png" alt="음식점 추가" />
+    </button>
+  `;
+
+  return header;
+}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
+import { createHeader } from "./components/Header";
+import { createRestaurantItem } from "./components/RestaurantItem";
+
 addEventListener("load", () => {
   const body = document.querySelector("body");
-  const header = createHeader({ title: "점심 뭐 먹지" });
+  const restaurantList = document.querySelector(".restaurant-list");
 
+  const header = createHeader({ title: "점심 뭐 먹지" });
   header.classList.add("gnb");
   body.prepend(header);
+
+  //////// 다음 컴포넌트
+  const restaurantItem = createRestaurantItem();
+  restaurantList.append(restaurantItem);
 });
-
-function createHeader({ title }) {
-  const header = document.createElement("header");
-
-  header.innerHTML = /*html*/ `
-    <h1 class="gnb__title text-title">${title}</h1>
-    <button type="button" class="gnb__button" aria-label="음식점 추가">
-      <img src="./add-button.png" alt="음식점 추가" />
-    </button>
-  `;
-
-  return header;
-}
