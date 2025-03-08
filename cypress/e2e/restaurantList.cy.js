824b17278893b4b72723b9a1e7efce402802fe6e
deokbin <deokbin@naver.com>
2025-03-08 16:37:49 +0900

    ✅ test: 테스트 명세 구체화

--- cypress/e2e/restaurantList.cy.js
+++ cypress/e2e/restaurantList.cy.js
@@ -4,10 +4,10 @@ describe("레스트랑 목록 컴포넌트 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/");
   });
-  it("레스토랑이 랜더링 된다.", () => {
+  it("레스토랑 아이템을 확인할 수 있다.", () => {
     cy.get(".restaurant__category").should("exist");
   });
-  it("레스토랑 갯수만큼 랜더링 된다.", () => {
+  it("화면에서 레스토랑 데이터 갯수만큼 확인할 수 있다.", () => {
     const length = RestaurantData.length;
     cy.get(".restaurant__category").should("have.length", length);
   });
