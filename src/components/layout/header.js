280120f239c059c982f3da9d6ef09f36e08728c6
yeji0214 <1225yj50@naver.com>
2025-03-08 23:33:53 +0900

    refactor: createElement() 방식으로 통일

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,10 +2,22 @@ const $header = ({ title, buttonTitle, buttonImage }) => {
   const header = document.createElement("header");
   header.classList.add("gnb");
 
-  header.innerHTML += `<h1 class="gnb__title text-title">${title}</h1>`;
-  header.innerHTML += `<button type="button" class="gnb__button" aria-label="${buttonTitle}">
-      <img src="${buttonImage}" alt="${buttonTitle}">
-    </button>`;
+  const headerTitle = document.createElement("h1");
+  headerTitle.classList.add("gnb__title", "text-title");
+  headerTitle.innerText = title;
+  header.appendChild(headerTitle);
+
+  const button = document.createElement("button");
+  button.type = "button";
+  button.classList.add("gnb__button");
+  button.setAttribute("aria-label", buttonTitle);
+
+  const img = document.createElement("img");
+  img.src = buttonImage;
+  img.alt = buttonTitle;
+
+  button.appendChild(img);
+  header.appendChild(button);
 
   return header;
 };
--- src/components/restaurant/restaurant-item.js
+++ src/components/restaurant/restaurant-item.js
@@ -2,14 +2,38 @@ const $restaurantItem = ({ categoryIcon, categoryTitle, name, distance, descript
   const restaurantItem = document.createElement("li");
   restaurantItem.classList.add("restaurant");
 
-  restaurantItem.innerHTML += `<div class="restaurant__category">
-            <img src="${categoryIcon}" alt="${categoryTitle}" class="category-icon">
-          </div>`;
-  restaurantItem.innerHTML += `<div class="restaurant__info">
-            <h3 class="restaurant__name text-subtitle">${name}</h3>
-            <span class="restaurant__distance text-body">${distance}</span>
-            <p class="restaurant__description text-body">${description}</p>
-          </div>`;
+  // 아이콘
+  const category = document.createElement("div");
+  category.classList.add("restaurant__category");
+
+  const categoryImg = document.createElement("img");
+  categoryImg.src = categoryIcon;
+  categoryImg.alt = categoryTitle;
+  categoryImg.classList.add("category");
+
+  category.appendChild(categoryImg);
+  restaurantItem.appendChild(category);
+
+  // 내용
+  const info = document.createElement("div");
+  info.classList.add("restaurant__info");
+
+  const restaurantName = document.createElement("h3");
+  restaurantName.classList.add("restaurant__name", "text-subtitle");
+  restaurantName.innerText = name;
+  info.appendChild(restaurantName);
+
+  const restaurantDistance = document.createElement("span");
+  restaurantDistance.classList.add("restaurant__distance", "text-body");
+  restaurantDistance.innerText = distance;
+  info.appendChild(restaurantDistance);
+
+  const restaurantDescription = document.createElement("p");
+  restaurantDescription.classList.add("restaurant__description", "text-body");
+  restaurantDescription.innerText = description;
+  info.appendChild(restaurantDescription);
+
+  restaurantItem.appendChild(info);
 
   return restaurantItem;
 };
