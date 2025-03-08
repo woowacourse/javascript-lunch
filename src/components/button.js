d17ac0b184f72e3c40340f967290a4258ce05f4c
jin123457 <jin_ox@naver.com>
2025-03-08 22:06:37 +0900

    refactor: buttonInfo.className가 없을 경우 방어 코드 추가

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,7 @@ const $button = (buttonInfo) => {
   const button = document.createElement("button");
   button.textContent = buttonInfo.text;
   button.type = buttonInfo.type;
-  button.classList.add(...buttonInfo.className);
+  button.classList.add(...(buttonInfo?.className || []));
 
   if (buttonInfo.event) {
     button.addEventListener("click", buttonInfo.event);
