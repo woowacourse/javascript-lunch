aa25099b85295fecd76815bbfae001dbca5e8025
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-10 04:06:00 +0900

    chore: 중복되는 로직 담긴 파일 제거 및 통합

--- src/InitRestaurantList.js
+++ src/InitRestaurantList.js
@@ -1,8 +1,9 @@
-import RestaurantList from "./components/RestaurantList";
+import { createRestaurantItem } from "./components/createRestaurantItem";
+import { DEFAULT_RESTAURANTS } from "./constants/options";
 
 export default function InitRestaurantList() {
-  const $restaurantListContainer = document.querySelector(
-    ".restaurant-list-container"
+  const $restaurantList = document.querySelector(".restaurant-list");
+  DEFAULT_RESTAURANTS.map((restaurant) =>
+    createRestaurantItem($restaurantList, restaurant)
   );
-  RestaurantList($restaurantListContainer);
 }
