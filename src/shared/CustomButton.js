92aa6b67beca1efafdb4200ded07c54c3c840780
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-10 03:12:52 +0900

    refactor: 더 범용적으로 쓰일 수 있도록 리팩토링

--- src/shared/CustomButton.js
+++ src/shared/CustomButton.js
@@ -1,14 +1,9 @@
-export default function CustomButton({
-  id = "",
-  type = "",
-  className = "",
-  text = "",
-}) {
+export default function CustomButton({ id, type, className, text }) {
   return `
     <button 
       ${id && `id="${id}"`}
-      type="${type}" 
-      class="button ${className} text-caption"
+      ${type && `type="${type}"`}
+      ${className && `class="button ${className} text-caption"`} 
     >
       ${text}
     </button>
