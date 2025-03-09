8c8b5348f09f94bd83fcd5c1b1830c31c2bc8005
aydenote <dslgpgh@gmail.com>
2025-03-09 15:08:02 +0900

    refactor: input 태그에 링크 타입 입력만 가능하도록 수정

--- src/controllers/ModalController.js
+++ src/controllers/ModalController.js
@@ -15,7 +15,7 @@ const MODAL_FORM = [
   { label: "설명", formComponent: () => TextareaForm("description"), notice: "메뉴 등 추가 정보를 입력해 주세요." },
   {
     label: "참고 링크",
-    formComponent: () => InputForm("text", "link"),
+    formComponent: () => InputForm("url", "link"),
     notice: "매장 정보를 확인할 수 있는 링크를 입력해 주세요.",
   },
 ];
