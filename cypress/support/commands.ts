60eebe897019cee556d39aa7a68ede392a712089
guesung <gueit214@naver.com>
2025-03-09 12:14:15 +0900

    feat: e2e 커맨드 타입 지원

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,45 +1,15 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      addRestaurant(name: string, category: string, distance: number): Chainable;
+    }
+  }
+}
 
-Cypress.Commands.add('addRestaurant', (name, category, distance) => {
+Cypress.Commands.add('addRestaurant', (name: string, category: string, distance: number) => {
   cy.get('.gnb__button').click();
   cy.get('#category').select(category);
   cy.get('#name').type(name);
-  cy.get('#distance').select(distance);
+  cy.get('#distance').select(String(distance));
   cy.get('#modal-add').click();
 });
