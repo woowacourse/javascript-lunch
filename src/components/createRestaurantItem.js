7e8bf38b4f9bd6f6ad2aafda269c57deb2704d82
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-10 04:06:29 +0900

    chore: 중복되는 함수 제거 및 수정

--- src/components/createRestaurantItem.js
+++ src/components/createRestaurantItem.js
@@ -2,7 +2,7 @@ export function createRestaurantItem(container, inputValue) {
   container.innerHTML += `
     <li class="restaurant">
       <div class="restaurant__category">
-        <img src="./category-${inputValue.categoryCode}.png" alt="${inputValue.categoryValue}" class="category-icon">
+        <img src="./category-${inputValue.category}.png" alt="${inputValue.categoryValue}" class="category-icon">
       </div>
       <div class="restaurant__info">
         <h3 class="restaurant__name text-subtitle">${inputValue.nameValue}</h3>
@@ -11,19 +11,4 @@ export function createRestaurantItem(container, inputValue) {
       </div>
     </li>
   `;
-}
-
-export function createDefaultRestaurantItem(inputValue) {
-  return `
-  <li class="restaurant">
-    <div class="restaurant__category">
-      <img src="./category-${inputValue.category}.png" alt="${inputValue.categoryValue}" class="category-icon">
-    </div>
-    <div class="restaurant__info">
-      <h3 class="restaurant__name text-subtitle">${inputValue.nameValue}</h3>
-      <span class="restaurant__distance text-body">캠퍼스부터 ${inputValue.distanceValue}</span>
-      <p class="restaurant__description text-body">${inputValue.descriptionValue}</p>
-    </div>
-  </li>
-`;
 }
