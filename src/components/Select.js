3398a6d20027cd6b2f2a3f4afd30fbdf45bbf228
youjeong Jeong <wjddbwjd1205@naver.com>
2025-03-10 00:15:21 +0900

    fix: label 태그의 for 속성을 올바르게 수정하여 입력 필드와 연결
    - label 태그의 for 속성이 올바르게 설정되지 않아 select, input, textarea와 연결되지 않는 문제 수정
    - 불필요하게 포함된 'text-caption' 문자열 제거
    - 스타일 적용을 위해 class='text-caption'을 추가

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,11 +1,11 @@
 function createSelect(fieldName) {
   const select = `<div class="form-item form-item--required">
-    <label for="${fieldName.name} text-caption">${fieldName.label}</label>
+    <label for="${fieldName.name}" class="text-caption">${fieldName.label}</label>
     <select name="${fieldName.name}" id="${fieldName.name}" required>
       <option value="">선택해 주세요</option>
       ${Array.from(fieldName.lists.values()).map((list) => {
-        return `<option value="${list}">${list}</option>`;
-      })}
+    return `<option value="${list}">${list}</option>`;
+  })}
     </select>
   </div>`;
 
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,6 +1,6 @@
 function createTextarea(fieldName) {
   const textarea = `<div class="form-item">
-      <label for="${fieldName.name} text-caption">${fieldName.label}</label>
+      <label for="${fieldName.name}" class="text-caption">${fieldName.label}</label>
       <textarea name="${fieldName.name}" id="${fieldName.name}" cols="30" rows="5"></textarea>
       <span class="help-text text-caption">${fieldName.helpText}</span>
     </div>`;
