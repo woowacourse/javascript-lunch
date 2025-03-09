e48d92a7024a6e2bd5986023ba01c0192d594ec7
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-10 01:12:33 +0900

    chore: import 경로 수정

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import InitHeader from "./initHeader.js";
+import InitHeader from "./InitHeader.js";
 import InitRestaurantList from "./InitRestaurantList.js";
 import InitModalHandler from "./InitModalHandler.js";
 
