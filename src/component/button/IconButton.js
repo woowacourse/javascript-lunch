c7069cf7c8351d41b365e5c1921cde7b13f81ee9
hanheel <hanhee630630@gmail.com>
2025-03-10 11:45:32 +0900

    [style] : iconButton의 css를 동적으로 수정

--- src/component/button/IconButton.js
+++ src/component/button/IconButton.js
@@ -1,7 +1,13 @@
-export function IconButton({ name, imgSrc, label, onClick = () => {} }) {
+export function IconButton({
+  cssType = "primary",
+  name,
+  imgSrc,
+  label,
+  onClick = () => {},
+}) {
   const container = document.createElement("div");
   container.innerHTML = `
-    <button type="button" class="gnb__button" aria-label="${label}" name=${name}>
+    <button type="button" class="icon-button--${cssType}" aria-label="${label}" name=${name}>
     <img src="${imgSrc}" alt="${label}" /></button
   >
   `;
