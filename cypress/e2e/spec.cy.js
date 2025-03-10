ff1421f581a14abbbb26e70e12cc16ea54e2c677
guesung <gueit214@naver.com>
2025-03-10 10:58:57 +0900

    refactor: beforeEach에서 페이지 접속 로직 처리

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,28 +1,30 @@
-describe('식당 목록', () => {
-  it('첫 화면에서 식당 리스트가 보인다.', () => {
-    cy.visit('http://localhost:5173');
-    cy.get('.restaurant-list');
-  });
-});
-
-describe('식당 추가', () => {
+describe('E2E테스트', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173');
-
-    cy.get('.gnb__button').click();
+  });
+  describe('식당 목록', () => {
+    it('첫 화면에서 식당 리스트가 보인다.', () => {
+      cy.get('.restaurant-list');
+    });
   });
 
-  it('식당 추가 버튼을 누르면 모달이 보인다.', () => {
-    cy.contains('새로운 음식점');
-  });
+  describe('식당 추가', () => {
+    beforeEach(() => {
+      cy.get('.gnb__button').click();
+    });
 
-  it('모달을 열어 각 값을 입력하여 ‘추가하기’ 버튼을 누르면 식당 리스트가 추가된다.', () => {
-    cy.get('#category').select('일식');
-    cy.get('#name').type('이름입니다');
-    cy.get('#distance').select('10');
-    cy.get('#modal-add').click();
+    it('식당 추가 버튼을 누르면 모달이 보인다.', () => {
+      cy.contains('새로운 음식점');
+    });
 
-    cy.contains('이름입니다');
-    cy.contains('10분 내');
+    it('모달을 열어 각 값을 입력하여 ‘추가하기’ 버튼을 누르면 식당 리스트가 추가된다.', () => {
+      cy.get('#category').select('일식');
+      cy.get('#name').type('이름입니다');
+      cy.get('#distance').select('10');
+      cy.get('#modal-add').click();
+
+      cy.contains('이름입니다');
+      cy.contains('10분 내');
+    });
   });
 });
