8becbde0d715f0b148093fe92ba06c354baa6ff0
jin123457 <jin_ox@naver.com>
2025-03-10 10:01:40 +0900

    refactor: header 및 restaurant-item 컴포넌트에서 innerHTML 사용 제거 및 DOM 조작 방식으로 변경

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,22 @@ const $header = (headerInfo) => {
   const header = document.createElement("header");
   header.classList.add("gnb");
 
-  header.innerHTML += `<h1 class="gnb__title text-title">${headerInfo.title}</h1>`;
-  header.innerHTML += `<button type="button" class="gnb__button" aria-label="${headerInfo.buttonTitle}">
-      <img src="${headerInfo.buttonImage}" alt="${headerInfo.buttonTitle}">
-    </button>`;
+  const title = document.createElement("h1");
+  title.classList.add("gnb__title", "text-title");
+  title.textContent = headerInfo.title;
+
+  const modalButton = document.createElement("button");
+  modalButton.type = "button";
+  modalButton.classList.add("gnb__button");
+  modalButton.ariaLabel = headerInfo.buttonTitle;
+
+  const modalButtonImage = document.createElement("img");
+  modalButtonImage.src = headerInfo.buttonImage;
+  modalButtonImage.alt = headerInfo.buttonTitle;
+  modalButton.appendChild(modalButtonImage);
+
+  header.appendChild(title);
+  header.appendChild(modalButton);
 
   return header;
 };
--- src/components/restaurant-item.js
+++ src/components/restaurant-item.js
@@ -2,15 +2,35 @@ const $restaurantItem = (restaurantInfo) => {
   const restaurantItem = document.createElement("li");
   restaurantItem.classList.add("restaurant");
 
-  restaurantItem.innerHTML += `<div class="restaurant__category">
-            <img src="${restaurantInfo.categoryIcon}" alt="${restaurantInfo.categoryTitle}" class="category-icon">
-          </div>`;
-  restaurantItem.innerHTML += `<div class="restaurant__info">
-            <h3 class="restaurant__name text-subtitle">${restaurantInfo.name}</h3>
-            <span class="restaurant__distance text-body">${restaurantInfo.distance}</span>
-            <p class="restaurant__description text-body">${restaurantInfo.description}</p>
-          </div>`;
+  const category = document.createElement("div");
+  category.classList.add("restaurant__category");
 
+  const categoryIcon = document.createElement("img");
+  categoryIcon.src = restaurantInfo.categoryIcon;
+  categoryIcon.alt = restaurantInfo.categoryTitle;
+  categoryIcon.classList.add("category-icon");
+  category.appendChild(categoryIcon);
+
+  const info = document.createElement("div");
+  info.classList.add("restaurant__info");
+
+  const name = document.createElement("h3");
+  name.classList.add("restaurant__name", "text-subtitle");
+  name.textContent = restaurantInfo.name;
+  info.appendChild(name);
+
+  const distance = document.createElement("span");
+  distance.classList.add("restaurant__distance", "text-body");
+  distance.textContent = restaurantInfo.distance;
+  info.appendChild(distance);
+
+  const description = document.createElement("p");
+  description.classList.add("restaurant__description", "text-body");
+  description.textContent = restaurantInfo.description;
+  info.appendChild(description);
+
+  restaurantItem.appendChild(category);
+  restaurantItem.appendChild(info);
   return restaurantItem;
 };
 
