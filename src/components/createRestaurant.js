b61b5ca9d7c71d25f2c9f95e5f6eb6b2c313acec
deokbin <deokbin@naver.com>
2025-03-10 12:08:06 +0900

    ✨ feat: 초기 로딩 시에만 레스토랑 리스트 생성하도록 수정

--- src/components/createRestaurant.js
+++ src/components/createRestaurant.js
@@ -2,11 +2,14 @@ import { RestaurantData } from "../constants/RestaurantData.js";
 import Restaurant from "./Restaurant/Restaurant.js";
 import "./restaurantList.css";
 const createRestaurant = () => {
-  RestaurantData.forEach((data) => {
-    const restaurantItem = document.createElement("li");
-    restaurantItem.classList.add("restaurant");
-    document.querySelector(".restaurant-list").appendChild(restaurantItem);
-    new Restaurant(restaurantItem, data);
-  });
+  const restaurantList = document.querySelector(".restaurant-list");
+  if (restaurantList.childElementCount === 0) {
+    RestaurantData.forEach((data) => {
+      const restaurantItem = document.createElement("li");
+      restaurantItem.classList.add("restaurant");
+      new Restaurant(restaurantItem, data);
+      restaurantList.appendChild(restaurantItem);
+    });
+  }
 };
 export default createRestaurant;
