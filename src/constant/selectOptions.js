225e05713ebd6ee8840ace24efc99f3aa188c212
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-10 15:15:41 +0900

    fix: constant 수정 못하게 freeze

--- src/constant/selectOptions.js
+++ src/constant/selectOptions.js
@@ -1,4 +1,6 @@
-const CATEGORY_OPTIONS = [
+import { deepFreeze } from '../util/deepFreeze.js';
+
+const CATEGORY_OPTIONS = deepFreeze([
   { value: '', option: '선택해 주세요' },
   { value: 'korean', option: '한식' },
   { value: 'chinese', option: '중식' },
@@ -6,15 +8,15 @@ const CATEGORY_OPTIONS = [
   { value: 'western', option: '양식' },
   { value: 'asian', option: '아시안' },
   { value: 'etc', option: '기타' },
-];
+]);
 
-const DISTANCE_OPTIONS = [
+const DISTANCE_OPTIONS = deepFreeze([
   { value: '', option: '선택해 주세요' },
   { value: '5', option: '5분 내' },
   { value: '10', option: '10분 내' },
   { value: '15', option: '15분 내' },
   { value: '20', option: '20분 내' },
   { value: '30', option: '30분 내' },
-];
+]);
 
 export { CATEGORY_OPTIONS, DISTANCE_OPTIONS };
