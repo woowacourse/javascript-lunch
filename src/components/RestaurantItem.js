52ead1f1a54f15f9782914e3a5e719ea75afacf5
ahhyun <wndkgus0225@naver.com>
2025-03-11 00:58:24 +0900

    refactor: RestaurantItem.js에서 innerHTML 일부를 textContent로 변경

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,27 +1,29 @@
 import { IMAGE_SRC_BY_RESTAURANTS_CATEGORY } from "../constants/constants";
 
-const createRestaurantItem = ({
-  category,
-  name,
-  distance,
-  description,
-  imgSrc,
-}) => {
+const createRestaurantItem = ({ category, name, distance, description }) => {
   const li = document.createElement("li");
   li.classList.add("restaurant");
 
-  const mappedImage = IMAGE_SRC_BY_RESTAURANTS_CATEGORY[category];
+  const mappedImage =
+    IMAGE_SRC_BY_RESTAURANTS_CATEGORY[category] || "images/default.png";
 
   li.innerHTML = `
       <div class="restaurant__category">
-        <img src="${mappedImage}" alt="${category}" class="category-icon" />
+        <img class="category-icon" />
       </div>
       <div class="restaurant__info">
-        <h3 class="restaurant__name text-subtitle">${name}</h3>
-        <span class="restaurant__distance text-body">${distance}</span>
-        <p class="restaurant__description text-body">${description}</p>
+        <h3 class="restaurant__name text-subtitle"></h3>
+        <span class="restaurant__distance text-body"></span>
+        <p class="restaurant__description text-body"></p>
       </div>
     `;
+
+  li.querySelector(".category-icon").src = mappedImage;
+  li.querySelector(".category-icon").alt = category;
+  li.querySelector(".restaurant__name").textContent = name;
+  li.querySelector(".restaurant__distance").textContent = distance;
+  li.querySelector(".restaurant__description").textContent = description;
+
   return li;
 };
 
