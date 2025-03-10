13cb0a6846828427df2dcb4283359823b6992295
H0ngJu <hongju4385@gmail.com>
2025-03-10 14:03:14 +0900

    refactor(inputText): 이름, 링크, 참조 링크 생성을 createInputText 하나로 관리하도록 수정

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,60 +1,61 @@
 import { convertStringToElement } from '../utils/convertStringToElement.js';
 
+const DESCRIPTION_PROPS = Object.freeze({
+  COLS: 30,
+  ROWS: 5,
+});
+
 const LABEL_TEMPLATE = (type, title) => {
   return `<label for="${type} text-caption">${title}</label>`;
 };
-const INPUT_TEMPLATE = (tag) => {
-  return `<input type="text" name="${tag}" id="${tag}" required />`;
+const INPUT_TEMPLATE = (tag, required) => {
+  return `<input type="text" name="${tag}" id="${tag}"  ${required ? 'required' : ''} />`;
 };
 const TEXTAREA_TEMPLATE = () => {
-  return `<textarea name="description" id="description" cols="30" rows="5"></textarea>
+  return `<textarea name="description" id="description" cols="${DESCRIPTION_PROPS.COLS}" rows="${DESCRIPTION_PROPS.ROWS}"></textarea>
     `;
 };
 const SPAN_TEMPLATE = (text) => {
   return `<span class="help-text text-caption">${text}</span>`;
 };
 
+const FEILD_INFO = Object.freeze({
+  이름: { type: 'name', title: '이름', required: true },
+  설명: {
+    type: 'description',
+    title: '설명',
+    isTextArea: true,
+    spanText: '매장 정보를 확인할 수 있는 링크를 입력해 주세요.',
+  },
+  '참조 링크': {
+    type: 'link',
+    title: '참조 링크',
+    required: false,
+    spanText: '매장 정보를 확인할 수 있는 링크를 입력해 주세요.',
+  },
+});
+
 class InputText {
   #input;
   #inputContainer;
 
   constructor(title) {
-    this.#inputContainer = this.#createInputText(title);
+    this.#inputContainer = this.#createInputText(FEILD_INFO[title]);
     return this;
   }
-  #createInputText = (title) => {
-    if (title === '이름') return this.#handleName(title);
-    if (title === '설명') return this.#handleDescription(title);
-    if (title === '참조 링크') return this.#handleLink(title);
-  };
 
-  #handleName = () => {
+  #createInputText = ({ type, title, required, isTextArea = false, spanText }) => {
     const formItem = document.createElement('div');
     formItem.classList.add('form-item');
-    formItem.classList.add('form-item--required');
-    formItem.innerHTML = `${LABEL_TEMPLATE('name', '이름')}${INPUT_TEMPLATE('name')}`;
+    if (required) formItem.classList.add('form-item--required');
 
-    this.#input = formItem.querySelector('#name');
-    return formItem;
-  };
+    const label = LABEL_TEMPLATE(type, title);
+    const input = isTextArea ? TEXTAREA_TEMPLATE() : INPUT_TEMPLATE(type, required);
+    const span = spanText ? SPAN_TEMPLATE(spanText) : '';
 
-  #handleDescription = () => {
-    const formItem = document.createElement('div');
-    formItem.classList.add('form-item');
-    formItem.innerHTML = `${LABEL_TEMPLATE('description', '설명')}${TEXTAREA_TEMPLATE()}${SPAN_TEMPLATE(
-      '메뉴 등 추가 정보를 입력해 주세요.',
-    )}`;
-    this.#input = formItem.querySelector('#description');
-    return formItem;
-  };
+    formItem.innerHTML = `${label}${input}${span}`;
+    this.#input = formItem.querySelector(`#${type}`);
 
-  #handleLink = () => {
-    const formItem = document.createElement('div');
-    formItem.classList.add('form-item');
-    formItem.innerHTML = `${LABEL_TEMPLATE('link', '참고 링크')}${INPUT_TEMPLATE('link')}${SPAN_TEMPLATE(
-      '매장 정보를 확인할 수 있는 링크를 입력해 주세요.',
-    )}`;
-    this.#input = formItem.querySelector('#link');
     return formItem;
   };
 
