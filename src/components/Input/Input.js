19fd5311c74c1c2e90a5ecaef6444eb60c9a6340
deokbin <deokbin@naver.com>
2025-03-10 11:40:03 +0900

    📝 chore: 불필요한 콘솔 출력 제거

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,6 @@
 import { label } from "../../constants/optionValue.js";
 
 const Input = ({ id, required, type }) => {
-  console.log({ id, required, type });
   return `
   <div id="name" class="form-item ${required ? "form-item--required" : ""}">
       <label for="${id} text-caption">${label[id]}</label>
