4df3ce52e41cefed4fbbe3152c0742c09768e528
Oliy <eunsoa0310@gmail.com>
2025-03-10 15:12:34 +0900

    refactor(Button): innerHTML에서 createElement로 변경
    - XSS 취약점 방지를 위해 innerHTML 제거
    - DOM 조작 방식을 createElement와 appendChild로 변경
    - 리뷰어 리뷰 반영 (https://github.com/woowacourse/javascript-lunch/pull/209#discussion_r1986038631)
    - createButton 오타 수정

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,14 @@
-import { convertStringToElement } from '../utils/convertStringToElement.js';
-
-const BUTTON_TEMPLATE = (className, buttonText) => {
-  return `<button type="button" class="button ${className} text-caption">${buttonText}</button>`;
-};
-
 class Button {
   constructor(className, buttonText) {
-    return this.#crerateButton(className, buttonText);
+    return this.#createButton(className, buttonText);
   }
 
-  #crerateButton = (className, buttonText) => {
-    return convertStringToElement(BUTTON_TEMPLATE(className, buttonText));
+  #createButton = (className, buttonText) => {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.classList.add('button', className);
+    button.textContent = buttonText;
+    return button;
   };
 }
 
