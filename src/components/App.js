39c41ccc5b92b0ebc7acfb666e30b214d1b89ce0
deokbin <deokbin@naver.com>
2025-03-10 12:08:47 +0900

    ✨ feat: 매번 전체 재렌더링 하는 방식에서 데이터가 바뀌면 추가된 데이터에 대해서만 추가 렌더링 하도록 변경

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import Header from "./Header/Header.js";
 import createRestaurant from "./createRestaurant.js";
 import Component from "./Component.js";
 import Modal from "./Modal/Modal.js";
+import Restaurant from "./Restaurant/Restaurant.js";
+import { RestaurantData } from "../constants/RestaurantData.js";
 class App extends Component {
   constructor($target) {
     super($target);
     this.state = this.setUp();
-    document.addEventListener("restaurantUpdated", () => this.render());
+    document.addEventListener("restaurantUpdated", this.addNewRestaurant);
   }
 
   setUp() {
@@ -23,12 +25,22 @@ class App extends Component {
       toggleModal: () => this.toggleModal(),
     });
 
-    document.querySelector(".restaurant-list").innerHTML = "";
     createRestaurant();
   }
 
   toggleModal() {
     this.setState({ isModalOpen: !this.state.isModalOpen });
+  }
+
+  addNewRestaurant() {
+    const restaurantList = document.querySelector(".restaurant-list");
+    const newRestaurant = RestaurantData[RestaurantData.length - 1];
+    if (newRestaurant) {
+      const restaurantItem = document.createElement("li");
+      restaurantItem.classList.add("restaurant");
+      new Restaurant(restaurantItem, newRestaurant);
+      restaurantList.appendChild(restaurantItem);
+    }
   }
 }
 export default App;
