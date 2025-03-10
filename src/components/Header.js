92780bc49ae72e824976a22d96e541fdcc0b59cb
ahhyun <wndkgus0225@naver.com>
2025-03-10 20:34:28 +0900

    refactor: header에서 innerHTML 대신 insertAdjacentHTML 사용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 const createHeader = ({ title }) => {
   const header = document.createElement("header");
-  header.innerHTML = `
-      <h1 class="gnb__title text-title">${title}</h1>
-      <button type="button" class="gnb__button" aria-label="음식점 추가">
-        <img src="images/add-button.png" alt="음식점 추가" />
-      </button>`;
   header.classList.add("gnb");
+
+  header.insertAdjacentHTML(
+    "beforeend",
+    `<h1 class="gnb__title text-title">${title}</h1>
+    <button type="button" class="gnb__button" aria-label="음식점 추가">
+      <img src="images/add-button.png" alt="음식점 추가" />
+    </button>`
+  );
+
   return header;
 };
 
