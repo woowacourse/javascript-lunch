9621b4f524c8e9fb63e2b42bb99a8384b5857fec
spoyodevelop <kjy9505@gmail.com>
2025-03-10 16:22:46 +0900

    refactor: 동적으로 타입을 가져오게 변경

--- types/restaurantTypes.ts
+++ types/restaurantTypes.ts
@@ -1,7 +1,19 @@
+import { extractByKey } from "../src/utils/extract";
+import { FOOD_CATEGORY, RESTAURANT_DISTANCE } from "../src/settings/settings";
+
+const FoodCategory = extractByKey(FOOD_CATEGORY, "value") as readonly string[];
+const Distance = extractByKey(
+  RESTAURANT_DISTANCE,
+  "value"
+) as readonly string[];
+
+type FoodCategory = (typeof FoodCategory)[number];
+type Distance = (typeof Distance)[number];
+
 export interface RestaurantForm {
-  category: string;
+  category: FoodCategory;
   name: string;
-  distance: string;
+  distance: Distance;
   description: string;
   link: string;
 }
