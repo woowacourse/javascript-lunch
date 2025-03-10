54d1be65ce4eed42289dc79c901fb5c1541991e8
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-10 14:44:46 +0900

    refactor: modal 관련 로직 modalController로 분리

--- src/components/button/PlusButton.js
+++ src/components/button/PlusButton.js
@@ -1,10 +1,11 @@
 import createDOMElement from '../../util/createDomElement.js';
 
-function PlusButton() {
+function PlusButton({ ...attribute }) {
   return createDOMElement({
     tag: 'button',
     type: 'button',
     class: 'gnb__button',
+    ...attribute,
     'aria-label': '음식점 추가',
     children: [
       createDOMElement({
