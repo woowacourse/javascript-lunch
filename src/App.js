3d3032d32b11b0c938ab415b9e789f65c2f972e5
sooyeoniya <tndus1306403@naver.com>
2025-03-10 15:59:28 +0900

    refactor(App): DI 적용 및 App 구조 리팩토링
    - App 클래스 생성자에서 RestaurantStore와 RestaurantService를 직접 생성하지 않고, 의존성을 외부에서 주입받도록 수정
    - App의 인스턴스화 로직을 index.js로 이동
    - 초기 렌더링 로직을 render() 메서드로 명칭 변경 -> 렌더링 메서드 전체 통일
    - RestaurantForm 인스턴스를 별도로 분리하여 BottomSheetBase에 전달
    - App 에서 store와 service를 직접 관리하지 않음 -> App에서는 UI 로직만 담당

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import Header from "./components/header/Header.js";
-import RestaurantList from "./components/restaurant-list-section/restaurant-list/RestaurantList.js";
 import BottomSheetBase from "./components/common/bottom-sheet-base/BottomSheetBase.js";
 import RestaurantForm from "./components/restaurant-form-section/restaurant-form/RestaurantForm.js";
-import RestaurantStore from "./stores/RestaurantStore.js";
-import RestaurantService from "./services/RestaurantService.js";
+import RestaurantList from "./components/restaurant-list-section/restaurant-list/RestaurantList.js";
 
 export default class App {
-  constructor() {
-    this.restaurantStore = new RestaurantStore();
-    this.restaurantService = new RestaurantService(this.restaurantStore);
-    this.#initElement();
+  constructor(restaurantStore, restaurantService) {
+    this.restaurantStore = restaurantStore;
+    this.restaurantService = restaurantService;
+
+    this.render();
     this.restaurantStore.subscribe(() => this.#renderRestaurantList());
   }
 
-  #renderRestaurantList() {
-    const restaurantList = this.restaurantService.getRestaurants();
-    this.$restaurantList.update(restaurantList);
-  }
-
-  #initElement() {
+  render() {
     const $body = document.querySelector("body");
     $body.append(
       new Header({ onOpen: () => this.$bottomSheet.open() }).render()
@@ -31,15 +25,17 @@ export default class App {
       this.restaurantService.getRestaurants()
     );
 
+    this.$restaurantForm = new RestaurantForm({
+      onSubmit: (newRestaurantInfo) => {
+        this.restaurantService.addRestaurant(newRestaurantInfo);
+        this.$bottomSheet.close();
+      },
+      onCancel: () => this.$bottomSheet.close(),
+    });
+
     this.$bottomSheet = new BottomSheetBase({
       title: "새로운 음식점",
-      $children: new RestaurantForm({
-        onSubmit: (newRestaurantInfo) => {
-          this.restaurantService.addRestaurant(newRestaurantInfo);
-          this.$bottomSheet.close();
-        },
-        onCancel: () => this.$bottomSheet.close(),
-      }).render(),
+      $children: this.$restaurantForm.render(),
     });
 
     this.$main.append(
@@ -47,6 +43,9 @@ export default class App {
       this.$bottomSheet.render()
     );
   }
-}
 
-new App();
+  #renderRestaurantList() {
+    const restaurantList = this.restaurantService.getRestaurants();
+    this.$restaurantList.update(restaurantList);
+  }
+}
