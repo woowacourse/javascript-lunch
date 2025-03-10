b77a800da0998ad8a9af8bc8f8dc43e414af84cc
yeji0214 <1225yj50@naver.com>
2025-03-10 14:44:32 +0900

    refactor: textarea 컴포넌트 TypeScript로 마이그레이션

--- src/components/common/textarea.js
+++
@@ -1,9 +0,0 @@
-const $textarea = ( {attribute} ) => {
-  const textarea = document.createElement("textarea");
-
-  Object.assign(textarea, attribute);
-
-  return textarea;
-};
-
-export default $textarea;
