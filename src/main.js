14a4bd1227030e6befccfea765d0b1f2ab00bc85
ahhyun <wndkgus0225@naver.com>
2025-03-10 19:40:27 +0900

    refactor: main.js에서 Modal과 RestaurantForm을 컴포넌트로 분리

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import validateRestaurant from "./validateRestaurant.js";
-import { restaurantsData } from "./restaurantsData";
-import { ERROR_MESSAGE } from "./constants/constants.js";
+import Modal from "./components/Modal.js";
+import RestaurantForm from "./components/restaurantForm.js";
 import createHeader from "./components/Header.js";
 import createRestaurantItem from "./components/RestaurantItem.js";
+import { restaurantsData } from "./restaurantsData.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector("body");
@@ -10,65 +10,28 @@ document.addEventListener("DOMContentLoaded", () => {
   body.prepend(header);
 
   const restaurantList = document.querySelector(".restaurant-list");
-
   const addRestaurantModalButton = header.querySelector(".gnb__button");
   const addNewRestaurantModal = document.getElementById(
     "add-restaurant-dialog"
   );
   const closeModalButton = document.getElementById("cancel-dialog-btn");
-  const form = addNewRestaurantModal.querySelector("form");
+  const formElement = addNewRestaurantModal.querySelector("form");
+
+  const modal = new Modal(
+    addNewRestaurantModal,
+    addRestaurantModalButton,
+    closeModalButton
+  );
+
+  new RestaurantForm(formElement, restaurantList, modal);
 
   restaurantsData.forEach((restaurantData) => {
     const restaurantItem = createRestaurantItem(restaurantData);
     restaurantList.appendChild(restaurantItem);
   });
 
-  addRestaurantModalButton.addEventListener("click", () => {
-    addNewRestaurantModal.showModal();
-  });
-
-  addNewRestaurantModal.addEventListener("click", (event) => {
-    if (!event.target.closest(".modal-container")) {
-      addNewRestaurantModal.close();
-    }
-  });
-
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const nameInput = document.getElementById("name");
-    const descriptionInput = document.getElementById("description");
-    const categoryInput = document.getElementById("category");
-    const distanceInput = document.getElementById("distance");
-    const linkInput = document.getElementById("link");
-
-    const restaurantsNameList = restaurantsData.map((restaurant) => {
-      return restaurant.name;
-    });
-
-    const newRestaurant = {
-      category: categoryInput.value,
-      name: nameInput.value,
-      distance: `${distanceInput.value}분 내`,
-      description: descriptionInput.value,
-      link: linkInput.value,
-    };
-
-    const errorMessage = validateRestaurant(newRestaurant, restaurantsNameList);
-    if (errorMessage) {
-      alert(errorMessage);
-      return;
-    }
-
-    const restaurantItem = createRestaurantItem(newRestaurant);
-    restaurantList.appendChild(restaurantItem);
-
-    form.reset();
-    addNewRestaurantModal.close();
-  });
-
   closeModalButton.addEventListener("click", () => {
-    form.reset();
+    formElement.reset();
     addNewRestaurantModal.close();
   });
 });
