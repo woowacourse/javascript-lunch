9720bbd246fbca18eb835cd264e33da5cd5ab223
spoyodevelop <kjy9505@gmail.com>
2025-03-10 19:10:23 +0900

    feat: restaurantList를 적용

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import createInputBox from "./components/input/input.js";
 import createRestaurantForm from "./components/restaurant/form/form.js";
 import createRestaurantItem from "./components/restaurant/item/item.js";
 import createTextAreaBox from "./components/textarea/textarea.js";
+import { restaurantList } from "./model/RestaurantList.js";
 
 const app = document.querySelector("#app");
 const modalContainer = document.querySelector(".modal-container");
 const restaurantFrom = createRestaurantForm();
+const restaurantListElement = document.querySelector(".restaurant-list");
 modalContainer.appendChild(restaurantFrom);
+restaurantList.forEach((restaurantItem) =>
+  restaurantListElement.appendChild(createRestaurantItem(restaurantItem))
+);
 
 function handleBottomSheetToggle(event) {
   const modal = document.querySelector(".modal");
@@ -21,5 +26,11 @@ function handleBottomSheetToggle(event) {
     modal.close();
   }
 }
+// document.addEventListener("click", function (e) {
+//   if (e.target.classList.contains("favorite-icon")) {
+//     const parent = e.target.parentElement;
+//     const name = parent.querySelector(".restaurant__name").textContent;
+//   }
+// });
 
 document.body.addEventListener("click", handleBottomSheetToggle);
