f66e91c0583cd36d3540913b5359f4f48680f968
spoyodevelop <kjy9505@gmail.com>
2025-03-11 19:40:03 +0900

    chores: 쓸모없는 로직 삭제

--- cypress/e2e/localStorage/localStroage.cy.js
+++ cypress/e2e/localStorage/localStroage.cy.js
@@ -1,7 +1,4 @@
-import {
-  ERROR_MESSAGE,
-  INITIAL_RESTAURANT,
-} from "../../../src/settings/settings";
+import { ERROR_MESSAGE } from "../../../src/settings/settings";
 
 describe("로컬 스토리지가 잘 되는지 확인하는 시나리오", () => {
   before(() => {
