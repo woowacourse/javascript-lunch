0c5b6449947d31ef3d173053c924526fe6d9c8d1
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-11 17:05:04 +0900

    feat: 레스토랑 리스트 출력 로직 함수 구현

--- src/components/createRestaurantItem.js
+++ src/components/createRestaurantItem.js
@@ -1,13 +1,47 @@
-export function createRestaurantItem(container, inputValue) {
+import { GetAllRestaurants } from "../domain/RestaurantStorage";
+
+export function createRestaurantList(restaurants) {
+  const $restaurantListContainer = document.querySelector(
+    ".restaurant-list-container"
+  );
+
+  const restaurantList = document.createElement("ul");
+  restaurantList.className = "restaurant-list";
+
+  restaurants.forEach((restaurant) => {
+    createRestaurantItem(restaurantList, restaurant);
+  });
+
+  $restaurantListContainer.innerHTML = restaurantList.outerHTML;
+}
+
+function createRestaurantItem(container, inputValue) {
   container.innerHTML += `
     <li class="restaurant">
       <div class="restaurant__category">
-        <img src="./category-${inputValue.category}.png" alt="${inputValue.categoryValue}" class="category-icon">
+        <img src="./category-${inputValue.category}.png" alt="${
+    inputValue.categoryValue
+  }" class="category-icon">
       </div>
       <div class="restaurant__info">
-        <h3 class="restaurant__name text-subtitle">${inputValue.nameValue}</h3>
-        <span class="restaurant__distance text-body">캠퍼스부터 ${inputValue.distanceValue}</span>
-        <p class="restaurant__description text-body">${inputValue.descriptionValue}</p>
+        <div class="restaurant-info-header">
+          <div>
+            <h3 class="restaurant__name text-subtitle">${
+              inputValue.nameValue
+            }</h3>
+            <span class="restaurant__distance text-body">캠퍼스부터 ${
+              inputValue.distanceValue
+            }</span>
+          </div>
+          <img class="restaurant-favorite-star" src=${
+            inputValue.favorite === false
+              ? "/favorite-icon-lined.png"
+              : "/favorite-icon-filled.png"
+          } alt="favorite star"/>
+        </div>
+        <p class="restaurant__description text-body">${
+          inputValue.descriptionValue
+        }</p>
       </div>
     </li>
   `;
