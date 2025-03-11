4a5c9765f0f965aa65066aa5656fb9db995844d5
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-11 18:30:25 +0900

    feat: main.js에서 SaveFavoriteRestaurant 함수 적용

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import InitHeader from "./InitHeader.js";
 import InitRestaurantList from "./InitRestaurantList.js";
 import InitModalHandler from "./InitModalHandler.js";
+import SaveFavoriteRestaurant from "./domain/SaveFavoriteRestaurant.js";
 
 addEventListener("load", () => {
   InitHeader();
   InitRestaurantList();
   InitModalHandler();
+  SaveFavoriteRestaurant();
 });
