ad3eb4b5cd4a84cd6b035841107a13cecf861944
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-11 16:56:07 +0900

    refactor: App 컴포넌트 독립화 및 캡슐화

--- src/App.js
+++ src/App.js
@@ -1,63 +1,57 @@
 import Header from "./components/Header.js";
 import RestaurantList from "./components/RestaurantList.js";
-import { restaurants } from "./database/restaurants.js";
+import { restaurants } from "./domains/restaurants.ts";
 import AddRestaurantModal from "./components/modal/AddRestaurantModal/index.js";
-import Component from "./components/core/Component.js";
 import RestaurantItem from "./components/RestaurantItem.js";
 
-class App extends Component {
-  setup() {
-    this.state = {
-      restaurants: restaurants,
-    };
+class App {
+  #restaurants;
+  #$target;
+
+  constructor($target) {
+    this.#$target = $target;
+    this.#restaurants = restaurants;
+
+    this.#$target.insertAdjacentHTML("beforeend", this.#template());
+    this.#componentDidMount();
   }
 
-  updateRestaurant(newRestaurant) {
-    this.setState({
-      restaurants: [...this.state.restaurants, newRestaurant],
-    });
-
-    this.componentDidUpdate(newRestaurant);
-  }
-
-  template() {
+  #template() {
     return /*html*/ `
-        ${Header()}
-        <main></main>
-        <div id="modal"></div>
+      ${Header()}
+      <main></main>
+      <div id="modal"></div>
     `;
   }
 
-  componentDidUpdate(newRestaurant) {
+  #componentDidMount() {
+    const $gnbButton = this.#$target.querySelector(".gnb__button");
+    const $modal = new AddRestaurantModal(document.querySelector("#modal"), {
+      addRestaurant: this.#addRestaurant.bind(this),
+    });
+
+    $gnbButton.addEventListener("click", () => {
+      $modal.open();
+    });
+
+    this.#renderRestaurantList();
+  }
+
+  #renderRestaurantList() {
+    const $main = document.querySelector("main");
+
+    $main.insertAdjacentHTML("beforeend", RestaurantList(this.#restaurants));
+  }
+
+  #addRestaurant(newRestaurant) {
+    this.#restaurants.push(newRestaurant);
+
     const $restaurantList = document.querySelector("#restaurant-list");
     $restaurantList.insertAdjacentHTML(
       "afterbegin",
       RestaurantItem(newRestaurant)
     );
   }
-
-  componentDidMount() {
-    const $modal = new AddRestaurantModal(document.querySelector("#modal"), {
-      updateRestaurant: this.updateRestaurant.bind(this),
-    });
-    const $gnbButton = this.$target.querySelector(".gnb__button");
-
-    $gnbButton.addEventListener("click", () => {
-      $modal.open();
-    });
-
-    this.renderRestaurantList();
-  }
-
-  renderRestaurantList() {
-    const $main = document.querySelector("main");
-
-    $main.insertAdjacentHTML(
-      "afterbegin",
-      RestaurantList(this.state.restaurants)
-    );
-  }
 }
 
-const app = document.querySelector("#app");
-new App(app);
+export default App;
