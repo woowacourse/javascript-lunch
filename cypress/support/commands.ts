e553b66d82119a0ad3ea228eedb8707234f70cd0
guesung <gueit214@naver.com>
2025-03-11 15:31:58 +0900

    chore: Cypress 타입 에러 ts-ignore처리

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -6,6 +6,7 @@ declare global {
   }
 }
 
+/** @ts-ignore */
 Cypress.Commands.add('addRestaurant', (name: string, category: string, distance: number) => {
   cy.get('.gnb__button').click();
   cy.get('#category').select(category);
