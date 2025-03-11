f5442dcef72d8796ffb86424f74fe14755448831
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-11 17:01:47 +0900

    feat: 새로운 레스토랑 추가시 localStorage에 저장 및 가져오는 로직 구현

---
+++ src/domain/RestaurantStorage.js
@@ -0,0 +1,21 @@
+import { DEFAULT_RESTAURANTS } from "../constants/options";
+
+export function AddNewRestaurant({ restaurant }) {
+  if (!GetRestaurantFromStorage()) {
+    localStorage.setItem("restaurants", JSON.stringify([restaurant]));
+    return;
+  }
+  const restaurants = GetRestaurantFromStorage();
+  restaurants.push(restaurant);
+  console.log(restaurants);
+  localStorage.setItem("restaurants", JSON.stringify(restaurants));
+}
+
+export function GetAllRestaurants() {
+  DEFAULT_RESTAURANTS.push(...GetRestaurantFromStorage());
+  return DEFAULT_RESTAURANTS;
+}
+
+export function GetRestaurantFromStorage() {
+  return JSON.parse(localStorage.getItem("restaurants"));
+}
