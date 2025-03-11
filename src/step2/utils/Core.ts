0867e65c54891c67527321e8f3b5702cbc50b75a
장정안 <wjddks96@naver.com>
2025-03-11 17:05:18 +0900

    feat: Core 내 useState 구현

---
+++ src/step2/utils/Core.ts
@@ -0,0 +1,62 @@
+import { debounce } from './@common/debounce';
+
+interface Dispatch<T> {
+  (value: T): void;
+}
+
+interface Options<T = unknown> {
+  currentStateKey: number;
+  renderCount: number;
+  states: T[];
+  root: null | Element;
+  rootComponent: null | (() => string);
+}
+
+function Core() {
+  const options: Options = {
+    currentStateKey: 0,
+    renderCount: 0,
+    states: [],
+    root: null,
+    rootComponent: null,
+  };
+
+  function useState<S = undefined>(initialState?: S): [S, Dispatch<S>];
+  function useState<S>(initialState?: S): [unknown, Dispatch<S>] {
+    const { currentStateKey: key, states } = options;
+    if (states.length === key) states.push(initialState);
+
+    const state = states[key];
+
+    const setState = (newState: S) => {
+      if (newState === state) return;
+
+      states[key] = newState;
+      _render();
+    };
+    options.currentStateKey += 1;
+
+    return [state, setState];
+  }
+
+  const _render = debounce(() => {
+    const { root, rootComponent } = options;
+    if (!root || !rootComponent) return;
+    root.innerHTML = rootComponent();
+    options.currentStateKey = 0;
+    options.renderCount += 1;
+  });
+
+  function render(
+    rootComponent: Options['rootComponent'],
+    root: Options['root']
+  ) {
+    options.root = root;
+    options.rootComponent = rootComponent;
+    _render();
+  }
+
+  return { useState, render };
+}
+
+export const { useState, render } = Core();
