d568fd793a5f27eddd57857e0bdb8dc3a028a7ee
spoyodevelop <kjy9505@gmail.com>
2025-03-11 14:17:32 +0900

    chores: 경로수정

--- src/components/restaurant/form/form.js
+++ src/components/restaurant/form/form.js
@@ -10,10 +10,7 @@ import { restaurantFormValidation } from "../../../validation/restaurantFormVali
 import { extractFormData } from "../../../utils/extract.ts";
 import createRestaurantItem from "../item/item.js";
 import Toast from "../../Toast/Toast.js";
-import {
-  addRestaurant,
-  restaurantList,
-} from "../../../model/RestaurantList.ts";
+import restaurantList from "../../../model/RestaurantList.ts";
 
 export default function createRestaurantForm() {
   const restaurantAddForm = createElement("form", {
@@ -87,7 +84,7 @@ export default function createRestaurantForm() {
       const restaurantForm = extractFormData(restaurantAddForm);
       const restaurant = restaurantFormValidation(restaurantForm);
       const restaurantListElement = document.querySelector(".restaurant-list");
-      addRestaurant(restaurantForm, restaurantList);
+      restaurantList.addRestaurant(restaurantForm, restaurantList);
       restaurantListElement.appendChild(createRestaurantItem(restaurantForm));
 
       Toast.showToast(`${restaurant.name} 음식점을 추가했습니다.`, "success");
