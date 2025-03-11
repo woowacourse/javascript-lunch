00e3b572b967d674edfa7c99a7e926149077de51
eunwoo-levi <eunwoo1341@gmail.com>
2025-03-11 17:02:41 +0900

    feat: '모든 음식점' 클릭 시 추가 된 레스토랑 포함된 리스트 로직 구현

---
+++ src/domain/HeaderCategory.js
@@ -0,0 +1,29 @@
+import { createRestaurantList } from "../components/createRestaurantItem";
+import { DEFAULT_RESTAURANTS } from "../constants/options";
+import { GetAllRestaurants, GetFavoriteRestaurant } from "./RestaurantStorage";
+
+export default function HeaderCategory() {
+  const $allButton = document.getElementById("all-button");
+  const $favoriteButton = document.getElementById("favorite-button");
+  const $restaurantList = document.querySelector(".restaurant-list");
+
+  $allButton.addEventListener("click", (e) => {
+    location.reload();
+
+    $allButton.classList.add("active");
+    $favoriteButton.classList.remove("active");
+
+    const restaurants = GetAllRestaurants();
+    createRestaurantList();
+  });
+
+  $favoriteButton.addEventListener("click", (e) => {
+    $favoriteButton.classList.add("active");
+    $allButton.classList.remove("active");
+    const favoriteList = GetFavoriteRestaurant();
+
+    if (favoriteList.length > 0) {
+      createRestaurantItem($restaurantList, favoriteList);
+    }
+  });
+}
