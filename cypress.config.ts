23e1c8e6e45bcc96de9283fcad6395a9470e2d42
spoyodevelop <kjy9505@gmail.com>
2025-03-11 19:36:34 +0900

    chores: cypress-localstorage-commands 추가

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
+import localStorageCommands from "cypress-localstorage-commands/plugin.js";
 
 export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      localStorageCommands(on, config);
+      return config;
     },
   },
 });
