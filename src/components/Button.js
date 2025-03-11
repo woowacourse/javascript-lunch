7f9430a54a3f111a1b16cc17579269fbea2e35dd
dev-dino22 <wlsskdud02@naver.com>
2025-03-12 01:05:47 +0900

    feat: Button 컴포넌트 추가 및 렌더링 기능 구현

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+export function Button(id, type, content, dataSet, styleType) {
+  const buttonType =
+    styleType === "primary"
+      ? {
+          classList: "button button--primary text-caption",
+        }
+      : {
+          classList: "button button--secondary text-caption",
+          id: "closeModalBtn",
+          dataAction: "removeModal",
+        };
+
+  function render() {
+    return `
+    <div>
+        <button 
+          ${id ? `id="${id}"` : ""}
+          type="${type}" 
+          class="${buttonType}"
+          ${dataAction ? `data-action="${dataAction}"` : ""}
+        >
+          ${content}
+        </button>
+      </div>
+    `;
+  }
+  render();
+}
