89cd617843b1a0df278d85efc0345812b617be2b
Minje Sung <minje813@dgu.ac.kr>
2025-03-11 15:55:11 +0900

    feat: Restaurant 타입 구현

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,3 +2,13 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.svg";
 declare module "*.css";
+
+type Label = "한식" | "중식" | "일식" | "양식" | "아시안" | "기타";
+
+interface Restaurant {
+  label: Label;
+  name: string;
+  distance: number;
+  description?: string;
+  link?: string;
+}
