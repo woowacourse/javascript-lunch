71aa6e83eb4ab585b36ca1973a6fd88234a247b1
ShinjungOh <tlswjd9260@gmail.com>
2025-03-11 16:08:28 +0900

    refactor: description 조건부 렌더링 처리

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -18,7 +18,7 @@ const createRestaurantItem = ({
       <div class="restaurant__info">
         <h3 class="restaurant__name text-subtitle">${name}</h3>
         <span class="restaurant__distance text-body">${distance}</span>
-        <p class="restaurant__description text-body">${description}</p>
+        ${description ? `<p class="restaurant__description text-body">${description}</p>` : ""}
       </div>
     `;
   return li;
