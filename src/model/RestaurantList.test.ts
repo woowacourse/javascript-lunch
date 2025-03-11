a05c9635705ce2e22ebd8d8f7e2352c65059cfc1
spoyodevelop <kjy9505@gmail.com>
2025-03-11 14:17:16 +0900

    refactor: RestaurantList를 class로 리펙터

--- src/model/RestaurantList.test.ts
+++ src/model/RestaurantList.test.ts
@@ -1,9 +1,9 @@
 import { ERROR_MESSAGE } from "../settings/settings.ts";
-import { addRestaurant } from "./RestaurantList.ts";
+import restaurantList from "./RestaurantList.ts";
 
 describe("RestaurantList test", () => {
   it("should throw error when adding duplicated restaurant", () => {
-    const restaurant = {
+    const newRestaurant = {
       name: "이태리키친",
       distance: "20",
       description:
@@ -12,7 +12,7 @@ describe("RestaurantList test", () => {
       category: "양식",
       link: "https://italykitchen.co.kr",
     };
-    expect(() => addRestaurant(restaurant)).toThrow(
+    expect(() => restaurantList.addRestaurant(newRestaurant)).toThrow(
       ERROR_MESSAGE.DUPLICATE_RESTAURANT
     );
   });
