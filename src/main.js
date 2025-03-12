86ba4f82abb3e3cbebde6b59d4960aec965737aa
dev-dino22 <wlsskdud02@naver.com>
2025-03-12 16:30:08 +0900

    chore: 주석 제거

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-// 만약 모든 리스트가 없을 경우도 처리하기
-// 테스트 코드
-// 리팩토링 포인트 고민
-
 import { LunchList } from "./components/LunchList.js";
 import SubmitEvent from "./utils/SubmitEvent.js";
 import { getHTML } from "./utils/utils.js";
