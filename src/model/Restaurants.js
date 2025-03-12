d101ddad823c7393f29158e9dfcce587ebe47411
minji2219 <aabb0219@naver.com>
2025-03-12 22:27:09 +0900

    refactor: filter의 타입 상태를 가지기 위해 restaurants 객체 생성

---
+++ src/model/Restaurants.js
@@ -0,0 +1,67 @@
+import RestaurantCard from "../components/restaurantCard";
+import { $ } from "../utils/dom";
+import { defaultRestaurants } from "../defaultRestaurants";
+
+class Restaurants {
+  #restaurants;
+  #filterType;
+
+  constructor() {
+    this.#restaurants = [...defaultRestaurants];
+    this.#filterType = {
+      category: "all",
+      option: "name",
+      favorite: false,
+    };
+    this.filter();
+  }
+
+  createRestaurant(restaurants) {
+    const ulTag = $(".restaurant-list");
+    ulTag.replaceChildren();
+
+    restaurants.forEach((restaurant) => {
+      ulTag.appendChild(RestaurantCard(restaurant));
+    });
+  }
+
+  pushList(restaurant) {
+    this.#restaurants.push(restaurant);
+    this.filter();
+  }
+
+  changeState(state) {
+    const sortType = [...Object.keys(state)];
+    const sortState = state[sortType];
+
+    this.#filterType[sortType] = sortState;
+    this.filter();
+  }
+
+  filter() {
+    const filtered = this.filterByCategory();
+    if (this.#filterType.option === "name") this.sortByName(filtered);
+    if (this.#filterType.option === "distance") this.sortByDistance(filtered);
+    this.createRestaurant(filtered);
+  }
+
+  sortByName(restaurants) {
+    return restaurants.sort((a, b) => a.info.name.localeCompare(b.info.name));
+  }
+
+  sortByDistance(restaurants) {
+    return restaurants.sort((a, b) => a.info.distance - b.info.distance);
+  }
+
+  filterByCategory() {
+    if (this.#filterType.category === "all") {
+      return [...this.#restaurants];
+    }
+
+    return [...this.#restaurants].filter(
+      (restaurant) => restaurant.info.category === this.#filterType.category
+    );
+  }
+}
+
+export default Restaurants;
