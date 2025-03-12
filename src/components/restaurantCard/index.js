85be75665cd21983cd3b28d74ed54759a9116a6f
Ko Daeun <drechsu@gmail.com>
2025-03-12 09:42:31 +0900

    refactor : restaurant의 info 필드 이름을 value 로 변경

--- src/components/restaurantCard/index.js
+++ src/components/restaurantCard/index.js
@@ -3,12 +3,12 @@ import CategoryImage from "./categoryImage";
 import RestaurantInfo from "./restaurantInfo";
 
 const RestaurantCard = (restaurant) => {
-  const { category, name, distance, description } = restaurant.info;
+  const { category, name, distance, description } = restaurant.value;
 
   const restaurantCard = createElement({
     tagName: "li",
     classNames: ["restaurant"],
-    children: [CategoryImage(category), RestaurantInfo(restaurant.info)],
+    children: [CategoryImage(category), RestaurantInfo(restaurant.value)],
   });
 
   return restaurantCard;
--- src/domain/Restaurant.js
+++ src/domain/Restaurant.js
@@ -2,27 +2,27 @@ import { ERROR_MESSAGE } from "../constants/error";
 import { validateEmptyString } from "../validate/validateEmptyString";
 
 class Restaurant {
-  #info;
+  #value;
   constructor({ category, name, distance, description, link }) {
-    this.#info = { category, name, distance, description, link };
+    this.#value = { category, name, distance, description, link };
     this.#validate();
   }
 
   #validate() {
     validateEmptyString(
-      this.#info.category,
+      this.#value.category,
       ERROR_MESSAGE.CATEGORY_FIELD_REQUIRED
     );
-    validateEmptyString(this.#info.name, ERROR_MESSAGE.NAME_FIELD_REQUIRED);
+    validateEmptyString(this.#value.name, ERROR_MESSAGE.NAME_FIELD_REQUIRED);
     validateEmptyString(
-      this.#info.distance,
+      this.#value.distance,
       ERROR_MESSAGE.DISTANCE_FIELD_REQUIRED
     );
   }
 
-  get info() {
+  get value() {
     return {
-      ...this.#info,
+      ...this.#value,
     };
   }
 }
