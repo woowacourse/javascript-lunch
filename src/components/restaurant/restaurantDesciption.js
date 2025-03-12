9202c35fa8987834e7e571d3af80f471f9624388
spoyodevelop <kjy9505@gmail.com>
2025-03-12 18:52:10 +0900

    feat:restaurantDescription을 제작

---
+++ src/components/restaurant/restaurantDesciption.js
@@ -0,0 +1,52 @@
+const categoryIcon = {
+  한식: "./category-korean.png",
+  중식: "./category-chinese.png",
+  일식: "./category-japanese.png",
+  양식: "./category-western.png",
+  아시안: "./category-asian.png",
+  기타: "./category-etc.png",
+};
+export default function createRestaurantDescription({
+  category,
+  name,
+  distance,
+  description,
+  link,
+  isFavorite,
+}) {
+  const restaurantItem = createElement("div", {
+    className: "restaurant-description",
+  });
+
+  restaurantItem.innerHTML = `
+    <div class="restaurant__category">
+      <img
+        src="${categoryIcon[category]}"
+        alt="${category}"
+        class="category-icon"
+      />
+    </div>
+    <div class="restaurant__info">
+  
+      <div class="restaurant__header"> 
+        <div> 
+        <h3 class="restaurant__name text-subtitle">${name}</h3>
+        <span class="restaurant__distance text-body"
+          >캠퍼스부터 ${distance}분 내</span
+        >
+        </div>
+         <img src="${
+           isFavorite ? "./Star.png" : "./Un-star.png"
+         }" class="favorite-icon"/>
+      </div>
+     
+    
+      <p class="restaurant__description text-body">
+        ${description}
+      </p>
+      
+    </div>
+    `;
+
+  return restaurantItem;
+}
