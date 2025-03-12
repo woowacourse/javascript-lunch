59793295cb2b54257e6ed998e35adc6d46114748
deokbin <deokbin@naver.com>
2025-03-12 18:36:19 +0900

    ✨ feat: 레스토랑 데이터, 모달 열려있는지 여부, 카테고리 타입 설정

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,3 +2,17 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.svg";
 declare module "*.css";
+
+export interface Restaurant {
+  imgSrc: string;
+  imgAlt: string;
+  name: string;
+  distance: number;
+  description: string;
+}
+
+export type isModalOpen = boolean;
+
+export interface Categories {
+  [key: string]: string;
+}
