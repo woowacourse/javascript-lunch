dd01c572c95ec2bce32c508c3474ed482239c1fa
ahhyun <wndkgus0225@naver.com>
2025-03-12 21:06:04 +0900

    refactor: CategoryFilter.js에서 CATEGORIES 상수 사용

--- src/components/Filter/CategoryFilter.js
+++ src/components/Filter/CategoryFilter.js
@@ -1,3 +1,5 @@
+import { CATEGORIES } from "../../constants/constants";
+
 const createCategoryFilter = () => {
   const addrestaurant_filter_container = document.querySelector(
     ".restaurant-filter-container"
@@ -5,12 +7,9 @@ const createCategoryFilter = () => {
   const categoryFilter = `
   <select name="category" id="category-filter" class="restaurant-filter">
     <option value="전체">전체</option>
-    <option value="한식">한식</option>
-    <option value="중식">중식</option>
-    <option value="일식">일식</option>
-    <option value="양식">양식</option>
-    <option value="아시안">아시안</option>
-    <option value="기타">기타</option>
+    ${CATEGORIES.map(
+      (category) => `<option value="${category}">${category}</option>`
+    )}
   </select>
   `;
 
