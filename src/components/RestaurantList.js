9e6a0eb3189331457d6d77804340e3de6dbb50d5
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-12 15:34:15 +0900

    feat: html 템플릿 태그 적용

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,16 +1,20 @@
 import RestaurantItem from "./RestaurantItem.js";
 
 const RestaurantList = (restaurants) => {
-  return /* html */ `
+  const htmlString = /* html */ `
     <section class="restaurant-list-container">
       <ul id="restaurant-list" class="restaurant-list" data-testid="restaurant-list">
         ${restaurants
           .map((restaurant) => RestaurantItem(restaurant))
-          .reverse()
           .join("")}
       </ul>
     </section>
   `;
+
+  const $template = document.createElement("template");
+  $template.innerHTML = htmlString;
+
+  return $template.content.firstElementChild;
 };
 
 export default RestaurantList;
