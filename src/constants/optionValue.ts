f2a9745a52af91fae255a12c00681ad7218ae42f
deokbin <deokbin@naver.com>
2025-03-12 23:22:07 +0900

    ✨ feat: 카테고리 필터링값 및 분류 기준값 상수에 추가

--- src/constants/optionValue.ts
+++ src/constants/optionValue.ts
@@ -23,3 +23,18 @@ export const label = {
   description: "설명",
   link: "참고 링크",
 };
+
+export const categoryFilterValue = {
+  전체: "전체",
+  한식: "한식",
+  중식: "중식",
+  일식: "일식",
+  양식: "양식",
+  아시안: "아시안",
+  기타: "기타",
+};
+
+export const sortingValue = {
+  name: "이름순",
+  distance: "거리순",
+};
