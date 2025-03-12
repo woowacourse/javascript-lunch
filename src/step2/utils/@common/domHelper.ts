8789fe44348018a5990fb0ff9c54ac051c25c14c
장정안 <wjddks96@naver.com>
2025-03-12 17:02:39 +0900

    feat: domHelper 함수 타입 설정

--- src/step2/utils/@common/domHelper.ts
+++ src/step2/utils/@common/domHelper.ts
@@ -1,15 +1,15 @@
-export const $ = (
+export function $<T extends HTMLElement = HTMLElement>(
   selector: string,
   scope: Document | HTMLElement = document
-) => {
+): T {
   if (!selector) throw new Error('Selector is not selected');
-  return scope.querySelector(selector);
-};
+  return scope.querySelector(selector) as T;
+}
 
-export const $$ = (
+export function $$<T extends NodeListOf<Element>>(
   selector: string,
   scope: Document | HTMLElement = document
-) => {
+): T {
   if (!selector) throw new Error('Selector is not selected');
-  return scope.querySelectorAll(selector);
-};
+  return scope.querySelectorAll(selector) as T;
+}
