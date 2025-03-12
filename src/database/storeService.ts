ae4edfbf3a1b6da9557f53827bdb9ae54bfcf1e5
Jeongeun Lee <leeju1009@gmail.com>
2025-03-12 16:12:49 +0900

    refactor: storeService에서 cache 의존적인 로직 제거
    - cache가 storeService에 의존하는 것이 더 적절하다고 판단함

--- src/database/storeService.ts
+++ src/database/storeService.ts
@@ -1,7 +1,6 @@
 import { Restaurant } from '../../types/domain';
 import store from './store.ts';
 import { parseJSON, parseStorageKey, stringifyJSON } from '../utils/data.ts';
-import cache from './cache.ts';
 import { STORE } from '../constants.js';
 
 interface StoreService {
@@ -14,17 +13,11 @@ interface StoreService {
 
 const storeService: StoreService = {
   getRestaurants() {
-    const cachedRestaurants = cache.getRestaurants();
-    if (cachedRestaurants.length > 0) {
-      return cachedRestaurants;
-    }
-
     const keys = Object.keys(store.storage).filter((key) => {
       return key.startsWith(STORE.keyPrefix);
     });
 
     const restaurants = keys.map((key) => parseJSON(store.getData(key) ?? '[]'));
-    cache.setRestaurants(restaurants);
     return restaurants;
   },
 
@@ -45,15 +38,11 @@ const storeService: StoreService = {
     const parsedKey = parseStorageKey(STORE.keyPrefix, name);
     const stringData = stringifyJSON(data);
     store.setData(parsedKey, stringData);
-
-    cache.clearRestaurants();
   },
 
   deleteRestaurantByName(name) {
     const parsedKey = parseStorageKey(STORE.keyPrefix, name);
     store.removeData(parsedKey);
-
-    cache.clearRestaurants();
   },
 };
 
