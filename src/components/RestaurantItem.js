b7bc4e3f56257bff83c55879617d590e33f9d349
youjeong Jeong <wjddbwjd1205@naver.com>
2025-03-12 18:08:58 +0900

    feat: 음식점 아이템에 즐겨찾기 추가 및 스타일 적용
    - createRestaurantItem에서 별 추가 (FavoriteStar 컴포넌트 활용)
    - titleStar__box 레이아웃 적용 (스타일 추가)
    - 음식점 아이템 UI 개선 및 정렬 구조 변경

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,4 +1,5 @@
-import { IMAGE } from '../constants/constants.ts';
+import { IMAGE, FAV_STAR } from '../constants/constants.ts';
+import createFavoriteStar from './FavoriteStar.js'
 
 function createRestaurantItem({ category, name, distance, description, link }) {
   const item = `<li class="restaurant">
@@ -6,8 +7,13 @@ function createRestaurantItem({ category, name, distance, description, link }) {
                 <img src="${IMAGE.get(category)}" alt="${category}" class="category-icon" />
               </div>
               <div class="restaurant__info">
-                <h3 class="restaurant__name text-subtitle">${name}</h3>
-                <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+                <div class="titleStar__box">
+                  <div class="title__box">
+                    <h3 class="restaurant__name text-subtitle">${name}</h3>
+                    <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+                  </div>
+                  ${createFavoriteStar(FAV_STAR)}
+                </div>
                 <p class="restaurant__description text-body">
                   ${description}
                 </p>
