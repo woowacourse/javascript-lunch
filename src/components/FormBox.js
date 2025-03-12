f0da332b423fc06006ac6f9f5df56646dec551bb
dev-dino22 <wlsskdud02@naver.com>
2025-03-12 14:13:24 +0900

    feat: FormBox 컴포넌트 추가 및 음식점 추가/삭제 폼 템플릿 구현

---
+++ src/components/FormBox.js
@@ -0,0 +1,76 @@
+import { Button } from "./Button.js";
+import { FormButtons } from "./FormButtons.js";
+import { InputBox } from "./InputBox.js";
+import { SelectBox } from "./SelectBox.js";
+import { TextareaBox } from "./TextareaBox.js";
+
+export function FormBox({ id, formName, label }) {
+  function template() {
+    return `
+        ${(label && `<h2 class="modal-title text-title">새로운 음식점</h2>`) || ""}
+        <form id="${id} class="modal-form">
+          ${(formName === "storeAdd" && storeAddTemplate) || ""}
+          ${(formName === "storeDelete" && storeDeleteTemplate) || ""}
+          ${FormButtons({ formName })}
+        </form>
+    `;
+  }
+
+  return template();
+}
+
+const storeAddTemplate = `
+      <!-- 카테고리 -->
+        ${SelectBox({
+          id: "category",
+          name: "category",
+          label: "카테고리",
+          optionName: "category",
+          required: true,
+        })}
+
+        <!-- 음식점 이름 -->
+        ${InputBox({
+          name: "name",
+          id: "name",
+          required: true,
+          label: "이름",
+          placeHolder: "파양콩 할마니",
+          maxLength: 15,
+          type: "text",
+        })}
+
+        <!-- 거리 -->
+        ${SelectBox({
+          id: "distance",
+          name: "distance",
+          label: "거리(도보 이동 시간)",
+          optionName: "distance",
+          required: true,
+        })}
+
+        <!-- 설명 -->
+        ${TextareaBox({
+          id: "description",
+          name: "description",
+          label: "설명",
+          required: false,
+          cols: "30",
+          rows: "5",
+          helpCaption: "메뉴 등 추가 정보를 입력해 주세요.",
+        })}
+
+        <!-- 링크 -->
+        ${InputBox({
+          name: "link",
+          id: "link",
+          required: false,
+          label: "참고 링크",
+          placeHolder: "https://",
+          maxLength: 30,
+          type: "url",
+          helpCaption: "매장 정보를 확인할 수 있는 링크를 입력해 주세요.",
+        })}
+`;
+
+const storeDeleteTemplate = ``;
