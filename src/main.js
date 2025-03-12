441627d7b9c79cb139d1611a61e50712c5102d5a
hanheel <hanhee630630@gmail.com>
2025-03-12 17:57:18 +0900

    [refactor] : window 객체 명시

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { foodItems } from "./mock/mockItem.js";
 import { FoodListPage } from "./pages/FoodListPage.js";
 
-addEventListener("load", () => {
+window.addEventListener("load", () => {
   FoodListPage();
 });
