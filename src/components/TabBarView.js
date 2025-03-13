406bf51ffcd745eb81ed28a066f390f002f2e1cf
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-13 14:57:01 +0900

    test: 음식점 목록과 상세 모달에서 즐겨찾기 추가 E2E 테스트

--- src/components/TabBarView.js
+++ src/components/TabBarView.js
@@ -1,8 +1,8 @@
 const TabBarView = () => {
   return /*html*/ `
     <section class="tab-bar">
-      <button class="tab-bar__button active" id="list-tab">모든 음식점</button>
-      <button class="tab-bar__button" id="favorite-tab">자주 가는 음식점</button>
+      <button class="tab-bar__button active" id="list-tab" data-testid="list-tab">모든 음식점</button>
+      <button class="tab-bar__button" id="favorite-tab" data-testid="favorite-tab">자주 가는 음식점</button>
     </section>
   `;
 };
