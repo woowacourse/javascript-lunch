1ed4622223ac1de7e4f477d26587f1564c44f540
장정안 <wjddks96@naver.com>
2025-03-13 21:47:32 +0900

    feat: CategorySelect 컴포넌트 구현

---
+++ src/step2/components/CategorySelect.ts
@@ -0,0 +1,34 @@
+import Select from './Select';
+import { Category } from '../types/restaurants';
+import EventManager from '../utils/@common/EventManager';
+import { $ } from '../utils/@common/domHelper';
+import { CATEGORIES } from '../constants/options';
+
+interface CategorySelectProps {
+  handleCategoryChange: (category: Category) => void;
+  category: Category;
+}
+
+export const CategorySelect = (props: CategorySelectProps) => {
+  const { handleCategoryChange, category } = props;
+  const eventManager = new EventManager($('#app'));
+
+  eventManager.addEvent('change', '#category-filter', (e: Event) => {
+    const target = e.target as HTMLSelectElement;
+    console.log('e.target.value', target.value);
+    handleCategoryChange(target.value as Category);
+  });
+
+  return Select({
+    attribute: {
+      id: 'category-filter',
+      class: 'restaurant-filter',
+      name: 'category',
+      value: category,
+    },
+    children: Select.Option({
+      options: CATEGORIES,
+      selectedValue: category,
+    }),
+  });
+};
