50dfb4d87933ed6d009f866eab95765532c408c7
spoyodevelop <kjy9505@gmail.com>
2025-03-13 19:47:51 +0900

    feat: deleteRestaurant 구현

--- src/model/RestaurantList.ts
+++ src/model/RestaurantList.ts
@@ -3,24 +3,32 @@ import { ERROR_MESSAGE } from "../settings/settings";
 import { restaurantFormValidation } from "../validation/restaurantFormValidation";
 
 class RestaurantList {
-  readonly restaurantList: Restaurant[] = [];
+  #restaurantList: Restaurant[] = [];
   constructor(restaurantList: Restaurant[]) {
-    this.restaurantList = restaurantList;
+    this.#restaurantList = restaurantList;
   }
   get List() {
-    return this.restaurantList;
+    return this.#restaurantList;
   }
   parseRestaurantForm(restaurantForm: RestaurantForm) {
     return restaurantFormValidation(restaurantForm);
   }
+  searchRestaurant(name: string): Restaurant | undefined {
+    return this.#restaurantList.find((item) => item.name === name);
+  }
   addRestaurant(restaurant: Restaurant) {
     if (this.searchRestaurant(restaurant.name))
       throw new Error(ERROR_MESSAGE.DUPLICATE_RESTAURANT);
-    this.restaurantList.push(restaurant);
+    this.#restaurantList.push(restaurant);
   }
-  searchRestaurant(name: string): Restaurant | undefined {
-    return this.restaurantList.find((item) => item.name === name);
+  deleteRestaurant(restaurantName: string) {
+    if (!this.searchRestaurant(restaurantName))
+      throw new Error(ERROR_MESSAGE.NO_RESTAURANT_FOUND);
+    this.#restaurantList = [
+      ...this.#restaurantList.filter((item) => item.name !== restaurantName),
+    ];
   }
+
   toggleFavoriteRestaurant(restaurant: Restaurant) {
     restaurant.isFavorite = !restaurant.isFavorite;
   }
