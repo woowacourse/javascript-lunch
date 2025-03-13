0f49a0f5ce365b99739d7ac5eb50d1fc14188b80
deokbin <deokbin@naver.com>
2025-03-13 11:32:24 +0900

    ♻️ refactor: 필터링 드롭다운 함수형에서 클래스로 변경

--- src/components/Dropdown/FilterDropdown.js
+++ src/components/Dropdown/FilterDropdown.js
@@ -1,13 +1,21 @@
 import { label } from "../../constants/optionValue.js";
+import Component from "../Component.js";
 
-const FilterDropdown = ({ id, optionValue }) => {
-  return `
-    <select name="${id}" id="${id}-filter" class="restaurant-filter">
-      ${Object.entries(optionValue)
-        .map(([key, value]) => `<option value="${key}">${value}</option>`)
-        .join("")}    
-    </select>
-  `;
-};
+class FilterDropdown extends Component {
+  template() {
+    return `
+        <select name="${this.props.id}" id="${this.props.id}-filter" class="restaurant-filter">
+        ${Object.entries(this.props.optionValue)
+          .map(([key, value]) => `<option value="${key}">${value}</option>`)
+          .join("")}
+        </select>
+        `;
+  }
 
+  setEvent() {
+    this.$target.addEventListener("change", (e) => {
+      this.props.onChange(e.target.value);
+    });
+  }
+}
 export default FilterDropdown;
