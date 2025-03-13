9634fabfb681a3d5cb241e15d67d272f1466da00
Jeongeun Lee <leeju1009@gmail.com>
2025-03-13 11:30:44 +0900

    feat: 자주 가는 음식점 추가 및 삭제 버튼 생성

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,19 +1,24 @@
 import { IMAGE } from '../constants.js';
 
-function createRestaurantItem({ category, name, distance, description, link }) {
+function createRestaurantItem({ category, name, distance, description, favorite }) {
   const item = `<li class="restaurant">
               <div class="restaurant__category">
                 <img src="${IMAGE.get(category)}" alt="${category}" class="category-icon" />
               </div>
               <div class="restaurant__info">
-                <h3 class="restaurant__name text-subtitle">${name}</h3>
-                <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
-                <p class="restaurant__description text-body">
-                  ${description}
-                </p>
-                <a href="${link}"></a>
+                <div class="restaurant__header">
+                  <div class="restaurant__details">
+                    <h3 class="restaurant__name text-subtitle">${name}</h3>
+                    <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
+                  </div>
+                  <button type="button" class="button restaurant__favorite">
+                    <img src="${favorite ? 'favorite-icon.png' : 'favorite-icon-lined.png'}" alt="favorite-icon" />
+                  </button>
+                </div>
+                <p class="restaurant__description text-body">${description}</p>
               </div>
-            </li>`;
+            </li>
+          `;
 
   return item;
 }
