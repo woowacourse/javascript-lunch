6f51515edec66c9b1e9de5b1fc84db11df5d37f9
deokbin <deokbin@naver.com>
2025-03-14 00:52:08 +0900

    ✨ feat: 카테고리 필터링 및 분류 필터링 분리

--- src/components/createFilterSelect.ts
+++ src/components/createFilterSelect.ts
@@ -1,25 +1,29 @@
 import { categoryFilterValue, sortingValue } from "../constants/optionValue.js";
 import FilterDropdown from "./Dropdown/FilterDropdown.js";
-import { RestaurantData } from "../constants/RestaurantData.js";
-import filterByCategory from "./filterRestaurants.js";
-import renderRestaurants from "./renderRestaurants.js";
-const createFilterSelect = (filter: string) => {
-  if (filter === "category") {
-    return new FilterDropdown(
-      document.querySelector(".restaurant-filter-container"),
-      {
-        id: "category",
-        optionValue: categoryFilterValue,
-        onChange: (selectedCategory: string) => {
-          const filteredRestaurants = filterByCategory(
-            RestaurantData,
-            selectedCategory,
-          );
-          renderRestaurants(filteredRestaurants);
-        },
-      },
-    );
-  }
+
+export const createCategoryFilter = (
+  onChange: void,
+  selectedCategory: string,
+) => {
+  return new FilterDropdown(
+    document.querySelector(".category-filter-container"),
+    {
+      id: "category",
+      optionValue: categoryFilterValue,
+      selectedValue: selectedCategory,
+      onChange,
+    },
+  );
 };
 
-export default createFilterSelect;
+export const createSortingFilter = (onChange: void, sortOption: string) => {
+  return new FilterDropdown(
+    document.querySelector(".sorting-filter-container"),
+    {
+      id: "sorting",
+      optionValue: sortingValue,
+      selectedValue: sortOption,
+      onChange,
+    },
+  );
+};
