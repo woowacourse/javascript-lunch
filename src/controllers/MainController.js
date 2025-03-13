57836c026135092552831ab9b98c8ea5971530e8
hoyyChoi <hoyeon8165@gmail.com>
2025-03-13 21:20:40 +0900

    fix: often-go -> favorite 네이밍 변경

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,4 +1,5 @@
 import CategoryFilterController from "./CategoryFilterController.js";
+import FavoriteListController from "./FavoriteListController.js";
 import HeaderController from "./HeaderController.js";
 import ListController from "./ListController.js";
 import ModalController from "./ModalController.js";
@@ -8,16 +9,17 @@ function MainController() {
   const app = document.getElementById("app");
   const mainElement = app.querySelector("main");
   const allListContainerElement = mainElement.querySelector(".all-restaurant-list-container");
-  const oftenGoListContainerElement = mainElement.querySelector(".often-go-restaurant-list-container");
+  const favoriteListContainerElement = mainElement.querySelector(".favorite-restaurant-list-container");
 
   const { listElement, restaurantList, updateList } = ListController(allListContainerElement);
+
   const { categoryFilterElement, sortingFilterElement } = CategoryFilterController(allListContainerElement, updateList);
   HeaderController(app);
   ModalController(mainElement, {
     updateList: () => updateList(category, sortOption),
     restaurantList,
   });
-  TabController(mainElement, { allListContainerElement, oftenGoListContainerElement });
+  TabController(mainElement, { allListContainerElement, favoriteListContainerElement });
 
   listElement.addEventListener("click", (event) => {
     const starElement = event.target.closest(".favorite-star");
--- src/controllers/TabController.js
+++ src/controllers/TabController.js
@@ -2,29 +2,29 @@ import TabWrapper from "../components/TabWrapper.js";
 
 const TAB_DATA = [
   { id: "all-restaurant", text: "모든 음식점" },
-  { id: "often-go-restaurant", text: "자주 가는 음식점" },
+  { id: "favorite-restaurant", text: "자주 가는 음식점" },
 ];
-function TabController(mainElement, { allListContainerElement, oftenGoListContainerElement }) {
+function TabController(mainElement, { allListContainerElement, favoriteListContainerElement }) {
   const tabContainerElement = TabWrapper(TAB_DATA);
   mainElement.prepend(tabContainerElement);
 
   const allRestaurantTab = tabContainerElement.querySelector("#all-restaurant");
-  const oftenGoRestaurantTab = tabContainerElement.querySelector("#often-go-restaurant");
+  const favoriteRestaurantTab = tabContainerElement.querySelector("#favorite-restaurant");
 
   allRestaurantTab.classList.add("active");
 
   allRestaurantTab.addEventListener("click", () => {
-    oftenGoRestaurantTab.classList.remove("active");
+    favoriteRestaurantTab.classList.remove("active");
     allRestaurantTab.classList.add("active");
-    oftenGoListContainerElement.classList.remove("active");
+    favoriteListContainerElement.classList.remove("active");
     allListContainerElement.classList.add("active");
   });
 
-  oftenGoRestaurantTab.addEventListener("click", () => {
+  favoriteRestaurantTab.addEventListener("click", () => {
     allRestaurantTab.classList.remove("active");
-    oftenGoRestaurantTab.classList.add("active");
+    favoriteRestaurantTab.classList.add("active");
     allListContainerElement.classList.remove("active");
-    oftenGoListContainerElement.classList.add("active");
+    favoriteListContainerElement.classList.add("active");
   });
 }
 
