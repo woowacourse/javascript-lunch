8cb255047d1ae76a45bac4178bc510c3a77b299e
ShinjungOh <tlswjd9260@gmail.com>
2025-03-13 11:11:37 +0900

    feat: 음식점 목록에 즐겨찾기 아이콘 추가

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,26 +1,30 @@
 import { IMAGE_SRC_BY_RESTAURANTS_CATEGORY } from "../constants/constants";
 
-const createRestaurantItem = ({
-  category,
-  name,
-  distance,
-  description,
-}) => {
+const createRestaurantItem = ({ category, name, distance, description }) => {
   const li = document.createElement("li");
   li.classList.add("restaurant");
 
   const mappedImage = IMAGE_SRC_BY_RESTAURANTS_CATEGORY[category];
 
-  li.innerHTML = `
-      <div class="restaurant__category">
-        <img src="${mappedImage}" alt="${category}" class="category-icon" />
-      </div>
-      <div class="restaurant__info">
-        <h3 class="restaurant__name text-subtitle">${name}</h3>
-        <span class="restaurant__distance text-body">${distance}</span>
-        ${description ? `<p class="restaurant__description text-body">${description}</p>` : ""}
-      </div>
-    `;
+  li.innerHTML = `<div class="restaurant__category">
+<img src="${mappedImage}" alt="${category}" class="category-icon" />
+</div>
+<div class="restaurant__info">
+<div class="restaurant__header">
+    <div class="restaurant__title">  
+    <h3 class="restaurant__name text-subtitle">${name}</h3>
+<span class="restaurant__distance text-body">${distance}</span>
+</div>
+<img src="images/favorite-icon-lined.png" alt="즐겨찾기" class="favorite-icon" />
+</div>
+${
+  description
+    ? `<p class="restaurant__description text-body">${description}</p>`
+    : ""
+}
+</div>
+  `;
+
   return li;
 };
 
