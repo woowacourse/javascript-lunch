0ce7752a469fc49e45431dd845ee5f0c09a2e966
kimyou1102 <yoosk5485@naver.com>
2025-03-13 13:39:22 +0900

    refactor: RestaurantListModel에 list값 더미 데이터로 변경
    - 개발 시 테스트를 위해 더미 데이터로 변경

--- src/domain/RestaurantListModel.js
+++ src/domain/RestaurantListModel.js
@@ -2,7 +2,25 @@ export default class RestaurantListModel {
   #restaurantList;
 
   constructor() {
-    this.#restaurantList = [];
+    const dummy = [
+      {
+        category: "한식",
+        name: "얌샘김밥",
+        distance: 5,
+        description: "김밥집",
+        link: "http://localhost:5174/",
+        bookmark: false,
+      },
+      {
+        category: "중식",
+        name: "친친",
+        distance: 5,
+        description: "가지덮밥",
+        link: "http://localhost:5174/",
+        bookmark: true,
+      },
+    ];
+    this.#restaurantList = dummy;
   }
 
   updateRestautantList(newRestaurantList) {
