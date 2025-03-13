b6182338012a4fcd28ecb9daca4b1abf32f0d20c
장정안 <wjddks96@naver.com>
2025-03-13 21:46:18 +0900

    feat: main.ts에서 초기 렌더링 시 로컬스토리지 값 저장하고 가져오게 구현

--- src/step2/main.ts
+++ src/step2/main.ts
@@ -1,5 +1,19 @@
 import App from './App';
+import RESTAURANT_INFO from './mocks/restaurantInfo';
 import { $ } from './utils/@common/domHelper';
+import { getStorage, saveStorage } from './utils/@common/localStorage';
 import { render } from './utils/core/Core';
 
-render(App, $('#app'));
+const initializeData = () => {
+  const existingRestaurants = getStorage();
+  if (!existingRestaurants) {
+    saveStorage(RESTAURANT_INFO);
+  }
+};
+
+const initialize = () => {
+  initializeData();
+  render(App, $('#app'));
+};
+
+initialize();
