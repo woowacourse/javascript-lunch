b954d54711379fef01ee4832a2b94b6392f273a4
ahhyun <wndkgus0225@naver.com>
2025-03-13 11:00:07 +0900

    refactor: CateforyFilter에서 join()을 사용하여 구분자 제거

--- src/components/Filter/CategoryFilter.js
+++ src/components/Filter/CategoryFilter.js
@@ -1,4 +1,6 @@
 import { CATEGORIES } from "../../constants/constants";
+import { restaurantsData } from "../../constants/restaurantsMockData";
+import renderRestaurantElement from "../RestaurantItem/RestaurantItem";
 
 const createCategoryFilter = () => {
   const addrestaurant_filter_container = document.querySelector(
@@ -9,14 +11,46 @@ const createCategoryFilter = () => {
     <option value="전체">전체</option>
     ${CATEGORIES.map(
       (category) => `<option value="${category}">${category}</option>`
-    )}
+    ).join("")}
   </select>
+  <p id="result"></p>
   `;
 
   addrestaurant_filter_container.insertAdjacentHTML(
     "beforeend",
     categoryFilter
   );
+
+  const selectElement = document.getElementById("category-filter");
+  selectElement.addEventListener("change", (event) =>
+    handleOnChange(event.target)
+  );
 };
 
+function handleOnChange(selectedCategory) {
+  const text = selectedCategory.options[selectedCategory.selectedIndex].text;
+  const selectedCategoryResult = document.getElementById("result");
+  showSelectedCategoryRestaurantList(text);
+}
+
+function showSelectedCategoryRestaurantList(selectedCategory) {
+  if (selectedCategory === "전체") {
+    console.log(selectedCategory);
+  }
+}
+
 export default createCategoryFilter;
+
+// function handleOnChange(selectedCategory) {
+//   const restaurantList = document.querySelector(".restaurant-list");
+//   restaurantList.innerHTML = "";
+
+//   const filteredData = selectedCategory === "전체"
+//     ? restaurantsData // "전체"를 선택하면 모든 데이터를 표시
+//     : restaurantsData.filter((restaurant) => restaurant.category === selectedCategory);
+
+//   filteredData.forEach((restaurantData) => {
+//     const restaurantItem = renderRestaurantElement(restaurantData);
+//     restaurantList.appendChild(restaurantItem);
+//   });
+// }
