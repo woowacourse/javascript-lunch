0ec1f68584938d1fdc24c18786a85e8a7bdcbbda
장정안 <wjddks96@naver.com>
2025-03-13 17:37:27 +0900

    feat: 로컬 스토리지 관련 에러 메세지 상수화

---
+++ src/step2/constants/messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = Object.freeze({
+  SAVE_LOCAL_STORAGE:
+    '로컬 스토리지에 데이터를 저장하는 중 오류가 발생했습니다.',
+  GET_LOCAL_STORAGE:
+    '로컬 스토리지에 데이터를 가져오는 중 오류가 발생했습니다.',
+} as const);
