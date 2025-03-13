b3f05609a8cec3a9dad478fa74a707cdeef0d59e
장정안 <wjddks96@naver.com>
2025-03-13 17:19:22 +0900

    feat: 카테고리 이미지 상수화

---
+++ src/step2/assets/images.ts
@@ -0,0 +1,8 @@
+export const CATEGORY_IMAGES = {
+  한식: './category-korean.png',
+  중식: './category-chinese.png',
+  일식: './category-japanese.png',
+  양식: './category-western.png',
+  아시안: './category-asian.png',
+  기타: './category-etc.png',
+} as const;
