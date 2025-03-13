913271f18203f8a4caba73228bb904df1b0497dc
Ko Daeun <drechsu@gmail.com>
2025-03-13 15:31:36 +0900

    feat : Restaurantcard 에서 즐겨찾기 아이콘 추가

--- src/components/RestaurantCard/index.js
+++ src/components/RestaurantCard/index.js
@@ -1,5 +1,7 @@
 import createElement from "../../utils/createElement/createElement";
+import Image from "../common/Image";
 import CategoryImage from "./CategoryImage";
+import FavoriteIcon from "./FavoriteIcon";
 import RestaurantInfo from "./RestaurantInfo";
 
 const RestaurantCard = (restaurant) => {
@@ -8,7 +10,11 @@ const RestaurantCard = (restaurant) => {
   const restaurantCard = createElement({
     tagName: "li",
     classNames: ["restaurant"],
-    children: [CategoryImage(category), RestaurantInfo(restaurant.value)],
+    children: [
+      CategoryImage(category),
+      RestaurantInfo(restaurant.value),
+      FavoriteIcon(),
+    ],
   });
 
   return restaurantCard;
