58833afa470ffde78eb315c17fe3a969b6e5bfbe
장정안 <wjddks96@naver.com>
2025-03-13 21:48:04 +0900

    feat: 카테고리 필터링 시 새롭게 렌더링하여 레스토랑 리스트 출력

--- src/step2/App.ts
+++ src/step2/App.ts
@@ -1,23 +1,52 @@
 import Header from './components/Header';
 import NavTab from './components/NavTab';
-import Select from './components/Select';
+import Restaurant from './components/Restaurant';
+import { CategorySelect } from './components/CategorySelect';
+import { SortingSelect } from './components/SortingSelect';
 import { TAB } from './constants/restaurantTypes';
 import useModal from './hooks/useModal';
 import useTab from './hooks/useTab';
-import { $ } from './utils/@common/domHelper';
-import EventManager from './utils/@common/EventManager';
+import RESTAURANT_INFO from './mocks/restaurantInfo';
+import { Category } from './types/restaurants';
+import { useState } from './utils/core/Core';
 
 function App() {
-  const eventManager = new EventManager($('#app'));
   const [isModalOpen, openModal, closeModal] = useModal(false);
   const [tab, setTabAll, setTabFavorite] = useTab(TAB.ALL);
-  console.log('렌더링 카운트');
-  // eventManager.addEvent('click', '.button', () => console.log('추가하기'));
+  const [category, setCategory] = useState<Category>('전체');
+
+  const handleCategoryChange = (selectedCategory: Category) => {
+    console.log('selectedCategory', selectedCategory);
+    setCategory(selectedCategory);
+  };
+
+  // 필터링된 레스토랑 목록 가져오기
+  const getFilteredRestaurants = () => {
+    console.log('category', category);
+    if (category === '전체') {
+      return RESTAURANT_INFO;
+    }
+    return RESTAURANT_INFO.filter(
+      (restaurant) => restaurant.category === category
+    );
+  };
+
+  const filteredRestaurants = getFilteredRestaurants();
+
   return `
     <div>
       ${Header({ openModal })}
       ${NavTab({ tab, setTabAll, setTabFavorite })}
-      ${Select()}
+      <section class="restaurant-filter-container">
+        ${CategorySelect({
+          handleCategoryChange,
+          category,
+        })}
+        ${SortingSelect()}
+      </section>
+      ${filteredRestaurants
+        .map((restaurant) => Restaurant(restaurant))
+        .join('')}
     </div>
   `;
 }
