8475346768e399ba2b308e63ae403ab7f8f4934b
spoyodevelop <kjy9505@gmail.com>
2025-03-13 16:42:18 +0900

    refactor: event binder를 declaretive하게 변경

--- src/event-binder/event-binder.js
+++ src/event-binder/event-binder.js
@@ -17,35 +17,46 @@ export default function bindEventHandlers(
   restaurantAddForm
 ) {
   document.body.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target.closest("#category-filter, #favorite-filter")) {
+      handleCombinedFilter(restaurantListElement, restaurantList);
+    }
+
     handleFormModalToggle(event);
     handleDescriptionModalToggle(event, restaurantList);
     handleFavoriteToggle(event, restaurantList, restaurantListElement);
   });
-  const filterElement = document.querySelectorAll(
+
+  // 필터 요소에 대한 change 이벤트 리스너 추가
+  const filterElements = document.querySelectorAll(
     "#category-filter, #favorite-filter"
   );
 
-  for (const el of filterElement) {
+  filterElements.forEach((el) => {
     el.addEventListener("change", () => {
       handleCombinedFilter(restaurantListElement, restaurantList);
     });
-  }
+  });
 
+  // 소팅 필터는 change 이벤트로 처리
   document
     .getElementById("sorting-filter")
     .addEventListener("change", (event) => {
       handleSort(event.target.value, restaurantList, restaurantListElement);
     });
 
+  // 레스토랑 추가 폼 제출 이벤트
   restaurantAddForm.addEventListener("submit", (event) =>
     handleAddRestaurantFormSubmit(event, restaurantList, restaurantAddForm)
   );
 
+  // Escape 키로 다이얼로그 닫기
   document.addEventListener("keydown", (event) => {
     if (event.key === "Escape") {
       const dialogs = document.getElementsByTagName("dialog");
       if (dialogs.length > 0 && dialogs[0].open) {
-        dialogs[0].close(); // esc로 닫기
+        dialogs[0].close();
       }
     }
   });
