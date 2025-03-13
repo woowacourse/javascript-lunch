898fde80e8ff3b53ccc5738547b8e7b2d0922bfe
hanheel <hanhee630630@gmail.com>
2025-03-13 15:54:12 +0900

    [feat] : 전역 타입 설정

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,4 +1,18 @@
+export {};
+
 declare module "*.png";
 declare module "*.jpg";
 declare module "*.svg";
 declare module "*.css";
+
+declare global {
+  type Category = "한식" | "중식" | "일식" | "양식" | "아시안" | "기타";
+  type Distance = "5분" | "10분" | "15분" | "20분" | "30분";
+
+  interface FoodItemProps {
+    category: Category;
+    name: string;
+    distance: Distance;
+    description: string;
+  }
+}
