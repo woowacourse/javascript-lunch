f2c68347bedc45e854e5f6363b0f9c31ba002f9c
Cron <hojoong.choi@woowahan.com>
2025-03-13 22:03:30 +0900

    refactor: 코드 정리

--- src/main.js
+++ src/main.js
@@ -3,39 +3,34 @@ import { createHeader } from "./components/Header";
 import { createRestaurantItem } from "./components/RestaurantItem";
 import Modal from "./components/Modal";
 
+const body = document.querySelector("body");
+const restaurantList = document.querySelector(".restaurant-list");
+const main = document.querySelector("main");
+
+// 식당 목록 추가 모달에서 보여야 할 항목
+const innerModalContent = document.createElement("div");
+innerModalContent.innerHTML = /*html*/ `
+      <h2>주렁 추가</h2>
+      이름: <input type="text" >
+      닉네임: <input type="text" >
+`;
+
+const modal = new Modal({ element: innerModalContent });
+main.append(modal.rendered);
+
+// 식당 아이템을 클릭하면 보여야 할 상세 정보
+const activatedRestaurantItem = {}; // 활성화된 식당 아이템 목록 정보
+const restaurantDetailModalContent = document.createElement("div");
+const modalDetailItem = createRestaurantItem(RESTAURANTS[0]);
+modalDetailItem.classList.add("flex-column");
+restaurantDetailModalContent.append(modalDetailItem);
+
 addEventListener("load", () => {
-  const body = document.querySelector("body");
-  const restaurantList = document.querySelector(".restaurant-list");
-  const main = document.querySelector("main");
-
-  const innerModalContent = document.createElement("div");
-  innerModalContent.innerHTML = /*html*/ `
-        <h2>주렁 추가</h2>
-        이름: <input type="text" >
-        닉네임: <input type="text" >
-  `;
-
-  const restaurantDetailModalContent = document.createElement("div");
-  const modalDetailItem = createRestaurantItem(RESTAURANTS[0]);
-  modalDetailItem.classList.add("flex-column");
-  restaurantDetailModalContent.append(modalDetailItem);
-
-  const modal = new Modal({ element: innerModalContent });
-  main.append(modal.rendered);
-
   const restaurantDetailModal = new Modal({
     element: restaurantDetailModalContent,
   });
   restaurantDetailModal.toggle();
   main.append(restaurantDetailModal.rendered);
-
-  const toggleModal = () => {
-    modal.toggle();
-  };
-
-  const header = createHeader({ title: "점심 뭐 먹지", onClick: toggleModal });
-  header.classList.add("gnb");
-  body.prepend(header);
 
   // 식당 아이템 불러오기 마이그레이션 완료
 
@@ -50,4 +45,13 @@ addEventListener("load", () => {
   });
 
   restaurantList.append(restaurantItems);
+
+  //////////
+  const toggleModal = () => {
+    modal.toggle();
+  };
+
+  const header = createHeader({ title: "점심 뭐 먹지", onClick: toggleModal });
+  header.classList.add("gnb");
+  body.prepend(header);
 });
