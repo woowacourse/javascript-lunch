715043441179525deb024ed7507a41fd726eff5b
hanheel <hanhee630630@gmail.com>
2025-03-13 14:32:05 +0900

    [style] : 즐겨찾기 버튼 추가

--- src/component/FoodItem.js
+++ src/component/FoodItem.js
@@ -5,16 +5,24 @@ export function FoodItem({ category, name, distance, description }) {
 
   const container = document.createElement("div");
   container.innerHTML = `
-     <li class="restaurant">
-          <div class="restaurant__category">
-            <img src=${imgSrc} alt=${imgAlt} class="category-icon">
-          </div>
-          <div class="restaurant__info">
-            <h3 class="restaurant__name text-subtitle">${name}</h3>
-            <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
-            <p class="restaurant__description text-body">${description}</p>
-          </div>
-        </li>`;
+  <li class="restaurant">
+    <div class="restaurant__category">
+      <img src="${imgSrc}" alt="${imgAlt}" class="category-icon" />
+    </div>
+    <div class="restaurant__info">
+      <div class="restaurant__info-header">
+        <div class="restaurant__title-container">
+          <h3 class="restaurant__name text-subtitle">${name}</h3>
+          <span class="restaurant__distance text-body"
+            >캠퍼스부터 ${distance}분 내</span
+          >
+        </div>
+        <img src="/favorite-icon-lined.png" alt="즐겨찾기" class="bookmark-icon">
+      </div>
+      <p class="restaurant__description text-body">${description}</p>
+    </div>
+  </li>
+  `;
 
   return container.firstElementChild;
 }
