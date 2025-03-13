c1bf6eb6dad1a8f297f28ca5142a31015541bf36
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-13 16:14:39 +0900

    refactor: App 컴포넌트 역할 분리

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import Header from "./components/Header.js";
-import RestaurantList from "./components/RestaurantList.js";
-import {
-  filterAndSortRestaurants,
-  restaurants,
-} from "./domains/restaurants.ts";
+import TabBarView from "./components/TabBar/TabBar.js";
 import AddRestaurantModal from "./components/modal/AddRestaurantModal/index.js";
-import FilterBar from "./components/FilterBar.js";
-import TabBarView from "./components/TabBarView.js";
+import FilterBarManager from "./components/FilterBar/FilterBarManager.js";
+import TabManager from "./components/TabBar/TabManager.js";
+import RestaurantManager from "./components/RestaurantList/RestaurantManager.js";
+import TabBar from "./components/TabBar/TabBar.js";
 
 class App {
-  #restaurants;
   #$target;
-  #selectedCategory = "전체";
-  #selectedSorting = "name";
+  #filterBarManager;
+  #tabManager;
+  #restaurantManager;
 
   constructor($target) {
     this.#$target = $target;
-    this.#restaurants = restaurants;
-
     this.#$target.insertAdjacentHTML("beforeend", this.#template());
+
+    this.#filterBarManager = new FilterBarManager();
+    this.#restaurantManager = new RestaurantManager(this.#filterBarManager);
+    this.#tabManager = new TabManager(
+      this.#restaurantManager,
+      this.#renderMainArea.bind(this)
+    );
+
     this.#mount();
   }
 
   #template() {
-    return /*html*/ `
+    return `
       ${Header()}
-      ${TabBarView()}
+      ${TabBar()}
       <main></main>
       <div id="modal"></div>
     `;
@@ -33,134 +37,25 @@ class App {
 
   #mount() {
     const $gnbButton = this.#$target.querySelector(".gnb__button");
-    const $listTab = this.#$target.querySelector("#list-tab");
-    const $favoriteTab = this.#$target.querySelector("#favorite-tab");
-
-    $listTab.addEventListener("click", () => this.#switchTab("list"));
-    $favoriteTab.addEventListener("click", () => this.#switchTab("favorite"));
 
     const $addModal = new AddRestaurantModal(
       document.querySelector("#modal"),
-      this.#addRestaurant.bind(this)
+      this.#restaurantManager.handleAddRestaurant.bind(this.#restaurantManager)
     );
 
     $gnbButton.addEventListener("click", () => $addModal.open());
-
     this.#renderMainArea();
-  }
-
-  #switchTab(type) {
-    const $listTab = this.#$target.querySelector("#list-tab");
-    const $favoriteTab = this.#$target.querySelector("#favorite-tab");
-
-    if (type === "list") {
-      $listTab.classList.add("active");
-      $favoriteTab.classList.remove("active");
-      this.#renderMainArea();
-      return;
-    }
-
-    $listTab.classList.remove("active");
-    $favoriteTab.classList.add("active");
-    this.#renderFavoriteList();
   }
 
   #renderMainArea() {
     const $main = document.querySelector("main");
     $main.replaceChildren();
 
-    new FilterBar($main, {
-      onCategoryChange: (selected) => {
-        this.#selectedCategory = selected;
-        this.#renderRestaurantList();
-      },
-      onSortingChange: (selected) => {
-        this.#selectedSorting = selected;
-        this.#renderRestaurantList();
-      },
-    });
-
-    this.#renderRestaurantList();
-  }
-
-  #renderFavoriteList() {
-    const $main = document.querySelector("main");
-    $main.replaceChildren();
-
-    const favorites = this.#restaurants.filter(
-      (restaurant) => restaurant.isFavorite
+    this.#filterBarManager.render(
+      $main,
+      this.#restaurantManager.updateList.bind(this.#restaurantManager)
     );
-    this.#renderList(favorites);
-  }
-
-  #renderRestaurantList() {
-    const filtered = filterAndSortRestaurants(
-      this.#restaurants,
-      this.#selectedCategory,
-      this.#selectedSorting
-    );
-    this.#renderList(filtered);
-  }
-
-  #renderList(restaurants) {
-    const $main = document.querySelector("main");
-    const $oldContainer = $main.querySelector(".restaurant-list-container");
-
-    const $newList = RestaurantList(restaurants, {
-      onToggleFavorite: this.#toggleFavorite.bind(this),
-      onDeleteRestaurant: this.#deleteRestaurant.bind(this),
-    });
-
-    if ($oldContainer) {
-      $main.replaceChild($newList, $oldContainer);
-    } else {
-      $main.appendChild($newList);
-    }
-  }
-
-  #toggleFavorite(clickedId) {
-    const target = this.#restaurants.find(
-      (restaurant) => restaurant.id === clickedId
-    );
-    if (!target) return;
-
-    target.isFavorite = !target.isFavorite;
-
-    this.#renderList(
-      this.#isFavoriteTabActive()
-        ? this.#restaurants.filter((r) => r.isFavorite)
-        : filterAndSortRestaurants(
-            this.#restaurants,
-            this.#selectedCategory,
-            this.#selectedSorting
-          )
-    );
-  }
-
-  #deleteRestaurant(clickedId) {
-    this.#restaurants = this.#restaurants.filter(
-      (restaurant) => restaurant.id !== clickedId
-    );
-    this.#renderList(
-      this.#isFavoriteTabActive()
-        ? this.#restaurants.filter((r) => r.isFavorite)
-        : filterAndSortRestaurants(
-            this.#restaurants,
-            this.#selectedCategory,
-            this.#selectedSorting
-          )
-    );
-  }
-
-  #isFavoriteTabActive() {
-    return this.#$target
-      .querySelector("#favorite-tab")
-      .classList.contains("active");
-  }
-
-  #addRestaurant(newRestaurant) {
-    this.#restaurants.push(newRestaurant);
-    this.#renderRestaurantList();
+    this.#restaurantManager.renderRestaurantList($main);
   }
 }
 
