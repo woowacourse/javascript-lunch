096352955184994ef8cb9789ef548fce992e3c6f
dev-dino22 <wlsskdud02@naver.com>
2025-03-14 19:06:18 +0900

    feat: 스토어 삭제 폼 구성 요소 추가

---
+++ src/components/common/StoreDeleteForm.js
@@ -0,0 +1,21 @@
+import { getStorage } from "../../utils/storage.js";
+import { StoreInfo } from "../StoreInfo.js";
+import { Button } from "./Button.js";
+import { FormButtons } from "./FormButtons.js";
+import { InputBox } from "./InputBox.js";
+import { SelectBox } from "./SelectBox.js";
+import { TextareaBox } from "./TextareaBox.js";
+
+export function StoreDeleteForm(lunchItemIndex) {
+  const lunchItem = getStorage("lunchItems")[lunchItemIndex];
+  function template() {
+    return `
+        <form id="storeDeleteForm" class="modal-form">
+          ${StoreInfo(lunchItem, "full")}
+          ${FormButtons("storeDelete")}
+        </form>
+    `;
+  }
+
+  return template();
+}
