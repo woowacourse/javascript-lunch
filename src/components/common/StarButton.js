3de70428d6137604d2fba369a586d41b1b48e2d9
dev-dino22 <wlsskdud02@naver.com>
2025-03-14 22:23:29 +0900

    feat: 스타 버튼 SVG 렌더링 방식 개선

--- src/components/common/StarButton.js
+++ src/components/common/StarButton.js
@@ -1,19 +1,13 @@
 export function StarButton(isSelected) {
-  const emptyStar = `
-        <svg width="28" height="26" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M27.3334 10.32L17.7467 9.49332L14 0.666656L10.2534 9.50666L0.666687 10.32L7.94669 16.6267L5.76002 26L14 21.0267L22.24 26L20.0667 16.6267L27.3334 10.32ZM14 18.5333L8.98669 21.56L10.32 15.8533L5.89335 12.0133L11.7334 11.5067L14 6.13332L16.28 11.52L22.12 12.0267L17.6934 15.8667L19.0267 21.5733L14 18.5333Z" fill="#EC4A0A" fill-opacity="0.5"/>
-        </svg>
-    `;
-  const fillStar = `
-        <svg width="28" height="26" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14 21.0267L22.24 26L20.0534 16.6267L27.3334 10.32L17.7467 9.50666L14 0.666656L10.2534 9.50666L0.666687 10.32L7.94669 16.6267L5.76002 26L14 21.0267Z" fill="#EC4A0A"/>
-        </svg>
-    `;
-
   return `
         <div>
             <button type="button">
-                ${isSelected === true ? fillStar : emptyStar}
+                <svg width="28" height="26" viewBox="0 0 28 26" fill=${
+                  isSelected === true ? "#EC4A0A" : "none"
+                } xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14 21.0267L22.24 26L20.0534 16.6267L27.3334 10.32L17.7467 9.50666L14 0.666656L10.2534 9.50666L0.666687 10.32L7.94669 16.6267L5.76002 26L14 21.0267Z" fill="none"/>
+                    <path d="M14.5168 20.1705L14 19.8586L13.4833 20.1705L7.27228 23.9192L8.92054 16.8538L9.05766 16.266L8.60146 15.8708L3.11285 11.116L10.3379 10.5031L10.9388 10.4521L11.1741 9.89688L14 3.22925L16.826 9.89688L17.0613 10.4521L17.6621 10.5031L24.8872 11.116L19.3986 15.8708L18.9424 16.266L19.0795 16.8538L20.7278 23.9192L14.5168 20.1705Z" stroke="#EC4A0A" stroke-opacity="0.5" stroke-width="2"/>
+                </svg>
             </button>
         </div>
     `;
