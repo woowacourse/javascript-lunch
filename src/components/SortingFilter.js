38c80338e49fff98f54cfe6683da5ee6a6a2d30c
dlsxjzld <sinjuk1@naver.com>
2025-03-14 20:02:15 +0900

    feat: 이름순,거리순 정렬 필터 컴포넌트 추가

---
+++ src/components/SortingFilter.js
@@ -0,0 +1,21 @@
+const sortingOptions = {
+  name: "이름",
+  distance: "거리",
+};
+
+const SortingFilter = () => {
+  const options = Object.entries(sortingOptions);
+
+  return /*html*/ `
+    <select name="sorting" id="sorting-filter" class="restaurant-filter">
+      ${options
+        .map(
+          ([option, text]) => ` <option value="${option}">${text}순</option>`
+        )
+        .join("")}
+
+    </select>
+  `;
+};
+
+export default SortingFilter;
