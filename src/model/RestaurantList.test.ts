911aaedfca841b418e495d64f858cbb90025cdd6
spoyodevelop <kjy9505@gmail.com>
2025-03-14 16:22:10 +0900

    test: 레스토랑을 삭제하는 유닛 테스트 추가

--- src/model/RestaurantList.test.ts
+++ src/model/RestaurantList.test.ts
@@ -32,19 +32,16 @@ describe("RestaurantList test", () => {
     restaurantList.addRestaurant(newRestaurant);
     expect(restaurantList.List).toEqual([...INITIAL_RESTAURANT, newRestaurant]);
   });
-  it("should throw error when adding duplicated restaurant", () => {
-    const newRestaurant = {
-      name: "이태리키친",
-      distance: 20,
-      description:
-        "정통 이탈리안 요리에 창의적인 변화를 더한 모던 다이닝 레스토랑.",
-      isFavorite: false,
-      category: "양식",
-      link: "https://italykitchen.co.kr",
-    };
+  it("should throw error when delete non-existing restaurant", () => {
     const restaurantList = new RestaurantList([...INITIAL_RESTAURANT]);
-    expect(() => restaurantList.addRestaurant(newRestaurant)).toThrow(
-      ERROR_MESSAGE.DUPLICATE_RESTAURANT
+    expect(() => restaurantList.deleteRestaurant("친친친")).toThrow(
+      ERROR_MESSAGE.NO_RESTAURANT_FOUND
+    );
+  });
+  it("should not throw error when delete existing restaurant", () => {
+    const restaurantList = new RestaurantList([...INITIAL_RESTAURANT]);
+    expect(() => restaurantList.deleteRestaurant("친친")).not.toThrow(
+      ERROR_MESSAGE.NO_RESTAURANT_FOUND
     );
   });
 });
