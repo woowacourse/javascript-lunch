be35a09e196485d2ae89126d2ee5cfa186ebfd42
장정안 <wjddks96@naver.com>
2025-03-14 15:18:33 +0900

    feat: 로컬 스토리지 키 상수화 처리

--- src/step2/utils/@common/localStorage.ts
+++ src/step2/utils/@common/localStorage.ts
@@ -1,12 +1,10 @@
+import { STORAGE_KEY } from '../../constants/key';
 import { ERROR_MESSAGE } from '../../constants/messages';
 import { RestaurantType } from '../../types/restaurants';
-
-const STORAGE_KEY = 'restaurant';
 
 export const saveStorage = (data: RestaurantType[]) => {
   try {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-    console.log('저장 완료');
   } catch (error) {
     console.error(ERROR_MESSAGE.SAVE_LOCAL_STORAGE);
   }
@@ -15,7 +13,6 @@ export const saveStorage = (data: RestaurantType[]) => {
 export const getStorage = (): RestaurantType[] | null => {
   try {
     const data = localStorage.getItem(STORAGE_KEY);
-    console.log('가져오기 완료');
     return data && JSON.parse(data);
   } catch (error) {
     console.error(ERROR_MESSAGE.GET_LOCAL_STORAGE);
