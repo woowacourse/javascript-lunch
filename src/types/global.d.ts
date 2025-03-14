caebaa0aac49b8f88154af4d286d8247c36e5613
hanheel <hanhee630630@gmail.com>
2025-03-15 05:02:26 +0900

    [refactor] : type 폴더 위치 이동

---
+++ src/types/global.d.ts
@@ -0,0 +1,21 @@
+export {};
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.svg";
+declare module "*.css";
+
+declare global {
+  type Category = "한식" | "중식" | "일식" | "양식" | "아시안" | "기타";
+  type Distance = "5분" | "10분" | "15분" | "20분" | "30분";
+
+  interface FoodItemType {
+    id: string;
+    category: Category;
+    name: string;
+    distance: Distance;
+    description: string;
+    isFavorite: boolean;
+    link: string;
+  }
+}
