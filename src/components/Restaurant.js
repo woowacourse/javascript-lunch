adb3f62fe980250d55d99d6b16e36f669c124ef4
ohgus <anakin1108@naver.com>
2025-03-14 22:35:45 +0900

    refactor: 레스토랑 컴포넌트 확장성 추가
    - 모달로 상세 정보를 띄울 때 추가 스타일 적용
    - 링크 추가

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,19 +1,33 @@
 import IMG_SRC from "../constants/imgSrc";
 
-const Restaurant = (restaurantProps) => {
+const Restaurant = (restaurantProps, isDetail = false) => {
   const imgSrc = getImgSrc(restaurantProps.category);
 
   return `
     <div class="restaurant__category">
-      <img src="${imgSrc}" alt=${restaurantProps.category} class="category-icon" />
+      <img src="${imgSrc}" alt=${
+    restaurantProps.category
+  } class="category-icon" />
     </div>
-    <div class="restaurant__info">
+    </div>
+    <div class="restaurant__info ${isDetail ? "restaurant__info_detail" : ""}">
       <h3 class="restaurant__name text-subtitle">${restaurantProps.name}</h3>
-      <span class="restaurant__distance text-body">캠퍼스부터 ${restaurantProps.dist}분 내</span>
-      <p class="restaurant__description text-body">
+      <span class="restaurant__distance text-body">캠퍼스부터 ${
+        restaurantProps.dist
+      }분 내</span>
+      <p class="restaurant__description text-body ${
+        isDetail ? "restaurant__description_detail" : ""
+      }">
         ${restaurantProps.description}
       </p>
-    </div>`;
+      ${
+        restaurantProps.link && isDetail
+          ? `<a href="${restaurantProps.link}" class="restaurant__link">${restaurantProps.link}</a>`
+          : ""
+      }
+    </div>
+    
+    `;
 };
 
 const getImgSrc = (category) => {
