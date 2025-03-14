3eb4e22ae629fab6ac1902d584ad084fa428b03a
dev-dino22 <wlsskdud02@naver.com>
2025-03-14 19:06:02 +0900

    feat: 타입 정의 개선 및 거리 범위 추가

--- src/type.ts
+++ src/type.ts
@@ -1,8 +1,12 @@
+type Category = "korean" | "chinese" | "japanese" | "western" | "asian" | "etc";
+type Distance = 5 | 10 | 15 | 20 | 30;
 export interface ILunchItem {
-  category: "한식" | "중식" | "일식" | "양식" | "아시안" | "기타";
+  category: Category;
   name: string;
-  distance: number;
+  distance: Distance;
   description?: string;
   link?: string;
   isFavorite: boolean;
 }
+
+export type HTMLTagName = keyof HTMLElementTagNameMap;
