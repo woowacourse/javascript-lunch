491be979d3e2df511f29703f48039fe868208bb6
guesung <gueit214@naver.com>
2025-03-14 15:25:54 +0900

    fix: CI

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,8 @@
 import { DEFAULT_RESTAURANT_LIST, LOCAL_STORAGE_KEY_MAP } from '../../src/lib/constants';
-import { ERROR_MESSAGES, LOCAL_HOST_URL } from './constants';
+import { ERROR_MESSAGES, PREVIEW_URL } from './constants';
 import { type LanguageType } from './types';
 
-const URL = LOCAL_HOST_URL; // TODO: 환경에 따라 다른 URL 사용
+const URL = PREVIEW_URL; // TODO: 환경에 따라 다른 URL 사용
 let language: LanguageType;
 
 describe('애플리케이션 테스트 케이스', () => {
