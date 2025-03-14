dd99ed6f9af3ddf20613dfd9fe30f615711cf44c
dev-dino22 <wlsskdud02@naver.com>
2025-03-14 11:07:25 +0900

    feat: StoreInfo 컴포넌트에 즐겨찾기 버튼 추가

--- src/components/StoreInfo.js
+++ src/components/StoreInfo.js
@@ -1,13 +1,37 @@
 import { createElement } from "../utils/utils.js";
+import { StarButton } from "./common/StarButton.js";
 
-export function StoreInfo({ name, distance, description, link, type }) {
+export function StoreInfo({
+  name,
+  distance,
+  description,
+  link,
+  type,
+  isFavorite,
+}) {
   function template() {
     return `
-            ${type === "summary" ? `<div class="restaurant__info">` : `<div class="restaurant__info__full">`}
+            ${
+              type === "summary"
+                ? `<div class="restaurant__info">`
+                : `<div class="restaurant__info__full">`
+            }
+            <div class="restaurant__title-box">
+              <div class="restaurant__title">
                 <h3 class="restaurant__name text-subtitle">${name}</h3>
                 <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
-                <p class="restaurant__description text-body">${description || "-"}</p>
-                ${link ? `<a href="${link}" target="_blank" class="restaurant__link">${link}</a>` : ""}
+              </div>
+              ${StarButton(isFavorite)}
+            </div>
+               
+                <p class="restaurant__description text-body">${
+                  description || "-"
+                }</p>
+                ${
+                  link
+                    ? `<a href="${link}" target="_blank" class="restaurant__link">${link}</a>`
+                    : ""
+                }
             </div>
     `;
   }
