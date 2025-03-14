9707e5c04ef9510375e5c4ff5ae98ac066fbbccc
youjeong Jeong <wjddbwjd1205@naver.com>
2025-03-14 17:22:56 +0900

    refactor: 음식점 아이템의 구조와 css 수정
    - 링크를 문자로 띄우기
    - 링크를 화면에서 안보이게
    - 별 컴포넌트로 전달할 매개변수 형태 수정

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -1,7 +1,11 @@
 import { IMAGE, FAV_STAR } from '../constants/constants.ts';
 import createFavoriteStar from './FavoriteStar.js'
 
-function createRestaurantItem({ category, name, distance, description, link }) {
+function createRestaurantItem({ category, name, distance, description, link }, isFavorite) {
+  const favoriteStarConfig = {
+    ...FAV_STAR,
+    inactiveSrc: isFavorite ? FAV_STAR.activeSrc : FAV_STAR.inactiveSrc,
+  };
   const item = `<li class="restaurant">
               <div class="restaurant__category">
                 <img src="${IMAGE.get(category)}" alt="${category}" class="category-icon" />
@@ -12,12 +16,12 @@ function createRestaurantItem({ category, name, distance, description, link }) {
                     <h3 class="restaurant__name text-subtitle">${name}</h3>
                     <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
                   </div>
-                  ${createFavoriteStar(FAV_STAR)}
+                  ${createFavoriteStar(favoriteStarConfig)}
                 </div>
                 <p class="restaurant__description text-body">
                   ${description}
-                </p>
-                <a href="${link}" class="restaurant__link"></a>
+                  </p>
+                  <a href="${link}" class="restaurant__link">${link}</a>
               </div>
             </li>`;
 
