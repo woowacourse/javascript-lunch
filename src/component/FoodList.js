c590eeec54d130f88a3fc7cd146abfe159ffc5b0
hanheel <hanhee630630@gmail.com>
2025-03-15 03:45:08 +0900

    [feat] : 음식점이 없을 경우 문구 출력

--- src/component/FoodList.js
+++ src/component/FoodList.js
@@ -21,6 +21,20 @@ export default class FoodList {
 
   render() {
     this.foodList.innerHTML = "";
+    if (this.#originFoodItems.length === 0) {
+      this.foodList.innerHTML = `
+      <p class="empty-message">음식점이 없습니다. 우측 상단 버튼을 눌러 추가해 주세요.</p>
+    `;
+
+      return;
+    }
+    if (this.#filteredFoodItems.length === 0) {
+      this.foodList.innerHTML = `
+        <p class="empty-message">즐겨찾기한 음식점이 없습니다.</p>
+      `;
+
+      return;
+    }
     const foodFragment = document.createDocumentFragment();
     this.#filteredFoodItems.forEach((foodItem) => {
       foodFragment.appendChild(
