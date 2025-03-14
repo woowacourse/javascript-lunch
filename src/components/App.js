2146861ecc25df2994153dd6a9bb5ff748d01c6a
deokbin <deokbin@naver.com>
2025-03-14 16:14:45 +0900

    ✨ feat: 카테고리 필터링 및 로컬스토리지에 데이터 추가하여 상태 업데이트

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,40 @@
 import Header from "./Header/Header.js";
-import createRestaurant from "./createRestaurant.js";
 import Component from "./Component.js";
 import Modal from "./Modal/Modal.js";
-import Restaurant from "./Restaurant/Restaurant.js";
-import { RestaurantData } from "../constants/RestaurantData.js";
-import getModalContent from "./Modal/getModalContent.js";
+import {
+  RestaurantData,
+  mappedRestaurantData,
+} from "../constants/RestaurantData.js";
+import {
+  addResturantContent,
+  restaurantInfoContent,
+} from "./Modal/getModalContent.js";
 import getRestaurant from "./Restaurant/Restaurant.js";
+import {
+  createCategoryFilter,
+  createSortingFilter,
+} from "./createFilterSelect.js";
+import renderRestaurants from "./renderRestaurants.js";
+import filterByCategory from "./filterRestaurants.js";
+import sortByOption from "./sortRestaurants.js";
 class App extends Component {
   constructor($target) {
     super($target);
     this.state = this.initState();
+    this.addNewRestaurant = this.addNewRestaurant.bind(this);
     document.addEventListener("restaurantUpdated", this.addNewRestaurant);
   }
 
   initState() {
-    return { isModalOpen: false };
+    localStorage.setItem("restaurantList", JSON.stringify(RestaurantData));
+
+    return {
+      isModalOpen: false,
+      restaurantList: [...RestaurantData],
+      selectedCategory: "전체",
+      sortOption: "name",
+      selectedRestaurant: null,
+    };
   }
 
   render() {
@@ -22,29 +42,57 @@ class App extends Component {
       toggleModal: () => this.toggleModal(),
     });
 
+    renderRestaurants(this.state.restaurantList, (restaurant) => {
+      this.toggleModal(restaurant);
+    });
+    createCategoryFilter((selectedCategory) => {
+      this.filterRestaurants(selectedCategory);
+    }, this.state.selectedCategory);
+    createSortingFilter((sortOption) => {
+      this.sortRestaurants(sortOption);
+    }, this.state.sortOption);
     new Modal(document.querySelector(".modal"), {
       isModalOpen: this.state.isModalOpen,
       toggleModal: () => this.toggleModal(),
-      content: getModalContent("restaurant"),
+      content: this.state.selectedRestaurant
+        ? restaurantInfoContent(this.state.selectedRestaurant) // ✅ 선택된 레스토랑 정보
+        : addResturantContent(),
     });
-
-    createRestaurant();
   }
 
-  toggleModal() {
-    this.setState({ isModalOpen: !this.state.isModalOpen });
+  toggleModal(restaurantData = null) {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+      restaurantList: this.state.restaurantList,
+      selectedRestaurant: restaurantData,
+    });
+  }
+
+  filterRestaurants(selectedCategory) {
+    const filtered = filterByCategory(RestaurantData, selectedCategory);
+    this.setState({
+      restaurantList: filtered,
+      selectedCategory,
+    });
+  }
+
+  sortRestaurants(sortOption) {
+    const sorted = sortByOption(this.state.restaurantList, sortOption);
+    this.setState({
+      restaurantList: sorted,
+      sortOption,
+    });
   }
 
   addNewRestaurant() {
-    const restaurantList = document.querySelector(".restaurant-list");
-    const newRestaurant = RestaurantData[RestaurantData.length - 1];
-    if (newRestaurant) {
-      const restaurantItem = document.createElement("li");
-      restaurantItem.classList.add("restaurant");
-      const restaurant = getRestaurant(newRestaurant);
-      restaurantItem.innerHTML = restaurant;
-      restaurantList.appendChild(restaurantItem);
-    }
+    const newRestaurantList = JSON.parse(
+      localStorage.getItem("restaurantList"),
+    );
+    this.setState({
+      restaurantList: newRestaurantList,
+      selectedCategory: this.state.selectedCategory,
+      sortOption: this.state.sortOption,
+    });
   }
 }
 export default App;
