29931bdf2b1b1cb13ad0bb8760f98b9f1745449a
keemsebeen <sebin0580@gmail.com>
2025-03-14 23:50:52 +0900

    feat: Layout 컴포넌트 스타일 수정

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -3,7 +3,7 @@ import Component from "../../core/Component.js";
 export default class Layout extends Component {
   template() {
     return `
-    <main class="max-w-390 w-full h-full flex flex-col justify-center items-center mx-16">
+    <main id="layout" class="max-w-390 w-full h-full flex flex-col justify-start items-center mx-16 bg-white">
     ${this.children.map((child) => child.template()).join("")}
     </main>`;
   }
