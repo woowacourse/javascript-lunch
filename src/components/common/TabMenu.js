be9324fc6183cf7196fe20f32618b5bc4158dc62
dev-dino22 <wlsskdud02@naver.com>
2025-03-14 14:42:49 +0900

    feat: TabMenu 컴포넌트의 버튼 타입을 수정하여 접근성 향상

--- src/components/common/TabMenu.js
+++ src/components/common/TabMenu.js
@@ -3,8 +3,8 @@ export function TabMenu() {
 <div class="tab-container">
   <div class="tab-menu">
     <!-- <div class="tab-menu-box"> -->
-        <button value="1" class="tab-item active" data-action="selectTab">모든 음식점</button>
-        <button value="2" class="tab-item" data-action="selectTab">자주 가는 음식점</button>
+        <button type="button" value="0" class="tab-item active" data-action="selectTab">모든 음식점</button>
+        <button type="button" value="1" class="tab-item" data-action="selectTab">자주 가는 음식점</button>
     </div>
     <div class="tab-indicator">
         <div class="tab-indicator-thumb"></div>
