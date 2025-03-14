4a034e787811c545967554fbe6dff2a42b3387fe
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-14 15:21:35 +0900

    refactor: TabBar template 태그 적용

--- src/components/TabBar/index.js
+++ src/components/TabBar/index.js
@@ -1,10 +1,13 @@
 const TabBar = () => {
-  return /*html*/ `
+  const template = document.createElement("template");
+  template.innerHTML = /* html */ `
     <section class="tab-bar">
       <button class="tab-bar__button active" id="list-tab" data-testid="list-tab">모든 음식점</button>
       <button class="tab-bar__button" id="favorite-tab" data-testid="favorite-tab">자주 가는 음식점</button>
     </section>
-  `;
+  `.trim();
+
+  return template.content.firstElementChild;
 };
 
 export default TabBar;
