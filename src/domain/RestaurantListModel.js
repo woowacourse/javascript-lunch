46735313c3faa721b4255bfd2b8c8329ae323a62
kimyou1102 <yoosk5485@naver.com>
2025-03-14 16:03:36 +0900

    refactor: RestaurantListModel의 초기 데이터를 로컬스토리지에서 불러오도록 변경
    - 기존 더미 데이터 대신 로컬스토리지 값을 사용하도록 수정

--- src/domain/RestaurantListModel.js
+++ src/domain/RestaurantListModel.js
@@ -1,26 +1,10 @@
+import { getItem, RESTAURANT_LIST_KEY } from "../components/utils/storage";
+
 export default class RestaurantListModel {
   #restaurantList;
 
   constructor() {
-    const dummy = [
-      {
-        category: "한식",
-        name: "얌샘김밥",
-        distance: 5,
-        description: "김밥집",
-        link: "http://localhost:5174/",
-        bookmark: false,
-      },
-      {
-        category: "중식",
-        name: "친친",
-        distance: 5,
-        description: "가지덮밥",
-        link: "http://localhost:5174/",
-        bookmark: true,
-      },
-    ];
-    this.#restaurantList = dummy;
+    this.#restaurantList = getItem(RESTAURANT_LIST_KEY, []);
   }
 
   updateRestautantList(newRestaurantList) {
