1c890dc5d44f79b1d458f55b7edf94860ab4cec9
Oliy <eunsoa0310@gmail.com>
2025-03-14 21:07:13 +0900

    refactor: Button을 TS로 마이그레이션

---
+++ src/UI/components/button/Button.ts
@@ -0,0 +1,28 @@
+type ButtonType = 'button' | 'submit' | 'reset';
+
+class Button {
+  #button: HTMLButtonElement;
+
+  constructor(type: ButtonType, className: string, buttonText: string, onClick: (event: MouseEvent) => void) {
+    this.#button = this.#createButton(type, className, buttonText);
+    this.#bindEvent(onClick);
+  }
+
+  #createButton(type: ButtonType, className: string, buttonText: string): HTMLButtonElement {
+    const button = document.createElement('button');
+    button.type = type;
+    button.classList.add('button', className);
+    button.textContent = buttonText;
+    return button;
+  }
+
+  #bindEvent(onClick: (event: MouseEvent) => void): void {
+    this.#button.addEventListener('click', onClick);
+  }
+
+  getElement(): HTMLButtonElement {
+    return this.#button;
+  }
+}
+
+export default Button;
