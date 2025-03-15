735dbec81934b19d9d19e8f087b74deca48a9cd8
hoyyChoi <hoyeon8165@gmail.com>
2025-03-15 23:57:04 +0900

    fix: detailItem 내부 요소들 css 변경

--- src/components/DetailItem.js
+++ src/components/DetailItem.js
@@ -12,7 +12,7 @@ function DetailItem({ category, name, distance, description, link, favoriteStar 
   divElement.classList.add("detail-restaurant");
 
   divElement.innerHTML = /*html*/ `
-  <div class="restaurant__category__star">
+  <div class="restaurant__category__star mb-16">
       <div class="restaurant__category">
         <img src=${CATRGORY_IMAGE_PATH[category]} alt=${category} class="category-icon" />
       </div>
@@ -22,12 +22,12 @@ function DetailItem({ category, name, distance, description, link, favoriteStar 
       >
       </div>
   </div>
-   <h3 class="restaurant__name text-title">${name}</h3>
-   <span class="restaurant__distance text-body">캠퍼스부터 ${distance}</span>
-   <p class="restaurant__description text-body">
+   <h3 class="restaurant__name text-title mb-16">${name}</h3>
+   <span class="restaurant__distance text-body mb-16">캠퍼스부터 ${distance}</span>
+   <p class="restaurant__description text-body mb-16">
      ${description}
    </p>
-   <a class="restaurant__link text-body"
+   <a class="restaurant__link text-body mb-16"
    href=${link} target="_blank">
      ${link}
    </a>
