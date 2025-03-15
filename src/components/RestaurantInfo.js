dde860382ff7df9f30324edaf2e07ed0a09d8bb4
Jeongeun Lee <leeju1009@gmail.com>
2025-03-15 16:46:00 +0900

    refactor: 자주 가는 음식점 추가 및 삭제 버튼의 css 조정
    - position을 absolute로 설정하여 불필요한 div 박스 제거

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -2,14 +2,12 @@ import { IMAGE } from '../constants/elements.js';
 
 function createRestaurantInfo({ id, category, name, distance, description, favorite }) {
   const information = `<div class="restaurant restaurant__body" data-id="${id}">
-              <div class="restaurant__header">
                 <div class="restaurant__category">
                   <img src="${IMAGE.get(category)}" alt="${category}" class="category-icon" />
                 </div>
                 <button type="button" class="button restaurant__favorite">
                   <img src="${favorite ? 'favorite-icon-filled.png' : 'favorite-icon-lined.png'}" alt="favorite-icon" />
                 </button>
-              </div>
               <div class="restaurant__info__details">
                 <h3 class="restaurant__name text-subtitle">${name}</h3>
                 <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -5,17 +5,13 @@ function createRestaurantItem({ id, category, name, distance, description, favor
               <div class="restaurant__category">
                 <img src="${IMAGE.get(category)}" alt="${category}" class="category-icon" />
               </div>
+              <button type="button" class="button restaurant__favorite">
+                <img src="${favorite ? 'favorite-icon-filled.png' : 'favorite-icon-lined.png'}" alt="favorite-icon" />
+              </button>
               <div class="restaurant__info">
-                <div class="restaurant__header">
-                  <div class="restaurant__details">
-                    <h3 class="restaurant__name text-subtitle">${name}</h3>
-                    <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
-                  </div>
-                  <button type="button" class="button restaurant__favorite">
-                    <img src="${
-                      favorite ? 'favorite-icon-filled.png' : 'favorite-icon-lined.png'
-                    }" alt="favorite-icon" />
-                  </button>
+                <div class="restaurant__details">
+                  <h3 class="restaurant__name text-subtitle">${name}</h3>
+                  <span class="restaurant__distance text-body">캠퍼스부터 ${distance}분 내</span>
                 </div>
                 <p class="restaurant__description text-body">${description}</p>
               </div>
