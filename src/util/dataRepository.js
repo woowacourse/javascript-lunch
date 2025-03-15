2023b9b8f559dfa2d44af35c2ebf0838fe330c73
yeongipark <yeongi0111@naver.com>
2025-03-15 21:21:45 +0900

    refactor: dataRepository 타입스크립트로 변경

--- src/util/dataRepository.js
+++
@@ -1,9 +0,0 @@
-const DATA_KEY = "restaurantData";
-
-export function getAllData() {
-  return JSON.parse(localStorage.getItem(DATA_KEY));
-}
-
-export function postData(data) {
-  localStorage.setItem(DATA_KEY, JSON.stringify(data));
-}
---
+++ src/util/dataRepository.ts
@@ -0,0 +1,11 @@
+import { IRestaurantDataProp } from "../../types/types";
+
+const DATA_KEY = "restaurantData";
+
+export function getAllData(): IRestaurantDataProp[] {
+  return JSON.parse(localStorage.getItem(DATA_KEY) || "");
+}
+
+export function postData(data: IRestaurantDataProp[]) {
+  localStorage.setItem(DATA_KEY, JSON.stringify(data));
+}
