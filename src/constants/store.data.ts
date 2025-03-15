51481ae3e34f28e5dc63f0723834c977b07800ff
keemsebeen <sebin0580@gmail.com>
2025-03-15 19:42:47 +0900

    feat: store mock data 정의

---
+++ src/constants/store.data.ts
@@ -0,0 +1,22 @@
+import { RestaurantItem } from "../types/restaurant.types";
+
+export const storeData: RestaurantItem[] = [
+  {
+    id: 1,
+    storeName: "한식당",
+    distance: 5,
+    category: "한식",
+    description: "맛있는 한식당",
+    link: "https://www.google.com",
+    isFavorite: false,
+  },
+  {
+    id: 2,
+    storeName: "양식당",
+    distance: 10,
+    category: "양식",
+    description: "파스타",
+    link: "https://www.google.com",
+    isFavorite: false,
+  },
+];
