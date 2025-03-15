7204200a1695e65f90a286c3b0643e1d0eba38c0
sooyeoniya <tndus1306403@naver.com>
2025-03-15 14:50:06 +0900

    chore: 바텀시트 닫는 테스트 코드 명세 수정

--- cypress/e2e/BottomSheetCloseAction.cy.js
+++ cypress/e2e/BottomSheetCloseAction.cy.js
@@ -3,7 +3,7 @@ describe("음식점 추가 바텀 시트 닫는 기능 테스트", () => {
     cy.initializeTestEnvironment();
   });
 
-  it("바텀 시트의 '취소하기' 버튼을 클릭했을 때 바텀 시트가 닫힌다.", () => {
+  it("음식점 추가 바텀 시트의 '취소하기' 버튼을 클릭했을 때 바텀 시트가 닫힌다.", () => {
     // when
     cy.openForm();
     cy.contains("button", "취소하기").should("exist").and("be.visible").click();
@@ -12,7 +12,7 @@ describe("음식점 추가 바텀 시트 닫는 기능 테스트", () => {
     cy.get("#submit-form").should("not.have.class", "modal--open");
   });
 
-  it("바텀 시트의 백드롭 화면을 클릭했을 때 바텀 시트가 닫힌다.", () => {
+  it("음식점 추가 바텀 시트의 백드롭 화면을 클릭했을 때 바텀 시트가 닫힌다.", () => {
     // when
     cy.openForm();
     cy.get("#submit-form .modal-backdrop").click({ force: true });
@@ -35,7 +35,7 @@ describe("음식점 상세 정보 바텀 시트 닫는 기능 테스트", () => 
     cy.initializeTestEnvironment();
   });
 
-  it("상세보기 바텀 시트의 '닫기' 버튼을 클릭했을 때 바텀 시트가 닫힌다.", () => {
+  it("음식점 상세 정보 바텀 시트의 '닫기' 버튼을 클릭했을 때 바텀 시트가 닫힌다.", () => {
     // when
     cy.openForm();
     cy.fillForm(formData);
@@ -48,7 +48,7 @@ describe("음식점 상세 정보 바텀 시트 닫는 기능 테스트", () => 
     cy.get("#open-detail").should("not.have.class", "modal--open");
   });
 
-  it("상세보기 바텀 시트의 백드롭 화면을 클릭했을 때 바텀 시트가 닫힌다.", () => {
+  it("음식점 상세 정보 바텀 시트의 백드롭 화면을 클릭했을 때 바텀 시트가 닫힌다.", () => {
     // when
     cy.openForm();
     cy.fillForm(formData);
