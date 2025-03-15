521ba6b8a2d79a1cb5e909b34711a04f4b76762d
yeongipark <yeongi0111@naver.com>
2025-03-15 22:54:31 +0900

    refactor: 에러 메시지 클래스 외부로 분리

--- src/domain/RestaurantData.ts
+++ src/domain/RestaurantData.ts
@@ -1,23 +1,23 @@
 import { IRestaurantDataProp, CategoryType } from "../../types/types";
 
-const CATEGORY_IMAGE = Object.freeze({
+const CATEGORY_IMAGE = {
   한식: "./category-korean.png",
   중식: "./category-chinese.png",
   일식: "./category-japanese.png",
   양식: "./category-western.png",
   아시안: "./category-asian.png",
   기타: "./category-etc.png",
-});
+} as const;
+
+export const ERROR_MESSAGE = {
+  category: "카테고리를 선택해 주세요",
+  distance: "거리를 선택해 주세요",
+  nameLength: "식당 이름은 2글자 이상 입력해 주세요",
+  nameChar: "올바른 식당 이름을 입력해 주세요.",
+  link: "올바른 링크 주소를 입력해 주세요.",
+} as const;
 
 export default class RestaurantData {
-  static ERROR_MESSAGE = Object.freeze({
-    category: "카테고리를 선택해 주세요",
-    distance: "거리를 선택해 주세요",
-    nameLength: "식당 이름은 2글자 이상 입력해 주세요",
-    nameChar: "올바른 식당 이름을 입력해 주세요.",
-    link: "올바른 링크 주소를 입력해 주세요.",
-  });
-
   private id: number | string;
   private src: string;
   private alt: string;
@@ -86,25 +86,25 @@ export default class RestaurantData {
   }
 
   private validateCategory(category: CategoryType): void {
-    if (!category) throw Error(RestaurantData.ERROR_MESSAGE.category);
+    if (!category) throw Error(ERROR_MESSAGE.category);
   }
 
   private validateDistance(distance: number): void {
-    if (!distance) throw Error(RestaurantData.ERROR_MESSAGE.distance);
+    if (!distance) throw Error(ERROR_MESSAGE.distance);
   }
 
   private validateName(name: string): void {
     if (this.isValidateNameLength(name)) {
-      throw Error(RestaurantData.ERROR_MESSAGE.nameLength);
+      throw Error(ERROR_MESSAGE.nameLength);
     }
     if (this.hasInvalidNameChar(name)) {
-      throw Error(RestaurantData.ERROR_MESSAGE.nameChar);
+      throw Error(ERROR_MESSAGE.nameChar);
     }
   }
 
   private validateLink(link: string): void {
     if (this.isValidateLink(link)) {
-      throw Error(RestaurantData.ERROR_MESSAGE.link);
+      throw Error(ERROR_MESSAGE.link);
     }
   }
 }
