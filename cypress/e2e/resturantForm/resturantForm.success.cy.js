013a2f1e75f7e5c2637ceb1aa7d6a5bcca716c90
spoyodevelop <kjy9505@gmail.com>
2025-03-15 14:14:48 +0900

    test: 음식점 추가 2개를 했을때, 자주 찾는 음식점이 토글 되어있으면, 음식점 리스트에 보이지 않아야 함.

--- cypress/e2e/resturantForm/resturantForm.success.cy.js
+++ cypress/e2e/resturantForm/resturantForm.success.cy.js
@@ -66,4 +66,38 @@ describe("음식점 추가가 잘 되는지 확인하는 시나리오", () => {
       .should("be.visible")
       .should("contain", "잇쇼이 음식점을 추가했습니다.");
   });
+  it("모달 열기 테스트, 음식점 추가 2개를 했을때, 자주 찾는 음식점이 토글 되어있으면, 음식점 리스트에 보이지 않아야 함.", () => {
+    cy.get('[for="filter-favorite"]').click();
+    // 모달 띄우기
+    cy.get(".gnb__button").click();
+    cy.get(".modal").should("be.visible");
+    cy.get(".restaurant-add-form").should("exist");
+
+    // 폼 데이터 입력
+    cy.get("#category").select("양식");
+    cy.get("#name").type("타코집");
+    cy.get("#distance").select("10");
+
+    // 폼 제출
+    cy.get(".restaurant-add-form").submit();
+    cy.get(".restaurant").should("have.length", 0);
+
+    // 모달 띄우기
+    cy.get(".gnb__button").click();
+    cy.get(".modal").should("be.visible");
+    cy.get(".restaurant-add-form").should("exist");
+
+    // 폼 데이터 입력
+    cy.get("#category").select("일식");
+    cy.get("#name").type("잇쇼이");
+    cy.get("#distance").select("15");
+
+    // 폼 제출
+    cy.get(".restaurant-add-form").submit();
+
+    cy.get(".restaurant").should("have.length", 0);
+    cy.get(".toast")
+      .should("be.visible")
+      .should("contain", "잇쇼이 음식점을 추가했습니다.");
+  });
 });
