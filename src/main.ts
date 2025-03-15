618917c53006ecb80bc0207a432d933456f6ddd7
ohgus <anakin1108@naver.com>
2025-03-15 17:00:54 +0900

    feat: 정렬 및 필터 구현

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,69 @@ import restaurantData from "./data/restaurantData.js";
 import querySelector from "./utils/querySelector.js";
 import { modalHandler } from "./handlers/modalHandler.ts";
 import { restaurantHandler } from "./handlers/restaurantHandler.ts";
+import { filterAndSortHandler } from "./handlers/filterAndSortHandler.ts";
 import { RestaurantItem } from "./types/restaurantItem.js";
+import { FilterAndSortOptions } from "./types/filterAndSortOptions.js";
 
 addEventListener("load", () => {
   const restaurantList = new RestaurantList(restaurantData);
-  restaurantList.list.forEach((restaurant: RestaurantItem) => {
-    restaurantHandler.addRestaurantItem(restaurant);
-  });
+  const categoryFilter = querySelector("#category-filter");
+  const sortingFilter = querySelector("#sorting-filter");
+
+  const renderRestaurantList = (
+    restaurants: RestaurantItem[],
+    category: FilterAndSortOptions["category"],
+    sortOption: FilterAndSortOptions["sortOption"]
+  ) => {
+    const filteredList = filterAndSortHandler.filterByCategory(
+      restaurants,
+      category
+    );
+    const sortedList = filterAndSortHandler.sortByOption(
+      filteredList,
+      sortOption
+    );
+
+    sortedList.forEach((restaurant: RestaurantItem) => {
+      restaurantHandler.addRestaurantItem(restaurant);
+    });
+  };
+
+  renderRestaurantList(
+    restaurantList.list,
+    categoryFilter.value as FilterAndSortOptions["category"],
+    sortingFilter.value as FilterAndSortOptions["sortOption"]
+  );
+
+  querySelector(".restaurant-filter-container").addEventListener(
+    "change",
+    (e: Event) => {
+      if (e.target instanceof HTMLSelectElement) {
+        const categoryFilter = querySelector("#category-filter");
+        const sortingFilter = querySelector("#sorting-filter");
+
+        updateRestaurantList(
+          restaurantList.list,
+          categoryFilter.value,
+          sortingFilter.value
+        );
+      }
+    }
+  );
+
+  const updateRestaurantList = (
+    restaurants: RestaurantItem[],
+    category: FilterAndSortOptions["category"],
+    sortOption: FilterAndSortOptions["sortOption"]
+  ) => {
+    const restaurantListElement = querySelector(
+      ".restaurant-list"
+    ) as HTMLUListElement;
+    restaurantListElement.innerHTML = "";
+
+    renderRestaurantList(restaurants, category, sortOption);
+  };
+
   const modal = Modal();
   querySelector("main").appendChild(modal);
 
---
+++ src/types/filterAndSortOptions.ts
@@ -0,0 +1,4 @@
+export type FilterAndSortOptions = {
+  category: "전체" | "한식" | "중식" | "일식" | "양식" | "아시안" | "기타";
+  sortOption: "name" | "distance";
+};
