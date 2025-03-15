3c418fa4390addb97ebf13cb4b3060dd0d0bbda9
deokbin <deokbin@naver.com>
2025-03-15 23:42:23 +0900

    ♻️ refactor: 필터링 후 원본 배열 반환하도록 변경

--- src/components/filterRestaurants.ts
+++ src/components/filterRestaurants.ts
@@ -2,7 +2,7 @@ import { Restaurant } from "../../types/global";
 
 const filterByCategory = (restaurants: Restaurant[], category: string) => {
   if (category === "전체") {
-    return [...restaurants];
+    return restaurants;
   }
   return restaurants.filter((restaurant) => restaurant.category === category);
 };
