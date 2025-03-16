9f135523c38300f5ca7539095afcae608e36fe4f
dev-dino22 <wlsskdud02@naver.com>
2025-03-17 00:35:46 +0900

    chore: StoreInfo 컴포넌트에 index 프로퍼티 추가 및 템플릿 수정

--- src/components/StoreInfo.ts
+++ src/components/StoreInfo.ts
@@ -1,20 +1,29 @@
 import { StarButton } from "./common/StarButton.js";
 import type { ILunchItem } from "../type.ts";
+import { CategoryIcon } from "./common/CategoryIcon.js";
 
 interface StoreInfoProps extends ILunchItem {
   type: string;
+  index: string;
 }
 
-export function StoreInfo(
-  { name, distance, description, link, isFavorite }: StoreInfoProps,
-  type = "summary"
-) {
+export function StoreInfo({
+  category,
+  name,
+  distance,
+  description,
+  link,
+  isFavorite,
+  type,
+  index,
+}: StoreInfoProps) {
   function template() {
     return `
+        ${CategoryIcon(category)}
             ${
               type === "summary"
                 ? `<div class="restaurant__info">`
-                : `<div class="restaurant__info full">`
+                : `<div class="restaurant__info full" data-index="${index}">`
             }
             <div class="restaurant__title-box">
               <div class="restaurant__title">
