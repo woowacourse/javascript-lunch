63c2f08cd242160a3664a52316b75b2c2e9ccb4b
guesung <gueit214@naver.com>
2025-03-16 15:16:54 +0900

    fix: 잘못된 타입

--- src/components/InputBox.ts
+++ src/components/InputBox.ts
@@ -4,16 +4,16 @@ import { html } from '../lib/utils.ts';
 interface InputBoxProps {
   label: string;
   input: string;
+  labelId: string;
   caption?: string;
   isRequired?: boolean;
-  labelId?: string;
 }
 
-export default class InputBox extends Component<InputBoxProps> {
+export default class InputBox extends Component<null, InputBoxProps> {
   override template() {
     return html`
       <div class="form-item ${this.props?.isRequired ? 'form-item--required' : ''}">
-        <label for="${this.props?.labelId}" class="text-caption">${this.props?.label ?? ''}</label>
+        <label for="${this.props.labelId}" class="text-caption">${this.props?.label ?? ''}</label>
         ${this.props?.input ?? ''}
         <span class="help-text text-caption">${this.props?.caption ?? ''}</span>
       </div>
