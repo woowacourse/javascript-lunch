153c79174f1a484e379be8f95851d5790078e414
jin123457 <jin_ox@naver.com>
2025-03-16 23:56:10 +0900

    refactor: 즐겨찾기 아이콘 기능 추가 및 관련 코드 정리

--- src/components/layout/tabContainer.js
+++ src/components/layout/tabContainer.js
@@ -1,6 +1,51 @@
+import { STORAGE_KEY_NAME } from "../../constants/storage";
+import { storageHandler } from "../../utils/storageHandler";
+import $restaurantItem from "../restaurant/restaurant-item";
+
+const activeTabEvent = (id) => {
+  const currentActiveTab = document.querySelector(".select-tab-active");
+  currentActiveTab.classList.remove("select-tab-active");
+  const currentClickTab = document.getElementById(id);
+  currentClickTab.classList.add("select-tab-active");
+};
+
+const toggleTabClick = (e) => {
+  activeTabEvent(e.target.id);
+  const restaurantList = document.querySelector(".restaurant-list");
+  restaurantList.replaceChildren();
+
+  const categoryFilter = document.getElementById("category-filter").value;
+  const sortFilter = document.getElementById("sorting-filter").value;
+  let restaurantItems;
+  if (e.target.id === "favorite") {
+    restaurantItems = storageHandler.findFavoriteItem(
+      STORAGE_KEY_NAME,
+      categoryFilter,
+      sortFilter
+    );
+  } else {
+    restaurantItems = storageHandler.filterItem(
+      STORAGE_KEY_NAME,
+      categoryFilter,
+      sortFilter
+    );
+  }
+  if (restaurantItems.length > 0) {
+    return restaurantItems.forEach((item) => {
+      restaurantList.appendChild($restaurantItem(item));
+    });
+  }
+  const noRestaurantBox = document.createElement("div");
+  const noRestaurant = document.createElement("p");
+  noRestaurant.id = "noRestaurant";
+  noRestaurant.textContent = "등록된 음식점이 없습니다.";
+  restaurantList.appendChild(noRestaurant);
+};
+
 const $tabContainer = (tabs) => {
   const container = document.createElement("nav");
   container.classList.add("tab-container");
+  container.addEventListener("click", toggleTabClick);
 
   tabs.forEach((tab) => {
     container.appendChild(tab);
--- src/components/modal/createRestaurantInfo.js
+++ src/components/modal/createRestaurantInfo.js
@@ -4,66 +4,7 @@ import { UI_CONFIG } from "../../constants/uiConfig.js";
 import { handleModalClose, handleModalOpen } from "./modal.js";
 import { storageHandler } from "../../utils/storageHandler.js";
 import { STORAGE_KEY_NAME } from "../../constants/storage.js";
-
-const $createFavoriteIcon = (isFavorite) => {
-  const favoriteIcon = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "svg"
-  );
-  favoriteIcon.classList.add("favorite-icon");
-  favoriteIcon.tabindex = 0;
-  favoriteIcon.role = "button";
-  favoriteIcon.setAttribute("fill", "#EC4A0A");
-  const favoriteIconStroke = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "path"
-  );
-  favoriteIconStroke.setAttribute(
-    "d",
-    `M14 21.0267L22.24 26L20.0534 16.6267L27.3334 10.32L17.7467 9.50666L14 0.666656L10.2534 9.50666L0.666687 10.32L7.94669 16.6267L5.76002 26L14 21.0267Z`
-  );
-  favoriteIconStroke.setAttribute("fill", `${isFavorite ? "#EC4A0A" : "none"}`);
-
-  const favoriteIconPath = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "path"
-  );
-  favoriteIconPath.setAttribute(
-    "d",
-    `M27.3334 10.32L17.7467 9.49332L14 0.666656L10.2534 9.50666L0.666687 10.32L7.94669 16.6267L5.76002 26L14 21.0267L22.24 26L20.0667 16.6267L27.3334 10.32ZM14 18.5333L8.98669 21.56L10.32 15.8533L5.89335 12.0133L11.7334 11.5067L14 6.13332L16.28 11.52L22.12 12.0267L17.6934 15.8667L19.0267 21.5733L14 18.5333Z`
-  );
-  favoriteIconPath.setAttribute("stroke", "#EC4A0A");
-  favoriteIconPath.setAttribute("stroke-opacity", "0.5");
-  favoriteIconPath.setAttribute("stroke-width", "1");
-  favoriteIcon.appendChild(favoriteIconStroke);
-  favoriteIcon.appendChild(favoriteIconPath);
-
-  return favoriteIcon;
-};
-
-const $updateFavoriteIcon = (restaurantInfo) => {
-  const favoriteState = storageHandler.updateFavorite(
-    STORAGE_KEY_NAME,
-    restaurantInfo
-  );
-
-  const favoriteIcon = document.querySelector(
-    `[data-id="${restaurantInfo.id}"] > .favorite-icon > path:first-of-type`
-  );
-
-  const modalFavoriteIcon = document.querySelector(
-    `.modal-container > .favorite-icon > path:first-of-type`
-  );
-
-  if (favoriteState) {
-    favoriteIcon.setAttribute("fill", "none");
-    modalFavoriteIcon.setAttribute("fill", "none");
-    return;
-  }
-
-  favoriteIcon.setAttribute("fill", "#EC4A0A");
-  modalFavoriteIcon.setAttribute("fill", "#EC4A0A");
-};
+import { $favoriteIcon, updateFavoriteIcon } from "../common/favoriteIcon.js";
 
 const $createRestaurantInfo = (restaurantInfo) => {
   const {
@@ -116,11 +57,11 @@ const $createRestaurantInfo = (restaurantInfo) => {
     container.appendChild(InfoLink);
   }
 
-  const favoriteIcon = $createFavoriteIcon(isFavorite);
+  const favoriteIcon = $favoriteIcon(isFavorite);
   favoriteIcon.addEventListener("click", (e) => {
     e.stopPropagation();
 
-    $updateFavoriteIcon(restaurantInfo);
+    updateFavoriteIcon(restaurantInfo, e);
   });
   container.appendChild(favoriteIcon);
 
