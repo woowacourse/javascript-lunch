e2f1050c7c4a5a5d4ab3786a20e594f10dbd4f54
hoyyChoi <hoyeon8165@gmail.com>
2025-03-17 00:32:10 +0900

    fix: category Image path 수정

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -7,10 +7,10 @@ export const SELECT_CATEGORY: string[] = ["전체", "한식", "중식", "일식"
 export const SELECT_SORTING: string[] = ["이름순", "거리순"];
 
 export const CATRGORY_IMAGE_PATH: { [key: string]: string } = {
-  한식: "/category-korean.png",
-  중식: "/category-chinese.png",
-  일식: "/category-japanese.png",
-  양식: "/category-western.png",
-  아시안: "/category-asian.png",
-  기타: "/category-etc.png",
+  한식: "./category-korean.png",
+  중식: "./category-chinese.png",
+  일식: "./category-japanese.png",
+  양식: "./category-western.png",
+  아시안: "./category-asian.png",
+  기타: "./category-etc.png",
 };
