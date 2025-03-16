41518b8376a25050703202aeb0e5f8aca1c4f0f5
keemsebeen <sebin0580@gmail.com>
2025-03-16 22:55:11 +0900

    chore: 이미지 경로, 폰트 경로 수정

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -14,14 +14,18 @@ export default class Icon extends Component {
 
   template() {
     const { size, iconName, classList, styles, id } = this.props;
+    const baseURL = window.location.origin.includes("github.io")
+      ? "/javascript-lunch"
+      : "";
+
     return `
       <img
         id="${id}"
         width="${size}"
         height="${size}"
         alt="${iconName}"
-        src="/${iconName}.png"
-        class=" ${classList.join(" ")} cursor-pointer" 
+        src="${baseURL}/${iconName}.png"
+        class=" ${classList.join(" ")}cursor-pointer" 
         style="${styleStr(styles)}"
       />
       `;
