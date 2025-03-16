1dfa1dadb61683a41f081f681bec70deb459d828
jaeyoung-kwon <61729032+jaeyoung-kwon@users.noreply.github.com>
2025-03-17 01:19:00 +0900

    fix: 테스트 오류 해결

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    viewportHeight: 1024,
+    viewportWidth: 768,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
