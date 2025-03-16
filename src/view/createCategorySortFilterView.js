43291a42941322f4001dceac857e6c0d067ac312
hoyyChoi <hoyeon8165@gmail.com>
2025-03-16 23:09:57 +0900

    chore: 필요없는 매개변수 삭제

--- src/view/createCategorySortFilterView.js
+++ src/view/createCategorySortFilterView.js
@@ -1,7 +1,7 @@
 import CategorySortFilterWrapper from "../components/CategoryFilterWrapper.js";
 import CATEGORY_SORT_FILTER_DATA from "../constants/categorySortFilterData.js";
 
-function createCategorySortFilterView(containerElement) {
+function createCategorySortFilterView() {
   const categorySortFilterContainerElement = CategorySortFilterWrapper(CATEGORY_SORT_FILTER_DATA);
 
   return categorySortFilterContainerElement;
