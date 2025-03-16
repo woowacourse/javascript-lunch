70aae3b114689f687a237b2c0689504cd11349f2
hoyyChoi <hoyeon8165@gmail.com>
2025-03-16 20:19:55 +0900

    refactor: buttonData 네이밍 변경

--- src/view/createDetailModalView.js
+++ src/view/createDetailModalView.js
@@ -3,14 +3,14 @@ import DetailItem from "../components/DetailItem.js";
 import ButtonsForm from "../components/Form/ButtonsForm.js";
 import Modal from "../components/Modal.js";
 
-const buttons = [
+const DETAIL_MODAL_BUTTONS = [
   { type: "submit", stylingBased: "secondary", text: "삭제하기" },
   { type: "button", stylingBased: "primary", text: "닫기" },
 ];
 
 function createDetailModalView(restaurant) {
   const detailItemElement = DetailItem(restaurant.restaurant);
-  const formButtons = buttons.map((buttonData) => Button(buttonData));
+  const formButtons = DETAIL_MODAL_BUTTONS.map((buttonData) => Button(buttonData));
   const buttonsFormElement = ButtonsForm(formButtons);
   const modalElement = Modal([detailItemElement, buttonsFormElement]);
 
