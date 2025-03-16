ac2389a5099b9b19171c676ffab67a4bf62f3a05
deokbin <deokbin@naver.com>
2025-03-16 22:05:56 +0900

    ✨ feat: 좋아하는 음식점 리스트 상태를 iconButton에서 가지고 있는 방식으로 변경

--- src/components/Button/IconButton.js
+++ src/components/Button/IconButton.js
@@ -1,32 +1,62 @@
 import Component from "../Component.js";
-
 class IconButton extends Component {
   initState() {
-    return { isButtonClicked: false };
+    const savedFavorites = JSON.parse(
+      localStorage.getItem("favoriteRestaurantList") || "[]",
+    );
+
+    return {
+      isButtonClicked: savedFavorites.includes(this.props.restaurantId),
+    };
   }
   template() {
+    const imgSrc = this.state.isButtonClicked
+      ? "./favorite-icon-filled.png"
+      : "./favorite-icon-lined.png";
+    const imgAlt = this.state.isButtonClicked ? "filled-star" : "empty-star";
+
     return `<button class="favorite_add_button">
-        <img src="./favorite-icon-lined.png" alt="empty-star" class="favorite-icon"/>
-      </button>`;
+    <img src="${imgSrc}" alt="${imgAlt}" class="favorite-icon"/>
+  </button>`;
   }
 
   setEvent() {
     const favoriteButton = this.$target.querySelector(".favorite_add_button");
     if (favoriteButton) {
-      favoriteButton.addEventListener("click", () => {
-        const img = favoriteButton.querySelector("img");
-        if (img.alt === "empty-star") {
-          img.src = "./favorite-icon-filled.png";
-          img.alt = "filled-star";
+      favoriteButton.addEventListener("click", (e) => {
+        e.stopPropagation();
+
+        const newState = !this.state.isButtonClicked;
+        this.setState({ isButtonClicked: newState });
+        const savedFavorites = JSON.parse(
+          localStorage.getItem("favoriteRestaurantList") || "[]",
+        );
+        let updatedFavorites = [...savedFavorites];
+
+        if (newState) {
+          if (!updatedFavorites.includes(this.props.restaurantId)) {
+            updatedFavorites.push(this.props.restaurantId);
+          }
         } else {
-          img.src = "./favorite-icon-lined.png";
-          img.alt = "empty-star";
+          updatedFavorites = updatedFavorites.filter(
+            (id) => id !== this.props.restaurantId,
+          );
         }
+
+        localStorage.setItem(
+          "favoriteRestaurantList",
+          JSON.stringify(updatedFavorites),
+        );
+
+        document.dispatchEvent(
+          new CustomEvent("favoriteUpdated", {
+            detail: {
+              favoriteRestaurants: updatedFavorites,
+            },
+          }),
+        );
       });
     }
-  }
-  render() {
-    return this.template();
   }
 }
 
