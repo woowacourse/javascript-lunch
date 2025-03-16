22e40e100baad8c8c1df5d87aa7528be310918bf
H0ngJu <hongju4385@gmail.com>
2025-03-16 21:45:17 +0900

    chore: img 경로 수정

--- src/components/Restaurant/RestaurantIcon.js
+++ src/components/Restaurant/RestaurantIcon.js
@@ -18,7 +18,7 @@ class RestaurantIcon {
   };
 
   #getImageSrc = (category) => {
-    const url = `https://h0ngju.github.io/javascript-lunch/public/assets/category-`;
+    const url = `https://h0ngju.github.io/javascript-lunch/assets/category-`;
     const key = CATEGORY[category] ? category : 'etc';
     return `${url}${key}.png`;
   };
