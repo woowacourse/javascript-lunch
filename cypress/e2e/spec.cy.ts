25b800fd7964cfdde9623bfc3e17dcd961c1749f
guesung <gueit214@naver.com>
2025-03-16 23:10:02 +0900

    fix: E2E 테스트

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -15,25 +15,25 @@ describe('애플리케이션 테스트 케이스', () => {
   describe('정상적인 경우', () => {
     describe('카테고리별로 필터링해서 확인할 수 있다.', () => {
       it('카테고리에서 한식을 필터링하면 한식만 확인할 수 있다.', () => {
-        cy.get('.restaurant-filter-sort > :nth-child(1) > select').select('한식');
+        cy.get('[name="filter"]').select('한식');
 
         cy.contains('피양콩할마니').should('exist');
         cy.contains('친친').should('not.exist');
       });
       it('카테고리에서 일식을 필터링하면 일식만 확인할 수 있다.', () => {
-        cy.get('.restaurant-filter-sort > :nth-child(1) > select').select('일식');
+        cy.get('[name="filter"]').select('일식');
 
         cy.contains('잇쇼우').should('exist');
         cy.contains('피양콩할마니').should('not.exist');
       });
       it('카테고리에서 중식을 필터링하면 중식만 확인할 수 있다.', () => {
-        cy.get('.restaurant-filter-sort > :nth-child(1) > select').select('중식');
+        cy.get('[name="filter"]').select('중식');
 
         cy.contains('친친').should('exist');
         cy.contains('피양콩할마니').should('not.exist');
       });
       it('카테고리에서 양식을 필터링하면 양식만 확인할 수 있다.', () => {
-        cy.get('.restaurant-filter-sort > :nth-child(1) > select').select('양식');
+        cy.get('[name="filter"]').select('양식');
 
         cy.contains('이태리키친').should('exist');
         cy.contains('피양콩할마니').should('not.exist');
@@ -42,12 +42,12 @@ describe('애플리케이션 테스트 케이스', () => {
 
     describe('이름순/거리순으로 정렬해서 확인할 수 있다.', () => {
       it('거리순으로 정렬하면 친친 식당이 첫 번째에 위치한다.', () => {
-        cy.get(':nth-child(2) > select').select('거리순');
+        cy.get('[name="sort"]').select('거리순');
 
         cy.get(':first-child > .restaurant').contains('친친');
       });
       it('이름순으로 정렬하면 "각" 식당이 첫 번째에, "힣" 식당이 마지막에 위치한다.', () => {
-        cy.get(':nth-child(2) > select').select('이름순');
+        cy.get('[name="sort"]').select('이름순');
 
         cy.addRestaurant({ name: '각', category: '일식', distance: '20' });
         cy.addRestaurant({ name: '힣', category: '일식', distance: '10' });
@@ -59,9 +59,10 @@ describe('애플리케이션 테스트 케이스', () => {
   });
 
   describe('음식점 상세 정보를 확인할 수 있다.', () => {
+    beforeEach(() => {
+      cy.get('[data-id="6"][data-action="restaurant-detail"]').click();
+    });
     it('카테고리, 이름, 거리, 설명, 참고 링크를 확인할 수 있다.', () => {
-      cy.get(':first-child > .restaurant').click();
-
       cy.get('.restaurant-detail-modal img[alt="기타"]').should('exist');
       cy.get('.restaurant-detail-modal').contains('도스타코스 선릉점').should('exist');
       cy.get('.restaurant-detail-modal').contains('캠퍼스부터 5분 내').should('exist');
@@ -69,22 +70,22 @@ describe('애플리케이션 테스트 케이스', () => {
       cy.get('.restaurant-detail-modal').contains('https://naver.me/G6DyD9tg').should('exist');
     });
     it('음식점을 삭제할 수 있다.', () => {
-      cy.get(':first-child > .restaurant').click();
       cy.get('.restaurant-detail-modal').contains('삭제하기').click();
 
       cy.get('.restaurant-list').contains('도스타코스 선릉점').should('not.exist');
     });
   });
+
   describe('자주 가는 음식점을 추가하고 목록으로 확인할 수 있다.', () => {
     it('음식점 목록에서 자주 가는 음식점을 추가할 수 있다.', () => {
-      cy.get(':first-child > .restaurant [data-action="restaurant-like"]').click();
+      cy.get('[data-id="6"][data-action="restaurant-detail"] [data-action="restaurant-like"]').click();
+
       cy.get('[data-tab="like"]').click();
-      cy.get(':first-child > .restaurant > .restaurant__info > .restaurant__info--inner').click();
 
       cy.get('.restaurant-list').contains('도스타코스 선릉점').should('exist');
     });
     it('음식점 상세 정보에서 자주 가는 음식점으로 추가할 수 있다.', () => {
-      cy.get(':first-child > .restaurant').click();
+      cy.get('[data-id="6"][data-action="restaurant-detail"]').click();
       cy.get('.restaurant-detail-modal [data-action="restaurant-like"]').click();
       cy.get('.modal-backdrop').click();
       cy.get('[data-tab="like"]').click();
