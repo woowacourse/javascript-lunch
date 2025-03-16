79f0d5805c8ec3c69ed99e27325de1870cb9d7df
ShinjungOh <tlswjd9260@gmail.com>
2025-03-16 19:14:24 +0900

    refactor: Header 태그를 html 파일로 이동

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -3,19 +3,18 @@ type HeaderProps = {
 };
 
 const createHeader = ({ title }: HeaderProps) => {
-  const header = document.createElement("header");
-  header.classList.add("gnb");
+  const header = document.querySelector("header");
 
   const h1 = document.createElement("h1");
   h1.classList.add("gnb__title", "text-title");
   h1.textContent = title;
-  header.appendChild(h1);
+  header?.appendChild(h1);
 
   const button = document.createElement("button");
   button.type = "button";
   button.classList.add("gnb__button");
   button.innerHTML = `<img src="images/add-button.png" alt="음식점 추가" />`;
-  header.appendChild(button);
+  header?.appendChild(button);
 
   return header;
 };
