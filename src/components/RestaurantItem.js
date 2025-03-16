653f3298c01e0a81c890837170174203ee81b5ee
ha-kuku <hakukudh@gmail.com>
2025-03-16 23:23:50 +0900

    feat: 별 클릭 기능 추가

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -11,20 +11,31 @@ function createTags(data) {
     'restaurant__distance text-body',
     `캠퍼스부터 ${data.distance}분 내`
   );
+
+  const starImg = createElement('div', 'restaurant__star');
+
   const descriptionPara = createElement('p', 'restaurant__description text-body', data.description);
 
-  return { categoryImg, nameHeading, distanceSpan, descriptionPara };
+  return { categoryImg, nameHeading, distanceSpan, starImg, descriptionPara };
 }
 
 function createRestaurantItem(data) {
   const restaurantItem = createElement('li', 'restaurant');
   const categoryDiv = createElement('div', 'restaurant__category');
   const infoDiv = createElement('div', 'restaurant__info');
+  const titleDiv = createElement('div', 'restaurant__title');
+  const flexDiv = createElement('div', 'flex');
 
-  const { categoryImg, nameHeading, distanceSpan, descriptionPara } = createTags(data);
+  const { categoryImg, nameHeading, distanceSpan, starImg, descriptionPara } = createTags(data);
+
+  starImg.addEventListener('click', () => {
+    starImg.classList.toggle('favorite');
+  });
 
   categoryDiv.append(categoryImg);
-  infoDiv.append(nameHeading, distanceSpan, descriptionPara);
+  titleDiv.append(nameHeading, distanceSpan);
+  flexDiv.append(titleDiv, starImg);
+  infoDiv.append(flexDiv, descriptionPara);
   restaurantItem.append(categoryDiv, infoDiv);
 
   return restaurantItem;
