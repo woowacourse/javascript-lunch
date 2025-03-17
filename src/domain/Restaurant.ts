0c6b903796eb289785ad5625937f81dbe33aae98
aydenote <dslgpgh@gmail.com>
2025-03-17 16:46:49 +0900

    feat: 도메인 데이터 타입 정의

---
+++ src/domain/Restaurant.ts
@@ -0,0 +1,27 @@
+import { RestaurantInformation } from "./type";
+
+class Restaurant {
+  #information: RestaurantInformation;
+
+  constructor({ id, category, name, distance, description = "", link = "", favorites = false }: RestaurantInformation) {
+    this.#information = {
+      id,
+      category,
+      name,
+      distance,
+      description,
+      link,
+      favorites,
+    };
+  }
+
+  get information(): RestaurantInformation {
+    return { ...this.#information };
+  }
+
+  updateInformation(): void {
+    this.#information.favorites = !this.#information.favorites;
+  }
+}
+
+export default Restaurant;
---
+++ src/domain/RestaurantList.ts
@@ -0,0 +1,55 @@
+import { generateUniqueId, setRestaurantStorage } from "../utils/store";
+import Restaurant from "./Restaurant";
+import { RestaurantInformation } from "./type";
+
+class RestaurantList {
+  #restaurants: Restaurant[];
+
+  constructor(listItemContents: RestaurantInformation[] = []) {
+    this.#restaurants = this.initialAddRestaurant(listItemContents);
+  }
+
+  private initialAddRestaurant(listItemContents: RestaurantInformation[]): Restaurant[] {
+    return listItemContents.map((listItemContent) => new Restaurant(listItemContent));
+  }
+
+  addRestaurant(restaurantInformation: RestaurantInformation): Restaurant {
+    const newRestaurant = new Restaurant({
+      ...restaurantInformation,
+      id: generateUniqueId(),
+    });
+    this.#restaurants.push(newRestaurant);
+    const newRestaurantData: RestaurantInformation[] = this.#restaurants.map((restaurant) => restaurant.information);
+    setRestaurantStorage(newRestaurantData);
+    return newRestaurant;
+  }
+
+  deleteRestaurant(id: string): void {
+    const targetIndex = this.#restaurants.findIndex((restaurant) => restaurant.information.id === id);
+    if (targetIndex !== -1) {
+      this.#restaurants.splice(targetIndex, 1);
+      const newRestaurantData: RestaurantInformation[] = this.#restaurants.map((restaurant) => restaurant.information);
+      setRestaurantStorage(newRestaurantData);
+    }
+  }
+
+  updateRestaurant(id: string): void {
+    const targetRestaurant = this.#restaurants.find((restaurant) => restaurant.information.id === id);
+    if (targetRestaurant) {
+      targetRestaurant.updateInformation();
+      const newRestaurantData: RestaurantInformation[] = this.#restaurants.map((restaurant) => restaurant.information);
+      setRestaurantStorage(newRestaurantData);
+    }
+  }
+
+  getRestaurantInformation(id: string): RestaurantInformation | undefined {
+    const found = this.#restaurants.find((restaurant) => restaurant.information.id === id);
+    return found ? found.information : undefined;
+  }
+
+  get restaurants(): Restaurant[] {
+    return [...this.#restaurants];
+  }
+}
+
+export default RestaurantList;
