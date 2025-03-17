0675762ea3824fb457105f1a53b818d74780238a
jin123457 <jin_ox@naver.com>
2025-03-17 11:46:05 +0900

    test: 모달의 취소 버튼 클릭 시 닫히는지 검사하는 테스트 버그 수정

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -42,7 +42,8 @@ describe("e2e 테스트", () => {
     });
 
     it("취소 버튼을 눌렀을 시 모달 닫히는지 검사", () => {
-      cy.get(".cancel-button").click({ force: true });
+      cy.get(".gnb__button").click();
+      cy.get(".cancel-button").should("exist").click({ force: true });
       cy.get(".modal").should("not.have.class", "modal--open");
     });
   });
