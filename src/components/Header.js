dc60f84d498f6d9c8896560a6f76e0c2ddf3000a
sanghee01 <sang6299@naver.com>
2025-03-17 17:59:37 +0900

    chore: 이미지 경로 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
       <header class="gnb">
         <h1 class="gnb__title text-title">${this.props.title}</h1>
         <button type="button" class="gnb__button" aria-label="음식점 추가">
-          <img src="./public/images/add-button.png" alt="음식점 추가" />
+          <img src="../../public/images/add-button.png" alt="음식점 추가" />
         </button>
       </header>
     `;
