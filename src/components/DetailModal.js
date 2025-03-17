9f90412cd525c86b9c1c3f97aad912961a7222aa
sanghee01 <sang6299@naver.com>
2025-03-17 11:03:44 +0900

    fix: 디테일 모달 안닫히는거 해결 - 이벤트 위임 사용!!!

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -8,24 +8,16 @@ class DetailModal extends Component {
     });
     this.element.appendChild(detailModal.element);
 
-    const $modalBackdrop = this.element.querySelector('.modal-backdrop');
-    const $modalCancelButton = this.element.querySelector('#modal-cancel');
     const $modal = this.element.querySelector('.modal');
 
-    $modalBackdrop.addEventListener('click', function () {
-      // TODO: 배경 클릭시에는 모달이 안꺼짐. 콘솔은 잘 출력되는데 왤까.. hidden 클래스 적용이 안됨..
-      console.log('바깥 배경 누를때 가리키는 모달', $modal);
-      $modal.classList.add('hidden');
+    document.addEventListener('click', function (event) {
+      if (event.target.closest('.modal-backdrop')) $modal.classList.add('hidden');
     });
 
-    // TODO: 닫기버튼도 hidden 클래스 적용이 안됨..
-    $modalCancelButton.addEventListener('click', function () {
-      console.log('닫기버튼 누를때 가리키는 모달', $modal);
-
-      $modal.classList.add('hidden');
+    document.addEventListener('click', function (event) {
+      if (event.target.closest('#modal-cancel')) $modal.classList.add('hidden');
     });
 
-    // ESC할 때는 hidden 클래스 잘 적용 됨..
     document.addEventListener('keydown', function (event) {
       if (event.key === 'Escape') $modal.classList.add('hidden');
     });
