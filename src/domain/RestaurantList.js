f9426d4ffd8106dee0e4f6b9d1c135afcc486569
aydenote <dslgpgh@gmail.com>
2025-03-17 16:09:37 +0900

    feat: 매장 정보 업데이트 기능 구현

--- src/domain/RestaurantList.js
+++ src/domain/RestaurantList.js
@@ -1,8 +1,10 @@
+import { generateUniqueId, setRestaurantStorage } from "../utils/store.js";
 import Restaurant from "./Restaurant.js";
 
 class RestaurantList {
   #restaurants;
-  constructor(listItemContents) {
+
+  constructor(listItemContents = []) {
     this.#restaurants = this.initialAddRestaurant(listItemContents);
   }
 
@@ -11,12 +13,37 @@ class RestaurantList {
   }
 
   addRestaurant(restaurantInformation) {
-    const newRestaurant = new Restaurant(restaurantInformation);
+    const newRestaurant = new Restaurant({ ...restaurantInformation, id: generateUniqueId() });
     this.#restaurants.push(newRestaurant);
+    const newRestaurantData = this.#restaurants.map((restaurant) => restaurant.information);
+    setRestaurantStorage(newRestaurantData);
+
     return newRestaurant;
   }
 
-  get resaurants() {
+  deleteRestaurant(id) {
+    const targetIndex = this.#restaurants.findIndex((restaurant) => restaurant.information.id === id);
+    this.#restaurants.splice(targetIndex, 1);
+    const newRestaurantData = this.#restaurants.map((restaurant) => restaurant.information);
+    setRestaurantStorage(newRestaurantData);
+  }
+
+  updateRestaurant(id) {
+    const targetRestaurant = this.#restaurants.find((restaurant) => restaurant.information.id === id);
+    if (targetRestaurant) {
+      targetRestaurant.updateInformation();
+
+      const newRestaurantData = this.#restaurants.map((restaurant) => restaurant.information);
+      setRestaurantStorage(newRestaurantData);
+    }
+  }
+
+  getRestaurantInformation(id) {
+    const findRestaurant = this.#restaurants.filter((restaurant) => restaurant.information.id === id);
+    return findRestaurant[0].information;
+  }
+
+  get restaurants() {
     return [...this.#restaurants];
   }
 }
