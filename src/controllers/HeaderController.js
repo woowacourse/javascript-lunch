7d956cce334bb09d9aaac0d40a32dd8059aea45a
aydenote <dslgpgh@gmail.com>
2025-03-17 16:00:43 +0900

    refactor: 관심사 분리하여 기능 구현

--- src/controllers/HeaderController.js
+++ src/controllers/HeaderController.js
@@ -1,11 +1,14 @@
 import Header from "../components/Header.js";
 import { HEADER_CONTENTS } from "../contants.js";
-import EventHandler from "./EventHandler.js";
+import { openRestaurantModal } from "./ModalController.js";
 
-export function HeaderController(app) {
-  app.prepend(Header(HEADER_CONTENTS));
+export function HeaderController(app, listContainerElement) {
+  app.appendChild(Header(HEADER_CONTENTS));
   const modalButtonElement = app.querySelector("header button.gnb__button");
-  modalButtonElement.addEventListener("click", () => EventHandler.modalToggle(app));
+
+  modalButtonElement.addEventListener("click", () => {
+    openRestaurantModal(app, listContainerElement);
+  });
 }
 
 export default HeaderController;
--- src/controllers/ModalController.js
+++ src/controllers/ModalController.js
@@ -1,21 +1,72 @@
+import Form from "../components/Form/Form.js";
+import { restaurantItemClick } from "../components/List.js";
+import ListItem from "../components/ListItem.js";
 import Modal from "../components/Modal.js";
+import Title from "../components/Title.js";
+import { MODAL_BUTTONS_PROPERTY } from "../contants.js";
+import RestaurantList from "../domain/RestaurantList.js";
 import { convertObjectToArray } from "../utils/format.js";
 import EventHandler from "./EventHandler.js";
+import ListController from "./ListController.js";
+import { createFormItems, INPUT_ITEMS } from "./MainController.js";
 
-export function ModalController({ listElement, restaurantList, children }) {
+export function getModalContent() {
+  const titleElement = Title({ type: "modal", text: "새로운 음식점" });
+  const formItems = createFormItems(INPUT_ITEMS);
+  const formElement = Form({ formItems, buttons: MODAL_BUTTONS_PROPERTY });
+  return { titleElement, formElement };
+}
+
+export function getModalHandlers({ restaurantList, listElement, mainElement, formElement }) {
+  const handleSubmit = (event) => {
+    const values = EventHandler.formDataParsing(event);
+    const restaurant = restaurantList.addRestaurant(values);
+    listElement.appendChild(
+      ListItem(restaurant.information, {
+        onClick: (event) => restaurantItemClick(event, restaurantList),
+      }),
+    );
+    EventHandler.modalToggle(mainElement, formElement);
+  };
+
+  const handleCancel = () => {
+    EventHandler.modalToggle(mainElement, formElement);
+  };
+
+  return { handleSubmit, handleCancel };
+}
+
+export function openRestaurantModal(app, listContainerElement) {
+  const mainElement = app.querySelector("main");
+  const { titleElement, formElement } = getModalContent();
+  const existingModal = document.querySelector(".modal");
+
+  if (existingModal) {
+    existingModal.remove();
+  }
+
+  const { listElement, restaurantList } = ListController(app, listContainerElement);
+  const { handleSubmit, handleCancel } = getModalHandlers({ restaurantList, listElement, mainElement, formElement });
+
+  ModalController({
+    children: { titleElement, formElement },
+    submit: handleSubmit,
+    cancle: handleCancel,
+  });
+
+  EventHandler.modalToggle(app);
+}
+
+export function ModalController({ children, submit = null, cancle = null }) {
   const mainElement = app.querySelector("main");
   const modalElement = Modal(convertObjectToArray(children));
   const closeButtonElement = children.formElement.querySelector("button[type='button']");
   const modalBackdropElement = modalElement.querySelector(".modal-backdrop");
+  const favoriteElement = document.querySelector(".favorites-icon");
 
-  closeButtonElement.addEventListener("click", () => EventHandler.modalToggle(mainElement, children.formElement));
   modalBackdropElement.addEventListener("click", () => EventHandler.modalToggle(mainElement, children.formElement));
-  children.formElement.addEventListener("submit", (event) => {
-    const values = EventHandler.formDataParsing(event);
-    const restaurant = restaurantList.addRestaurant(values);
-    listElement.appendChild(ListItem(restaurant.information));
-    EventHandler.modalToggle(mainElement, children.formElement);
-  });
+  closeButtonElement.addEventListener("click", () => cancle());
+  children.formElement.addEventListener("submit", (event) => submit(event));
 
   mainElement.appendChild(modalElement);
 }
