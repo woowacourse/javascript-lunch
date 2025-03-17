48a754a5cd6ea3a78f468340db7e2e76a338c28f
guesung <gueit214@naver.com>
2025-03-17 11:09:03 +0900

    refactor: 옵셔널 체이닝 제거

--- src/components/RestaurantDetailModal.ts
+++ src/components/RestaurantDetailModal.ts
@@ -10,37 +10,37 @@ interface RestaurantDetailModalProps extends RestaurantType {
 
 export default class RestaurantDetailModal extends Component<RestaurantDetailModalProps> {
   override template() {
-    const currentRestaurant = this.props ?? null;
+    const currentRestaurant = this.props;
 
     return html`<div class="restaurant-detail-modal">
       <div class="restaurant-detail-modal__header">
         <div class="restaurant__category">
           <img
-            src="images/category-${CATEGORY_MAP[currentRestaurant?.category as keyof typeof CATEGORY_MAP]}.png"
-            alt="${currentRestaurant?.category ?? ''}"
+            src="images/category-${CATEGORY_MAP[currentRestaurant.category as keyof typeof CATEGORY_MAP]}.png"
+            alt="${currentRestaurant.category ?? ''}"
             class="category-icon"
           />
         </div>
         <img
-          src="images/star${currentRestaurant?.isLike ? '_filled' : ''}.svg"
+          src="images/star${currentRestaurant.isLike ? '_filled' : ''}.svg"
           alt="음식점 추가"
           class="restaurant-detail-modal__like"
           data-action="restaurant-like"
-          data-id="${currentRestaurant?.id ?? ''}"
+          data-id="${currentRestaurant.id ?? ''}"
         />
       </div>
       <div class="restaurant__info">
         <div class="restaurant__info--inner">
           <div>
-            <h3 class="restaurant__name text-subtitle">${currentRestaurant?.name ?? ''}</h3>
+            <h3 class="restaurant__name text-subtitle">${currentRestaurant.name ?? ''}</h3>
             <span class="restaurant-detail-modal__distance text-body"
-              >캠퍼스부터 ${currentRestaurant?.distance ?? 5}분 내</span
+              >캠퍼스부터 ${currentRestaurant.distance ?? 5}분 내</span
             >
           </div>
         </div>
-        <p class="text-body">${currentRestaurant?.description ?? ''}</p>
-        <a class="text-body restaurant-detail-modal__url" href="${currentRestaurant?.url ?? ''}" target="_blank">
-          ${currentRestaurant?.url ?? ''}
+        <p class="text-body">${currentRestaurant.description ?? ''}</p>
+        <a class="text-body restaurant-detail-modal__url" href="${currentRestaurant.url ?? ''}" target="_blank">
+          ${currentRestaurant.url ?? ''}
         </a>
         <div class="buttons"></div>
       </div>
