16181a8c927d0be5bfdb9b2fb4cf8293269d3efc
guesung <gueit214@naver.com>
2025-03-17 17:47:05 +0900

    feat: 이스케이프 처리로 XSS 보안 강화

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,26 @@
-import { concat, flat, pipe, reduce, zip } from '@fxts/core';
+import { concat, flat, map, pipe, reduce, zip } from '@fxts/core';
 import type { HTMLType } from './types';
 
-export function html(strings: TemplateStringsArray, ...values: any[]): HTMLType {
+const escape = (str: string) =>
+  String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+
+export function html(strings: TemplateStringsArray, ...values: any[]) {
   return pipe(
-    zip(strings, concat(values, [''])),
+    zip(
+      strings,
+      concat(
+        map((value) => escape(value), values),
+        [''],
+      ),
+    ),
     flat,
     reduce((a, b) => a + b),
-  );
+  ) as HTMLType;
 }
 
 export function generateId() {
