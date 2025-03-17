5e1306b2b87cdf8ad0338fa66194bed13a7bdf45
aydenote <dslgpgh@gmail.com>
2025-03-17 16:04:05 +0900

    feat: 매장 정보 모달 구현

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,76 @@
+import { RESTAURANT_MODAL_PROPERTY } from "../contants.js";
+import EventHandler from "../controllers/EventHandler.js";
+import ListController from "../controllers/ListController.js";
+import ModalController from "../controllers/ModalController.js";
+import RestaurantList from "../domain/RestaurantList.js";
+import { getRestaurantStorage } from "../utils/store.js";
+import Form from "./Form/Form.js";
 import ListItem from "./ListItem.js";
 
-function List(listItems) {
+export function toggleFavorite(event, restaurantList, restaurantId) {
+  restaurantList.updateRestaurant(restaurantId);
+  const favoriteIcon = event.target.closest("img.favorites-icon");
+  const restaurantFavoriteIconElement = document.querySelector(
+    `.restaurant-list-container [data-id="${restaurantId}"] img.favorites-icon`,
+  );
+
+  if (!favoriteIcon) return;
+
+  if (favoriteIcon.src.includes("favorite-icon-lined.png")) {
+    favoriteIcon.src = "./public/favorite-icon-filled.png";
+    restaurantFavoriteIconElement.src = "./public/favorite-icon-filled.png";
+  } else {
+    favoriteIcon.src = "./public/favorite-icon-lined.png";
+    restaurantFavoriteIconElement.src = "./public/favorite-icon-lined.png";
+  }
+}
+
+function openModal(restaurantList, restaurantId) {
+  const filteredRestaurant = restaurantList.getRestaurantInformation(restaurantId);
+  const formItems = [
+    ListItem(filteredRestaurant, {
+      onClick: (event) => toggleFavorite(event, restaurantList, restaurantId),
+      className: "information",
+    }),
+  ];
+  const mainElement = app.querySelector("main");
+  const formElement = Form({ formItems, buttons: RESTAURANT_MODAL_PROPERTY });
+
+  const modalElement = document.querySelector(".modal");
+  if (modalElement) modalElement.remove();
+  ModalController({
+    children: { formElement },
+    submit: (event) => {
+      const restaurantElement = document.querySelector(".restaurant");
+      const storedRestaurants = getRestaurantStorage();
+      const restaurantList = new RestaurantList(storedRestaurants);
+      restaurantList.deleteRestaurant(restaurantElement.dataset.id);
+    },
+    cancle: () => {
+      EventHandler.modalToggle(mainElement, formElement);
+    },
+  });
+  EventHandler.modalToggle(app);
+}
+
+export const restaurantItemClick = (event, restaurantList) => {
+  const restaurantId = event.currentTarget.dataset.id;
+  if (event.target.className === "favorites-icon") {
+    toggleFavorite(event, restaurantList, restaurantId);
+  } else {
+    openModal(restaurantList, restaurantId);
+  }
+};
+
+function List(listItems, restaurantList) {
   const listElement = document.createElement("ul");
   listElement.classList.add("restaurant-list");
+
   listItems.forEach((item) => {
-    listElement.appendChild(ListItem(item.information));
+    const listItemElement = ListItem(item.information, {
+      onClick: (event) => restaurantItemClick(event, restaurantList),
+    });
+    listElement.appendChild(listItemElement);
   });
 
   return listElement;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,9 +7,32 @@ const CATRGORY_IMAGE_PATH = {
   기타: "./public/category-etc.png",
 };
 
-function ListItem({ category, name, distance, description }) {
+function renderFavoritesImg(favorites) {
+  if (favorites) {
+    return "./public/favorite-icon-filled.png";
+  }
+  return "./public/favorite-icon-lined.png";
+}
+
+function ListItem({ id, category, name, distance, description, favorites, link }, { onClick = null, className = "" }) {
   const listElement = document.createElement("li");
-  listElement.classList.add("restaurant");
+  listElement.classList.add(`restaurant`);
+  listElement.dataset.id = id;
+  if (onClick) {
+    listElement.addEventListener("click", (event) => onClick(event));
+  }
+
+  if (className === "information") {
+    listElement.classList.add(className);
+  }
+
+  function createLink() {
+    if (className === "information") {
+      return `<a href="${link} class="link__text">${link}</a>`;
+    }
+    return "";
+  }
+
   listElement.innerHTML = `
     <div class="restaurant__category">
       <img src=${CATRGORY_IMAGE_PATH[category]} alt=${category} class="category-icon" />
@@ -18,9 +41,11 @@ function ListItem({ category, name, distance, description }) {
       <h3 class="restaurant__name text-subtitle">${name}</h3>
       <span class="restaurant__distance text-body">캠퍼스부터 ${distance}</span>
       <p class="restaurant__description text-body">
-        ${description}
+      ${description}
       </p>
-    </div>
+      </div>
+      <img src="${renderFavoritesImg(favorites)}" alt=favorites class="favorites-icon" />
+      ${createLink()}
     `;
   return listElement;
 }
