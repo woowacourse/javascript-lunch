077243f58dfa6ac9664c5c8a16430b8b40c1be73
guesung <gueit214@naver.com>
2025-03-17 17:32:45 +0900

    feat: 옵저버 객체

--- src/lib/modules/index.ts
+++ src/lib/modules/index.ts
@@ -1,3 +1,4 @@
 export { default as eventHandlerInstance } from './EventHandler';
 export { default as LocalStorage } from './LocalStorage';
 export { default as Portal } from './Portal';
+export { default as Observer } from './Observer';
