29d715ee209865f4e67c41a096cc9ae4f048f81f
eunoia-jaxson <eunoia@handong.ac.kr>
2025-03-17 14:04:53 +0900

    refactor: Restarant category 타입 narrowing

--- src/domains/restaurants.ts
+++ src/domains/restaurants.ts
@@ -1,6 +1,6 @@
 export interface Restaurant {
   id: string;
-  category: string;
+  category: "한식" | "중식" | "일식" | "양식" | "아시안" | "기타";
   name: string;
   distance: number;
   description?: string;
