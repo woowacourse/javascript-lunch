9fda21ec11eb3bf099430a39c5a152c91456ff1b
Ko Daeun <drechsu@gmail.com>
2025-03-17 16:41:48 +0900

    refactor : createRestaurantCards 에 넘겨주는 이벤트를 eventHandler객체를 이용해 전달

--- src/ui/renderFavoritePage.js
+++ src/ui/renderFavoritePage.js
@@ -1,4 +1,5 @@
 import storage from "../domain/storage.ts";
+import eventHandlers from "../events/eventHandlers.js";
 import createRestaurantCards from "../service/createRestaurantCards";
 import { $ } from "../utils/dom";
 import renderRestaurants from "./renderRestaurant";
@@ -9,12 +10,7 @@ const renderFavoritePage = (restaurantList) => {
 
   const favoriteCardList = createRestaurantCards(
     restaurantList.getFavoriteList(),
-    {
-      clickFavorite: () => {
-        storage.saveRestaurantList(restaurantList.value);
-        renderFavoritePage(restaurantList);
-      },
-    }
+    eventHandlers.favorite(restaurantList)
   );
 
   return renderRestaurants(favoriteCardList);
