33200c63d4d448d4f13024eace39516741fa9d3e
aydenote <dslgpgh@gmail.com>
2025-03-17 15:59:29 +0900

    feat: 매장 고유 아이디 값 필드 생성

--- src/domain/Restaurant.js
+++ src/domain/Restaurant.js
@@ -1,16 +1,22 @@
 class Restaurant {
   #information = {};
 
-  constructor({ category, name, distance, description = "", link = "" }) {
+  constructor({ id, category, name, distance, description = "", link = "", favorites = false }) {
     this.#information.category = category;
+    this.#information.id = id;
     this.#information.name = name;
     this.#information.distance = distance;
     this.#information.description = description;
     this.#information.link = link;
+    this.#information.favorites = favorites;
   }
 
   get information() {
     return { ...this.#information };
+  }
+
+  updateInformation() {
+    this.#information.favorites = !this.#information.favorites;
   }
 }
 export default Restaurant;
