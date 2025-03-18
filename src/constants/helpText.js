7ed6992be7e9b678ee50301eb90094bbf379333f
ohgus <anakin1108@naver.com>
2025-03-18 21:02:32 +0900

    chore: 병합 과정에서 생긴 불필요한 파일 제거

--- src/constants/helpText.js
+++
@@ -1,6 +0,0 @@
-const helpText = {
-  DESCRIPTION: "메뉴 등 추가 정보를 입력해 주세요.",
-  LINK: "매장 정보를 확인할 수 있는 링크를 입력해 주세요.",
-};
-
-export default helpText;
--- src/constants/selectOptions.js
+++
@@ -1,6 +0,0 @@
-const selectOptions = {
-  CATEGORY: ["한식", "중식", "일식", "양식", "아시안", "기타"],
-  DISTANCE: ["5", "10", "15", "20", "25", "30"],
-};
-
-export default selectOptions;
