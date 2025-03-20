b998bdc59a6a403ce612e73a1ac24c215b892fc8
Jeongeun Lee <leeju1009@gmail.com>
2025-03-21 00:30:17 +0900

    refactor: 컴포넌트에 적용되는 이벤트 함수, DOM 조작 함수가 명시되도록 구조 개선

--- src/main.ts
+++ src/main.ts
@@ -12,85 +12,186 @@ import {
 
 import stateStore from './domain/stateStore.ts';
 import restaurantService from './service/restaurantService.ts';
-import {
-  renderAddRestaurantModal,
-  renderHeader,
-  renderItemsController,
-  renderModalContents,
-  renderRestaurantInfo,
-  renderRestaurantItems,
-  renderTabs,
-  setRequired,
-} from './ui/renderer.ts';
-import { renderElement, selectElement } from './utils/dom.ts';
+import { selectElement } from './utils/dom.ts';
 import { RESTAURANTS } from './data/initialData.ts';
-import createRestaurantInfo from './templates/RestaurantInfo.ts';
+import {
+  header,
+  tab,
+  restaurantItems,
+  itemsController,
+  addRestaurantModal,
+  restaurantInfoModal,
+} from './components/index.ts';
+import {
+  createButton,
+  createHeader,
+  createInput,
+  createModal,
+  createRestaurantItem,
+  createSelect,
+  createTextarea,
+  createRestaurantInfo,
+} from './templates/index.ts';
+import {
+  ADD_BUTTON,
+  ADD_RESTAURANT_MODAL,
+  CANCEL_BUTTON,
+  CATEGORY,
+  CATEGORY_FILTER,
+  CLOSE_INFO_BUTTON,
+  DELETE_INFO_BUTTON,
+  DESCRIPTION,
+  DISTANCE,
+  FREQUENT_ITEMS_TAB,
+  LINK,
+  NAME,
+  RESTAURANT_INFO_MODAL,
+  SORT_SELECTOR,
+  TOTAL_ITEMS_TAB,
+} from './constants/elements.ts';
 
 addEventListener('load', () => {
-  renderHeader();
-  renderTabs();
-  renderItemsController();
-  initRestaurantItems();
-  updateRestaurantElements();
-  renderAddRestaurantModal();
-  renderModalContents();
-  renderRestaurantInfo();
+  handleAddRestaurantModal();
+  handleRestaurantInfoModal();
+  closeModal();
 
-  const nameInputElement = selectElement('#name') as HTMLInputElement;
-  const categorySelectElement = selectElement('#category') as HTMLInputElement;
-  const distanceSelectElement = selectElement('#distance') as HTMLInputElement;
-
-  setRequired(nameInputElement);
-  setRequired(categorySelectElement);
-  setRequired(distanceSelectElement);
-
-  addEventHandlers();
+  handleHeader();
+  handleTab();
+  handleItemsController();
+  handleRestaurantItems();
 });
 
-function addEventHandlers() {
-  openAddRestaurantModal();
-  openRestaurantInfoModal(renderRestaurantInfoContents);
-  readNewRestaurant(restaurantService.addRestaurant.bind(restaurantService), updateRestaurantElements);
-  closeModal();
-  switchTab(stateStore.updateState.bind(stateStore), updateRestaurantElements);
-  selectSortKey(stateStore.updateState.bind(stateStore), updateRestaurantElements);
-  selectCategory(stateStore.updateState.bind(stateStore), updateRestaurantElements);
-  toggleFavoriteButton(restaurantService.toggleFavorite.bind(restaurantService), updateFavoriteIcon);
-  deleteRestaurant(restaurantService.deleteRestaurant.bind(restaurantService), updateRestaurantElements);
-}
+const handleAddRestaurantModal = () => {
+  addRestaurantModal.render({
+    modalContainer: createModal(ADD_RESTAURANT_MODAL),
+    modalContents: {
+      categorySelect: createSelect(CATEGORY),
+      nameInput: createInput(NAME),
+      distanceSelect: createSelect(DISTANCE),
+      descriptionTextarea: createTextarea(DESCRIPTION),
+      linkInput: createInput(LINK),
+    },
+    modalButton: {
+      addButton: createButton(ADD_BUTTON),
+      cancelButton: createButton(CANCEL_BUTTON),
+    },
+  });
 
-function initRestaurantItems() {
+  addRestaurantModal.setEvent([
+    () => initFormValidation,
+    () =>
+      readNewRestaurant(restaurantService.addRestaurant.bind(restaurantService), () =>
+        restaurantItems.render({
+          restaurants: () => filterAndSortRestaurants(),
+        }),
+      ),
+  ]);
+};
+
+const handleRestaurantInfoModal = () => {
+  restaurantInfoModal.render({
+    modalContainer: createModal(RESTAURANT_INFO_MODAL),
+    modalButton: {
+      deleteButton: createButton(DELETE_INFO_BUTTON),
+      closeButton: createButton(CLOSE_INFO_BUTTON),
+    },
+  });
+
+  restaurantInfoModal.setEvent([
+    () => {
+      const modalContents = (id: number) => {
+        const targetData = restaurantService.getRestaurantById(id);
+        return createRestaurantInfo(targetData);
+      };
+
+      const renderer = (id: number) => restaurantInfoModal.renderContents({ id, restaurantsInfo: modalContents });
+      openRestaurantInfoModal(renderer);
+    },
+
+    () =>
+      deleteRestaurant(restaurantService.deleteRestaurant.bind(restaurantService), () =>
+        restaurantItems.render({
+          restaurants: () => filterAndSortRestaurants(),
+        }),
+      ),
+  ]);
+};
+
+const handleHeader = () => {
+  header.render({ header: createHeader({ title: '점심 뭐 먹지' }) });
+  header.setEvent([openAddRestaurantModal]);
+};
+
+const handleTab = () => {
+  tab.render({
+    totalItemsTab: createButton(TOTAL_ITEMS_TAB),
+    frequentItemsTab: createButton(FREQUENT_ITEMS_TAB),
+  });
+
+  tab.setEvent([
+    () =>
+      switchTab(stateStore.updateState.bind(stateStore), () =>
+        restaurantItems.render({
+          restaurants: () => filterAndSortRestaurants(),
+        }),
+      ),
+  ]);
+};
+
+const handleItemsController = () => {
+  itemsController.render({
+    categoryFilter: createSelect(CATEGORY_FILTER),
+    sortSelector: createSelect(SORT_SELECTOR),
+  });
+
+  itemsController.setEvent([
+    () =>
+      selectSortKey(stateStore.updateState.bind(stateStore), () =>
+        restaurantItems.render({
+          restaurants: () => filterAndSortRestaurants(),
+        }),
+      ),
+
+    () =>
+      selectCategory(stateStore.updateState.bind(stateStore), () =>
+        restaurantItems.render({
+          restaurants: () => filterAndSortRestaurants(),
+        }),
+      ),
+  ]);
+};
+
+const handleRestaurantItems = () => {
+  restaurantItems.render({
+    restaurants: () => {
+      const restaurantsData = restaurantService.getRestaurants();
+      return restaurantsData.map((restaurant) => createRestaurantItem(restaurant)).join('');
+    },
+  });
+
+  restaurantItems.setEvent([
+    () => initRestaurantItems,
+    () => toggleFavoriteButton(restaurantService.toggleFavorite.bind(restaurantService), updateFavoriteIcon),
+  ]);
+};
+
+const initRestaurantItems = () => {
   const hasKey = restaurantService.checkHasRestaurantData();
   if (!hasKey) {
     [...RESTAURANTS].forEach((restaurant) => {
       restaurantService.addRestaurant(restaurant);
     });
   }
-}
+};
 
-function updateRestaurantElements() {
+const filterAndSortRestaurants = () => {
   const states = stateStore.getState();
-  const restaurants = restaurantService.getRestaurants();
-  const filteredRestaurants = restaurantService.getFilteredRestaurants(states, restaurants);
+  const restaurantsData = restaurantService.getRestaurants();
+  const filteredData = restaurantService.getFilteredRestaurants(states, restaurantsData);
+  return filteredData.map((restaurant) => createRestaurantItem(restaurant)).join('');
+};
 
-  renderRestaurantItems(filteredRestaurants);
-}
-
-function renderRestaurantInfoContents(id: number) {
-  const targetData = restaurantService.getRestaurantById(id);
-  const contents = createRestaurantInfo(targetData);
-
-  const targetModal = document.querySelector('.restaurant-info-modal > .modal-container') as HTMLElement;
-  const prevInformation = targetModal.querySelector('.restaurant');
-
-  if (prevInformation) {
-    targetModal.removeChild(prevInformation);
-  }
-
-  renderElement('.restaurant-info-modal > .modal-container', contents, 'afterbegin');
-}
-
-function updateFavoriteIcon(id: number, favorite: boolean) {
+const updateFavoriteIcon = (id: number, favorite: boolean) => {
   const targetItems = document.querySelectorAll(`[data-id="${id}"]`);
 
   targetItems.forEach((target) => {
@@ -102,6 +203,22 @@ function updateFavoriteIcon(id: number, favorite: boolean) {
   const { isFavoriteTab } = stateStore.getState();
 
   if (isFavoriteTab) {
-    updateRestaurantElements();
+    restaurantItems.render({
+      restaurants: () => filterAndSortRestaurants(),
+    });
   }
-}
+};
+
+const initFormValidation = () => {
+  const nameInputElement = selectElement('#name') as HTMLInputElement;
+  const categorySelectElement = selectElement('#category') as HTMLInputElement;
+  const distanceSelectElement = selectElement('#distance') as HTMLInputElement;
+
+  setRequired(nameInputElement);
+  setRequired(categorySelectElement);
+  setRequired(distanceSelectElement);
+};
+
+const setRequired = (element: HTMLInputElement) => {
+  element.required = true;
+};
