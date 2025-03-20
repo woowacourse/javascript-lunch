d5cf0ac3090b674bc962a6ab1e2f4452934d9dbb
Oliy <eunsoa0310@gmail.com>
2025-03-20 22:14:45 +0900

    refactor: 배포를 위한 이미지 경로 수정

--- public/assets/imagePaths.ts
+++ public/assets/imagePaths.ts
@@ -3,21 +3,21 @@ interface ImagePath {
 }
 
 const BUTTON_IMAGE_SRC: ImagePath = Object.freeze({
-  ADD_BUTTON: './public/assets/add-button.png',
+  ADD_BUTTON: './assets/add-button.png',
 });
 
 const CATEGORY_IMAGE_SRC: ImagePath = Object.freeze({
-  KOREAN: './public/assets/category-korean.png',
-  CHINESE: './public/assets/category-chinese.png',
-  JAPANESE: './public/assets/category-japanese.png',
-  WESTERN: './public/assets/category-western.png',
-  ASIAN: './public/assets/category-asian.png',
-  ETC: './public/assets/category-etc.png',
+  KOREAN: './assets/category-korean.png',
+  CHINESE: './assets/category-chinese.png',
+  JAPANESE: './assets/category-japanese.png',
+  WESTERN: './assets/category-western.png',
+  ASIAN: './assets/category-asian.png',
+  ETC: './assets/category-etc.png',
 });
 
 const FAVORITE_IMAGE_SRC: ImagePath = Object.freeze({
-  FAVORITE: './public/assets/favorite-icon-filled.png',
-  UNFAVORITE: './public/assets/favorite-icon-lined.png',
+  FAVORITE: './assets/favorite-icon-filled.png',
+  UNFAVORITE: './assets/favorite-icon-lined.png',
 });
 
 export { BUTTON_IMAGE_SRC, CATEGORY_IMAGE_SRC, FAVORITE_IMAGE_SRC };
