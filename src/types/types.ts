19f2ffc124ee60fa00e5120b66050c4cf5c0e215
jin123457 <jin_ox@naver.com>
2025-03-20 21:53:21 +0900

    refactor: 객체 요소 type을 type -> interface로 변경

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,22 +4,6 @@ export type Category = "한식" | "중식" | "일식" | "양식" | "아시안" |
 export type Distance = 5 | 10 | 15 | 20 | 30;
 export type Sort = "" | "name" | "distance";
 
-export interface IRestaurant {
-  category: Category;
-  distance: Distance;
-  id: string;
-  name: string;
-  isFavorite: boolean;
-  link?: string;
-  description?: string;
-}
-
-export type EventType = { eventType: string; eventHandler: (e: Event) => void };
-export type FormEventType = {
-  eventType: string;
-  eventHandler: () => void;
-};
-
 type InputFields = typeof FORM_FIELDS.INPUTS;
 type InputFieldKey = keyof InputFields;
 export type TypeInput = Exclude<InputFields[InputFieldKey], "create">;
@@ -31,3 +15,22 @@ export type TypeSelect = Exclude<SelectFields[SelectFieldKey], "create">;
 type TextareaFields = typeof FORM_FIELDS.TEXTAREAS;
 type TextareaFieldKey = keyof TextareaFields;
 export type TypeTextarea = Exclude<TextareaFields[TextareaFieldKey], "create">;
+
+export interface IRestaurant {
+  category: Category;
+  distance: Distance;
+  id: string;
+  name: string;
+  isFavorite: boolean;
+  link?: string;
+  description?: string;
+}
+
+export interface EventType {
+  eventType: string;
+  eventHandler: (e: Event) => void;
+}
+export interface FormEventType {
+  eventType: string;
+  eventHandler: () => void;
+}
