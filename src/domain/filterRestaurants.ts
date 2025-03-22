544cb99a42e3629122fc900802a8e76378f5767e
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-22 10:29:23 +0900

    ♻️ refactor(filterRestaurants): 기본값 localStorage 활용

--- src/domain/filterRestaurants.ts
+++ src/domain/filterRestaurants.ts
@@ -1,20 +1,30 @@
 import { createRestaurant } from "../components/createRestaurant.js";
 import { getStoredRestaurantData } from "./storeRestaurantData.ts";
 
-export const filterRestaurants = (category: string, sort: string) => {
+export const filterRestaurants = (category?: string, sort?: string) => {
+  const savedCategory = localStorage.getItem("selectedCategory") || "전체";
+  const savedSort = localStorage.getItem("sortType") || "distance";
+
+  const finalCategory = category || savedCategory;
+  const finalSort = sort || savedSort;
+
   let filteredData = getStoredRestaurantData();
-  if (category !== "전체") {
+
+  if (finalCategory !== "전체") {
     filteredData = filteredData.filter(
-      (restaurant) => restaurant.imgAlt === category,
+      (restaurant) => restaurant.imgAlt === finalCategory,
     );
   }
-  if (sort === "name") {
+
+  if (finalSort === "name") {
     filteredData.sort((a, b) => a.name.localeCompare(b.name, "ko-KR"));
   } else {
     filteredData.sort((a, b) => a.distance - b.distance);
   }
-  localStorage.setItem("selectedCategory", category);
-  localStorage.setItem("sortType", sort);
+
+  localStorage.setItem("selectedCategory", finalCategory);
+  localStorage.setItem("sortType", finalSort);
+
   createRestaurant(filteredData);
   return filteredData;
 };
