eb258ad6cd9818e7d735eb3a0579011559d8f64a
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-03-22 10:44:26 +0900

    ♻️ refactor: 불필요한 리턴 값 제거

--- src/domain/filterRestaurants.ts
+++ src/domain/filterRestaurants.ts
@@ -26,5 +26,4 @@ export const filterRestaurants = (category?: string, sort?: string) => {
   localStorage.setItem("sortType", finalSort);
 
   createRestaurant(filteredData);
-  return filteredData;
 };
