68282ef8d40f3d3121546ccdcc7db25c76a69d14
Ko Daeun <drechsu@gmail.com>
2025-03-22 15:40:57 +0900

    refactor : storage 저장할때 쓰이는 key값들 상수화

--- src/domain/storage.ts
+++ src/domain/storage.ts
@@ -6,46 +6,56 @@ import {
   TabInfo,
 } from "./types";
 
+const STORAGE_KEYS = {
+  RESTAURANT_LIST: "restaurantList",
+  CATEGORY: "category",
+  NAME_OR_DISTANCE: "nameOrDistance",
+  TAB_INFO: "TabInfo",
+};
+
 const storage = {
   saveRestaurantList(value: Restaurant[]) {
     const restaurantListValue = value.map((restaurant) => {
       return restaurant.value;
     });
-    localStorage.setItem("restaurnatList", JSON.stringify(restaurantListValue));
+    localStorage.setItem(
+      STORAGE_KEYS.RESTAURANT_LIST,
+      JSON.stringify(restaurantListValue)
+    );
   },
 
   loadRestaurantList(): RestaurantValue[] | null {
-    const restaurantList = localStorage.getItem("restaurnatList");
+    const restaurantList = localStorage.getItem(STORAGE_KEYS.RESTAURANT_LIST);
 
     return restaurantList ? JSON.parse(restaurantList) : null;
   },
 
   saveCategory(value: CategoryFilter) {
-    localStorage.setItem("category", JSON.stringify(value));
+    localStorage.setItem(STORAGE_KEYS.CATEGORY, JSON.stringify(value));
   },
 
   loadCategory(): CategoryFilter | null {
-    const category = localStorage.getItem("category");
+    const category = localStorage.getItem(STORAGE_KEYS.CATEGORY);
 
     return category ? JSON.parse(category) : null;
   },
 
   saveNameOrDistance(value: NameOrDistanceFilter) {
-    localStorage.setItem("nameOrDistance", JSON.stringify(value));
+    localStorage.setItem(STORAGE_KEYS.NAME_OR_DISTANCE, JSON.stringify(value));
   },
 
   loadNameOrDistance(): NameOrDistanceFilter | null {
-    const nameOrDistance = localStorage.getItem("nameOrDistance");
+    const nameOrDistance = localStorage.getItem(STORAGE_KEYS.NAME_OR_DISTANCE);
 
     return nameOrDistance ? JSON.parse(nameOrDistance) : null;
   },
 
   saveTabInfo(value: TabInfo) {
-    localStorage.setItem("TabInfo", JSON.stringify(value));
+    localStorage.setItem(STORAGE_KEYS.TAB_INFO, JSON.stringify(value));
   },
 
   loadTabInfo(): TabInfo | null {
-    const tabInfo = localStorage.getItem("TabInfo");
+    const tabInfo = localStorage.getItem(STORAGE_KEYS.TAB_INFO);
 
     return tabInfo ? JSON.parse(tabInfo) : null;
   },
