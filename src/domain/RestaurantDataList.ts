ee8950173d259c7d35c4fd5088d91ef8a0285793
kaori-killer <sksbthwjd@daum.net>
2025-03-23 21:40:32 +0900

    fix: LocalStorage에 데이터가 없을 때 더미데이터를 불러오도록 함

--- src/domain/RestaurantDataList.ts
+++ src/domain/RestaurantDataList.ts
@@ -1,10 +1,12 @@
 import Restaurant from "../types/Restaurant.ts";
 import RestaurantData from "./RestaurantData.ts";
+import restaurants from "../constants/restaurants.js";
 
 class RestaurantDataList {
   #dataList: RestaurantData[];
 
   constructor() {
+    this.#initializeLocalStorage();
     const dataList = this.#getLocalStorage() || [];
     this.#dataList = dataList.map((data: Restaurant) => this.#createData(data));
   }
@@ -46,6 +48,13 @@ class RestaurantDataList {
     this.#setLocalStorage(this.#dataList);
   }
 
+  #initializeLocalStorage(): void {
+    const existing = localStorage.getItem("dataList");
+    if (!existing || JSON.parse(existing).length === 0) {
+      localStorage.setItem("dataList", JSON.stringify(restaurants));
+    }
+  }
+
   #getLocalStorage(): Restaurant[] | null {
     const dataList = localStorage.getItem("dataList");
     return dataList ? JSON.parse(dataList) : null;
@@ -59,37 +68,8 @@ class RestaurantDataList {
   }
 }
 
-const dummy: Restaurant[] = [
-  {
-    id: "1234",
-    category: "한식",
-    name: "피양콩할마니",
-    distance: 10,
-    description:
-      "평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩 할마니. 두부를 빼지 않은 되비지를 맛볼 수 있는 곳으로, ‘피양’은 평안도 사투리로 ‘평양’을 의미한다. 딸과 함께 운영하는 이곳에선 맷돌로 직접 간 콩만을 사용하며, 일체의 조미료를 넣지 않은 건강식을 선보인다. 콩비지와 피양 만두가 이곳의 대표 메뉴지만, 할머니가 옛날 방식을 고수하며 만들어내는 비지전골 또한 이 집의 역사를 느낄 수 있는 특별한 메뉴다. 반찬은 손님들이 먹고 싶은 만큼 덜어 먹을 수 있게 준비돼 있다",
-    isWish: false,
-    link: "https://www.youtube.com/watch?v=66aWspy73tg",
-  },
-  {
-    id: "2345",
-    category: "양식",
-    name: "파스타",
-    distance: 5,
-    description: "레전드 파스타 맛집",
-    isWish: true,
-    link: "https://www.figma.com/design/KcViH81qTQERbbJtBlTEqZ/%EB%A0%88%EB%B2%A81-%EB%AF%B8%EC%85%98-%EB%94%94%EC%9E%90%EC%9D%B8-(%ED%81%AC%EB%A3%A8-%EA%B3%B5%EC%9C%A0%EC%9A%A9)?node-id=1-2&p=f&t=rUKgIzZFY2Hr1J7T-0",
-  },
-  {
-    id: "3456",
-    category: "일식",
-    name: "참치방어스시",
-    distance: 7,
-    description: "참치와 방어가 맛있는 참지입니다. 또 가고 싶어요",
-    isWish: false,
-    link: "https://ofcourse.kr/css-course/cursor-%EC%86%8D%EC%84%B1",
-  },
-];
-localStorage.setItem("dataList", JSON.stringify(dummy));
+
+
 
 const restaurantDataList = new RestaurantDataList();
 export default restaurantDataList;
