37c0b886cd5ff405329821aa5d326021b7191dc5
dev-dino22 <wlsskdud02@naver.com>
2025-03-23 21:52:30 +0900

    refactor: TabMenu data-action의 } 오타 수정

--- src/components/common/TabMenu.js
+++ src/components/common/TabMenu.js
@@ -5,7 +5,7 @@ export function TabMenu() {
 <div class="tab-container">
   <div class="tab-menu">
     <!-- <div class="tab-menu-box"> -->
-        <button type="button" value="0" class="tab-item active" data-action=${ClickActions.SELECT_TAB}}>모든 음식점</button>
+        <button type="button" value="0" class="tab-item active" data-action=${ClickActions.SELECT_TAB}>모든 음식점</button>
         <button type="button" value="1" class="tab-item" data-action=${ClickActions.SELECT_TAB}>자주 가는 음식점</button>
     </div>
     <div class="tab-indicator">
