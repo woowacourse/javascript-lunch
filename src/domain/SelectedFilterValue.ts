2189ca2faa1f898cef515ea234e97fbfdb5922f7
kaori-killer <sksbthwjd@daum.net>
2025-03-23 22:56:57 +0900

    refactor: as 대신 다른 구문으로 타입 검사

--- src/domain/SelectedFilterValue.ts
+++ src/domain/SelectedFilterValue.ts
@@ -10,16 +10,16 @@ class SelectedFilterValue {
     }
 
     updateSelectedFilterValue(id: string, value: string | boolean): void {
-        if (id === "category-filter") {
-            this.#category = value as string;
+        if (id === "category-filter" && typeof value === "string") {
+            this.#category = value;
         }
 
-        if (id === "sorting-filter") {
-            this.#sorting = value as string;
+        if (id === "sorting-filter" && typeof value === "string") {
+            this.#sorting = value;
         }
 
-        if (id === "restaurant-tab") {
-            this.#isWishList = value as boolean;
+        if (id === "restaurant-tab" && typeof value === "boolean") {
+            this.#isWishList = value;
         }
     }
 
