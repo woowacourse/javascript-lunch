78f01b788bc73b0561fa0e15aa45684fef4ab2f5
youjeong Jeong <wjddbwjd1205@naver.com>
2025-03-23 21:30:03 +0900

    refactor: 즐겨찾기 아이콘에 alt 속성 추가로 접근성 향상

--- src/components/FavoriteStar.js
+++ src/components/FavoriteStar.js
@@ -1,6 +1,6 @@
 function createFavoriteStar(fieldName) {
   const dataIdAttr = fieldName.dataId ? `data-id="${fieldName.dataId}"` : '';
-  const favoriteStar = `<img src="${fieldName.inactiveSrc}" class="${fieldName.className}" ${dataIdAttr}>`;
+  const favoriteStar = `<img src="${fieldName.inactiveSrc}" class="${fieldName.className}" ${dataIdAttr} alt="자주 가는 음식점 아이콘">`;
   return favoriteStar;
 }
 
