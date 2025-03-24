f98c2deac412f4ca3f71a6fde78d130fbe428a2b
deokbin <deokbin@naver.com>
2025-03-24 11:30:15 +0900

    remove: 불필요 코드 제거

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,20 +2,3 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.svg";
 declare module "*.css";
-
-export interface Restaurant {
-  imgSrc: string;
-  imgAlt: string;
-  name: string;
-  distance: number;
-  description: string;
-  category: string;
-  link: string;
-  id: string;
-}
-
-export type isModalOpen = boolean;
-
-export interface Categories {
-  [key: string]: string;
-}
