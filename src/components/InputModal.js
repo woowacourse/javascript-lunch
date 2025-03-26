dcea908f25a7a57ef171124ca5ffa346edfce142
sanghee01 <sang6299@naver.com>
2025-03-26 13:17:50 +0900

    chore: 이미지 경로 수정

--- src/components/InputModal.js
+++ src/components/InputModal.js
@@ -6,11 +6,11 @@ import { inputBoxList } from '../config/InputBoxList.js';
 
 class InputModal extends Component {
   getModalInput(modalForm) {
-    const baseURL = window.location.origin.includes('github.io') ? '/javascript-lunch' : '';
+    const baseURL = window.location.origin.includes('github.io') ? '/javascript-lunch' : '../..';
     const formData = new FormData(modalForm);
     const modalInput = {
       id: Date.now(),
-      imgUrl: `../..${baseURL}/public/images/category-${FOOD_CATEGORY[formData.get(INPUT_FIELDS.CATEGORY)]}.png`,
+      imgUrl: `${baseURL}/public/images/category-${FOOD_CATEGORY[formData.get(INPUT_FIELDS.CATEGORY)]}.png`,
       category: formData.get(INPUT_FIELDS.CATEGORY),
       name: formData.get(INPUT_FIELDS.NAME),
       distance: formData.get(INPUT_FIELDS.DISTANCE),
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -7,34 +7,34 @@ class Restaurant extends Component {
   }
 
   template() {
-    const baseURL = window.location.origin.includes('github.io') ? '/javascript-lunch' : '';
+    const baseURL = window.location.origin.includes('github.io') ? '/javascript-lunch' : '../..';
 
     return ` 
-      <div class="restaurant__category">
-        <img
-          src=".${baseURL}/public/images/${this.props.imgUrl}"
-          alt=${this.props.category}
-          class="category-icon"
-        />
+  <div class="restaurant__category">
+    <img
+      src="${baseURL}/public/images/${this.props.imgUrl}"
+      alt="${this.props.category}"
+      class="category-icon"
+    />
+  </div>
+  <div class="restaurant__info">
+    <div class="restaurant__info--header">
+      <div>
+        <h3 class="restausrant__name text-subtitle">${this.props.name}</h3>
+        <span class="restaurant__distance text-body">
+          캠퍼스로부터 ${this.props.distance}분 내
+        </span>
       </div>
-      <div class="restaurant__info">
-        <div class="restaurant__info--header">
-          <div>
-            <h3 class="restausrant__name text-subtitle">${this.props.name}</h3>
-            <span class="restaurant__distance text-body"
-              >캠퍼스로부터 ${this.props.distance}분 내</span
-            >
-          </div>
-          <img class="restaurant__favorite" src="/images/favorite-icon-${
-            this.props.favorite ? 'filled' : 'lined'
-          }.png" alt="favorite" />
-        </div>
-        <div>
-        <p class="restaurant__description text-body">
-          ${this.props.description}
-        </p>
-      </div>
-  `;
+      <img class="restaurant__favorite" src="${baseURL}/images/favorite-icon-${
+      this.props.favorite ? 'filled' : 'lined'
+    }.png" alt="favorite" />
+    </div>
+    <div>
+      <p class="restaurant__description text-body">
+        ${this.props.description}
+      </p>
+    </div>
+`;
   }
 
   onRender() {
